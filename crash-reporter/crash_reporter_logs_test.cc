39ae7f461b57c8d98b01f63e48ed7b45f80768ac
Mike Frysinger <vapier@chromium.org>
2018-07-19 12:12:05 -0400

    crash: verify the config file is valid shell
    Add a unittest to make sure all entries in the config file are valid
    shell scripts.  Otherwise the only way to check is indirectly when the
    specific program crashes, and not everyone writes integration tests.
    
    BUG=None
    TEST=unittest passes
    
    Change-Id: I6b8da73c34331f33e7d5783cd0b1ec7ca2e212e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1143680
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>

--- crash-reporter/crash_reporter_logs_test.cc
+++ crash-reporter/crash_reporter_logs_test.cc
@@ -5,7 +5,9 @@
 #include <string>
 
 #include <base/files/file_path.h>
+#include <base/files/file_util.h>
 #include <brillo/key_value_store.h>
+#include <brillo/process/process.h>
 #include <gtest/gtest.h>
 
 #include "crash-reporter/test_util.h"
@@ -29,4 +31,27 @@ TEST(CrashReporterLogsTest, ReadConfig) {
   std::string command;
   EXPECT_TRUE(store.GetString(kChromeExecName, &command));
   EXPECT_FALSE(command.empty());
+}
+
+// Verify that all entries are valid shell.
+TEST(CrashReporterLogsTest, ValidShell) {
+  brillo::KeyValueStore store;
+  ASSERT_TRUE(store.Load(
+      test_util::GetTestDataPath(kLogConfigFileName, /*use_testdata=*/false)));
+
+  for (const auto& key : store.GetKeys()) {
+    std::string command;
+    ASSERT_TRUE(store.GetString(key, &command));
+    brillo::ProcessImpl diag_process;
+    diag_process.AddArg("/bin/sh");
+    diag_process.AddArg("-n");
+    // While bash supports `-n -c ...` as one expects, dash ignores -n and tries
+    // to actually execute the command.  Both respect -n when using stdin.
+    diag_process.RedirectUsingPipe(STDIN_FILENO, true);
+    ASSERT_TRUE(diag_process.Start());
+    int stdin_fd = diag_process.GetPipe(STDIN_FILENO);
+    ASSERT_TRUE(base::WriteFileDescriptor(stdin_fd, command));
+    ASSERT_GE(close(stdin_fd), 0);
+    EXPECT_EQ(0, diag_process.Wait());
+  }
 }
