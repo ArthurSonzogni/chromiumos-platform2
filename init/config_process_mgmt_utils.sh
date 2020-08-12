9ca474984df118fec30b61e9838111ff42fa79f8
Thomas Cedeno <thomascedeno@google.com>
2020-08-12 17:30:23 +0000

    init: update init script configuration SafeSetID
    To support changes made to the SafeSetID LSM in kernel 5.9, update
    the existing scripts to work with the future version once it lands
    
    BUG=chromium:1077756
    TEST=test_that localhost:9222 security_ProcessManagementPolicy
    tast -verbose run root@localhost:9222 security.SafesetidEnsurePolicy
    
    Change-Id: I8e55efcf65013c33ddbb0ee4edc5b1217690fdd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2353034
    Tested-by: Thomas Cedeno <thomascedeno@google.com>
    Commit-Queue: Thomas Cedeno <thomascedeno@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- init/config_process_mgmt_utils.sh
+++ init/config_process_mgmt_utils.sh
@@ -19,17 +19,23 @@ SAFESETID_PROCESS_MGMT_POLICIES="/sys/kernel/security/safesetid\
 /whitelist_policy"
 
 # Path to the securityfs file for configuring process management security
-# policies, for UIDs, in the SafeSetID LSM (used for kernel version >= 5.9).
-SAFESETID_PROCESS_UID_MGMT_POLICIES="/sys/kernel/security/safesetid\
+# policies in the SafeSetID LSM (used for kernel version >= 5.9).
+SAFESETID_PROCESS_MGMT_POLICIES_UID="/sys/kernel/security/safesetid\
 /uid_allowlist_policy"
+
+# Path to securityfs file for configuring GID policies in the SafeSetID
+# LSM. (only used/tested in kernels >= 5.9)
+SAFESETID_PROCESS_MGMT_POLICIES_GID="/sys/kernel/security/safesetid\
+/gid_allowlist_policy"
 
 # Project-specific process management policies. Projects may add policies by
 # adding a file under /usr/share/cros/startup/process_management_policies/
-# whose contents are one or more lines specifying a parent UID and a child UID
-# that the parent can use for the purposes of process management. There should
-# be one line for every UID-to-UID mapping that is to be put in the whitelist.
-# Lines in the file should use the following format:
-# <UID>:<UID>
+# for UID's and under /usr/share/cros/startup/gid_process_management_policies/
+# for GID's, whose contents are one or more lines specifying a parent ID and a
+# child ID that the parent can use for the purposes of process management.
+# There should be one line for every mapping that is to be put in the
+# allowlist. Lines in the file should use the following format:
+# <UID>:<UID> or <GID>:<GID>
 #
 # For example, if the 'shill' user needs to use 'dhcp', 'openvpn' and 'ipsec'
 # and 'syslog' for process management, the file would look like:
@@ -37,10 +43,16 @@ SAFESETID_PROCESS_UID_MGMT_POLICIES="/sys/kernel/security/safesetid\
 # 20104:217
 # 20104:212
 # 20104:202
-configure_process_mgmt_security() {
+
+# _accumulate_policy_files takes in all the files contained in "${filepaths}"
+# reads their contents, copies and appends them to a file determined by
+# "${output}"
+_accumulate_policy_files() {
   local file policy
   local accum=""
-  for file in /usr/share/cros/startup/process_management_policies/*; do
+  local output="$1"
+  shift
+  for file in "$@"; do
     if [ -f "${file}" ]; then
       while read -r policy; do
         case "${policy}" in
@@ -48,9 +60,13 @@ configure_process_mgmt_security() {
         "") ;;
         # Ignore comments.
         "#"*) ;;
+        # Don't record GID policies into CHROMIUMOS_PROCESS_MGMT_POLICIES.
         *)
           if [ -e "${CHROMIUMOS_PROCESS_MGMT_POLICIES}" ]; then
-            printf "%s" "${policy}" > "${CHROMIUMOS_PROCESS_MGMT_POLICIES}"
+            if [ "${output}" != \
+                  "${SAFESETID_PROCESS_MGMT_POLICIES_GID}" ]; then
+              printf "%s" "${policy}" > "${CHROMIUMOS_PROCESS_MGMT_POLICIES}"
+            fi
           else
             accum="${accum}${policy}\n"
           fi
@@ -59,10 +75,27 @@ configure_process_mgmt_security() {
       done < "${file}"
     fi
   done
+  printf "%b" "${accum}" > "${output}"
+}
 
-  if [ -e "${SAFESETID_PROCESS_UID_MGMT_POLICIES}" ]; then
-    printf "%b" "${accum}" > "${SAFESETID_PROCESS_UID_MGMT_POLICIES}"
+# Determine where securityfs files are placed.
+# No inputs, checks for which securityfs file paths
+# exist and accumulates files for securityfs.
+configure_process_mgmt_security() {
+  # For UID relevant files.
+  if [ -e "${SAFESETID_PROCESS_MGMT_POLICIES_UID}" ]; then
+    _accumulate_policy_files \
+      "${SAFESETID_PROCESS_MGMT_POLICIES_UID}" \
+      /usr/share/cros/startup/process_management_policies/*
   elif [ -e "${SAFESETID_PROCESS_MGMT_POLICIES}" ]; then
-    printf "%b" "${accum}" > "${SAFESETID_PROCESS_MGMT_POLICIES}"
+    _accumulate_policy_files \
+      "${SAFESETID_PROCESS_MGMT_POLICIES}" \
+      /usr/share/cros/startup/process_management_policies/*
+  fi
+  # For GID relevant files.
+  if [ -e "${SAFESETID_PROCESS_MGMT_POLICIES_GID}" ]; then
+    _accumulate_policy_files \
+      "${SAFESETID_PROCESS_MGMT_POLICIES_GID}" \
+      /usr/share/cros/startup/gid_process_management_policies/*
   fi
 }
