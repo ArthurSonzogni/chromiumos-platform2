23cd638121e67215583ecb41b3269a65cfece348
Kevin Lin <kevinptt@chromium.org>
2021-01-18 13:31:07 +0800

    runtime_probe: fix input_device with i2c touchscreen
    input_device function could not get correct attributes, vendor ID for
    example, from procfs on some i2c touchscreen.  Therefore, this CL makes
    input_device obtain these attributes from sysfs manually.
    
    This CL also refactors input_device with some syntactic sugar in C++17.
    
    BUG=b:173091191
    TEST=test on DROID with touchscreen with raydium_ts driver.
    
    Change-Id: Icd956ea431445ef314b6b47f3904f092f424fac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2634887
    Tested-by: Kevin Lin <kevinptt@chromium.org>
    Reviewed-by: Clark Chung <ckclark@chromium.org>
    Commit-Queue: Kevin Lin <kevinptt@chromium.org>

--- runtime_probe/functions/input_device.cc
+++ runtime_probe/functions/input_device.cc
@@ -4,23 +4,42 @@
 
 #include "runtime_probe/functions/input_device.h"
 
+#include <map>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include <base/files/file_util.h>
-#include <base/json/json_reader.h>
 #include <base/json/json_writer.h>
 #include <base/strings/string_split.h>
 #include <base/strings/string_util.h>
+#include <base/strings/stringprintf.h>
 #include <pcrecpp.h>
 
-#include "runtime_probe/utils/value_utils.h"
+#include "runtime_probe/utils/file_utils.h"
 
 namespace runtime_probe {
 
 namespace {
 constexpr auto kInputDevicesPath = "/proc/bus/input/devices";
 const pcrecpp::RE kEventPatternRe(R"(event[\d]+)");
+
+using FieldType = std::pair<std::string, std::string>;
+
+const std::vector<FieldType> kTouchscreenI2cFields = {
+    {"name", "name"}, {"product", "hw_version"}, {"fw_version", "fw_version"}};
+const std::map<std::string, std::string> kTouchscreenI2cDriverToVid = {
+    {"elants_i2c", "04f3"}, {"raydium_ts", "27a3"}, {"atmel_ext_ts", "03eb"}};
+
+void AppendInputDevice(base::Value* list_value, base::Value&& value) {
+  const auto* sysfs = value.FindStringKey("sysfs");
+  if (sysfs) {
+    const auto path = base::StringPrintf("/sys%s", sysfs->c_str());
+    value.RemoveKey("sysfs");
+    value.SetStringKey("path", path);
+  }
+  list_value->Append(std::move(value));
+}
 
 base::Value LoadInputDevices() {
   base::Value results(base::Value::Type::LIST);
@@ -31,46 +50,48 @@ base::Value LoadInputDevices() {
     return results;
   }
 
-  base::Value data(base::Value::Type::DICTIONARY);
+  base::Value data;
   auto input_devices_lines = base::SplitStringPiece(
       input_devices_str, "\n", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
   for (const auto& line : input_devices_lines) {
-    if (line.length() > 0) {
-      const auto prefix = line[0];
-      const auto& content = line.substr(3);
-      if (prefix == 'I') {
-        if (!data.DictEmpty()) {
-          RenameKey(&data, "sysfs", "path");
-          results.Append(std::move(data));
-          data = base::Value(base::Value::Type::DICTIONARY);
+    if (line.length() < 3) {
+      DCHECK_EQ(line.length(), 0);
+      continue;
+    }
+    const auto& content = line.substr(3);
+    base::StringPairs keyVals;
+    switch (const auto prefix = line[0]; prefix) {
+      case 'I': {
+        if (data.is_dict() && !data.DictEmpty()) {
+          AppendInputDevice(&results, std::move(data));
         }
-        base::StringPairs keyVals;
         if (!base::SplitStringIntoKeyValuePairs(content, '=', ' ', &keyVals)) {
           LOG(ERROR) << "Failed to parse input devices (I).";
           return base::Value(base::Value::Type::LIST);
         }
-        for (const auto& keyVal : keyVals) {
-          base::StringPiece key, value;
-          std::tie(key, value) = keyVal;
+        data = base::Value(base::Value::Type::DICTIONARY);
+        for (const auto& [key, value] : keyVals) {
           data.SetStringKey(base::ToLowerASCII(key), value);
         }
-      } else if (prefix == 'N' || prefix == 'S') {
-        base::StringPairs keyVals;
+        break;
+      }
+      case 'N':
+      case 'S': {
         if (!base::SplitStringIntoKeyValuePairs(content, '=', '\n', &keyVals)) {
           LOG(ERROR) << "Failed to parse input devices (N/S).";
           return base::Value(base::Value::Type::LIST);
         }
-        base::StringPiece key, value;
-        std::tie(key, value) = keyVals[0];
+        const auto& [key, value] = keyVals[0];
         data.SetStringKey(base::ToLowerASCII(key),
                           base::TrimString(value, "\"", base::TRIM_ALL));
-      } else if (prefix == 'H') {
-        base::StringPairs keyVals;
+        break;
+      }
+      case 'H': {
         if (!base::SplitStringIntoKeyValuePairs(content, '=', '\n', &keyVals)) {
           LOG(ERROR) << "Failed to parse input devices (H).";
           return base::Value(base::Value::Type::LIST);
         }
-        base::StringPiece value = keyVals[0].second;
+        const auto& value = keyVals[0].second;
         const auto& handlers = base::SplitStringPiece(
             value, " ", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
         for (const auto& handler : handlers) {
@@ -79,14 +100,55 @@ base::Value LoadInputDevices() {
             break;
           }
         }
+        break;
+      }
+      default: {
+        DVLOG(1) << "Unknown prefix '" << prefix << "'. line: " << line;
+        break;
       }
     }
   }
   if (!data.DictEmpty()) {
-    RenameKey(&data, "sysfs", "path");
-    results.Append(std::move(data));
+    AppendInputDevice(&results, std::move(data));
   }
   return results;
+}
+
+std::string GetDriverName(const base::FilePath& node_path) {
+  const auto driver_path = node_path.Append("driver");
+  const auto real_driver_path = base::MakeAbsoluteFilePath(driver_path);
+  if (real_driver_path.value().length() == 0)
+    return "";
+  const auto driver_name = real_driver_path.BaseName().value();
+  return driver_name;
+}
+
+void FixTouchscreenI2cDevices(base::Value* devices) {
+  for (auto& device : devices->GetList()) {
+    const auto* path = device.FindStringKey("path");
+    if (!path)
+      continue;
+
+    if (const auto* vid_old = device.FindStringKey("vendor");
+        vid_old && *vid_old != "0000")
+      continue;
+
+    const auto node_path = base::FilePath{*path}.Append("device");
+    const auto driver_name = GetDriverName(node_path);
+    const auto entry = kTouchscreenI2cDriverToVid.find(driver_name);
+    if (entry == kTouchscreenI2cDriverToVid.end())
+      continue;
+
+    auto dict_value = MapFilesToDict(node_path, kTouchscreenI2cFields, {});
+    if (!dict_value) {
+      DVLOG(1) << "touchscreen_i2c-specific fields do not exist on node \""
+               << node_path << "\"";
+      continue;
+    }
+
+    device.SetStringKey("vendor", entry->second);
+    device.MergeDictionary(&*dict_value);
+  }
 }
 
 }  // namespace
@@ -107,6 +169,7 @@ InputDeviceFunction::DataType InputDeviceFunction::Eval() const {
 
 int InputDeviceFunction::EvalInHelper(std::string* output) const {
   auto results = LoadInputDevices();
+  FixTouchscreenI2cDevices(&results);
 
   if (!base::JSONWriter::Write(results, output)) {
     LOG(ERROR) << "Failed to serialize usb probed result to json string";
