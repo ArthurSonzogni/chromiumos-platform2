854afc82ac3d45787c372da7ee3ad6a3a580391d
Prashant Malani <pmalani@chromium.org>
2021-01-19 12:54:56 -0800

    typecd: Add active user mode entry unit tests
    Update the PortManagerTest SimpleModeEntry test to check for various
    "active" user hotplug mode entry cases i.e TBT supported, and both DP and TBT
    supported.
    
    BUG=b:168653671
    TEST=All unit tests pass.
    
    Change-Id: If01bd48ae098655f9d0f6a3264d34cb5fea5937a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2638415
    Reviewed-by: Mengqi Guo <mqg@chromium.org>
    Reviewed-by: Prashant Malani <pmalani@chromium.org>
    Tested-by: Prashant Malani <pmalani@chromium.org>
    Commit-Queue: Prashant Malani <pmalani@chromium.org>

--- typecd/port_manager_test.cc
+++ typecd/port_manager_test.cc
@@ -42,9 +42,17 @@ TEST_F(PortManagerTest, ModeEntryNotSupported) {
   // met.
 }
 
-// Test the basic case of DP mode entry.
+// Test the basic case of "active" user hotplug mode entry for the following
+// scenarios:
+// - Only DP supported.
+// - Only TBT supported.
+// - Both DP & TBT supported.
 TEST_F(PortManagerTest, SimpleModeEntry) {
   auto port_manager = std::make_unique<PortManager>();
+
+  // Since we only have a MockECUtil, just force the |mode_entry_supported_|
+  // flag.
+  port_manager->SetModeEntrySupported(true);
 
   // Create the MockECUtil and set the expectations (enter DP called once).
   auto ec_util = std::make_unique<MockECUtil>();
@@ -69,16 +77,70 @@ TEST_F(PortManagerTest, SimpleModeEntry) {
   port_manager->ports_.insert(
       std::pair<int, std::unique_ptr<Port>>(0, std::move(port)));
 
-  // Since we only have a MockECUtil, just force the |mode_entry_supported_|
-  // flag.
-  port_manager->SetModeEntrySupported(true);
-
-  // For one-shot entry, assume that the user is active.
+  // Assume that the user is active.
   port_manager->SetUserActive(true);
 
+  // Simulate a hotplug.
   port_manager->RunModeEntry(0);
 
-  // There is no explicit test here, just that the Mock expectations should be
+  // Update the MockECUtil to check for TBT entry.
+  ec_util = std::make_unique<MockECUtil>();
+  EXPECT_CALL(*ec_util, ModeEntrySupported()).Times(0);
+  EXPECT_CALL(*ec_util, EnterMode(0, TypeCMode::kTBT))
+      .WillOnce(testing::Return(true));
+  EXPECT_CALL(*ec_util, ExitMode(_)).Times(0);
+  port_manager->SetECUtil(ec_util.get());
+
+  // Replace with a fake port that supports only TBT.
+  port_manager->ports_.erase(0);
+  port = std::make_unique<MockPort>(base::FilePath("fakepath"), 0);
+  EXPECT_CALL(*port, GetDataRole())
+      .WillRepeatedly(testing::Return(std::string("host")));
+  EXPECT_CALL(*port, IsPartnerDiscoveryComplete())
+      .WillRepeatedly(testing::Return(true));
+  EXPECT_CALL(*port, IsCableDiscoveryComplete())
+      .WillRepeatedly(testing::Return(true));
+  EXPECT_CALL(*port, CanEnterUSB4()).WillRepeatedly(testing::Return(false));
+  EXPECT_CALL(*port, CanEnterTBTCompatibilityMode())
+      .WillRepeatedly(testing::Return(true));
+  EXPECT_CALL(*port, CanEnterDPAltMode())
+      .WillRepeatedly(testing::Return(false));
+  port_manager->ports_.insert(
+      std::pair<int, std::unique_ptr<Port>>(0, std::move(port)));
+
+  // Simulate a hotplug.
+  port_manager->RunModeEntry(0);
+
+  // Update the MockECUtil to check for TBT entry again.
+  // NOTE: If both DP & TBT are supported, and this is unlocked hotplug, then
+  // TBT should be picked.
+  ec_util = std::make_unique<MockECUtil>();
+  EXPECT_CALL(*ec_util, ModeEntrySupported()).Times(0);
+  EXPECT_CALL(*ec_util, EnterMode(0, TypeCMode::kTBT))
+      .WillOnce(testing::Return(true));
+  EXPECT_CALL(*ec_util, ExitMode(_)).Times(0);
+  port_manager->SetECUtil(ec_util.get());
+
+  // Replace with a fake port that supports both DP & TBT.
+  port_manager->ports_.erase(0);
+  port = std::make_unique<MockPort>(base::FilePath("fakepath"), 0);
+  EXPECT_CALL(*port, GetDataRole())
+      .WillRepeatedly(testing::Return(std::string("host")));
+  EXPECT_CALL(*port, IsPartnerDiscoveryComplete())
+      .WillRepeatedly(testing::Return(true));
+  EXPECT_CALL(*port, IsCableDiscoveryComplete())
+      .WillRepeatedly(testing::Return(true));
+  EXPECT_CALL(*port, CanEnterUSB4()).WillRepeatedly(testing::Return(false));
+  EXPECT_CALL(*port, CanEnterTBTCompatibilityMode())
+      .WillRepeatedly(testing::Return(true));
+  EXPECT_CALL(*port, CanEnterDPAltMode()).WillRepeatedly(testing::Return(true));
+  port_manager->ports_.insert(
+      std::pair<int, std::unique_ptr<Port>>(0, std::move(port)));
+
+  // Simulate a hotplug.
+  port_manager->RunModeEntry(0);
+
+  // There is no explicit test here, just that the mock expectations should be
   // met.
 }
 
