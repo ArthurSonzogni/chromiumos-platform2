5ac2f2c55f7f7e8b3a0101c6725f9a005583e2b2
John L Chen <zuan@chromium.org>
2021-01-19 18:13:49 +0800

    cryptohome: Remove option to start legacy service in cryptohomed.cc
    BUG=b:177880754
    TEST=tast run $dut hwsec.*
    
    Change-Id: Iba345ca55f42ea8d51c342fbce178be485703601
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2637142
    Tested-by: John L Chen <zuan@chromium.org>
    Reviewed-by: Leo Lai <cylai@google.com>
    Commit-Queue: John L Chen <zuan@chromium.org>

--- cryptohome/cryptohomed.cc
+++ cryptohome/cryptohomed.cc
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 #include "cryptohome/dbus_service.h"
-#include "cryptohome/service.h"
 
 #include <cstdlib>
 #include <string>
@@ -17,8 +16,6 @@
 #include <base/strings/string_number_conversions.h>
 #include <chaps/pkcs11/cryptoki.h>
 #include <brillo/syslog_logging.h>
-#include <dbus/dbus.h>
-#include <glib.h>
 #include <openssl/evp.h>
 
 #include "cryptohome/cryptohome_metrics.h"
@@ -35,7 +32,6 @@ static const char* kNoLegacyMount = "nolegacymount";
 static const char* kNoDownloadsBindMount = "no_downloads_bind_mount";
 static const char* kDirEncryption = "direncryption";
 static const char* kNoDaemonize = "nodaemonize";
-static const char* kUserDataAuthInterface = "user_data_auth_interface";
 static const char* kCleanupThreshold = "cleanup_threshold";
 static const char* kAggressiveThreshold = "aggressive_cleanup_threshold";
 static const char* kTargetFreeSpace = "target_free_space";
@@ -86,7 +82,6 @@ int main(int argc, char** argv) {
   bool nodownloadsbind = cl->HasSwitch(switches::kNoDownloadsBindMount);
   bool direncryption = cl->HasSwitch(switches::kDirEncryption);
   bool daemonize = !cl->HasSwitch(switches::kNoDaemonize);
-  bool use_new_dbus_interface = cl->HasSwitch(switches::kUserDataAuthInterface);
   uint64_t cleanup_threshold =
       ReadCleanupThreshold(cl, switches::kCleanupThreshold,
                            cryptohome::kFreeSpaceThresholdToTriggerCleanup);
@@ -111,76 +106,41 @@ int main(int argc, char** argv) {
   // Make sure scrypt parameters are correct.
   cryptohome::CryptoLib::AssertProductionScryptParams();
 
-  if (use_new_dbus_interface) {
-    // Note that there's an AtExitManager in the constructor of
-    // UserDataAuthDaemon
-    cryptohome::UserDataAuthDaemon user_data_auth_daemon;
+  // Note that there's an AtExitManager in the constructor of
+  // UserDataAuthDaemon
+  cryptohome::UserDataAuthDaemon user_data_auth_daemon;
 
-    // Set options on whether we are going to use legacy mount. See comments on
-    // Mount::MountLegacyHome() for more information.
-    user_data_auth_daemon.GetUserDataAuth()->set_legacy_mount(!nolegacymount);
-    user_data_auth_daemon.GetUserDataAuth()->set_bind_mount_downloads(
-        !nodownloadsbind);
+  // Set options on whether we are going to use legacy mount. See comments on
+  // Mount::MountLegacyHome() for more information.
+  user_data_auth_daemon.GetUserDataAuth()->set_legacy_mount(!nolegacymount);
+  user_data_auth_daemon.GetUserDataAuth()->set_bind_mount_downloads(
+      !nodownloadsbind);
 
-    // Set options on whether we are going to use ext4 directory encryption or
-    // eCryptfs.
-    user_data_auth_daemon.GetUserDataAuth()->set_force_ecryptfs(!direncryption);
+  // Set options on whether we are going to use ext4 directory encryption or
+  // eCryptfs.
+  user_data_auth_daemon.GetUserDataAuth()->set_force_ecryptfs(!direncryption);
 
-    // Set automatic cleanup thresholds.
-    user_data_auth_daemon.GetUserDataAuth()->set_cleanup_threshold(
-        cleanup_threshold);
-    user_data_auth_daemon.GetUserDataAuth()->set_aggressive_cleanup_threshold(
-        aggressive_cleanup_threshold);
-    user_data_auth_daemon.GetUserDataAuth()->set_target_free_space(
-        target_free_space);
+  // Set automatic cleanup thresholds.
+  user_data_auth_daemon.GetUserDataAuth()->set_cleanup_threshold(
+      cleanup_threshold);
+  user_data_auth_daemon.GetUserDataAuth()->set_aggressive_cleanup_threshold(
+      aggressive_cleanup_threshold);
+  user_data_auth_daemon.GetUserDataAuth()->set_target_free_space(
+      target_free_space);
 
-    // Note the startup sequence is as following:
-    // 1. UserDataAuthDaemon constructor => UserDataAuth constructor
-    // 2. UserDataAuthDaemon::OnInit() (called by Daemon::Run())
-    // 3. UserDataAuthDaemon::RegisterDBusObjectsAsync() (called by 2.)
-    // 4. UserDataAuth::Initialize() (called by 3.)
-    // 5. UserDataAuth::PostDBusInitialize() (called by 3.)
-    // Daemon::OnInit() needs to be called before Initialize(), because
-    // Initialize() create threads, and thus mess with Daemon's
-    // AsynchronousSignalHandler.
+  // Note the startup sequence is as following:
+  // 1. UserDataAuthDaemon constructor => UserDataAuth constructor
+  // 2. UserDataAuthDaemon::OnInit() (called by Daemon::Run())
+  // 3. UserDataAuthDaemon::RegisterDBusObjectsAsync() (called by 2.)
+  // 4. UserDataAuth::Initialize() (called by 3.)
+  // 5. UserDataAuth::PostDBusInitialize() (called by 3.)
+  // Daemon::OnInit() needs to be called before Initialize(), because
+  // Initialize() create threads, and thus mess with Daemon's
+  // AsynchronousSignalHandler.
 
-    // Start UserDataAuth daemon if the option is selected
-    user_data_auth_daemon.Run();
-  } else {
-    // Start the old interface if nothing is selected
+  // Start UserDataAuth daemon if the option is selected
+  user_data_auth_daemon.Run();
 
-    // Setup threading. This needs to be called before other calls into glib and
-    // before multiple threads are created that access dbus.
-    dbus_threads_init_default();
-
-    // Create an AtExitManager
-    base::AtExitManager exit_manager;
-
-    cryptohome::Service* service = cryptohome::Service::CreateDefault();
-
-    service->set_legacy_mount(!nolegacymount);
-    service->set_bind_mount_downloads(!nodownloadsbind);
-    service->set_force_ecryptfs(!direncryption);
-
-    if (!service->Initialize()) {
-      LOG(FATAL) << "Service initialization failed";
-      return 1;
-    }
-
-    service->set_cleanup_threshold(cleanup_threshold);
-    service->set_aggressive_cleanup_threshold(aggressive_cleanup_threshold);
-    service->set_target_free_space(target_free_space);
-
-    if (!service->Register(brillo::dbus::GetSystemBusConnection())) {
-      LOG(FATAL) << "DBUS service registration failed";
-      return 1;
-    }
-
-    if (!service->Run()) {
-      LOG(FATAL) << "Service run failed.";
-      return 1;
-    }
-  }
   // If PKCS #11 was initialized, this will tear it down.
   C_Finalize(NULL);
 
