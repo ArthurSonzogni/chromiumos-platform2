b12c367a4fb60086dd220848e64dbc1036162943
Willie Koomson <wvk@google.com>
2021-01-25 15:02:29 -0800

    arc: arcvm-forward-pstore copies pstore back to /run/arcvm
    This change modifies arcvm-forward-pstore to copy the pstore file
    back to /run/arcvm/arcvm.pstore before ARCVM restarts. This allows
    the guest to generate /sys/fs/pstore/dmesg-ramoops-0 on next boot.
    
    This change also modifies the lifecycle of this service: instead of
    stopping on 'stopping arcvm-pre-login-services', it now persists across
    ARCVM restarts in a single session. This is so that it can delete
    /run/arcvm/arcvm.pstore on exit to prevent it from leaking out of the
    user session.
    
    BUG=b:177626889
    BUG=b:177682644
    TEST=tast run <DUT> arc.KernelCrash.mock_consent
    
    Change-Id: Id6dbe007125a216d4e54cabe87690cc88f185cba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2648254
    Tested-by: Willie Koomson <wvk@google.com>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Kimiyuki Onaka <kimiyuki@google.com>
    Commit-Queue: Willie Koomson <wvk@google.com>

--- arc/vm/forward-pstore/service.h
+++ arc/vm/forward-pstore/service.h
@@ -5,9 +5,12 @@
 #ifndef ARC_VM_FORWARD_PSTORE_SERVICE_H_
 #define ARC_VM_FORWARD_PSTORE_SERVICE_H_
 
+#include <memory>
 #include <string>
 
 #include <base/callback_forward.h>
+#include <base/files/scoped_file.h>
+#include <base/files/file_descriptor_watcher_posix.h>
 #include <base/memory/scoped_refptr.h>
 #include <base/memory/weak_ptr.h>
 #include <base/timer/timer.h>
@@ -27,19 +30,25 @@ class Service {
   void Start();
 
  private:
-  void OnSignalConnected(const std::string& interface_name,
-                         const std::string& signal_name,
-                         bool is_connected);
-  void OnVmStoppedSignal(dbus::Signal* signal);
+  void OnDbusSignalConnected(const std::string& interface_name,
+                             const std::string& signal_name,
+                             bool is_connected);
+
+  void HandleSigterm();
+
   void OnVmIdChangedSignal(dbus::Signal* signal);
+  void OnVmStoppedSignal(dbus::Signal* signal);
 
   void ForwardPstore(const std::string& owner_id);
   void ForwardContents(const std::string& owner_id);
+  void CopyPstoreToSourcePath(const std::string& owner_id);
 
   scoped_refptr<dbus::Bus> bus_;
   brillo::SafeFD root_fd_;
   brillo::SafeFD pstore_fd_;
   brillo::SafeFD dest_fd_;
+  base::ScopedFD signal_fd_;
+  std::unique_ptr<base::FileDescriptorWatcher::Controller> watcher_;
   base::Closure quit_closure_;
   base::RepeatingTimer timer_;
   base::WeakPtrFactory<Service> weak_ptr_factory_;
