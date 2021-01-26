c8760e7df3f8923199a58e279b0e0c3c86dd64f7
Manoj Gupta <manojgupta@chromium.org>
2021-01-26 18:59:28 +0000

    vm_tools: garcon: Fix ini_parse_util_fuzzer
    Use separate string variables so that any StringPiece
    API usages do not run into invalidation of memory storage.
    
    BUG=chromium:1170615
    TEST=local test
    
    Change-Id: Id424b315f6da7172263d37d04c473192bf591489
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2648839
    Tested-by: Manoj Gupta <manojgupta@chromium.org>
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>

--- vm_tools/garcon/ini_parse_util_fuzzer.cc
+++ vm_tools/garcon/ini_parse_util_fuzzer.cc
@@ -23,19 +23,23 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   std::string str =
       data_provider.ConsumeRandomLengthString(kRandomStringLength);
   vm_tools::garcon::ParseGroupName(str);
-  str = data_provider.ConsumeRandomLengthString(kRandomStringLength);
-  vm_tools::garcon::ExtractKeyLocale(str);
-  str = data_provider.ConsumeRandomLengthString(kRandomStringLength);
-  vm_tools::garcon::ExtractKeyValuePair(str);
-  str = data_provider.ConsumeRandomLengthString(kRandomStringLength);
-  vm_tools::garcon::UnescapeString(str);
-  str = data_provider.ConsumeRandomLengthString(kRandomStringLength);
+  std::string locale =
+      data_provider.ConsumeRandomLengthString(kRandomStringLength);
+  vm_tools::garcon::ExtractKeyLocale(locale);
+  std::string keyval =
+      data_provider.ConsumeRandomLengthString(kRandomStringLength);
+  vm_tools::garcon::ExtractKeyValuePair(keyval);
+  std::string unescapestr =
+      data_provider.ConsumeRandomLengthString(kRandomStringLength);
+  vm_tools::garcon::UnescapeString(unescapestr);
+  std::string multistr =
+      data_provider.ConsumeRandomLengthString(kRandomStringLength);
   std::vector<std::string> out;
   std::string delim = data_provider.ConsumeBytesAsString(1);
   if (delim.empty()) {
-    vm_tools::garcon::ParseMultiString(str, &out);
+    vm_tools::garcon::ParseMultiString(multistr, &out);
   } else {
-    vm_tools::garcon::ParseMultiString(str, &out, delim[0]);
+    vm_tools::garcon::ParseMultiString(multistr, &out, delim[0]);
   }
 
   return 0;
