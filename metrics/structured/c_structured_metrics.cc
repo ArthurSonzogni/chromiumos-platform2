34228220bd65702e3e1be497ba381a3ef715b71c
Yu Liu <yudiliu@google.com>
2021-02-03 16:44:31 -0800

    metrics: Support bluetooth metrics using structured metrics.
    BUG=b:173428018
    TEST=NONE
    
    Change-Id: I045940cb58244a6d10fb7097a00a52b74985a60c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2674411
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Manish Mandlik <mmandlik@chromium.org>
    Tested-by: Yu Liu <yudiliu@google.com>
    Commit-Queue: Yu Liu <yudiliu@google.com>

---
+++ metrics/structured/c_structured_metrics.cc
@@ -0,0 +1,79 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//
+// C wrapper to structured metrics code
+//
+
+#include "metrics/structured/c_structured_metrics.h"
+#include "metrics/structured/structured_events.h"
+
+namespace bluetooth = metrics::structured::events::bluetooth;
+
+extern "C" void BluetoothAdapterStateChanged(int64_t system_time, int state) {
+  bluetooth::BluetoothAdapterStateChanged()
+      .SetSystemTime(system_time)
+      .SetAdapterState(state)
+      .Record();
+}
+
+extern "C" void BluetoothPairingStateChanged(int64_t system_time,
+                                             const char* device_id,
+                                             int device_type,
+                                             int state) {
+  bluetooth::BluetoothPairingStateChanged()
+      .SetSystemTime(system_time)
+      .SetDeviceId(device_id)
+      .SetDeviceType(device_type)
+      .SetPairingState(state)
+      .Record();
+}
+
+extern "C" void BluetoothAclConnectionStateChanged(int64_t system_time,
+                                                   const char* device_id,
+                                                   int device_type,
+                                                   int state_change_type,
+                                                   int state) {
+  bluetooth::BluetoothAclConnectionStateChanged()
+      .SetSystemTime(system_time)
+      .SetDeviceId(device_id)
+      .SetDeviceType(device_type)
+      .SetStateChangeType(state_change_type)
+      .SetAclConnectionState(state)
+      .Record();
+}
+
+extern "C" void BluetoothProfileConnectionStateChanged(int64_t system_time,
+                                                       const char* device_id,
+                                                       int state_change_type,
+                                                       int profile,
+                                                       int state) {
+  bluetooth::BluetoothProfileConnectionStateChanged()
+      .SetSystemTime(system_time)
+      .SetDeviceId(device_id)
+      .SetStateChangeType(state_change_type)
+      .SetProfile(profile)
+      .SetProfileConnectionState(state)
+      .Record();
+}
+
+extern "C" void BluetoothDeviceInfoReport(int64_t system_time,
+                                          const char* device_id,
+                                          int device_type,
+                                          int device_class,
+                                          int vendor_id,
+                                          int vendor_id_source,
+                                          int product_id,
+                                          int product_version) {
+  bluetooth::BluetoothDeviceInfoReport()
+      .SetSystemTime(system_time)
+      .SetDeviceId(device_id)
+      .SetDeviceType(device_type)
+      .SetDeviceClass(device_class)
+      .SetVendorId(vendor_id)
+      .SetVendorIdSource(vendor_id_source)
+      .SetProductId(product_id)
+      .SetProductVersion(product_version)
+      .Record();
+}
---
+++ metrics/structured/c_structured_metrics.h
@@ -0,0 +1,58 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef METRICS_STRUCTURED_C_STRUCTURED_METRICS_H_
+#define METRICS_STRUCTURED_C_STRUCTURED_METRICS_H_
+
+#include <stdint.h>
+
+#include <brillo/brillo_export.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+// C wrapper for
+// metrics::structured::events::bluetooth::BluetoothAdapterStateChanged.
+BRILLO_EXPORT void BluetoothAdapterStateChanged(int64_t system_time, int state);
+
+// C wrapper for
+// metrics::structured::events::bluetooth::BluetoothPairingStateChanged.
+BRILLO_EXPORT void BluetoothPairingStateChanged(int64_t system_time,
+                                                const char* device_id,
+                                                int device_type,
+                                                int state);
+
+// C wrapper for
+// metrics::structured::events::bluetooth::BluetoothAclConnectionStateChanged.
+BRILLO_EXPORT void BluetoothAclConnectionStateChanged(int64_t system_time,
+                                                      const char* device_id,
+                                                      int device_type,
+                                                      int state_change_type,
+                                                      int state);
+
+// C wrapper for
+// metrics::structured::events::bluetooth::
+// BluetoothProfileConnectionStateChanged.
+BRILLO_EXPORT void BluetoothProfileConnectionStateChanged(int64_t system_time,
+                                                          const char* device_id,
+                                                          int state_change_type,
+                                                          int profile,
+                                                          int state);
+
+// C wrapper for
+// metrics::structured::events::bluetooth::BluetoothDeviceInfoReport.
+BRILLO_EXPORT void BluetoothDeviceInfoReport(int64_t system_time,
+                                             const char* device_id,
+                                             int device_type,
+                                             int device_class,
+                                             int vendor_id,
+                                             int vendor_id_source,
+                                             int product_id,
+                                             int product_version);
+
+#if defined(__cplusplus)
+}
+#endif
+#endif  // METRICS_STRUCTURED_C_STRUCTURED_METRICS_H_
