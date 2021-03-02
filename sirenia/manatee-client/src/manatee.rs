6f2cd7c53267b0e59b6bf1c1176e2da6f7423871
Allen Webb <allenwebb@google.com>
2021-03-02 11:13:58 -0600

    sirenia: Add 'manatee -r' option for local testing environment setup.
    This adds the '-r' command line option to 'manatee' that automates the
    test environment setup for the ManaTEE runtime environment.
    
    BUG=None
    TEST='manatee -r' in one shell and 'manatee' in another
    
    Cq-Depend: chromium:2729499
    Change-Id: Ie1d3e5f6b90497c1586d40a84728453d2283e83e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2728734
    Commit-Queue: Allen Webb <allenwebb@google.com>
    Tested-by: Allen Webb <allenwebb@google.com>
    Reviewed-by: Nicole Anderson-Au <nvaa@google.com>

--- sirenia/manatee-client/src/manatee.rs
+++ sirenia/manatee-client/src/manatee.rs
@@ -6,32 +6,55 @@
 
 use std::env;
 use std::fs::File;
-use std::io::{self, copy, stdin, stdout};
+use std::io::{self, copy, stdin, stdout, BufRead, BufReader, Read};
+use std::mem::replace;
 use std::os::unix::io::FromRawFd;
-use std::process::exit;
-use std::thread::spawn;
+use std::path::PathBuf;
+use std::process::{exit, ChildStderr, Command, Stdio};
+use std::str::FromStr;
+use std::thread::{spawn, JoinHandle};
 use std::time::Duration;
 
 use dbus::blocking::Connection;
 use getopts::Options;
+use libchromeos::linux::KillOnDrop;
 use manatee_client::client::OrgChromiumManaTEEInterface;
 use thiserror::Error as ThisError;
+use which;
 
 const DEFAULT_DBUS_TIMEOUT: Duration = Duration::from_secs(25);
 
 const DEVELOPER_SHELL_APP_ID: &str = "shell";
 const SANDBOXED_SHELL_APP_ID: &str = "sandboxed-shell";
 
+const MINIJAIL_NAME: &str = "minijail0";
+const CRONISTA_NAME: &str = "cronista";
+const TRICHECHUS_NAME: &str = "trichechus";
+const DUGONG_NAME: &str = "dugong";
+
+const CRONISTA_USER: &str = "cronista";
+const DUGONG_USER: &str = "dugong";
+
 #[derive(ThisError, Debug)]
 enum Error {
     #[error("failed parse command line options: {0:}")]
     OptionsParse(getopts::Fail),
-    #[error("failed set incompatible options: {0:} {1:}")]
-    ConflictingOptions(String, String),
+    #[error("failed set incompatible options: {0:?}")]
+    ConflictingOptions(Vec<String>),
     #[error("failed to get D-Bus connection: {0:}")]
     NewDBusConnection(dbus::Error),
     #[error("failed to call D-Bus method: {0:}")]
     DBusCall(dbus::Error),
+    #[error("failed to locate command '{0:}' because: {1:}")]
+    LocateCmd(String, which::Error),
+    #[error("failed to start command '{0:}' because: {1:}")]
+    StartCmd(String, io::Error),
+    #[error("failed to read stderr of {0:}: '{1:}'")]
+    ReadLineFailed(String, io::Error),
+    #[error("failed to locate listening URI for {0:}; last line: '{1:}'")]
+    FindUri(String, String),
+    #[error("failed to parse port number from line '{0:}'")]
+    ParsePort(String),
     #[error("start app failed with code: {0:}")]
     StartApp(i32),
     #[error("copy failed: {0:}")]
@@ -75,8 +98,138 @@ fn start_manatee_app(app_id: &str) -> Result<()> {
     Ok(())
 }
 
+fn locate_command(name: &str) -> Result<PathBuf> {
+    which::which(name).map_err(|err| Error::LocateCmd(name.to_string(), err))
+}
+
+fn read_line<R: Read>(
+    job_name: &str,
+    reader: &mut BufReader<R>,
+    mut line: &mut String,
+) -> Result<()> {
+    line.clear();
+    reader
+        .read_line(&mut line)
+        .map_err(|err| Error::ReadLineFailed(job_name.to_string(), err))?;
+    eprint!("{}", &line);
+    Ok(())
+}
+
+fn get_listening_port<'a, R: Read + Send + 'static, C: Fn(&str) -> bool>(
+    job_name: &'static str,
+    read: R,
+    conditions: &[C],
+) -> Result<(u32, JoinHandle<()>)> {
+    let mut reader = BufReader::new(read);
+    let mut line = String::new();
+    read_line(job_name, &mut reader, &mut line)?;
+
+    for condition in conditions {
+        if condition(&line) {
+            read_line(job_name, &mut reader, &mut line)?;
+        }
+    }
+
+    if !line.contains("waiting for connection at: ip://127.0.0.1:") {
+        return Err(Error::FindUri(job_name.to_string(), line));
+    }
+    let port = u32::from_str(&line[line.rfind(":").unwrap() + 1..line.len() - 1])
+        .map_err(|_| Error::ParsePort(line.clone()))?;
+
+    let join_handle =
+        spawn(
+            move || {
+                while read_line(job_name, &mut reader, &mut line).is_ok() && !line.is_empty() {}
+            },
+        );
+    Ok((port, join_handle))
+}
+
+fn run_test_environment() -> Result<()> {
+    let minijail_path = locate_command(MINIJAIL_NAME)?;
+    let cronista_path = locate_command(CRONISTA_NAME)?;
+    let trichechus_path = locate_command(TRICHECHUS_NAME)?;
+    let dugong_path = locate_command(DUGONG_NAME)?;
+
+    // Cronista.
+    let mut cronista = KillOnDrop::from(
+        Command::new(&minijail_path)
+            .args(&[
+                "-u",
+                CRONISTA_USER,
+                "--",
+                cronista_path.to_str().unwrap(),
+                "-U",
+                "ip://127.0.0.1:0",
+            ])
+            .stderr(Stdio::piped())
+            .spawn()
+            .map_err(|err| Error::StartCmd(CRONISTA_NAME.to_string(), err))?,
+    );
+
+    let (cronista_port, cronista_stderr_print) = get_listening_port(
+        CRONISTA_NAME,
+        replace(&mut cronista.as_mut().stderr, Option::<ChildStderr>::None).unwrap(),
+        &[|l: &str| l.ends_with("starting cronista\n")],
+    )?;
+
+    // Trichechus.
+    let mut trichechus = KillOnDrop::from(
+        Command::new(trichechus_path)
+            .args(&[
+                "-U",
+                "ip://127.0.0.1:0",
+                "-C",
+                &format!("ip://127.0.0.1:{}", cronista_port),
+            ])
+            .stderr(Stdio::piped())
+            .spawn()
+            .map_err(|err| Error::StartCmd(TRICHECHUS_NAME.to_string(), err))?,
+    );
+
+    let conditions = [
+        |l: &str| l == "Syslog exists.\n" || l == "Creating syslog.\n",
+        |l: &str| l.contains("starting trichechus:"),
+        |l: &str| l.contains("Unable to start new process group:"),
+    ];
+    let (trichechus_port, trichechus_stderr_print) = get_listening_port(
+        TRICHECHUS_NAME,
+        replace(&mut trichechus.as_mut().stderr, Option::<ChildStderr>::None).unwrap(),
+        &conditions,
+    )?;
+
+    // Dugong.
+    let dugong = KillOnDrop::from(
+        Command::new(&minijail_path)
+            .args(&[
+                "-u",
+                DUGONG_USER,
+                "--",
+                dugong_path.to_str().unwrap(),
+                "-U",
+                &format!("ip://127.0.0.1:{}", trichechus_port),
+            ])
+            .spawn()
+            .map_err(|err| Error::StartCmd(DUGONG_NAME.to_string(), err))?,
+    );
+
+    println!("*** Press enter to continue. ***");
+    let mut _line = String::new();
+    stdin().read_line(&mut _line).ok();
+
+    drop(dugong);
+    drop(trichechus);
+    drop(cronista);
+
+    trichechus_stderr_print.join().unwrap();
+    cronista_stderr_print.join().unwrap();
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
     const HELP_SHORT_NAME: &str = "h";
+    const RUN_SERVICES_LOCALLY_SHORT_NAME: &str = "r";
     const SANDBOX_SHORT_NAME: &str = "s";
     const APP_ID_SHORT_NAME: &str = "a";
 
@@ -86,6 +239,11 @@ fn main() -> Result<()> {
         SANDBOX_SHORT_NAME,
         "enable-sandbox",
         "Run the shell in the default sandbox.",
+    );
+    options.optflag(
+        RUN_SERVICES_LOCALLY_SHORT_NAME,
+        "run-services-locally",
+        "Run a test sirenia environment locally.",
     );
     options.optopt(
         APP_ID_SHORT_NAME,
@@ -104,14 +262,27 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
-    let app_id = if matches.opt_present(SANDBOX_SHORT_NAME) {
-        if matches.opt_present(APP_ID_SHORT_NAME) {
-            eprintln!("Error: -s is incompatible with -a \n{}", options.usage(""));
-            return Err(Error::ConflictingOptions(
-                SANDBOX_SHORT_NAME.to_string(),
-                APP_ID_SHORT_NAME.to_string(),
-            ));
+    let mut opts = Vec::<String>::new();
+    for short_name in &[
+        SANDBOX_SHORT_NAME,
+        RUN_SERVICES_LOCALLY_SHORT_NAME,
+        APP_ID_SHORT_NAME,
+    ] {
+        if matches.opt_present(short_name) {
+            opts.push(format!("-{}", short_name));
         }
+    }
+    if opts.len() > 1 {
+        eprintln!("{}", options.usage(""));
+        return Err(Error::ConflictingOptions(opts));
+    }
+
+    if matches.opt_present(RUN_SERVICES_LOCALLY_SHORT_NAME) {
+        return run_test_environment();
+    }
+
+    let app_id = if matches.opt_present(SANDBOX_SHORT_NAME) {
+        if matches.opt_present(APP_ID_SHORT_NAME) {}
         SANDBOXED_SHELL_APP_ID.to_string()
     } else if let Some(app_id) = matches.opt_get(APP_ID_SHORT_NAME).unwrap() {
         app_id
