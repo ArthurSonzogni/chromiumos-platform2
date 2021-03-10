26f5dc876805f1fd4991b5d9a8982a8b69fdb041
Allen Webb <allenwebb@google.com>
2021-03-10 11:20:55 -0600

    libchromeos-rs: Add base::rand_util for getting random bytes.
    This adds a Rust implementation of functionality parallel to libchrome's
    base/rand_util.h
    
    BUG=b:162088715
    TEST=cargo test --workspace
    
    Change-Id: I29731ff091c2ea2db9f72bc8bc5a5bf33d82ae4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2748044
    Tested-by: Allen Webb <allenwebb@google.com>
    Commit-Queue: Allen Webb <allenwebb@google.com>
    Reviewed-by: Stephen Barber <smbarber@chromium.org>

---
+++ libchromeos-rs/src/base/mod.rs
@@ -0,0 +1,7 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Rust implementations of functionality parallel to libchrome's base.
+
+pub mod rand_util;
--- libchromeos-rs/src/lib.rs
+++ libchromeos-rs/src/lib.rs
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+pub mod base;
 #[cfg(feature = "chromeos-module")]
 pub mod chromeos;
-
 pub mod linux;
 pub mod net;
 mod read_dir;
