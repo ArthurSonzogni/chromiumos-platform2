ba0f885d267a9efd922b3133d386fd5175c49017
Allen Webb <allenwebb@google.com>
2021-03-12 07:15:11 -0600

    sirenia: Add MockCronista for unit testing.
    BUG=b:162088715
    TEST=cargo build --workspace
    
    Change-Id: Ia8a483d3882ff65414a3b8441ba0b70d8c0686bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2755593
    Commit-Queue: Allen Webb <allenwebb@google.com>
    Tested-by: Allen Webb <allenwebb@google.com>
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>

--- sirenia/libsirenia/src/communication/persistence.rs
+++ sirenia/libsirenia/src/communication/persistence.rs
@@ -5,8 +5,14 @@
 //! Defines messages used for communication between Trichechus and Cronista for storing and
 //! retrieving persistent data.
 
+use std::collections::{HashMap, VecDeque};
+use std::ops::{Deref, DerefMut};
+use std::sync::Mutex;
+
 use serde::{Deserialize, Serialize};
 use sirenia_rpc_macros::sirenia_rpc;
+
+use crate::rpc;
 
 /// Represents the possible status codes from an RPC.
 /// Values are assigned to make it easier to interface with D-Bus.
@@ -20,7 +26,7 @@ pub enum Status {
 
 /// Should the data be globally available or only available within the users' sessions.
 /// Values are assigned to make it easier to interface with D-Bus.
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub enum Scope {
     System = 0,
     Session = 1,
@@ -45,4 +51,105 @@ pub trait Cronista {
         domain: String,
         identifier: String,
     ) -> std::result::Result<(Status, Vec<u8>), Self::Error>;
+}
+
+#[derive(Clone, Hash, Eq, PartialEq)]
+struct CronistaIdentifier {
+    scope: Scope,
+    domain: String,
+    identifier: String,
+}
+
+impl CronistaIdentifier {
+    fn new(scope: Scope, domain: String, identifier: String) -> Self {
+        CronistaIdentifier {
+            scope,
+            domain,
+            identifier,
+        }
+    }
+}
+
+/// A in memory implementation of the Cronista interface for unit tests.
+pub struct MockCronista {
+    storage: Mutex<HashMap<CronistaIdentifier, Vec<u8>>>,
+    next_error: Mutex<VecDeque<rpc::Error>>,
+    fail_next: bool,
+}
+
+impl MockCronista {
+    pub fn new() -> Self {
+        MockCronista {
+            storage: Mutex::new(HashMap::new()),
+            next_error: Mutex::new(VecDeque::new()),
+            fail_next: false,
+        }
+    }
+
+    pub fn next_error_push_back(&mut self, err: rpc::Error) {
+        self.next_error.lock().unwrap().deref_mut().push_back(err);
+    }
+
+    pub fn fail_next(&self) -> bool {
+        self.fail_next
+    }
+
+    pub fn fail_next_mut(&mut self) -> &bool {
+        &mut self.fail_next
+    }
+}
+
+impl Default for MockCronista {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Cronista for MockCronista {
+    type Error = rpc::Error;
+
+    fn persist(
+        &self,
+        scope: Scope,
+        domain: String,
+        identifier: String,
+        data: Vec<u8>,
+    ) -> Result<Status, Self::Error> {
+        if let Some(err) = self.next_error.lock().unwrap().deref_mut().pop_front() {
+            return Err(err);
+        }
+        if self.fail_next {
+            return Ok(Status::Failure);
+        }
+        self.storage
+            .lock()
+            .unwrap()
+            .deref_mut()
+            .insert(CronistaIdentifier::new(scope, domain, identifier), data);
+        Ok(Status::Success)
+    }
+
+    fn retrieve(
+        &self,
+        scope: Scope,
+        domain: String,
+        identifier: String,
+    ) -> Result<(Status, Vec<u8>), Self::Error> {
+        if let Some(err) = self.next_error.lock().unwrap().deref_mut().pop_front() {
+            return Err(err);
+        }
+        if self.fail_next {
+            return Ok((Status::Failure, Vec::default()));
+        }
+        match self
+            .storage
+            .lock()
+            .unwrap()
+            .deref()
+            .get(&CronistaIdentifier::new(scope, domain, identifier))
+        {
+            Some(data) => Ok((Status::Success, data.clone())),
+            None => Ok((Status::IdNotFound, Vec::default())),
+        }
+    }
 }
