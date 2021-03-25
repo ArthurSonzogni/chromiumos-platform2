c361410a64938a10abffdb9038eb65efc079cc87
Betul Soysal <betuls@google.com>
2021-03-25 07:19:26 +0000

    libbrillo: Make Chrome mount namespace noexec
    Chrome mount namespace created at /run/namespaces/mnt_chrome needs to
    be noexec. Remount and add MS_NOEXEC mount option.
    
    Before this, CL:2780067 was submitted to make the mount namespace
    readonly and noexec. But it is reverted since readonly proc fs caused
    boot failures with kernel 3.18. So for now resubmitting the CL with
    only noexec option.
    
    BUG=chromium:1191297
    TEST=findmnt -o TARGET,SOURCE,OPTIONS | grep mnt_chrome
    | | |-/run/namespaces/mnt_chrome nsfs[mnt:[4026532401]] rw,noexec
    TEST=Unit tests pass.
    TEST=Start Guest session and check the mount namespace of Chrome to be
    different from init mount namespace, i. e., readlink /proc/1/ns/mnt and
    readlink /proc/<Chrome Process ID>/ns/mnt needs to be different.
    
    Change-Id: Iad105b81f040d5f69e3ea7788b99d3888321e9ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2848284
    Tested-by: Betul Soysal <betuls@google.com>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- libbrillo/brillo/namespaces/mount_namespace_test.cc
+++ libbrillo/brillo/namespaces/mount_namespace_test.cc
@@ -41,7 +41,9 @@ TEST_F(MountNamespaceTest, CreateNamespace) {
   std::unique_ptr<MountNamespace> ns =
       std::make_unique<MountNamespace>(base::FilePath(), &platform_);
   EXPECT_CALL(platform_, Fork()).WillOnce(Return(1));
-  EXPECT_CALL(platform_, Mount(_, _, _, _, _)).WillOnce(Return(0));
+  EXPECT_CALL(platform_, Mount(_, _, _, _, _))
+      .Times(2)
+      .WillRepeatedly(Return(0));
   EXPECT_CALL(platform_, Waitpid(_, _))
       .WillOnce(DoAll(SetArgPointee<1>(0x00000000), Return(0)));
   EXPECT_TRUE(ns->Create());
@@ -52,7 +54,9 @@ TEST_F(MountNamespaceTest, CreateNamespaceFailedOnWaitpid) {
   std::unique_ptr<MountNamespace> ns =
       std::make_unique<MountNamespace>(base::FilePath(), &platform_);
   EXPECT_CALL(platform_, Fork()).WillOnce(Return(1));
-  EXPECT_CALL(platform_, Mount(_, _, _, _, _)).WillOnce(Return(0));
+  EXPECT_CALL(platform_, Mount(_, _, _, _, _))
+      .Times(2)
+      .WillRepeatedly(Return(0));
   EXPECT_CALL(platform_, Waitpid(_, _)).WillOnce(Return(-1));
   EXPECT_FALSE(ns->Create());
 }
@@ -69,7 +73,9 @@ TEST_F(MountNamespaceTest, CreateNamespaceFailedOnStatus) {
   std::unique_ptr<MountNamespace> ns =
       std::make_unique<MountNamespace>(base::FilePath(), &platform_);
   EXPECT_CALL(platform_, Fork()).WillOnce(Return(1));
-  EXPECT_CALL(platform_, Mount(_, _, _, _, _)).WillOnce(Return(0));
+  EXPECT_CALL(platform_, Mount(_, _, _, _, _))
+      .Times(2)
+      .WillRepeatedly(Return(0));
   EXPECT_CALL(platform_, Waitpid(_, _))
       .WillOnce(DoAll(SetArgPointee<1>(0xFFFFFFFF), Return(0)));
   EXPECT_FALSE(ns->Create());
@@ -79,7 +85,9 @@ TEST_F(MountNamespaceTest, DestroyAfterUnmountFailsAndUnmountSucceeds) {
   std::unique_ptr<MountNamespace> ns =
       std::make_unique<MountNamespace>(base::FilePath(), &platform_);
   EXPECT_CALL(platform_, Fork()).WillOnce(Return(1));
-  EXPECT_CALL(platform_, Mount(_, _, _, _, _)).WillOnce(Return(0));
+  EXPECT_CALL(platform_, Mount(_, _, _, _, _))
+      .Times(2)
+      .WillRepeatedly(Return(0));
   EXPECT_CALL(platform_, Waitpid(_, _))
       .WillOnce(DoAll(SetArgPointee<1>(0x00000000), Return(0)));
   EXPECT_TRUE(ns->Create());
