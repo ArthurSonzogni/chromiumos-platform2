3c7679d93cdc6c1641811f84c7341d6fc8f9647e
Jorge Lucangeli Obes <jorgelo@chromium.org>
2021-04-02 10:10:32 -0400

    secanomalyd: Add MountEntry unit tests.
    Test the basic functionality of MountEntry objects:
    -They properly detect W+X mounts.
    -They don't get tripped by confusingly-named flags.
    -They correctly detect USB drives and archives.
    
    BUG=chromium:1195363
    TEST=Run new unit tests.
    
    Disallowed-Recycled-Builds: test-failures
    Change-Id: Id1ba8dc44fbc0a09acd759d6bb8b463a6fd28f45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2801693
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Tested-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Auto-Submit: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

---
+++ secanomalyd/mount_entry_test.cc
@@ -0,0 +1,55 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Unit tests for MountEntry.
+
+#include "secanomalyd/mount_entry.h"
+
+#include <gtest/gtest.h>
+
+TEST(MountEntryTest, RoRootFs) {
+  MountEntry e("/dev/root / ext2 ro,seclabel,relatime 0 0");
+  ASSERT_FALSE(e.IsWX());
+}
+
+TEST(MountEntryTest, WNoExecStateful) {
+  MountEntry e(
+      "/dev/sda1 /mnt/stateful_partition ext4 "
+      "rw,seclabel,nosuid,nodev,noexec,noatime,"
+      "resgid=20119,commit=600,data=ordered 0 0");
+  ASSERT_FALSE(e.IsWX());
+}
+
+TEST(MountEntryTest, WXUsrLocal) {
+  MountEntry e(
+      "/dev/sda1 /usr/local ext4 "
+      "rw,seclabel,nodev,noatime,resgid=20119,commit=600,data=ordered 0 0");
+  ASSERT_TRUE(e.IsWX());
+}
+
+TEST(MountEntryTest, MisplacedRW) {
+  MountEntry e("/dev/sda1 /pointless_path ext4 ro,nodev,somethingrw");
+  ASSERT_FALSE(e.IsWX());
+}
+
+TEST(MountEntryTest, MisplacedNoExec) {
+  MountEntry e("/dev/sda1 /pointless_path ext4 rw,nodev,notreallynoexec");
+  ASSERT_TRUE(e.IsWX());
+}
+
+TEST(MountEntryTest, UsbDrive) {
+  MountEntry e(
+      "/dev/sdb1 /media/removable/USB\040Drive ext2 "
+      "rw,dirsync,nosuid,nodev,noexec,seclabel,relatime,nosymfollow");
+  ASSERT_TRUE(e.IsUsbDriveOrArchive());
+}
+
+TEST(MountEntryTest, ArchiveFileInDownloads) {
+  MountEntry e(
+      "fuse:/home/chronos/u-f0df208cd7759644d43f8d7c4c5900e4a4875275/MyFiles/"
+      "Downloads/sample.rar /media/archive/sample.rar fuse.rarfs "
+      "ro,dirsync,nosuid,nodev,noexec,relatime,nosymfollow,"
+      "user_id=1000,group_id=1001,default_permissions,allow_other 0 0");
+  ASSERT_TRUE(e.IsUsbDriveOrArchive());
+}
