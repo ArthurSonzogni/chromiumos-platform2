296e477be41a47b1dc38d083febc3565b2e793f3
Hugo Benichi <hugobenichi@google.com>
2021-04-16 11:38:29 +0900

    shill: update UserAgent string used for captive portal probes
    This patch updates the UserAgent string used by CaptivePortal to
    impersonate Chrome running on Linux with a more recent version, using
    the most recent stable build number on the stable channel right now (m89
    at the time of this patch).
    
    A follow up patch will change the Chrome build id to be dynamically
    inserted into the UserAgent string template.
    
    BUG=b:184767681
    TEST=unit tests. Flashed rammus, checked that the http probe sent by
    CaptivePortal uses the new UserAgent string.
    
    Change-Id: I8c76203e3cf7d43c6f9f2fbcddce64b9c242464b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2830712
    Tested-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Hugo Benichi <hugobenichi@google.com>

--- shill/portal_detector.cc
+++ shill/portal_detector.cc
@@ -27,8 +27,8 @@ using std::string;
 
 namespace {
 const char kLinuxUserAgent[] =
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (kHTML, like Gecko) "
-    "Chrome/7.0.38.09.132 Safari/537.36";
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/89.0.4389.114 Safari/537.36";
 const brillo::http::HeaderList kHeaders{
     {brillo::http::request_header::kUserAgent, kLinuxUserAgent},
 };
