a58e705dd9b2ae2657c87db0ed3e28da2056de28
Maksim Ivanov <emaxx@chromium.org>
2021-04-16 04:16:14 +0200

    cryptohome: Tool for cryptohome recovery crypto
    Add a test-only CLI tool that allows to exercise parts of the cryptohome
    recovery crypto.
    
    The main goal is to use this tool for preparing test vectors for
    server-side implementation, but the tool can also be used for manual or
    automated client-side testing.
    
    BUG=b:185533257
    TEST=cryptohome-test-tool \
      --action=recovery_crypto_create \
      --destination_share_out_file=./dst \
      --mediator_share_out_file=./med \
      --publisher_pub_key_out_file=./pub \
      --recovery_secret_out_file=./secr-crea \
      && \
      cryptohome-test-tool \
      --action=recovery_crypto_mediate \
      --mediator_share_in_file=./med \
      --publisher_pub_key_in_file=./pub \
      --mediated_publisher_pub_key_out_file=./mediated \
      && \
      cryptohome-test-tool \
      --action=recovery_crypto_decrypt \
      --destination_share_in_file=./dst \
      --publisher_pub_key_in_file=./pub \
      --mediated_publisher_pub_key_in_file=./mediated \
      --recovery_secret_out_file=./secr-decr \
      && \
      diff secr-crea secr-decr
    
    Change-Id: Ic00c440140b7e67ff12c436aa2ab583feec9c45b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2830930
    Reviewed-by: Tomasz Dobrowolski <tomdobro@chromium.org>
    Reviewed-by: Leo Lai <cylai@google.com>
    Tested-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>

---
+++ cryptohome/cryptohome-test-tool.cc
@@ -0,0 +1,224 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+
+#include <cstdlib>
+#include <memory>
+#include <string>
+
+#include <base/containers/span.h>
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
+#include <base/logging.h>
+#include <brillo/flag_helper.h>
+#include <brillo/secure_blob.h>
+#include <brillo/syslog_logging.h>
+
+#include "cryptohome/crypto/recovery_crypto.h"
+
+using base::FilePath;
+using brillo::SecureBlob;
+using cryptohome::RecoveryCrypto;
+
+namespace {
+
+bool CheckMandatoryFlag(const std::string& flag_name,
+                        const std::string& flag_value) {
+  if (!flag_value.empty())
+    return true;
+  LOG(ERROR) << "--" << flag_name << " is mandatory.";
+  return false;
+}
+
+bool ReadFileToSecureBlobLogged(const FilePath& file_path,
+                                SecureBlob* contents) {
+  std::string contents_string;
+  if (!base::ReadFileToString(file_path, &contents_string)) {
+    LOG(ERROR) << "Failed to read from file " << file_path.value() << ".";
+    return false;
+  }
+  *contents = SecureBlob(contents_string);
+  return true;
+}
+
+bool WriteFileLogged(const FilePath& file_path,
+                     base::span<const uint8_t> contents) {
+  if (base::WriteFile(file_path, contents))
+    return true;
+  LOG(ERROR) << "Failed to write to file " << file_path.value() << ".";
+  return false;
+}
+
+bool DoRecoveryCryptoCreateAction(
+    const FilePath& destination_share_out_file_path,
+    const FilePath& mediator_share_out_file_path,
+    const FilePath& publisher_pub_key_out_file_path,
+    const FilePath& recovery_secret_out_file_path) {
+  std::unique_ptr<RecoveryCrypto> recovery_crypto = RecoveryCrypto::Create();
+  if (!recovery_crypto) {
+    LOG(ERROR) << "Failed to create recovery crypto object.";
+    return false;
+  }
+  SecureBlob mediator_share, destination_share, dealer_pub_key;
+  if (!recovery_crypto->GenerateShares(&mediator_share, &destination_share,
+                                       &dealer_pub_key)) {
+    LOG(ERROR) << "Failed to generate recovery shares.";
+    return false;
+  }
+  SecureBlob publisher_pub_key, publisher_dh;
+  if (!recovery_crypto->GeneratePublisherKeys(
+          dealer_pub_key, &publisher_pub_key, &publisher_dh)) {
+    LOG(ERROR) << "Failed to generate recovery publisher keys.";
+    return false;
+  }
+  return WriteFileLogged(destination_share_out_file_path, destination_share) &&
+         WriteFileLogged(mediator_share_out_file_path, mediator_share) &&
+         WriteFileLogged(publisher_pub_key_out_file_path, publisher_pub_key) &&
+         WriteFileLogged(recovery_secret_out_file_path, publisher_dh);
+}
+
+bool DoRecoveryCryptoMediateAction(
+    const FilePath& mediator_share_in_file_path,
+    const FilePath& publisher_pub_key_in_file_path,
+    const FilePath& mediated_publisher_pub_key_out_file_path) {
+  SecureBlob mediator_share, publisher_pub_key;
+  if (!ReadFileToSecureBlobLogged(mediator_share_in_file_path,
+                                  &mediator_share) ||
+      !ReadFileToSecureBlobLogged(publisher_pub_key_in_file_path,
+                                  &publisher_pub_key)) {
+    return false;
+  }
+  std::unique_ptr<RecoveryCrypto> recovery_crypto = RecoveryCrypto::Create();
+  if (!recovery_crypto) {
+    LOG(ERROR) << "Failed to create recovery crypto object.";
+    return false;
+  }
+  SecureBlob mediated_publisher_pub_key;
+  if (!recovery_crypto->Mediate(publisher_pub_key, mediator_share,
+                                &mediated_publisher_pub_key)) {
+    LOG(ERROR) << "Failed to perform recovery mediation.";
+    return false;
+  }
+  return WriteFileLogged(mediated_publisher_pub_key_out_file_path,
+                         mediated_publisher_pub_key);
+}
+
+bool DoRecoveryCryptoDecryptAction(
+    const FilePath& destination_share_in_file_path,
+    const FilePath& publisher_pub_key_in_file_path,
+    const FilePath& mediated_publisher_pub_key_in_file_path,
+    const FilePath& recovery_secret_out_file_path) {
+  SecureBlob destination_share, publisher_pub_key, mediated_publisher_pub_key;
+  if (!ReadFileToSecureBlobLogged(destination_share_in_file_path,
+                                  &destination_share) ||
+      !ReadFileToSecureBlobLogged(publisher_pub_key_in_file_path,
+                                  &publisher_pub_key) ||
+      !ReadFileToSecureBlobLogged(mediated_publisher_pub_key_in_file_path,
+                                  &mediated_publisher_pub_key)) {
+    return false;
+  }
+  std::unique_ptr<RecoveryCrypto> recovery_crypto = RecoveryCrypto::Create();
+  if (!recovery_crypto) {
+    LOG(ERROR) << "Failed to create recovery crypto object.";
+    return false;
+  }
+  SecureBlob destination_dh;
+  if (!recovery_crypto->RecoverDestination(publisher_pub_key, destination_share,
+                                           mediated_publisher_pub_key,
+                                           &destination_dh)) {
+    LOG(ERROR) << "Failed to perform destination recovery.";
+    return false;
+  }
+  return WriteFileLogged(recovery_secret_out_file_path, destination_dh);
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  brillo::InitLog(brillo::kLogToStderr);
+
+  DEFINE_string(action, "",
+                "One of: recovery_crypto_create, recovery_crypto_mediate, "
+                "recovery_crypto_decrypt.");
+  DEFINE_string(destination_share_out_file, "",
+                "Path to the file where to store the Cryptohome Recovery "
+                "encrypted destination share.");
+  DEFINE_string(destination_share_in_file, "",
+                "Path to the file containing the Cryptohome Recovery encrypted "
+                "destination share.");
+  DEFINE_string(mediator_share_out_file, "",
+                "Path to the file where to store the Cryptohome Recovery "
+                "encrypted mediator share.");
+  DEFINE_string(mediator_share_in_file, "",
+                "Path to the file containing the Cryptohome Recovery encrypted "
+                "mediator share.");
+  DEFINE_string(publisher_pub_key_out_file, "",
+                "Path to the file where to store the Cryptohome Recovery "
+                "publisher public key.");
+  DEFINE_string(publisher_pub_key_in_file, "",
+                "Path to the file containing the Cryptohome Recovery publisher "
+                "public key.");
+  DEFINE_string(mediated_publisher_pub_key_out_file, "",
+                "Path to the file where to store the Cryptohome Recovery "
+                "mediated publisher public key.");
+  DEFINE_string(mediated_publisher_pub_key_in_file, "",
+                "Path to the file containing the Cryptohome Recovery "
+                "mediated publisher public key.");
+  DEFINE_string(
+      recovery_secret_out_file, "",
+      "Path to the file where to store the Cryptohome Recovery secret.");
+  brillo::FlagHelper::Init(argc, argv,
+                           "cryptohome-test-tool - Test tool for cryptohome.");
+
+  bool success = false;
+  if (FLAGS_action.empty()) {
+    LOG(ERROR) << "--action is required.";
+  } else if (FLAGS_action == "recovery_crypto_create") {
+    if (CheckMandatoryFlag("destination_share_out_file",
+                           FLAGS_destination_share_out_file) &&
+        CheckMandatoryFlag("mediator_share_out_file",
+                           FLAGS_mediator_share_out_file) &&
+        CheckMandatoryFlag("publisher_pub_key_out_file",
+                           FLAGS_publisher_pub_key_out_file) &&
+        CheckMandatoryFlag("recovery_secret_out_file",
+                           FLAGS_recovery_secret_out_file)) {
+      success = DoRecoveryCryptoCreateAction(
+          FilePath(FLAGS_destination_share_out_file),
+          FilePath(FLAGS_mediator_share_out_file),
+          FilePath(FLAGS_publisher_pub_key_out_file),
+          FilePath(FLAGS_recovery_secret_out_file));
+    }
+  } else if (FLAGS_action == "recovery_crypto_mediate") {
+    if (CheckMandatoryFlag("mediator_share_in_file",
+                           FLAGS_mediator_share_in_file) &&
+        CheckMandatoryFlag("publisher_pub_key_in_file",
+                           FLAGS_publisher_pub_key_in_file) &&
+        CheckMandatoryFlag("mediated_publisher_pub_key_out_file",
+                           FLAGS_mediated_publisher_pub_key_out_file)) {
+      success = DoRecoveryCryptoMediateAction(
+          FilePath(FLAGS_mediator_share_in_file),
+          FilePath(FLAGS_publisher_pub_key_in_file),
+          FilePath(FLAGS_mediated_publisher_pub_key_out_file));
+    }
+  } else if (FLAGS_action == "recovery_crypto_decrypt") {
+    if (CheckMandatoryFlag("destination_share_in_file",
+                           FLAGS_destination_share_in_file) &&
+        CheckMandatoryFlag("publisher_pub_key_in_file",
+                           FLAGS_publisher_pub_key_in_file) &&
+        CheckMandatoryFlag("mediated_publisher_pub_key_in_file",
+                           FLAGS_mediated_publisher_pub_key_in_file) &&
+        CheckMandatoryFlag("recovery_secret_out_file",
+                           FLAGS_recovery_secret_out_file)) {
+      success = DoRecoveryCryptoDecryptAction(
+          FilePath(FLAGS_destination_share_in_file),
+          FilePath(FLAGS_publisher_pub_key_in_file),
+          FilePath(FLAGS_mediated_publisher_pub_key_in_file),
+          FilePath(FLAGS_recovery_secret_out_file));
+    }
+  } else {
+    LOG(ERROR) << "Unknown --action.";
+  }
+  return success ? EXIT_SUCCESS : EXIT_FAILURE;
+}
