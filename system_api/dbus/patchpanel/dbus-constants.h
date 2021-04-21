acc45b0d961af2130a6bf6721d85074dde22e966
Jason Jeremy Iman <jasongustaman@google.com>
2021-04-21 21:45:01 +0900

    system_api: Add SetDnsRedirectionRuleRequest in patchpanel
    This patch adds a new DBus API for patchpanel. The
    API is used to add routing rules for DNS proxy
    (go/cros-dns-forwarding).
    
    BUG=b:171363265
    TEST=emerge-rammus-arc-r system_api
    
    Change-Id: If16d78a653f14aa6ed498e3081f62ab69d0edb11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2847520
    Tested-by: Jason Jeremy Iman <jasongustaman@chromium.org>
    Commit-Queue: Jason Jeremy Iman <jasongustaman@chromium.org>
    Reviewed-by: Garrick Evans <garrick@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>

--- system_api/dbus/patchpanel/dbus-constants.h
+++ system_api/dbus/patchpanel/dbus-constants.h
@@ -26,6 +26,7 @@ const char kGetTrafficCountersMethod[] = "GetTrafficCounters";
 const char kModifyPortRuleMethod[] = "ModifyPortRule";
 const char kGetDevicesMethod[] = "GetDevices";
 const char kSetVpnLockdown[] = "SetVpnLockdown";
+const char kSetDnsRedirectionRuleMethod[] = "SetDnsRedirectionRule";
 
 // Signals.
 const char kNetworkDeviceChangedSignal[] = "NetworkDeviceChanged";
