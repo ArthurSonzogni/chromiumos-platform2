ccc2af0781effd4d33a7c690c0c8d7060cc6324c
Fergus Dall <sidereal@google.com>
2021-04-22 05:33:26 +0000

    Revert "vm_tools: sommelier: Don't commit new windows eagerly"
    This reverts commit b4647d95c60cf2178ef29ed934ec98573dd097ca.
    
    Reason for revert: Speculative revert
    
    Original change's description:
    > vm_tools: sommelier: Don't commit new windows eagerly
    >
    > There were two cases where sommelier would commit windows, in response
    > to configure and in response to an update of a new window (i.e. the
    > first update).
    >
    > Only the former of these was guarded by the "don't commit until
    > everything is finalized" check (called
    > sl_process_pending_configure_acks), the result of which is that the
    > window would be committed before its position was agreed upon by the
    > compositor.
    >
    > This only affects X11 windows which are position relative to a parent,
    > i.e. child windows like popups.
    >
    > BUG=b:184705438
    > TEST=open a bunch of menu popups, observe their location.
    >
    > Change-Id: I259d920e8c059220a42eb185f4da06c4ceed9b9e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2809920
    > Reviewed-by: Fergus Dall <sidereal@google.com>
    > Tested-by: Nic Hollingum <hollingum@google.com>
    > Commit-Queue: Nic Hollingum <hollingum@google.com>
    
    Bug: b:184705438
    Bug: chromium:1199870
    Change-Id: I2288f697ae444110afe6e0c827775b30d224d732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2845752
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Fergus Dall <sidereal@google.com>

--- vm_tools/sommelier/sommelier.cc
+++ vm_tools/sommelier/sommelier.cc
@@ -556,13 +556,6 @@ int sl_process_pending_configure_acks(struct sl_window* window,
   return 1;
 }
 
-void sl_commit(struct sl_window* window, struct sl_host_surface* host_surface) {
-  if (sl_process_pending_configure_acks(window, host_surface)) {
-    if (host_surface)
-      wl_surface_commit(host_surface->proxy);
-  }
-}
-
 static void sl_internal_xdg_surface_configure(
     void* data, struct zxdg_surface_v6* xdg_surface, uint32_t serial) {
   TRACE_EVENT("surface", "sl_internal_xdg_surface_configure");
@@ -581,7 +574,11 @@ static void sl_internal_xdg_surface_configure(
           wl_resource_get_user_data(host_resource));
 
     sl_configure_window(window);
-    sl_commit(window, host_surface);
+
+    if (sl_process_pending_configure_acks(window, host_surface)) {
+      if (host_surface)
+        wl_surface_commit(host_surface->proxy);
+    }
   }
 }
 
@@ -937,7 +934,7 @@ void sl_window_update(struct sl_window* window) {
                              (window->y - parent->y) / ctx->scale);
   }
 
-  sl_commit(window, host_surface);
+  wl_surface_commit(host_surface->proxy);
   if (host_surface->contents_width && host_surface->contents_height)
     window->realized = 1;
 }
