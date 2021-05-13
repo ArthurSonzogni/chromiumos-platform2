a1c80b211a5cdf96f306634640f1b5f614e7f8fd
Harvey Yang <chenghaoyang@chromium.org>
2021-05-13 18:11:26 +0800

    Reland "camera: Add SensorHalClientImpl"
    This is a reland of e36461a7530410f8beecc8c502061757ada5b094
    
    Reason for reland: The original commit starts connection to Chromium
    right after c'tor, which causes an issue in cros-camera-test.
    
    Original change's description:
    > camera: Add CameraHalClientImpl
    >
    > This commit adds SensorhalClientImpl to enumerate sensors devices.
    >
    > BUG=b:144747001, b:184281732
    > TEST=builds and run on coachz
    >
    > Change-Id: Ie5252b390780caf096989e01d98e0201450db277
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2894073
    > Tested-by: Cheng-Hao Yang <chenghaoyang@chromium.org>
    > Commit-Queue: Cheng-Hao Yang <chenghaoyang@chromium.org>
    > Reviewed-by: Ricky Liang <jcliang@chromium.org>
    > Reviewed-by: Hanlin Chen <hanlinchen@chromium.org>
    
    BUG=b:144747001, b:184281732, b:194021054
    TEST=unit tests and tast test camera.HAL3PortraitMode &
    camera.CaptureUnittests on kohaku
    
    Change-Id: I6db8ae5834c02b4af0e47da644378f892bc09784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3041119
    Commit-Queue: Cheng-Hao Yang <chenghaoyang@chromium.org>
    Tested-by: Cheng-Hao Yang <chenghaoyang@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>

--- camera/common/camera_mojo_channel_manager_impl.cc
+++ camera/common/camera_mojo_channel_manager_impl.cc
@@ -26,6 +26,10 @@ namespace cros {
 
 namespace {
 
+constexpr char kServerTokenPath[] = "/run/camera_tokens/server/token";
+constexpr char kServerSensorClientTokenPath[] =
+    "/run/camera_tokens/server/sensor_client_token";
+
 constexpr ino_t kInvalidInodeNum = 0;
 
 // Gets the socket file by |socket_path| and checks if it is in correct group
@@ -56,10 +60,8 @@ ino_t GetSocketInodeNumber(const base::FilePath& socket_path) {
   return st.st_ino;
 }
 
-base::UnguessableToken ReadServerToken() {
-  static constexpr char kServerTokenPath[] = "/run/camera_tokens/server/token";
-
-  base::FilePath token_path(kServerTokenPath);
+base::UnguessableToken ReadToken(std::string path) {
+  base::FilePath token_path(path);
   std::string token_string;
   if (!base::ReadFileToString(token_path, &token_string)) {
     LOGF(ERROR) << "Failed to read server token";
@@ -98,6 +100,8 @@ CameraMojoChannelManagerImpl::CameraMojoChannelManagerImpl()
 
 CameraMojoChannelManagerImpl::~CameraMojoChannelManagerImpl() {
   if (ipc_thread_.IsRunning()) {
+    base::AutoLock lock(sensor_lock_);
+    sensor_hal_client_.reset();
     ipc_thread_.task_runner()->PostTask(
         FROM_HERE,
         base::Bind(&CameraMojoChannelManagerImpl::TearDownMojoEnvOnIpcThread,
@@ -203,6 +207,29 @@ CameraMojoChannelManagerImpl::CreateCameraAlgorithmOpsRemote(
   return algorithm_ops;
 }
 
+SensorHalClient* CameraMojoChannelManagerImpl::GetSensorHalClient() {
+  base::AutoLock lock(sensor_lock_);
+  if (!sensor_hal_client_) {
+    sensor_hal_client_ = std::make_unique<SensorHalClientImpl>(this);
+  }
+  return sensor_hal_client_.get();
+}
+
+void CameraMojoChannelManagerImpl::RegisterSensorHalClient(
+    mojo::PendingRemote<mojom::SensorHalClient> client,
+    mojom::CameraHalDispatcher::RegisterSensorClientWithTokenCallback
+        on_construct_callback,
+    Callback on_error_callback) {
+  sensor_hal_client_task_ = {
+      .pendingReceiverOrRemote = std::move(client),
+      .on_construct_callback = std::move(on_construct_callback),
+      .on_error_callback = std::move(on_error_callback)};
+  ipc_thread_.task_runner()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&CameraMojoChannelManagerImpl::TryConnectToDispatcher,
+                     base::Unretained(this)));
+}
+
 void CameraMojoChannelManagerImpl::OnSocketFileStatusChange(
     const base::FilePath& socket_path, bool error) {
   if (error) {
@@ -281,7 +308,7 @@ void CameraMojoChannelManagerImpl::TryConsumePendingMojoTasks() {
   VLOGF_ENTER();
 
   if (camera_hal_server_task_.pendingReceiverOrRemote) {
-    auto server_token = ReadServerToken();
+    auto server_token = ReadToken(kServerTokenPath);
     if (server_token.is_empty()) {
       LOGF(ERROR) << "Failed to read server token";
       std::move(camera_hal_server_task_.on_construct_callback)
@@ -294,6 +321,22 @@ void CameraMojoChannelManagerImpl::TryConsumePendingMojoTasks() {
           std::move(camera_hal_server_task_.pendingReceiverOrRemote),
           std::move(token),
           std::move(camera_hal_server_task_.on_construct_callback));
+    }
+  }
+
+  if (sensor_hal_client_task_.pendingReceiverOrRemote) {
+    auto server_sensor_client_token = ReadToken(kServerSensorClientTokenPath);
+    if (server_sensor_client_token.is_empty()) {
+      LOGF(ERROR) << "Failed to read server token for sensor";
+      std::move(sensor_hal_client_task_.on_construct_callback).Run(-EPERM);
+    } else {
+      auto token = mojo_base::mojom::UnguessableToken::New();
+      token->high = server_sensor_client_token.GetHighForSerialization();
+      token->low = server_sensor_client_token.GetLowForSerialization();
+      dispatcher_->RegisterSensorClientWithToken(
+          std::move(sensor_hal_client_task_.pendingReceiverOrRemote),
+          std::move(token),
+          std::move(sensor_hal_client_task_.on_construct_callback));
     }
   }
 
@@ -329,6 +372,11 @@ void CameraMojoChannelManagerImpl::ResetDispatcherPtr() {
   if (camera_hal_server_task_.on_error_callback) {
     std::move(camera_hal_server_task_.on_error_callback).Run();
     camera_hal_server_task_ = {};
+  }
+
+  if (sensor_hal_client_task_.on_error_callback) {
+    std::move(sensor_hal_client_task_.on_error_callback).Run();
+    sensor_hal_client_task_ = {};
   }
 
   for (auto& task : jda_tasks_) {
