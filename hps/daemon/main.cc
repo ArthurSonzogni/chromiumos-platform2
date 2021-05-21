76c6424d8968313de1012a827a0fbe6bd9de4b30
Andrew McRae <amcrae@google.com>
2021-05-21 10:22:40 +1000

    hps: Use CLI options for daemon configuration
    Use CLI flags for configuring hpsd.
    
    BUG=b:186610248
    TEST=Build hpsd.
    
    Change-Id: Ib6384aa1ed9169a1794c85e9b500458e77f89c55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2910217
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Alan Green <avg@chromium.org>
    Commit-Queue: Andrew McRae <amcrae@google.com>
    Tested-by: Andrew McRae <amcrae@google.com>

--- hps/daemon/main.cc
+++ hps/daemon/main.cc
@@ -8,8 +8,8 @@
 #include <string.h>
 
 #include <base/check.h>
+#include <base/command_line.h>
 #include <base/logging.h>
-#include <base/strings/string_number_conversions.h>
 #include <base/task/thread_pool/thread_pool_instance.h>
 #include <brillo/flag_helper.h>
 #include <brillo/syslog_logging.h>
@@ -19,20 +19,12 @@
 #include "hps/lib/i2c.h"
 #include "hps/lib/uart.h"
 
-namespace {
-
-// Extract I2C address from env I2C_ADDR.
-uint8_t GetI2CAddress() {
-  const char* i2c_addr = getenv("I2C_ADDR");
-  CHECK(i2c_addr) << "Missing I2C address (I2C_ADDR)";
-  unsigned address;
-  CHECK(base::StringToUint(i2c_addr, &address)) << "Illegal I2C address";
-  return address;
-}
-
-}  //  namespace
-
 int main(int argc, char* argv[]) {
+  DEFINE_string(bus, "/dev/i2c-2", "I2C device");
+  DEFINE_uint32(addr, 0x30, "I2C address of module");
+  DEFINE_bool(ftdi, false, "Use FTDI connection");
+  DEFINE_bool(test, false, "Use internal test fake");
+  DEFINE_string(uart, "", "Use UART connection");
   brillo::FlagHelper::Init(argc, argv, "hps_daemon - HPS services daemon");
 
   // Always log to syslog and log to stderr if we are connected to a tty.
@@ -41,27 +33,20 @@ int main(int argc, char* argv[]) {
   base::ThreadPoolInstance::CreateAndStartWithDefaultParams(
       "hps_daemon_thread_pool");
 
-  // Retrieve the DevInterface provider string.
-  const char* dev_type = getenv("HPS_HW");
-  CHECK(dev_type) << "Missing hardware device (HPS_HW)";
-  // Create the selected h/w device.
+  // Determine the hardware connection.
   std::unique_ptr<hps::DevInterface> dev;
-  if (strcmp(dev_type, "ftdi") == 0) {
-    dev = hps::Ftdi::Create(GetI2CAddress());
-  } else if (strcmp(dev_type, "test") == 0) {
+  if (FLAGS_ftdi) {
+    dev = hps::Ftdi::Create(FLAGS_addr);
+  } else if (FLAGS_test) {
+    // Initialise the fake device as already booted so that
+    // features can be enabled/disabled.
     auto fake = hps::FakeHps::Create();
     fake->SkipBoot();
     dev = fake->CreateDevInterface();
-  } else if (strcmp(dev_type, "uart") == 0) {
-    const char* uart_dev = getenv("UART_DEV");
-    CHECK(uart_dev) << "Missing UART device (UART_DEV)";
-    dev = hps::Uart::Create(uart_dev);
-  } else if (strcmp(dev_type, "i2c") == 0) {
-    const char* i2c_dev = getenv("I2C_BUS");
-    CHECK(i2c_dev) << "Missing I2C device (I2C_BUS)";
-    dev = hps::I2CDev::Create(i2c_dev, GetI2CAddress());
+  } else if (!FLAGS_uart.empty()) {
+    dev = hps::Uart::Create(FLAGS_uart.c_str());
   } else {
-    LOG(FATAL) << "No matching hardware device for HPS_HW (" << dev_type << ")";
+    dev = hps::I2CDev::Create(FLAGS_bus.c_str(), FLAGS_addr);
   }
   CHECK(dev) << "Hardware device failed to initialise";
   LOG(INFO) << "Starting HPS Service.";
