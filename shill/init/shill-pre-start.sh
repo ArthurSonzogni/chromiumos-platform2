acd771346ddd56ac9248bc1b99fab0a1f04ddbb4
Allen Webb <allenwebb@google.com>
2021-05-24 10:27:13 -0500

    shill: Cleanup shill-pre-start.sh
    Since shill now has a tmpfiles.d configuration, clean up
    shill-pre-start.sh.
    
    BUG=chromium:1179755
    TEST=CQ passes
    
    Change-Id: I25b43f392c182d7ad77adaf46f897f33de303973
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2913562
    Tested-by: Allen Webb <allenwebb@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Allen Webb <allenwebb@google.com>

--- shill/init/shill-pre-start.sh
+++ shill/init/shill-pre-start.sh
@@ -5,67 +5,29 @@
 
 bootstat shill-start
 
-# Create state directory for shill.
-mkdir -p /run/shill
-chown -R shill:shill /run/shill
-chmod 0755 /run/shill
+# Double check to make sure shill related paths have been created with the
+# intended ownership and permissions. These paths should already have been
+# handled during boot, but check them again.
+systemd-tmpfiles --create --remove --clean /usr/lib/tmpfiles.d/shill.conf
 
-# Create state directory for IPsec
-mkdir -p /run/ipsec
-chown -R vpn:vpn /run/ipsec
-chmod 0770 /run/ipsec
+# tmpfiles.d does not handle chown/chmod -R because of unsafe ownership
+# transitions.
+(
+  # Log any cases where the old behavior actually results in a change,
+  # for /run paths until we are sure these are not needed.
+  chown -c -R shill:shill /run/shill
 
-# Create state directory for entire L2TP/IPsec subtree
-mkdir -p /run/l2tpipsec_vpn
-chown -R vpn:vpn /run/l2tpipsec_vpn
-chmod 0770 /run/l2tpipsec_vpn
+  chown -c -R vpn:vpn \
+    /run/ipsec \
+    /run/l2tpipsec_vpn \
+    /run/wireguard
 
-# Create state directory for wireguard
-mkdir -p /run/wireguard
-chown -R vpn:vpn /run/wireguard
-chmod 0770 /run/wireguard
+  chown -c -R dhcp:dhcp /run/dhcpcd
+) | logger -p err -t "shill-pre-start"
 
-# Create storage for the shill global profile.
-mkdir -p /var/cache/shill
-chown -R shill:shill /var/cache/shill
-chmod 0755 /var/cache/shill
+chown -R shill:shill \
+  /var/cache/shill \
+  /var/lib/shill/metrics
 
-# Set up dhcpcd's /var/{lib|run} dirs to run as user 'dhcp'.
-mkdir -m 0755 -p /var/lib/dhcpcd
-mkdir -m 0755 -p /run/dhcpcd
-chmod -R u+rwX,g+rX,o+rX /var/lib/dhcpcd
 chown -R dhcp:dhcp /var/lib/dhcpcd
-chown -R dhcp:dhcp /run/dhcpcd
-
-# Shill needs read access to this file, which is part of the in-kernel
-# Connection Tracking System.
-chown root:shill /proc/net/ip_conntrack
-chmod g+r /proc/net/ip_conntrack
-
-# Shill needs kernel debug access
-if [ -e /sys/kernel/debug/ieee80211/phy0/iwlwifi/iwlmvm/fw_dbg_collect ]; then
- chown root:shill /sys/kernel/debug/ieee80211/phy0/iwlwifi/iwlmvm/fw_dbg_collect
- chmod g+w /sys/kernel/debug/ieee80211/phy0/iwlwifi/iwlmvm/fw_dbg_collect
-fi
-
-# Create private directory for data which needs to persists across sessions.
-mkdir -p /var/lib/shill
-
-# Create directory for backing files for metrics.
-# mkdir before `chown shill` to remove requirement of dac_override capability.
-mkdir -p /var/lib/shill/metrics
-
-# Set permissions for /var/lib/shill
-chown shill:shill /var/lib/shill
-chmod 0755 /var/lib/shill
-
-# Set permissions for /var/lib/shill/metrics
-chown -R shill:shill /var/lib/shill/metrics
-chmod 0755 /var/lib/shill/metrics
-
-# TODO(mortonm): Previous versions of this code used the
-# shill_sandboxing_{enabled/disabled} files when sandboxing was being
-# rolled out. These lines clean up those leftover files. Remove them
-# after M78 branches.
-rm -f /var/lib/shill/shill_sandboxing_enabled
-rm -f /var/lib/shill/shill_sandboxing_disabled
+chmod -R u+rwX,g+rX,o+rX /var/lib/dhcpcd
