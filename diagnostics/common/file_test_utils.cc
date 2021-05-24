9bf4aa8ec42f064a20ae6d70b08004e69a331bd8
Chung-Sheng Wu <chungsheng@google.com>
2021-05-24 13:38:32 +0800

    diagnostics: Add BaseFileTest for file related testing
    Add |BaseFileTest| to provide some interfaces for creating the test
    rootfs for testing. |BaseFileTest::PathType| is a helper type which can
    implicitly convert |std::string| and |{str, str...}| into
    |base::FilePath|. This can reduce many type converting in the test file.
    
    BUG=b:189182183
    TEST=FEATURES=test emerge-$BOARD diagnostics
    
    Change-Id: I931e0dc706eb1b952ffb33167f6e87a1737bfbb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2920943
    Tested-by: Chung-Sheng Wu <chungsheng@chromium.org>
    Reviewed-by: Meng-Huan Yu <menghuan@chromium.org>
    Reviewed-by: Yilin Yang (kerker) <kerker@chromium.org>
    Commit-Queue: Chung-Sheng Wu <chungsheng@chromium.org>

--- diagnostics/common/file_test_utils.cc
+++ diagnostics/common/file_test_utils.cc
@@ -4,8 +4,12 @@
 
 #include "diagnostics/common/file_test_utils.h"
 
+#include <string>
+
+#include <base/check.h>
+#include <base/files/file_path.h>
 #include <base/files/file_util.h>
-#include <base/logging.h>
+#include <base/files/scoped_temp_dir.h>
 
 namespace diagnostics {
 
@@ -33,6 +37,48 @@ bool WriteFileAndCreateSymbolicLink(const base::FilePath& file_path,
   if (!base::CreateDirectory(symlink_path.DirName()))
     return false;
   return base::CreateSymbolicLink(file_path, symlink_path);
+}
+
+BaseFileTest::PathType::PathType(std::initializer_list<std::string> paths) {
+  auto it = paths.begin();
+  file_path_ = base::FilePath(*it);
+  for (++it; it != paths.end(); ++it) {
+    file_path_ = file_path_.Append(*it);
+  }
+}
+
+void BaseFileTest::CreateTestRoot() {
+  ASSERT_TRUE(root_dir_.empty());
+  ASSERT_TRUE(scoped_temp_dir_.CreateUniqueTempDir());
+  SetTestRoot(scoped_temp_dir_.GetPath());
+  ASSERT_FALSE(root_dir_.empty());
+}
+
+void BaseFileTest::SetTestRoot(const base::FilePath& path) {
+  ASSERT_TRUE(root_dir_.empty());
+  ASSERT_FALSE(path.empty());
+  root_dir_ = path;
+}
+
+void BaseFileTest::UnsetPath(const PathType& path) const {
+  ASSERT_FALSE(root_dir_.empty());
+  ASSERT_TRUE(base::DeletePathRecursively(GetPathUnderRoot(path)));
+}
+
+base::FilePath BaseFileTest::GetPathUnderRoot(const PathType& path) const {
+  CHECK(!root_dir_.empty());
+  // Check if the path already under the test rootfs.
+  CHECK(!root_dir_.IsParent(path.file_path()));
+  if (!path.file_path().IsAbsolute())
+    return root_dir_.Append(path.file_path());
+  auto res = root_dir_;
+  CHECK(base::FilePath("/").AppendRelativePath(path.file_path(), &res));
+  return res;
+}
+
+const base::FilePath& BaseFileTest::root_dir() const {
+  CHECK(!root_dir_.empty());
+  return root_dir_;
 }
 
 }  // namespace diagnostics
