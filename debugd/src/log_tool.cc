3b344fe4a9db44596a6e67d39d49db4f8de0e791
Prashant Malani <pmalani@chromium.org>
2021-05-26 14:09:05 -0700

    debugd: Make USB4 helper root user
    This is needed to allow the helper binary to access some Thunderbolt
    driver debugfs files
    
    BUG=b:185277647
    TEST=chrome://system shows the Thunderbolt controller type.
    
    Change-Id: I02ef360a62646a564f5d00f1aa2ebfdc57603cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2920709
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Prashant Malani <pmalani@chromium.org>
    Commit-Queue: Prashant Malani <pmalani@chromium.org>

--- debugd/src/log_tool.cc
+++ debugd/src/log_tool.cc
@@ -369,7 +369,7 @@ const std::vector<Log> kCommandLogs {
   {kFile, "upstart", "/var/log/upstart.log"},
   {kCommand, "uptime", "/usr/bin/cut -d' ' -f1 /proc/uptime"},
   {kCommand, "usb4 devices", "/usr/libexec/debugd/helpers/usb4_devinfo_helper",
-    SandboxedProcess::kDefaultUser, kDebugfsGroup},
+    kRoot, kDebugfsGroup},
   {kFile, "verified boot", "/var/log/debug_vboot_noisy.log"},
   {kFile, "vmlog.1.LATEST", "/var/log/vmlog/vmlog.1.LATEST"},
   {kFile, "vmlog.1.PREVIOUS", "/var/log/vmlog/vmlog.1.PREVIOUS"},
