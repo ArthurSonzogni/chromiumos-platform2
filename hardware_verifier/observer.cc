d5f6fcf8fe94f66254621f98c2a5a803f2b2c363
Kevin Lin <kevinptt@chromium.org>
2021-06-03 20:22:25 +0800

    hardware_verifier: Record storage to structured metrics
    This CL add storage component to the structured metrics.  Also add the
    metric "ComponentCategory" to identify the category of components.
    
    BUG=b:183682775
    TEST=test on DUT
    
    Change-Id: Ia395f62d233de5092b56012c0d7714d8494e97fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2936741
    Reviewed-by: Clark Chung <ckclark@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Tested-by: Kevin Lin <kevinptt@chromium.org>
    Commit-Queue: Kevin Lin <kevinptt@chromium.org>

--- hardware_verifier/observer.cc
+++ hardware_verifier/observer.cc
@@ -23,6 +23,82 @@
 
 namespace hardware_verifier {
 
+namespace {
+
+using StructuredComponentInfo =
+    metrics::structured::events::hardware_verifier::ComponentInfo;
+using StructuredHwVerificationReport =
+    metrics::structured::events::hardware_verifier::HwVerificationReport;
+
+StructuredComponentInfo CreateStructuredComponentInfo(
+    const runtime_probe::Edid::Fields& device) {
+  runtime_probe::Edid::Vendor vendor = runtime_probe::Edid::VENDOR_UNKNOWN;
+  if (!runtime_probe::Edid::Vendor_Parse(
+          base::StringPrintf("VENDOR_%s", device.vendor().c_str()), &vendor)) {
+    VLOG(3) << "Unknown EDID vendor : " << device.vendor();
+  }
+  return StructuredComponentInfo()
+      .SetComponentCategory(
+          runtime_probe::ProbeRequest_SupportCategory_display_panel)
+      .SetDisplayPanelVendor(vendor)
+      .SetDisplayPanelProductId(device.product_id())
+      .SetDisplayPanelHeight(device.height())
+      .SetDisplayPanelWidth(device.width());
+}
+
+StructuredComponentInfo CreateStructuredComponentInfo(
+    const runtime_probe::Storage::Fields& device) {
+  auto info = StructuredComponentInfo();
+  if (device.type() == "MMC") {
+    info.SetComponentCategory(
+            runtime_probe::ProbeRequest_SupportCategory_storage)
+        .SetStorageMmcManfid(device.mmc_manfid())
+        .SetStorageMmcHwrev(device.mmc_hwrev())
+        .SetStorageMmcOemid(device.mmc_oemid())
+        .SetStorageMmcPrv(device.mmc_prv());
+  } else if (device.type() == "NVMe") {
+    info.SetComponentCategory(
+            runtime_probe::ProbeRequest_SupportCategory_storage)
+        .SetStoragePciVendor(device.pci_vendor())
+        .SetStoragePciDevice(device.pci_device())
+        .SetStoragePciClass(device.pci_class());
+  } else if (device.type() == "ATA") {
+    // Do nothing since all fields for ATA are string type.
+  }
+  return info;
+}
+
+void RecordStructuredHwVerificationReport(const HwVerificationReport& report) {
+  auto report_event = StructuredHwVerificationReport();
+  report_event.SetIsCompliant(report.is_compliant());
+  for (auto i = 0; i < report.found_component_infos_size(); i++) {
+    const auto& info = report.found_component_infos(i);
+    const auto& qualification_status = info.qualification_status();
+    switch (info.component_category()) {
+      case runtime_probe::ProbeRequest_SupportCategory_display_panel:
+        report_event.SetQualificationStatusDisplayPanel(qualification_status);
+        break;
+      case runtime_probe::ProbeRequest_SupportCategory_storage:
+        report_event.SetQualificationStatusStorage(qualification_status);
+        break;
+      default:
+        break;
+    }
+  }
+  report_event.Record();
+
+  for (const auto& device : report.generic_device_info().display_panel()) {
+    CreateStructuredComponentInfo(device).Record();
+  }
+  for (const auto& device : report.generic_device_info().storage()) {
+    auto event = CreateStructuredComponentInfo(device);
+    if (!event.metrics().empty())
+      event.Record();
+  }
+}
+
+}  // namespace
+
 Observer* Observer::GetInstance() {
   static base::NoDestructor<Observer> instance;
   return instance.get();
@@ -58,15 +134,12 @@ void Observer::SetMetricsLibrary(
 }
 
 void Observer::RecordHwVerificationReport(const HwVerificationReport& report) {
-  auto sm_verification_report =
-      metrics::structured::events::hardware_verifier::HwVerificationReport();
   {
     auto key = base::StrCat({kMetricVerifierReportPrefix, "IsCompliant"});
     LOG(INFO) << key << ": " << report.is_compliant();
     if (metrics_) {
       metrics_->SendBoolToUMA(key, report.is_compliant());
     }
-    sm_verification_report.SetIsCompliant(report.is_compliant());
   }
 
   for (auto i = 0; i < report.found_component_infos_size(); i++) {
@@ -84,30 +157,8 @@ void Observer::RecordHwVerificationReport(const HwVerificationReport& report) {
       metrics_->SendEnumToUMA(uma_key, qualification_status,
                               QualificationStatus_ARRAYSIZE);
     }
-
-    if (info.component_category() ==
-        runtime_probe::ProbeRequest_SupportCategory_display_panel) {
-      sm_verification_report.SetQualificationStatusDisplayPanel(
-          qualification_status);
-    }
   }
-  sm_verification_report.Record();
-
-  for (const auto& device : report.generic_device_info().display_panel()) {
-    runtime_probe::Edid::Vendor vendor;
-    if (!runtime_probe::Edid::Vendor_Parse(
-            base::StringPrintf("VENDOR_%s", device.vendor().c_str()),
-            &vendor)) {
-      VLOG(3) << "Unknown EDID vendor : " << device.vendor();
-      vendor = runtime_probe::Edid::VENDOR_UNKNOWN;
-    }
-    metrics::structured::events::hardware_verifier::ComponentInfo()
-        .SetDisplayPanelVendor(vendor)
-        .SetDisplayPanelProductId(device.product_id())
-        .SetDisplayPanelHeight(device.height())
-        .SetDisplayPanelWidth(device.width())
-        .Record();
-  }
+  RecordStructuredHwVerificationReport(report);
 }
 
 }  // namespace hardware_verifier
