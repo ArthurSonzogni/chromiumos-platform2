03fdf6b1b2d69f4e3ba075005fd8b378dc8def11
Yi Chou <yich@google.com>
2021-06-16 20:21:59 +0800

    cryptohome: parallel the scrypt and load key steps
    The scrypt step in cryptohome takes about 80ms on soraka, and the
    loading key operationa takes about 100ms ~ 200ms(due to the handle
    context switching inside trunks).
    These two step didn't consume same resource (CPU/TPM), paralleling
    these two steps could reduce the mount time about 80ms.
    
    BUG=b:188742402
    TEST=unittest of cryptohome
    TEST=tast run $DUT hwsec.*
    
    Change-Id: I824d6adc568fb4cbadddb5c8a77621f75a60477c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2966434
    Tested-by: Yi Chou <yich@google.com>
    Commit-Queue: Yi Chou <yich@google.com>
    Reviewed-by: Betul Soysal <betuls@google.com>
    Reviewed-by: Leo Lai <cylai@google.com>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>

--- cryptohome/tpm_bound_to_pcr_auth_block.h
+++ cryptohome/tpm_bound_to_pcr_auth_block.h
@@ -7,10 +7,12 @@
 
 #include "cryptohome/auth_block.h"
 
+#include <memory>
 #include <string>
 
 #include <base/gtest_prod_util.h>
 #include <base/macros.h>
+#include <base/threading/thread.h>
 
 #include "cryptohome/crypto.h"
 #include "cryptohome/cryptohome_key_loader.h"
@@ -48,6 +50,11 @@ class TpmBoundToPcrAuthBlock : public AuthBlock {
   Tpm* tpm_;
   CryptohomeKeyLoader* cryptohome_key_loader_;
   TpmAuthBlockUtils utils_;
+
+  // The thread for performing scrypt operations.
+  std::unique_ptr<base::Thread> scrypt_thread_;
+  // The task runner that belongs to the scrypt thread.
+  scoped_refptr<base::SingleThreadTaskRunner> scrypt_task_runner_;
 
   FRIEND_TEST_ALL_PREFIXES(TPMAuthBlockTest, DecryptBoundToPcrTest);
 };
