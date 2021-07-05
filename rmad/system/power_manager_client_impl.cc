f96e0208ea0acd534b54d4b838d56b65f0ea7a3e
Cheng-Han Yang <chenghan@google.com>
2021-07-06 03:00:36 +0800

    rmad: Add power_manager dbus client
    Add PowerManagerClient class to call power_manager dbus interface.
    
    BUG=b:187196699
    TEST=emerge-volteer rmad;
         cros_run_unit_tests --board volteer --packages rmad
    
    Change-Id: I7d76baa331d35dde39f34c257af8ba4d9bbdfc46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3006299
    Tested-by: Cheng-Han Yang <chenghan@chromium.org>
    Commit-Queue: Cheng-Han Yang <chenghan@chromium.org>
    Reviewed-by: Gene Chang <genechang@google.com>

---
+++ rmad/system/power_manager_client_impl.cc
@@ -0,0 +1,61 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "rmad/system/power_manager_client_impl.h"
+
+#include <base/logging.h>
+#include <dbus/message.h>
+#include <dbus/object_proxy.h>
+#include <dbus/power_manager/dbus-constants.h>
+
+namespace {
+
+constexpr int kDefaultTimeoutMs = 10 * 1000;  // 10 seconds.
+
+}  // namespace
+
+namespace rmad {
+
+PowerManagerClientImpl::PowerManagerClientImpl(
+    const scoped_refptr<dbus::Bus>& bus) {
+  proxy_ = bus->GetObjectProxy(
+      power_manager::kPowerManagerServiceName,
+      dbus::ObjectPath(power_manager::kPowerManagerServicePath));
+}
+
+bool PowerManagerClientImpl::Restart() {
+  dbus::MethodCall method_call(power_manager::kPowerManagerInterface,
+                               power_manager::kRequestRestartMethod);
+  dbus::MessageWriter writer(&method_call);
+  writer.AppendInt32(power_manager::REQUEST_RESTART_OTHER);
+  writer.AppendString("rmad request restart");
+
+  std::unique_ptr<dbus::Response> response =
+      proxy_->CallMethodAndBlock(&method_call, kDefaultTimeoutMs);
+
+  if (!response.get()) {
+    LOG(ERROR) << "Failed to call powerd service";
+    return false;
+  }
+  return true;
+}
+
+bool PowerManagerClientImpl::Shutdown() {
+  dbus::MethodCall method_call(power_manager::kPowerManagerInterface,
+                               power_manager::kRequestShutdownMethod);
+  dbus::MessageWriter writer(&method_call);
+  writer.AppendInt32(power_manager::REQUEST_SHUTDOWN_OTHER);
+  writer.AppendString("rmad request shutdown");
+
+  std::unique_ptr<dbus::Response> response =
+      proxy_->CallMethodAndBlock(&method_call, kDefaultTimeoutMs);
+
+  if (!response.get()) {
+    LOG(ERROR) << "Failed to call powerd service";
+    return false;
+  }
+  return true;
+}
+
+}  // namespace rmad
