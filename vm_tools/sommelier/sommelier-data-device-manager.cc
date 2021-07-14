ArthurSonzogni/chromiumos-platform2

commit a0080bfa2e72348a26c673d3edd38c9dd15bf21e

vm_tools: sommelier: Add a fuzzer
Current limitations:
 - The fuzzer will only send messages without file descriptors. This
   excludes a large portion of the code-base from testing.
 - The fuzzer only covers the pure wayland proxy mode, as the way
   libxcb handles async processing makes fuzzing it much harder
 - This CL doesn't attempt to fix leak or crash issues beyond the
   minimum required for the fuzzer to run at all.
 - The fuzzer is expected to find lots of more-or-less junk results as
   sommelier uses assert statements improperly and leaks memory everywhere.

BUG=chromium:1225104
TEST=Ran fuzzer

Change-Id: Id5f063447d2208ae81085f96159e137d71eac476
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3000303
Commit-Queue: Fergus Dall <sidereal@google.com>
Tested-by: Fergus Dall <sidereal@google.com>
Reviewed-by: Nicholas Verne <nverne@chromium.org>

vm_tools/sommelier/sommelier-data-device-manager.cc
@@ -43,15 +43,13 @@ struct sl_data_transfer {
   size_t offset;
   size_t bytes_left;
   uint8_t data[4096];
-  struct wl_event_source* read_event_source;
-  struct wl_event_source* write_event_source;
+  std::unique_ptr<struct wl_event_source> read_event_source;
+  std::unique_ptr<struct wl_event_source> write_event_source;
 };
 
 static void sl_data_transfer_destroy(struct sl_data_transfer* transfer) {
-  assert(transfer->read_event_source);
-  wl_event_source_remove(transfer->read_event_source);
-  assert(transfer->write_event_source);
-  wl_event_source_remove(transfer->write_event_source);
+  transfer->read_event_source.reset();
+  transfer->write_event_source.reset();
   close(transfer->read_fd);
   close(transfer->write_fd);
   free(transfer);
@@ -87,8 +85,9 @@ static int sl_handle_data_transfer_read(int fd, uint32_t mask, void* data) {
     transfer->offset = 0;
     // There may still be data to read from the event source, but we have no
     // room in our buffer so move to the writing state.
-    wl_event_source_fd_update(transfer->read_event_source, 0);
-    wl_event_source_fd_update(transfer->write_event_source, WL_EVENT_WRITABLE);
+    wl_event_source_fd_update(transfer->read_event_source.get(), 0);
+    wl_event_source_fd_update(transfer->write_event_source.get(),
+                              WL_EVENT_WRITABLE);
   } else {
     // On a read error or EOF, end the transfer.
     sl_data_transfer_destroy(transfer);
@@ -127,8 +126,9 @@ static int sl_handle_data_transfer_write(int fd, uint32_t mask, void* data) {
 
   if (!transfer->bytes_left) {
     // If all data has been written, move back to the reading state.
-    wl_event_source_fd_update(transfer->write_event_source, 0);
-    wl_event_source_fd_update(transfer->read_event_source, WL_EVENT_READABLE);
+    wl_event_source_fd_update(transfer->write_event_source.get(), 0);
+    wl_event_source_fd_update(transfer->read_event_source.get(),
+                              WL_EVENT_READABLE);
   }
 
   // If there is still data left, continue in the writing state.
@@ -154,11 +154,11 @@ static void sl_data_transfer_create(struct wl_event_loop* event_loop,
   transfer->write_fd = write_fd;
   transfer->offset = 0;
   transfer->bytes_left = 0;
-  transfer->read_event_source =
+  transfer->read_event_source.reset(
       wl_event_loop_add_fd(event_loop, read_fd, WL_EVENT_READABLE,
-                           sl_handle_data_transfer_read, transfer);
-  transfer->write_event_source = wl_event_loop_add_fd(
-      event_loop, write_fd, 0, sl_handle_data_transfer_write, transfer);
+                           sl_handle_data_transfer_read, transfer));
+  transfer->write_event_source.reset(wl_event_loop_add_fd(
+      event_loop, write_fd, 0, sl_handle_data_transfer_write, transfer));
 }
 
 static void sl_data_offer_accept(struct wl_client* client,

vm_tools/sommelier/BUILD.gn
@@ -10,6 +10,9 @@ group("all") {
     ":sommelier",
     ":sommelier_test",
   ]
+  if (use.fuzzer) {
+    deps += [ ":sommelier_wayland_fuzzer" ]
+  }
 }
 
 # Set this to the Xwayland path.
@@ -142,5 +145,15 @@ executable("sommelier_test") {
     "gtest",
   ]
   defines = sommelier_defines
+  deps = [ ":libsommelier" ]
+}
+
+executable("sommelier_wayland_fuzzer") {
+  sources = [ "sommelier-wayland-fuzzer.cc" ]
+
+  configs += [ "//common-mk/common_fuzzer" ]
+
+  defines = sommelier_defines
+
   deps = [ ":libsommelier" ]
 }

vm_tools/sommelier/sommelier-ctx.cc
@@ -343,12 +343,12 @@ bool sl_context_init_virtwl(struct sl_context* ctx,
       return false;
     }
 
-    ctx->virtwl_socket_event_source = wl_event_loop_add_fd(
+    ctx->virtwl_socket_event_source.reset(wl_event_loop_add_fd(
         event_loop, ctx->virtwl_socket_fd, WL_EVENT_READABLE,
-        sl_handle_virtwl_socket_event, ctx);
-    ctx->virtwl_ctx_event_source =
+        sl_handle_virtwl_socket_event, ctx));
+    ctx->virtwl_ctx_event_source.reset(
         wl_event_loop_add_fd(event_loop, ctx->virtwl_ctx_fd, WL_EVENT_READABLE,
-                             sl_handle_virtwl_ctx_event, ctx);
+                             sl_handle_virtwl_ctx_event, ctx));
   }
   return true;
 }

vm_tools/sommelier/sommelier-util.cc
@@ -21,3 +21,12 @@ __attribute__((__format__(__printf__, 1, 0))) char* sl_xasprintf(
   va_end(args);
   return str;
 }
+
+#define DEFAULT_DELETER(TypeName, DeleteFunction)            \
+  namespace std {                                            \
+  void default_delete<TypeName>::operator()(TypeName* ptr) { \
+    DeleteFunction(ptr);                                     \
+  }                                                          \
+  }
+
+DEFAULT_DELETER(struct wl_event_source, wl_event_source_remove);

vm_tools/sommelier/sommelier-util.h
@@ -6,6 +6,9 @@
 #define VM_TOOLS_SOMMELIER_SOMMELIER_UTIL_H_
 
 #include <assert.h>
+#include <memory>
+
+#include <wayland-server.h>
 
 #define errno_assert(rv)                                          \
   {                                                               \
@@ -23,5 +26,15 @@
 // taking a double pointer argument like asprintf.
 __attribute__((__format__(__printf__, 1, 0))) char* sl_xasprintf(
     const char* fmt, ...);
+
+#define DEFAULT_DELETER_FDECL(TypeName) \
+  namespace std {                       \
+  template <>                           \
+  struct default_delete<TypeName> {     \
+    void operator()(TypeName* ptr);     \
+  };                                    \
+  }
+
+DEFAULT_DELETER_FDECL(struct wl_event_source);
 
 #endif  // VM_TOOLS_SOMMELIER_SOMMELIER_UTIL_H_

vm_tools/sommelier/sommelier-wayland-fuzzer.cc
@@ -1,0 +1,244 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cstddef>
+#include <cstdint>
+#include <dirent.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include <wayland-client.h>
+
+#include "sommelier.h"                       // NOLINT(build/include_directory)
+#include "sommelier-ctx.h"                   // NOLINT(build/include_directory)
+#include "virtualization/wayland_channel.h"  // NOLINT(build/include_directory)
+
+class FuzzChannel : public WaylandChannel {
+ private:
+  int recv_fd = -1;
+
+ public:
+  int send_fd = -1;
+
+  ~FuzzChannel() {
+    if (send_fd != -1) {
+      close(send_fd);
+    }
+    if (recv_fd != -1) {
+      close(recv_fd);
+    }
+  }
+
+  int32_t init() override { return 0; }
+
+  bool supports_dmabuf() override { return false; }
+
+  int32_t create_context(int* out_socket_fd) override {
+    int sv[2];
+    if (socketpair(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0, sv)) {
+      return -errno;
+    }
+
+    send_fd = sv[0];
+    recv_fd = sv[1];
+    *out_socket_fd = sv[1];
+    return 0;
+  }
+
+  int32_t create_pipe(int* out_pipe_fd) override { return -1; }
+
+  int32_t send(const struct WaylandSendReceive& send) override { return 0; }
+
+  int32_t receive(struct WaylandSendReceive& receive) override {
+    uint8_t* buffer = static_cast<uint8_t*>(malloc(4096));
+    int bytes = recv(receive.socket_fd, buffer, 4096, 0);
+    if (bytes < 0) {
+      return -errno;
+    }
+
+    receive.data = buffer;
+    receive.data_size = bytes;
+    receive.num_fds = 0;
+
+    return 0;
+  }
+
+  int32_t allocate(const struct WaylandBufferCreateInfo& create_info,
+                   struct WaylandBufferCreateOutput& create_output) override {
+    return -1;
+  }
+
+  int32_t sync(int dmabuf_fd, uint64_t flags) override { return 0; }
+};
+
+void null_logger(const char*, va_list) {}
+
+class Environment {
+ public:
+  Environment() {
+    wl_log_set_handler_client(null_logger);
+    wl_log_set_handler_server(null_logger);
+  }
+};
+
+static int handle_host_to_sommelier_event(int fd, uint32_t mask, void* data) {
+  struct sl_context* ctx = (struct sl_context*)data;
+  int count = 0;
+
+  if ((mask & WL_EVENT_HANGUP) || (mask & WL_EVENT_ERROR)) {
+    return 0;
+  }
+
+  if (mask & WL_EVENT_READABLE)
+    count = wl_display_dispatch(ctx->display);
+  if (mask & WL_EVENT_WRITABLE)
+    wl_display_flush(ctx->display);
+
+  if (mask == 0) {
+    count = wl_display_dispatch_pending(ctx->display);
+    wl_display_flush(ctx->display);
+  }
+
+  return count;
+}
+
+int drain_socket(int fd, uint32_t mask, void* data) {
+  uint8_t buffer[4096];
+  return recv(fd, buffer, 4096, 0) > 0;
+}
+
+// Count the number of open file descriptors to make sure we don't leak any.
+// Aborts on error, as this should never fail.
+int count_fds() {
+  DIR* dir = opendir("/proc/self/fd");
+  if (!dir) {
+    fprintf(stderr, "Failed to open /proc/self/fd: %m\n");
+    abort();
+  }
+
+  // Ignore the "." and ".." entries, along with the fd we just opened.
+  int count = -3;
+
+  // Needed to distinguish between eof and errors.
+  errno = 0;
+  while (struct dirent* dirent = readdir(dir)) {
+    count++;
+  }
+  if (errno) {
+    fprintf(stderr, "Failed to read from /proc/self/fd: %m\n");
+    abort();
+  }
+
+  if (closedir(dir)) {
+    fprintf(stderr, "Failed to close /proc/self/fd: %m\n");
+    abort();
+  }
+
+  return count;
+}
+
+int LLVMFuzzerTestOneInput_real(const uint8_t* data, size_t size) {
+  static Environment env;
+  FuzzedDataProvider source(data, size);
+
+  struct sl_context ctx;
+  FuzzChannel channel;
+  int host_to_sommelier, client_to_sommelier;
+
+  sl_context_init_default(&ctx);
+
+  // Create a connection from the host to sommelier. This is done via a
+  // WaylandChannel implementation that keeps a socketpair internally. One end
+  // goes to sommelier to listen on/write to, the other end is kept by us. The
+  // channel implements send with a no-op, so we don't ever have to read from
+  // our end.
+  ctx.host_display = wl_display_create();
+  struct wl_event_loop* event_loop =
+      wl_display_get_event_loop(ctx.host_display);
+  ctx.channel = &channel;
+  sl_context_init_virtwl(&ctx, event_loop, /*display=*/false);
+  // `display` takes ownership of `virtwl_display_fd`
+  ctx.display = wl_display_connect_to_fd(ctx.virtwl_display_fd);
+
+  // Create a connection from the client to sommelier. One end is passed into
+  // libwayland for sommelier to handle, the other end is owned by the
+  // fuzzer. We set up the event loop to drain any data send by sommelier to our
+  // end, and write fuzz data to our end in the main loop.
+  int sv[2];
+  int ret = socketpair(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0, sv);
+  assert(!ret);
+  // wl_client takes ownership of its file descriptor
+  ctx.client = wl_client_create(ctx.host_display, sv[0]);
+  std::unique_ptr<struct wl_event_source> drain_event(wl_event_loop_add_fd(
+      event_loop, sv[1], WL_EVENT_READABLE, drain_socket, nullptr));
+
+  // Add the host-to-sommelier display to the event loop. The client wayland
+  // library doesn't have an event loop system, instead you're expected to
+  // integrate into another event loop using wl_display_get_fd if you need to.
+  std::unique_ptr<struct wl_event_source> display_event(
+      wl_event_loop_add_fd(event_loop, wl_display_get_fd(ctx.display),
+                           WL_EVENT_READABLE | WL_EVENT_WRITABLE,
+                           handle_host_to_sommelier_event, &ctx));
+
+  // Getting the registry object from the host and listening for events is the
+  // top-level task in sommelier. Everything else is driven by messages from the
+  // host or the client.
+  auto* registry = wl_display_get_registry(ctx.display);
+  wl_registry_add_listener(registry, &sl_registry_listener, &ctx);
+
+  host_to_sommelier = channel.send_fd;
+  client_to_sommelier = sv[1];
+
+  while (source.remaining_bytes() &&
+         !wl_list_empty(wl_display_get_client_list(ctx.host_display)) &&
+         !wl_display_get_error(ctx.display)) {
+    // Randomly pick a connection to write too.
+    int fd = source.ConsumeBool() ? host_to_sommelier : client_to_sommelier;
+
+    // The random length string extractor uses a terminator character to prevent
+    // the boundaries of a message from shifting when the fuzzer inserts or
+    // removed bytes. This is (probably) better then consuming a length and then
+    // consuming that many characters.
+    std::string data = source.ConsumeRandomLengthString(4096);
+
+    int sent = send(fd, data.data(), data.length(), 0);
+    assert(sent == data.length());
+
+    wl_display_flush_clients(ctx.host_display);
+    wl_event_loop_dispatch(event_loop, 0);
+  }
+
+  wl_registry_destroy(registry);
+
+  close(ctx.virtwl_socket_fd);
+  close(client_to_sommelier);
+
+  ctx.virtwl_ctx_event_source.reset();
+  ctx.virtwl_socket_event_source.reset();
+  drain_event.reset();
+  display_event.reset();
+
+  wl_display_destroy_clients(ctx.host_display);
+  wl_display_destroy(ctx.host_display);
+
+  wl_display_disconnect(ctx.display);
+
+  return 0;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  int start_fds = count_fds();
+
+  int ret = LLVMFuzzerTestOneInput_real(data, size);
+
+  int end_fds = count_fds();
+  if (start_fds != end_fds) {
+    fprintf(stderr, "Leaked %d file descriptors!\n", end_fds - start_fds);
+    abort();
+  }
+
+  return ret;
+}

vm_tools/sommelier/sommelier.cc
@@ -812,8 +812,8 @@ static void sl_registry_remover(void* data,
   assert(0);
 }
 
-static const struct wl_registry_listener sl_registry_listener = {
-    sl_registry_handler, sl_registry_remover};
+const struct wl_registry_listener sl_registry_listener = {sl_registry_handler,
+                                                          sl_registry_remover};
 
 static int sl_handle_event(int fd, uint32_t mask, void* data) {
   TRACE_EVENT("other", "sl_handle_event");
@@ -1762,8 +1762,7 @@ static int sl_handle_selection_fd_writable(int fd, uint32_t mask, void* data) {
   free(ctx->selection_property_reply);
   ctx->selection_property_reply = NULL;
   if (ctx->selection_send_event_source) {
-    wl_event_source_remove(ctx->selection_send_event_source);
-    ctx->selection_send_event_source = NULL;
+    ctx->selection_send_event_source.reset();
   }
   if (fd < 0) {
     ctx->selection_data_source_send_fd = -1;
@@ -1783,10 +1782,10 @@ static void sl_write_selection_property(struct sl_context* ctx,
     return;
 
   assert(!ctx->selection_send_event_source);
-  ctx->selection_send_event_source = wl_event_loop_add_fd(
+  ctx->selection_send_event_source.reset(wl_event_loop_add_fd(
       wl_display_get_event_loop(ctx->host_display),
       ctx->selection_data_source_send_fd, WL_EVENT_WRITABLE,
-      sl_handle_selection_fd_writable, ctx);
+      sl_handle_selection_fd_writable, ctx));
 }
 
 static void sl_send_selection_notify(struct sl_context* ctx,
@@ -1880,8 +1879,7 @@ static int sl_handle_selection_fd_readable(int fd, uint32_t mask, void* data) {
     }
   }
 
-  wl_event_source_remove(ctx->selection_event_source);
-  ctx->selection_event_source = NULL;
+  ctx->selection_event_source.reset();
   return 1;
 }
 
@@ -2137,10 +2135,10 @@ static void sl_handle_property_notify(struct sl_context* ctx,
           sl_send_selection_data(ctx);
 
         if (!ctx->selection_event_source) {
-          ctx->selection_event_source = wl_event_loop_add_fd(
+          ctx->selection_event_source.reset(wl_event_loop_add_fd(
               wl_display_get_event_loop(ctx->host_display),
               ctx->selection_data_offer_receive_fd, WL_EVENT_READABLE,
-              sl_handle_selection_fd_readable, ctx);
+              sl_handle_selection_fd_readable, ctx));
         }
         return;
       }
@@ -2383,10 +2381,10 @@ static void sl_send_data(struct sl_context* ctx, xcb_atom_t data_type) {
     free(atom_name_reply);
     free(name);
 
-    ctx->selection_event_source = wl_event_loop_add_fd(
+    ctx->selection_event_source.reset(wl_event_loop_add_fd(
         wl_display_get_event_loop(ctx->host_display),
         ctx->selection_data_offer_receive_fd, WL_EVENT_READABLE,
-        sl_handle_selection_fd_readable, ctx);
+        sl_handle_selection_fd_readable, ctx));
   } else {
     // If getting the atom name failed, notify the requestor that there won't be
     // any data, and close our end of the pipe.
@@ -2608,10 +2606,10 @@ static void sl_connect(struct sl_context* ctx) {
   change_attributes_cookie = xcb_change_window_attributes(
       ctx->connection, ctx->screen->root, XCB_CW_EVENT_MASK, values);
 
-  ctx->connection_event_source = wl_event_loop_add_fd(
+  ctx->connection_event_source.reset(wl_event_loop_add_fd(
       wl_display_get_event_loop(ctx->host_display),
       xcb_get_file_descriptor(ctx->connection), WL_EVENT_READABLE,
-      &sl_handle_x_connection_event, ctx);
+      &sl_handle_x_connection_event, ctx));
 
   ctx->xfixes_extension =
       xcb_get_extension_data(ctx->connection, &xcb_xfixes_id);
@@ -2878,8 +2876,7 @@ static int sl_handle_display_ready_event(int fd, uint32_t mask, void* data) {
 
   sl_connect(ctx);
 
-  wl_event_source_remove(ctx->display_ready_event_source);
-  ctx->display_ready_event_source = NULL;
+  ctx->display_ready_event_source.reset();
   close(fd);
 
   // Calculate scale now that the default scale factor is known. This also
@@ -3437,9 +3434,9 @@ int real_main(int argc, char** argv) {
     }
   }
 
-  ctx.display_event_source =
+  ctx.display_event_source.reset(
       wl_event_loop_add_fd(event_loop, wl_display_get_fd(ctx.display),
-                           WL_EVENT_READABLE, sl_handle_event, &ctx);
+                           WL_EVENT_READABLE, sl_handle_event, &ctx));
 
   wl_registry_add_listener(wl_display_get_registry(ctx.display),
                            &sl_registry_listener, &ctx);
@@ -3451,8 +3448,8 @@ int real_main(int argc, char** argv) {
   sl_set_display_implementation(&ctx);
 
   if (ctx.runprog || ctx.xwayland) {
-    ctx.sigchld_event_source =
-        wl_event_loop_add_signal(event_loop, SIGCHLD, sl_handle_sigchld, &ctx);
+    ctx.sigchld_event_source.reset(
+        wl_event_loop_add_signal(event_loop, SIGCHLD, sl_handle_sigchld, &ctx));
 
     // Unset DISPLAY to prevent X clients from connecting to an existing X
     // server when X forwarding is not enabled.
@@ -3470,9 +3467,9 @@ int real_main(int argc, char** argv) {
       rv = socketpair(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0, ds);
       errno_assert(!rv);
 
-      ctx.display_ready_event_source =
+      ctx.display_ready_event_source.reset(
           wl_event_loop_add_fd(event_loop, ds[0], WL_EVENT_READABLE,
-                               sl_handle_display_ready_event, &ctx);
+                               sl_handle_display_ready_event, &ctx));
 
       // X connection to Xwayland.
       rv = socketpair(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0, wm);
@@ -3567,8 +3564,8 @@ int real_main(int argc, char** argv) {
   if (ctx.trace_filename || ctx.trace_system) {
     initialize_tracing(ctx.trace_filename, ctx.trace_system);
     enable_tracing(!ctx.trace_system);
-    ctx.sigusr1_event_source =
-        wl_event_loop_add_signal(event_loop, SIGUSR1, sl_handle_sigusr1, &ctx);
+    ctx.sigusr1_event_source.reset(
+        wl_event_loop_add_signal(event_loop, SIGUSR1, sl_handle_sigusr1, &ctx));
   }
 
   wl_client_add_destroy_listener(ctx.client, &client_destroy_listener);

vm_tools/sommelier/sommelier.h
@@ -6,6 +6,7 @@
 #define VM_TOOLS_SOMMELIER_SOMMELIER_H_
 
 #include <linux/types.h>
+#include <memory>
 #include <sys/types.h>
 #include <wayland-server.h>
 #include <wayland-util.h>
@@ -60,6 +61,8 @@ struct zcr_gaming_input_v2;
 
 class WaylandChannel;
 
+extern const struct wl_registry_listener sl_registry_listener;
+
 struct sl_context {
   char** runprog;
   struct wl_display* display;
@@ -84,10 +87,10 @@ struct sl_context {
   struct sl_pointer_constraints* pointer_constraints;
   struct wl_list outputs;
   struct wl_list seats;
-  struct wl_event_source* display_event_source;
-  struct wl_event_source* display_ready_event_source;
-  struct wl_event_source* sigchld_event_source;
-  struct wl_event_source* sigusr1_event_source;
+  std::unique_ptr<struct wl_event_source> display_event_source;
+  std::unique_ptr<struct wl_event_source> display_ready_event_source;
+  std::unique_ptr<struct wl_event_source> sigchld_event_source;
+  std::unique_ptr<struct wl_event_source> sigusr1_event_source;
   struct wl_array dpi;
   int shm_driver;
   int data_driver;
@@ -95,8 +98,8 @@ struct sl_context {
   int virtwl_ctx_fd;
   int virtwl_socket_fd;
   int virtwl_display_fd;
-  struct wl_event_source* virtwl_ctx_event_source;
-  struct wl_event_source* virtwl_socket_event_source;
+  std::unique_ptr<struct wl_event_source> virtwl_ctx_event_source;
+  std::unique_ptr<struct wl_event_source> virtwl_socket_event_source;
   const char* vm_id;
   const char* drm_device;
   struct gbm_device* gbm;
@@ -111,7 +114,7 @@ struct sl_context {
   struct wl_list host_outputs;
   int next_global_id;
   xcb_connection_t* connection;
-  struct wl_event_source* connection_event_source;
+  std::unique_ptr<struct wl_event_source> connection_event_source;
   const xcb_query_extension_reply_t* xfixes_extension;
   xcb_screen_t* screen;
   xcb_window_t window;
@@ -142,10 +145,10 @@ struct sl_context {
   struct sl_data_source* selection_data_source;
   int selection_data_source_send_fd;
   struct wl_list selection_data_source_send_pending;
-  struct wl_event_source* selection_send_event_source;
+  std::unique_ptr<struct wl_event_source> selection_send_event_source;
   xcb_get_property_reply_t* selection_property_reply;
   int selection_property_offset;
-  struct wl_event_source* selection_event_source;
+  std::unique_ptr<struct wl_event_source> selection_event_source;
   xcb_atom_t selection_data_type;
   struct wl_array selection_data;
   int selection_data_offer_receive_fd;

commit b79fd36f8db52480efb787da28ed7cc4751a99c8

vm_tools/sommelier: malloc(..) --> calloc(..)
Otherwise, the following segfault is observed with copy + paste:

   from /lib/x86_64-linux-gnu/libwayland-server.so.0
    this=0x5555555e4698, ptr=0x211) at ../sommelier-util.cc:34
    at /usr/include/c++/8/bits/unique_ptr.h:382
    read_fd=22, write_fd=21) at ../sommelier-data-device-manager.cc:157
    resource=0x555555610040,
    mime_type=0x5555555b781c "text/plain;charset=utf-8", fd=21)
    at ../sommelier-data-device-manager.cc:189

Fixes: a0080bfa2e723 ("vm_tools: sommelier: Add a fuzzer")

BUG=b:150239451
TEST=copy + paste with a Wayland client

Change-Id: I1d42395dcd070c436dbadb1661e78654c0da04c6
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3021936
Commit-Queue: Gurchetan Singh <gurchetansingh@chromium.org>
Tested-by: Gurchetan Singh <gurchetansingh@chromium.org>
Reviewed-by: Fergus Dall <sidereal@google.com>
Reviewed-by: Nic Hollingum <hollingum@google.com>

vm_tools/sommelier/sommelier-data-device-manager.cc
@@ -148,7 +148,7 @@ static void sl_data_transfer_create(struct wl_event_loop* event_loop,
 
   // Start out the transfer in the reading state.
   struct sl_data_transfer* transfer =
-      static_cast<sl_data_transfer*>(malloc(sizeof(*transfer)));
+      static_cast<sl_data_transfer*>(calloc(1, sizeof(*transfer)));
   assert(transfer);
   transfer->read_fd = read_fd;
   transfer->write_fd = write_fd;