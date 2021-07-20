0260b3c1024e7d7094ebc4937369c14bd348f6db
Craig Hesling <hesling@chromium.org>
2021-07-20 15:56:00 -0700

    biod: Add cros pkg deps comment to fpstudy.py
    BUG=b:144061947, b:142877666
    TEST=none
    
    Change-Id: I777ae05329219256804cbfb44a53766733309eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3043181
    Tested-by: Craig Hesling <hesling@chromium.org>
    Reviewed-by: Josie Nordrum <josienordrum@google.com>

--- biod/study/fpstudy.py
+++ biod/study/fpstudy.py
@@ -3,6 +3,11 @@
 # Copyright 2021 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+#
+# Chromium OS Dependencies:
+# shred       --> sys-apps/coreutils
+# mogrify     --> media-gfx/imagemagick
+# python gpg  --> dev-python/python-gnupg --> app-crypt/gnupg
 
 """A tool to manage the fingerprint study."""
 
