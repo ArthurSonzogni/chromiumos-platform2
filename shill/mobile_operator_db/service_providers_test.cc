27724d5d1a04a03d777ceefad94f5659418269ff
Andrew Lassalle <andrewlassalle@chromium.org>
2021-07-30 10:29:46 -0700

    shill: cellular: Validate IMSI filters in service providers db
    Add more unit tests to validate the service providers database.
    Validate that IMSI filters don't overlap with each other.
    Validate IMSI values.
    
    BUG=b:192575460
    TEST=Run unit tests.
    
    Change-Id: Ibbd9c91796dde1ec7781fc179101d1a92cdc0c83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3063879
    Tested-by: Andrew Lassalle <andrewlassalle@chromium.org>
    Auto-Submit: Andrew Lassalle <andrewlassalle@chromium.org>
    Commit-Queue: Andrew Lassalle <andrewlassalle@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- shill/mobile_operator_db/service_providers_test.cc
+++ shill/mobile_operator_db/service_providers_test.cc
@@ -17,6 +17,10 @@ using testing::Test;
 
 namespace shill {
 
+using MvnoMnoPair =
+    std::pair<const shill::mobile_operator_db::MobileVirtualNetworkOperator*,
+              const shill::mobile_operator_db::MobileNetworkOperator*>;
+
 class ServiceProvidersTest : public testing::Test {
  protected:
   // Per-test-suite set-up.
@@ -33,12 +37,26 @@ class ServiceProvidersTest : public testing::Test {
     ASSERT_NE(database_stream, nullptr);
     database_ = std::make_unique<shill::mobile_operator_db::MobileOperatorDB>();
     ASSERT_TRUE(database_->ParseFromZeroCopyStream(database_stream.get()));
+
+    // Load all MVNOs into a vector with their MNO as a secondary variable for
+    // easy iteration.
+    for (const auto& mno : database_->mno()) {
+      for (const auto& mvno : mno.mvno()) {
+        MvnoMnoPair mvno_pair(&mvno, &mno);
+        mvnos_.push_back(mvno_pair);
+      }
+    }
+    for (const auto& mvno : database_->mvno()) {
+      MvnoMnoPair mvno_pair(&mvno, nullptr);
+      mvnos_.push_back(mvno_pair);
+    }
   }
   // Per-test-suite tear-down.
   // Called after the last test in this test suite.
   static void TearDownTestSuite() {
     database_.reset();
     database_ = nullptr;
+    mvnos_.clear();
   }
 
   void ValidateUuid(const shill::mobile_operator_db::Data& data,
@@ -51,10 +69,12 @@ class ServiceProvidersTest : public testing::Test {
 
   // Expensive resource shared by all tests.
   static std::unique_ptr<mobile_operator_db::MobileOperatorDB> database_;
+  static std::vector<MvnoMnoPair> mvnos_;
 };
 
 std::unique_ptr<mobile_operator_db::MobileOperatorDB>
     ServiceProvidersTest::database_ = nullptr;
+std::vector<MvnoMnoPair> ServiceProvidersTest::mvnos_;
 
 TEST_F(ServiceProvidersTest, CheckUniqueUUIDs) {
   // Verify that we are not using the same uuid for different MNOs/MVNOs.
@@ -78,6 +98,89 @@ TEST_F(ServiceProvidersTest, CheckRootLevelMvnosWithoutFilters) {
     EXPECT_TRUE(mvno.mvno_filter_size() > 0)
         << "MVNO with uuid: " << mvno.data().uuid()
         << " does not have a filter.";
+  }
+}
+
+TEST_F(ServiceProvidersTest, CheckIMSIMatchesMCCMNC) {
+  // Verify that the IMSI values start with one of the MCCMNC values.
+  for (auto mvno_mno_pair : mvnos_) {
+    auto mvno = mvno_mno_pair.first;
+    auto mno = mvno_mno_pair.second;
+    for (const auto& filter : mvno->mvno_filter()) {
+      if (filter.type() == mobile_operator_db::Filter_Type_IMSI) {
+        // Combine MNO and MVNO MCCMNCs
+        std::set<std::string> mccmncs;
+        for (const auto& mccmnc : mvno->data().mccmnc())
+          mccmncs.insert(mccmnc);
+        if (mno)
+          for (const auto& mccmnc : mno->data().mccmnc())
+            mccmncs.insert(mccmnc);
+
+        // Validate ranges
+        for (auto range : filter.range()) {
+          bool found_match = false;
+          for (const auto& mccmnc : mccmncs) {
+            if (std::to_string(range.start()).rfind(mccmnc, 0) == 0 &&
+                std::to_string(range.end()).rfind(mccmnc, 0) == 0) {
+              found_match = true;
+              break;
+            }
+          }
+          EXPECT_TRUE(found_match)
+              << "IMSI range " << range.start() << "-" << range.end()
+              << " doesn't match any MCCMNCs in that MVNO.";
+        }
+        // Validate regex for simple cases:
+        // - Regex that starts with 5 numeric characters.
+        if (mccmncs.size() > 0 && filter.has_regex() &&
+            filter.regex().size() >= 5 &&
+            (filter.regex().substr(0, 5).find_first_not_of("0123456789") ==
+             std::string::npos)) {
+          bool found_match = false;
+          for (const auto& mccmnc : mccmncs) {
+            if (filter.regex().rfind(mccmnc, 0) == 0) {
+              found_match = true;
+              break;
+            }
+          }
+          EXPECT_TRUE(found_match)
+              << "IMSI regex " << filter.regex()
+              << " doesn't match any MCCMNCs in that MVNO.";
+        }
+      }
+    }
+  }
+}
+
+TEST_F(ServiceProvidersTest, CheckIMSIRangesAreValid) {
+  // Verify that different IMSI ranges don't overlap.
+  // Verify that the IMSI ranges are valid(start<end).
+  std::map<uint64_t, uint64_t> ranges;
+  for (auto mvno_mno_pair : mvnos_) {
+    auto mvno = mvno_mno_pair.first;
+    for (const auto& filter : mvno->mvno_filter()) {
+      if (filter.type() == mobile_operator_db::Filter_Type_IMSI) {
+        for (auto range : filter.range()) {
+          EXPECT_LT(range.start(), range.end());
+          ASSERT_EQ(ranges.count(range.start()), 0);
+          EXPECT_GT(range.start(), 0);
+          EXPECT_GT(range.end(), 0);
+          // Insert all ranges into |ranges| and process them later
+          ranges[range.start()] = range.end();
+        }
+      }
+    }
+  }
+  uint64_t previous_start = 0;
+  uint64_t previous_end = 0;
+  for (const auto& range : ranges) {
+    if (previous_end > 0) {
+      EXPECT_GT(range.first, previous_end)
+          << " The IMSI ranges " << previous_start << ":" << previous_end
+          << " and " << range.first << ":" << range.second << " overlap.";
+    }
+    previous_start = range.first;
+    previous_end = range.second;
   }
 }
 
