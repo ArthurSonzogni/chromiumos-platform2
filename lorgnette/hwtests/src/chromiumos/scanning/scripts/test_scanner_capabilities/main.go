fb6c64db8e931e53634b296f5afc06900dac7b37
Paul Moy <pmoy@chromium.org>
2021-09-01 10:22:26 -0600

    lorgnette: add test_scanner_capabilities script
    For now, this script just runs the NoCameraSourceTest. It will be
    extended to run the rest of the WWCB tests related to a scanner's
    reported capabilities.
    
    BUG=b:191933619
    TEST=(on dut) test_scanner_capabilities --identifier="TR8500"
    
    Change-Id: I647c72d45b48bb9c0ada2bb2cf1bcf7165eaff24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3136793
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Tested-by: Paul Moy <pmoy@chromium.org>
    Commit-Queue: Paul Moy <pmoy@chromium.org>

---
+++ lorgnette/hwtests/src/chromiumos/scanning/scripts/test_scanner_capabilities/main.go
@@ -0,0 +1,63 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"chromiumos/scanning/hwtests"
+	"chromiumos/scanning/utils"
+)
+
+// Runs various tests to verify that a scanner's reported capabilities satisfy
+// the WWCB specification.
+func main() {
+	identifierFlag := flag.String("identifier", "", "Substring of the identifier printed by lorgnette_cli of the scanner to test.")
+	flag.Parse()
+
+	logFile, err := utils.CreateLogFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.SetOutput(logFile)
+	fmt.Printf("Created log file at: %s\n", logFile.Name())
+
+	listOutput, err := utils.LorgnetteCLIList()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scannerInfo, err := utils.GetLorgnetteScannerInfo(listOutput, *identifierFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	caps, err := utils.GetScannerCapabilities(scannerInfo.Address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tests := map[string]utils.TestFunction{"NoCameraSource": hwtests.NoCameraSourceTest(caps.CameraInputCaps)}
+	failed := []string{}
+
+	for name, test := range tests {
+		if !utils.RunTest(name, test) {
+			failed = append(failed, name)
+		}
+	}
+
+	if len(failed) != 0 {
+		fmt.Printf("%d tests failed:\n", len(failed))
+		for _, failedTest := range failed {
+			fmt.Println(failedTest)
+		}
+	} else {
+		fmt.Println("All tests passed.")
+	}
+
+}
