2de18f61abc36e49001341c1fa8189c5b7227798
Maksim Ivanov <emaxx@chromium.org>
2021-09-10 19:02:39 +0200

    cryptohome: Improve fuzzing via blob_mutator
    Change the algorithm used in blob_mutator to increase the chance of the
    fuzzing framework finding an interesting input.
    
    This is achieved by switching from interpreting the fuzzer input as
    enums + 32-bit integers (i.e., commands with arguments), and switch to
    operating with masks that build the input byte-by-byte in a more direct
    fashion. I.e., the backslash byte is now interpreted as "copy one byte
    from the source blob", and any other byte is interpreted as "insert
    this new byte". There are other combinations with backslashes - see the
    comments in the code for the details.
    
    This heuristic proved to be much more successful in quickly generating
    interesting inputs, for example in generating structurally correct
    mutated flatbuffers. In principle, this heuristic is based on the
    existing implementation in
    FuzzedDataProvider::ConsumeRandomLengthString().
    
    BUG=b:199547188
    TEST=USE=fuzzer emerge-$BOARD cryptohome
    
    Change-Id: I6ec9b9797528e8f2ff3903dbad627b77527f71f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3151851
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Tested-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Leo Lai <cylai@google.com>

--- cryptohome/fuzzers/blob_mutator.cc
+++ cryptohome/fuzzers/blob_mutator.cc
@@ -4,7 +4,10 @@
 
 #include "cryptohome/fuzzers/blob_mutator.h"
 
+#include <stdint.h>
+
 #include <algorithm>
+#include <utility>
 
 #include <base/check_op.h>
 #include <base/logging.h>
@@ -17,13 +20,68 @@ namespace {
 // The "commands" that the MutateBlob() function uses for enterpreting the
 // fuzzer input and performing the mutations it implements.
 enum class BlobMutatorCommand {
-  kCopyRemainingData,
-  kCopyChunk,
-  kDeleteChunk,
-  kInsertByte,
-
-  kMaxValue = kInsertByte
+  kCopyInputByte,
+  kSkipInputByte,
+  kAddNewByte,
+  kEnd,
+  kEndWithCopyingRestOfInput,
 };
+
+// Interprets the byte(s) from the fuzzed data provider as a mutation command.
+// |consumed_byte_count| is populated with 1 or 2 - the number of bytes consumed
+// from the fuzzed data provider. When returning |kAddNewByte|, |to_add| is
+// assigned to the value to be added.
+//
+// The algorithm implemented in this function allows the following easy
+// transformations:
+// * input_blob="foo", fuzzed_data_provider=\ \ \ ==> "foo";
+// * input_blob="foo", fuzzed_data_provider=\ 0x1 ==> "foo";
+// * input_blob="foo", fuzzed_data_provider=b a r ==> "bar";
+// * input_blob="foo", fuzzed_data_provider=\ \ b a r \ ==> "fobaro";
+// * input_blob="foo", fuzzed_data_provider=\ 0x2 \ \ ==> "oo";
+// etc.
+BlobMutatorCommand ReadCommandFromFuzzedData(uint8_t current_byte,
+                                             uint8_t next_byte,
+                                             int* consumed_byte_count,
+                                             uint8_t* to_add) {
+  if (current_byte != '\\') {
+    *consumed_byte_count = 1;
+    *to_add = current_byte;
+    return BlobMutatorCommand::kAddNewByte;
+  }
+  if (next_byte == 0) {
+    *consumed_byte_count = 2;
+    return BlobMutatorCommand::kEnd;
+  }
+  if (next_byte == 1) {
+    *consumed_byte_count = 2;
+    return BlobMutatorCommand::kEndWithCopyingRestOfInput;
+  }
+  if (next_byte == 2) {
+    *consumed_byte_count = 2;
+    return BlobMutatorCommand::kSkipInputByte;
+  }
+  if (next_byte == 3) {
+    // This case allows the fuzzer to insert the backslash character. Note that
+    // this is only needed for the backslash, since it has a special meaning.
+    *consumed_byte_count = 2;
+    *to_add = '\\';
+    return BlobMutatorCommand::kAddNewByte;
+  }
+  if (next_byte == 4) {
+    // This case is similar to below, but allows the fuzzer to insert a new byte
+    // with the value from [0; 3] after a segment of bytes copied from the
+    // input. This is because the backslash followed by such a value has a
+    // special meaning handled above.
+    *consumed_byte_count = 2;
+    return BlobMutatorCommand::kCopyInputByte;
+  }
+  // This case is similar to above, but without consuming the character that
+  // follows the backslash. This allows the fuzzer to use a compact form
+  // "...\\\\\\..." in order to represent a segment copied from the input blob.
+  *consumed_byte_count = 1;
+  return BlobMutatorCommand::kCopyInputByte;
+}
 
 }  // namespace
 
@@ -35,45 +93,51 @@ Blob MutateBlob(const Blob& input_blob,
   Blob fuzzed_blob;
   fuzzed_blob.reserve(max_length);
   int input_index = 0;
-  while (fuzzed_blob.size() < max_length) {
-    switch (fuzzed_data_provider->ConsumeEnum<BlobMutatorCommand>()) {
-      case BlobMutatorCommand::kCopyRemainingData: {
-        // Take all remaining data from the input blob and stop.
+  uint8_t next_byte = fuzzed_data_provider->ConsumeIntegral<uint8_t>();
+  do {
+    uint8_t current_byte = next_byte;
+    next_byte = fuzzed_data_provider->ConsumeIntegral<uint8_t>();
+
+    int consumed_byte_count;
+    uint8_t to_add;
+    BlobMutatorCommand command = ReadCommandFromFuzzedData(
+        current_byte, next_byte, &consumed_byte_count, &to_add);
+    switch (command) {
+      case BlobMutatorCommand::kCopyInputByte: {
+        if (input_index < input_blob.size() &&
+            fuzzed_blob.size() < max_length) {
+          fuzzed_blob.push_back(input_blob[input_index]);
+          ++input_index;
+        }
+        break;
+      }
+      case BlobMutatorCommand::kSkipInputByte: {
+        if (input_index < input_blob.size())
+          ++input_index;
+        break;
+      }
+      case BlobMutatorCommand::kAddNewByte: {
+        if (fuzzed_blob.size() < max_length)
+          fuzzed_blob.push_back(to_add);
+        break;
+      }
+      case BlobMutatorCommand::kEnd: {
+        return fuzzed_blob;
+      }
+      case BlobMutatorCommand::kEndWithCopyingRestOfInput: {
         const int bytes_to_copy = std::min(input_blob.size() - input_index,
                                            max_length - fuzzed_blob.size());
         fuzzed_blob.insert(fuzzed_blob.end(), input_blob.begin() + input_index,
                            input_blob.begin() + input_index + bytes_to_copy);
-        CHECK_LE(fuzzed_blob.size(), max_length);
         return fuzzed_blob;
       }
-      case BlobMutatorCommand::kCopyChunk: {
-        // Take the specified number of bytes from the current position in the
-        // input blob.
-        const int max_bytes_to_copy = std::min(input_blob.size() - input_index,
-                                               max_length - fuzzed_blob.size());
-        const int bytes_to_copy =
-            fuzzed_data_provider->ConsumeIntegralInRange(0, max_bytes_to_copy);
-        fuzzed_blob.insert(fuzzed_blob.end(), input_blob.begin() + input_index,
-                           input_blob.begin() + input_index + bytes_to_copy);
-        break;
-      }
-      case BlobMutatorCommand::kDeleteChunk: {
-        // Skip (delete) the specified number of bytes from the current position
-        // in the input blob.
-        const int max_bytes_to_delete = input_blob.size() - input_index;
-        const int bytes_to_delete =
-            fuzzed_data_provider->ConsumeIntegralInRange(0,
-                                                         max_bytes_to_delete);
-        input_index += bytes_to_delete;
-        break;
-      }
-      case BlobMutatorCommand::kInsertByte: {
-        // Append the specified byte.
-        fuzzed_blob.push_back(fuzzed_data_provider->ConsumeIntegral<uint8_t>());
-        break;
-      }
     }
-  }
-  CHECK_LE(fuzzed_blob.size(), max_length);
+
+    if (consumed_byte_count == 2) {
+      // If the current command consumed both bytes from the data provider,
+      // extract one more for the next command.
+      next_byte = fuzzed_data_provider->ConsumeIntegral<uint8_t>();
+    }
+  } while (fuzzed_data_provider->remaining_bytes());
   return fuzzed_blob;
 }
