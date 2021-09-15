4c908a212e0bf8a154ca676adafc55160d05ba5f
Evan Benn <evanbenn@chromium.org>
2021-09-15 15:28:34 +1000

    hps: Add VLOG to dev ReadReg functions
    BUG=b:196129231
    TEST=hps --v=2 --mcp status 0 11
    
    Change-Id: I5a4279d236ba108d92701e5dd29a394278f7fb27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3177560
    Tested-by: Evan Benn <evanbenn@chromium.org>
    Commit-Queue: Evan Benn <evanbenn@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>

--- hps/dev.cc
+++ hps/dev.cc
@@ -5,8 +5,12 @@
 /*
  * Device access interface common functions.
  */
+
 #include "hps/dev.h"
+
+#include "base/logging.h"
 #include "hps/hps_reg.h"
+#include "hps/utils.h"
 
 namespace hps {
 
@@ -25,9 +29,12 @@ int DevInterface::ReadReg(HpsReg r) {
 
   for (int i = 0; i < kIoRetries; i++) {
     if (this->Read(I2cReg(r), res, sizeof(res))) {
-      return (static_cast<int>(res[0]) << 8) | static_cast<int>(res[1]);
+      int ret = (static_cast<int>(res[0]) << 8) | static_cast<int>(res[1]);
+      VLOG(2) << "ReadReg: " << HpsRegToString(r) << " " << ret << " OK";
+      return ret;
     }
   }
+  VLOG(2) << "ReadReg: " << HpsRegToString(r) << " FAILED";
   return -1;
 }
 
@@ -37,14 +44,15 @@ int DevInterface::ReadReg(HpsReg r) {
  */
 bool DevInterface::WriteReg(HpsReg r, uint16_t data) {
   uint8_t buf[2];
-
   buf[0] = data >> 8;
   buf[1] = data & 0xFF;
   for (int i = 0; i < kIoRetries; i++) {
     if (this->Write(I2cReg(r), buf, sizeof(buf))) {
+      VLOG(2) << "WriteReg: " << HpsRegToString(r) << " " << data << " OK";
       return true;
     }
   }
+  VLOG(2) << "WriteReg: " << HpsRegToString(r) << " " << data << " FAILED";
   return false;
 }
 /*
