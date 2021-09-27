0ba9c76fb9e8fb0d08f5cb7f4b04f7b2e307efb8
Qijiang Fan <fqj@chromium.org>
2021-09-27 15:47:57 +0900

    init: include base/logging.h for LOG-related macros.
    BUG=b:186486897
    TEST=emerge chromeos-init
    
    Change-Id: I9770951d59af3f5239d137dc90a1ddb3ae41f828
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3185103
    Tested-by: Qijiang Fan <fqj@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Qijiang Fan <fqj@google.com>

--- init/clobber_ui.cc
+++ init/clobber_ui.cc
@@ -11,6 +11,7 @@
 #include <string>
 #include <utility>
 
+#include <base/logging.h>
 #include <base/strings/stringprintf.h>
 
 namespace {
--- init/file_attrs_cleaner_test.cc
+++ init/file_attrs_cleaner_test.cc
@@ -17,6 +17,7 @@
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
 #include <base/files/scoped_temp_dir.h>
+#include <base/logging.h>
 #include <gtest/gtest.h>
 
 using file_attrs_cleaner::AttributeCheckStatus;
