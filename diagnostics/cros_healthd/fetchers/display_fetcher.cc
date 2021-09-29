66cedb6bc0c9e25188f9408c265d86d29c966919
Kerker Yang <kerker@google.com>
2021-09-29 12:45:14 +0800

    diagnostics: Fetch ePrivacy screen through executor
    BUG=b:201358693
    TEST=none
    
    Change-Id: I3bd0204ba897c507a8ed7e3abb0f261f08dc77e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3193344
    Tested-by: Kerker Yang <kerker@chromium.org>
    Commit-Queue: Kerker Yang <kerker@chromium.org>
    Reviewed-by: Meng-Huan Yu <menghuan@chromium.org>
    Reviewed-by: Chung-Sheng Wu <chungsheng@chromium.org>

--- diagnostics/cros_healthd/fetchers/display_fetcher.cc
+++ diagnostics/cros_healthd/fetchers/display_fetcher.cc
@@ -2,6 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
+#include <vector>
+
+#include <base/strings/stringprintf.h>
+#include <re2/re2.h>
+
 #include "diagnostics/cros_healthd/fetchers/display_fetcher.h"
 #include "diagnostics/cros_healthd/utils/error_utils.h"
 
@@ -14,13 +20,79 @@ namespace {
 namespace executor_ipc = chromeos::cros_healthd_executor::mojom;
 namespace mojo_ipc = ::chromeos::cros_healthd::mojom;
 
+void PopulatePrivacyScreenInfo(const std::string& result,
+                               bool* supported,
+                               bool* enabled) {
+  *supported = result.find("privacy-screen") != std::string::npos;
+  *enabled = false;
+  if (!*supported) {
+    return;
+  }
+
+  // TODO(kerker) Maybe we should parse the whole output to retrieve more info.
+  // But it depends on the request. Moreover, some info can be retrieved from
+  // EDID, we need to consider this two data sources according to the request in
+  // the future.
+  std::string content = result;
+
+  // Find the first eDP section start point.
+  auto edp_pos = result.find("eDP");
+
+  // From eDP section, we find the next status(*) position, it must be next
+  // connector section.
+  // (*) status is "connected" or "disconnected".
+  auto next_section_pos = result.find("connected", edp_pos);
+  content = content.substr(edp_pos, next_section_pos - edp_pos);
+
+  // Find the privacy screen field position.
+  auto privacy_screen_pos = content.find("privacy-screen");
+  content = content.substr(privacy_screen_pos);
+
+  // Find the first "value" after privacy screen section.
+  auto value_pos = content.find("value");
+  content = content.substr(value_pos);
+
+  std::string value;
+  const char regex[] = R"(value: (.*)\n.*)";
+  RE2::FullMatch(content, regex, &value);
+
+  *enabled = value == "1";
+}
+
+void FinishFetchingDisplayInfo(
+    base::OnceCallback<void(mojo_ipc::DisplayResultPtr)> callback,
+    executor_ipc::ProcessResultPtr result) {
+  std::string err = result->err;
+  int32_t return_code = result->return_code;
+  if (!err.empty() || return_code != EXIT_SUCCESS) {
+    std::move(callback).Run(
+        mojo_ipc::DisplayResult::NewError(CreateAndLogProbeError(
+            mojo_ipc::ErrorType::kSystemUtilityError,
+            base::StringPrintf(
+                "RunModetest failed with return code: %d and error: %s",
+                return_code, err.c_str()))));
+    return;
+  }
+
+  auto display_info = mojo_ipc::DisplayInfo::New();
+
+  // EmbeddedDisplayInfo
+  auto edp_info = mojo_ipc::EmbeddedDisplayInfo::New();
+  PopulatePrivacyScreenInfo(result->out, &edp_info->privacy_screen_supported,
+                            &edp_info->privacy_screen_enabled);
+  display_info->edp_info = std::move(edp_info);
+
+  std::move(callback).Run(
+      mojo_ipc::DisplayResult::NewDisplayInfo(std::move(display_info)));
+}
+
 }  // namespace
 
 void DisplayFetcher::FetchDisplayInfo(
     DisplayFetcher::FetchDisplayInfoCallback&& callback) {
-  std::move(callback).Run(
-      mojo_ipc::DisplayResult::NewError(CreateAndLogProbeError(
-          mojo_ipc::ErrorType::kServiceUnavailable, "Not implemented.")));
+  context_->executor()->RunModetest(
+      executor_ipc::ModetestOptionEnum::kListConnector,
+      base::BindOnce(&FinishFetchingDisplayInfo, std::move(callback)));
 }
 
 }  // namespace diagnostics
