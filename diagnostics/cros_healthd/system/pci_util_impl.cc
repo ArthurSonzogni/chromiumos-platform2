e9cb1cf59fdbaf84365359213c44e2db88a64643
Chung-Sheng Wu <chungsheng@google.com>
2021-10-07 16:27:45 +0800

    diagnostics: healthd: Add error handling to pci util
    PCI util will call |exit(1)| when errors occur. This can be overridden
    by assign error handling function. Add error handling function to log
    the error and don't exit the process. This prevent the health crashing
    at |pci_init()|.
    
    BUG=b:200752022
    TEST=emerge-trogdor64 diagnostics
    
    Change-Id: Ibe41dc48f2af897291546a289cf7db0db378b33e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3211312
    Tested-by: Chung-Sheng Wu <chungsheng@chromium.org>
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Commit-Queue: Chung-Sheng Wu <chungsheng@chromium.org>

--- diagnostics/cros_healthd/system/pci_util_impl.cc
+++ diagnostics/cros_healthd/system/pci_util_impl.cc
@@ -2,7 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <setjmp.h>
+#include <stdarg.h>
+
 #include <base/check.h>
+#include <base/logging.h>
 
 #include "diagnostics/cros_healthd/system/pci_util_impl.h"
 
@@ -16,29 +20,75 @@ namespace {
 // These buffer sizes are referred from pciutils/lspci.c.
 const int kVendorBufferSize = 128;
 const int kDeviceBufferSize = 128;
+const int kErrorBufferSize = 256;
+
+// The |jmp_buf| for error handling.
+jmp_buf error_handle_env;
+
+// HandleError is a error handler for pcilib. It is similar to the default one
+// in pcilib but replace |exit()| with |longjmp()| to prevent exiting the whole
+// process.
+void HandleError(char* msg, ...) {
+  va_list args;
+  char buf[kErrorBufferSize];
+
+  va_start(args, msg);
+  vsnprintf(buf, kErrorBufferSize, msg, args);
+  va_end(args);
+
+  VLOG(1) << "Error from pcilib: " << buf;
+
+  longjmp(error_handle_env, 1);
+}
+
+void HandleWarning(char* msg, ...) {
+  va_list args;
+  char buf[kErrorBufferSize];
+
+  va_start(args, msg);
+  vsnprintf(buf, kErrorBufferSize, msg, args);
+  va_end(args);
+
+  VLOG(1) << "Warning from pcilib: " << buf;
+}
 }  // namespace
 
 PciUtilImpl::PciUtilImpl() {
   pacc_ = pci_alloc();
   CHECK(pacc_);
-  pci_init(pacc_);
+  pacc_->error = &HandleError;
+  pacc_->warning = &HandleWarning;
+  if (setjmp(error_handle_env) == 0) {
+    pci_init(pacc_);
+    return;
+  }
+  // Handle longjmp from HandleError
+  pci_cleanup(pacc_);
+  pacc_ = nullptr;
 }
 
 PciUtilImpl::~PciUtilImpl() {
-  pci_cleanup(pacc_);
+  if (pacc_)
+    pci_cleanup(pacc_);
 }
 
 std::string PciUtilImpl::GetVendorName(uint16_t vendor_id) {
   char buf[kVendorBufferSize];
-  return pci_lookup_name(pacc_, buf, sizeof(buf), PCI_LOOKUP_VENDOR,
-                         static_cast<int>(vendor_id));
+  if (pacc_ && setjmp(error_handle_env) == 0) {
+    return pci_lookup_name(pacc_, buf, sizeof(buf), PCI_LOOKUP_VENDOR,
+                           static_cast<int>(vendor_id));
+  }
+  return "";
 }
 
 std::string PciUtilImpl::GetDeviceName(uint16_t vendor_id, uint16_t device_id) {
   char buf[kDeviceBufferSize];
-  return pci_lookup_name(pacc_, buf, sizeof(buf), PCI_LOOKUP_DEVICE,
-                         static_cast<int>(vendor_id),
-                         static_cast<int>(device_id));
+  if (pacc_ && setjmp(error_handle_env) == 0) {
+    return pci_lookup_name(pacc_, buf, sizeof(buf), PCI_LOOKUP_DEVICE,
+                           static_cast<int>(vendor_id),
+                           static_cast<int>(device_id));
+  }
+  return "";
 }
 
 }  // namespace diagnostics
