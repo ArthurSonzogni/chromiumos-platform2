191b8a63f81919dc93622808863ea532d761ef73
Yi Chou <yich@google.com>
2021-10-20 23:37:58 +0800

    tpm2-simulator: include base/logging.h for LOG macros.
    BUG=b:186486897, b:202096123
    TEST=emerge tpm2-simulator
    
    Change-Id: I224ac06fdbd82d1a40a2c61e14903eb2d610f646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3233474
    Tested-by: Yi Chou <yich@google.com>
    Reviewed-by: Raul E Rangel <rrangel@chromium.org>
    Commit-Queue: Yi Chou <yich@google.com>

--- tpm2-simulator/tpm_executor_version.cc
+++ tpm2-simulator/tpm_executor_version.cc
@@ -6,6 +6,7 @@
 
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
+#include <base/logging.h>
 #include <base/strings/string_number_conversions.h>
 #include <base/strings/string_util.h>
 #include <brillo/syslog_logging.h>
