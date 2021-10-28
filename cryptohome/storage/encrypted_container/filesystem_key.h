a0f906fd539e0810b5a07124260832bea1715ee3
Daniil Lunev <dlunev@chromium.org>
2021-10-28 11:24:03 +1100

    cryptohome: make FileSystemKey and Reference comparable
    BUG=b:172344610,b:172622448
    TEST=FEATURES="test" USE="test" emerge-hatch cryptohome
    
    Change-Id: Iadae18b309f74916f7c7cc45f24608e11d2599c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3249701
    Tested-by: Daniil Lunev <dlunev@chromium.org>
    Commit-Queue: Daniil Lunev <dlunev@chromium.org>
    Reviewed-by: Sarthak Kukreti <sarthakkukreti@chromium.org>

--- cryptohome/storage/encrypted_container/filesystem_key.h
+++ cryptohome/storage/encrypted_container/filesystem_key.h
@@ -9,17 +9,38 @@
 
 namespace cryptohome {
 
-typedef struct {
+struct FileSystemKey {
   brillo::SecureBlob fek;
   brillo::SecureBlob fnek;
   brillo::SecureBlob fek_salt;
   brillo::SecureBlob fnek_salt;
-} FileSystemKey;
+};
 
-typedef struct {
+struct FileSystemKeyReference {
   brillo::SecureBlob fek_sig;
   brillo::SecureBlob fnek_sig;
-} FileSystemKeyReference;
+};
+
+// TODO(dlunev): those are inline since otherwise they cause symbol collision
+// in mount_encrypted. Fix the hierarchy so that doesn't need to be inline.
+inline bool operator==(const FileSystemKey& lhs, const FileSystemKey& rhs) {
+  return (lhs.fek == rhs.fek && lhs.fnek == rhs.fnek &&
+          lhs.fek_salt == rhs.fek_salt && lhs.fnek_salt == rhs.fnek_salt);
+}
+
+inline bool operator!=(const FileSystemKey& lhs, const FileSystemKey& rhs) {
+  return !(lhs == rhs);
+}
+
+inline bool operator==(const FileSystemKeyReference& lhs,
+                       const FileSystemKeyReference& rhs) {
+  return (lhs.fek_sig == rhs.fek_sig && lhs.fnek_sig == rhs.fnek_sig);
+}
+
+inline bool operator!=(const FileSystemKeyReference& lhs,
+                       const FileSystemKeyReference& rhs) {
+  return !(lhs == rhs);
+}
 
 }  // namespace cryptohome
 
