98cd168b265480fb7eb09333844b3759d36239b0
Yi Chou <yich@google.com>
2021-11-01 17:47:08 +0800

    cryptohome: Add the interface of asynchronous AuthBlocks
    BUG=b:203995038
    TEST=unittest of cryptohome
    
    Change-Id: I9e26c3a7766e9d3be7773ea0502e01b7905aa014
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3255100
    Commit-Queue: Yi Chou <yich@google.com>
    Tested-by: Yi Chou <yich@google.com>
    Reviewed-by: Leo Lai <cylai@google.com>
    Reviewed-by: Betul Soysal <betuls@google.com>

--- cryptohome/auth_blocks/auth_block.h
+++ cryptohome/auth_blocks/auth_block.h
@@ -5,17 +5,76 @@
 #ifndef CRYPTOHOME_AUTH_BLOCKS_AUTH_BLOCK_H_
 #define CRYPTOHOME_AUTH_BLOCKS_AUTH_BLOCK_H_
 
+#include <memory>
+
+#include <base/callback.h>
+#include <base/optional.h>
+
 #include "cryptohome/auth_blocks/auth_block_state.h"
 #include "cryptohome/crypto_error.h"
 #include "cryptohome/key_objects.h"
 #include "cryptohome/vault_keyset.h"
 
-#include <base/optional.h>
-
 namespace cryptohome {
 
 // Defined in cryptohome_metrics.h
 enum DerivationType : int;
+
+// This is a pure virtual interface designed to be implemented by the different
+// authentication methods - U2F, PinWeaver, TPM backed passwords, etc. - so that
+// they take some arbitrary user input and give out a key.
+class AuthBlock {
+ public:
+  virtual ~AuthBlock() = default;
+
+  // If the operation succeeds, |key_blobs| will contain the constructed
+  // KeyBlobs, AuthBlockState will be populated in |auth_block_state| and
+  // |error| will be CryptoError::CE_NONE. On failure, error will be
+  // populated, and should not rely on the value of key_blobs and
+  // auth_block_state.
+  using CreateCallback = base::OnceCallback<void(
+      CryptoError error,
+      std::unique_ptr<KeyBlobs> key_blobs,
+      std::unique_ptr<AuthBlockState> auth_block_state)>;
+
+  // This is implemented by concrete auth methods to create a fresh key from
+  // user input.
+  // This asynchronous API receives a callback to construct the KeyBlobs with
+  // the released TPM secrets in an unblocking way. Once the callback is done,
+  // on success, CryptoError will be CryptoError::CE_NONE, KeyBlobs and
+  // AuthBlockState will be populated. On Failure, CryptoError is assigned the
+  // related error value, the value of KeyBlobs and AuthBlockState are not valid
+  // to use.
+  virtual void Create(const AuthInput& user_input, CreateCallback callback) = 0;
+
+  // If the operation succeeds, |key_blobs| will contain the constructed
+  // KeyBlobs and |error| will be CryptoError::CE_NONE. On failure, error will
+  // be populated, and should not rely on the value of key_blobs.
+  using DeriveCallback = base::OnceCallback<void(
+      CryptoError error, std::unique_ptr<KeyBlobs> key_blobs)>;
+
+  // This is implemented by concrete auth methods to map the user secret
+  // input/credentials into a key.
+  // This asynchronous API receives a callback to construct the KeyBlobs with
+  // the released TPM secrets in an unblocking way. Once the callback is done,
+  // on success, CryptoError will be CryptoError::CE_NONE, KeyBlobs  will be
+  // populated. On Failure, CryptoError is assigned the related error value, the
+  // value of KeyBlobs are not valid to use.
+  virtual void Derive(const AuthInput& auth_input,
+                      const AuthBlockState& state,
+                      DeriveCallback callback) = 0;
+
+  DerivationType derivation_type() const { return derivation_type_; }
+
+ protected:
+  // This is a virtual interface that should not be directly constructed.
+  explicit AuthBlock(DerivationType derivation_type)
+      : derivation_type_(derivation_type) {}
+
+ private:
+  // For UMA - keeps track of the encryption type used in Derive().
+  const DerivationType derivation_type_;
+};
 
 // This is a pure virtual interface designed to be implemented by the different
 // authentication methods - U2F, PinWeaver, TPM backed passwords, etc. - so that
