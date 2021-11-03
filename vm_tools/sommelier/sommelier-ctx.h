e1e0906696ebd4a787ed11ac872da43061fa6c97
Chloe Pelling <cpelling@google.com>
2021-11-03 12:51:37 +1100

    vm_tools: Move sl_context to sommelier-ctx.h.
    This is more logical home for it, and reduces the size of
    the monolithic sommelier.h. Although sl_context contains pointers
    to many structs declared in sommelier.h, they can be (and were
    already) forward-declared, which seems fine.
    
    BUG=b:173977876
    TEST=use sommelier as normal, this is just a refactor
    
    Change-Id: I4d74fc096815930e0a45fe72fce3be257be498bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3261311
    Tested-by: Chloe Pelling <cpelling@google.com>
    Auto-Submit: Chloe Pelling <cpelling@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Nic Hollingum <hollingum@google.com>

--- vm_tools/sommelier/sommelier-ctx.h
+++ vm_tools/sommelier/sommelier-ctx.h
@@ -5,6 +5,15 @@
 #ifndef VM_TOOLS_SOMMELIER_SOMMELIER_CTX_H_
 #define VM_TOOLS_SOMMELIER_SOMMELIER_CTX_H_
 
+#include <memory>
+#include <wayland-server.h>
+#include <wayland-util.h>
+#include <xcb/xcb.h>
+
+#include "sommelier-timing.h"  // NOLINT(build/include_directory)
+#include "sommelier-util.h"    // NOLINT(build/include_directory)
+#include "virtualization/wayland_channel.h"
+
 // A list of atoms to intern (create/fetch) when connecting to the X server.
 //
 // To add an atom, declare it here and define it in |sl_context_atom_name|.
@@ -41,6 +50,112 @@ enum {
   ATOM_GTK_THEME_VARIANT,
   ATOM_XWAYLAND_RANDR_EMU_MONITOR_RECTS,
   ATOM_LAST = ATOM_XWAYLAND_RANDR_EMU_MONITOR_RECTS,
+};
+
+struct sl_context {
+  char** runprog;
+  struct wl_display* display;
+  struct wl_display* host_display;
+  struct wl_client* client;
+  struct sl_compositor* compositor;
+  struct sl_subcompositor* subcompositor;
+  struct sl_shm* shm;
+  struct sl_shell* shell;
+  struct sl_data_device_manager* data_device_manager;
+  struct sl_xdg_shell* xdg_shell;
+  struct sl_aura_shell* aura_shell;
+  struct sl_viewporter* viewporter;
+  struct sl_linux_dmabuf* linux_dmabuf;
+  struct sl_linux_explicit_synchronization* linux_explicit_synchronization;
+  struct sl_keyboard_extension* keyboard_extension;
+  struct sl_text_input_manager* text_input_manager;
+#ifdef GAMEPAD_SUPPORT
+  struct sl_gaming_input_manager* gaming_input_manager;
+#endif
+  struct sl_relative_pointer_manager* relative_pointer_manager;
+  struct sl_pointer_constraints* pointer_constraints;
+  struct wl_list outputs;
+  struct wl_list seats;
+  std::unique_ptr<struct wl_event_source> display_event_source;
+  std::unique_ptr<struct wl_event_source> display_ready_event_source;
+  std::unique_ptr<struct wl_event_source> sigchld_event_source;
+  std::unique_ptr<struct wl_event_source> sigusr1_event_source;
+  std::unique_ptr<struct wl_event_source> clipboard_event_source;
+  struct wl_array dpi;
+  int wm_fd;
+  int wayland_channel_fd;
+  int virtwl_socket_fd;
+  int virtwl_display_fd;
+  std::unique_ptr<struct wl_event_source> wayland_channel_event_source;
+  std::unique_ptr<struct wl_event_source> virtwl_socket_event_source;
+  const char* vm_id;
+  const char* drm_device;
+  struct gbm_device* gbm;
+  int xwayland;
+  pid_t xwayland_pid;
+  pid_t child_pid;
+  pid_t peer_pid;
+  struct xkb_context* xkb_context;
+  struct wl_list accelerators;
+  struct wl_list registries;
+  struct wl_list globals;
+  struct wl_list host_outputs;
+  int next_global_id;
+  xcb_connection_t* connection;
+  std::unique_ptr<struct wl_event_source> connection_event_source;
+  const xcb_query_extension_reply_t* xfixes_extension;
+  xcb_screen_t* screen;
+  xcb_window_t window;
+  struct wl_list windows, unpaired_windows;
+  struct sl_window* host_focus_window;
+  int needs_set_input_focus;
+#ifdef GAMEPAD_SUPPORT
+  struct wl_list gamepads;
+#endif
+  double desired_scale;
+  double scale;
+  const char* application_id;
+  int exit_with_child;
+  const char* sd_notify;
+  int clipboard_manager;
+  uint32_t frame_color;
+  uint32_t dark_frame_color;
+  bool support_damage_buffer;
+  int fullscreen_mode;
+  struct sl_host_seat* default_seat;
+  xcb_window_t selection_window;
+  xcb_window_t selection_owner;
+  int selection_incremental_transfer;
+  xcb_selection_request_event_t selection_request;
+  xcb_timestamp_t selection_timestamp;
+  struct wl_data_device* selection_data_device;
+  struct sl_data_offer* selection_data_offer;
+  struct sl_data_source* selection_data_source;
+  int selection_data_source_send_fd;
+  struct wl_list selection_data_source_send_pending;
+  std::unique_ptr<struct wl_event_source> selection_send_event_source;
+  xcb_get_property_reply_t* selection_property_reply;
+  int selection_property_offset;
+  std::unique_ptr<struct wl_event_source> selection_event_source;
+  xcb_atom_t selection_data_type;
+  struct wl_array selection_data;
+  int selection_data_offer_receive_fd;
+  int selection_data_ack_pending;
+  union {
+    const char* name;
+    xcb_intern_atom_cookie_t cookie;
+    xcb_atom_t value;
+  } atoms[ATOM_LAST + 1];
+  xcb_visualid_t visual_ids[256];
+  xcb_colormap_t colormaps[256];
+  Timing* timing;
+  const char* trace_filename;
+  bool trace_system;
+  bool use_explicit_fence;
+  bool use_virtgpu_channel;
+  // Never freed after allocation due the fact sommelier doesn't have a
+  // shutdown function yet.
+  WaylandChannel* channel;
 };
 
 // Returns the string mapped to the given ATOM_ enum value.
