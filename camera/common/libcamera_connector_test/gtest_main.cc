8ae40e9806794b91a2b16aa907980287f5f35429
hscham <hscham@chromium.org>
2021-11-04 15:16:42 +0900

    camera: include base/containers/cxx20_erase.h for base::Erase{,If}
    The functions will be moved from base/stl_util.h to their own files and
    include base/containers/cxx20_erase.h.
    
    BUG=b:198993705
    TEST=emerge-hatch cros-camera-libs with new libchrome
    
    Change-Id: If52a6a910f59a4e9bbfdbad6dc67758fa1fc72ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3261315
    Tested-by: Grace Cham <hscham@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Grace Cham <hscham@chromium.org>

--- camera/common/libcamera_connector_test/gtest_main.cc
+++ camera/common/libcamera_connector_test/gtest_main.cc
@@ -10,10 +10,10 @@
 
 #include <base/check_op.h>
 #include <base/command_line.h>
+#include <base/containers/cxx20_erase.h>
 #include <base/files/file_util.h>
 #include <base/process/launch.h>
 #include <base/posix/safe_strerror.h>
-#include <base/stl_util.h>
 #include <base/strings/string_piece.h>
 #include <base/strings/string_split.h>
 #include <base/strings/string_util.h>
