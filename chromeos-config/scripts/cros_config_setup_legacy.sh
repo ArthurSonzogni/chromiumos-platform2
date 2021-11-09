e05b3dc780598f9a923d8ffa084cbb179e80b54e
Jack Rosenthal <jrosenth@chromium.org>
2021-11-09 10:13:10 -0700

    chromeos-config: Add cros_config_setup script for pre-unibuild
    Add a script to be used on the limited number of remaining
    pre-unibuild devices in place of cros_config_setup.
    
    Note: we can probably reduce the set of configs we export, but
    matching 1:1 with libcros_config at first.
    
    BUG=b:202766035
    TEST=provided unit tests pass
    
    Change-Id: Ib69fbe3095c3a060a33d07fbd196a633710a694c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3270403
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Raul E Rangel <rrangel@chromium.org>

---
+++ chromeos-config/scripts/cros_config_setup_legacy.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Set up /run/chromeos-config during boot (assumes pre-unibuild).
+# Note: This is written to target busybox ash and bash, as it needs to
+# run in recovery initramfs, where we only have busybox.
+
+# TODO(jrosenth): Delete this script once we're 100% unibuild.
+
+set -e
+
+: "${MOUNTPOINT:=/run/chromeos-config}"
+CONFIG_OUT="${MOUNTPOINT}/private"
+
+# Magic environment variable that lets us get access to hidden mosys
+# commands accessible only to cros-config now.
+export I_AM_CROS_CONFIG=1
+
+# Set a config to a value.
+#   $1: path
+#   $2: property
+#   $3: value
+setconfig() {
+    local path="$1"
+    local property="$2"
+    local value="$3"
+
+    if [ -n "${value}" ]; then
+        mkdir -p "${CONFIG_OUT}${path}"
+        echo -n "${value}" > "${CONFIG_OUT}${path}/${property}"
+    fi
+}
+
+model="$(mosys platform model)"
+brand_code="$(mosys platform brand)"
+sku_id="$(mosys platform sku || true)"
+customization_id="$(mosys platform customization || true)"
+platform_name="$(mosys platform name)"
+psu_type="$(mosys psu type || true)"
+eval "$(grep DEVICETYPE /etc/lsb-release)"
+
+case "${DEVICETYPE}" in
+    CHROMEBOOK )
+        has_backlight=true
+        : "${psu_type:=battery}"
+        ;;
+    * )
+        has_backlight=false
+        : "${psu_type:=AC_only}"
+esac
+
+setconfig / brand-code "${brand_code}"
+setconfig / name "${model}"
+setconfig /firmware image-name "${model}"
+setconfig /hardware-properties form-factor "${DEVICETYPE}"
+setconfig /hardware-properties has-backlight "${has_backlight}"
+setconfig /hardware-properties psu-type "${psu_type}"
+setconfig /identity customization-id "${customization_id}"
+setconfig /identity platform-name "${platform_name}"
+setconfig /identity sku-id "${sku_id}"
+
+mount -n -obind,ro,nodev,noexec,nosuid "${CONFIG_OUT}" "${MOUNTPOINT}/v1"
