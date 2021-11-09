7a133caa3b37b80d7641cec6590705b531c52217
Taoyu Li <taoyl@google.com>
2021-11-09 21:37:18 +0900

    shill: vpn: Improve OpenVpnManagementServerFuzzer
    This patch fuzzes OnInput(), the general entry point of
    OpenVpnManagementServer, as an additional of current fuzzer on
    parsing helper functions.
    
    BUG=b:203712717
    TEST=ran fuzzer for a few minutes
    
    Change-Id: I4bfc279345894175b42ae7e571874822fb636300
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3270089
    Tested-by: Taoyu Li <taoyl@chromium.org>
    Reviewed-by: Garrick Evans <garrick@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Jie Jiang <jiejiang@chromium.org>
    Commit-Queue: Manoj Gupta <manojgupta@chromium.org>

--- shill/vpn/openvpn_management_server_fuzzer.cc
+++ shill/vpn/openvpn_management_server_fuzzer.cc
@@ -8,9 +8,50 @@
 #include <base/logging.h>
 #include <fuzzer/FuzzedDataProvider.h>
 
+#include "shill/net/io_handler.h"
+#include "shill/net/sockets.h"
+#include "shill/vpn/openvpn_driver.h"
 #include "shill/vpn/openvpn_management_server.h"
 
+namespace {
+const int kPlaceholderSocket = 234;
+}
+
 namespace shill {
+
+class FakeOpenVPNDriver : public OpenVPNDriver {
+ public:
+  FakeOpenVPNDriver() : OpenVPNDriver(nullptr, nullptr) {}
+  FakeOpenVPNDriver(const FakeOpenVPNDriver&) = delete;
+  FakeOpenVPNDriver& operator=(const FakeOpenVPNDriver&) = delete;
+
+  ~FakeOpenVPNDriver() = default;
+
+  void OnReconnecting(ReconnectReason) override{};
+  void FailService(Service::ConnectFailure, const std::string&) override{};
+  void ReportCipherMetrics(const std::string&) override{};
+};
+
+class FakeSockets : public Sockets {
+ public:
+  FakeSockets() = default;
+  FakeSockets(const FakeSockets&) = delete;
+  FakeSockets& operator=(const FakeSockets&) = delete;
+
+  ~FakeSockets() override = default;
+  int Accept(int sockfd,
+             struct sockaddr* addr,
+             socklen_t* addrlen) const override {
+    return kPlaceholderSocket;
+  };
+  int Close(int fd) const override { return 0; };
+  ssize_t Send(int sockfd,
+               const void* buf,
+               size_t len,
+               int flags) const override {
+    return len;
+  };
+};
 
 class OpenVPNManagementServerFuzzer {
  public:
@@ -32,6 +73,16 @@ class OpenVPNManagementServerFuzzer {
         provider.ConsumeRandomLengthString(1024));
     OpenVPNManagementServer::ParsePasswordFailedReason(
         provider.ConsumeRandomLengthString(1024));
+
+    // Send remaining data to test general entry point OnInput().
+    auto data_vector = provider.ConsumeRemainingBytes<uint8_t>();
+    InputData input_data(data_vector.data(), data_vector.size());
+    FakeOpenVPNDriver driver;
+    FakeSockets sockets;
+    OpenVPNManagementServer server(&driver);
+    server.connected_socket_ = kPlaceholderSocket;
+    server.sockets_ = &sockets;
+    server.OnInput(&input_data);
   }
 };
 
