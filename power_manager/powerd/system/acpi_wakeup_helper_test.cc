9c363cb8f299cf1b9ebbcbcc3dbae1bd12088385
Prashant Malani <pmalani@chromium.org>
2021-11-10 14:49:55 -0800

    power: Add AcpiWakeupHelperFuzzer
    Create a fuzzer for the AcpiWakeupHelper class. In order to re-use the
    FakeAcpiWakeupFile, factor it out into a separate header.
    
    BUG=b:205892888
    FIXES=b:205892888
    TEST=Run fuzzer on local machine for 30 minutes using cros_fuzz.
    
    Change-Id: I93aad6a93f3701ec5f3d7e4a05562c4c35220099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3274016
    Tested-by: Prashant Malani <pmalani@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Prashant Malani <pmalani@chromium.org>

--- power_manager/powerd/system/acpi_wakeup_helper_test.cc
+++ power_manager/powerd/system/acpi_wakeup_helper_test.cc
@@ -9,6 +9,8 @@
 #include "gtest/gtest.h"
 #include "power_manager/common/test_main_loop_runner.h"
 
+#include "power_manager/powerd/system/fake_acpi_wakeup_file.h"
+
 namespace power_manager {
 namespace system {
 
@@ -32,57 +34,6 @@ const char* const kFileContents[] = {
     "HDEF\t  S3\t*disabled  pci:0000:00:1b.0\n"
     "EHCI\t  S3\t*disabled\n"
     "XHCI\t  S3\t*enabled   pci:0000:00:14.0\n"};
-
-class FakeAcpiWakeupFile : public AcpiWakeupFileInterface {
- public:
-  FakeAcpiWakeupFile()
-      : contents_(nullptr),
-        expected_write_(nullptr),
-        contents_after_write_(nullptr) {}
-
-  bool Exists() override { return contents_ != nullptr; }
-
-  bool Read(std::string* contents) override {
-    if (!contents_)
-      return false;
-    *contents = contents_;
-    return true;
-  }
-
-  bool Write(const std::string& contents) override {
-    if (!expected_write_ || contents != std::string(expected_write_)) {
-      ADD_FAILURE() << "Unexpected write";
-      return false;
-    }
-    contents_ = contents_after_write_;
-    expected_write_ = nullptr;
-    contents_after_write_ = nullptr;
-    return true;
-  }
-
-  void set_contents(const char* contents) {
-    contents_ = contents;
-    expected_write_ = nullptr;
-    contents_after_write_ = nullptr;
-  }
-
-  void ExpectWrite(const char* expected_write,
-                   const char* contents_after_write) {
-    expected_write_ = expected_write;
-    contents_after_write_ = contents_after_write;
-  }
-
-  void Verify() {
-    if (expected_write_) {
-      ADD_FAILURE() << "Expected write did not happen";
-    }
-  }
-
- private:
-  const char* contents_;
-  const char* expected_write_;
-  const char* contents_after_write_;
-};
 
 }  // namespace
 
