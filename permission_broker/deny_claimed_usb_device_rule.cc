11656a8080603aca4c3ce7336c335e4ff5eb6288
Daniel Verkamp <dverkamp@chromium.org>
2021-11-18 12:40:43 -0800

    permission_broker: allow USB serial access to ValueCAN4 devices
    These devices expose CDC-ACM interfaces so we need to allow
    disconnecting them from the kernel driver in order to let them be shared
    with Linux.
    
    BUG=b:197176243
    TEST=Attach ValueCAN4 device to Linux
    
    Change-Id: I00c685bb5d067e1e257d4ff6820ddec3a8540fa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3291848
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
    Tested-by: Daniel Verkamp <dverkamp@chromium.org>

--- permission_broker/deny_claimed_usb_device_rule.cc
+++ permission_broker/deny_claimed_usb_device_rule.cc
@@ -182,6 +182,8 @@ bool IsDeviceAllowedSerial(udev_device* device) {
   // grep -o -E  "vid\..*=(0x.*)" *boards.txt | sed "s/vid\..=//g" | sort -f | \
   // uniq -i
   const DevicePolicy::UsbDeviceId kAllowedIds[] = {
+      {0x093c, 0x1101},  // Intrepid Control Systems ValueCAN 4
+
       {0x0d28, 0x0204},  // BBC micro:bit
 
       {0x2341, 0},  // Arduino
