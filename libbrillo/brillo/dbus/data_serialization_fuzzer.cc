201bb8718d87a848209bd548857e62367a36b485
Maksim Ivanov <emaxx@chromium.org>
2021-11-19 13:31:03 +0100

    libbrillo: Fuzz with fully random D-Bus messages
    Enhance the libbrillo_dbus_data_serialization_fuzzer to additionally
    generate fully random D-Bus response messages, so that the parsing code
    is exercised not only on correct messages generated by MessageWriter.
    
    BUG=none
    TEST=run the fuzzer locally and check the coverage increases
    
    Change-Id: I8f52bbe15738918a84852e0ac2da96142cb256f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3291656
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Tested-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- libbrillo/brillo/dbus/data_serialization_fuzzer.cc
+++ libbrillo/brillo/dbus/data_serialization_fuzzer.cc
@@ -89,7 +89,153 @@ std::string GenerateValidUTF8(FuzzedDataProvider* data_provider) {
   return str;
 }
 
-}  // namespace
+std::unique_ptr<dbus::Response> DemarshalRandomDBusResponse(
+    FuzzedDataProvider* data_provider) {
+  std::string string_to_demarshal = data_provider->ConsumeRandomLengthString();
+  DBusMessage* raw_message =
+      dbus_message_demarshal(string_to_demarshal.data(),
+                             string_to_demarshal.size(), /*error=*/nullptr);
+  if (!raw_message)
+    return nullptr;
+  if (dbus_message_get_type(raw_message) != DBUS_MESSAGE_TYPE_METHOD_RETURN) {
+    dbus_message_unref(raw_message);
+    return nullptr;
+  }
+  return dbus::Response::FromRawMessage(raw_message);
+}
+
+std::unique_ptr<dbus::Response> WriteRandomDBusResponse(
+    FuzzedDataProvider* data_provider) {
+  // Consume a random fraction of our data writing random things to a D-Bus
+  // message, and then consume the remaining data reading randomly from that
+  // same D-Bus message.  Given the templated nature of these functions and that
+  // they support essentially an infinite amount of types, we are constraining
+  // this to a fixed set of types defined above.
+  std::unique_ptr<dbus::Response> message = dbus::Response::CreateEmpty();
+
+  dbus::MessageWriter writer(message.get());
+
+  int bytes_left_for_read =
+      static_cast<int>(data_provider->ConsumeProbability<float>() *
+                       data_provider->remaining_bytes());
+  while (data_provider->remaining_bytes() > bytes_left_for_read) {
+    DataType curr_type = data_provider->ConsumeEnum<DataType>();
+    bool variant = data_provider->ConsumeBool();
+    switch (curr_type) {
+      case kUint8:
+        GenerateIntAndAppendValue<uint8_t>(data_provider, &writer, variant);
+        break;
+      case kUint16:
+        GenerateIntAndAppendValue<uint16_t>(data_provider, &writer, variant);
+        break;
+      case kUint32:
+        GenerateIntAndAppendValue<uint32_t>(data_provider, &writer, variant);
+        break;
+      case kUint64:
+        GenerateIntAndAppendValue<uint64_t>(data_provider, &writer, variant);
+        break;
+      case kInt16:
+        GenerateIntAndAppendValue<int16_t>(data_provider, &writer, variant);
+        break;
+      case kInt32:
+        GenerateIntAndAppendValue<int32_t>(data_provider, &writer, variant);
+        break;
+      case kInt64:
+        GenerateIntAndAppendValue<int64_t>(data_provider, &writer, variant);
+        break;
+      case kBool:
+        AppendValue(&writer, variant, data_provider->ConsumeBool());
+        break;
+      case kDouble:
+        AppendValue(&writer, variant,
+                    data_provider->ConsumeProbability<double>());
+        break;
+      case kString:
+        AppendValue(&writer, variant, GenerateValidUTF8(data_provider));
+        break;
+      case kObjectPath: {
+        std::string object_path =
+            data_provider->ConsumeRandomLengthString(kRandomMaxDataLength);
+        // If this isn't valid we'll hit a CHECK failure.
+        if (dbus::IsValidObjectPath(object_path))
+          AppendValue(&writer, variant, dbus::ObjectPath(object_path));
+        break;
+      }
+      case kVectorInt16: {
+        int vec_size = data_provider->ConsumeIntegralInRange<int>(
+            0, kRandomMaxContainerSize);
+        std::vector<int16_t> vec(vec_size);
+        for (int i = 0; i < vec_size; i++)
+          vec[i] = data_provider->ConsumeIntegral<int16_t>();
+        AppendValue(&writer, variant, vec);
+        break;
+      }
+      case kVectorString: {
+        int vec_size = data_provider->ConsumeIntegralInRange<int>(
+            0, kRandomMaxContainerSize);
+        std::vector<std::string> vec(vec_size);
+        for (int i = 0; i < vec_size; i++)
+          vec[i] = GenerateValidUTF8(data_provider);
+        AppendValue(&writer, variant, vec);
+        break;
+      }
+      case kPairBoolInt64:
+        AppendValue(&writer, variant,
+                    std::pair<bool, int64_t>{
+                        data_provider->ConsumeBool(),
+                        data_provider->ConsumeIntegral<int64_t>()});
+        break;
+      case kPairUint32String:
+        AppendValue(&writer, variant,
+                    std::pair<uint32_t, std::string>{
+                        data_provider->ConsumeIntegral<uint32_t>(),
+                        GenerateValidUTF8(data_provider)});
+        break;
+      case kTupleUint16StringBool:
+        AppendValue(&writer, variant,
+                    std::tuple<uint32_t, std::string, bool>{
+                        data_provider->ConsumeIntegral<uint32_t>(),
+                        GenerateValidUTF8(data_provider),
+                        data_provider->ConsumeBool()});
+        break;
+      case kTupleDoubleInt32ObjectPath: {
+        std::string object_path =
+            data_provider->ConsumeRandomLengthString(kRandomMaxDataLength);
+        // If this isn't valid we'll hit a CHECK failure.
+        if (dbus::IsValidObjectPath(object_path)) {
+          AppendValue(&writer, variant,
+                      std::tuple<double, int32_t, dbus::ObjectPath>{
+                          data_provider->ConsumeProbability<double>(),
+                          data_provider->ConsumeIntegral<int32_t>(),
+                          dbus::ObjectPath(object_path)});
+        }
+        break;
+      }
+      case kMapInt32String: {
+        int map_size = data_provider->ConsumeIntegralInRange<int>(
+            0, kRandomMaxContainerSize);
+        std::map<int32_t, std::string> map;
+        for (int i = 0; i < map_size; i++)
+          map[data_provider->ConsumeIntegral<int32_t>()] =
+              GenerateValidUTF8(data_provider);
+        AppendValue(&writer, variant, map);
+        break;
+      }
+      case kMapDoubleBool: {
+        int map_size = data_provider->ConsumeIntegralInRange<int>(
+            0, kRandomMaxContainerSize);
+        std::map<double, bool> map;
+        for (int i = 0; i < map_size; i++)
+          map[data_provider->ConsumeProbability<double>()] =
+              data_provider->ConsumeBool();
+        AppendValue(&writer, variant, map);
+        break;
+      }
+    }
+  }
+
+  return message;
+}
 
 class Environment {
  public:
@@ -99,134 +245,20 @@ class Environment {
   }
 };
 
+}  // namespace
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static Environment env;
-  FuzzedDataProvider data_provider(data, size);
-  // Consume a random fraction of our data writing random things to a D-Bus
-  // message, and then consume the remaining data reading randomly from that
-  // same D-Bus message.  Given the templated nature of these functions and that
-  // they support essentially an infinite amount of types, we are constraining
-  // this to a fixed set of types defined above.
-  std::unique_ptr<dbus::Response> message = dbus::Response::CreateEmpty();
-  dbus::MessageWriter writer(message.get());
 
-  int bytes_left_for_read =
-      static_cast<int>(data_provider.ConsumeProbability<float>() * size);
-  while (data_provider.remaining_bytes() > bytes_left_for_read) {
-    DataType curr_type = data_provider.ConsumeEnum<DataType>();
-    bool variant = data_provider.ConsumeBool();
-    switch (curr_type) {
-      case kUint8:
-        GenerateIntAndAppendValue<uint8_t>(&data_provider, &writer, variant);
-        break;
-      case kUint16:
-        GenerateIntAndAppendValue<uint16_t>(&data_provider, &writer, variant);
-        break;
-      case kUint32:
-        GenerateIntAndAppendValue<uint32_t>(&data_provider, &writer, variant);
-        break;
-      case kUint64:
-        GenerateIntAndAppendValue<uint64_t>(&data_provider, &writer, variant);
-        break;
-      case kInt16:
-        GenerateIntAndAppendValue<int16_t>(&data_provider, &writer, variant);
-        break;
-      case kInt32:
-        GenerateIntAndAppendValue<int32_t>(&data_provider, &writer, variant);
-        break;
-      case kInt64:
-        GenerateIntAndAppendValue<int64_t>(&data_provider, &writer, variant);
-        break;
-      case kBool:
-        AppendValue(&writer, variant, data_provider.ConsumeBool());
-        break;
-      case kDouble:
-        AppendValue(&writer, variant,
-                    data_provider.ConsumeProbability<double>());
-        break;
-      case kString:
-        AppendValue(&writer, variant, GenerateValidUTF8(&data_provider));
-        break;
-      case kObjectPath: {
-        std::string object_path =
-            data_provider.ConsumeRandomLengthString(kRandomMaxDataLength);
-        // If this isn't valid we'll hit a CHECK failure.
-        if (dbus::IsValidObjectPath(object_path))
-          AppendValue(&writer, variant, dbus::ObjectPath(object_path));
-        break;
-      }
-      case kVectorInt16: {
-        int vec_size = data_provider.ConsumeIntegralInRange<int>(
-            0, kRandomMaxContainerSize);
-        std::vector<int16_t> vec(vec_size);
-        for (int i = 0; i < vec_size; i++)
-          vec[i] = data_provider.ConsumeIntegral<int16_t>();
-        AppendValue(&writer, variant, vec);
-        break;
-      }
-      case kVectorString: {
-        int vec_size = data_provider.ConsumeIntegralInRange<int>(
-            0, kRandomMaxContainerSize);
-        std::vector<std::string> vec(vec_size);
-        for (int i = 0; i < vec_size; i++)
-          vec[i] = GenerateValidUTF8(&data_provider);
-        AppendValue(&writer, variant, vec);
-        break;
-      }
-      case kPairBoolInt64:
-        AppendValue(
-            &writer, variant,
-            std::pair<bool, int64_t>{data_provider.ConsumeBool(),
-                                     data_provider.ConsumeIntegral<int64_t>()});
-        break;
-      case kPairUint32String:
-        AppendValue(&writer, variant,
-                    std::pair<uint32_t, std::string>{
-                        data_provider.ConsumeIntegral<uint32_t>(),
-                        GenerateValidUTF8(&data_provider)});
-        break;
-      case kTupleUint16StringBool:
-        AppendValue(&writer, variant,
-                    std::tuple<uint32_t, std::string, bool>{
-                        data_provider.ConsumeIntegral<uint32_t>(),
-                        GenerateValidUTF8(&data_provider),
-                        data_provider.ConsumeBool()});
-        break;
-      case kTupleDoubleInt32ObjectPath: {
-        std::string object_path =
-            data_provider.ConsumeRandomLengthString(kRandomMaxDataLength);
-        // If this isn't valid we'll hit a CHECK failure.
-        if (dbus::IsValidObjectPath(object_path)) {
-          AppendValue(&writer, variant,
-                      std::tuple<double, int32_t, dbus::ObjectPath>{
-                          data_provider.ConsumeProbability<double>(),
-                          data_provider.ConsumeIntegral<int32_t>(),
-                          dbus::ObjectPath(object_path)});
-        }
-        break;
-      }
-      case kMapInt32String: {
-        int map_size = data_provider.ConsumeIntegralInRange<int>(
-            0, kRandomMaxContainerSize);
-        std::map<int32_t, std::string> map;
-        for (int i = 0; i < map_size; i++)
-          map[data_provider.ConsumeIntegral<int32_t>()] =
-              GenerateValidUTF8(&data_provider);
-        AppendValue(&writer, variant, map);
-        break;
-      }
-      case kMapDoubleBool: {
-        int map_size = data_provider.ConsumeIntegralInRange<int>(
-            0, kRandomMaxContainerSize);
-        std::map<double, bool> map;
-        for (int i = 0; i < map_size; i++)
-          map[data_provider.ConsumeProbability<double>()] =
-              data_provider.ConsumeBool();
-        AppendValue(&writer, variant, map);
-        break;
-      }
-    }
-  }
+  FuzzedDataProvider data_provider(data, size);
+
+  std::unique_ptr<dbus::Response> message;
+  if (data_provider.ConsumeBool())
+    message = DemarshalRandomDBusResponse(&data_provider);
+  else
+    message = WriteRandomDBusResponse(&data_provider);
+  if (!message)
+    return 0;
 
   dbus::MessageReader reader(message.get());
   while (data_provider.remaining_bytes()) {
