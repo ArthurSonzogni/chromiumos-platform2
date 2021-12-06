02bd89c3ae64eec843344851716d030636636316
hscham <hscham@chromium.org>
2021-12-06 10:55:55 +0900

    camera: include base/files/file.h explicitly
    For use of base::File.
    
    BUG=b:198993705
    TEST=FEATURES=test emerge-atlas cros-camera-libs
    
    Change-Id: Id77f023b2060cbf14f5f377878cfb155aff7973c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3316167
    Tested-by: Grace Cham <hscham@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Grace Cham <hscham@chromium.org>

--- camera/common/camera_gpu_algorithm.cc
+++ camera/common/camera_gpu_algorithm.cc
@@ -9,6 +9,7 @@
 #include <base/bind.h>
 #include <base/check_op.h>
 #include <base/containers/contains.h>
+#include <base/files/file.h>
 #include <base/logging.h>
 #include <base/numerics/safe_conversions.h>
 
