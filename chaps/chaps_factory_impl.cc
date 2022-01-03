feb6abda0a0729e24a84185e9bb706c8fcf4101e
Howard Yang <hcyang@google.com>
2022-01-03 13:29:58 +0800

    chaps: Add fuzzer for ChapsServiceImpl
    Add a fuzzer for ChapsServiceImpl, fuzzing its methods and the
    TokenManagerInterface methods in its SlotManager since they interact
    together.
    
    BUG=b:172219720
    TEST=run chaps_service_fuzzer for 10 mins
    
    Cq-Depend: chromium:3359410
    Change-Id: I58f9657566ba501e91d6aa9510c276b14bff2718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3365882
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Tested-by: Howard Yang <hcyang@google.com>
    Commit-Queue: Howard Yang <hcyang@google.com>

--- chaps/chaps_factory_impl.cc
+++ chaps/chaps_factory_impl.cc
@@ -54,7 +54,12 @@ ObjectPool* ChapsFactoryImpl::CreateObjectPool(
 
 ObjectStore* ChapsFactoryImpl::CreateObjectStore(const FilePath& file_name) {
   std::unique_ptr<ObjectStoreImpl> store(new ObjectStoreImpl());
+#if USE_FUZZER
+  // Use in-memory object store when fuzzing.
+  if (!store->Init(base::FilePath(":memory:"))) {
+#else
   if (!store->Init(file_name)) {
+#endif  // USE_FUZZER
     // The approach here is to limp along without a persistent object store so
     // crypto services do not become unavailable. The side-effect is that all
     // objects will disappear when the token is removed (e.g. at logout).
