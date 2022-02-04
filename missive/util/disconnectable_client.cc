f399eac14161326d12a63bda5c458302744f082e
Leonid Baraz <lbaraz@chromium.org>
2022-02-04 01:59:59 +0000

    missive: Implement disconnectable client
    This is a wrapper on top of an abstract client which may become
    available or unavailable at any moment. Calls made through the client
    are rejected with error if the client is not available, and cancelled
    immediately returning an error, if it becomes unavailable while the call
    is in progress.
    The wrapper is intended to be used for dBus connection to missive client
    in Chrome and from daemon to Chrome in missived.
    
    BUG=b:205814822
    TEST=FEATURES="test" emerge-$BOARD missive
    
    Change-Id: I9d0bea844454822ef48fd38f2d2ab4c11bf31019
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3438146
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: Hong Xu <xuhong@google.com>
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Tested-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>

---
+++ missive/util/disconnectable_client.cc
@@ -0,0 +1,78 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "missive/util/disconnectable_client.h"
+
+#include <memory>
+#include <utility>
+
+#include <base/callback.h>
+#include <base/containers/fixed_flat_map.h>
+#include <base/logging.h>
+#include <base/memory/scoped_refptr.h>
+#include <base/memory/weak_ptr.h>
+#include <base/rand_util.h>
+#include <base/sequence_checker.h>
+#include <base/task/sequenced_task_runner.h>
+
+#include "missive/util/status.h"
+
+namespace reporting {
+
+DisconnectableClient::DisconnectableClient(
+    scoped_refptr<base::SequencedTaskRunner> task_runner)
+    : task_runner_(task_runner) {
+  // Client may be created not on the |task_runner|.
+  DETACH_FROM_SEQUENCE(sequence_checker_);
+}
+
+DisconnectableClient::~DisconnectableClient() = default;
+
+void DisconnectableClient::MaybeMakeCall(std::unique_ptr<Delegate> delegate) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  // Bail out, if missive daemon is not available over dBus.
+  if (!is_available_) {
+    delegate->Respond(
+        Status(reporting::error::UNAVAILABLE, "Service is unavailable"));
+    return;
+  }
+  // Add the delegate to the map.
+  const auto id = base::RandUint64();
+  auto res = outstanding_delegates_.emplace(id, std::move(delegate));
+  DCHECK(res.second) << "Duplicate call id " << id;
+  // Make a call, resume on CallResponded, when response is received.
+  res.first->second->DoCall(base::BindOnce(&DisconnectableClient::CallResponded,
+                                           weak_ptr_factory_.GetWeakPtr(), id));
+}
+
+void DisconnectableClient::CallResponded(uint64_t id) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  auto it = outstanding_delegates_.find(id);
+  if (it == outstanding_delegates_.end()) {
+    // Callback has already been removed, no action needed.
+    return;
+  }
+  // Respond through the |delegate|.
+  auto delegate = std::move(it->second);
+  outstanding_delegates_.erase(it);
+  // Respond.
+  delegate->Respond(Status::StatusOK());
+}
+
+void DisconnectableClient::SetAvailability(bool is_available) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  is_available_ = is_available;
+  LOG(WARNING) << "Service became " << (is_available_ ? "" : "un")
+               << "available";
+  if (!is_available_) {
+    // Cancel all pending calls.
+    for (auto& p : outstanding_delegates_) {
+      std::move(p.second)->Respond(
+          Status(reporting::error::UNAVAILABLE, "Service is unavailable"));
+    }
+    outstanding_delegates_.clear();
+  }
+}
+
+}  // namespace reporting
