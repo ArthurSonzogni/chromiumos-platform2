6a73d2592d40da6cfe1055183262d6c40e444a6d
Chung-Sheng Wu <chungsheng@google.com>
2022-02-05 07:20:28 +0800

    mojo_service_manager: Add service manager mojom
    Add service manager mojom file. The mojom will be used by users to
    access the service manager api.
    
    BUG=b:218776811
    TEST=emerge-zork mojo_service_manager
    
    Change-Id: I828876aeb5646f64c1a391a0a1584b7b4bebe3e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3449393
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Chung-Sheng Wu <chungsheng@chromium.org>
    Tested-by: Chung-Sheng Wu <chungsheng@chromium.org>

---
+++ mojo_service_manager/lib/mojom/time_mojom_traits.h
@@ -0,0 +1,30 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MOJO_SERVICE_MANAGER_LIB_MOJOM_TIME_MOJOM_TRAITS_H_
+#define MOJO_SERVICE_MANAGER_LIB_MOJOM_TIME_MOJOM_TRAITS_H_
+
+#include <base/time/time.h>
+#include <mojo/public/cpp/bindings/struct_traits.h>
+
+#include "mojo_service_manager/lib/mojom/time.mojom.h"
+
+namespace mojo {
+
+template <>
+struct StructTraits<mojo_base::mojom::TimeDeltaDataView, base::TimeDelta> {
+  static int64_t microseconds(const base::TimeDelta& delta) {
+    return delta.InMicroseconds();
+  }
+
+  static bool Read(mojo_base::mojom::TimeDeltaDataView data,
+                   base::TimeDelta* delta) {
+    *delta = base::Microseconds(data.microseconds());
+    return true;
+  }
+};
+
+}  // namespace mojo
+
+#endif  // MOJO_SERVICE_MANAGER_LIB_MOJOM_TIME_MOJOM_TRAITS_H_
