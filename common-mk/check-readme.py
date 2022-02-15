d6bf85fd3ef3a02609f5850b3e4ab607d7e6cd1e
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2022-02-15 15:41:53 +0900

    common-mk: fix pylint errors in check-readme.py
    4-spaces indentation is adopted in the style guide.
    https://chromium.googlesource.com/chromiumos/docs/+/HEAD/styleguide/python.md#official-style-guide
    
    The file was formatted by yapf 0.32.0 with config:
    [style]
    based_on_style = yapf
    split_before_bitwise_operator = false
    blank_line_before_module_docstring = true
    indent_width = 4
    
    and then manually fixed for max line width.
    Also annotated for wrong-import-position exception.
    
    BUG=chromium:1239315
    TEST=../../../chromite/bin/cros lint ./check-readme.py
    
    Change-Id: I44d9cea5c7e0c21f4c2bcfd27502459a6680a53b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3463474
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>

--- common-mk/check-readme.py
+++ common-mk/check-readme.py
@@ -16,147 +16,153 @@ TOP_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 # Find chromite!
 sys.path.insert(0, os.path.join(TOP_DIR, '..', '..'))
 
+# pylint: disable=wrong-import-position
 from chromite.lib import commandline
 from chromite.lib import git
 from chromite.lib import osutils
+# pylint: enable=wrong-import-position
 
 
 def GetActiveProjects():
-  """Return the list of active projects."""
-  # Look at all the paths (files & dirs) in the top of the git repo.  This way
-  # we ignore local directories devs created that aren't actually committed.
-  cmd = ['ls-tree', '--name-only', '-z', 'HEAD']
-  result = git.RunGit(TOP_DIR, cmd)
+    """Return the list of active projects."""
+    # Look at all the paths (files & dirs) in the top of the git repo. This way
+    # we ignore local directories devs created that aren't actually committed.
+    cmd = ['ls-tree', '--name-only', '-z', 'HEAD']
+    result = git.RunGit(TOP_DIR, cmd)
 
-  # Split the output on NULs to avoid whitespace/etc... issues.
-  paths = result.stdout.split('\0')
+    # Split the output on NULs to avoid whitespace/etc... issues.
+    paths = result.stdout.split('\0')
 
-  # ls-tree -z will include a trailing NUL on all entries, not just seperation,
-  # so filter it out if found (in case ls-tree behavior changes on us).
-  for path in [x for x in paths if x]:
-    if os.path.isdir(os.path.join(TOP_DIR, path)):
-      yield path
+    # ls-tree -z will include a trailing NUL on all entries, not just
+    # separation, so filter it out if found (in case ls-tree behavior changes
+    # on us).
+    for path in [x for x in paths if x]:
+        if os.path.isdir(os.path.join(TOP_DIR, path)):
+            yield path
 
 
 def CheckTopLevel():
-  """Check the top level README.md list."""
-  ret = 0
-  path = os.path.join(TOP_DIR, 'README.md')
-  data = osutils.ReadFile(path).splitlines()
+    """Check the top level README.md list."""
+    ret = 0
+    path = os.path.join(TOP_DIR, 'README.md')
+    data = osutils.ReadFile(path).splitlines()
 
-  # Look for the directory header.
-  try:
-    i = data.index('# Local Project Directory')
-  except ValueError:
-    logging.error('README.md index out of sync')
-    return 1
-  data = data[i + 1:]
+    # Look for the directory header.
+    try:
+        i = data.index('# Local Project Directory')
+    except ValueError:
+        logging.error('README.md index out of sync')
+        return 1
+    data = data[i + 1:]
 
-  # Pull out all the linked projects.
-  listed_projs = []
-  listed_dirs = []
-  for line in data:
-    # Break once we hit the end of the table.
-    if line.startswith('#'):
-      break
+    # Pull out all the linked projects.
+    listed_projs = []
+    listed_dirs = []
+    for line in data:
+        # Break once we hit the end of the table.
+        if line.startswith('#'):
+            break
 
-    m = re.match(r'^[|] \[([^]]+)*\]\(\./([^)]*)/\)', line)
-    if m:
-      listed_projs.append(m.group(1))
-      listed_dirs.append(m.group(2))
-  logging.debug('README.md projects: %s', listed_projs)
+        m = re.match(r'^[|] \[([^]]+)*\]\(\./([^)]*)/\)', line)
+        if m:
+            listed_projs.append(m.group(1))
+            listed_dirs.append(m.group(2))
+    logging.debug('README.md projects: %s', listed_projs)
 
-  sorted_projs = sorted(listed_projs)
-  if listed_projs != sorted_projs:
-    ret = 1
-    lines = list(difflib.unified_diff(listed_projs, sorted_projs, lineterm=''))
-    logging.error('README.md project listing should be kept sorted:\n%s',
-                  '\n'.join(lines[2:]))
+    sorted_projs = sorted(listed_projs)
+    if listed_projs != sorted_projs:
+        ret = 1
+        lines = list(
+            difflib.unified_diff(listed_projs, sorted_projs, lineterm=''))
+        logging.error('README.md project listing should be kept sorted:\n%s',
+                      '\n'.join(lines[2:]))
 
-  # Check the list in README.md for outdated entries.
-  old_dirs = []
-  for project_dir in listed_dirs:
-    path = os.path.join(TOP_DIR, project_dir)
-    if not os.path.isdir(path):
-      old_dirs.append(path)
-  if old_dirs:
-    ret = 1
-    logging.error('README.md: found stale project entries: %s', old_dirs)
+    # Check the list in README.md for outdated entries.
+    old_dirs = []
+    for project_dir in listed_dirs:
+        path = os.path.join(TOP_DIR, project_dir)
+        if not os.path.isdir(path):
+            old_dirs.append(path)
+    if old_dirs:
+        ret = 1
+        logging.error('README.md: found stale project entries: %s', old_dirs)
 
-  # Check the local source repo for missing entries.
-  existing_projs = sorted(GetActiveProjects())
-  logging.debug('Found active projects: %s', existing_projs)
-  new_projs = []
-  for proj in existing_projs:
-    path = os.path.join(TOP_DIR, proj)
-    if os.path.isdir(path) and proj not in listed_projs:
-      new_projs.append(proj)
-  if new_projs:
-    ret = 1
-    logging.error('README.md: document new projects: %s', new_projs)
+    # Check the local source repo for missing entries.
+    existing_projs = sorted(GetActiveProjects())
+    logging.debug('Found active projects: %s', existing_projs)
+    new_projs = []
+    for proj in existing_projs:
+        path = os.path.join(TOP_DIR, proj)
+        if os.path.isdir(path) and proj not in listed_projs:
+            new_projs.append(proj)
+    if new_projs:
+        ret = 1
+        logging.error('README.md: document new projects: %s', new_projs)
 
-  return ret
+    return ret
 
 
 def CheckSubdirs():
-  """Check the subdir README.md files exist."""
-  # Legacy projects that don't have a README.md file.
-  # Someone should write some docs :D.
-  LEGACYLIST = (
-      'attestation',
-      'avtest_label_detect',
-      'buffet',
-      'cros-disks',
-      'fitpicker',
-      'image-burner',
-      'init',
-      'libchromeos-ui',
-      'libcontainer',
-      'modem-utilities',
-      'mtpd',
-      'salsa',
-      'thd',
-      'timberslide',
-      'tpm_manager',
-      'trim',
-      'userfeedback',
-      'userspace_touchpad',
-      'vpn-manager',
-  )
+    """Check the subdir README.md files exist."""
+    # Legacy projects that don't have a README.md file.
+    # Someone should write some docs :D.
+    LEGACYLIST = (
+        'attestation',
+        'avtest_label_detect',
+        'buffet',
+        'cros-disks',
+        'fitpicker',
+        'image-burner',
+        'init',
+        'libchromeos-ui',
+        'libcontainer',
+        'modem-utilities',
+        'mtpd',
+        'salsa',
+        'thd',
+        'timberslide',
+        'tpm_manager',
+        'trim',
+        'userfeedback',
+        'userspace_touchpad',
+        'vpn-manager',
+    )
 
-  ret = 0
-  for proj in GetActiveProjects():
-    readme = os.path.join(TOP_DIR, proj, 'README.md')
-    if os.path.exists(readme):
-      if proj in LEGACYLIST:
-        logging.error(
-            '*** Project "%s" is in no-README LEGACYLIST, but actually has '
-            'one. Please remove it from LEGACYLIST!', proj)
-    else:
-      if not proj in LEGACYLIST:
-        logging.error('*** Project "%s" needs a README.md file', proj)
-        ret = 1
-  return ret
+    ret = 0
+    for proj in GetActiveProjects():
+        readme = os.path.join(TOP_DIR, proj, 'README.md')
+        if os.path.exists(readme):
+            if proj in LEGACYLIST:
+                logging.error(
+                    '*** Project "%s" is in no-README LEGACYLIST, but '
+                    'actually has one. Please remove it from '
+                    'LEGACYLIST!', proj)
+        else:
+            if not proj in LEGACYLIST:
+                logging.error('*** Project "%s" needs a README.md file', proj)
+                ret = 1
+    return ret
 
 
 def GetParser():
-  """Return an argument parser."""
-  parser = commandline.ArgumentParser(description=__doc__)
-  parser.add_argument('--extensions', default='gyp,gypi',
-                      help='Comma delimited file extensions to check. '
-                           '(default: %(default)s)')
-  parser.add_argument('files', nargs='*',
-                      help='Files to run lint.')
-  return parser
+    """Return an argument parser."""
+    parser = commandline.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '--extensions',
+        default='gyp,gypi',
+        help='Comma delimited file extensions to check. '
+        '(default: %(default)s)')
+    parser.add_argument('files', nargs='*', help='Files to run lint.')
+    return parser
 
 
 def main(argv):
-  parser = GetParser()
-  opts = parser.parse_args(argv)
-  opts.Freeze()
+    parser = GetParser()
+    opts = parser.parse_args(argv)
+    opts.Freeze()
 
-  return CheckTopLevel() | CheckSubdirs()
+    return CheckTopLevel() | CheckSubdirs()
 
 
 if __name__ == '__main__':
-  commandline.ScriptWrapperMain(lambda _: main)
+    commandline.ScriptWrapperMain(lambda _: main)
