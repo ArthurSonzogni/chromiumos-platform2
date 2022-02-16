d2232d7a3172ebdface7df7bd07d068b41fbf04e
Wei-Luan Wang <weiluanwang@google.com>
2022-02-16 09:29:56 +0000

    diagnostics: Inline wilco_dtc_supportd constants
    BUG=b:190796403
    TEST=FEATURES=test emerge-volteer diagnostics
    
    Change-Id: Icdadc81d575e499fd376f6addb15c981d38017e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3466610
    Tested-by: Wei-Luan Wang <weiluanwang@google.com>
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Commit-Queue: Wei-Luan Wang <weiluanwang@google.com>

--- diagnostics/wilco_dtc_supportd/ec_constants.h
+++ diagnostics/wilco_dtc_supportd/ec_constants.h
@@ -5,22 +5,33 @@
 #ifndef DIAGNOSTICS_WILCO_DTC_SUPPORTD_EC_CONSTANTS_H_
 #define DIAGNOSTICS_WILCO_DTC_SUPPORTD_EC_CONSTANTS_H_
 
+#include <poll.h>
+
 #include <cstdint>
 
 namespace diagnostics {
 namespace wilco {
 
-extern const char kEcDriverSysfsPath[];
+// Folder path exposed by sysfs EC driver.
+inline constexpr char kEcDriverSysfsPath[] =
+    "sys/bus/platform/devices/GOOG000C:00/";
 
-extern const char kEcDriverSysfsPropertiesPath[];
+// Folder path to EC properties exposed by sysfs EC driver. Relative path to
+// |kEcDriverSysfsPath|.
+inline constexpr char kEcDriverSysfsPropertiesPath[] = "properties/";
 
-extern const int64_t kEcGetTelemetryPayloadMaxSize;
+// Max request and response payload size for EC telemetry command.
+inline constexpr const int64_t kEcGetTelemetryPayloadMaxSize = 32;
 
-extern const char kEcGetTelemetryFilePath[];
+// Devfs node exposed by EC driver to EC telemetry data.
+inline constexpr char kEcGetTelemetryFilePath[] = "dev/wilco_telem0";
 
-extern const char kEcEventFilePath[];
+// EC event file path.
+inline constexpr char kEcEventFilePath[] = "dev/wilco_event0";
 
-extern const int16_t kEcEventFilePollEvents;
+// The driver is expected to populate the |kEcEventFilePath| file, therefore
+// this constant holds the specific flag for use with poll().
+inline constexpr int16_t kEcEventFilePollEvents = POLLIN;
 
 }  // namespace wilco
 }  // namespace diagnostics
