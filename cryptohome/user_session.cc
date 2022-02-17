ArthurSonzogni/chromiumos-platform2

commit c3885d14a377d8db20404881c2a51604ae2ec1e1

cryptohome: Add login cleanup helpers
Add helpers to allow emergency cleanup during login.

The feature is gated behind a policy as a killswitch.

BUG=chromium:1284559, chromium:1231192
TEST=tast run <ip> cryptohome.AutomaticCleanupCritical

Change-Id: I00ff0e21f69837dda52a98125c48c17124d67923
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3420290
Reviewed-by: Sarthak Kukreti <sarthakkukreti@chromium.org>
Reviewed-by: Daniil Lunev <dlunev@chromium.org>
Tested-by: Victor-Gabriel Savu <vsavu@google.com>
Commit-Queue: Victor-Gabriel Savu <vsavu@google.com>

cryptohome/userdataauth.cc
@@ -204,6 +204,8 @@ UserDataAuth::UserDataAuth()
       disk_cleanup_threshold_(kFreeSpaceThresholdToTriggerCleanup),
       disk_cleanup_aggressive_threshold_(
           kFreeSpaceThresholdToTriggerAggressiveCleanup),
+      disk_cleanup_critical_threshold_(
+          kFreeSpaceThresholdToTriggerCriticalCleanup),
       disk_cleanup_target_free_space_(kTargetFreeSpaceAfterCleanup),
       default_mount_factory_(new cryptohome::MountFactory()),
       mount_factory_(default_mount_factory_.get()),
@@ -334,6 +336,8 @@ bool UserDataAuth::Initialize() {
       disk_cleanup_threshold_);
   low_disk_space_handler_->disk_cleanup()->set_aggressive_cleanup_threshold(
       disk_cleanup_aggressive_threshold_);
+  low_disk_space_handler_->disk_cleanup()->set_critical_cleanup_threshold(
+      disk_cleanup_critical_threshold_);
   low_disk_space_handler_->disk_cleanup()->set_target_free_space(
       disk_cleanup_target_free_space_);
 
@@ -1069,6 +1073,11 @@ void UserDataAuth::set_cleanup_threshold(uint64_t cleanup_threshold) {
 void UserDataAuth::set_aggressive_cleanup_threshold(
     uint64_t aggressive_cleanup_threshold) {
   disk_cleanup_aggressive_threshold_ = aggressive_cleanup_threshold;
+}
+
+void UserDataAuth::set_critical_cleanup_threshold(
+    uint64_t critical_cleanup_threshold) {
+  disk_cleanup_critical_threshold_ = critical_cleanup_threshold;
 }
 
 void UserDataAuth::set_target_free_space(uint64_t target_free_space) {

cryptohome/cleanup/disk_cleanup.cc
@@ -59,8 +59,10 @@ DiskCleanup::FreeSpaceState DiskCleanup::GetFreeDiskSpaceState(
     return DiskCleanup::FreeSpaceState::kAboveThreshold;
   } else if (value >= aggressive_cleanup_threshold_) {
     return DiskCleanup::FreeSpaceState::kNeedNormalCleanup;
-  } else {
+  } else if (value >= critical_cleanup_threshold_) {
     return DiskCleanup::FreeSpaceState::kNeedAggressiveCleanup;
+  } else {
+    return DiskCleanup::FreeSpaceState::kNeedCriticalCleanup;
   }
 }
 
@@ -94,6 +96,7 @@ bool DiskCleanup::FreeDiskSpace() {
 
     case DiskCleanup::FreeSpaceState::kNeedNormalCleanup:
     case DiskCleanup::FreeSpaceState::kNeedAggressiveCleanup:
+    case DiskCleanup::FreeSpaceState::kNeedCriticalCleanup:
       // Trigger cleanup.
       VLOG(1) << "Starting cleanup with " << *free_space << " space available";
       break;
@@ -143,6 +146,77 @@ bool DiskCleanup::FreeDiskSpace() {
   VLOG(1) << "Disk cleanup cleared " << cleaned_in_mb << "MB.";
 
   LOG(INFO) << "Disk cleanup complete.";
+
+  return result;
+}
+
+bool DiskCleanup::FreeDiskSpaceDuringLogin(const std::string& obfuscated) {
+  base::ElapsedTimer total_timer;
+
+  // Only runs for enterprise users.
+  if (!homedirs_->enterprise_owned()) {
+    VLOG(1) << "Login cleanup skipped on a consumer device";
+    return true;
+  }
+
+  // Only run if enabled by policy.
+  if (!homedirs_->MustRunAutomaticCleanupOnLogin()) {
+    VLOG(1) << "Login cleanup not enabled by policy";
+    return true;
+  }
+
+  auto free_space = AmountOfFreeDiskSpace();
+
+  switch (GetFreeDiskSpaceState(free_space)) {
+    case DiskCleanup::FreeSpaceState::kAboveTarget:
+    case DiskCleanup::FreeSpaceState::kAboveThreshold:
+    case DiskCleanup::FreeSpaceState::kNeedNormalCleanup:
+    case DiskCleanup::FreeSpaceState::kNeedAggressiveCleanup:
+      // Already have enough space. No need to clean up.
+      VLOG(1) << "Skipping login cleanup with " << *free_space
+              << " space available";
+      ReportLoginDiskCleanupResult(DiskCleanupResult::kDiskCleanupSkip);
+      return true;
+
+    case DiskCleanup::FreeSpaceState::kNeedCriticalCleanup:
+      // Trigger cleanup.
+      break;
+
+    case DiskCleanup::FreeSpaceState::kError:
+      LOG(ERROR) << "Failed to get the amount of free disk space";
+      return false;
+    default:
+      LOG(ERROR) << "Unhandled free disk state";
+      return false;
+  }
+
+  LOG(WARNING) << "Starting login cleanup with " << *free_space
+               << " space available for " << obfuscated;
+
+  bool result = FreeDiskSpaceDuringLoginInternal(obfuscated);
+
+  if (result) {
+    ReportLoginDiskCleanupResult(DiskCleanupResult::kDiskCleanupSuccess);
+  } else {
+    ReportLoginDiskCleanupResult(DiskCleanupResult::kDiskCleanupError);
+  }
+
+  int cleanup_time = total_timer.Elapsed().InMilliseconds();
+  ReportLoginDiskCleanupTotalTime(cleanup_time);
+  VLOG(1) << "Login disk cleanup took " << cleanup_time << "ms.";
+
+  auto after_cleanup = AmountOfFreeDiskSpace();
+  if (!after_cleanup) {
+    LOG(ERROR) << "Failed to get the amount of free disk space";
+    return false;
+  }
+
+  auto cleaned_in_mb =
+      MAX(0, after_cleanup.value() - free_space.value()) / 1024 / 1024;
+
+  VLOG(1) << "Login disk cleanup cleared " << cleaned_in_mb << "MB.";
+
+  LOG(INFO) << "Login disk cleanup complete.";
 
   return result;
 }
@@ -279,6 +353,7 @@ bool DiskCleanup::FreeDiskSpaceInternal() {
           DiskCleanupProgress::kCacheVaultsCleanedAboveMinimum);
       return result;
     case DiskCleanup::FreeSpaceState::kNeedAggressiveCleanup:
+    case DiskCleanup::FreeSpaceState::kNeedCriticalCleanup:
       // continue cleanup
       break;
     case DiskCleanup::FreeSpaceState::kError:
@@ -323,6 +398,7 @@ bool DiskCleanup::FreeDiskSpaceInternal() {
           DiskCleanupProgress::kAndroidCacheCleanedAboveMinimum);
       return result;
     case DiskCleanup::FreeSpaceState::kNeedAggressiveCleanup:
+    case DiskCleanup::FreeSpaceState::kNeedCriticalCleanup:
       // continue cleanup
       break;
     case DiskCleanup::FreeSpaceState::kError:
@@ -401,6 +477,43 @@ bool DiskCleanup::FreeDiskSpaceInternal() {
             : DiskCleanupProgress::kWholeUserProfilesCleaned);
   } else {
     ReportDiskCleanupProgress(DiskCleanupProgress::kNoUnmountedCryptohomes);
+  }
+
+  return result;
+}
+
+bool DiskCleanup::FreeDiskSpaceDuringLoginInternal(
+    const std::string& logging_in) {
+  auto unmounted_homedirs = homedirs_->GetHomeDirs();
+  FilterMountedHomedirs(&unmounted_homedirs);
+
+  std::sort(
+      unmounted_homedirs.begin(), unmounted_homedirs.end(),
+      [&](const HomeDirs::HomeDir& a, const HomeDirs::HomeDir& b) {
+        return timestamp_manager_->GetLastUserActivityTimestamp(a.obfuscated) >
+               timestamp_manager_->GetLastUserActivityTimestamp(b.obfuscated);
+      });
+
+  bool result = true;
+
+  for (auto dir = unmounted_homedirs.rbegin(); dir != unmounted_homedirs.rend();
+       dir++) {
+    if (dir->obfuscated == logging_in) {
+      LOG(INFO) << "Skipped deletion of the user logging in.";
+      continue;
+    }
+
+    LOG(INFO) << "Freeing disk space by deleting user " << dir->obfuscated;
+    if (!routines_->DeleteUserProfile(dir->obfuscated))
+      result = false;
+    timestamp_manager_->RemoveUser(dir->obfuscated);
+
+    // Login cleanup stops at kAboveThreshold.
+    auto state = GetFreeDiskSpaceState();
+    if (state == DiskCleanup::FreeSpaceState::kAboveThreshold ||
+        state == DiskCleanup::FreeSpaceState::kAboveTarget) {
+      break;
+    }
   }
 
   return result;

cryptohome/cleanup/disk_cleanup.h
@@ -11,6 +11,7 @@
 #include <cstdint>
 #include <memory>
 #include <optional>
+#include <string>
 #include <vector>
 
 #include <base/time/time.h>
@@ -25,6 +26,7 @@ namespace cryptohome {
 // Cleanup parameters in bytes.
 const int64_t kFreeSpaceThresholdToTriggerCleanup = 1LL << 30;
 const int64_t kFreeSpaceThresholdToTriggerAggressiveCleanup = 768 * 1024 * 1024;
+const int64_t kFreeSpaceThresholdToTriggerCriticalCleanup = 512 * 1024 * 1024;
 const int64_t kTargetFreeSpaceAfterCleanup = 2LL << 30;
 
 class DiskCleanupRoutines;
@@ -40,6 +42,7 @@ class DiskCleanup {
     kAboveThreshold,         // above cleanup threshold but below cleanup target
     kNeedNormalCleanup,      // below threshold for normal cleanup
     kNeedAggressiveCleanup,  // below threshold for aggressive cleanup
+    kNeedCriticalCleanup,    // below threshold for critical cleanup
   };
 
   DiskCleanup() = default;
@@ -80,6 +83,12 @@ class DiskCleanup {
   // it goes up to cleanup_target_.
   virtual bool FreeDiskSpace();
 
+  // Frees disk space for unused cryptohomes to make sure enough space is
+  // available during login.
+  // If the available disk space is below critical_cleanup_threshold_, attempts
+  // to free space until it goes up to normal_cleanup_threshold_.
+  virtual bool FreeDiskSpaceDuringLogin(const std::string& logging_in);
+
   // Setters for cleanup thresholds.
   virtual void set_cleanup_threshold(uint64_t cleanup_threshold) {
     normal_cleanup_threshold_ = cleanup_threshold;
@@ -87,6 +96,10 @@ class DiskCleanup {
   virtual void set_aggressive_cleanup_threshold(
       uint64_t aggressive_cleanup_threshold) {
     aggressive_cleanup_threshold_ = aggressive_cleanup_threshold;
+  }
+  virtual void set_critical_cleanup_threshold(
+      uint64_t critical_cleanup_threshold) {
+    critical_cleanup_threshold_ = critical_cleanup_threshold;
   }
   virtual void set_target_free_space(uint64_t target_free_space) {
     target_free_space_ = target_free_space;
@@ -99,6 +112,9 @@ class DiskCleanup {
  private:
   // Actually performs disk cleanup. Called by FreeDiskSpace.
   bool FreeDiskSpaceInternal();
+
+  // Actually performs disk cleanup. Called by FreeDiskSpaceDuringLogin.
+  bool FreeDiskSpaceDuringLoginInternal(const std::string& obfuscated);
 
   // Removes all mounted homedirs from the vector
   void FilterMountedHomedirs(std::vector<HomeDirs::HomeDir>* homedirs);
@@ -118,6 +134,9 @@ class DiskCleanup {
   uint64_t normal_cleanup_threshold_ = kFreeSpaceThresholdToTriggerCleanup;
   uint64_t aggressive_cleanup_threshold_ =
       kFreeSpaceThresholdToTriggerAggressiveCleanup;
+  uint64_t critical_cleanup_threshold_ =
+      kFreeSpaceThresholdToTriggerCriticalCleanup;
+
   uint64_t target_free_space_ = kTargetFreeSpaceAfterCleanup;
 
   // Cleanup times.

commit d7d995c7009f4b5f4e47586391eda5982e573374

cryptohome: Move call to FreeDiskSpaceDuringLogin
Move call to FreeDiskSpaceDuringLogin to userdataauth.cc to
simplify user_session.h/cc. UserDataAuth already has access
to DiskCleanup for the call.

Fixes: commit c3885d14a377 ("cryptohome: Add login cleanup helpers")

BUG=chromium:1284559, chromium:1231192
TEST=tast run <ip> cryptohome.AutomaticCleanupCritical

Change-Id: I13fd2e540ba0dd2e1ffcbd15f85eafe16f567cf3
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3461889
Tested-by: Victor-Gabriel Savu <vsavu@google.com>
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Daniil Lunev <dlunev@chromium.org>
Commit-Queue: Victor-Gabriel Savu <vsavu@google.com>

cryptohome/user_session.cc
@@ -43,14 +43,12 @@ UserSession::UserSession() {}
 UserSession::~UserSession() {}
 UserSession::UserSession(
     HomeDirs* homedirs,
-    DiskCleanup* disk_cleanup,
     KeysetManagement* keyset_management,
     UserOldestActivityTimestampManager* user_activity_timestamp_manager,
     Pkcs11TokenFactory* pkcs11_token_factory,
     const brillo::SecureBlob& salt,
     const scoped_refptr<Mount> mount)
     : homedirs_(homedirs),
-      disk_cleanup_(disk_cleanup),
       keyset_management_(keyset_management),
       user_activity_timestamp_manager_(user_activity_timestamp_manager),
       pkcs11_token_factory_(pkcs11_token_factory),
@@ -63,16 +61,12 @@ MountError UserSession::MountVault(
     const CryptohomeVault::Options& vault_options) {
   MountError error = MOUNT_ERROR_NONE;
 
-  obfuscated_username_ = SanitizeUserName(username);
-
-  // Make sure user has enough space to log in.
-  disk_cleanup_->FreeDiskSpaceDuringLogin(obfuscated_username_);
-
   error = mount_->MountCryptohome(username, fs_keyset, vault_options);
   if (error != MOUNT_ERROR_NONE) {
     return error;
   }
 
+  obfuscated_username_ = SanitizeUserName(username);
   user_activity_timestamp_manager_->UpdateTimestamp(obfuscated_username_,
                                                     base::TimeDelta());
   pkcs11_token_ = pkcs11_token_factory_->New(

cryptohome/user_session.h
@@ -12,7 +12,6 @@
 #include <base/timer/timer.h>
 #include <brillo/secure_blob.h>
 
-#include "cleanup/disk_cleanup.h"
 #include "cryptohome/auth_session.h"
 #include "cryptohome/cleanup/user_oldest_activity_timestamp_manager.h"
 #include "cryptohome/credential_verifier.h"
@@ -33,7 +32,6 @@ class UserSession : public base::RefCountedThreadSafe<UserSession> {
   UserSession();
   UserSession(
       HomeDirs* homedirs,
-      DiskCleanup* disk_cleanup,
       KeysetManagement* keyset_management,
       UserOldestActivityTimestampManager* user_activity_timestamp_manager,
       Pkcs11TokenFactory* pkcs11_token_factory,
@@ -137,7 +135,6 @@ class UserSession : public base::RefCountedThreadSafe<UserSession> {
   void ClearHibernateSecret();
 
   HomeDirs* homedirs_;
-  DiskCleanup* disk_cleanup_;
   KeysetManagement* keyset_management_;
   UserOldestActivityTimestampManager* user_activity_timestamp_manager_;
   Pkcs11TokenFactory* pkcs11_token_factory_;

cryptohome/userdataauth.cc
@@ -1232,10 +1232,9 @@ scoped_refptr<UserSession> UserDataAuth::GetOrCreateUserSession(
     if (!m) {
       return nullptr;
     }
-    sessions_[username] =
-        new UserSession(homedirs_, low_disk_space_handler_->disk_cleanup(),
-                        keyset_management_, user_activity_timestamp_manager_,
-                        pkcs11_token_factory_, system_salt_, m);
+    sessions_[username] = new UserSession(
+        homedirs_, keyset_management_, user_activity_timestamp_manager_,
+        pkcs11_token_factory_, system_salt_, m);
   }
   return sessions_[username];
 }
@@ -1996,6 +1995,8 @@ MountError UserDataAuth::AttemptUserMount(
     return MOUNT_ERROR_FATAL;
   }
 
+  low_disk_space_handler_->disk_cleanup()->FreeDiskSpaceDuringLogin(
+      obfuscated_username);
   error = user_session->MountVault(credentials.username(),
                                    FileSystemKeyset(*vk.get()),
                                    MountArgsToVaultOptions(mount_args));

cryptohome/user_session_unittest.cc
@@ -17,7 +17,6 @@
 #include <policy/libpolicy.h>
 #include <policy/mock_device_policy.h>
 
-#include "cryptohome/cleanup/mock_disk_cleanup.h"
 #include "cryptohome/credentials.h"
 #include "cryptohome/crypto.h"
 #include "cryptohome/crypto/hmac.h"
@@ -41,7 +40,6 @@ using ::testing::Invoke;
 using ::testing::NiceMock;
 using ::testing::Return;
 using ::testing::SetArgPointee;
-using ::testing::StrictMock;
 
 namespace cryptohome {
 
@@ -98,8 +96,7 @@ class UserSessionTest : public ::testing::Test {
           return std::make_unique<FakePkcs11Token>();
         }));
 
-    session_ = new UserSession(homedirs_.get(), &disk_cleanup_,
-                               keyset_management_.get(),
+    session_ = new UserSession(homedirs_.get(), keyset_management_.get(),
                                user_activity_timestamp_manager_.get(),
                                &pkcs11_token_factory_, system_salt_, mount_);
   }
@@ -121,7 +118,6 @@ class UserSessionTest : public ::testing::Test {
   // Information about users' homedirs. The order of users is equal to kUsers.
   std::vector<UserInfo> users_;
   NiceMock<MockPlatform> platform_;
-  StrictMock<MockDiskCleanup> disk_cleanup_;
   NiceMock<MockPkcs11TokenFactory> pkcs11_token_factory_;
   Crypto crypto_;
   brillo::SecureBlob system_salt_;
@@ -192,7 +188,6 @@ TEST_F(UserSessionTest, MountVaultOk) {
   std::unique_ptr<VaultKeyset> vk = keyset_management_->GetValidKeyset(
       users_[0].credentials, nullptr /*error*/);
   FileSystemKeyset fs_keyset(*vk.get());
-  EXPECT_CALL(disk_cleanup_, FreeDiskSpaceDuringLogin(users_[0].obfuscated));
   EXPECT_CALL(*mount_,
               MountCryptohome(users_[0].name, _, VaultOptionsEqual(options)))
       .WillOnce(Return(MOUNT_ERROR_NONE));
@@ -229,7 +224,6 @@ TEST_F(UserSessionTest, MountVaultOk) {
   // Set the credentials with |users_[0].credentials| so that
   // |obfuscated_username_| is explicitly set during the Unmount test.
   session_->SetCredentials(users_[0].credentials);
-  EXPECT_CALL(disk_cleanup_, FreeDiskSpaceDuringLogin(users_[0].obfuscated));
   EXPECT_CALL(*mount_,
               MountCryptohome(users_[0].name, _, VaultOptionsEqual(options)))
       .WillOnce(Return(MOUNT_ERROR_NONE));
@@ -351,7 +345,6 @@ TEST_F(UserSessionTest, WebAuthnAndHibernateSecretReadTwice) {
   EXPECT_NE(vk, nullptr);
 
   FileSystemKeyset fs_keyset(*vk.get());
-  EXPECT_CALL(disk_cleanup_, FreeDiskSpaceDuringLogin(users_[0].obfuscated));
   EXPECT_CALL(*mount_,
               MountCryptohome(users_[0].name, _, VaultOptionsEqual(options)))
       .WillOnce(Return(MOUNT_ERROR_NONE));
@@ -402,7 +395,6 @@ TEST_F(UserSessionTest, WebAuthnSecretTimeout) {
       .force_type = EncryptedContainerType::kEcryptfs,
   };
 
-  EXPECT_CALL(disk_cleanup_, FreeDiskSpaceDuringLogin(users_[0].obfuscated));
   EXPECT_CALL(*mount_,
               MountCryptohome(users_[0].name, _, VaultOptionsEqual(options)))
       .WillOnce(Return(MOUNT_ERROR_NONE));
@@ -452,8 +444,8 @@ class UserSessionReAuthTest : public ::testing::Test {
 
 TEST_F(UserSessionReAuthTest, VerifyUser) {
   Credentials credentials("username", SecureBlob("password"));
-  scoped_refptr<UserSession> session = new UserSession(
-      nullptr, nullptr, nullptr, nullptr, nullptr, salt, nullptr);
+  scoped_refptr<UserSession> session =
+      new UserSession(nullptr, nullptr, nullptr, nullptr, salt, nullptr);
   EXPECT_TRUE(session->SetCredentials(credentials));
 
   EXPECT_TRUE(session->VerifyUser(credentials.GetObfuscatedUsername(salt)));
@@ -465,8 +457,8 @@ TEST_F(UserSessionReAuthTest, VerifyCredentials) {
   Credentials credentials_2("username", SecureBlob("password2"));
   Credentials credentials_3("username2", SecureBlob("password2"));
 
-  scoped_refptr<UserSession> session = new UserSession(
-      nullptr, nullptr, nullptr, nullptr, nullptr, salt, nullptr);
+  scoped_refptr<UserSession> session =
+      new UserSession(nullptr, nullptr, nullptr, nullptr, salt, nullptr);
   EXPECT_TRUE(session->SetCredentials(credentials_1));
   EXPECT_TRUE(session->VerifyCredentials(credentials_1));
   EXPECT_FALSE(session->VerifyCredentials(credentials_2));

cryptohome/userdataauth_auth_session_unittest.cc
@@ -15,8 +15,6 @@
 
 #include "cryptohome/auth_factor/auth_factor_manager.h"
 #include "cryptohome/auth_session_manager.h"
-#include "cryptohome/cleanup/mock_disk_cleanup.h"
-#include "cryptohome/cleanup/mock_low_disk_space_handler.h"
 #include "cryptohome/cleanup/mock_user_oldest_activity_timestamp_manager.h"
 #include "cryptohome/credentials.h"
 #include "cryptohome/crypto.h"
@@ -41,7 +39,6 @@ using ::testing::Eq;
 using ::testing::NiceMock;
 using ::testing::Return;
 using ::testing::SetArgPointee;
-using ::testing::StrictMock;
 
 using base::test::TaskEnvironment;
 using brillo::cryptohome::home::kGuestUserName;
@@ -85,9 +82,6 @@ class AuthSessionInterfaceTest : public ::testing::Test {
 
     userdataauth_.set_platform(&platform_);
     userdataauth_.set_homedirs(&homedirs_);
-    userdataauth_.set_low_disk_space_handler(&low_disk_space_handler_);
-    ON_CALL(low_disk_space_handler_, disk_cleanup())
-        .WillByDefault(Return(&disk_cleanup_));
     userdataauth_.set_mount_factory(&mount_factory_);
     userdataauth_.set_keyset_management(&keyset_management_);
     userdataauth_.set_auth_factor_manager_for_testing(&auth_factor_manager_);
@@ -114,8 +108,6 @@ class AuthSessionInterfaceTest : public ::testing::Test {
   NiceMock<MockPlatform> platform_;
   Crypto crypto_;
   NiceMock<MockHomeDirs> homedirs_;
-  NiceMock<MockLowDiskSpaceHandler> low_disk_space_handler_;
-  StrictMock<MockDiskCleanup> disk_cleanup_;
   NiceMock<MockMountFactory> mount_factory_;
   AuthFactorManager auth_factor_manager_{&platform_};
   UserSecretStashStorage user_secret_stash_storage_{&platform_};
@@ -273,8 +265,6 @@ TEST_F(AuthSessionInterfaceTest, PrepareEphemeralVault) {
   EXPECT_CALL(homedirs_, Exists(SanitizeUserName(kUsername3)))
       .WillRepeatedly(Return(true));
   ExpectAuth(kUsername3, brillo::SecureBlob(kPassword3));
-  EXPECT_CALL(disk_cleanup_,
-              FreeDiskSpaceDuringLogin(SanitizeUserName(kUsername3)));
 
   AuthSession* auth_session3 =
       auth_session_manager_->CreateAuthSession(kUsername3, 0);
@@ -309,8 +299,6 @@ TEST_F(AuthSessionInterfaceTest, PreparePersistentVault) {
   EXPECT_CALL(*mount, MountCryptohome(kUsername, _, _))
       .WillOnce(Return(MOUNT_ERROR_NONE));
   ExpectAuth(kUsername, brillo::SecureBlob(kPassword));
-  EXPECT_CALL(disk_cleanup_,
-              FreeDiskSpaceDuringLogin(SanitizeUserName(kUsername)));
 
   ASSERT_THAT(auth_session->Authenticate(CreateAuthorization(kPassword)),
               Eq(MOUNT_ERROR_NONE));
@@ -364,8 +352,6 @@ TEST_F(AuthSessionInterfaceTest, PreparePersistentVault) {
   EXPECT_CALL(homedirs_, Exists(SanitizeUserName(kUsername3)))
       .WillRepeatedly(Return(true));
   ExpectAuth(kUsername3, brillo::SecureBlob(kPassword3));
-  EXPECT_CALL(disk_cleanup_,
-              FreeDiskSpaceDuringLogin(SanitizeUserName(kUsername3)));
 
   AuthSession* auth_session3 =
       auth_session_manager_->CreateAuthSession(kUsername3, 0);

cryptohome/userdataauth_unittest.cc
@@ -197,7 +197,7 @@ class UserDataAuthTestBase : public ::testing::Test {
     brillo::SecureBlob salt;
     AssignSalt(&salt);
     mount_ = new NiceMock<MockMount>();
-    session_ = new UserSession(&homedirs_, &disk_cleanup_, &keyset_management_,
+    session_ = new UserSession(&homedirs_, &keyset_management_,
                                &user_activity_timestamp_manager_,
                                &pkcs11_token_factory_, salt, mount_);
     userdataauth_->set_session_for_user(username, session_.get());
@@ -353,7 +353,6 @@ class UserDataAuthTestTasked : public UserDataAuthTestBase {
 
   void CreatePkcs11TokenInSession(scoped_refptr<NiceMock<MockMount>> mount,
                                   scoped_refptr<UserSession> session) {
-    EXPECT_CALL(disk_cleanup_, FreeDiskSpaceDuringLogin(_));
     EXPECT_CALL(*mount, MountCryptohome(_, _, _))
         .WillOnce(Return(MOUNT_ERROR_NONE));
 
@@ -830,17 +829,15 @@ TEST_F(UserDataAuthTest, Unmount_AllDespiteFailures) {
   constexpr char kUsername2[] = "bar@gmail.com";
 
   scoped_refptr<NiceMock<MockMount>> mount1 = new NiceMock<MockMount>();
-  scoped_refptr<UserSession> session1 =
-      new UserSession(&homedirs_, &disk_cleanup_, &keyset_management_,
-                      &user_activity_timestamp_manager_, &pkcs11_token_factory_,
-                      brillo::SecureBlob(), mount1);
+  scoped_refptr<UserSession> session1 = new UserSession(
+      &homedirs_, &keyset_management_, &user_activity_timestamp_manager_,
+      &pkcs11_token_factory_, brillo::SecureBlob(), mount1);
   userdataauth_->set_session_for_user(kUsername1, session1.get());
 
   scoped_refptr<NiceMock<MockMount>> mount2 = new NiceMock<MockMount>();
-  scoped_refptr<UserSession> session2 =
-      new UserSession(&homedirs_, &disk_cleanup_, &keyset_management_,
-                      &user_activity_timestamp_manager_, &pkcs11_token_factory_,
-                      brillo::SecureBlob(), mount2);
+  scoped_refptr<UserSession> session2 = new UserSession(
+      &homedirs_, &keyset_management_, &user_activity_timestamp_manager_,
+      &pkcs11_token_factory_, brillo::SecureBlob(), mount2);
   userdataauth_->set_session_for_user(kUsername2, session2.get());
 
   InSequence sequence;
@@ -1030,15 +1027,15 @@ TEST_F(UserDataAuthTest, Pkcs11IsTpmTokenReady) {
 
   scoped_refptr<NiceMock<MockMount>> mount1 = new NiceMock<MockMount>();
   scoped_refptr<UserSession> session1 = new UserSession(
-      &homedirs_, &disk_cleanup_, &keyset_management_,
-      &user_activity_timestamp_manager_, &pkcs11_token_factory_, salt, mount1);
+      &homedirs_, &keyset_management_, &user_activity_timestamp_manager_,
+      &pkcs11_token_factory_, salt, mount1);
   userdataauth_->set_session_for_user(kUsername1, session1.get());
   CreatePkcs11TokenInSession(mount1, session1);
 
   scoped_refptr<NiceMock<MockMount>> mount2 = new NiceMock<MockMount>();
   scoped_refptr<UserSession> session2 = new UserSession(
-      &homedirs_, &disk_cleanup_, &keyset_management_,
-      &user_activity_timestamp_manager_, &pkcs11_token_factory_, salt, mount2);
+      &homedirs_, &keyset_management_, &user_activity_timestamp_manager_,
+      &pkcs11_token_factory_, salt, mount2);
   userdataauth_->set_session_for_user(kUsername2, session2.get());
   CreatePkcs11TokenInSession(mount2, session2);
 