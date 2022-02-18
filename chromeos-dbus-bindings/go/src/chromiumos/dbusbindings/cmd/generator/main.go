cf53f13302cb3eef9236b4cabfa4b8bb39a810c6
hscham <hscham@chromium.org>
2022-02-18 14:14:31 +0900

    chromeos-dbus-bindings: initialize go directory
    BUG=b:220636021, chromium:983008
    TEST=go build chromiumos/dbusbindings/cmd/generator/ with \
         ${CROS}/src/platform2/chromeos-dbus-bindings/go in GOPATH
    
    Change-Id: Idf276580cef995e201e12b17c562d53663dc41b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3474311
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Grace Cham <hscham@chromium.org>
    Commit-Queue: Grace Cham <hscham@chromium.org>

---
+++ chromeos-dbus-bindings/go/src/chromiumos/dbusbindings/cmd/generator/main.go
@@ -0,0 +1,15 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package main implements the generate-chromeos-dbus-bindings used to generate
+// dbus bindings
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("this is generate-chromeos-dbus-bindings in go!")
+}
