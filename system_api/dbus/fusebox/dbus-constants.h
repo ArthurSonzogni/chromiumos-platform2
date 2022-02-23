4c3017667f2f52223dcffa12ada4e7bfb8505150
Nigel Tao <nigeltao@chromium.org>
2022-02-23 15:06:37 +1100

    system_api: remove deprecated FuseBox names
    Also add kWriteMethod.
    
    BUG=chromium:1244007
    TEST=emerge-zork chromeos-base/system_api
    
    Change-Id: I55dd149e338c636140b0eb74d36ae49fcabba29f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3483080
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Nigel Tao <nigeltao@chromium.org>
    Auto-Submit: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>

--- system_api/dbus/fusebox/dbus-constants.h
+++ system_api/dbus/fusebox/dbus-constants.h
@@ -15,22 +15,13 @@ const char kFuseBoxServicePath[] = "/org/chromium/FuseBoxService";
 // FuseBoxService methods.
 const char kCloseMethod[] = "Close";
 const char kFlushMethod[] = "Flush";
-const char kFuseBoxOperationMethod[] = "FuseBoxOperation";
 const char kOpenMethod[] = "Open";
 const char kReadDirMethod[] = "ReadDir";
 const char kReadMethod[] = "Read";
 const char kStatMethod[] = "Stat";
+const char kWriteMethod[] = "Write";
 
-// FuseBoxClient interface/name/path (chromeos)
-//
-// Deprecated: replace "Client" by "ReverseService".
-//
-// TODO(crbug.com/1244007): delete this group when nothing refers to them.
-const char kFuseBoxClientInterface[] = "org.chromium.FuseBoxClient";
-const char kFuseBoxClientName[] = "org.chromium.FuseBoxClient";
-const char kFuseBoxClientPath[] = "/org/chromium/FuseBoxClient";
-
-// FuseBoxReverseService interface/name/path (chromeos)
+// FuseBoxReverseService interface/name/path (chromeos /usr/bin/fusebox daemon)
 const char kFuseBoxReverseServiceInterface[] =
     "org.chromium.FuseBoxReverseService";
 const char kFuseBoxReverseServiceName[] = "org.chromium.FuseBoxReverseService";
@@ -39,18 +30,7 @@ const char kFuseBoxReverseServicePath[] = "/org/chromium/FuseBoxReverseService";
 // FuseBoxReverseService methods.
 const char kAttachStorageMethod[] = "AttachStorage";
 const char kDetachStorageMethod[] = "DetachStorage";
-const char kReadDirResponseMethod[] = "ReadDirResponse";  // Deprecated.
 const char kReplyToReadDirMethod[] = "ReplyToReadDir";
-
-// Deprecated: use the group above's variables instead. They have the same
-// values, but the group above have consistent kFooBarMethod variable names.
-// The "FuseBox" (note that it's "FuseBox" not "FuseBoxClient") parts of the
-// variable names are unnecessary. We are already in "namespace fusebox".
-//
-// TODO(crbug.com/1244007): delete this group when nothing refers to them.
-const char kFuseBoxAttachStorageMethod[] = "AttachStorage";
-const char kFuseBoxDetachStorageMethod[] = "DetachStorage";
-const char kFuseBoxReadDirResponseMethod[] = "ReadDirResponse";
 
 }  // namespace fusebox
 
