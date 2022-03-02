82847eb55db3be020caab6b611c5db0c449756c8
Ian Barkley-Yeung <iby@chromium.org>
2022-03-01 16:57:06 -0800

    crash: Fix chrome_collector_fuzzer
    Fixed issue where the user hash would start with a /, causing FilePath
    to CHECK fail because it looked like an absolute path.
    
    Also fixed issue where the non_exe_error_key branch could never be taken
    because the fuzzer logic would return 0 too early.
    
    Also added some ability to fuzz some parts like ProcessDriErrorState
    which weren't reachable before, and otherwise cleaned up.
    
    BUG=b:220329902
    Fixed: b:220329902
    TEST=Ran fuzzer for over an hour without it aborting.
    
    Change-Id: I9246f9974e79e6f414fd956631168b2795e5a1a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3498662
    Tested-by: Ian Barkley-Yeung <iby@chromium.org>
    Auto-Submit: Ian Barkley-Yeung <iby@chromium.org>
    Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Commit-Queue: Miriam Zimmerman <mutexlox@chromium.org>

--- crash-reporter/chrome_collector_fuzzer.cc
+++ crash-reporter/chrome_collector_fuzzer.cc
@@ -4,17 +4,23 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <limits>
 #include <memory>
 #include <string>
 #include <utility>
 
 #include <base/check.h>
+#include <base/command_line.h>
 #include <base/files/file.h>
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
 #include <base/files/scoped_temp_dir.h>
 #include <base/logging.h>
+#include <base/test/task_environment.h>
+#include <base/test/test_timeouts.h>
+#include <debugd/dbus-proxy-mocks.h>
 #include <fuzzer/FuzzedDataProvider.h>
+#include <gmock/gmock.h>
 #include <session_manager/dbus-proxy-mocks.h>
 
 #include "crash-reporter/chrome_collector.h"
@@ -23,11 +29,22 @@
 
 namespace {
 
+using ::testing::_;
+using ::testing::WithArgs;
+
 class Environment {
  public:
   Environment() {
     // Disable logging per instructions.
     logging::SetMinLogLevel(logging::LOGGING_FATAL);
+
+    // Needed for TestTimeouts::Initialize(). We don't have access to the real
+    // command-line, and even if we did, it's not clear we would want to use it.
+    const char* const kFakeCommandLine[] = {"chrome_collector_fuzzer"};
+    base::CommandLine::Init(std::size(kFakeCommandLine), kFakeCommandLine);
+
+    // Needed for SingleThreadTaskEnvironment.
+    TestTimeouts::Initialize();
   }
 };
 
@@ -35,24 +52,58 @@ class ChromeCollectorForFuzzing : public ChromeCollector {
  public:
   explicit ChromeCollectorForFuzzing(CrashSendingMode crash_sending_mode,
                                      std::string user_name,
-                                     std::string user_hash)
+                                     std::string user_hash,
+                                     std::string dri_error_state,
+                                     std::string dmesg_result)
       : ChromeCollector(crash_sending_mode),
         user_name_(std::move(user_name)),
-        user_hash_(std::move(user_hash)) {}
+        user_hash_(std::move(user_hash)),
+        dri_error_state_(std::move(dri_error_state)),
+        dmesg_result_(std::move(dmesg_result)) {}
 
   void SetUpDBus() override {
     // Mock out all DBus calls so (a) we don't actually call DBus and (b) we
     // don't CHECK fail when the DBus calls fail.
-    auto mock =
+    auto session_manager_mock =
         std::make_unique<org::chromium::SessionManagerInterfaceProxyMock>();
-    test_util::SetActiveSessions(mock.get(), {{user_name_, user_hash_}});
-    session_manager_proxy_ = std::move(mock);
+    test_util::SetActiveSessions(session_manager_mock.get(),
+                                 {{user_name_, user_hash_}});
+    session_manager_proxy_ = std::move(session_manager_mock);
+
+    auto proxy_mock = std::make_unique<org::chromium::debugdProxyMock>();
+    std::function<void(base::OnceCallback<void(const std::string&)> &&)>
+        handler_dri_error_state =
+            [this](base::OnceCallback<void(const std::string&)> callback) {
+              task_environment_.GetMainThreadTaskRunner()->PostNonNestableTask(
+                  FROM_HERE,
+                  base::BindOnce(std::move(callback), dri_error_state_));
+            };
+    ON_CALL(*proxy_mock, GetLogAsync("i915_error_state", _, _, _))
+        .WillByDefault(WithArgs<1>(handler_dri_error_state));
+
+    std::function<void(base::OnceCallback<void(const std::string&)> &&)>
+        handler_dmesg =
+            [this](base::OnceCallback<void(const std::string&)> callback) {
+              task_environment_.GetMainThreadTaskRunner()->PostNonNestableTask(
+                  FROM_HERE,
+                  base::BindOnce(std::move(callback), dmesg_result_));
+            };
+    ON_CALL(*proxy_mock, CallDmesgAsync(_, _, _, _))
+        .WillByDefault(WithArgs<1>(handler_dmesg));
+
+    debugd_proxy_ = std::move(proxy_mock);
   }
 
  private:
+  // RunLoop requires a task environment.
+  base::test::SingleThreadTaskEnvironment task_environment_;
   // Results from the fake RetrieveActiveSessions call
   const std::string user_name_;
   const std::string user_hash_;
+  // Results for the fake GetLogAsync call
+  const std::string dri_error_state_;
+  // Results for the fake CallDmesgAsync call
+  const std::string dmesg_result_;
 };
 
 }  // namespace
@@ -67,25 +118,49 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   paths::SetPrefixForTesting(test_dir);
 
   FuzzedDataProvider provider(data, size);
-  const int kArbitraryMaxNameLength = 4096;
+  // Force daemon store on or off, since fuzzers should not have
+  // non-deterministic behavior.
+  bool use_daemon_store = provider.ConsumeBool();
+
   // Exactly one of exe_name and non_exe_error_key can be set or we CHECK fail.
   std::string exe_name;
   std::string non_exe_error_key;
   if (provider.ConsumeBool()) {
-    exe_name = provider.ConsumeRandomLengthString(kArbitraryMaxNameLength);
+    exe_name = provider.ConsumeRandomLengthString();
+    if (exe_name.empty()) {
+      return 0;  // Or we'll CHECK-fail. Fuzzers shouldn't exit on any input.
+    }
   } else {
-    non_exe_error_key =
-        provider.ConsumeRandomLengthString(kArbitraryMaxNameLength);
+    non_exe_error_key = provider.ConsumeRandomLengthString();
+    if (non_exe_error_key.empty()) {
+      return 0;  // Or we'll CHECK-fail. Fuzzers shouldn't exit on any input.
+    }
   }
-  pid_t pid = provider.ConsumeIntegral<pid_t>();
-  uid_t uid = provider.ConsumeIntegral<uid_t>();
-  if (exe_name.empty() || pid < (pid_t)0 || uid < (uid_t)0) {
-    return 0;  // Or we'll CHECK-fail. Fuzzers shouldn't exit on any input.
+
+  // pid and uid must be >= 0 Or we'll CHECK-fail. Fuzzers shouldn't exit on any
+  // input.
+  pid_t pid = provider.ConsumeIntegralInRange(
+      (pid_t)0, std::numeric_limits<pid_t>::max());
+  uid_t uid = provider.ConsumeIntegralInRange(
+      (uid_t)0, std::numeric_limits<uid_t>::max());
+  std::string user_name = provider.ConsumeRandomLengthString();
+  std::string user_hash = provider.ConsumeRandomLengthString();
+  // If the user_hash looks like an absolute directory path,
+  // GetDaemonStoreCrashDirectories will CHECK fail when calling
+  // base::FilePath::Append().
+  base::FilePath user_hash_path(user_hash);
+  if (user_hash_path.IsAbsolute()) {
+    return 0;
   }
-  std::string user_name =
-      provider.ConsumeRandomLengthString(kArbitraryMaxNameLength);
-  std::string user_hash =
-      provider.ConsumeRandomLengthString(kArbitraryMaxNameLength);
+
+  // If the user_hash_path uses "..", the fuzzer can "escape" from the temp
+  // directory and overwrite random files.
+  if (user_hash_path.ReferencesParent()) {
+    return 0;
+  }
+
+  std::string dri_error_state = provider.ConsumeRandomLengthString();
+  std::string dmesg_result = provider.ConsumeRandomLengthString();
 
   // Despite the Memfd in the name of HandleCrashThroughMemfd, we can pass a
   // file handle to a normal file. memfd isn't supported by QEMU so better to
@@ -104,10 +179,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
   // kNormalCrashSendMode -- This makes it much simpler to mock out the DBus
   // calls, and we're not fuzzing the crash loop logic.
-  ChromeCollectorForFuzzing collector(CrashCollector::kNormalCrashSendMode,
-                                      std::move(user_name),
-                                      std::move(user_hash));
+  ChromeCollectorForFuzzing collector(
+      CrashCollector::kNormalCrashSendMode, std::move(user_name),
+      std::move(user_hash), std::move(dri_error_state),
+      std::move(dmesg_result));
   collector.Initialize(false);
+  collector.force_daemon_store_for_testing(use_daemon_store);
   collector.HandleCrashThroughMemfd(test_input.TakePlatformFile(), pid, uid,
                                     exe_name, non_exe_error_key, kEmptyDumpDir);
   return 0;
