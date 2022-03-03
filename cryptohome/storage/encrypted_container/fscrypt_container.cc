ArthurSonzogni/chromiumos-platform2

commit 462408aacae0f7be455bb921c7ccd899136dbfda

cryptohome: add keyring abstraction
BUG=b:172344610
TEST=FEATURES="test" USE="test lvm_stateful_partition" emerge-hatch cryptohome

Change-Id: I774d9534842159a457860b3995240f5f4df2129b
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3397880
Reviewed-by: Sarthak Kukreti <sarthakkukreti@chromium.org>
Tested-by: Daniil Lunev <dlunev@chromium.org>
Commit-Queue: Daniil Lunev <dlunev@chromium.org>

cryptohome/storage/encrypted_container/fscrypt_container.cc
@@ -9,17 +9,20 @@
 
 #include "cryptohome/platform.h"
 #include "cryptohome/storage/encrypted_container/filesystem_key.h"
+#include "cryptohome/storage/keyring/keyring.h"
 
 namespace cryptohome {
 
 FscryptContainer::FscryptContainer(const base::FilePath& backing_dir,
                                    const FileSystemKeyReference& key_reference,
                                    bool allow_v2,
-                                   Platform* platform)
+                                   Platform* platform,
+                                   Keyring* keyring)
     : backing_dir_(backing_dir),
-      key_reference_({.reference = key_reference.fek_sig}),
+      key_reference_(key_reference),
       allow_v2_(allow_v2),
-      platform_(platform) {}
+      platform_(platform),
+      keyring_(keyring) {}
 
 bool FscryptContainer::Purge() {
   return platform_->DeletePathRecursively(backing_dir_);
@@ -39,25 +42,20 @@ bool FscryptContainer::Setup(const FileSystemKey& encryption_key) {
     }
   }
 
-  key_reference_.policy_version =
-      platform_->GetDirectoryPolicyVersion(backing_dir_);
-
-  if (key_reference_.policy_version < 0) {
-    key_reference_.policy_version =
-        (allow_v2_ && platform_->CheckFscryptKeyIoctlSupport())
-            ? FSCRYPT_POLICY_V2
-            : FSCRYPT_POLICY_V1;
-  }
-
-  if (!platform_->AddDirCryptoKeyToKeyring(encryption_key.fek,
-                                           &key_reference_)) {
+  auto key_type = UseV2() ? Keyring::KeyType::kFscryptV2Key
+                          : Keyring::KeyType::kFscryptV1Key;
+  if (!keyring_->AddKey(key_type, encryption_key, &key_reference_)) {
     LOG(ERROR) << "Failed to add fscrypt key to kernel";
     return false;
   }
 
   // `SetDirectoryKey` is a set-or-verify function: for directories with the
   // encryption policy already set, this function call acts as a verifier.
-  if (!platform_->SetDirCryptoKey(backing_dir_, key_reference_)) {
+  dircrypto::KeyReference ref = {
+      .policy_version = UseV2() ? FSCRYPT_POLICY_V2 : FSCRYPT_POLICY_V1,
+      .reference = key_reference_.fek_sig,
+  };
+  if (!platform_->SetDirCryptoKey(backing_dir_, ref)) {
     LOG(ERROR) << "Failed to set fscrypt key for backing directory";
     return false;
   }
@@ -71,11 +69,19 @@ bool FscryptContainer::SetLazyTeardownWhenUnused() {
 }
 
 bool FscryptContainer::Teardown() {
-  return platform_->InvalidateDirCryptoKey(key_reference_, backing_dir_);
+  auto key_type = UseV2() ? Keyring::KeyType::kFscryptV2Key
+                          : Keyring::KeyType::kFscryptV1Key;
+  return keyring_->RemoveKey(key_type, key_reference_);
 }
 
 base::FilePath FscryptContainer::GetBackingLocation() const {
   return backing_dir_;
+}
+
+bool FscryptContainer::UseV2() {
+  return (platform_->GetDirectoryPolicyVersion(backing_dir_) ==
+          FSCRYPT_POLICY_V2) ||
+         (allow_v2_ && platform_->CheckFscryptKeyIoctlSupport());
 }
 
 }  // namespace cryptohome

cryptohome/encrypted_reboot_vault/encrypted_reboot_vault.cc
@@ -8,6 +8,7 @@
 #include <cryptohome/platform.h>
 #include <cryptohome/storage/encrypted_container/filesystem_key.h>
 #include <cryptohome/storage/encrypted_container/fscrypt_container.h>
+#include <cryptohome/storage/keyring/real_keyring.h>
 
 #include <base/files/file_enumerator.h>
 #include <base/files/file_path.h>
@@ -88,14 +89,16 @@ cryptohome::FileSystemKey RetrieveKey() {
 }  // namespace
 
 EncryptedRebootVault::EncryptedRebootVault()
-    : vault_path_(base::FilePath(kEncryptedRebootVaultPath)) {
+    : vault_path_(base::FilePath(kEncryptedRebootVaultPath)),
+      keyring_(std::make_unique<cryptohome::RealKeyring>()) {
   cryptohome::FileSystemKeyReference key_reference;
   key_reference.fek_sig = brillo::SecureBlob(kEncryptionKeyTag);
 
   // TODO(dlunev): change the allow_v2 to true once all the boards are on
   // 5.4+
   encrypted_container_ = std::make_unique<cryptohome::FscryptContainer>(
-      vault_path_, key_reference, /*allow_v2=*/false, &platform_);
+      vault_path_, key_reference, /*allow_v2=*/false, &platform_,
+      keyring_.get());
 }
 
 bool EncryptedRebootVault::CreateVault() {

cryptohome/encrypted_reboot_vault/encrypted_reboot_vault.h
@@ -14,6 +14,7 @@
 #include <cryptohome/dircrypto_util.h>
 #include <cryptohome/platform.h>
 #include <cryptohome/storage/encrypted_container/encrypted_container.h>
+#include <cryptohome/storage/keyring/keyring.h>
 
 class EncryptedRebootVault {
  public:
@@ -31,6 +32,7 @@ class EncryptedRebootVault {
  private:
   base::FilePath vault_path_;
   cryptohome::Platform platform_;
+  std::unique_ptr<cryptohome::Keyring> keyring_;
   std::unique_ptr<cryptohome::EncryptedContainer> encrypted_container_;
 };
 

cryptohome/libs/BUILD.gn
@@ -141,6 +141,8 @@ static_library("libcrosplatform") {
     "../storage/encrypted_container/fscrypt_container.cc",
     "../storage/encrypted_container/loopback_device.cc",
     "../storage/encrypted_container/ramdisk_device.cc",
+    "../storage/keyring/real_keyring.cc",
+    "../storage/keyring/utils.cc",
     "../storage/mount_constants.cc",
     "../user_secret_stash.cc",
     "../user_secret_stash_storage.cc",

cryptohome/mock_platform.h
@@ -370,20 +370,11 @@ class MockPlatform : public Platform {
               (const, override));
   MOCK_METHOD(bool, CheckFscryptKeyIoctlSupport, (), (const, override));
   MOCK_METHOD(bool,
-              AddDirCryptoKeyToKeyring,
-              (const brillo::SecureBlob& key, dircrypto::KeyReference*),
-              (override));
-  MOCK_METHOD(bool,
               InvalidateDirCryptoKey,
               (const dircrypto::KeyReference&, const base::FilePath&),
               (override));
   MOCK_METHOD(bool, ClearUserKeyring, (), (override));
-  MOCK_METHOD(bool,
-              AddEcryptfsAuthToken,
-              (const brillo::SecureBlob&,
-               const std::string&,
-               const brillo::SecureBlob&),
-              (override));
+  MOCK_METHOD(bool, IsDmcryptKeyringSupported, (), (const, override));
   MOCK_METHOD(bool, FirmwareWriteProtected, (), (override));
   MOCK_METHOD(std::string, GetHardwareID, (), (override));
   MOCK_METHOD(bool, GetBlkSize, (const base::FilePath&, uint64_t*), (override));

cryptohome/mount_encrypted/encrypted_fs_unittest.cc
@@ -22,6 +22,7 @@
 #include "cryptohome/storage/encrypted_container/encrypted_container.h"
 #include "cryptohome/storage/encrypted_container/fake_backing_device.h"
 #include "cryptohome/storage/encrypted_container/filesystem_key.h"
+#include "cryptohome/storage/keyring/fake_keyring.h"
 
 using ::testing::_;
 using ::testing::DoAll;
@@ -58,6 +59,7 @@ class EncryptedFsTest : public ::testing::Test {
 
     auto container = std::make_unique<cryptohome::DmcryptContainer>(
         config_, std::move(fake_backing_device), key_reference_, &platform_,
+        &keyring_,
         std::make_unique<brillo::DeviceMapper>(
             base::BindRepeating(&brillo::fake::CreateDevmapperTask)));
 
@@ -93,6 +95,7 @@ class EncryptedFsTest : public ::testing::Test {
   cryptohome::DmcryptConfig config_;
 
   NiceMock<cryptohome::MockPlatform> platform_;
+  cryptohome::FakeKeyring keyring_;
   brillo::DeviceMapper device_mapper_;
   cryptohome::FakeBackingDeviceFactory fake_backing_device_factory_;
   cryptohome::FileSystemKey key_;

cryptohome/platform.cc
@@ -62,6 +62,7 @@
 #include <base/system/sys_info.h>
 #include <base/threading/thread.h>
 #include <base/time/time.h>
+#include <brillo/blkdev_utils/device_mapper.h>
 #include <brillo/blkdev_utils/loop_device.h>
 #include <brillo/file_utils.h>
 #include <brillo/files/safe_fd.h>
@@ -1423,31 +1424,6 @@ bool Platform::SetupProcessKeyring() {
   return true;
 }
 
-// Encapsulate these helpers to avoid include conflicts.
-namespace ecryptfs {
-extern "C" {
-#include <ecryptfs.h>  // NOLINT(build/include_alpha)
-}
-
-long AddEcryptfsAuthToken(  // NOLINT(runtime/int)
-    const brillo::SecureBlob& key,
-    const std::string& key_sig,
-    const brillo::SecureBlob& salt) {
-  DCHECK_EQ(static_cast<size_t>(ECRYPTFS_MAX_KEY_BYTES), key.size());
-  DCHECK_EQ(static_cast<size_t>(ECRYPTFS_SIG_SIZE) * 2, key_sig.length());
-  DCHECK_EQ(static_cast<size_t>(ECRYPTFS_SALT_SIZE), salt.size());
-
-  struct ecryptfs_auth_tok auth_token;
-
-  generate_payload(&auth_token, const_cast<char*>(key_sig.c_str()),
-                   const_cast<char*>(salt.char_data()),
-                   const_cast<char*>(key.char_data()));
-
-  return ecryptfs_add_auth_tok_to_keyring(&auth_token,
-                                          const_cast<char*>(key_sig.c_str()));
-}
-}  // namespace ecryptfs
-
 dircrypto::KeyState Platform::GetDirCryptoKeyState(const FilePath& dir) {
   return dircrypto::GetDirectoryKeyState(dir);
 }
@@ -1461,18 +1437,13 @@ int Platform::GetDirectoryPolicyVersion(const base::FilePath& dir) const {
   return dircrypto::GetDirectoryPolicyVersion(dir);
 }
 
-bool Platform::CheckFscryptKeyIoctlSupport() const {
-  return dircrypto::CheckFscryptKeyIoctlSupport();
-}
-
-bool Platform::AddDirCryptoKeyToKeyring(
-    const brillo::SecureBlob& key, dircrypto::KeyReference* key_reference) {
-  return dircrypto::AddDirectoryKey(key, key_reference);
-}
-
 bool Platform::InvalidateDirCryptoKey(
     const dircrypto::KeyReference& key_reference, const FilePath& shadow_root) {
   return dircrypto::RemoveDirectoryKey(key_reference, shadow_root);
+}
+
+bool Platform::CheckFscryptKeyIoctlSupport() const {
+  return dircrypto::CheckFscryptKeyIoctlSupport();
 }
 
 bool Platform::ClearUserKeyring() {
@@ -1480,10 +1451,14 @@ bool Platform::ClearUserKeyring() {
   return (keyctl(KEYCTL_CLEAR, KEY_SPEC_USER_KEYRING) == 0);
 }
 
-bool Platform::AddEcryptfsAuthToken(const brillo::SecureBlob& key,
-                                    const std::string& key_sig,
-                                    const brillo::SecureBlob& salt) {
-  return (ecryptfs::AddEcryptfsAuthToken(key, key_sig, salt) >= 0);
+bool Platform::IsDmcryptKeyringSupported() const {
+  const brillo::DeviceMapperVersion reference_version({1, 15, 0});
+
+  if (brillo::DeviceMapper().GetTargetVersion("crypt") < reference_version) {
+    return false;
+  }
+
+  return true;
 }
 
 FileEnumerator* Platform::GetFileEnumerator(const FilePath& root_path,

cryptohome/platform.h
@@ -761,10 +761,6 @@ class Platform {
   virtual bool SetDirCryptoKey(const base::FilePath& dir,
                                const dircrypto::KeyReference& key_reference);
 
-  // Adds the key to the dircrypto keyring and sets permissions.
-  virtual bool AddDirCryptoKeyToKeyring(const brillo::SecureBlob& key,
-                                        dircrypto::KeyReference* key_reference);
-
   // Invalidates the key to make dircrypto data inaccessible.
   virtual bool InvalidateDirCryptoKey(
       const dircrypto::KeyReference& key_reference,
@@ -773,15 +769,9 @@ class Platform {
   // Clears the kernel-managed user keyring
   virtual bool ClearUserKeyring();
 
-  // Creates an ecryptfs auth token and installs it in the kernel keyring.
-  //
-  // Parameters
-  //   key - The key to add
-  //   key_sig - The key's (ascii) signature
-  //   salt - The salt
-  virtual bool AddEcryptfsAuthToken(const brillo::SecureBlob& key,
-                                    const std::string& key_sig,
-                                    const brillo::SecureBlob& salt);
+  // Check whether the dm-crypt driver version is greater than 1.15.0 and can
+  // support key provisioning via the kernel keyring.
+  virtual bool IsDmcryptKeyringSupported() const;
 
   // Override the location of the mountinfo file used.
   // Default is kMountInfoFile.

commit 219dfd6e2b04a3103fd495eb9576abeaa423ec1e

cryptohome: fix fscrypt v1/v2 detection
Prevent using v2 key on devices that support it, but where the
cryptohome has been created with v1 key.

BUG=b:221864887, b:172344610
TEST=Create v1 vault on older image and mount it on newer

Fixes: 462408aacae0 ("cryptohome: add keyring abstraction")
Change-Id: I1fdd6d6263dab8c884270426ea74743aff71be08
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3499534
Reviewed-by: Sarthak Kukreti <sarthakkukreti@chromium.org>
Reviewed-by: Ross Zwisler <zwisler@google.com>
Commit-Queue: Ross Zwisler <zwisler@google.com>
Tested-by: Ross Zwisler <zwisler@google.com>

cryptohome/storage/encrypted_container/fscrypt_container.cc
@@ -79,9 +79,14 @@ base::FilePath FscryptContainer::GetBackingLocation() const {
 }
 
 bool FscryptContainer::UseV2() {
-  return (platform_->GetDirectoryPolicyVersion(backing_dir_) ==
-          FSCRYPT_POLICY_V2) ||
-         (allow_v2_ && platform_->CheckFscryptKeyIoctlSupport());
+  auto existing_policy = platform_->GetDirectoryPolicyVersion(backing_dir_);
+  if (existing_policy == FSCRYPT_POLICY_V1) {
+    return false;
+  }
+  if (existing_policy == FSCRYPT_POLICY_V2) {
+    return true;
+  }
+  return (allow_v2_ && platform_->CheckFscryptKeyIoctlSupport());
 }
 
 }  // namespace cryptohome