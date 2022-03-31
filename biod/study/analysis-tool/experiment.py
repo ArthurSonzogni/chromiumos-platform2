052c93af34fcefed5270ba113589f44881c58047
Craig Hesling <hesling@chromium.org>
2022-03-30 18:51:31 -0700

    fpstudy/analysis-tool: Move Experiment and FPCBETResults to their own files
    BUG=b:323261182
    TEST=None
    
    Change-Id: I7fd6b6e650c9fd7904fd8427a75e06953a8a25d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5392712
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Craig Hesling <hesling@chromium.org>
    Tested-by: Craig Hesling <hesling@chromium.org>

---
+++ biod/study/analysis-tool/experiment.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+
+from enum import Enum
+
+import pandas as pd
+
+
+class Experiment:
+
+    class Finger(Enum):
+
+        Thumb_Left = 0
+        Thumb_Right = 1
+        Index_Left = 2
+        Index_Right = 3
+        Middle_Left = 4
+        Middle_Right = 5
+
+    class UserGroup(Enum):
+
+        A = 0
+        B = 1
+        C = 2
+        D = 3
+        E = 4
+        F = 5
+
+    class FalseTableCol(Enum):
+        Enroll_User = 'EnrollUser'
+        Enroll_Finger = 'EnrollFinger'
+        Verify_User = 'VerifyUser'
+        Verify_Finger = 'VerifyFinger'
+        Verify_Sample = 'VerifySample'
+
+        @classmethod
+        def all(cls) -> list:
+            return list(level for level in cls)
+
+        @classmethod
+        def all_values(cls) -> list:
+            return list(level.value for level in cls)
+
+    def _FalseTableQuery(false_table: pd.DataFrame,
+                         enroll_user_id: int = None,
+                         enroll_finger_id: int = None,
+                         verify_user_id: int = None,
+                         verify_finger_id: int = None,
+                         verify_sample_index: int = None) -> pd.DataFrame:
+        query_parts = []
+
+        for arg, col in [
+            (enroll_user_id, Experiment.FalseTableCol.Enroll_User),
+            (enroll_finger_id, Experiment.FalseTableCol.Enroll_Finger),
+            (verify_user_id, Experiment.FalseTableCol.Verify_User),
+            (verify_finger_id, Experiment.FalseTableCol.Verify_Finger),
+            (verify_sample_index, Experiment.FalseTableCol.Verify_Sample),
+        ]:
+            if arg:
+                query_parts.append(f'({col.value} == {arg})')
+
+        query_str = ' & '.join(query_parts)
+        # print('Query string:', query_str)
+
+        return false_table.query(query_str) if query_str else false_table
+
+    def _FalseTableQuery2(false_table: pd.DataFrame,
+                          enroll_user_id: int = None,
+                          enroll_finger_id: int = None,
+                          verify_user_id: int = None,
+                          verify_finger_id: int = None,
+                          verify_sample_index: int = None) -> pd.DataFrame:
+
+        query_cols = []
+        query_vals = ()
+        # query_dict = {}
+
+        for arg, col in [
+            (enroll_user_id, Experiment.FalseTableCol.Enroll_User),
+            (enroll_finger_id, Experiment.FalseTableCol.Enroll_Finger),
+            (verify_user_id, Experiment.FalseTableCol.Verify_User),
+            (verify_finger_id, Experiment.FalseTableCol.Verify_Finger),
+            (verify_sample_index, Experiment.FalseTableCol.Verify_Sample),
+        ]:
+            if arg:
+                # query_dict[col.value] = arg
+                query_cols.append(col.value)
+                query_vals += (arg,)
+
+        # if query_dict:
+            # return false_table.isin(query_dict)
+
+        if query_cols:
+            res = (false_table[query_cols] == query_vals)
+            return false_table.loc[res.all(axis=1)]
+
+        return false_table
+
+    def __init__(self,
+                 #  num_enrollment: int,
+                 num_verification: int,
+                 num_fingers: int,
+                 num_users: int,
+                 fa_list: pd.DataFrame):
+        # self.num_enrollment = num_enrollment
+        self.num_verification = num_verification
+        self.num_fingers = num_fingers
+        self.num_users = num_users
+        self.fa_list = fa_list
+
+    def FAList(self) -> pd.DataFrame:
+        return self.fa_list
+
+    def FAQuery(self,
+                enroll_user_id: int = None,
+                enroll_finger_id: int = None,
+                verify_user_id: int = None,
+                verify_finger_id: int = None,
+                verify_sample_index: int = None) -> pd.DataFrame:
+        return Experiment._FalseTableQuery(false_table=self.FAList(),
+                                           enroll_user_id=enroll_user_id,
+                                           enroll_finger_id=enroll_finger_id,
+                                           verify_user_id=verify_user_id,
+                                           verify_finger_id=verify_finger_id,
+                                           verify_sample_index=verify_sample_index)
+
+    def FAQuery2(self,
+                 enroll_user_id: int = None,
+                 enroll_finger_id: int = None,
+                 verify_user_id: int = None,
+                 verify_finger_id: int = None,
+                 verify_sample_index: int = None) -> pd.DataFrame:
+        return Experiment._FalseTableQuery2(false_table=self.FAList(),
+                                            enroll_user_id=enroll_user_id,
+                                            enroll_finger_id=enroll_finger_id,
+                                            verify_user_id=verify_user_id,
+                                            verify_finger_id=verify_finger_id,
+                                            verify_sample_index=verify_sample_index)
+
+    def FATestResult() -> pd.DataFrame:
+        pass
---
+++ biod/study/analysis-tool/fpc_bet_results.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+
+from enum import Enum
+from typing import List
+
+import pandas as pd
+
+from experiment import Experiment
+
+
+class FPCBETResults:
+
+    class TestCase(Enum):
+        '''Identify which experiment test case.'''
+
+        TUDisabled = 'TC-01'
+        TUSpecificSamples = 'TC-02-TU'
+        TUEnabled = 'TC-03-TU-Continuous'
+
+    class TableType(Enum):
+        '''Identify what type of experiment data is represented in a table.'''
+
+        FAR = 'FAR_stats_4levels.txt'
+        FRR = 'FRR_stats_4levels.txt'
+        FA_List = 'FalseAccepts.txt'
+        FAR_Decision = 'FAR_decision.csv'
+        FRR_Decision = 'FRR_decision.csv'
+
+    class SecLevel(Enum):
+        '''The order of these item are in increasing security level.'''
+
+        Target_20K = 'FPC_BIO_SECURITY_LEVEL_LOW'
+        Target_50K = 'FPC_BIO_SECURITY_LEVEL_STANDARD'
+        Target_100K = 'FPC_BIO_SECURITY_LEVEL_SECURE'
+        Target_500K = 'FPC_BIO_SECURITY_LEVEL_HIGH'
+
+        @classmethod
+        def all(cls) -> list:
+            return list(level for level in cls)
+
+        @classmethod
+        def all_values(cls) -> list:
+            return list(level.value for level in cls)
+
+        def column_false(self) -> str:
+            return self.name + '_False'
+
+        def column_total(self) -> str:
+            return self.name + '_Total'
+
+    def __init__(self, report_directory):
+        self.dir = report_directory
+
+    def file_name(self, test_case: TestCase, table_type: TableType) -> str:
+        return self.dir + '/' + test_case.value + '/' + table_type.value
+
+    @staticmethod
+    def find_blank_lines(file_name: str) -> int:
+        with open(file_name, 'r') as f:
+            return list(i for i, l in enumerate(f.readlines()) if l.isspace())
+
+    def read_fa_list_file(self, test_case: TestCase) -> pd.DataFrame:
+        '''Read the `TableType.FA_List` (FalseAccepts.txt) file.
+
+        The table will include the following columns:
+        VerifyUser, VerifyFinger, VerifySample, EnrollUser, EnrollFinger, Strong FA
+        '''
+
+        assert test_case in self.TestCase
+
+        file_name = self.file_name(test_case, self.TableType.FA_List)
+        tbl = pd.read_csv(
+            file_name,
+            skiprows=[0, 1, 2, 3, 4],
+            header=None,
+            names=['VerifyUser', 'VerifyFinger', 'VerifySample',
+                   'EnrollUser', 'EnrollFinger'] + ['StrongFA'],
+            sep=' ?[,\/] ?',
+            engine='python',
+        )
+
+        for col in Experiment.FalseTableCol.all_values():
+            col_text = tbl[col].str.extract('= (\d+)', expand=False)
+            tbl[col] = pd.to_numeric(col_text)
+
+        tbl['StrongFA'] = (tbl['StrongFA'] != 'no')
+
+        tbl.attrs['ReportDir'] = self.dir
+        tbl.attrs['TestCase'] = test_case.name
+        tbl.attrs['TableType'] = self.TableType.FA_List
+        return tbl
+
+    def read_decision_file(self,
+                           test_case: TestCase,
+                           table_type: TableType) -> pd.DataFrame:
+        '''Read the `TableType.FAR_Decision` or `TableType.FRR_Decision` file.
+
+        The table will include the following columns:
+        VerifyUser, VerifyFinger, VerifySample, EnrollUser, EnrollFinger, Strong FA
+        '''
+
+        assert test_case in self.TestCase
+        assert table_type in [self.TableType.FAR_Decision,
+                              self.TableType.FRR_Decision]
+
+        file_name = self.file_name(test_case, table_type)
+        tbl = pd.read_csv(file_name)
+
+        tbl.attrs['ReportDir'] = self.dir
+        tbl.attrs['TestCase'] = test_case.name
+        tbl.attrs['TableType'] = table_type.name
+        return tbl
+
+    def read_far_frr_file(self,
+                          test_case: TestCase,
+                          table_type: TableType,
+                          sec_levels: List[SecLevel] = SecLevel.all()) -> pd.DataFrame:
+        '''Read `TableType.FAR` and `TableType.FRR` (F[AR]R_stats_4level.txt) file.
+
+        This only reads the last/bottom table of the file.
+        '''
+
+        assert test_case in self.TestCase
+        assert table_type in [self.TableType.FAR, self.TableType.FRR]
+
+        file_name = self.file_name(test_case, table_type)
+        blank_lines = self.find_blank_lines(file_name)
+        # Account for possibly extra blank lines.
+        if len(blank_lines) < 2:
+            return None
+
+        tbl: pd.DataFrame = pd.read_table(
+            file_name,
+            skiprows=blank_lines[1] + 1,
+            header=None,
+            names=['User', 'Finger'] + self.SecLevel.all_values(),
+            # Comma or more than 2 spaces.
+            sep=r'\, | {2,}',
+            engine='python',
+        )
+
+        user_id = tbl['User'].str.extract('(\d+)', expand=False)
+        finger_id = tbl['Finger'].str.extract('(\d+)', expand=False)
+        tbl['User'] = pd.to_numeric(user_id)
+        tbl['Finger'] = pd.to_numeric(finger_id)
+
+        for level in sec_levels:
+            false_count = tbl[level.value].str.extract('(\d+)\/', expand=False)
+            total_count = tbl[level.value].str.extract('\/(\d+)', expand=False)
+            tbl[level.column_false()] = pd.to_numeric(false_count)
+            tbl[level.column_total()] = pd.to_numeric(total_count)
+        tbl = tbl.drop(columns=self.SecLevel.all_values())
+        tbl.attrs['ReportDir'] = self.dir
+        tbl.attrs['TestCase'] = test_case.name
+        tbl.attrs['TableType'] = table_type.name
+
+        return tbl
+
+    def read_file(self,
+                  test_case: TestCase,
+                  table_type: TableType) -> pd.DataFrame:
+        '''Read specified BET generated table for the specified `test_case`.
+
+        This is an interface that calls on the correct read file function for
+        the specified table.
+        '''
+
+        assert test_case in self.TestCase
+        assert table_type in self.TableType
+
+        if table_type in [self.TableType.FAR, self.TableType.FRR]:
+            return self.read_far_frr_file(test_case, table_type)
+        elif table_type is self.TableType.FA_List:
+            return self.read_fa_list_file(test_case)
+        elif table_type in [self.TableType.FAR_Decision, self.TableType.FRR_Decision]:
+            return self.read_decision_file(test_case, table_type)
+        else:
+            return None
