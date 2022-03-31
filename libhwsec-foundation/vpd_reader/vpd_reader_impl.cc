83008c7815d4687c272a5987df37448a9d597868
Leo Lai <cylai@google.com>
2022-03-31 15:42:52 +0800

    libhwsec-foundation: add vpd reader
    Reading vpd data is at least needed by attestation service, which
    currently utilizes bash script.
    
    Having a vpd reader here is better for maintenance in the long run.
    
    BUG=b:227463940
    TEST=CQ
    
    Change-Id: I7997c2c84ad7ecb273e5324814d8ea9022d787cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3817768
    Reviewed-by: Yi Chou <yich@google.com>
    Commit-Queue: Leo Lai <cylai@google.com>
    Tested-by: Leo Lai <cylai@google.com>

---
+++ libhwsec-foundation/vpd_reader/vpd_reader_impl.cc
@@ -0,0 +1,106 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "libhwsec-foundation/vpd_reader/vpd_reader_impl.h"
+
+#include <unistd.h>
+
+#include <cstdio>
+#include <memory>
+#include <optional>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include <base/logging.h>
+#include <brillo/process/process.h>
+#include <base/strings/string_split.h>
+
+namespace hwsec_foundation {
+
+namespace {
+
+constexpr char kDefaultVpdPath[] = "/usr/sbin/vpd";
+
+// Parses the output of `vpd -l` into a table of key-value pairs. If any parsing
+// error occurs, returns `std::nullopt`.
+std::optional<std::unordered_map<std::string, std::string> > ParsekeyValuePairs(
+    const std::string vpd_output) {
+  std::unordered_map<std::string, std::string> table;
+  std::vector<std::string> pairs = base::SplitString(
+      vpd_output, "\n", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+
+  // TODO(b/227463940): Rewrite with regex.
+  for (const std::string& pair_str : pairs) {
+    const size_t split_pos = pair_str.find('=');
+    if (split_pos == std::string::npos) {
+      LOG(ERROR) << __func__
+                 << ": Missing '=' in vpd output line: " << pair_str;
+      return std::nullopt;
+    }
+    // Make the string of key and value, respectively.
+    std::string pair[2] = {pair_str.substr(0, split_pos),
+                           pair_str.substr(split_pos + 1)};
+    // Remove the the doule quote marks at head and tail after validate the
+    // existence.
+    for (std::string& key_or_value : pair) {
+      if (key_or_value.size() < 2 || key_or_value.front() != '"' ||
+          key_or_value.back() != '"') {
+        LOG(ERROR) << __func__ << ": Missing double quotes in vpd output line: "
+                   << pair_str;
+        return std::nullopt;
+      }
+      key_or_value = key_or_value.substr(1, key_or_value.size() - 2);
+    }
+    table.emplace(std::move(pair[0]), std::move(pair[1]));
+  }
+
+  return table;
+}
+
+}  // namespace
+
+VpdReaderImpl::VpdReaderImpl(std::unique_ptr<brillo::Process> process,
+                             const std::string& vpd_path)
+    : process_(std::move(process)), vpd_path_(vpd_path) {
+  Prepare();
+}
+
+VpdReaderImpl::VpdReaderImpl()
+    : VpdReaderImpl(std::make_unique<brillo::ProcessImpl>(), kDefaultVpdPath) {}
+
+void VpdReaderImpl::Prepare() {
+  if (table_.has_value()) {
+    LOG(DFATAL) << __func__ << " should be called only once.";
+    return;
+  }
+
+  // Invoke `vpd_path_`.
+  process_->AddArg(vpd_path_);
+  // List all key-value pairs in RO_VPD.
+  process_->AddArg("-l");
+  // Redirect the outputs to memory for later use.
+  process_->RedirectUsingMemory(STDOUT_FILENO);
+  process_->RedirectUsingMemory(STDERR_FILENO);
+  if (process_->Run() != 0) {
+    LOG(ERROR) << "Failed to run vpd: "
+               << process_->GetOutputString(STDERR_FILENO);
+    return;
+  }
+  table_ = ParsekeyValuePairs(process_->GetOutputString(STDOUT_FILENO));
+}
+
+std::optional<std::string> VpdReaderImpl::Get(const std::string& key) {
+  // `Prepare()` should print error messages already.
+  if (table_ == std::nullopt) {
+    return std::nullopt;
+  }
+  if (table_->count(key) == 0) {
+    LOG(WARNING) << __func__ << ": " << key << " missing in RO_VPD.";
+    return std::nullopt;
+  }
+  return table_->at(key);
+}
+
+}  // namespace hwsec_foundation
---
+++ libhwsec-foundation/vpd_reader/vpd_reader_impl_test.cc
@@ -0,0 +1,143 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "libhwsec-foundation/vpd_reader/vpd_reader_impl.h"
+
+#include <optional>
+#include <string>
+
+#include <base/logging.h>
+#include <brillo/process/process_mock.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace hwsec_foundation {
+
+namespace {
+
+using ::testing::Return;
+using ::testing::StrictMock;
+
+const char kFakeVpdPath[] = "/fake/pvd/path";
+const char kVpdListOption[] = "-l";
+
+// Prefer to call `GetFakeVpdOutput()` since the extra newline at head.
+constexpr char kFakeVpdOutput[] = R"(
+"ABC"="DEF"
+"JFK"="XYZ"
+"QQQ"="""""
+"EEE"="==="
+)";
+
+// Returns the string formatted as `vpd -l` would dump.
+const std::string GetFakeVpdOutput() {
+  // Offset by 1 to skip the first newline.
+  return std::string(kFakeVpdOutput + 1);
+}
+
+}  // namespace
+
+class VpdReaderImplTest : public ::testing::Test {
+ public:
+  VpdReaderImplTest() {
+    ON_CALL(*process_mock_, GetOutputString(STDOUT_FILENO))
+        .WillByDefault(Return(fake_output_));
+  }
+
+ protected:
+  // The mock process that is owned and used by the object under test later.
+  StrictMock<brillo::ProcessMock>* process_mock_ =
+      new StrictMock<brillo::ProcessMock>();
+  std::string fake_output_ = GetFakeVpdOutput();
+};
+
+namespace {
+
+TEST_F(VpdReaderImplTest, GetSuccess) {
+  EXPECT_CALL(*process_mock_, AddArg(kFakeVpdPath));
+  EXPECT_CALL(*process_mock_, AddArg(kVpdListOption));
+  EXPECT_CALL(*process_mock_, RedirectUsingMemory(STDOUT_FILENO));
+  EXPECT_CALL(*process_mock_, RedirectUsingMemory(STDERR_FILENO));
+  EXPECT_CALL(*process_mock_, Run()).WillOnce(Return(0));
+  EXPECT_CALL(*process_mock_, GetOutputString(STDOUT_FILENO));
+  VpdReaderImpl reader(std::unique_ptr<brillo::Process>(process_mock_),
+                       kFakeVpdPath);
+  std::optional<std::string> output = reader.Get("ABC");
+  ASSERT_TRUE(output.has_value());
+  EXPECT_EQ(output.value(), "DEF");
+  // Query another entry should not invoke the vpd process again; the strict
+  // mock will verify.
+  output = reader.Get("JFK");
+  ASSERT_TRUE(output.has_value());
+  EXPECT_EQ(output.value(), "XYZ");
+  // The value with double quotes should be supported.
+  output = reader.Get("QQQ");
+  ASSERT_TRUE(output.has_value());
+  EXPECT_EQ(output.value(), "\"\"\"");
+  // The value with '=' quoshould be supported.
+  output = reader.Get("EEE");
+  ASSERT_TRUE(output.has_value());
+  EXPECT_EQ(output.value(), "===");
+}
+
+TEST_F(VpdReaderImplTest, GetFailureAbsentKey) {
+  EXPECT_CALL(*process_mock_, AddArg(kFakeVpdPath));
+  EXPECT_CALL(*process_mock_, AddArg(kVpdListOption));
+  EXPECT_CALL(*process_mock_, RedirectUsingMemory(STDOUT_FILENO));
+  EXPECT_CALL(*process_mock_, RedirectUsingMemory(STDERR_FILENO));
+  EXPECT_CALL(*process_mock_, Run()).WillOnce(Return(0));
+  EXPECT_CALL(*process_mock_, GetOutputString(STDOUT_FILENO));
+  VpdReaderImpl reader(std::unique_ptr<brillo::Process>(process_mock_),
+                       kFakeVpdPath);
+  std::optional<std::string> output = reader.Get("a non-existent key");
+  EXPECT_FALSE(output.has_value());
+}
+
+TEST_F(VpdReaderImplTest, GetFailureKeyValueFormatError) {
+  EXPECT_CALL(*process_mock_, AddArg(kFakeVpdPath));
+  EXPECT_CALL(*process_mock_, AddArg(kVpdListOption));
+  EXPECT_CALL(*process_mock_, RedirectUsingMemory(STDOUT_FILENO));
+  EXPECT_CALL(*process_mock_, RedirectUsingMemory(STDERR_FILENO));
+  EXPECT_CALL(*process_mock_, Run()).WillOnce(Return(0));
+
+  fake_output_.erase(fake_output_.find('='), 1);
+  EXPECT_CALL(*process_mock_, GetOutputString(STDOUT_FILENO))
+      .WillOnce(Return(fake_output_));
+
+  VpdReaderImpl reader(std::unique_ptr<brillo::Process>(process_mock_),
+                       kFakeVpdPath);
+  std::optional<std::string> output = reader.Get("ABC");
+  EXPECT_FALSE(output.has_value());
+}
+
+TEST_F(VpdReaderImplTest, GetFailureMissingExpectedDoubleQuote) {
+  size_t double_quote_pos = 0;
+  for (int i = 0; i < 4; ++i) {
+    // Make the number of iteration verbose for debugging.
+    LOG(INFO) << "Removing \"; iteration " << i;
+    EXPECT_CALL(*process_mock_, AddArg(kFakeVpdPath));
+    EXPECT_CALL(*process_mock_, AddArg(kVpdListOption));
+    EXPECT_CALL(*process_mock_, RedirectUsingMemory(STDOUT_FILENO));
+    EXPECT_CALL(*process_mock_, RedirectUsingMemory(STDERR_FILENO));
+    EXPECT_CALL(*process_mock_, Run()).WillOnce(Return(0));
+
+    fake_output_ = GetFakeVpdOutput();
+    double_quote_pos = fake_output_.find('\"', double_quote_pos);
+    fake_output_.erase(double_quote_pos, 1);
+    ++double_quote_pos;
+    EXPECT_CALL(*process_mock_, GetOutputString(STDOUT_FILENO))
+        .WillOnce(Return(fake_output_));
+
+    VpdReaderImpl reader(std::unique_ptr<brillo::Process>(process_mock_),
+                         kFakeVpdPath);
+    std::optional<std::string> output = reader.Get("ABC");
+    EXPECT_FALSE(output.has_value());
+    // AS a workaround, re-create the process mock.
+    process_mock_ = new StrictMock<brillo::ProcessMock>();
+  }
+}
+
+}  // namespace
+
+}  // namespace hwsec_foundation
