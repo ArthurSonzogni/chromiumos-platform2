dbfb9024d2254ea329cdd8e7275e442c466da4f1
Craig Hesling <hesling@chromium.org>
2022-04-01 22:33:30 -0700

    fpstudy/analysis-tool: Add group handling to Experiment
    BUG=b:323261182
    TEST=None
    
    Change-Id: I11761c1c50eedec180e25c1eaa680c253bc8a3bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5393921
    Commit-Queue: Craig Hesling <hesling@chromium.org>
    Tested-by: Craig Hesling <hesling@chromium.org>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- biod/study/analysis-tool/experiment.py
+++ biod/study/analysis-tool/experiment.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from enum import Enum
+import glob
+import pathlib
 from typing import Optional
 
 import pandas as pd
@@ -113,6 +115,58 @@ class Experiment:
 
         return false_table
 
+    def _AddGroupsTo(tbl: pd.DataFrame, user_groups: pd.DataFrame) -> pd.DataFrame:
+        '''Adds the appropriate group columns for any user columns in `tbl`.
+
+        This joins the `Group` columns from `users_groups` with any user columns
+        in `tbl`.
+
+        The `user_groups` table is expected to have a `User` and `Group` column.
+        '''
+
+        # Add Group column.
+        if Experiment.TableCol.User.value in tbl.columns:
+
+            tbl = tbl.join(
+                user_groups.set_index(Experiment.TableCol.Verify_User.value),
+                on=Experiment.TableCol.Verify_User.value,
+            )
+
+        # Add Verify_Group column.
+        if Experiment.TableCol.Verify_User.value in tbl.columns:
+            tbl = tbl.join(
+                user_groups.rename(columns={
+                    Experiment.TableCol.User.value: Experiment.TableCol.Verify_User.value,
+                    Experiment.TableCol.Group.value: Experiment.TableCol.Verify_Group.value,
+                }).set_index(Experiment.TableCol.Verify_User.value),
+                on=Experiment.TableCol.Verify_User.value,
+            )
+
+        # Add Enroll_Group column.
+        if Experiment.TableCol.Enroll_User.value in tbl.columns:
+            tbl = tbl.join(
+                user_groups.rename(columns={
+                    Experiment.TableCol.User.value: Experiment.TableCol.Enroll_User.value,
+                    Experiment.TableCol.Group.value: Experiment.TableCol.Enroll_Group.value,
+                }).set_index(Experiment.TableCol.Enroll_User.value),
+                on=Experiment.TableCol.Enroll_User.value,
+            )
+
+        return tbl
+
+    def _UserGroupsFromCollectionDir(collection_dir: pathlib.Path) -> pd.DataFrame:
+        assert collection_dir.exists()
+
+        user_groups_set = []
+        for path in glob.glob(str(collection_dir) + '/*/*'):
+            path_split = path.split('/')
+            user_id, user_group = path_split[-2], path_split[-1]
+            user_groups_set.append((int(user_id), user_group))
+
+        return pd.DataFrame(user_groups_set,
+                            columns=[Experiment.TableCol.User.value,
+                                     Experiment.TableCol.Group.value])
+
     def __init__(self,
                  #  num_enrollment: int,
                  num_verification: int,
@@ -159,6 +213,27 @@ class Experiment:
                                             verify_user_id=verify_user_id,
                                             verify_finger_id=verify_finger_id,
                                             verify_sample_index=verify_sample_index)
+
+    def AddGroups(self, user_groups: pd.DataFrame):
+        '''Add the appropriate group columns to all saved tables.'''
+
+        if not self.tbl_far_decisions is None:
+            self.tbl_far_decisions = Experiment._AddGroupsTo(
+                self.tbl_far_decisions, user_groups)
+
+        if not self.tbl_fa_list is None:
+            self.tbl_fa_list = Experiment._AddGroupsTo(
+                self.tbl_fa_list, user_groups)
+
+    def AddGroupsFromCollectionDir(self, collection_dir: pathlib.Path):
+        '''Add the appropriate group columns to all saved tables.
+
+        This group information is learned from the subdirectory structure
+        of the raw collection directory.
+        '''
+
+        user_groups = Experiment._UserGroupsFromCollectionDir(collection_dir)
+        self.AddGroups(user_groups)
 
     def FATestResult() -> pd.DataFrame:
         pass
