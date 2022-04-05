436ffec73d7c01003e071f1cbfe92c37fcf6727e
Craig Hesling <hesling@chromium.org>
2022-04-05 11:46:01 -0700

    fpstudy/analysis-tool: Move experiment benchmarks to experiment_test
    BUG=b:323261182
    TEST=None
    
    Change-Id: I787fbecd5152a8775e0985cc39d071badeb4ee61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5393935
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Craig Hesling <hesling@chromium.org>
    Commit-Queue: Craig Hesling <hesling@chromium.org>

--- biod/study/analysis-tool/ResultsEval.py
+++ biod/study/analysis-tool/ResultsEval.py
@@ -315,33 +315,6 @@ for s in range(NUM_SAMPLES):
     print('')
     # print('samples =', sample)
     print('sum(samples) =', sum(sample))
-# %%
-
-#! %timeit b.FAList()
-#! %timeit b.FAQuery()
-
-
-# %%
-print('Query 1 Timing')
-#! %timeit b.FAQuery(verify_user_id=10071)
-#! %timeit b.FAQuery(verify_user_id=10071, verify_finger_id=0)
-#! %timeit b.FAQuery(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60)
-#! %timeit b.FAQuery(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60, enroll_user_id=10057)
-#! %timeit b.FAQuery(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60, enroll_user_id=10057, enroll_finger_id=4)
-
-# %%
-print('Query 2 Timing')
-#! %timeit b.FAQuery2(verify_user_id=10071)
-# %timeit b.FAQuery2(verify_user_id=10071, verify_finger_id=0)
-# %timeit b.FAQuery2(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60)
-# %timeit b.FAQuery2(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60, enroll_user_id=10057)
-#! %timeit b.FAQuery2(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60, enroll_user_id=10057, enroll_finger_id=4)
-
-# %%
-
-print('Query 3 Timing')
-s = fpsutils.DataFrameSetAccess(b.FAList())
-#! %timeit s.isin((10011, 5, 69, 10012, 0, False))
 
 # %%
 
---
+++ biod/study/analysis-tool/experiment_test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Test and benchmark the experiment module."""
+
+import unittest
+
+import fpsutils
+import simulate_fpstudy
+from experiment import Experiment
+
+
+class Test_FAQuery(unittest.TestCase):
+    def setUp(self):
+        # VerifyUser VerifyFinger VerifySample EnrollUser EnrollFinger Decision
+        # This can take about 2 seconds.
+        self.far_decisions = simulate_fpstudy.GenerateFARResults(
+            num_users=72,
+            num_fingers=3,
+            num_verify_samples=40,
+            user_groups=None,
+            verbose=True,
+        )
+        self.exp = Experiment(
+            num_users=72,
+            num_fingers=3,
+            num_verification=40,
+            far_decisions=self.far_decisions,
+        )
+
+    def test_benchmark_faquery1(self):
+        print('Query 1 Timing')
+
+        fpsutils.autorange('self.exp.FAQuery(verify_user_id=10071)',
+                           globals={**globals(), **locals()})
+        fpsutils.autorange('self.exp.FAQuery(verify_user_id=10071, verify_finger_id=0)',
+                           globals={**globals(), **locals()})
+        fpsutils.autorange('self.exp.FAQuery(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60)',
+                           globals={**globals(), **locals()})
+        fpsutils.autorange('self.exp.FAQuery(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60, enroll_user_id=10057)',
+                           globals={**globals(), **locals()})
+        fpsutils.autorange('self.exp.FAQuery(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60, enroll_user_id=10057, enroll_finger_id=4)',
+                           globals={**globals(), **locals()})
+
+    def test_benchmark_faquery2(self):
+        print('Query 2 Timing')
+
+        fpsutils.autorange('self.exp.FAQuery2(verify_user_id=10071)',
+                           globals={**globals(), **locals()})
+        # fpsutils.autorange('self.exp.FAQuery2(verify_user_id=10071, verify_finger_id=0)',
+        #                    globals={**globals(), **locals()})
+        # fpsutils.autorange('self.exp.FAQuery2(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60)',
+        #                    globals={**globals(), **locals()})
+        # fpsutils.autorange('self.exp.FAQuery2(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60, enroll_user_id=10057)',
+        #                    globals={**globals(), **locals()})
+        fpsutils.autorange('self.exp.FAQuery2(verify_user_id=10071, verify_finger_id=0, verify_sample_index=60, enroll_user_id=10057, enroll_finger_id=4)',
+                           globals={**globals(), **locals()})
+
+
+if __name__ == '__main__':
+    unittest.main()
