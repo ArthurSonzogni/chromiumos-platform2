d680824e5cdfbd1fba58960a485cdd7a595ec17c
Allen Webb <allenwebb@google.com>
2022-04-07 14:35:52 -0500

    libchromeos-rs: Add alternative dev-mode check.
    vboot_reference is not available in the ManaTEE hypervisor, so check the
    kernel command line parameters instead.
    
    BUG=b:219020445
    TEST=manatee shell works on ToT
    
    Change-Id: I874a8331bfd5d2181f9b03a3e98ddc8db5b94510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3576585
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Tested-by: Allen Webb <allenwebb@google.com>
    Reviewed-by: Paramjit Oberoi <psoberoi@google.com>
    Commit-Queue: Allen Webb <allenwebb@google.com>

--- libchromeos-rs/src/lib.rs
+++ libchromeos-rs/src/lib.rs
@@ -4,5 +4,29 @@
 
 #[cfg(feature = "chromeos-module")]
 pub mod chromeos;
+
+// Fallback dev-mode check if vboot_reference is not available.
+#[cfg(not(feature = "chromeos-module"))]
+pub mod chromeos {
+    use std::fs::read_to_string;
+    use std::io;
+    use std::path::Path;
+
+    use thiserror::Error as ThisError;
+
+    #[derive(ThisError, Debug)]
+    pub enum Error {
+        #[error("failed to get kernel command line: {0}")]
+        ReadError(io::Error),
+    }
+
+    pub type Result<R> = std::result::Result<R, Error>;
+
+    pub fn is_dev_mode() -> Result<bool> {
+        let contents = read_to_string(Path::new("/proc/cmdline")).map_err(Error::ReadError)?;
+        Ok(contents.split(' ').any(|token| token == "cros_debug"))
+    }
+}
+
 pub mod secure_blob;
 pub mod syslog;
