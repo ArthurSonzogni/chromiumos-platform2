497fff86997c2e769c915ab5c3ad21556a8a0592
David Stevens <stevensd@chromium.org>
2022-04-08 15:54:12 +0900

    vm_tools: Read VVU UUID via VFIO
    Use VFIO to read the VVU device's UUID, instead of using sysfs. This
    requires less permissions.
    
    BUG=b:222424090
    TEST=vmc start termina
    
    Change-Id: I5d98e026ac9974f3aa4c6037d088e704904a0b92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3577765
    Reviewed-by: Abhishek Bhardwaj <abhishekbh@chromium.org>
    Tested-by: David Stevens <stevensd@chromium.org>
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>
    Commit-Queue: Keiichi Watanabe <keiichiw@chromium.org>

--- vm_tools/concierge/sibling_vms.cc
+++ vm_tools/concierge/sibling_vms.cc
@@ -5,7 +5,9 @@
 #include "vm_tools/concierge/sibling_vms.h"
 
 #include <linux/pci_regs.h>
+#include <linux/vfio.h>
 #include <linux/virtio_pci.h>
+#include <sys/ioctl.h>
 #include <sys/mman.h>
 
 #include <string>
@@ -18,6 +20,7 @@
 #include <base/files/memory_mapped_file.h>
 #include <base/strings/string_number_conversions.h>
 #include <base/strings/string_util.h>
+#include <base/threading/thread.h>
 
 namespace vm_tools {
 namespace concierge {
@@ -47,19 +50,12 @@ constexpr int32_t kVvuSocketIndexByte = 15;
 constexpr int64_t kPciDeviceConfigurationSize = 256;
 
 // Name of the file within the PCI device directory which contains a device's
-// 256 byte configuration header.
-constexpr char kPciConfigFileName[] = "config";
-
-// Name of the file within the PCI device directory which contains a device's
 // vendor ID.
 constexpr char kPciVendorIdFileName[] = "vendor";
 
 // Name of the file within the PCI device directory which contains a device's
 // dwvicw ID.
 constexpr char kPciDeviceIdFileName[] = "device";
-
-// BAR 0 is present in the file "resource0", BAR 1 is present at "resource1".
-constexpr char kPciBarFilePrefix[] = "resource";
 
 // Offset in the configuration header at which the location of the first PCI
 // capability is present.
@@ -132,20 +128,58 @@ base::Optional<int64_t> GetPciDeviceDeviceId(const base::FilePath& pci_device) {
   return parsed_device_id;
 }
 
-// Walks all the PCI capabilities of |pci_device| and tries to find the bar and
-// offset corresponding to the device's configuration.
+// Opens the VFIO group file associated with |pci_device|.
+base::File OpenVfioGroup(const base::FilePath& pci_device) {
+  // The vfio group number is the same as the kernel iommu_group number
+  // this file is symlinked to.
+  base::FilePath dev_iommu_group = pci_device.Append("iommu_group");
+  base::FilePath iommu_group;
+  if (!base::ReadSymbolicLink(dev_iommu_group, &iommu_group)) {
+    LOG(ERROR) << "Failed to read iommu group " << dev_iommu_group;
+    return base::File();
+  }
+
+  // We need to wait for udev to update permissions on the vfio group file
+  // before we can open it, which happens asynchronously after we rebind the
+  // device to vfio-pci. Unfortunately, there is no easy way to wait for
+  // this, so just poll. In practice, this should take <100ms.
+  for (int i = 0; i < 50; i++) {
+    base::File file(base::FilePath("/dev/vfio").Append(iommu_group.BaseName()),
+                    base::File::Flags::FLAG_OPEN |
+                        base::File::Flags::FLAG_READ |
+                        base::File::Flags::FLAG_WRITE);
+    if (file.IsValid()) {
+      return file;
+    }
+    base::PlatformThread::Sleep(base::Milliseconds(50));
+  }
+
+  PLOG(ERROR) << "Failed to open vfio group";
+  return base::File();
+}
+
+// Walks all the PCI capabilities of |vfio_device| and tries to find the bar
+// and offset corresponding to the device's configuration.
 //
 // Returns base::nullopt if there is a parsing error or it can't find the
 // location.
-base::Optional<PciDeviceConfigLocation> GetPciDeviceConfigLocation(
-    const base::FilePath& pci_device) {
+base::Optional<PciDeviceConfigLocation> FindPciDeviceConfigLocation(
+    base::File* vfio_device) {
   uint8_t config[kPciDeviceConfigurationSize] = {0};
-  base::File pci_device_config(
-      pci_device.Append(kPciConfigFileName),
-      base::File::Flags::FLAG_OPEN | base::File::Flags::FLAG_READ);
 
-  if (!pci_device_config.ReadAndCheck(0 /* offset */, config)) {
-    LOG(ERROR) << "Failed to read PCI config for device: " << pci_device;
+  struct vfio_region_info reg = {};
+  reg.argsz = sizeof(reg);
+  reg.index = VFIO_PCI_CONFIG_REGION_INDEX;
+  int ret =
+      ioctl(vfio_device->GetPlatformFile(), VFIO_DEVICE_GET_REGION_INFO, &reg);
+  if (ret != 0) {
+    LOG(ERROR) << "Failed to get config region info: " << ret;
+    return base::nullopt;
+  }
+
+  if (!vfio_device->ReadAndCheck(
+          reg.offset, base::make_span(config).subspan(0, reg.size))) {
+    PLOG(ERROR) << "Failed to read config";
     return base::nullopt;
   }
 
@@ -160,8 +194,7 @@ base::Optional<PciDeviceConfigLocation> GetPciDeviceConfigLocation(
     // We don't want to be in an endless list of PCI capabilities. It may be a
     // malicious or malformed device. Bail in this situation.
     if (num_tries >= kMaxPciCapabilities) {
-      LOG(ERROR) << "Maxed out capability walk iterations for PCI devices: "
-                 << pci_device;
+      LOG(ERROR) << "Maxed out capability walk iterations for PCI devices";
       return base::nullopt;
     }
 
@@ -170,8 +203,7 @@ base::Optional<PciDeviceConfigLocation> GetPciDeviceConfigLocation(
     // header. This could both be a functionality as well as security issue.
     if (capability_offset + sizeof(virtio_pci_cap) >=
         kPciDeviceConfigurationSize) {
-      LOG(ERROR) << "Encountered bad capability offset: " << capability_offset
-                 << " for PCI device: " << pci_device;
+      LOG(ERROR) << "Encountered bad capability offset: " << capability_offset;
       return base::nullopt;
     }
     memcpy(&virtio_pci_cap, &config[capability_offset], sizeof(virtio_pci_cap));
@@ -199,50 +231,129 @@ base::Optional<PciDeviceConfigLocation> GetPciDeviceConfigLocation(
 // The caller must ensure that |pci_device| is a VVU device.
 base::Optional<VvuProxyDeviceConfig> ReadVvuProxyDeviceConfig(
     const base::FilePath& pci_device) {
+  // Initialize VFIO access to |pci_device|.
+  base::File vfio_container(base::FilePath("/dev/vfio/vfio"),
+                            base::File::Flags::FLAG_OPEN |
+                                base::File::Flags::FLAG_READ |
+                                base::File::Flags::FLAG_WRITE);
+  if (!vfio_container.IsValid()) {
+    PLOG(ERROR) << "Failed to open vfio container";
+    return base::nullopt;
+  }
+
+  if (ioctl(vfio_container.GetPlatformFile(), VFIO_GET_API_VERSION) !=
+      VFIO_API_VERSION) {
+    LOG(ERROR) << "VFIO API version mismatch";
+    return base::nullopt;
+  }
+
+  base::File vfio_group = OpenVfioGroup(pci_device);
+  if (!vfio_group.IsValid()) {
+    return base::nullopt;
+  }
+
+  // Store the fd in a local variable because VFIO_GROUP_SET_CONTAINER
+  // needs a pointer to the fd.
+  base::PlatformFile container_fd = vfio_container.GetPlatformFile();
+  int ret = ioctl(vfio_group.GetPlatformFile(), VFIO_GROUP_SET_CONTAINER,
+                  &container_fd);
+  if (ret != 0) {
+    LOG(ERROR) << "Failed to set container: " << ret;
+    return base::nullopt;
+  }
+
+  // We're not doing any IO, but we still can't get the device fd
+  // without an IOMMU.
+  ret =
+      ioctl(vfio_container.GetPlatformFile(), VFIO_SET_IOMMU, VFIO_TYPE1_IOMMU);
+  if (ret != 0) {
+    LOG(ERROR) << "Failed to set VFIO IOMMU: " << ret;
+    return base::nullopt;
+  }
+
+  ret = ioctl(vfio_group.GetPlatformFile(), VFIO_GROUP_GET_DEVICE_FD,
+              pci_device.BaseName().MaybeAsASCII().c_str());
+  if (ret < 0) {
+    LOG(ERROR) << "Failed to get device fd: " << ret;
+    return base::nullopt;
+  }
+  base::File vfio_device(ret);
+
   // Figure out which bar and what offset the device configuration is located.
-  auto device_config_location = GetPciDeviceConfigLocation(pci_device);
+  auto device_config_location = FindPciDeviceConfigLocation(&vfio_device);
   if (!device_config_location) {
+    LOG(ERROR) << "Failed to find device config for " << pci_device;
     return base::nullopt;
   }
 
   // Read the bar at the offset calculated above to get the VVU device's
   // configuration.
-  //
-  // BAR 0 is present in the file "resource0", BAR 1 is present at "resource1".
-  base::FilePath pci_device_config_bar_path(pci_device.Append(
-      kPciBarFilePrefix + std::to_string(device_config_location->bar)));
-  // The BAR file can only be read via mmap.
+  struct vfio_region_info reg = {};
+  reg.argsz = sizeof(reg);
+  reg.index = VFIO_PCI_BAR0_REGION_INDEX + device_config_location->bar;
+  ret = ioctl(vfio_device.GetPlatformFile(), VFIO_DEVICE_GET_REGION_INFO, &reg);
+  if (ret != 0) {
+    LOG(ERROR) << "Failed to get config region info: " << ret;
+    return base::nullopt;
+  }
+
   VvuProxyDeviceConfig vvu_proxy_device_config;
-  base::MemoryMappedFile::Region device_config_region;
-  device_config_region.offset = device_config_location->offset_in_bar;
-  device_config_region.size = sizeof(vvu_proxy_device_config);
-  base::MemoryMappedFile pci_device_config_bar;
-  if (!pci_device_config_bar.Initialize(
-          base::File(
-              pci_device_config_bar_path,
-              base::File::Flags::FLAG_OPEN | base::File::Flags::FLAG_READ),
-          device_config_region) ||
-      !pci_device_config_bar.IsValid()) {
-    PLOG(ERROR) << "Failed to mmap BAR file: " << pci_device_config_bar_path;
+  if (!vfio_device.ReadAndCheck(
+          reg.offset + device_config_location->offset_in_bar,
+          base::as_writable_bytes(
+              base::make_span(&vvu_proxy_device_config, 1)))) {
+    PLOG(ERROR) << "Failed to read device config";
     return base::nullopt;
   }
-
-  if (pci_device_config_bar.length() < sizeof(vvu_proxy_device_config)) {
-    LOG(ERROR) << "BAR size invalid: " << pci_device_config_bar.length();
-    return base::nullopt;
-  }
-
-  uint8_t* addr = pci_device_config_bar.data();
-  // TODO(b/196186396): For some reason the |memcpy| command below causes this
-  // process to crash. For now we just populate the socket index value in the
-  // UUID from |addr|. This is all we need right now.
-  // memcpy((void*)(&vvu_proxy_device_config), addr,
-  // sizeof(vvu_proxy_device_config));
-  //
-  // The socket index is the last byte present in |addr| and |uuid|.
-  vvu_proxy_device_config.uuid[kVvuSocketIndexByte] =
-      addr[sizeof(vvu_proxy_device_config) - 1];
   return vvu_proxy_device_config;
+}
+
+// Writes 'str' to the file specified to 'path'.
+bool WriteToFile(const base::FilePath& path, std::string str) {
+  base::File file(path,
+                  base::File::Flags::FLAG_OPEN | base::File::Flags::FLAG_WRITE);
+  if (!file.IsValid()) {
+    PLOG(ERROR) << "Failed to open file " << path;
+    return false;
+  }
+
+  if (!file.WriteAtCurrentPosAndCheck(base::as_bytes(base::make_span(str)))) {
+    PLOG(ERROR) << "Failed to write " << str << " to " << path;
+    return false;
+  }
+
+  return true;
+}
+
+// Rebinds |pci_device| to the driver named |driver|.
+bool RebindDevice(const base::FilePath& pci_device, std::string driver) {
+  // The Bus:Device.Function identifying what device to unbind/bind.
+  std::string bdf = pci_device.BaseName().MaybeAsASCII();
+
+  // If currently bound to a driver, unbind.
+  auto unbind_path = base::FilePath("/sys/bus/pci/devices")
+                         .Append(bdf)
+                         .Append("driver/unbind");
+  if (base::PathExists(unbind_path) && !WriteToFile(unbind_path, bdf)) {
+    return false;
+  }
+
+  // Force usage of |driver| via the 'driver_override' setting.
+  if (!WriteToFile(pci_device.Append("driver_override"), driver)) {
+    return false;
+  }
+
+  // Bind to the |driver|.
+  if (!WriteToFile(
+          base::FilePath("/sys/bus/pci/drivers").Append(driver).Append("bind"),
+          bdf)) {
+    return false;
+  }
+
+  // Write the empty string to driver_override to return the device to using
+  // the standard matching rules. Note that this operation won't unbind the
+  // current driver or load a new driver.
+  return WriteToFile(pci_device.Append("driver_override"), "");
 }
 
 // This function returns the socket index corresponding to |pci_device|. It does
@@ -252,6 +363,11 @@ base::Optional<VvuProxyDeviceConfig> ReadVvuProxyDeviceConfig(
 // The caller must ensure that |pci_device| is a VVU device.
 base::Optional<int32_t> GetVvuDeviceSocketIndex(
     const base::FilePath& pci_device) {
+  // Rebind so we can access VVU device via VFIO.
+  if (!RebindDevice(pci_device, "vfio-pci")) {
+    return base::nullopt;
+  }
+
   auto vvu_proxy_device_config = ReadVvuProxyDeviceConfig(pci_device);
   if (!vvu_proxy_device_config) {
     return base::nullopt;
