17440ce8a3bcdd198c8b613991fbee860c4b82aa
Craig Hesling <hesling@chromium.org>
2022-04-08 00:55:04 +0000

    fpstudy/analysis-tool: Remove shebang from ResultsEval.py
    The #! is interpreted as a magic line in jupyter, which
    then causes issues.
    
    BUG=b:323261182
    TEST=None
    
    Change-Id: Ibc060447337a1033ee60ab604a0a7eb64553c563
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5393963
    Tested-by: Craig Hesling <hesling@chromium.org>
    Commit-Queue: Craig Hesling <hesling@chromium.org>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

--- biod/study/analysis-tool/ResultsEval.py
+++ biod/study/analysis-tool/ResultsEval.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Copyright 2022 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
