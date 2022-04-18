6846c32230c14794a57aee15a69f94bad354f7f4
Craig Hesling <hesling@chromium.org>
2022-04-18 20:52:48 +0000

    fpstudy/analysis-tool: Add cached data files module
    BUG=b:323261182
    TEST=None
    
    Change-Id: I37b96cb0011a4c962a34c9786ccadf9b8c22611e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5393971
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Craig Hesling <hesling@chromium.org>
    Tested-by: Craig Hesling <hesling@chromium.org>

---
+++ biod/study/analysis-tool/cached_data_file.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import mmap
+import glob
+import hashlib
+import pathlib
+import pickle
+from typing import Any, List, Union
+
+import pandas as pd
+
+
+class CachedDataFile:
+    def _cache_file_path(self, ver: str) -> pathlib.Path:
+        """Build the path to the file's cache file with the given version."""
+        orig_path = self._orig_file_path
+        name_parts = orig_path.name.split('.')
+        assert len(name_parts) >= 2
+        # <parent_dirs>/<basename>-<ver>.<extensions...>.pickle
+        name = name_parts[0] + '-' + ver + '.' + \
+            '.'.join(name_parts[1:]) + '.pickle'
+        return orig_path.parent / name
+
+    def _cache_file_pattern(self) -> str:
+        """Build the glob pattern to find cache files for all file versions."""
+        return str(self._cache_file_path('*'))
+
+    def _find_cache_files(self) -> List[pathlib.Path]:
+        """Return a list of all cache files for the data file."""
+        cache_files = glob.glob(self._cache_file_pattern())
+        return [pathlib.Path(p) for p in cache_files]
+
+    def __init__(self,
+                 data_file_path: Union[pathlib.Path, str],
+                 verbose: bool = False) -> None:
+        self._ver = None
+        self._orig_file_path = pathlib.Path(data_file_path)
+        self._verbose = verbose
+
+    def open(self):
+        self._f = open(self._orig_file_path, 'r+b')
+        # memory-map the file, size 0 means whole file
+        self._mm = mmap.mmap(self._f.fileno(), 0)
+
+    def close(self):
+        self._mm.close()
+        self._f.close()
+
+    def version(self) -> str:
+        """Return the md5 checksum of the file as a version string."""
+        if not self._ver:
+            self._ver = hashlib.md5(self._mm).hexdigest()
+        return self._ver
+
+    def prune(self):
+        """Remove obsolete cache files."""
+        ver = self.version()
+        cached_file_path = self._cache_file_path(ver)
+        cache_files = self._find_cache_files()
+
+        for cache_file in (set(cache_files) - {cached_file_path}):
+            cache_file.unlink()
+
+    def remove(self):
+        """Remove all cache files."""
+        for cache_file in self._find_cache_files():
+            cache_file.unlink()
+
+    def _parse(self) -> Any:
+        """Parse the original file into a usable object.
+
+        Implement me.
+        """
+        return None
+
+    def get(self, disable_cache: bool = False) -> Any:
+        """Get the parsed data using the caching mechanism."""
+        if disable_cache:
+            return self._parse()
+        else:
+            cached_file_path = self._cache_file_path(self.version())
+            if cached_file_path in self._find_cache_files():
+                if self._verbose:
+                    print(f'Using cache file {cached_file_path}.')
+                with open(cached_file_path, 'rb') as f:
+                    return pickle.load(f)
+            else:
+                parsed = self._parse()
+                if self._verbose:
+                    print(f'Saving to cache file {cached_file_path}.')
+                with open(cached_file_path, 'wb') as f:
+                    # Might want pickle.HIGHEST_PROTOCOL arg.
+                    pickle.dump(parsed, f)
+                return parsed
+
+    def __enter__(self):
+        """Allow for the use of the python `with` statement."""
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Allow for the use of the python `with` statement."""
+        self.close()
+
+
+class CachedCSVFile(CachedDataFile):
+    def __init__(self,
+                 data_file_path: Union[pathlib.Path, str],
+                 verbose: bool = False,
+                 **read_csv_args) -> None:
+        self._read_csv_arg = read_csv_args
+        return super().__init__(data_file_path=data_file_path, verbose=verbose)
+
+    def _parse(self) -> Any:
+        """Parse the CSV file into a pandas DataFrame."""
+        return pd.read_csv(self._mm, **self._read_csv_arg)
---
+++ biod/study/analysis-tool/cached_data_file_test.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import glob
+import pathlib
+import time
+import pandas as pd
+import unittest
+import tempfile
+import simulate_fpstudy
+from cached_data_file import CachedCSVFile
+
+
+class Test_CachedCSVFile(unittest.TestCase):
+    def setUp(self) -> None:
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.temp_csv = pathlib.Path(self.temp_dir.name) / 'test.csv'
+        print(f'Temp CSV file is {self.temp_csv}.')
+        self._generate_decisions_file()
+
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.temp_dir.cleanup()
+        return super().tearDown()
+
+    def _generate_decisions_file(self):
+        # VerifyUser VerifyFinger VerifySample EnrollUser EnrollFinger Decision
+        # This can take about 2 seconds.
+        self.far_decisions = simulate_fpstudy.GenerateFARResults(
+            num_users=72,
+            num_fingers=3,
+            num_verify_samples=40,
+            user_groups=None,
+            verbose=False,
+        )
+        self.far_decisions.to_csv(self.temp_csv, index=False)
+
+    def test_benchmark(self):
+        # Get timing for running the alternative pd.read_csv.
+        time_original_start = time.time()
+        _ = pd.read_csv(self.temp_csv)
+        time_original_end = time.time()
+
+        # Get timing for running just the parser of CachedCSVFile.
+        # This asseses the overhead of the wrapper.
+        time_parse_nocache_start = time.time()
+        with CachedCSVFile(self.temp_csv, verbose=False) as c:
+            _ = c.get(disable_cache=True)
+        time_parse_nocache_end = time.time()
+
+        # Get timing of hashing, parsing and saving cache.
+        time_parse_start = time.time()
+        with CachedCSVFile(self.temp_csv, verbose=False) as c:
+            _ = c.get()
+        time_parse_end = time.time()
+
+        # Get timing of hashing, and using cached version.
+        time_load_start = time.time()
+        with CachedCSVFile(self.temp_csv, verbose=False) as c:
+            _ = c.get()
+        time_load_end = time.time()
+
+        delta_original = time_original_end-time_original_start
+        delta_parse_nocache = time_parse_nocache_end-time_parse_nocache_start
+        delta_parse = time_parse_end-time_parse_start
+        delta_load = time_load_end-time_load_start
+
+        print(f'The original pd.read_csv took {delta_original}s.')
+        print(f'It took {delta_parse_nocache}s to parse.')
+        print(f'It took {delta_parse}s to hash, parse, and save cache.')
+        print(f'It took {delta_load}s to hash and load from cache.')
+        print('SUMMARY:')
+        slowdown = delta_parse/delta_original
+        speedup = delta_original/delta_load
+        print(f'For an initial {slowdown:.3f} slowdown, '
+              f'we see an {speedup:.3f} speedup for using cache.')
+
+    def test_basic(self):
+        # Get timing for running the alternative pd.read_csv.
+        original = pd.read_csv(self.temp_csv)
+
+        # Compare output of just running the parser without caching.
+        with CachedCSVFile(self.temp_csv, verbose=True) as c:
+            parsed_nocache = c.get(disable_cache=True)
+
+        self.assertTrue(original.equals(parsed_nocache))
+
+        # Compare output of hashing, parsing and saving cache.
+        with CachedCSVFile(self.temp_csv, verbose=True) as c:
+            parsed = c.get()
+
+        self.assertTrue(original.equals(parsed))
+
+        # Compare output of hashing, and using cached version.
+        with CachedCSVFile(self.temp_csv, verbose=True) as c:
+            loaded = c.get()
+
+        self.assertTrue(original.equals(loaded))
+
+    def test_reparse(self):
+        """Ensure that if the data file changes that we reparse and recache."""
+        with CachedCSVFile(self.temp_csv, verbose=False) as c:
+            first = c.get()
+
+        pickles = glob.glob(str(pathlib.Path(self.temp_dir.name) / '*.pickle'))
+        self.assertEqual(len(pickles), 1)
+
+        self._generate_decisions_file()
+        with CachedCSVFile(self.temp_csv, verbose=False) as c:
+            second = c.get()
+
+        pickles = glob.glob(str(pathlib.Path(self.temp_dir.name) / '*.pickle'))
+        self.assertEqual(len(pickles), 2)
+
+        self.assertFalse(first.equals(second))
+
+    def test_prune(self):
+        with CachedCSVFile(self.temp_csv, verbose=False) as c:
+            _ = c.get()
+        self._generate_decisions_file()
+        with CachedCSVFile(self.temp_csv, verbose=False) as c:
+            _ = c.get()
+
+        pickles = glob.glob(str(pathlib.Path(self.temp_dir.name) / '*.pickle'))
+        self.assertEqual(len(pickles), 2)
+
+        with CachedCSVFile(self.temp_csv, verbose=False) as c:
+            c.prune()
+
+        pickles = glob.glob(str(pathlib.Path(self.temp_dir.name) / '*.pickle'))
+        self.assertEqual(len(pickles), 1)
+
+    def test_remove(self):
+        with CachedCSVFile(self.temp_csv, verbose=False) as c:
+            _ = c.get()
+        self._generate_decisions_file()
+        with CachedCSVFile(self.temp_csv, verbose=False) as c:
+            _ = c.get()
+
+        with CachedCSVFile(self.temp_csv, verbose=False) as c:
+            c.remove()
+
+        pickles = glob.glob(str(pathlib.Path(self.temp_dir.name) / '*.pickle'))
+        self.assertEqual(len(pickles), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
