28df846f35cebee48e4824f9c234541bd6ddeecd
Andrea Grandi <agrandi@google.com>
2022-04-18 11:35:06 -0700

    fpstudy: Add script to filter logs for FPS study
    Add a script to generate system logs and filter the events that are
    relevant to the FPS UX study for co-located power button and fingerprint
    sensor.
    
    BUG=b:229310488
    TEST=On Chromebook: bash filtered_logs.sh
    
    Change-Id: Ie5927164ccbb070dc5313b10bcb002d82a01c24d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3591276
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Andrea Grandi <agrandi@google.com>
    Commit-Queue: Andrea Grandi <agrandi@google.com>

---
+++ biod/study/power-button/README.md
@@ -0,0 +1,6 @@
+# Power-button FPS Study
+
+This folder contains scripts and resources used for the "power-button study".
+
+The goal is to analyze the interaction between co-located fingerprint sensor
+and power button.
---
+++ biod/study/power-button/filtered_logs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Generate ChromeOS system logs and filter events for FPS UX Study
+#
+# Usage:
+# - Copy the script in the Downloads folder
+# - Press Ctrl + Alt + T to open ChromeOS Developer Shell with
+# - Run `shell` to open the command line shell
+# - Open the Downloads folder: `cd /home/user/*/Downloads`
+# - Run the script: `bash filtered_logs.sh`
+#
+# Save output on a file:
+#   bash filtered_logs.sh > filtered.log
+#
+# Log custom events:
+#   logger FpsUxStudy <custom message>
+#
+# Notes:
+# - Edit the list of patterns below to filter different events
+# - Save both the **full** and filtered system logs
+
+PATTERNS_FILENAME=log_patterns.txt
+
+cat <<\EOF >"${PATTERNS_FILENAME}"
+Power button down
+Power button up
+Shutting down
+all displays off
+all displays on
+Turning screen
+imming screen
+StartEnrollSession
+EndEnrollSession
+DoEnrollImageEvent
+Enrolling
+Enroll =>
+StartAuthSession
+EndAuthSession
+DoMatchEvent result
+Capturing
+Matching
+Match =>
+FpsUxStudy
+EOF
+
+LOG_FILENAME=cros_$(date --iso-8601=seconds).log
+generate_logs --compress=false --output="${LOG_FILENAME}"
+
+grep --text -f "${PATTERNS_FILENAME}" "${LOG_FILENAME}" | sort | uniq
+
+rm "${PATTERNS_FILENAME}"
