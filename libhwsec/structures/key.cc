c3effe5eb5ccf0f4b575af128e0a67b28faea47f
Yi Chou <yich@google.com>
2022-04-19 16:24:37 +0800

    libhwsec: Add the implementation of scoped key
    The scoped key would be able to flush itself when it destructed.
    
    BUG=b:174816474
    TEST=Build OK.
    
    Change-Id: Idefbbd664fe46860af9e99e995917e4184b4bd88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3592087
    Tested-by: Yi Chou <yich@google.com>
    Commit-Queue: Yi Chou <yich@google.com>
    Reviewed-by: Leo Lai <cylai@google.com>

---
+++ libhwsec/structures/key.cc
@@ -0,0 +1,54 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <optional>
+#include <utility>
+
+#include <libhwsec-foundation/status/status_chain_macros.h>
+
+#include "libhwsec/backend/backend.h"
+#include "libhwsec/middleware/middleware.h"
+#include "libhwsec/structures/key.h"
+
+namespace hwsec {
+
+ScopedKey::ScopedKey(ScopedKey&& scoped_key)
+    : key_(std::move(scoped_key.key_)),
+      middleware_derivative_(std::move(scoped_key.middleware_derivative_)) {
+  scoped_key.key_ = std::nullopt;
+}
+
+ScopedKey::~ScopedKey() {
+  Invalidate();
+}
+
+void ScopedKey::Invalidate() {
+  if (key_.has_value()) {
+    Key key = GetKey();
+    key_ = std::nullopt;
+    RETURN_IF_ERROR(Middleware(middleware_derivative_)
+                        .CallSync<&hwsec::Backend::KeyManagerment::Flush>(key))
+        .With([](auto linker) {
+          return linker.LogError() << "Failed to flush scoped key";
+        })
+        .ReturnVoid();
+  }
+}
+
+ScopedKey& ScopedKey::operator=(ScopedKey&& scoped_key) {
+  Invalidate();
+  key_ = std::move(scoped_key.key_);
+  middleware_derivative_ = std::move(scoped_key.middleware_derivative_);
+  scoped_key.key_ = std::nullopt;
+  return *this;
+}
+
+ScopedKey::ScopedKey(Key key, MiddlewareDerivative middleware_derivative)
+    : key_(key), middleware_derivative_(std::move(middleware_derivative)) {}
+
+const Key& ScopedKey::GetKey() const {
+  return key_.value();
+}
+
+}  // namespace hwsec
