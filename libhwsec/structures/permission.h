d9f38bb178920876f190b49aa1a04ada664302d1
Yi Chou <yich@google.com>
2022-04-19 15:12:27 +0800

    libhwsec: Add structures
    The directory contains some important structures that would be useful to
    abstract the communication between client and libhwsec.
    
    BUG=b:174816474
    TEST=Build OK.
    
    Change-Id: I28db1d1c42728e4d29ea99313880215d50313e7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3592161
    Reviewed-by: Leo Lai <cylai@google.com>
    Commit-Queue: Yi Chou <yich@google.com>
    Tested-by: Yi Chou <yich@google.com>

---
+++ libhwsec/structures/permission.h
@@ -0,0 +1,22 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIBHWSEC_STRUCTURES_PERMISSION_H_
+#define LIBHWSEC_STRUCTURES_PERMISSION_H_
+
+#include <optional>
+
+#include <brillo/secure_blob.h>
+
+#include "libhwsec/structures/device_config.h"
+
+namespace hwsec {
+
+struct Permission {
+  std::optional<brillo::SecureBlob> auth_value;
+};
+
+}  // namespace hwsec
+
+#endif  // LIBHWSEC_STRUCTURES_PERMISSION_H_
---
+++ libhwsec/structures/space.h
@@ -0,0 +1,29 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIBHWSEC_STRUCTURES_SPACE_H_
+#define LIBHWSEC_STRUCTURES_SPACE_H_
+
+namespace hwsec {
+
+enum class Space {
+  kFirmwareManagementParameters,
+  kPlatformFirmwareManagementParameters,
+  kInstallAttribute,
+  kBootlockbox,
+};
+
+enum class RoSpace {
+  kEndorsementRsaCert,
+  kEndorsementEccCert,
+  kBoardId,
+  kSNData,
+  kG2fCert,
+  kRsuDeviceId,
+  kRmaBytes,
+};
+
+}  // namespace hwsec
+
+#endif  // LIBHWSEC_STRUCTURES_SPACE_H_
