a7292f350cfe58038921ad2c7ee1026cc8573fcf
Eric Caruso <ejcaruso@chromium.org>
2022-04-27 14:02:34 -0400

    hermes: inline sgp_22.cc into sgp_22.h
    We can mark std::array as constexpr, obviating the need for
    an extern definition.
    
    BUG=None
    TEST=build, unit tests, CQ
    
    Change-Id: Iac3ffc1d331b5b2069b2bcbf7eea7847d01309e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3611080
    Tested-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Pavan Holla <pholla@google.com>
    Commit-Queue: Eric Caruso <ejcaruso@chromium.org>
    Auto-Submit: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Pavan Holla <pholla@google.com>

--- hermes/sgp_22.h
+++ hermes/sgp_22.h
@@ -14,9 +14,16 @@ constexpr uint8_t kLpaTerminalCapabilityTag = 0x83;
 constexpr uint8_t kLpaTerminalCapabilityValue = 0x07;
 
 // Application identifier for the eUICC's ISD-R, as per SGP.02 2.2.3
-extern const std::array<uint8_t, 16> kAidIsdr;
+constexpr std::array<uint8_t, 16> kAidIsdr = {
+    0xA0, 0x00, 0x00, 0x05, 0x59, 0x10, 0x10, 0xFF,
+    0xFF, 0xFF, 0xFF, 0x89, 0x00, 0x00, 0x01, 0x00,
+};
+
 // Application identifier for the eUICC's ECASD, as per SGP.02 2.2.3
-extern const std::array<uint8_t, 16> kAidEcasd;
+constexpr std::array<uint8_t, 16> kAidEcasd = {
+    0xA0, 0x00, 0x00, 0x05, 0x59, 0x10, 0x10, 0xFF,
+    0xFF, 0xFF, 0xFF, 0x89, 0x00, 0x00, 0x02, 0x00,
+};
 
 }  // namespace hermes
 
