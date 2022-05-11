e03a0103cd3653f70e96b174c5e02e773724f1e9
Chung-Sheng Wu <chungsheng@google.com>
2022-05-11 17:06:36 +0800

    diagnostics: Refactor MojoRelay
    * Remove MockMojoRelay because we can just set a remote to a fake
      implementation so this is not needed.
    * Remove MojoRelayImpl and put implementation in MojoRelay.
    * Move initialize code from the constructor to prevent the
      mocked_context calling mojo functions (some unittest doesn't
      initialize mojo.
    * Use |mojo::FusePipes()|.
    
    BUG=b:222405345
    TEST=FEATURES=test emerge-zork diagnostics
    
    Change-Id: I51fcdde55ec3f0cc79c934ba4630f31426abe7e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3641212
    Tested-by: Chung-Sheng Wu <chungsheng@google.com>
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Commit-Queue: Chung-Sheng Wu <chungsheng@google.com>

--- diagnostics/cros_healthd/utils/mojo_relay.h
+++ diagnostics/cros_healthd/utils/mojo_relay.h
@@ -5,32 +5,50 @@
 #ifndef DIAGNOSTICS_CROS_HEALTHD_UTILS_MOJO_RELAY_H_
 #define DIAGNOSTICS_CROS_HEALTHD_UTILS_MOJO_RELAY_H_
 
-#include <mojo/public/cpp/bindings/pending_remote.h>
+#include <memory>
+#include <utility>
+
+#include <mojo/public/cpp/bindings/remote.h>
+#include <mojo/public/cpp/bindings/receiver.h>
 
 namespace diagnostics {
-// MojoRelay can adapt a mojo::Remote to another mojo::Remote. In cases we
-// cannot create a remote directly (e.g. retrieving a remote from another
-// interface), this can be used as a remote object so all the method calls can
-// be queued until the interface is connected.
-// This is done by creating a proxy mojo remote/recevier pair. The proxy mojo
-// recevier will be bound to the real mojo remote after available. So the
-// behavior is the same as a bound mojo remote object.
+
+// MojoRelay can bind a mojo::PendingRemote to a mojo::Remote, by using
+// |mojo::FusePipes|. In cases we cannot create a remote directly (e.g.
+// retrieving a remote from another interface), this can be used as a remote
+// object so all the method calls can be queued until the interface is
+// connected.
 template <typename Interface>
 class MojoRelay {
  public:
   MojoRelay() = default;
-  virtual ~MojoRelay() = default;
-  MojoRelay(MojoRelay&) = delete;
-  MojoRelay& operator=(MojoRelay&) = delete;
+  ~MojoRelay() = default;
+  MojoRelay(const MojoRelay&) = delete;
+  MojoRelay& operator=(const MojoRelay&) = delete;
 
  public:
+  // Initialize the relay so the |remote_| can be used.
+  void InitNewPipeAndWaitForIncomingRemote() {
+    pending_receiver_ = remote_.BindNewPipeAndPassReceiver();
+  }
+
   // Returns the pointer of the Interface.
-  virtual typename Interface::Proxy_* Get() = 0;
-  // Returns true if the real remote is bound.
-  virtual bool IsBound() = 0;
+  auto Get() { return remote_.get(); }
+
   // Binds the real remote object to this relay.
-  virtual void Bind(mojo::PendingRemote<Interface> pending_remote) = 0;
+  bool Bind(mojo::PendingRemote<Interface> pending_remote) {
+    CHECK(pending_receiver_.is_valid());
+    return mojo::FusePipes(std::move(pending_receiver_),
+                           std::move(pending_remote));
+  }
+
+ private:
+  // Used to queue all the method call.
+  mojo::Remote<Interface> remote_;
+  // Keeps the connection until the real remote is set.
+  mojo::PendingReceiver<Interface> pending_receiver_;
 };
+
 }  // namespace diagnostics
 
 #endif  // DIAGNOSTICS_CROS_HEALTHD_UTILS_MOJO_RELAY_H_
