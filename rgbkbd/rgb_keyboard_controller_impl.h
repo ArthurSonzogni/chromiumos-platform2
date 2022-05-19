c1a085867fb027300d462fcbc05f1d722fbf7521
Michael Checo <michaelcheco@google.com>
2022-05-19 21:15:49 +0000

    rgbkbd: Update rainbow mode mapping for IndividualKey
    BUG=b:214077425
    TEST=cros_workon_make --board=${BOARD} chromeos-base/rgbkbd --test
    
    Change-Id: I9fb7b5ac6b9e2af79403586e68088ee22bea3c5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3656309
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Tested-by: Michael Checo <michaelcheco@google.com>
    Auto-Submit: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>

--- rgbkbd/rgb_keyboard_controller_impl.h
+++ rgbkbd/rgb_keyboard_controller_impl.h
@@ -56,22 +56,93 @@ static constexpr Color kRainbowLightBlue =
 static constexpr Color kRainbowIndigo = Color(/*r=*/25, /*g=*/55, /*b=*/210);
 static constexpr Color kRainbowPurple = Color(/*r=*/132, /*g=*/32, /*b=*/180);
 
-// TODO(michaelcheco): Update values for keys.
 const KeyColor kRainbowModeIndividualKey[] = {
-    {kLeftShiftKey, kCapsLockHighlightDefault},
-    {kRightShiftKey, kCapsLockHighlightDefault},
-    {3, kRainbowRed},
-    {4, kRainbowRed},
-    {5, kRainbowYellow},
-    {6, kRainbowYellow},
-    {7, kRainbowGreen},
-    {8, kRainbowGreen},
-    {9, kRainbowLightBlue},
-    {10, kRainbowLightBlue},
-    {11, kRainbowIndigo},
-    {12, kRainbowIndigo},
-    {13, kRainbowPurple},
-    {14, kRainbowPurple},
+    {1, kRainbowRed},         // ~
+    {2, kRainbowRed},         // 1
+    {3, kRainbowYellow},      // 2
+    {4, kRainbowYellow},      // 3
+    {5, kRainbowGreen},       // 4
+    {6, kRainbowGreen},       // 5
+    {7, kRainbowGreen},       // 6
+    {8, kRainbowLightBlue},   // 7
+    {9, kRainbowLightBlue},   // 8
+    {10, kRainbowLightBlue},  // 9
+    {11, kRainbowIndigo},     // 0
+    {12, kRainbowIndigo},     // -
+    {13, kRainbowPurple},     // =
+    // Key 14 not present in this layout.
+    {15, kRainbowPurple},     // Backspace
+    {16, kRainbowRed},        // Tab
+    {17, kRainbowRed},        // Q
+    {18, kRainbowYellow},     // W
+    {19, kRainbowYellow},     // E
+    {20, kRainbowGreen},      // R
+    {21, kRainbowGreen},      // T
+    {22, kRainbowGreen},      // Y
+    {23, kRainbowLightBlue},  // U
+    {24, kRainbowLightBlue},  // I
+    {25, kRainbowIndigo},     // O
+    {26, kRainbowIndigo},     // P
+    {27, kRainbowIndigo},     // [
+    {28, kRainbowPurple},     // ]
+    {29, kRainbowPurple},     // '\'
+    {30, kRainbowRed},        // Search/Launcher
+    {31, kRainbowYellow},     // A
+    {32, kRainbowYellow},     // S
+    {33, kRainbowYellow},     // D
+    {34, kRainbowGreen},      // F
+    {35, kRainbowGreen},      // G
+    {36, kRainbowLightBlue},  // H
+    {37, kRainbowLightBlue},  // J
+    {38, kRainbowLightBlue},  // K
+    {39, kRainbowIndigo},     // L
+    {40, kRainbowIndigo},     // ;
+    {41, kRainbowIndigo},     // '
+    // Key 42 not present in this layout.
+    {43, kRainbowPurple},          // Enter
+    {kLeftShiftKey, kRainbowRed},  // Left Shift
+    // Key 45 not present in this layout.
+    {46, kRainbowYellow},     // Z
+    {47, kRainbowYellow},     // X
+    {48, kRainbowGreen},      // C
+    {49, kRainbowGreen},      // V
+    {50, kRainbowGreen},      // B
+    {51, kRainbowLightBlue},  // N
+    {52, kRainbowLightBlue},  // M
+    {53, kRainbowLightBlue},  // ,
+    {54, kRainbowIndigo},     // .
+    {55, kRainbowIndigo},     // /
+    // Key 56 not present in this layout.
+    {kRightShiftKey, kRainbowPurple},  // Right Shift
+    {58, kRainbowRed},                 // Ctrl
+    {59, kRainbowPurple},              // Power
+    {60, kRainbowYellow},              // Left Alt
+    {61, kRainbowGreen},               // Space Bar
+    {62, kRainbowLightBlue},           // Right Alt
+    // Key 63 not present in this layout.
+    {64, kRainbowIndigo},  // Right Ctrl
+    // Keys [65-78] not present in this layout.
+    {79, kRainbowIndigo},  // Left Arrow
+    // Keys [80-82] not present in this layout.
+    {83, kRainbowPurple},  // Top Arrow
+    {84, kRainbowPurple},  // Bottom Arrow
+    // Keys [85-88] not present in this layout.
+    {89, kRainbowPurple},  // Right Arrow
+    // Keys [90-109] not present in this layout.
+    {110, kRainbowRed},        // Escape
+    {111, kRainbowRed},        // T1: Back
+    {112, kRainbowYellow},     // T2: Refresh
+    {113, kRainbowYellow},     // T3: Full Screen
+    {114, kRainbowGreen},      // T4: Overview
+    {115, kRainbowGreen},      // T5: Snapshot
+    {116, kRainbowGreen},      // T6: Brightness Down
+    {117, kRainbowGreen},      // T7: Brightness Up
+    {118, kRainbowLightBlue},  // T8: RGB Backlight Off
+    {119, kRainbowLightBlue},  // T9: Play/Pause
+    {120, kRainbowLightBlue},  // T10: Mic Mute
+    {121, kRainbowIndigo},     // T11: Volume Mute
+    {122, kRainbowIndigo},     // T12: Volume Down
+    {123, kRainbowIndigo},     // T13: Volume Up
 };
 
 const KeyColor kRainbowModeFiveZone[] = {
