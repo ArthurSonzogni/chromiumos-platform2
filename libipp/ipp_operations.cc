30afe76b4c8f86471dc25e64c1409766e5ec776a
Brian Norris <briannorris@chromium.org>
2022-05-25 17:18:45 +0000

    Revert "libipp: remove attributes from get-printer-attributes"
    This reverts commit 215aa2bc62b9678288ff046d6c4867db1cae79cb.
    
    Reason for revert: broke builds/tests, related to b/233800358
    
    (Dependent CLs were effectively chumped, then reverted;
    but this still submitted)
    
    Original change's description:
    > libipp: remove attributes from get-printer-attributes
    >
    > Not all get-printer-attributes requests contain the
    > requesting-user-name, document-format or requested-attributes
    > attributes. Remove these from the get-printer-attributes struct. Note
    > that requests can still add these attributes when desired, but now they
    > won't be present in every get-printer-attributes request.
    >
    > BUG=b:175631323
    > TEST=FEATURES=test emerge-nami libipp
    >
    > Change-Id: I05f3f9afd802fba7a01261d635f58839bcfb294f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3651476
    > Tested-by: Paul Moy <pmoy@chromium.org>
    > Commit-Queue: Paul Moy <pmoy@chromium.org>
    > Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    
    Bug: b:175631323, b:233800358
    Change-Id: I9e84475bbe5712b82fd89f6c2cf151fb8a43ed49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3668527
    Tested-by: Aaron Massey <aaronmassey@google.com>
    Owners-Override: Aaron Massey <aaronmassey@google.com>
    Commit-Queue: Aaron Massey <aaronmassey@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>

--- libipp/ipp_operations.cc
+++ libipp/ipp_operations.cc
@@ -7714,12 +7714,16 @@ std::vector<const Group*> Request_Get_Printer_Attributes::GetKnownGroups()
 }
 std::vector<Attribute*>
 Request_Get_Printer_Attributes::G_operation_attributes::GetKnownAttributes() {
-  return {&attributes_charset, &attributes_natural_language, &printer_uri};
+  return {&attributes_charset,   &attributes_natural_language,
+          &printer_uri,          &requesting_user_name,
+          &requested_attributes, &document_format};
 }
 std::vector<const Attribute*>
 Request_Get_Printer_Attributes::G_operation_attributes::GetKnownAttributes()
     const {
-  return {&attributes_charset, &attributes_natural_language, &printer_uri};
+  return {&attributes_charset,   &attributes_natural_language,
+          &printer_uri,          &requesting_user_name,
+          &requested_attributes, &document_format};
 }
 const std::map<AttrName, AttrDef>
     Request_Get_Printer_Attributes::G_operation_attributes::defs_{
@@ -7727,7 +7731,13 @@ const std::map<AttrName, AttrDef>
          {AttrType::charset, InternalType::kString, false}},
         {AttrName::attributes_natural_language,
          {AttrType::naturalLanguage, InternalType::kString, false}},
-        {AttrName::printer_uri, {AttrType::uri, InternalType::kString, false}}};
+        {AttrName::printer_uri, {AttrType::uri, InternalType::kString, false}},
+        {AttrName::requesting_user_name,
+         {AttrType::name, InternalType::kStringWithLanguage, false}},
+        {AttrName::requested_attributes,
+         {AttrType::keyword, InternalType::kString, true}},
+        {AttrName::document_format,
+         {AttrType::mimeMediaType, InternalType::kString, false}}};
 Response_Get_Printer_Attributes::Response_Get_Printer_Attributes()
     : Response(Operation::Get_Printer_Attributes) {}
 std::vector<Group*> Response_Get_Printer_Attributes::GetKnownGroups() {
