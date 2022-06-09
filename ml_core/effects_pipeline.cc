10c037f5b24cdebe8f4be20c0cd4d1ec52f3ebd9
Jim Pollock <jmpollock@google.com>
2022-06-09 13:06:23 +1000

    ml_core: refactor EffectsPipeline to conceal implementation details
    The public interface currently has a bunch of implementation detail
    that's not needed to be visible to clients. Moving those things into the
    implementation file.
    
    BUG=b:232703726
    TEST=emerge-brya ml-core
    
    Change-Id: Ifabdf42578c12e34f252a56932685addeac89ef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3695841
    Reviewed-by: Tom Shafron <shafron@google.com>
    Tested-by: Jim Pollock <jmpollock@chromium.org>
    Commit-Queue: Jim Pollock <jmpollock@chromium.org>

--- ml_core/effects_pipeline.cc
+++ ml_core/effects_pipeline.cc
@@ -5,7 +5,9 @@
 #include "ml_core/effects_pipeline.h"
 
 #include <utility>
+#include <optional>
 
+#include <base/scoped_native_library.h>
 #include <base/files/file_path.h>
 #include <base/logging.h>
 
@@ -13,108 +15,125 @@ namespace {
 
 constexpr char kLibraryPath[] = "libcros_ml_core_internal.so";
 
+class EffectsPipelineImpl : public cros::EffectsPipeline {
+ public:
+  virtual ~EffectsPipelineImpl() {
+    if (pipeline_ && delete_fn_) {
+      delete_fn_(pipeline_);
+    }
+  }
+
+  virtual bool ProcessFrame(int64_t timestamp,
+                            const uint8_t* frame_data,
+                            uint32_t frame_width,
+                            uint32_t frame_height,
+                            uint32_t stride) {
+    frames_started_ = true;
+    return process_frame_fn_(pipeline_, timestamp, frame_data, frame_width,
+                             frame_height, stride);
+  }
+
+  virtual bool Wait() { return wait_fn_(pipeline_); }
+
+  virtual bool SetRenderedImageObserver(
+      std::unique_ptr<cros::ProcessedFrameObserver> observer) {
+    if (!frames_started_) {
+      rendered_image_observer_ = std::move(observer);
+      return true;
+    }
+    return false;
+  }
+
+ protected:
+  EffectsPipelineImpl() {}
+  bool Initialize() {
+    base::NativeLibraryOptions native_library_options;
+    base::NativeLibraryLoadError load_error;
+    native_library_options.prefer_own_symbols = true;
+    library_.emplace(base::LoadNativeLibraryWithOptions(
+        base::FilePath(kLibraryPath), native_library_options, &load_error));
+
+    if (!library_->is_valid()) {
+      LOG(ERROR) << "Pipeline library load error: " << load_error.ToString();
+      return false;
+    }
+
+    create_fn_ = reinterpret_cast<cros_ml_effects_CreateEffectsPipelineFn>(
+        library_->GetFunctionPointer("cros_ml_effects_CreateEffectsPipeline"));
+    delete_fn_ = reinterpret_cast<cros_ml_effects_DeleteEffectsPipelineFn>(
+        library_->GetFunctionPointer("cros_ml_effects_DeleteEffectsPipeline"));
+    process_frame_fn_ = reinterpret_cast<cros_ml_effects_ProcessFrameFn>(
+        library_->GetFunctionPointer("cros_ml_effects_ProcessFrame"));
+    wait_fn_ = reinterpret_cast<cros_ml_effects_WaitFn>(
+        library_->GetFunctionPointer("cros_ml_effects_Wait"));
+    set_rendered_image_observer_fn_ =
+        reinterpret_cast<cros_ml_effects_SetRenderedImageObserverFn>(
+            library_->GetFunctionPointer(
+                "cros_ml_effects_SetRenderedImageObserver"));
+
+    bool load_ok = (create_fn_ != nullptr) && (delete_fn_ != nullptr) &&
+                   (process_frame_fn_ != nullptr) && (wait_fn_ != nullptr) &&
+                   (set_rendered_image_observer_fn_ != nullptr);
+
+    if (!load_ok) {
+      LOG(ERROR) << "create_fn_" << create_fn_;
+      LOG(ERROR) << "delete_fn_" << delete_fn_;
+      LOG(ERROR) << "process_frame_fn_" << process_frame_fn_;
+      LOG(ERROR) << "wait_fn_" << wait_fn_;
+      LOG(ERROR) << "set_rendered_image_observer_fn_"
+                 << set_rendered_image_observer_fn_;
+      LOG(ERROR) << "Pipeline cannot load the expected functions";
+      return false;
+    }
+
+    pipeline_ = create_fn_();
+    LOG(INFO) << "Pipeline created";
+    set_rendered_image_observer_fn_(
+        pipeline_, this, &EffectsPipelineImpl::RenderedImageFrameHandler);
+
+    return true;
+  }
+
+ private:
+  static void RenderedImageFrameHandler(void* handler,
+                                        int64_t timestamp,
+                                        const uint8_t* frame_data,
+                                        uint32_t frame_width,
+                                        uint32_t frame_height,
+                                        uint32_t stride) {
+    EffectsPipelineImpl* pipeline = static_cast<EffectsPipelineImpl*>(handler);
+    if (pipeline->rendered_image_observer_) {
+      pipeline->rendered_image_observer_->OnFrameProcessed(
+          timestamp, frame_data, frame_width, frame_height, stride);
+    }
+  }
+
+  std::optional<base::ScopedNativeLibrary> library_;
+  cros_ml_effects_CreateEffectsPipelineFn create_fn_ = nullptr;
+  cros_ml_effects_DeleteEffectsPipelineFn delete_fn_ = nullptr;
+  cros_ml_effects_ProcessFrameFn process_frame_fn_ = nullptr;
+  cros_ml_effects_WaitFn wait_fn_ = nullptr;
+  cros_ml_effects_SetRenderedImageObserverFn set_rendered_image_observer_fn_ =
+      nullptr;
+  void* pipeline_ = nullptr;
+  bool frames_started_ = false;
+
+  std::unique_ptr<cros::ProcessedFrameObserver> rendered_image_observer_;
+
+  friend class EffectsPipeline;
+};
+
 }  // namespace
 
 namespace cros {
 
 std::unique_ptr<EffectsPipeline> EffectsPipeline::Create() {
-  auto pipeline = std::unique_ptr<EffectsPipeline>(new EffectsPipeline());
+  auto pipeline =
+      std::unique_ptr<EffectsPipelineImpl>(new EffectsPipelineImpl());
   if (!pipeline->Initialize()) {
     return nullptr;
   }
   return pipeline;
-}
-
-EffectsPipeline::EffectsPipeline() {}
-
-bool EffectsPipeline::Initialize() {
-  base::NativeLibraryOptions native_library_options;
-  base::NativeLibraryLoadError load_error;
-  native_library_options.prefer_own_symbols = true;
-  library_.emplace(base::LoadNativeLibraryWithOptions(
-      base::FilePath(kLibraryPath), native_library_options, &load_error));
-
-  if (!library_->is_valid()) {
-    LOG(ERROR) << "Pipeline library load error: " << load_error.ToString();
-    return false;
-  }
-
-  create_fn_ = reinterpret_cast<cros_ml_effects_CreateEffectsPipelineFn>(
-      library_->GetFunctionPointer("cros_ml_effects_CreateEffectsPipeline"));
-  delete_fn_ = reinterpret_cast<cros_ml_effects_DeleteEffectsPipelineFn>(
-      library_->GetFunctionPointer("cros_ml_effects_DeleteEffectsPipeline"));
-  process_frame_fn_ = reinterpret_cast<cros_ml_effects_ProcessFrameFn>(
-      library_->GetFunctionPointer("cros_ml_effects_ProcessFrame"));
-  wait_fn_ = reinterpret_cast<cros_ml_effects_WaitFn>(
-      library_->GetFunctionPointer("cros_ml_effects_Wait"));
-  set_rendered_image_observer_fn_ =
-      reinterpret_cast<cros_ml_effects_SetRenderedImageObserverFn>(
-          library_->GetFunctionPointer(
-              "cros_ml_effects_SetRenderedImageObserver"));
-
-  bool load_ok = (create_fn_ != nullptr) && (delete_fn_ != nullptr) &&
-                 (process_frame_fn_ != nullptr) && (wait_fn_ != nullptr) &&
-                 (set_rendered_image_observer_fn_ != nullptr);
-
-  if (!load_ok) {
-    LOG(ERROR) << "create_fn_" << create_fn_;
-    LOG(ERROR) << "delete_fn_" << delete_fn_;
-    LOG(ERROR) << "process_frame_fn_" << process_frame_fn_;
-    LOG(ERROR) << "wait_fn_" << wait_fn_;
-    LOG(ERROR) << "set_rendered_image_observer_fn_"
-               << set_rendered_image_observer_fn_;
-    LOG(ERROR) << "Pipeline cannot load the expected functions";
-    return false;
-  }
-
-  pipeline_ = create_fn_();
-  LOG(INFO) << "Pipeline created";
-  set_rendered_image_observer_fn_(pipeline_, this,
-                                  &EffectsPipeline::RenderedImageFrameHandler);
-
-  return true;
-}
-
-EffectsPipeline::~EffectsPipeline() {
-  if (pipeline_ && delete_fn_) {
-    delete_fn_(pipeline_);
-  }
-}
-
-bool EffectsPipeline::ProcessFrame(int64_t timestamp,
-                                   const uint8_t* frame_data,
-                                   uint32_t frame_width,
-                                   uint32_t frame_height,
-                                   uint32_t stride) {
-  frames_started_ = true;
-  return process_frame_fn_(pipeline_, timestamp, frame_data, frame_width,
-                           frame_height, stride);
-}
-
-bool EffectsPipeline::Wait() {
-  return wait_fn_(pipeline_);
-}
-
-bool EffectsPipeline::SetRenderedImageObserver(
-    std::unique_ptr<ProcessedFrameObserver> observer) {
-  if (!frames_started_) {
-    rendered_image_observer_ = std::move(observer);
-    return true;
-  }
-  return false;
-}
-
-void EffectsPipeline::RenderedImageFrameHandler(void* handler,
-                                                int64_t timestamp,
-                                                const uint8_t* frame_data,
-                                                uint32_t frame_width,
-                                                uint32_t frame_height,
-                                                uint32_t stride) {
-  EffectsPipeline* pipeline = static_cast<EffectsPipeline*>(handler);
-  if (pipeline->rendered_image_observer_) {
-    pipeline->rendered_image_observer_->OnFrameProcessed(
-        timestamp, frame_data, frame_width, frame_height, stride);
-  }
 }
 
 }  // namespace cros
--- ml_core/effects_pipeline.h
+++ ml_core/effects_pipeline.h
@@ -5,14 +5,10 @@
 #ifndef ML_CORE_EFFECTS_PIPELINE_H_
 #define ML_CORE_EFFECTS_PIPELINE_H_
 
-#include <base/scoped_native_library.h>
 #include <brillo/brillo_export.h>
 
-#include <stdint.h>
-
 #include <memory>
-#include <optional>
-#include <string>
+#include <stdint.h>
 
 #include "bindings/effects_pipeline_bindings.h"
 
@@ -35,6 +31,8 @@ class BRILLO_EXPORT ProcessedFrameObserver {
 class BRILLO_EXPORT EffectsPipeline {
  public:
   virtual ~EffectsPipeline();
+
+  // Create an instance of the Pipeline.
   static std::unique_ptr<EffectsPipeline> Create();
 
   // Queue an input frame (ImageFormat::SRGB / RG24 / RGB888) for processing.
@@ -42,37 +40,15 @@ class BRILLO_EXPORT EffectsPipeline {
                             const uint8_t* frame_data,
                             uint32_t frame_width,
                             uint32_t frame_height,
-                            uint32_t stride);
+                            uint32_t stride) = 0;
 
   // Wait until all the queued frames are processed.
-  virtual bool Wait();
+  virtual bool Wait() = 0;
 
   // Sets an observer for receiving the final rendered image. Must be called
   // before ProcessFrame. Takes ownership of the observer.
   virtual bool SetRenderedImageObserver(
-      std::unique_ptr<ProcessedFrameObserver> observer);
-
- private:
-  EffectsPipeline();
-  bool Initialize();
-  static void RenderedImageFrameHandler(void* handler,
-                                        int64_t timestamp,
-                                        const uint8_t* frame_data,
-                                        uint32_t frame_width,
-                                        uint32_t frame_height,
-                                        uint32_t stride);
-
-  std::optional<base::ScopedNativeLibrary> library_;
-  cros_ml_effects_CreateEffectsPipelineFn create_fn_ = nullptr;
-  cros_ml_effects_DeleteEffectsPipelineFn delete_fn_ = nullptr;
-  cros_ml_effects_ProcessFrameFn process_frame_fn_ = nullptr;
-  cros_ml_effects_WaitFn wait_fn_ = nullptr;
-  cros_ml_effects_SetRenderedImageObserverFn set_rendered_image_observer_fn_ =
-      nullptr;
-  void* pipeline_ = nullptr;
-  bool frames_started_ = false;
-
-  std::unique_ptr<ProcessedFrameObserver> rendered_image_observer_;
+      std::unique_ptr<ProcessedFrameObserver> observer) = 0;
 };
 
 }  // namespace cros
