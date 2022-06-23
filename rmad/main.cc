cfeddc16f02ce21c5d763d6a64094b964d0aaf94
Cheng-Han Yang <chenghan@google.com>
2022-06-23 20:07:14 +0800

    rmad: Extract minijail function
    Move the minijail function to another header, and remove some
    permissions:
      - /var/lib/devicesettings is no longer required after using
        hardware_verifier dbus API
      - /sys/devices only requires read permission due to calibration
        process improvement
    
    BUG=b:210075797
    TEST=FEATURES=test emerge-octopus rmad
    
    Change-Id: Ibd42f0759baef6a4712f07f45a6127e728a74dc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3726652
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Gene Chang <genechang@google.com>
    Tested-by: Cheng-Han Yang <chenghan@chromium.org>
    Commit-Queue: Cheng-Han Yang <chenghan@chromium.org>

--- rmad/main.cc
+++ rmad/main.cc
@@ -2,97 +2,32 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <sys/capability.h>
-#include <sys/mount.h>
-
 #include <base/logging.h>
 #include <base/task/thread_pool/thread_pool_instance.h>
 #include <brillo/flag_helper.h>
 #include <brillo/syslog_logging.h>
-#include <libminijail.h>
-#include <scoped_minijail.h>
 
 #include "rmad/daemon/dbus_service.h"
 #include "rmad/interface/rmad_interface_impl.h"
+#include "rmad/minijail/minijail_configuration.h"
 #include "rmad/utils/crossystem_utils_impl.h"
 
 namespace {
 
-constexpr char kRmadUser[] = "rmad";
-constexpr char kRmadGroup[] = "rmad";
-constexpr char kSeccompFilterPath[] = "/usr/share/policy/rmad-seccomp.policy";
-
-}  // namespace
-
-void EnterMinijail() {
-  ScopedMinijail j(minijail_new());
-  minijail_no_new_privs(j.get());
-  minijail_remount_proc_readonly(j.get());
-  minijail_namespace_ipc(j.get());
-  minijail_namespace_net(j.get());
-  minijail_namespace_uts(j.get());
-  minijail_namespace_vfs(j.get());
-
-  minijail_change_user(j.get(), kRmadUser);
-  minijail_change_group(j.get(), kRmadGroup);
-  minijail_inherit_usergroups(j.get());
-
-  minijail_enter_pivot_root(j.get(), "/mnt/empty");
-
-  minijail_mount_tmp(j.get());
-  minijail_bind(j.get(), "/", "/", 0);
-  minijail_bind(j.get(), "/dev/", "/dev", 0);
-  minijail_bind(j.get(), "/proc", "/proc", 0);
-
-  minijail_remount_mode(j.get(), MS_SLAVE);
-  minijail_mount(j.get(), "/media", "/media", "none", MS_BIND | MS_REC);
-
-  minijail_mount_with_data(j.get(), "tmpfs", "/run", "tmpfs", 0, nullptr);
-  minijail_bind(j.get(), "/run/chromeos-config/v1", "/run/chromeos-config/v1",
-                0);
-  minijail_bind(j.get(), "/run/dbus", "/run/dbus", 0);
-  minijail_bind(j.get(), "/run/lock", "/run/lock", 1);
-
-  minijail_mount_with_data(j.get(), "tmpfs", "/var", "tmpfs", 0, nullptr);
-  minijail_bind(j.get(), "/var/lib/devicesettings", "/var/lib/devicesettings",
-                0);
-  minijail_bind(j.get(), "/var/lib/metrics/structured",
-                "/var/lib/metrics/structured", 1);
-  minijail_bind(j.get(), "/var/lib/rmad", "/var/lib/rmad", 1);
-  minijail_bind(j.get(), "/var/log", "/var/log", 0);
-
-  minijail_mount_with_data(j.get(), "tmpfs", "/sys", "tmpfs", 0, nullptr);
-  minijail_bind(j.get(), "/sys/devices", "/sys/devices", 1);
-  minijail_bind(j.get(), "/sys/class", "/sys/class", 0);
-  minijail_bind(j.get(), "/sys/bus", "/sys/bus", 0);
-
-  minijail_bind(j.get(), "/usr/share/vboot", "/usr/share/vboot", 0);
-
-  minijail_mount_with_data(j.get(), "tmpfs", "/mnt/stateful_partition", "tmpfs",
-                           0, nullptr);
-  minijail_bind(j.get(), "/mnt/stateful_partition/unencrypted/rma-data",
-                "/mnt/stateful_partition/unencrypted/rma-data", 1);
-  minijail_bind(j.get(), "/mnt/stateful_partition/unencrypted/preserve",
-                "/mnt/stateful_partition/unencrypted/preserve", 0);
-
+void CheckWriteProtectAndEnterMinijail() {
+  bool set_admin_caps = false;
   rmad::CrosSystemUtilsImpl crossystem_utils;
   int hwwp_status;
   if (crossystem_utils.GetHwwpStatus(&hwwp_status) && hwwp_status == 0) {
     VLOG(1) << "Hardware write protection off.";
-    minijail_use_caps(j.get(), CAP_TO_MASK(CAP_SYS_RAWIO) |
-                                   CAP_TO_MASK(CAP_DAC_OVERRIDE) |
-                                   CAP_TO_MASK(CAP_SYS_ADMIN));
-    minijail_set_ambient_caps(j.get());
-    minijail_bind(j.get(), "/dev/mem", "/dev/mem", 0);
+    set_admin_caps = true;
   } else {
     VLOG(1) << "Hardware write protection on.";
   }
-
-  minijail_use_seccomp_filter(j.get());
-  minijail_parse_seccomp_filters(j.get(), kSeccompFilterPath);
-
-  minijail_enter(j.get());
+  rmad::EnterMinijail(set_admin_caps);
 }
+
+}  // namespace
 
 int main(int argc, char* argv[]) {
   brillo::InitLog(brillo::kLogToSyslog | brillo::kLogToStderrIfTty);
@@ -100,7 +35,7 @@ int main(int argc, char* argv[]) {
 
   VLOG(1) << "Starting Chrome OS RMA Daemon.";
 
-  EnterMinijail();
+  CheckWriteProtectAndEnterMinijail();
 
   base::ThreadPoolInstance::CreateAndStartWithDefaultParams("rmad_thread_pool");
 
---
+++ rmad/minijail/minijail_configuration.cc
@@ -0,0 +1,103 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "rmad/minijail/minijail_configuration.h"
+
+#include <sys/capability.h>
+#include <sys/mount.h>
+
+#include <libminijail.h>
+#include <scoped_minijail.h>
+
+namespace rmad {
+
+namespace {
+
+constexpr char kRmadUser[] = "rmad";
+constexpr char kRmadGroup[] = "rmad";
+constexpr char kSeccompFilterPath[] = "/usr/share/policy/rmad-seccomp.policy";
+
+}  // namespace
+
+void EnterMinijail(bool set_admin_caps) {
+  ScopedMinijail j(minijail_new());
+  minijail_no_new_privs(j.get());
+  minijail_remount_proc_readonly(j.get());
+  minijail_namespace_ipc(j.get());
+  minijail_namespace_net(j.get());
+  minijail_namespace_uts(j.get());
+  minijail_namespace_vfs(j.get());
+
+  minijail_change_user(j.get(), kRmadUser);
+  minijail_change_group(j.get(), kRmadGroup);
+  minijail_inherit_usergroups(j.get());
+
+  minijail_enter_pivot_root(j.get(), "/mnt/empty");
+
+  minijail_mount_tmp(j.get());
+  minijail_bind(j.get(), "/", "/", 0);
+  minijail_bind(j.get(), "/dev/", "/dev", 0);
+  minijail_bind(j.get(), "/proc", "/proc", 0);
+
+  // TODO(b/241859099): Not needed after we use the executor to copy the
+  // firmware updater and remove cros-disks dependency.
+  minijail_remount_mode(j.get(), MS_SLAVE);
+  minijail_mount(j.get(), "/media", "/media", "none", MS_BIND | MS_REC);
+
+  minijail_mount_with_data(j.get(), "tmpfs", "/run", "tmpfs", 0, nullptr);
+  // Required to read cros_config.
+  minijail_bind(j.get(), "/run/chromeos-config/v1", "/run/chromeos-config/v1",
+                0);
+  // Required for using D-Bus.
+  minijail_bind(j.get(), "/run/dbus", "/run/dbus", 0);
+  // Required by |vpd| utility.
+  minijail_bind(j.get(), "/run/lock", "/run/lock", 1);
+
+  minijail_mount_with_data(j.get(), "tmpfs", "/var", "tmpfs", 0, nullptr);
+  // Required to write structured metrics.
+  minijail_bind(j.get(), "/var/lib/metrics/structured",
+                "/var/lib/metrics/structured", 1);
+  // Required to access rmad working directory.
+  minijail_bind(j.get(), "/var/lib/rmad", "/var/lib/rmad", 1);
+  // Required to read system logs.
+  minijail_bind(j.get(), "/var/log", "/var/log", 0);
+
+  minijail_mount_with_data(j.get(), "tmpfs", "/sys", "tmpfs", 0, nullptr);
+  // Required to read HWWP GPIO and sensor attributes.
+  minijail_bind(j.get(), "/sys/devices", "/sys/devices", 0);
+  // Required to read HWWP GPIO and sensor attributes.
+  minijail_bind(j.get(), "/sys/class", "/sys/class", 0);
+  // Required to read VPD and sensor attributes.
+  minijail_bind(j.get(), "/sys/bus", "/sys/bus", 0);
+
+  // Required for get_gbb_flags.sh and set_gbb_flags.sh.
+  minijail_bind(j.get(), "/usr/share/vboot", "/usr/share/vboot", 0);
+
+  minijail_mount_with_data(j.get(), "tmpfs", "/mnt/stateful_partition", "tmpfs",
+                           0, nullptr);
+  // Required to write rmad state file.
+  minijail_bind(j.get(), "/mnt/stateful_partition/unencrypted/rma-data",
+                "/mnt/stateful_partition/unencrypted/rma-data", 1);
+  // Required to read powerwash_count.
+  minijail_bind(j.get(), "/mnt/stateful_partition/unencrypted/preserve",
+                "/mnt/stateful_partition/unencrypted/preserve", 0);
+
+  if (set_admin_caps) {
+    minijail_use_caps(j.get(), CAP_TO_MASK(CAP_SYS_RAWIO) |
+                                   CAP_TO_MASK(CAP_DAC_OVERRIDE) |
+                                   CAP_TO_MASK(CAP_SYS_ADMIN));
+    minijail_set_ambient_caps(j.get());
+    // Required to read (not even write) VPD, but only accessible with the
+    // capabilities above.
+    // TODO(chenghan): Can we move VPD to executor?
+    minijail_bind(j.get(), "/dev/mem", "/dev/mem", 0);
+  }
+
+  minijail_use_seccomp_filter(j.get());
+  minijail_parse_seccomp_filters(j.get(), kSeccompFilterPath);
+
+  minijail_enter(j.get());
+}
+
+}  // namespace rmad
