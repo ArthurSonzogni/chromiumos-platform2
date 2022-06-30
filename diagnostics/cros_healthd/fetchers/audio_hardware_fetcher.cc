919f03e05233c62d46321ed97748885e2f025032
Chung-Sheng Wu <chungsheng@google.com>
2022-07-01 00:32:12 +0800

    diagnostics: Fetch audio card information
    Fetch audio card information from /proc/asound/card*.
    
    BUG=b:222405551
    TEST=FEATURES=test emerge-zork diagnostics
    
    Change-Id: I414df71805919c6ec00b80439b06f3870dbb20bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3710651
    Commit-Queue: Chung-Sheng Wu <chungsheng@google.com>
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Tested-by: Chung-Sheng Wu <chungsheng@google.com>

--- diagnostics/cros_healthd/fetchers/audio_hardware_fetcher.cc
+++ diagnostics/cros_healthd/fetchers/audio_hardware_fetcher.cc
@@ -4,13 +4,129 @@
 
 #include "diagnostics/cros_healthd/fetchers/audio_hardware_fetcher.h"
 
-#include <base/notreached.h>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include <base/files/file_enumerator.h>
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
+#include <base/strings/string_number_conversions.h>
+#include <base/strings/string_split.h>
+
+#include "diagnostics/cros_healthd/utils/error_utils.h"
+#include "diagnostics/cros_healthd/utils/file_utils.h"
 
 namespace diagnostics {
+namespace {
+
+std::tuple<mojom::HDAudioCodecPtr, mojom::ProbeErrorPtr> FetchCodec(
+    const base::FilePath& codec_file) {
+  auto codec = mojom::HDAudioCodec::New();
+  std::string codec_str;
+  if (!ReadAndTrimString(codec_file, &codec_str)) {
+    return {nullptr, mojom::ProbeError::New(
+                         mojom::ErrorType::kFileReadError,
+                         "Failed to read file: " + codec_file.value())};
+  }
+
+  base::StringPairs pairs;
+  base::SplitStringIntoKeyValuePairs(codec_str, ':', '\n', &pairs);
+  bool flag_codec = false;
+  bool flag_address = false;
+  for (const auto& key_value : pairs) {
+    std::string key;
+    base::TrimWhitespaceASCII(key_value.first, base::TRIM_ALL, &key);
+    std::string value;
+    base::TrimWhitespaceASCII(key_value.second, base::TRIM_ALL, &value);
+    if (key == "Codec") {
+      codec->name = value;
+      flag_codec = true;
+    }
+    if (key == "Address") {
+      unsigned num;
+      if (!base::StringToUint(value, &num) || num >= 1u << 8) {
+        return {nullptr, mojom::ProbeError::New(
+                             mojom::ErrorType::kParseError,
+                             "Failed to parse value to uint8_t: " + value)};
+      }
+      codec->address = static_cast<uint8_t>(num);
+      flag_address = true;
+    }
+  }
+  if (!flag_codec)
+    return {nullptr, mojom::ProbeError::New(mojom::ErrorType::kParseError,
+                                            "Missing field: Codec")};
+  if (!flag_address)
+    return {nullptr, mojom::ProbeError::New(mojom::ErrorType::kParseError,
+                                            "Missing field: Address")};
+  return {std::move(codec), nullptr};
+}
+
+std::tuple<mojom::AudioCardPtr, mojom::ProbeErrorPtr> FetchAudioCard(
+    const base::FilePath& asound_path) {
+  auto audio_card = mojom::AudioCard::New();
+  if (!ReadAndTrimString(asound_path, "id", &audio_card->alsa_id)) {
+    return {nullptr,
+            mojom::ProbeError::New(
+                mojom::ErrorType::kFileReadError,
+                "Failed to read file: " + asound_path.Append("id").value())};
+  }
+  base::FileEnumerator file_enum(asound_path, /*recursive=*/false,
+                                 base::FileEnumerator::FileType::FILES,
+                                 "codec#*");
+  for (auto path = file_enum.Next(); !path.empty(); path = file_enum.Next()) {
+    auto [codec, err] = FetchCodec(path);
+    if (!err.is_null()) {
+      return {nullptr,
+              WrapProbeError(std::move(err),
+                             "Failed to parse codec file" + path.value())};
+    }
+    audio_card->hd_audio_codecs.push_back(std::move(codec));
+  }
+  return {std::move(audio_card), nullptr};
+}
+
+std::tuple<std::vector<mojom::AudioCardPtr>, mojom::ProbeErrorPtr>
+FetchAudioCards(const base::FilePath& root_dir) {
+  std::vector<mojom::AudioCardPtr> audio_cards;
+
+  const base::FilePath asound_dir = root_dir.Append("proc/asound");
+  base::FileEnumerator file_enum(asound_dir, /*recursive=*/false,
+                                 base::FileEnumerator::FileType::DIRECTORIES,
+                                 "card*");
+  for (auto path = file_enum.Next(); !path.empty(); path = file_enum.Next()) {
+    auto [audio_card, err] = FetchAudioCard(path);
+    if (!err.is_null()) {
+      return {std::vector<mojom::AudioCardPtr>{},
+              WrapProbeError(std::move(err),
+                             "Failed to parse audio card " + path.value())};
+    }
+    audio_cards.push_back(std::move(audio_card));
+  }
+  return {std::move(audio_cards), nullptr};
+}
+
+mojom::AudioHardwareResultPtr FetchAudioHardwareInfoInner(Context* context) {
+  auto hardware_info = mojom::AudioHardwareInfo::New();
+  mojom::ProbeErrorPtr error;
+  std::tie(hardware_info->audio_cards, error) =
+      FetchAudioCards(context->root_dir());
+  if (!error.is_null()) {
+    LOG(ERROR) << error->msg;
+    return mojom::AudioHardwareResult::NewError(std::move(error));
+  }
+
+  return mojom::AudioHardwareResult::NewAudioHardwareInfo(
+      std::move(hardware_info));
+}
+
+}  // namespace
 
 void FetchAudioHardwareInfo(Context* context,
                             FetchAudioHardwareInfoCallback callback) {
-  NOTIMPLEMENTED();
+  std::move(callback).Run(FetchAudioHardwareInfoInner(context));
 }
 
 }  // namespace diagnostics
--- diagnostics/cros_healthd/fetchers/audio_hardware_fetcher_test.cc
+++ diagnostics/cros_healthd/fetchers/audio_hardware_fetcher_test.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include <utility>
+#include <vector>
 
 #include <base/run_loop.h>
 #include <base/test/bind.h>
@@ -16,9 +17,16 @@
 namespace diagnostics {
 namespace {
 
+constexpr char kAsoundPath[] = "/proc/asound";
+constexpr char kFakeAlsaId[] = "FakeId";
+
 class AudioHardwareFetcherTest : public BaseFileTest {
  protected:
-  void SetUp() override { SetTestRoot(mock_context_.root_dir()); }
+  void SetUp() override {
+    SetTestRoot(mock_context_.root_dir());
+    // Set id so by default we can get valid result.
+    SetFile({kAsoundPath, "card0", "id"}, kFakeAlsaId);
+  }
 
   mojom::AudioHardwareResultPtr FetchAudioHardwareInfoSync() {
     base::RunLoop run_loop;
@@ -37,6 +45,60 @@ class AudioHardwareFetcherTest : public BaseFileTest {
   base::test::SingleThreadTaskEnvironment env_;
   MockContext mock_context_;
 };
+
+TEST_F(AudioHardwareFetcherTest, FetchAudioCardsId) {
+  SetFile({kAsoundPath, "card0", "id"}, kFakeAlsaId);
+
+  auto result = FetchAudioHardwareInfoSync();
+  EXPECT_EQ(result->get_audio_hardware_info()->audio_cards[0]->alsa_id,
+            kFakeAlsaId);
+}
+
+TEST_F(AudioHardwareFetcherTest, FetchAudioCardsNoId) {
+  UnsetPath({kAsoundPath, "card0", "id"});
+
+  auto result = FetchAudioHardwareInfoSync();
+  EXPECT_TRUE(result->is_error());
+}
+
+TEST_F(AudioHardwareFetcherTest, FetchAudioCardsCodec) {
+  // Test the parser with syntax found in a real HDA codec file.
+  SetFile({kAsoundPath, "card0", "codec#2"},
+          R"CODEC(Codec: Test Codec Name
+Address: 2
+Field A: A
+  Indended Field B: B
+  Attr = Value, Attr = Value
+  Field: value: another value
+    value
+)CODEC");
+
+  auto result = FetchAudioHardwareInfoSync();
+  const auto& codec =
+      result->get_audio_hardware_info()->audio_cards[0]->hd_audio_codecs[0];
+  EXPECT_EQ(codec->name, "Test Codec Name");
+  EXPECT_EQ(codec->address, 2);
+}
+
+TEST_F(AudioHardwareFetcherTest, FetchAudioCardsCodecNoName) {
+  // Missing "Codec:" field.
+  SetFile({kAsoundPath, "card0", "codec#0"},
+          R"CODEC(Address: 0
+)CODEC");
+
+  auto result = FetchAudioHardwareInfoSync();
+  EXPECT_TRUE(result->is_error());
+}
+
+TEST_F(AudioHardwareFetcherTest, FetchAudioCardsCodecNoAddress) {
+  // Missing "Address:" field.
+  SetFile({kAsoundPath, "card0", "codec#0"},
+          R"CODEC(Codec: Test Codec Name
+)CODEC");
+
+  auto result = FetchAudioHardwareInfoSync();
+  EXPECT_TRUE(result->is_error());
+}
 
 }  // namespace
 }  // namespace diagnostics
