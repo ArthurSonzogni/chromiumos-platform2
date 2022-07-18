c7e53c3bc7a5737fa4955618cd8e9fcb26ab120e
Jimmy Gong <jimmyxgong@chromium.org>
2022-07-18 18:07:30 +0000

    rgbkbd: Add fuzzer for rgbkbd daemon
    Fuzzes the DBusAdaptor API calls with random call branching.
    
    BUG=b:214077425
    TEST=Run fuzzer
    
    Cq-Depend: chromium:3777648
    Change-Id: I0f838beb234f513e521b7de30054d605c8512bc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3773726
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Tested-by: Jimmy Gong <jimmyxgong@chromium.org>

--- rgbkbd/rgb_keyboard_controller_impl.cc
+++ rgbkbd/rgb_keyboard_controller_impl.cc
@@ -71,7 +71,6 @@ void RgbKeyboardControllerImpl::SetKeyboardClient(RgbKeyboard* keyboard) {
 
 void RgbKeyboardControllerImpl::SetKeyboardCapabilityForTesting(
     RgbKeyboardCapabilities capability) {
-  DCHECK(!capabilities_.has_value());
   capabilities_ = capability;
   if (capabilities_.value() == RgbKeyboardCapabilities::kIndividualKey) {
     PopulateRainbowModeMap();
