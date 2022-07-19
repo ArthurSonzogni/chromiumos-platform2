bb3feea747d6ebff2ac715b891937546b1299785
Jack Rosenthal <jrosenth@chromium.org>
2022-07-19 11:21:41 -0600

    vm_tools: Format Python with Black
    Format all Python files with Black, which is being enabled across
    platform2.
    
    This CL was generated by running the command:
    
       black --config ~/chromiumos/src/repohooks/pyproject.toml vm_tools
    
    BUG=b:239567381
    TEST=none
    
    Change-Id: I138b8fcfbf81bddcd0677010c5212cd9bef04d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3774333
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Alex Klein <saklein@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Fergus Dall <sidereal@google.com>

--- vm_tools/cros_im/test/run_tests.py
+++ vm_tools/cros_im/test/run_tests.py
@@ -20,50 +20,59 @@ import os
 import subprocess
 from typing import List, Optional
 
-TEST_BINARY = './cros_im_tests'
+TEST_BINARY = "./cros_im_tests"
+
 
 def verify_in_build_directory() -> bool:
     if not os.path.isfile(TEST_BINARY):
-        print(f'Could not find {TEST_BINARY}. '
-              'This script should be run from a cros_im build directory.')
+        print(
+            f"Could not find {TEST_BINARY}. "
+            "This script should be run from a cros_im build directory."
+        )
         return False
     return True
 
+
 def set_up_immodules_cache() -> None:
-    with open('test_immodules.cache', 'w') as f:
-        subprocess.call([
-            '/usr/lib/x86_64-linux-gnu/libgtk-3-0/gtk-query-immodules-3.0',
-            'libim_test_cros_gtk.so'
-            ], stdout=f)
+    with open("test_immodules.cache", "w") as f:
+        subprocess.call(
+            [
+                "/usr/lib/x86_64-linux-gnu/libgtk-3-0/gtk-query-immodules-3.0",
+                "libim_test_cros_gtk.so",
+            ],
+            stdout=f,
+        )
+
 
 def get_test_names(test_filter: Optional[str]) -> List[str]:
-    args = [TEST_BINARY, '--gtest_list_tests']
+    args = [TEST_BINARY, "--gtest_list_tests"]
     if test_filter is not None:
-        args.append(f'--gtest_filter={test_filter}')
+        args.append(f"--gtest_filter={test_filter}")
     stdout = subprocess.check_output(args)
-    lines = stdout.decode().strip().split('\n')
+    lines = stdout.decode().strip().split("\n")
     result = []
-    assert lines[0].startswith('Running main() from ')
+    assert lines[0].startswith("Running main() from ")
     # The output of --gtest_list_tests is formatted like:
     # GroupName1.
     #     TestName1
     #     TestName2
     for line in lines[1:]:
-        if line.endswith('.'):
+        if line.endswith("."):
             group = line
         else:
-            assert line.startswith('  ')
+            assert line.startswith("  ")
             result.append(group + line.strip())
 
     return result
 
+
 def run_gtk3_wayland_tests(test_filter: Optional[str]) -> None:
     env_override = {
-        'GTK_IM_MODULE_FILE': 'test_immodules.cache',
-        'GTK_IM_MODULE': 'test-cros',
-        'GDK_BACKEND': 'wayland',
+        "GTK_IM_MODULE_FILE": "test_immodules.cache",
+        "GTK_IM_MODULE": "test-cros",
+        "GDK_BACKEND": "wayland",
     }
-    env_override_str = ' '.join(f'{k}={v}' for k, v in env_override.items())
+    env_override_str = " ".join(f"{k}={v}" for k, v in env_override.items())
     env = os.environ.copy()
     env.update(env_override)
 
@@ -72,17 +81,21 @@ def run_gtk3_wayland_tests(test_filter: Optional[str]) -> None:
     successes = []
     failures = []
     for test in get_test_names(test_filter):
-        args = [TEST_BINARY, f'--gtest_filter={test}']
-        print('='*80)
+        args = [TEST_BINARY, f"--gtest_filter={test}"]
+        print("=" * 80)
         print(f'Running: {env_override_str} {" ".join(args)}')
         try:
-            completed_process = subprocess.run(args, stdout=subprocess.PIPE,
-                                               stderr=subprocess.STDOUT,
-                                               timeout=timeout_s, check=True,
-                                               env=env)
+            completed_process = subprocess.run(
+                args,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                timeout=timeout_s,
+                check=True,
+                env=env,
+            )
             output = completed_process.stdout.decode()
             print(output)
-            success = 'BACKEND ERROR: ' not in output
+            success = "BACKEND ERROR: " not in output
         except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
             print(e.output.decode())
             print(e)
@@ -93,26 +106,28 @@ def run_gtk3_wayland_tests(test_filter: Optional[str]) -> None:
         else:
             failures.append(test)
 
-    print('='*80)
+    print("=" * 80)
     if successes:
-        print('Passed:')
+        print("Passed:")
         for test in successes:
-            print(f'- {test}')
+            print(f"- {test}")
     if failures:
-        print('Failed:')
+        print("Failed:")
         for test in failures:
-            print(f'- {test}')
+            print(f"- {test}")
+
 
 def main() -> None:
     if not verify_in_build_directory():
         return
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--gtest_filter', help='Restrict test cases run')
+    parser.add_argument("--gtest_filter", help="Restrict test cases run")
     args = parser.parse_args()
 
     set_up_immodules_cache()
     run_gtk3_wayland_tests(args.gtest_filter)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- vm_tools/sommelier/scripts/get_frame_summary.py
+++ vm_tools/sommelier/scripts/get_frame_summary.py
@@ -18,6 +18,7 @@ _FP_ERROR = 0.01
 
 class EventType(Enum):
     """Wayland event type."""
+
     COMMIT = 1
     ATTACH = 2
     RELEASE = 3
@@ -26,6 +27,7 @@ class EventType(Enum):
 
 class EventInfo(NamedTuple):
     """Stores information of an event."""
+
     event_type: EventType
     surface_id: int
     buffer_id: int
@@ -34,14 +36,14 @@ class EventInfo(NamedTuple):
 
 def parse_event_type(event_type):
     EVENT_MAP = {
-        'a': EventType.ATTACH,
-        'c': EventType.COMMIT,
-        'r': EventType.RELEASE,
+        "a": EventType.ATTACH,
+        "c": EventType.COMMIT,
+        "r": EventType.RELEASE,
     }
     return EVENT_MAP.get(event_type, EventType.UNKNOWN)
 
 
-class FrameLog():
+class FrameLog:
     """Manages access to the Sommelier timing logs."""
 
     def __init__(self, filename):
@@ -56,16 +58,16 @@ class FrameLog():
         """
         self.frame_log = []
         self.surfaces = set()
-        with open(filename, 'r') as f:
+        with open(filename, "r") as f:
             lines = f.read().splitlines()
             total_delta_time = 0
-            last_line = lines[-1].split(' ')
-            if len(last_line) != 3 or last_line[0] != 'EndTime':
-                print(f'Invalid EndTime: {lines[-1]}')
+            last_line = lines[-1].split(" ")
+            if len(last_line) != 3 or last_line[0] != "EndTime":
+                print(f"Invalid EndTime: {lines[-1]}")
                 return
             self.end_time = float(last_line[2])
             for l in reversed(lines[1:-1]):
-                line = l.rstrip().split(' ')
+                line = l.rstrip().split(" ")
                 # Skip parsing line that is improperly formatted
                 if len(line) != 4:
                     continue
@@ -75,7 +77,7 @@ class FrameLog():
                     event_type=parse_event_type(line[0]),
                     surface_id=surface_id,
                     buffer_id=int(line[2]),
-                    time=self.end_time - total_delta_time
+                    time=self.end_time - total_delta_time,
                 )
                 self.frame_log.append(info)
                 self.surfaces.add(surface_id)
@@ -95,7 +97,7 @@ class FrameLog():
             # average is closer to 30 or 60.
             # TODO(asriniva): Revisit this methodology. What should a title
             # averaging 31-33 FPS target? Non 30/60 FPS targets?
-            target_fps = min([30, 60], key=lambda x: abs(x-avg_fps))
+            target_fps = min([30, 60], key=lambda x: abs(x - avg_fps))
         # Acceptable frame time ranges, based on Battlestar's metrics.
         # The +/-3 bounds do not scale with FPS (small variance for high FPS,
         # large variance for low FPS) but can account for variability in
@@ -115,14 +117,16 @@ class FrameLog():
         max_frame_ms = 0
         # only check for commit events on the given surface
         # events are in reverse chronological order
-        events = [e for e in self.frame_log if e.surface_id ==
-                  surface and e.event_type == EventType.COMMIT]
+        events = [
+            e
+            for e in self.frame_log
+            if e.surface_id == surface and e.event_type == EventType.COMMIT
+        ]
         if not events:
-            print(f'No commit events found for surface {surface}\n')
+            print(f"No commit events found for surface {surface}\n")
             return
         total_sec = self.end_time - events[-1].time
-        ft_target_ms = self.get_target_ft(target_fps,
-                                          len(events) / total_sec)
+        ft_target_ms = self.get_target_ft(target_fps, len(events) / total_sec)
         for w_sec in windows + [total_sec]:
             # num frames in acceptable range
             target_frames = 0
@@ -144,23 +148,26 @@ class FrameLog():
                 # handles floating point error in the case when
                 # w_sec == total_sec
                 if current_sec > w_sec - _FP_ERROR:
-                    print(f'Summary for last {w_sec} seconds')
-                    print('-------------------------------')
+                    print(f"Summary for last {w_sec} seconds")
+                    print("-------------------------------")
+                    print(f"FPS: {frame_count / current_sec}")
+                    print(f"Max frame time: {max_frame_ms} ms")
+                    print(f"Frame count: {frame_count} frames")
                     print(
-                        f'FPS: {frame_count / current_sec}')
-                    print(
-                        f'Max frame time: {max_frame_ms} ms')
-                    print(f'Frame count: {frame_count} frames')
-                    print(f'Percentage frames within acceptable target '
-                          f'{ft_target_ms} ms: '
-                          f'{target_frames * 100/frame_count}%')
+                        f"Percentage frames within acceptable target "
+                        f"{ft_target_ms} ms: "
+                        f"{target_frames * 100/frame_count}%"
+                    )
                     if len(frames_ms) > 1:
-                        c_var = (statistics.stdev(
-                            frames_ms) / statistics.mean(frames_ms))
-                        print(f'Coefficient of variation: {c_var}')
-                    print(f'Frames exceeding max frame time threshold'
-                          f' {max_ft_ms} ms:'
-                          f' {max_ft_events} frames')
+                        c_var = statistics.stdev(frames_ms) / statistics.mean(
+                            frames_ms
+                        )
+                        print(f"Coefficient of variation: {c_var}")
+                    print(
+                        f"Frames exceeding max frame time threshold"
+                        f" {max_ft_ms} ms:"
+                        f" {max_ft_events} frames"
+                    )
                     print()
                     break
                 prev_sec = event.time
@@ -176,15 +183,18 @@ class FrameLog():
             target_fps: Target FPS, either 30, 60, or
                         None (automatically determined).
         """
-        print(f'Sliding window aggregates for {window} events')
-        print('-------------------------------')
+        print(f"Sliding window aggregates for {window} events")
+        print("-------------------------------")
 
         # only check for commit events on the given surface
         # events are in reverse chronological order
-        events = [e for e in self.frame_log if e.surface_id ==
-                  surface and e.event_type == EventType.COMMIT]
+        events = [
+            e
+            for e in self.frame_log
+            if e.surface_id == surface and e.event_type == EventType.COMMIT
+        ]
         if not events:
-            print(f'No commit events found for surface {surface}\n')
+            print(f"No commit events found for surface {surface}\n")
             return
         # For a sliding window, aggregate the following:
         # cvars: Coefficient of variation over window
@@ -192,54 +202,78 @@ class FrameLog():
         cvars = []
         max_ft_events = []
         for i in range(window, len(events)):
-            sl_window = events[i-window:i]
-            frames_ms = [(sl_window[x].time - sl_window[x+1].time)
-                         * _MS_PER_SEC for x in range(window-1)]
+            sl_window = events[i - window : i]
+            frames_ms = [
+                (sl_window[x].time - sl_window[x + 1].time) * _MS_PER_SEC
+                for x in range(window - 1)
+            ]
 
             max_fts = sum([1 for x in frames_ms if x > max_ft_ms])
-            c_var = (statistics.stdev(
-                frames_ms) / statistics.mean(frames_ms))
+            c_var = statistics.stdev(frames_ms) / statistics.mean(frames_ms)
             cvars.append(c_var)
             max_ft_events.append(max_fts)
         if len(cvars) > 1:
-            print('Arithmetic mean of coefficient of variation:',
-                  statistics.fmean(cvars))
-            print('Geometric mean of coefficient of variation:',
-                  statistics.geometric_mean(cvars))
+            print(
+                "Arithmetic mean of coefficient of variation:",
+                statistics.fmean(cvars),
+            )
+            print(
+                "Geometric mean of coefficient of variation:",
+                statistics.geometric_mean(cvars),
+            )
         if len(max_ft_events) > 1:
-            print(f'Average number of frame time events over threshold'
-                  f' {max_ft_ms} ms:', statistics.mean(max_ft_events))
+            print(
+                f"Average number of frame time events over threshold"
+                f" {max_ft_ms} ms:",
+                statistics.mean(max_ft_events),
+            )
         print()
+
     print()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Return frame summary based on Sommelier timing log.')
+        description="Return frame summary based on Sommelier timing log."
+    )
 
-    parser.add_argument('file', help='Filename of timing log')
-    parser.add_argument('--windows', action='extend', type=int,
-                        nargs='+', help='Time windows for summary (in seconds)',
-                        default=[10, 60, 300])
-    parser.add_argument('--target-fps', type=int, help='Target FPS',
-                        default=None)
-    parser.add_argument('--max-frame-time', type=float,
-                        help='Max frame time threshold (in milliseconds)',
-                        default=200)
-    parser.add_argument('--sliding', type=int,
-                        help='Use sliding window with size in number of frames',
-                        default=300)
+    parser.add_argument("file", help="Filename of timing log")
+    parser.add_argument(
+        "--windows",
+        action="extend",
+        type=int,
+        nargs="+",
+        help="Time windows for summary (in seconds)",
+        default=[10, 60, 300],
+    )
+    parser.add_argument(
+        "--target-fps", type=int, help="Target FPS", default=None
+    )
+    parser.add_argument(
+        "--max-frame-time",
+        type=float,
+        help="Max frame time threshold (in milliseconds)",
+        default=200,
+    )
+    parser.add_argument(
+        "--sliding",
+        type=int,
+        help="Use sliding window with size in number of frames",
+        default=300,
+    )
     args = parser.parse_args()
     if args.target_fps and args.target_fps < 20:
-        parser.error('Choose target FPS above 20 FPS')
+        parser.error("Choose target FPS above 20 FPS")
     log = FrameLog(args.file)
     for s in log.surfaces:
-        print(f'Summary for surface {s}')
-        print('-------------------------------')
-        log.output_fps_sliding(s,
-                               max_ft_ms=args.max_frame_time,
-                               window=args.sliding)
-        log.output_fps(s,
-                       windows=sorted(args.windows),
-                       max_ft_ms=args.max_frame_time,
-                       target_fps=args.target_fps)
+        print(f"Summary for surface {s}")
+        print("-------------------------------")
+        log.output_fps_sliding(
+            s, max_ft_ms=args.max_frame_time, window=args.sliding
+        )
+        log.output_fps(
+            s,
+            windows=sorted(args.windows),
+            max_ft_ms=args.max_frame_time,
+            target_fps=args.target_fps,
+        )
