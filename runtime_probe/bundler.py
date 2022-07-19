60c05aac46f407272a5f20971b4fbbc7cc34dd9d
Jack Rosenthal <jrosenth@chromium.org>
2022-07-19 11:21:38 -0600

    runtime_probe: Format Python with Black
    Format all Python files with Black, which is being enabled across
    platform2.
    
    This CL was generated by running the command:
    
       black --config ~/chromiumos/src/repohooks/pyproject.toml runtime_probe
    
    BUG=b:239567381
    TEST=none
    
    Change-Id: Id26f3dde24d68c99efbd102dbdc6e881e7ee8045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3774270
    Reviewed-by: Yong Hong <yhong@chromium.org>
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

--- runtime_probe/bundler.py
+++ runtime_probe/bundler.py
@@ -17,40 +17,63 @@ from typing import List
 
 # Hard-coded the absolute path to `lddtree` because the dirname of it might
 # not be listed in the environment variable `PATH` when this script is invoked.
-_LDDTREE_BIN_PATH = '/mnt/host/source/chromite/bin/lddtree'
+_LDDTREE_BIN_PATH = "/mnt/host/source/chromite/bin/lddtree"
 
 
 def main(argv: List[str]) -> int:
-  ap = argparse.ArgumentParser(description=__doc__)
-  ap.add_argument(
-      '--root-dir',
-      required=True,
-      help='Path to the root directory containing build dependencies.')
-  ap.add_argument('--target-path',
-                  required=True,
-                  help='Path to the ELF executable binary to bundle.')
-  ap.add_argument('--bundle-description',
-                  help='The description for the bundled file.')
-  ap.add_argument('--output-path',
-                  required=True,
-                  help='Path to the output bundled file.')
-  opts = ap.parse_args(argv)
+    ap = argparse.ArgumentParser(description=__doc__)
+    ap.add_argument(
+        "--root-dir",
+        required=True,
+        help="Path to the root directory containing build dependencies.",
+    )
+    ap.add_argument(
+        "--target-path",
+        required=True,
+        help="Path to the ELF executable binary to bundle.",
+    )
+    ap.add_argument(
+        "--bundle-description", help="The description for the bundled file."
+    )
+    ap.add_argument(
+        "--output-path", required=True, help="Path to the output bundled file."
+    )
+    opts = ap.parse_args(argv)
 
-  with tempfile.TemporaryDirectory() as archive_dir:
-    subprocess.check_call([
-        _LDDTREE_BIN_PATH, '--copy-to-tree', archive_dir, '--root',
-        opts.root_dir, '--no-auto-root', '--generate-wrappers', '--bindir', '/',
-        '--verbose', opts.target_path
-    ])
+    with tempfile.TemporaryDirectory() as archive_dir:
+        subprocess.check_call(
+            [
+                _LDDTREE_BIN_PATH,
+                "--copy-to-tree",
+                archive_dir,
+                "--root",
+                opts.root_dir,
+                "--no-auto-root",
+                "--generate-wrappers",
+                "--bindir",
+                "/",
+                "--verbose",
+                opts.target_path,
+            ]
+        )
 
-    # Build the bundled file.
-    subprocess.check_call([
-        'makeself', '--license', '/dev/null', '--nox11', '--current', '--bzip2',
-        archive_dir, opts.output_path, opts.bundle_description
-    ])
+        # Build the bundled file.
+        subprocess.check_call(
+            [
+                "makeself",
+                "--license",
+                "/dev/null",
+                "--nox11",
+                "--current",
+                "--bzip2",
+                archive_dir,
+                opts.output_path,
+                opts.bundle_description,
+            ]
+        )
 
-  return 0
+    return 0
 
 
-if __name__ == '__main__':
-  sys.exit(main(sys.argv[1:]))
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
