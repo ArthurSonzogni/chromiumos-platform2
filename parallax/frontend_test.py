f90b75893ea927dbe0f27815307a964bd4bb8c09
Jack Rosenthal <jrosenth@chromium.org>
2022-07-19 11:21:36 -0600

    parallax: Format Python with Black
    Format all Python files with Black, which is being enabled across
    platform2.
    
    This CL was generated by running the command:
    
       black --config ~/chromiumos/src/repohooks/pyproject.toml parallax
    
    BUG=b:239567381
    TEST=none
    
    Change-Id: I4a80f4457c38fc6f5968ad715c8b75deb5baf76b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3774266
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Ziting Shen <zitingshen@google.com>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

--- parallax/frontend_test.py
+++ parallax/frontend_test.py
@@ -12,17 +12,17 @@ import threading
 import time
 
 
-class SamplingTest():
+class SamplingTest:
     """Stores samples and provides JSON output."""
 
     def __init__(self, names):
         num_samples = len(names)
         self.samples = []
-        self.meta = {'rowMeta': []}
+        self.meta = {"rowMeta": []}
         for i in range(num_samples):
             self.samples.append([])
-            rowMeta = {'name': names[i]}
-            self.meta['rowMeta'].append(rowMeta)
+            rowMeta = {"name": names[i]}
+            self.meta["rowMeta"].append(rowMeta)
 
     def addSamples(self, samples):
         assert len(self.samples) == len(samples)
@@ -34,41 +34,44 @@ class SamplingTest():
         if start_time:
             start = bisect.bisect(self.samples[0], start_time)
         end = len(self.samples[-1])
-        print('JSON', start_time, start, end, end-start)
+        print("JSON", start_time, start, end, end - start)
         transmit = []
         for samples in self.samples:
-            transmit.append(samples[start: end])
+            transmit.append(samples[start:end])
         data = {
-            'type': out_type,
-            'meta': self.meta,
-            'matrix': transmit,
+            "type": out_type,
+            "meta": self.meta,
+            "matrix": transmit,
         }
         return json.dumps(data)
 
-sample_container = SamplingTest(['time', 'A', 'B', 'C'])
+
+sample_container = SamplingTest(["time", "A", "B", "C"])
+
 
 class StreamingTest(server.BaseHTTPRequestHandler):
     """HTTP request handler."""
 
     def postInput(self):
-        length = int(self.headers['Content-Length'])
+        length = int(self.headers["Content-Length"])
         payload = self.rfile.read(length)
-        text = payload.decode('utf_8')
+        text = payload.decode("utf_8")
         return json.loads(text)
 
     def do_POST(self):
         data = self.postInput()
-        start_time = data.get('startTime')
+        start_time = data.get("startTime")
 
-        payload = sample_container.toJSON('streaming_chart', start_time)
-        payload = payload.encode('utf_8')
+        payload = sample_container.toJSON("streaming_chart", start_time)
+        payload = payload.encode("utf_8")
 
         self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Content-Type', 'text/plain')
-        self.send_header('Content-Length', len(payload))
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Content-Type", "text/plain")
+        self.send_header("Content-Length", len(payload))
         self.end_headers()
         self.wfile.write(payload)
+
 
 def sample_generator():
     while True:
@@ -80,11 +83,13 @@ def sample_generator():
         sample_container.addSamples([time.time()] + latest_samples)
         time.sleep(0.01)
 
+
 def start_server(port):
     threading.Thread(target=sample_generator).start()
-    with server.HTTPServer(('localhost', port), StreamingTest) as serv:
-        print('Starting Server')
+    with server.HTTPServer(("localhost", port), StreamingTest) as serv:
+        print("Starting Server")
         serv.serve_forever()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     start_server(9000)
