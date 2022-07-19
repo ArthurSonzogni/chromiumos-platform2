09c5bc2d23a4e625fdd74032e385e630e9caa0bb
Jack Rosenthal <jrosenth@chromium.org>
2022-07-19 11:21:38 -0600

    regions: Format Python with Black
    Format all Python files with Black, which is being enabled across
    platform2.
    
    This CL was generated by running the command:
    
       black --config ~/chromiumos/src/repohooks/pyproject.toml regions
    
    BUG=b:239567381
    TEST=none
    
    Change-Id: I12908084848ee8aa657c7a4178e52f77c2b7c08c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3774269
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Alex Klein <saklein@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>

--- regions/update_testdata.py
+++ regions/update_testdata.py
@@ -12,6 +12,7 @@ from __future__ import print_function
 import argparse
 import base64
 import json
+
 # pylint: disable=cros-logging-import
 import logging
 import os
@@ -23,130 +24,149 @@ import yaml
 
 
 # URLs to GIT paths.
-SRC_GIT_URL = 'https://chromium.googlesource.com/chromium/src/+/HEAD/'
+SRC_GIT_URL = "https://chromium.googlesource.com/chromium/src/+/HEAD/"
 
-TESTDATA_PATH = os.path.join(os.path.dirname(__file__), 'testdata')
+TESTDATA_PATH = os.path.join(os.path.dirname(__file__), "testdata")
 
 
 def GetChromiumSource(file_path):
-  """Gets Chromium source code by given path.
+    """Gets Chromium source code by given path.
 
-  Args:
-    file_path: The relative path to retrieve.
-  """
-  return base64.b64decode(subprocess.check_output(
-      ['curl', '-s', SRC_GIT_URL + file_path + '?format=TEXT'])).decode('utf-8')
+    Args:
+      file_path: The relative path to retrieve.
+    """
+    return base64.b64decode(
+        subprocess.check_output(
+            ["curl", "-s", SRC_GIT_URL + file_path + "?format=TEXT"]
+        )
+    ).decode("utf-8")
 
 
 def WriteTestData(name, value):
-  if not value:
-    sys.exit('No values found for %s' % name)
+    if not value:
+        sys.exit("No values found for %s" % name)
 
-  path = os.path.join(TESTDATA_PATH, name + '.yaml')
-  logging.info('%s: writing %r', path, value)
-  with open(path, 'w') as f:
-    f.write('# Automatically generated from ToT Chromium sources\n'
-            '# by update_testdata.py. Do not edit manually.\n'
-            '\n')
-    yaml.dump(value, f, default_flow_style=False)
+    path = os.path.join(TESTDATA_PATH, name + ".yaml")
+    logging.info("%s: writing %r", path, value)
+    with open(path, "w") as f:
+        f.write(
+            "# Automatically generated from ToT Chromium sources\n"
+            "# by update_testdata.py. Do not edit manually.\n"
+            "\n"
+        )
+        yaml.dump(value, f, default_flow_style=False)
 
 
 def UpdateLocales():
-  """Updates locales.
+    """Updates locales.
 
-  Valid locales are entries of the kAcceptLanguageList array in
-  l10n_util.cc <http://goo.gl/z8XsZJ>.
-  """
-  cpp_code = GetChromiumSource('ui/base/l10n/l10n_util.cc')
-  match = re.search(r'static[^\n]+kAcceptLanguageList\[\] = \{(.+?)^\}',
-                    cpp_code, re.DOTALL | re.MULTILINE)
-  if not match:
-    sys.exit('Unable to find language list')
+    Valid locales are entries of the kAcceptLanguageList array in
+    l10n_util.cc <http://goo.gl/z8XsZJ>.
+    """
+    cpp_code = GetChromiumSource("ui/base/l10n/l10n_util.cc")
+    match = re.search(
+        r"static[^\n]+kAcceptLanguageList\[\] = \{(.+?)^\}",
+        cpp_code,
+        re.DOTALL | re.MULTILINE,
+    )
+    if not match:
+        sys.exit("Unable to find language list")
 
-  locales = re.findall(r'"(.+)"', match.group(1))
-  if not locales:
-    sys.exit('Unable to parse language list')
+    locales = re.findall(r'"(.+)"', match.group(1))
+    if not locales:
+        sys.exit("Unable to parse language list")
 
-  WriteTestData('locales', sorted(locales))
+    WriteTestData("locales", sorted(locales))
 
 
 def UpdateTimeZones():
-  """Updates time zones.
+    """Updates time zones.
 
-  Valid time zones are values of the kTimeZones array in timezone_settings.cc
-  <http://goo.gl/WSVUeE>.
-  """
-  cpp_code = GetChromiumSource('ash/components/settings/timezone_settings.cc')
-  match = re.search(r'static[^\n]+kTimeZones\[\] = \{(.+?)^\}',
-                    cpp_code, re.DOTALL | re.MULTILINE)
-  if not match:
-    sys.exit('Unable to find time zones')
+    Valid time zones are values of the kTimeZones array in timezone_settings.cc
+    <http://goo.gl/WSVUeE>.
+    """
+    cpp_code = GetChromiumSource("ash/components/settings/timezone_settings.cc")
+    match = re.search(
+        r"static[^\n]+kTimeZones\[\] = \{(.+?)^\}",
+        cpp_code,
+        re.DOTALL | re.MULTILINE,
+    )
+    if not match:
+        sys.exit("Unable to find time zones")
 
-  time_zones = re.findall(r'"(.+)"', match.group(1))
-  if not time_zones:
-    sys.exit('Unable to parse time zones')
+    time_zones = re.findall(r'"(.+)"', match.group(1))
+    if not time_zones:
+        sys.exit("Unable to parse time zones")
 
-  WriteTestData('time_zones', time_zones)
+    WriteTestData("time_zones", time_zones)
 
 
 def UpdateMigrationMap():
-  """Updates the input method migration map.
+    """Updates the input method migration map.
 
-  The source is the kEngineIdMigrationMap array in input_method_util.cc
-  <https://chromium.googlesource.com/chromium/src/+/HEAD/ui/base/ime/ash/input_method_util.cc>.
-  """
-  cpp_code = GetChromiumSource('ui/base/ime/ash/input_method_util.cc')
-  match = re.search(r'kEngineIdMigrationMap\[\]\[2\] = \{(.+?)^\}',
-                    cpp_code, re.DOTALL | re.MULTILINE)
-  if not match:
-    sys.exit('Unable to find kEngineIdMigrationMap')
+    The source is the kEngineIdMigrationMap array in input_method_util.cc
+    <https://chromium.googlesource.com/chromium/src/+/HEAD/ui/base/ime/ash/input_method_util.cc>.
+    """
+    cpp_code = GetChromiumSource("ui/base/ime/ash/input_method_util.cc")
+    match = re.search(
+        r"kEngineIdMigrationMap\[\]\[2\] = \{(.+?)^\}",
+        cpp_code,
+        re.DOTALL | re.MULTILINE,
+    )
+    if not match:
+        sys.exit("Unable to find kEngineIdMigrationMap")
 
-  map_code = match.group(1)
-  migration_map = re.findall(r'{"(.+?)", "(.+?)"}', map_code)
-  if not migration_map:
-    sys.exit('Unable to parse kEngineIdMigrationMap')
+    map_code = match.group(1)
+    migration_map = re.findall(r'{"(.+?)", "(.+?)"}', map_code)
+    if not migration_map:
+        sys.exit("Unable to parse kEngineIdMigrationMap")
 
-  WriteTestData('migration_map', migration_map)
+    WriteTestData("migration_map", migration_map)
 
 
 def UpdateInputMethods():
-  """Updates input method IDs.
+    """Updates input method IDs.
 
-  This is the union of all 'id' fields in input_method/*.json
-  <http://goo.gl/z4JGvK>.
-  """
-  # entry format: 100644 blob 48de6e64885e472c6743543cc988ac0fd8edd55e    FILE
-  json_dir = 'chrome/browser/resources/chromeos/input_method'
-  files = [line.strip().split()[-1]
-           for line in GetChromiumSource(json_dir).splitlines()]
-  pattern = re.compile(r'\.json$')
-  json_files = [f for f in files if pattern.search(f)]
+    This is the union of all 'id' fields in input_method/*.json
+    <http://goo.gl/z4JGvK>.
+    """
+    # entry format: 100644 blob 48de6e64885e472c6743543cc988ac0fd8edd55e    FILE
+    json_dir = "chrome/browser/resources/chromeos/input_method"
+    files = [
+        line.strip().split()[-1]
+        for line in GetChromiumSource(json_dir).splitlines()
+    ]
+    pattern = re.compile(r"\.json$")
+    json_files = [f for f in files if pattern.search(f)]
 
-  input_methods = set()
-  for f in json_files:
-    contents = json.loads(GetChromiumSource(os.path.join(json_dir, f)))
-    for c in contents['input_components']:
-      input_methods.add(str(c['id']))
+    input_methods = set()
+    for f in json_files:
+        contents = json.loads(GetChromiumSource(os.path.join(json_dir, f)))
+        for c in contents["input_components"]:
+            input_methods.add(str(c["id"]))
 
-  WriteTestData('input_methods', sorted(input_methods))
+    WriteTestData("input_methods", sorted(input_methods))
 
 
 def main():
-  parser = argparse.ArgumentParser(
-      description=('Updates some constants in regions_unittest_data.py based '
-                   'on data pulled from Chromium sources. This overwrites '
-                   'files in testdata, which you must then submit.'))
-  unused_args = parser.parse_args()
-  logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(
+        description=(
+            "Updates some constants in regions_unittest_data.py based "
+            "on data pulled from Chromium sources. This overwrites "
+            "files in testdata, which you must then submit."
+        )
+    )
+    unused_args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
 
-  UpdateLocales()
-  UpdateTimeZones()
-  UpdateInputMethods()
-  UpdateMigrationMap()
+    UpdateLocales()
+    UpdateTimeZones()
+    UpdateInputMethods()
+    UpdateMigrationMap()
 
-  logging.info('Run "git diff %s" to see changes (if any).', TESTDATA_PATH)
-  logging.info('Make sure to submit any changes to %s!', TESTDATA_PATH)
+    logging.info('Run "git diff %s" to see changes (if any).', TESTDATA_PATH)
+    logging.info("Make sure to submit any changes to %s!", TESTDATA_PATH)
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
