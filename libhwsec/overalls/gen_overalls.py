cce6e05cc04245457d8d2f7d999cc165e8a84573
Alex Klein <saklein@chromium.org>
2022-07-20 10:37:38 -0600

    libhwsec: Updated overalls generated license string.
    The license string for new files must use ChromiumOS instead of
    Chromium OS, and should not include "All right reserved."
    
    BUG=b:230609017
    TEST=None
    
    Change-Id: Ifa5921a24b39d60c73d208889131b5e1abdd89e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3777355
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Reviewed-by: John L Chen <zuan@chromium.org>
    Tested-by: Alex Klein <saklein@chromium.org>
    Commit-Queue: Alex Klein <saklein@chromium.org>

--- libhwsec/overalls/gen_overalls.py
+++ libhwsec/overalls/gen_overalls.py
@@ -16,7 +16,7 @@ import sys
 
 _LICENSE_STRING = (
     """
-// Copyright %d The Chromium OS Authors. All rights reserved.
+// Copyright %d The ChromiumOS Authors.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 """.strip()
