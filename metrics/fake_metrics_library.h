21ef314f166d1c49a4b388e89bace09ac13e3d50
Steven Bennetts <stevenjb@chromium.org>
2022-07-27 15:52:39 +0000

    metrics: Add FakeMetricsLibrary
    The fake library will be used in unit tests to test UMA metrics
    without the need for setting complex mock expectations in advance.
    
    The code is provided as a source_set for use in tests within
    platform2.
    
    BUG=b:237118799
    TEST=P2_TEST_FILTER="FakeMetrics*" FEATURES=test emerge-$BOARD metrics
    
    Change-Id: If05902cc996d8fce9e085fad5e1ff95bd1e42ca0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3788973
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Tested-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>

---
+++ metrics/fake_metrics_library.h
@@ -0,0 +1,69 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef METRICS_FAKE_METRICS_LIBRARY_H_
+#define METRICS_FAKE_METRICS_LIBRARY_H_
+
+#include <limits>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "metrics/metrics_library.h"
+
+// FakeMetricsLibrary provides a fake MetricsLibraryInterface implementation
+// that keeps track of calls to Send*ToUMA and the values sent. It provides
+// additional getters allowing tests to examine the results. The implementation
+// is intentionally minimal, please extend as needed.
+class FakeMetricsLibrary : public MetricsLibraryInterface {
+ public:
+  FakeMetricsLibrary() = default;
+  ~FakeMetricsLibrary() = default;
+  FakeMetricsLibrary(const FakeMetricsLibrary&) = delete;
+  FakeMetricsLibrary& operator=(const FakeMetricsLibrary&) = delete;
+
+  // MetricsLibraryInterface
+  void Init() override;
+  bool AreMetricsEnabled() override;
+  bool IsGuestMode() override;
+  bool SendToUMA(const std::string& name,
+                 int sample,
+                 int min,
+                 int max,
+                 int nbuckets) override;
+  bool SendEnumToUMA(const std::string& name,
+                     int sample,
+                     int exclusive_max) override;
+  bool SendLinearToUMA(const std::string& name, int sample, int max) override;
+  bool SendPercentageToUMA(const std::string& name, int sample) override;
+  bool SendBoolToUMA(const std::string& name, bool sample) override;
+  bool SendSparseToUMA(const std::string& name, int sample) override;
+
+  // The following MetricsLibraryInterface methods are not implemented:
+  bool SendUserActionToUMA(const std::string& action) override;
+  bool SendCrashToUMA(const char* crash_kind) override;
+  bool SendCrosEventToUMA(const std::string& event) override;
+#if USE_METRICS_UPLOADER
+  bool SendRepeatedToUMA(const std::string& name,
+                         int sample,
+                         int min,
+                         int max,
+                         int nbuckets,
+                         int num_samples) override;
+#endif
+  void SetOutputFile(const std::string& output_file) override;
+
+  // Test getters
+  std::vector<int> GetCalls(const std::string& name);
+  size_t NumCalls(const std::string& name);
+  // Returns kInvalid if no metric was recorded.
+  int GetLast(const std::string& name);
+
+  static constexpr int kInvalid = std::numeric_limits<int>::min();
+
+ private:
+  std::map<std::string, std::vector<int>> metrics_;
+};
+
+#endif  // METRICS_FAKE_METRICS_LIBRARY_H_
