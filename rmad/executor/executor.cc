a4be28b952b257376b0653482d12a9b2315708c3
Cheng-Han Yang <chenghan@google.com>
2022-07-28 18:06:11 +0800

    rmad: Implement the executor
    Mount the external disk, then write logs and extract firmware updater in
    the executor.
    
    BUG=b:210075797
    TEST=FEATURES=test emerge-octopus rmad
    
    Change-Id: I44de2559f8101e386e06aeb3ee0e559f9f5a7e05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3784701
    Reviewed-by: Gene Chang <genechang@google.com>
    Commit-Queue: Cheng-Han Yang <chenghan@chromium.org>
    Tested-by: Cheng-Han Yang <chenghan@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- rmad/executor/executor.cc
+++ rmad/executor/executor.cc
@@ -4,13 +4,40 @@
 
 #include "rmad/executor/executor.h"
 
+#include <cctype>
 #include <optional>
 #include <string>
 #include <utility>
 
 #include <base/bind.h>
+#include <base/check.h>
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
+#include <base/files/scoped_temp_dir.h>
+#include <base/strings/stringprintf.h>
+#include <brillo/asynchronous_signal_handler.h>
+#include <brillo/file_utils.h>
 
 #include "rmad/executor/mojom/executor.mojom.h"
+#include "rmad/executor/mount.h"
+
+namespace {
+
+// The executor process runs in a light sandbox with /tmp mounted as tmpfs.
+constexpr char kTmpPath[] = "/tmp";
+
+constexpr char kDevicePathFormat[] = "/dev/sd%c%d";
+constexpr char kSourceFirmwareUpdaterRelPath[] =
+    "usr/sbin/chromeos-firmwareupdate";
+constexpr char kTargetFirmwareUpdaterAbsPath[] =
+    "/var/lib/rmad/chromeos-firmwareupdate";
+
+// Partition for writing the log file. Only check the first partition.
+constexpr int kWriteLogPartitionIndex = 1;
+// Partition for rootfs A in a ChromeOS image. We don't check rootfs B.
+constexpr int kRootfsPartitionIndex = 3;
+
+}  // namespace
 
 namespace rmad {
 
@@ -24,13 +51,64 @@ Executor::Executor(
 void Executor::MountAndWriteLog(uint8_t device_id,
                                 const std::string& log_string,
                                 MountAndWriteLogCallback callback) {
-  // TODO(chenghan): This is now fake.
+  // Input argument check.
+  if (!islower(device_id)) {
+    std::move(callback).Run(std::nullopt);
+    return;
+  }
+  // Create temporary mount point.
+  base::ScopedTempDir temp_dir;
+  if (!temp_dir.CreateUniqueTempDirUnderPath(base::FilePath(kTmpPath))) {
+    std::move(callback).Run(std::nullopt);
+    return;
+  }
+
+  const base::FilePath device_path(base::StringPrintf(
+      kDevicePathFormat, device_id, kWriteLogPartitionIndex));
+  const base::FilePath mount_point = temp_dir.GetPath();
+  const Mount mount(device_path, mount_point, "vfat", false);
+  if (mount.IsValid()) {
+    // TODO(chenghan): Append timestamp to the log filename.
+    const base::FilePath log_path = mount_point.Append("rma.log");
+    if (base::WriteFile(log_path, log_string.c_str())) {
+      brillo::SyncFileOrDirectory(log_path, false, true);
+      std::move(callback).Run(log_path.value());
+      return;
+    }
+  }
   std::move(callback).Run(std::nullopt);
 }
 
 void Executor::MountAndCopyFirmwareUpdater(
     uint8_t device_id, MountAndCopyFirmwareUpdaterCallback callback) {
-  // TODO(chenghan): This is now fake.
+  // Input argument check.
+  if (!islower(device_id)) {
+    std::move(callback).Run(false);
+    return;
+  }
+  // Create temporary mount point.
+  base::ScopedTempDir temp_dir;
+  if (!temp_dir.CreateUniqueTempDirUnderPath(base::FilePath(kTmpPath))) {
+    std::move(callback).Run(false);
+    return;
+  }
+
+  const base::FilePath device_path(
+      base::StringPrintf(kDevicePathFormat, device_id, kRootfsPartitionIndex));
+  const base::FilePath mount_point = temp_dir.GetPath();
+  const Mount mount(device_path, mount_point, "ext2", true);
+  if (mount.IsValid()) {
+    const base::FilePath source_updater_path =
+        mount_point.Append(kSourceFirmwareUpdaterRelPath);
+    const base::FilePath target_updater_path(kTargetFirmwareUpdaterAbsPath);
+    if (base::PathExists(source_updater_path) &&
+        base::CopyFile(source_updater_path, target_updater_path)) {
+      brillo::SyncFileOrDirectory(base::FilePath(target_updater_path), false,
+                                  true);
+      std::move(callback).Run(true);
+      return;
+    }
+  }
   std::move(callback).Run(false);
 }
 
