515a095b036c3fd4afbfc67a4579506fa7e51eb5
Isaac Bosompem <mrisaacb@google.com>
2022-08-11 20:14:31 +0000

    vm_tools: sommelier: Add surface level scale override
    Adding the ability to provide surface level scaling override.
    This functionality will be used by a subsequent patch to fix
    issues encountered with GLBench.
    
    BUG=b:217976094
    TEST=Verified that Sommelier functioned in direct/non-direct mode.
    
    Change-Id: I7cab8f89680200121497ae47bbd45c8cf02749d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3833449
    Tested-by: Isaac Bosompem <mrisaacb@google.com>
    Commit-Queue: Isaac Bosompem <mrisaacb@google.com>
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Reviewed-by: Nic Hollingum <hollingum@google.com>

--- vm_tools/sommelier/sommelier-transform.cc
+++ vm_tools/sommelier/sommelier-transform.cc
@@ -7,16 +7,36 @@
 #include "sommelier-tracing.h"    // NOLINT(build/include_directory)
 #include "sommelier-transform.h"  // NOLINT(build/include_directory)
 
+static void sl_transform_get_scale_factors(
+    struct sl_context* ctx,
+    const struct sl_host_surface* surface,
+    double* scalex,
+    double* scaley) {
+  if (ctx->use_direct_scale && surface && surface->has_own_scale) {
+    *scalex = surface->xdg_scale_x;
+    *scaley = surface->xdg_scale_y;
+  } else {
+    *scalex = ctx->xdg_scale_x;
+    *scaley = ctx->xdg_scale_y;
+  }
+}
+
 static double sl_transform_direct_axis_scale(struct sl_context* ctx,
                                              struct sl_host_surface* surface,
                                              uint32_t axis) {
-  return (axis == 0) ? ctx->xdg_scale_y : ctx->xdg_scale_x;
+  double scalex, scaley;
+
+  sl_transform_get_scale_factors(ctx, surface, &scalex, &scaley);
+  return (axis == 0) ? scaley : scalex;
 }
 
-static void sl_transform_direct_to_host_damage(int64_t* x,
-                                               int64_t* y,
-                                               double scale_x,
-                                               double scale_y) {
+static void sl_transform_direct_to_host_damage(
+    struct sl_context* ctx,
+    const struct sl_host_surface* surface,
+    int64_t* x,
+    int64_t* y,
+    double scale_x,
+    double scale_y) {
   double xwhole = trunc(static_cast<double>(*x) / scale_x);
   double ywhole = trunc(static_cast<double>(*y) / scale_y);
 
@@ -38,8 +58,12 @@ static void sl_transform_direct_to_guest_fixed(struct sl_context* ctx,
                                                struct sl_host_surface* surface,
                                                wl_fixed_t* x,
                                                wl_fixed_t* y) {
-  double resultx = wl_fixed_to_double(*x) * ctx->xdg_scale_x;
-  double resulty = wl_fixed_to_double(*y) * ctx->xdg_scale_y;
+  double scalex, scaley;
+
+  sl_transform_get_scale_factors(ctx, surface, &scalex, &scaley);
+
+  double resultx = wl_fixed_to_double(*x) * scalex;
+  double resulty = wl_fixed_to_double(*y) * scaley;
 
   *x = wl_fixed_from_double(resultx);
   *y = wl_fixed_from_double(resulty);
@@ -59,8 +83,12 @@ static void sl_transform_direct_to_host_fixed(struct sl_context* ctx,
                                               struct sl_host_surface* surface,
                                               wl_fixed_t* x,
                                               wl_fixed_t* y) {
-  double resultx = wl_fixed_to_double(*x) / ctx->xdg_scale_x;
-  double resulty = wl_fixed_to_double(*y) / ctx->xdg_scale_y;
+  double scalex, scaley;
+
+  sl_transform_get_scale_factors(ctx, surface, &scalex, &scaley);
+
+  double resultx = wl_fixed_to_double(*x) / scalex;
+  double resulty = wl_fixed_to_double(*y) / scaley;
 
   *x = wl_fixed_from_double(resultx);
   *y = wl_fixed_from_double(resulty);
@@ -70,8 +98,12 @@ static void sl_transform_direct_to_guest(struct sl_context* ctx,
                                          struct sl_host_surface* surface,
                                          int32_t* x,
                                          int32_t* y) {
-  double xwhole = trunc(ctx->xdg_scale_x * static_cast<double>(*x));
-  double ywhole = trunc(ctx->xdg_scale_y * static_cast<double>(*y));
+  double scalex, scaley;
+
+  sl_transform_get_scale_factors(ctx, surface, &scalex, &scaley);
+
+  double xwhole = trunc(scalex * static_cast<double>(*x));
+  double ywhole = trunc(scaley * static_cast<double>(*y));
 
   *x = static_cast<int32_t>(xwhole);
   *y = static_cast<int32_t>(ywhole);
@@ -81,8 +113,12 @@ static void sl_transform_direct_to_host(struct sl_context* ctx,
                                         struct sl_host_surface* surface,
                                         int32_t* x,
                                         int32_t* y) {
-  double xwhole = trunc(static_cast<double>(*x) / ctx->xdg_scale_x);
-  double ywhole = trunc(static_cast<double>(*y) / ctx->xdg_scale_y);
+  double scalex, scaley;
+
+  sl_transform_get_scale_factors(ctx, surface, &scalex, &scaley);
+
+  double xwhole = trunc(static_cast<double>(*x) / scalex);
+  double ywhole = trunc(static_cast<double>(*y) / scaley);
 
   *x = static_cast<int32_t>(xwhole);
   *y = static_cast<int32_t>(ywhole);
@@ -111,19 +147,26 @@ bool sl_transform_viewport_scale(struct sl_context* ctx,
 
 void sl_transform_damage_coord(struct sl_context* ctx,
                                const struct sl_host_surface* surface,
-                               double scalex,
-                               double scaley,
+                               double damage_scalex,
+                               double damage_scaley,
                                int64_t* x1,
                                int64_t* y1,
                                int64_t* x2,
                                int64_t* y2) {
-  double sx = scalex * ctx->scale;
-  double sy = scaley * ctx->scale;
-
   if (ctx->use_direct_scale) {
-    sl_transform_direct_to_host_damage(x1, y1, sx, sy);
-    sl_transform_direct_to_host_damage(x2, y2, sx, sy);
+    double scalex, scaley;
+
+    sl_transform_get_scale_factors(ctx, surface, &scalex, &scaley);
+
+    scalex *= damage_scalex;
+    scaley *= damage_scaley;
+
+    sl_transform_direct_to_host_damage(ctx, surface, x1, y1, scalex, scaley);
+    sl_transform_direct_to_host_damage(ctx, surface, x2, y2, scalex, scaley);
   } else {
+    double sx = damage_scalex * ctx->scale;
+    double sy = damage_scaley * ctx->scale;
+
     // Enclosing rect after scaling and outset by one pixel to account for
     // potential filtering.
     *x1 = MAX(MIN_SIZE, (*x1) - 1) / sx;
