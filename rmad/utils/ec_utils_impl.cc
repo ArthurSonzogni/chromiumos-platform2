1bb7829dba5070694bc277025d32c177c7ae0924
Cheng-Han Yang <chenghan@google.com>
2022-08-15 16:37:22 +0800

    rmad: Add EcUtils for EC reboot
    A wrapper of libec to call EC commands. Currently only using the EC
    reboot command.
    
    BUG=b:242015925
    TEST=FEATURES=test emerge-octopus rmad
    
    Cq-Depend: chromium:3831741
    Change-Id: Iab274946f948a3ca350d113cdbf14cc095a380b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3829010
    Reviewed-by: Gene Chang <genechang@google.com>
    Tested-by: Cheng-Han Yang <chenghan@chromium.org>
    Commit-Queue: Cheng-Han Yang <chenghan@chromium.org>

---
+++ rmad/utils/ec_utils_impl.cc
@@ -0,0 +1,39 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "rmad/utils/ec_utils_impl.h"
+
+#include <fcntl.h>
+
+#include <base/files/scoped_file.h>
+#include <base/logging.h>
+#include <libec/reboot_command.h>
+
+namespace {
+
+constexpr char kEcPath[] = "/dev/cros_ec";
+
+}  // namespace
+
+namespace rmad {
+
+bool EcUtilsImpl::Reboot() {
+  base::ScopedFD ec_fd = GetEcFd();
+  if (!ec_fd.is_valid()) {
+    return false;
+  }
+  ec::RebootCommand reboot_cmd;
+  return reboot_cmd.Run(ec_fd.get());
+}
+
+base::ScopedFD EcUtilsImpl::GetEcFd() const {
+  int ec_fd = open(kEcPath, O_RDWR | O_CLOEXEC);
+  if (ec_fd == -1) {
+    LOG(ERROR) << "Failed to get EC FD";
+    return base::ScopedFD();
+  }
+  return base::ScopedFD(ec_fd);
+}
+
+}  // namespace rmad
