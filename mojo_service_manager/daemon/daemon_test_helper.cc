962798b4d424cd312e6350ec58c635fc73eb3a6b
Chung-Sheng Wu <chungsheng@google.com>
2022-08-16 17:42:54 +0800

    mojo_service_manager: Implement connect method
    Implement the connect method for others to connect to service manager.
    Make daemon_test_helper use the new function to connect for unit test
    coverage.
    
    BUG=b:233329926
    TEST=emerge-zork mojo_service_manager
    
    Change-Id: I7ff384ddca1f7538561bc8243e1467b1777baaf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3811047
    Tested-by: Chung-Sheng Wu <chungsheng@google.com>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Chung-Sheng Wu <chungsheng@google.com>

--- mojo_service_manager/daemon/daemon_test_helper.cc
+++ mojo_service_manager/daemon/daemon_test_helper.cc
@@ -26,60 +26,13 @@
 #include <mojo/public/cpp/system/invitation.h>
 
 #include "mojo_service_manager/daemon/mojo_test_environment.h"
+#include "mojo_service_manager/lib/connect.h"
 #include "mojo_service_manager/lib/mojom/service_manager.mojom.h"
 
 namespace {
 
 namespace mojo_service_manager = chromeos::mojo_service_manager;
 namespace mojom = mojo_service_manager::mojom;
-
-bool WaitForSocketReady(const base::FilePath& socket_path) {
-  for (base::ElapsedTimer t; t.Elapsed() < TestTimeouts::action_timeout();
-       base::PlatformThread::Sleep(base::Milliseconds(1))) {
-    if (base::PathExists(socket_path))
-      return true;
-  }
-  return false;
-}
-
-base::ScopedFD ConnectToSocket(const base::FilePath& socket_path) {
-  if (!WaitForSocketReady(socket_path))
-    return base::ScopedFD{};
-
-  base::ScopedFD sock{socket(AF_UNIX, SOCK_STREAM, 0)};
-  if (!sock.is_valid()) {
-    LOG(ERROR) << "Failed to create socket";
-    return base::ScopedFD{};
-  }
-
-  struct sockaddr_un unix_addr {
-    .sun_family = AF_UNIX,
-  };
-  constexpr size_t kMaxSize =
-      sizeof(unix_addr.sun_path) - /*NULL-terminator*/ 1;
-  CHECK_LE(socket_path.value().size(), kMaxSize);
-  strncpy(unix_addr.sun_path, socket_path.value().c_str(), kMaxSize);
-
-  int rc = HANDLE_EINTR(connect(sock.get(),
-                                reinterpret_cast<const sockaddr*>(&unix_addr),
-                                sizeof(unix_addr)));
-  if (rc == -1 && errno != EISCONN) {
-    LOG(ERROR) << "Failed to connect to socket";
-    return base::ScopedFD{};
-  }
-  return sock;
-}
-
-mojo::Remote<mojom::ServiceManager> ConnectToMojoServiceManager(
-    base::ScopedFD peer) {
-  CHECK(peer.is_valid());
-  auto invitation = mojo::IncomingInvitation::Accept(
-      mojo::PlatformChannelEndpoint(mojo::PlatformHandle(std::move(peer))));
-  mojo::ScopedMessagePipeHandle pipe = invitation.ExtractMessagePipe(
-      mojo_service_manager::kMojoInvitationPipeName);
-  return mojo::Remote<mojom::ServiceManager>{
-      mojo::PendingRemote<mojom::ServiceManager>(std::move(pipe), 0u)};
-}
 
 }  // namespace
 
@@ -92,8 +45,8 @@ int main(int argc, char** argv) {
   base::FilePath socket_path =
       base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
           mojo_service_manager::kSocketPathSwitch);
-  mojo::Remote<mojom::ServiceManager> mojo_service_manager =
-      ConnectToMojoServiceManager(ConnectToSocket(socket_path));
+  mojo::Remote<mojom::ServiceManager> mojo_service_manager{
+      mojo_service_manager::ConnectToMojoServiceManagerForTesting(socket_path)};
   CHECK(mojo_service_manager.is_connected());
 
   mojo_service_manager::DaemonTestHelperResult result =
--- mojo_service_manager/lib/connect.h
+++ mojo_service_manager/lib/connect.h
@@ -5,6 +5,7 @@
 #ifndef MOJO_SERVICE_MANAGER_LIB_CONNECT_H_
 #define MOJO_SERVICE_MANAGER_LIB_CONNECT_H_
 
+#include <base/files/file_path.h>
 #include <brillo/brillo_export.h>
 #include <mojo/public/cpp/bindings/pending_remote.h>
 
@@ -12,8 +13,15 @@
 
 namespace chromeos::mojo_service_manager {
 
+// Connects to the mojo service manager. This will try to connect to the mojo
+// service manager socket and bootstrap the mojo connection. If fails to
+// connect, this will return an invalid pending remote.
 BRILLO_EXPORT mojo::PendingRemote<mojom::ServiceManager>
 ConnectToMojoServiceManager();
+
+// Allows caller to change the socket path for testing.
+mojo::PendingRemote<mojom::ServiceManager>
+ConnectToMojoServiceManagerForTesting(const base::FilePath& socket_path);
 
 }  // namespace chromeos::mojo_service_manager
 
