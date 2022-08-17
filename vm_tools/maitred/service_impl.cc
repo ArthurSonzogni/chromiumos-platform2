7b73b52bca54c5584fb9eaf3b01a1a34e931efb1
James Ye <jamesye@google.com>
2022-08-17 17:58:22 +1000

    vm_tools: Don't try to enable LRO
    This doesn't work on our 5.10 kernels, and the replacement GRO_HW
    offload is enabled by default.
    
    BUG=b:223425630
    TEST=Deploy to termina
    
    Change-Id: I763ccc94275d3b658d7715a7254d644cf1d0267e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3834958
    Reviewed-by: David Munro <davidmunro@google.com>
    Tested-by: James Ye <jamesye@google.com>
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Commit-Queue: James Ye <jamesye@google.com>

--- vm_tools/maitred/service_impl.cc
+++ vm_tools/maitred/service_impl.cc
@@ -147,41 +147,6 @@ bool SetSysctl(const char* path, const char* val, string* out_error) {
   return true;
 }
 
-bool EnableLro(const char* ifname, string* out_error) {
-  DCHECK(out_error);
-
-  base::ScopedFD sockfd(socket(AF_INET, SOCK_DGRAM | SOCK_CLOEXEC, 0));
-  if (!sockfd.is_valid()) {
-    PLogAndSaveError("failed to create socket for ethtool ioctl", out_error);
-    return false;
-  }
-
-  struct ifreq ifr;
-  strncpy(ifr.ifr_name, ifname, sizeof(ifr.ifr_name));
-
-  struct ethtool_value val;
-  val.cmd = ETHTOOL_GFLAGS;
-  ifr.ifr_data = reinterpret_cast<char*>(&val);
-
-  if (HANDLE_EINTR(ioctl(sockfd.get(), SIOCETHTOOL, &ifr)) != 0) {
-    PLogAndSaveError(
-        base::StringPrintf("failed to get ethtool flags for %s", ifname),
-        out_error);
-    return false;
-  }
-
-  val.cmd = ETHTOOL_SFLAGS;
-  val.data |= ETH_FLAG_LRO;
-
-  if (HANDLE_EINTR(ioctl(sockfd.get(), SIOCETHTOOL, &ifr)) != 0) {
-    PLogAndSaveError(base::StringPrintf("failed to enable LRO for %s", ifname),
-                     out_error);
-    return false;
-  }
-
-  return true;
-}
-
 // Writes a resolv.conf with the supplied |nameservers| and |search_domains|.
 // The default Chrome OS resolver options will be used. Returns true on
 // success, and returns false on failure with an error message stored in
@@ -288,8 +253,7 @@ grpc::Status ServiceImpl::ConfigureNetwork(grpc::ServerContext* ctx,
     return grpc::Status(grpc::INVALID_ARGUMENT, "IPv4 gateway cannot be 0");
   }
 
-  // Enable IP forwarding first. This will disable LRO, which we'll then have
-  // to reenable manually later.
+  // Enable IP forwarding.
   string error;
   if (!SetSysctl("/proc/sys/net/ipv4/ip_forward", "1", &error)) {
     return grpc::Status(grpc::INTERNAL, error);
@@ -366,16 +330,6 @@ grpc::Status ServiceImpl::ConfigureNetwork(grpc::ServerContext* ctx,
         string("failed to enable network interface: ") + strerror(ret));
   }
   LOG(INFO) << "Set interface " << kInterfaceName << " up and running";
-
-  // Forcibly enables Large Receive Offload via ethtool. Linux will
-  // conservatively disable LRO if IP forwarding is enabled since LRO mangles
-  // packets in a way that makes it unsafe to forward. virtio-net uses Generic
-  // Receive Offload, which is safe to use with IP forwarding.
-  if (!EnableLro(kInterfaceName, &error)) {
-    // Don't fail the entire network config since this may run with a 4.19
-    // kernel that doesn't support setting LRO on virtio-net.
-    LOG(WARNING) << "Failed to enable LRO: " << error;
-  }
 
   // Bring up the loopback interface too.
   ret = EnableInterface(fd.get(), kLoopbackName);
