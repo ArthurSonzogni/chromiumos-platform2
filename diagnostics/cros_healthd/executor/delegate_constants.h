4dbb637bd14016b9c5bc487b65b810151eeb0e20
Kerker Yang <kerker@google.com>
2022-09-02 09:11:40 +0800

    diagnostics: Executor delegate accepts mojo invitation from executor
    In this CL, delegate daemon accepts the mojo invitation from executor
    and bind the receiver to the DelegateImpl. From now on, it's ready to
    process remote's request.
    
    One thing to note, timing to terminate the daemon:
    I'd like to make this daemon more flexible, so the remote is able to
    launch it once and run several requests in a row, hence we shouldn't
    terminate it once we fulfill one request. Instead, remote SHOULD take
    care of the life cycle of the daemon, that is, in this CL, remote
    SHOULD disconnect the mojo connection so the daemon will stop.
    
    BUG=b:243603240
    TEST=Test with following CLs
    
    Change-Id: Ifa7acb85e4674ed9bb4560dbba0772436ad5123f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3877838
    Reviewed-by: Chung-Sheng Wu <chungsheng@google.com>
    Tested-by: Kerker Yang <kerker@chromium.org>
    Commit-Queue: Kerker Yang <kerker@chromium.org>

---
+++ diagnostics/cros_healthd/executor/delegate_constants.h
@@ -0,0 +1,15 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DIAGNOSTICS_CROS_HEALTHD_EXECUTOR_DELEGATE_CONSTANTS_H_
+#define DIAGNOSTICS_CROS_HEALTHD_EXECUTOR_DELEGATE_CONSTANTS_H_
+
+namespace diagnostics {
+
+inline constexpr char kDelegateMojoChannelHandle[] =
+    "delegate-mojo-channel-handle";
+
+}  // namespace diagnostics
+
+#endif  // DIAGNOSTICS_CROS_HEALTHD_EXECUTOR_DELEGATE_CONSTANTS_H_
--- diagnostics/cros_healthd/executor/delegate_main.cc
+++ diagnostics/cros_healthd/executor/delegate_main.cc
@@ -5,18 +5,45 @@
 #include <base/command_line.h>
 #include <brillo/daemons/daemon.h>
 #include <brillo/syslog_logging.h>
+#include <mojo/core/embedder/embedder.h>
+#include <mojo/core/embedder/scoped_ipc_support.h>
+#include <mojo/public/cpp/bindings/pending_receiver.h>
+#include <mojo/public/cpp/bindings/receiver.h>
+#include <mojo/public/cpp/platform/platform_channel.h>
+#include <mojo/public/cpp/system/invitation.h>
+#include <mojo/public/cpp/system/message_pipe.h>
+
+#include "diagnostics/cros_healthd/executor/delegate_constants.h"
+#include "diagnostics/cros_healthd/executor/delegate_impl.h"
 
 namespace diagnostics {
 
 class DelegateDaemon : public brillo::Daemon {
  public:
-  DelegateDaemon();
+  explicit DelegateDaemon(mojo::PlatformChannelEndpoint endpoint)
+      : scoped_ipc_support_(
+            base::ThreadTaskRunnerHandle::Get() /* io_thread_task_runner */,
+            mojo::core::ScopedIPCSupport::ShutdownPolicy::
+                CLEAN /* blocking shutdown */) {
+    mojo::IncomingInvitation invitation =
+        mojo::IncomingInvitation::Accept(std::move(endpoint));
+    mojo::ScopedMessagePipeHandle pipe = invitation.ExtractMessagePipe(0);
+    receiver_.Bind(mojo::PendingReceiver<mojom::Delegate>(std::move(pipe)));
+    receiver_.set_disconnect_handler(
+        base::BindOnce(&DelegateDaemon::Quit, base::Unretained(this)));
+  }
   DelegateDaemon(const DelegateDaemon&) = delete;
   DelegateDaemon& operator=(const DelegateDaemon&) = delete;
   ~DelegateDaemon();
+
+ private:
+  mojo::core::ScopedIPCSupport scoped_ipc_support_;
+
+  DelegateImpl delegate_;
+
+  mojo::Receiver<mojom::Delegate> receiver_{&delegate_};
 };
 
-DelegateDaemon::DelegateDaemon() {}
 DelegateDaemon::~DelegateDaemon() {}
 
 }  // namespace diagnostics
@@ -25,6 +52,11 @@ int main(int argc, char* argv[]) {
   brillo::InitLog(brillo::kLogToSyslog | brillo::kLogToStderr);
   base::CommandLine::Init(argc, argv);
 
-  diagnostics::DelegateDaemon daemon;
+  mojo::core::Init();
+
+  diagnostics::DelegateDaemon daemon(
+      mojo::PlatformChannel::RecoverPassedEndpointFromString(
+          base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+              diagnostics::kDelegateMojoChannelHandle)));
   return daemon.Run();
 }
