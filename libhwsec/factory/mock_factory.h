6facc0309d7864b9cd021f2656eb72267e782842
Yi Chou <yich@google.com>
2022-09-03 11:26:33 +0800

    libhwsec: Add MockFactory
    BUG=b:243781077
    TEST=unittest of libhwsec.
    
    Change-Id: Ieead60ec077dafcf7fe3a6bb5a1e9b4e5220a4b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3873125
    Reviewed-by: Leo Lai <cylai@google.com>
    Commit-Queue: Yi Chou <yich@google.com>
    Tested-by: Yi Chou <yich@google.com>

---
+++ libhwsec/factory/mock_factory.h
@@ -0,0 +1,40 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIBHWSEC_FACTORY_MOCK_FACTORY_H_
+#define LIBHWSEC_FACTORY_MOCK_FACTORY_H_
+
+#include <memory>
+#include <utility>
+
+#include <gmock/gmock.h>
+
+#include "libhwsec/factory/factory.h"
+
+// Factory holds the ownership of the middleware and backend.
+// And generates different frontend for different usage.
+
+namespace hwsec {
+
+class MockFactory : public Factory {
+ public:
+  MockFactory() = default;
+  ~MockFactory() override = default;
+  MOCK_METHOD(std::unique_ptr<CryptohomeFrontend>,
+              GetCryptohomeFrontend,
+              (),
+              (override));
+  MOCK_METHOD(std::unique_ptr<PinWeaverFrontend>,
+              GetPinWeaverFrontend,
+              (),
+              (override));
+  MOCK_METHOD(std::unique_ptr<RecoveryCryptoFrontend>,
+              GetRecoveryCryptoFrontend,
+              (),
+              (override));
+};
+
+}  // namespace hwsec
+
+#endif  // LIBHWSEC_FACTORY_MOCK_FACTORY_H_
