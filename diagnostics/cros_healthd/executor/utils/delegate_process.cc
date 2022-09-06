80235a58a55769b3a75e826a983e51422af841fb
Kerker Yang <kerker@google.com>
2022-09-06 17:12:29 +0800

    diagnostics: Create DelegateProcess class to launch delegate
    BUG=b:243603240
    TEST=Test with https://crrev.com/c/3879062
    TEST=FEATURES=test emerge-hatch diagnostics
    
    Change-Id: I21fb73e148e54f02cb4950de35f1af690c0c3611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3877839
    Commit-Queue: Kerker Yang <kerker@chromium.org>
    Reviewed-by: Chung-Sheng Wu <chungsheng@google.com>
    Tested-by: Kerker Yang <kerker@chromium.org>

---
+++ diagnostics/cros_healthd/executor/utils/delegate_process.cc
@@ -0,0 +1,80 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "diagnostics/cros_healthd/executor/utils/delegate_process.h"
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <mojo/public/cpp/bindings/remote.h>
+#include <mojo/public/cpp/bindings/pending_remote.h>
+#include <mojo/public/cpp/system/invitation.h>
+#include <mojo/public/cpp/system/message_pipe.h>
+
+#include "diagnostics/cros_healthd/executor/delegate_constants.h"
+#include "diagnostics/cros_healthd/executor/mojom/delegate.mojom.h"
+
+namespace {
+
+constexpr char kDelegateBinary[] = "/usr/libexec/diagnostics/executor-delegate";
+
+}  // namespace
+
+namespace diagnostics {
+
+DelegateProcess::DelegateProcess() = default;
+
+DelegateProcess::~DelegateProcess() = default;
+
+DelegateProcess::DelegateProcess(
+    const std::string& seccomp_filename,
+    const std::string& user,
+    uint64_t capabilities_mask,
+    const std::vector<base::FilePath>& readonly_mount_points,
+    const std::vector<base::FilePath>& writable_mount_points)
+    : SandboxedProcess({kDelegateBinary},
+                       seccomp_filename,
+                       user,
+                       capabilities_mask,
+                       readonly_mount_points,
+                       writable_mount_points) {
+  SendMojoInvitation();
+  RunDelegate();
+}
+
+DelegateProcess::DelegateProcess(
+    const std::string& seccomp_filename,
+    const std::vector<base::FilePath>& readonly_mount_points)
+    : SandboxedProcess(
+          {kDelegateBinary}, seccomp_filename, readonly_mount_points) {
+  SendMojoInvitation();
+  RunDelegate();
+}
+
+void DelegateProcess::SendMojoInvitation() {
+  mojo::OutgoingInvitation invitation;
+  mojo::ScopedMessagePipeHandle pipe = invitation.AttachMessagePipe(0);
+  mojo::OutgoingInvitation::Send(std::move(invitation),
+                                 base::kNullProcessHandle,
+                                 channel_.TakeLocalEndpoint());
+  remote_.Bind(mojo::PendingRemote<mojom::Delegate>(std::move(pipe), 0));
+}
+
+void DelegateProcess::RunDelegate() {
+  base::LaunchOptions options;
+  std::string value;
+  channel_.PrepareToPassRemoteEndpoint(&options.fds_to_remap, &value);
+
+  AddArg(std::string("--") + kDelegateMojoChannelHandle + "=" + value);
+
+  for (const auto& pii : options.fds_to_remap) {
+    BindFd(pii.first, pii.second);
+  }
+
+  SandboxedProcess::Start();
+  channel_.RemoteProcessLaunchAttempted();
+}
+
+}  // namespace diagnostics
---
+++ diagnostics/cros_healthd/executor/utils/delegate_process.h
@@ -0,0 +1,86 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DIAGNOSTICS_CROS_HEALTHD_EXECUTOR_UTILS_DELEGATE_PROCESS_H_
+#define DIAGNOSTICS_CROS_HEALTHD_EXECUTOR_UTILS_DELEGATE_PROCESS_H_
+
+#include <string>
+#include <vector>
+
+#include <mojo/public/cpp/bindings/remote.h>
+#include <mojo/public/cpp/platform/platform_channel.h>
+
+#include "diagnostics/cros_healthd/executor/mojom/delegate.mojom.h"
+#include "diagnostics/cros_healthd/executor/utils/sandboxed_process.h"
+
+namespace diagnostics {
+
+namespace mojom = chromeos::cros_healthd::mojom;
+
+// Run executor delegate.
+//
+// Argument definition can be found in the following header:
+//     cros_healthd/executor/utils/sandboxed_process.h.
+//
+// Notice:
+// 1. |DelegateProcess| can't be initialized when the current thread is dealing
+// with a mojo task. For example, when calling `remote->Func()`, we can't create
+// |DelegateProcess| in `Func()`. A recommended way to create it is to launch a
+// new task in same thread.
+//
+// 2. The users should be aware of the lifecycle of this object. Once it's
+// destroyed, the mojo connection to the delegate will disconnect.
+//
+// Example usage:
+//   // Assume `Func()` is the mojo receiver interface in the executor.
+//   void Executor::Func(callback) {
+//     base::SequencedTaskRunnerHandle::Get()->PostTask(
+//         FROM_HERE,
+//         base::BindOnce(&Executor::FuncTask, weak_factory_.GetWeakPtr(),
+//                        std::move(callback)));
+//   }
+//
+//   void Executor::FuncTask {
+//     auto delegate = std::make_unique<DelegateProcess>("seccomp.policy");
+//     auto cb = mojo::WrapCallbackWithDefaultInvokeIfNotRun(
+//         std::move(callback), default error params);
+//
+//     // Remember to move the |delegate| in to prevent being destroyed.
+//     delegate->remote()->GetData(
+//         base::BindOnce(&GetDataCallback, std::move(cb),
+//         std::move(delegate)));
+//   }
+//
+class DelegateProcess : public SandboxedProcess {
+ public:
+  using SandboxedProcess::SandboxedProcess;
+  DelegateProcess(const std::string& seccomp_filename,
+                  const std::string& user,
+                  uint64_t capabilities_mask,
+                  const std::vector<base::FilePath>& readonly_mount_points,
+                  const std::vector<base::FilePath>& writable_mount_points);
+  DelegateProcess(
+      const std::string& seccomp_filename,
+      const std::vector<base::FilePath>& readonly_mount_points = {});
+
+  DelegateProcess(const DelegateProcess&) = delete;
+  DelegateProcess& operator=(const DelegateProcess&) = delete;
+  ~DelegateProcess() override;
+
+  auto remote() { return remote_.get(); }
+
+ protected:
+  DelegateProcess();
+
+ private:
+  virtual void SendMojoInvitation();
+  virtual void RunDelegate();
+
+  mojo::PlatformChannel channel_;
+  mojo::Remote<mojom::Delegate> remote_;
+};
+
+}  // namespace diagnostics
+
+#endif  // DIAGNOSTICS_CROS_HEALTHD_EXECUTOR_UTILS_DELEGATE_PROCESS_H_
