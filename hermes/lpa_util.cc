e7c39fd4d9464fdb6f11096e087b8456254fddb8
Pavan Holla <pholla@chromium.org>
2022-09-08 20:56:34 +0000

    hermes: Report error_code=4 from eUICC as kMalformedResponse
    If the eUICC reports error code =4 during AuthenticateServer, report a
    kMalformedResponse error to Chrome. Previously, it would be reported as
    kAlreadyDisabled.
    
    BUG=b:216849625
    FIXED=b:216849625
    TEST=modem esim install "1$prod.smdp-plus.rsp.goog$fake"; modem esim install "1$prod.smdp-plus.rsp.goog$fake"
    
    Cros-Add-Test-Suites=cellular_ota,cellular_ota_flaky
    Cros-Add-TS-Boards-BuildTarget=dedede,trogdor
    Cros-Add-TS-Pool=cellular
    Change-Id: I67aec26a8881439bbb0ca8661be486b00ca86f39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3885419
    Tested-by: Pavan Holla <pholla@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Pavan Holla <pholla@google.com>

--- hermes/lpa_util.cc
+++ hermes/lpa_util.cc
@@ -136,6 +136,21 @@ const std::map<int, InnerError>& GetInnerErrorMap() {
   return err_map;
 }
 
+const std::map<int, InnerError>& GetInnerErrorMapForAuthenticateServer() {
+  constexpr int kPreviouslySeenInvalidActivationCode = 4;
+  static std::map<int, InnerError> err_map{
+      {lpa::card::EuiccCard::kNoResponses,
+       {kErrorNoResponse, "No response from eUICC"}},
+      {kPreviouslySeenInvalidActivationCode,
+       {kErrorMalformedResponse,
+        "Malformed response from eUICC. Invalid activation code may have been "
+        "reused"}},
+      {lpa::card::EuiccCard::kSendApduError,
+       {kErrorSendApduFailure, "Failed to send APDU to eUICC"}},
+  };
+  return err_map;
+}
+
 }  // namespace
 
 brillo::ErrorPtr LpaErrorToBrillo(const base::Location& location, int error) {
@@ -153,8 +168,13 @@ brillo::ErrorPtr LpaErrorToBrillo(const base::Location& location, int error) {
   error_message +=
       brillo::GetOrDefault(GetMidErrorMap(), lpa_mid_code, "UnknownMid: ");
 
+  const auto& error_map =
+      lpa_mid_code == lpa::card::EuiccCard::kAuthenticateServerError
+          ? GetInnerErrorMapForAuthenticateServer()
+          : GetInnerErrorMap();
   const auto& inner_error = brillo::GetOrDefault(
-      GetInnerErrorMap(), lpa_inner_code, {kErrorUnknown, "Unknown error"});
+      error_map, lpa_inner_code, {kErrorUnknown, "Unknown error"});
+
   error_message +=
       base::StringPrintf("%s (%d)", inner_error.error_message_, lpa_inner_code);
 
