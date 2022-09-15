1c6445b81df46eae8be967167a808645f820d58d
Victor Wong <vwon@google.com>
2022-09-16 01:12:00 +1000

    faced: Enable connecting faced_cli to faced
    This CL enables connectin faced_cli to faced by bootstrapping a Mojo
    connection over a DBus connection.
    
    BUG=b:245835057
    TEST=Emerged and deployed and ran `faced` alongside
    `faced_cli connect`
    
    Change-Id: Ia1ba2b0d6e5e6c482b16db80baf1d2d0b5e8e03d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3899673
    Commit-Queue: Victor Wong <vwon@chromium.org>
    Reviewed-by: David Greenaway <dgreenaway@google.com>
    Tested-by: Victor Wong <vwon@chromium.org>
    Reviewed-by: Ben Kersten <bkersten@google.com>

--- faced/faced_cli/faced_client.cc
+++ faced/faced_cli/faced_client.cc
@@ -5,11 +5,64 @@
 #include "faced/faced_cli/faced_client.h"
 
 #include <absl/status/status.h>
+#include <brillo/dbus/dbus_connection.h>
+#include <chromeos/dbus/service_constants.h>
+#include <mojo/public/cpp/platform/platform_channel.h>
+#include <mojo/public/cpp/system/invitation.h>
+#include <mojo/public/cpp/system/message_pipe.h>
+
+#include <iostream>
+#include <string>
+#include <utility>
+
+#include "dbus_proxies/dbus-proxies.h"
+#include "faced/util/status.h"
 
 namespace faced {
 
+absl::StatusOr<FacedConnection> ConnectToFaced() {
+  FacedConnection connection;
+
+  mojo::PlatformChannel channel;
+  mojo::OutgoingInvitation invitation;
+
+  // This sends an invitation to faced for a Mojo connection to be bootstrapped.
+
+  // Attach a message pipe to be extracted by the receiver. The other end of the
+  // pipe is returned for us to use locally.
+  connection.pipe =
+      invitation.AttachMessagePipe(faced::kBootstrapMojoConnectionChannelToken);
+  mojo::OutgoingInvitation::Send(std::move(invitation),
+                                 base::kNullProcessHandle,
+                                 channel.TakeLocalEndpoint());
+
+  // Setup libbrillo dbus.
+  brillo::DBusConnection dbus_connection;
+  connection.bus = dbus_connection.Connect();
+  if (!connection.bus) {
+    return absl::InternalError(
+        "Could not connect to the faced system service: Failed to connect to "
+        "system bus through libbrillo.");
+  }
+
+  org::chromium::FaceAuthDaemonProxy proxy(connection.bus,
+                                           faced::kFaceAuthDaemonName);
+  brillo::dbus_utils::FileDescriptor handle(
+      channel.TakeRemoteEndpoint().TakePlatformHandle().TakeFD());
+  if (!proxy.BootstrapMojoConnection(handle, /*error=*/nullptr)) {
+    return absl::InternalError(
+        "Could not connect to the faced system service: Failed to send handle "
+        "over DBus");
+  }
+
+  return connection;
+}
+
 absl::Status ConnectAndDisconnectFromFaced() {
-  // TODO(b/245835057): Implement
+  FACE_ASSIGN_OR_RETURN(FacedConnection connection, ConnectToFaced());
+
+  std::cout << "Could successfully connect to the faced system service.\n";
+
   return absl::OkStatus();
 }
 
--- faced/faced_cli/faced_client.h
+++ faced/faced_cli/faced_client.h
@@ -6,9 +6,25 @@
 #define FACED_FACED_CLI_FACED_CLIENT_H_
 
 #include <absl/status/status.h>
+#include <absl/status/statusor.h>
+#include <brillo/dbus/dbus_connection.h>
+#include <mojo/public/cpp/system/message_pipe.h>
 
 namespace faced {
 
+// Components of a connection to the Faced daemon
+struct FacedConnection {
+  // DBus connection to Faced
+  scoped_refptr<dbus::Bus> bus;
+
+  // Pipe for Mojo communication
+  mojo::ScopedMessagePipeHandle pipe;
+};
+
+// Establish Mojo connection to Faced bootstrapped over DBus
+absl::StatusOr<FacedConnection> ConnectToFaced();
+
+// Establish a Mojo connection to Faced bootstrapped over DBus then disconnect
 absl::Status ConnectAndDisconnectFromFaced();
 
 }  // namespace faced
