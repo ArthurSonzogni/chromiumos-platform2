e367b5be6a32864c8a25c3f97a87e48f082ab993
Piotr Pawliczek <pawliczek@google.com>
2022-09-30 16:46:38 +0000

    libipp: Convert the fuzzer to the new API
    Convert the existing fuzzer to the new API. Also, add to the fuzzer
    a simple procedure to browse the parsed structure.
    
    BUG=none
    TEST=USE="asan fuzzer" emerge-amd64-generic libipp
    
    Change-Id: I33ce6a392027ae22dc4d5d8e3c25755506823ebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3929740
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Auto-Submit: Piotr Pawliczek <pawliczek@chromium.org>
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Tested-by: Piotr Pawliczek <pawliczek@chromium.org>
    Commit-Queue: Benjamin Gordon <bmgordon@chromium.org>

--- libipp/ipp_fuzzer.cc
+++ libipp/ipp_fuzzer.cc
@@ -2,35 +2,34 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "libipp/ipp.h"
+#include "libipp/attribute.h"
+#include "libipp/frame.h"
 
 #include <cstdint>
-#include <fuzzer/FuzzedDataProvider.h>
-#include <limits>
 #include <vector>
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  FuzzedDataProvider fuzz_data(data, size);
-  const bool is_client = fuzz_data.ConsumeBool();
-  const ipp::Operation oper_id =
-      static_cast<ipp::Operation>(fuzz_data.ConsumeIntegral<uint16_t>());
-  const std::string packet_str = fuzz_data.ConsumeRemainingBytesAsString();
-  const std::vector<uint8_t> packet_bytes(packet_str.begin(), packet_str.end());
+void BrowseCollection(ipp::Collection* coll) {
+  const std::vector<ipp::Attribute*> attrs = coll->GetAllAttributes();
+  for (ipp::Attribute* attr : attrs) {
+    if (attr->Tag() == ipp::ValueTag::collection) {
+      const size_t n = attr->Size();
+      for (size_t i = 0; i < n; ++i) {
+        BrowseCollection(attr->GetCollection(i));
+      }
+    }
+  }
+}
 
-  if (is_client) {
-    ipp::Client client;
-    client.ReadResponseFrameFrom(packet_bytes);
-    auto response = ipp::Response::NewResponse(oper_id);
-    if (response == nullptr)
-      response = std::make_unique<ipp::Response>(oper_id);
-    client.ParseResponseAndSaveTo(response.get());
-  } else {
-    ipp::Server server;
-    server.ReadRequestFrameFrom(packet_bytes);
-    auto request = ipp::Request::NewRequest(oper_id);
-    if (request == nullptr)
-      request = std::make_unique<ipp::Request>(oper_id);
-    server.ParseRequestAndSaveTo(request.get());
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  // Parse the input data.
+  ipp::ParsingResults log;
+  ipp::Frame frame(data, size, &log);
+  // Browse the obtained frame.
+  for (ipp::GroupTag gt : ipp::kGroupTags) {
+    std::vector<ipp::Collection*> colls = frame.GetGroups(gt);
+    for (ipp::Collection* coll : colls) {
+      BrowseCollection(coll);
+    }
   }
 
   return 0;
