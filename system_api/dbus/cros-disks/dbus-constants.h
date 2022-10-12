d7fed308c3ce2d92ecc62fa9e0f62266157f8186
François Degros <fdegros@chromium.org>
2022-10-12 14:44:01 +1100

    cros-disks: Add/change MOUNT_ERROR_*_EX values
    Changed or added the numeric values of the MOUNT_ERROR_*_EX constants.
    These numeric values are not used yet.
    
    BUG=chromium:1368408
    TEST=CQ
    
    Change-Id: Ibd0a29a43c0f230a7c30c4b137a5e296a458a630
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3945327
    Tested-by: François Degros <fdegros@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Marcello Salomao <msalomao@google.com>

--- system_api/dbus/cros-disks/dbus-constants.h
+++ system_api/dbus/cros-disks/dbus-constants.h
@@ -122,23 +122,30 @@ enum MountErrorType {
 
   // Either the FUSE mounter needs a password, or the provided password is
   // incorrect.
-  MOUNT_ERROR_NEED_PASSWORD_EX = 13,
+  MOUNT_ERROR_NEED_PASSWORD_EX = 17,
   MOUNT_ERROR_NEED_PASSWORD = 117,
 
   // The FUSE mounter is currently launching, and it hasn't daemonized yet.
-  MOUNT_ERROR_IN_PROGRESS_EX = 14,
+  MOUNT_ERROR_IN_PROGRESS_EX = 18,
   MOUNT_ERROR_IN_PROGRESS = 118,
 
   // The FUSE mounter was cancelled (killed) while it was launching.
-  MOUNT_ERROR_CANCELLED_EX = 15,
+  MOUNT_ERROR_CANCELLED_EX = 19,
   MOUNT_ERROR_CANCELLED = 119,
 
   // The device is busy.
   MOUNT_ERROR_BUSY = 20,
 
+  MOUNT_ERROR_INVALID_DEVICE_PATH_EX = 13,
   MOUNT_ERROR_INVALID_DEVICE_PATH = 100,
+
+  MOUNT_ERROR_UNKNOWN_FILESYSTEM_EX = 14,
   MOUNT_ERROR_UNKNOWN_FILESYSTEM = 101,
+
+  MOUNT_ERROR_UNSUPPORTED_FILESYSTEM_EX = 15,
   MOUNT_ERROR_UNSUPPORTED_FILESYSTEM = 102,
+
+  MOUNT_ERROR_INVALID_ARCHIVE_EX = 16,
   MOUNT_ERROR_INVALID_ARCHIVE = 201,
 };
 
