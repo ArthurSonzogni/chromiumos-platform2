e8b9996c129e95bd5fbe2ae9910fdbcc573666ed
Leonid Baraz <lbaraz@chromium.org>
2022-10-14 00:05:58 +0000

    missive: Add Recorder to HealthModule
    Recorder is an RAAI object that allows to compose a single history record which
    will be added to the history when the Recorded is destructed.
    It is a moveable class, so the recorded may be handed over from one
    stage of the processor to another.
    
    BUG=b:241968258
    TEST=FEATURES="test" emerge-$BOARD missive
    
    Change-Id: I1e44d317effdab0e583c1b4cedfbc759366efc54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3953403
    Reviewed-by: Hong Xu <xuhong@google.com>
    Tested-by: Leonid Baraz <lbaraz@chromium.org>

--- missive/health/health_module.h
+++ missive/health/health_module.h
@@ -6,12 +6,15 @@
 #define MISSIVE_HEALTH_HEALTH_MODULE_H_
 
 #include <memory>
+#include <utility>
 
 #include <base/bind.h>
+#include <base/time/time.h>
 #include <base/sequence_checker.h>
 #include <base/memory/ref_counted.h>
 
 #include "missive/health/health_module_delegate.h"
+#include "missive/proto/health.pb.h"
 #include "missive/proto/record.pb.h"
 #include "missive/util/status.h"
 
@@ -23,6 +26,48 @@ namespace reporting {
 // are done with mutual exclusion
 class HealthModule : public base::RefCountedThreadSafe<HealthModule> {
  public:
+  // Instance of `Recorder` class provides an easy to use access for
+  // the caller to compose a single history record, which is posted when
+  // the instance is destructed.
+  // The class is non-copyable but moveable, so its instance may be handed
+  // over by std::move from one stage of the process to another, until it
+  // is destructed at the end of the processing (posting the accumulated
+  // health history).
+  class Recorder {
+   public:
+    // Recorder constructor is associated with health module;
+    // nullptr is used only when debugging not enabled.
+    explicit Recorder(scoped_refptr<HealthModule> health_module = nullptr)
+        : health_module_(health_module) {
+      if (health_module_) {
+        // Time in seconds since Epoch.
+        history_.set_timestamp_seconds(base::Time::Now().ToTimeT());
+      }
+    }
+
+    Recorder(Recorder&& other) = default;
+    Recorder& operator=(Recorder&& other) = default;
+    ~Recorder() {
+      if (health_module_) {
+        health_module_->PostHealthRecord(std::move(history_));
+      }
+    }
+
+    // Returns true if debuggung is active (health_module is present).
+    // When the result is false, other actions are not doing anything.
+    explicit operator bool() const noexcept {
+      return health_module_.get() != nullptr;
+    }
+
+    // Acccessors that present history record to be set up.
+    HealthDataHistory& operator*() noexcept { return history_; }
+    HealthDataHistory* operator->() noexcept { return &history_; }
+
+   private:
+    HealthDataHistory history_;
+    scoped_refptr<HealthModule> health_module_;
+  };
+
   // Static class factory method.
   static scoped_refptr<HealthModule> Create(
       std::unique_ptr<HealthModuleDelegate> delegate);
@@ -47,10 +92,10 @@ class HealthModule : public base::RefCountedThreadSafe<HealthModule> {
   virtual ~HealthModule();  // `virtual` is mandated by RefCounted.
 
  private:
-  // Task Runner which tasks are posted to.
-  scoped_refptr<base::SequencedTaskRunner> task_runner_;
-
   friend base::RefCountedThreadSafe<HealthModule>;
+
+  // Task Runner which tasks are posted to.
+  const scoped_refptr<base::SequencedTaskRunner> task_runner_;
 };
 }  // namespace reporting
 
