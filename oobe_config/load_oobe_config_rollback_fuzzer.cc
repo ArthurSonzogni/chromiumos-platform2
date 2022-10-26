717ae4e8e180469e06b169ba39bff8a32bb4e621
Victor-Gabriel Savu <vsavu@google.com>
2022-10-26 08:06:18 +0000

    oobe_config: Implement load_oobe_config_rollback_fuzzer
    Implement load_oobe_config_rollback_fuzzer to fuzz loading proto data
    from disk.
    
    BUG=b:249975249
    TEST=USE="asan fuzzer" build_packages --board=sarien --skip_chroot_upgrade oobe_config
    TEST=Run fuzzer locally
    
    Change-Id: I2edca4237ae0dc5fc875432fcb7285f8d6f775f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3977831
    Tested-by: Victor-Gabriel Savu <vsavu@google.com>
    Reviewed-by: Miriam Polzer <mpolzer@google.com>
    Commit-Queue: Victor-Gabriel Savu <vsavu@google.com>

--- oobe_config/load_oobe_config_rollback_fuzzer.cc
+++ oobe_config/load_oobe_config_rollback_fuzzer.cc
@@ -2,18 +2,83 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <cstddef>
-#include <cstdint>
+#include <memory>
+
+#include <libprotobuf-mutator/src/libfuzzer/libfuzzer_macro.h>
+#include <base/files/scoped_temp_dir.h>
+#include <base/json/json_reader.h>
+#include <base/logging.h>
+#include <base/files/file_path.h>
+#include <base/strings/strcat.h>
+#include <base/strings/string_number_conversions.h>
+
+#include "oobe_config/load_oobe_config_rollback.h"
+#include "oobe_config/oobe_config.h"
+#include "oobe_config/rollback_constants.h"
+#include "oobe_config/rollback_data.pb.h"
+#include "oobe_config/rollback_openssl_encryption.h"
 
 class Environment {
  public:
   Environment() {
-    // Set-up code.
+    logging::SetMinLogLevel(logging::LOGGING_FATAL);  // <- DISABLE LOGGING.
   }
 };
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+namespace oobe_config {
+
+namespace {
+
+// TODO(b/234826714): Remove.
+constexpr char kRollbackDataKey[] = "rollback_data";
+
+}  // namespace
+
+DEFINE_PROTO_FUZZER(const RollbackData& input) {
   static Environment env;
-  // Fuzzing code. Empty for now.
-  return 0;
+
+  base::ScopedTempDir fake_root_dir;
+  CHECK(fake_root_dir.CreateUniqueTempDir());
+
+  base::CreateDirectory(fake_root_dir.GetPath().Append(
+      base::FilePath(std::string(kRestoreTempPath).substr(1))));
+
+  std::string serialized_input;
+  CHECK(input.SerializeToString(&serialized_input));
+
+  OobeConfig oobe_config;
+  oobe_config.set_prefix_path_for_testing(fake_root_dir.GetPath());
+  LoadOobeConfigRollback load_config(&oobe_config);
+
+  auto encrypted_data = Encrypt(brillo::SecureBlob(serialized_input));
+  CHECK(encrypted_data.has_value());
+
+  // TODO(b/234826714): Pass data directly to load_config instead of relying on
+  // files.
+  CHECK(oobe_config.WriteFile(
+      base::FilePath(kUnencryptedStatefulRollbackDataFile),
+      brillo::BlobToString(encrypted_data->data)));
+
+  std::string hex_data_with_header =
+      base::StrCat({kRollbackDataKey, " ",
+                    base::HexEncode(encrypted_data->key.data(),
+                                    encrypted_data->key.size())});
+
+  CHECK(oobe_config.WriteFile(
+      base::FilePath(kPstorePath).Append("pmsg-ramoops-0"),
+      hex_data_with_header));
+
+  std::string config, enrollment_domain;
+  CHECK(load_config.GetOobeConfigJson(&config, &enrollment_domain));
+
+  auto root = base::JSONReader::Read(config);
+  CHECK(root);
+  auto dict = root->GetIfDict();
+  CHECK(dict != nullptr);
+
+  auto networkConfig = dict->FindString("networkConfig");
+  CHECK(networkConfig != nullptr);
+  CHECK_EQ(*networkConfig, input.network_config());
 }
+
+}  // namespace oobe_config
