e6bc8ce02e2f5996ac4876892d24930e65fc1ed2
Phil Kiossoglou <pkiossoglou@google.com>
2022-10-26 22:06:57 +1100

    power: remove string to number wrapper functions
    Remove a set of wrapper functions put in place to absorb libchrome
    update signature change in 2019.  TODO indicated they were to be removed
    after the uprev.
    
    BUG=b:231172820
    TEST=emerged, ran unit tests
    
    Change-Id: I2c12a0b3991d15c2b38d2e1eeb986d384e318913
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3984570
    Reviewed-by: David Greenaway <dgreenaway@google.com>
    Reviewed-by: Mengqi Guo <mqg@chromium.org>
    Commit-Queue: Phil Kiossoglou <pkiossoglou@chromium.org>
    Tested-by: Phil Kiossoglou <pkiossoglou@chromium.org>
    Auto-Submit: Phil Kiossoglou <pkiossoglou@chromium.org>

--- power_manager/common/util.cc
+++ power_manager/common/util.cc
@@ -44,21 +44,6 @@ bool ReadTypeFile(const base::FilePath& path,
   return true;
 }
 
-// Wrapper of string to number conversion function to absorb the libchrome
-// update signature change.
-// TODO(crbug.com/909719): Remove them after the uprev.
-bool StringToInt64(base::StringPiece s, int64_t* out) {
-  return base::StringToInt64(s, out);
-}
-
-bool StringToUint64(base::StringPiece s, uint64_t* out) {
-  return base::StringToUint64(s, out);
-}
-
-bool HexStringToUInt(base::StringPiece s, uint32_t* out) {
-  return base::HexStringToUInt(s, out);
-}
-
 }  // namespace
 
 double ClampPercent(double percent) {
@@ -120,15 +105,15 @@ bool MaybeReadStringFile(const base::FilePath& path, std::string* value_out) {
 }
 
 bool ReadInt64File(const base::FilePath& path, int64_t* value_out) {
-  return ReadTypeFile(path, StringToInt64, value_out);
+  return ReadTypeFile(path, base::StringToInt64, value_out);
 }
 
 bool ReadUint64File(const base::FilePath& path, uint64_t* value_out) {
-  return ReadTypeFile(path, StringToUint64, value_out);
+  return ReadTypeFile(path, base::StringToUint64, value_out);
 }
 
 bool ReadHexUint32File(const base::FilePath& path, uint32_t* value_out) {
-  return ReadTypeFile(path, HexStringToUInt, value_out);
+  return ReadTypeFile(path, base::HexStringToUInt, value_out);
 }
 
 bool DeleteFile(const base::FilePath& path) {
