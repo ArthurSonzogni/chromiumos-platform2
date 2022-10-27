4be6c1cbcbb13a62f4e088591fa8b34e9e612180
Pi-Hsun Shih <pihsun@chromium.org>
2022-10-27 15:47:21 +0800

    camera: hal_adapter: Post CameraDeviceAdapter::Close to ops thread
    HAL expects CloseDevice to be all called on the ops thread, and the
    CameraDeviceAdapter destructor is the only place that calls it on the
    other thread. Post the Close call to ops thread to solve this.
    
    BUG=b:255693467
    TEST=manually
    
    Change-Id: Idb476e88792cf4296f1a6c60495d86865f649574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3984475
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>

--- camera/hal_adapter/camera_device_adapter.cc
+++ camera/hal_adapter/camera_device_adapter.cc
@@ -203,7 +203,9 @@ CameraDeviceAdapter::~CameraDeviceAdapter() {
   VLOGF_ENTER() << ":" << camera_device_;
 
   // Make sure that the camera is closed when the device adapter is destructed.
-  Close();
+  camera_device_ops_thread_.task_runner()->PostTask(
+      FROM_HERE, base::BindOnce(base::IgnoreResult(&CameraDeviceAdapter::Close),
+                                base::Unretained(this)));
 
   camera_device_ops_thread_.task_runner()->PostTask(
       FROM_HERE,
