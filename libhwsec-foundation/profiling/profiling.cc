ecdf3dfd63b3002306978089d8137d1343983af6
Md Sadman Sakib <sadmansakib@google.com>
2022-11-01 17:44:16 +0800

    libhwsec-foundation: Added random int to make the profraw filename unique.
    BUG=b:256644148
    TEST=(chroot) USE=profiling emerge-nami libhwsec-foundation
    TEST=(dut) restart ${instrumented-daemon}
    TEST=(dut) observed expected profraw filename.
    
    Change-Id: I0fe4246a4095d3960682ecad9f7d2e75c35b142b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3993583
    Reviewed-by: Ching-Kang Yen <chingkang@chromium.org>
    Tested-by: Md Sadman Sakib <sadmansakib@google.com>
    Commit-Queue: Md Sadman Sakib <sadmansakib@google.com>
    Reviewed-by: Yi Chou <yich@google.com>

--- libhwsec-foundation/profiling/profiling.cc
+++ libhwsec-foundation/profiling/profiling.cc
@@ -13,6 +13,7 @@
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
 #include <base/logging.h>
+#include <base/rand_util.h>
 #include <base/strings/string_util.h>
 
 extern "C" {
@@ -25,7 +26,7 @@ namespace hwsec_foundation {
 #if ENABLE_PROFILING
 namespace {
 
-constexpr char kProcessCommmmandNameFilename[] = "/proc/self/comm";
+constexpr char kProcessCommandNameFilename[] = "/proc/self/comm";
 constexpr char kProfileFileDir[] =
     "/mnt/stateful_partition/unencrypted/profraws";
 constexpr char kProfileFileSuffix[] = "-%m-%p.profraw";
@@ -33,7 +34,7 @@ constexpr char kDefaultPrefix[] = "UNKNOWN";
 
 std::optional<std::string> GetProcessCommandName() {
   std::string name;
-  if (!base::ReadFileToString(base::FilePath(kProcessCommmmandNameFilename),
+  if (!base::ReadFileToString(base::FilePath(kProcessCommandNameFilename),
                               &name)) {
     return {};
   }
@@ -42,27 +43,42 @@ std::optional<std::string> GetProcessCommandName() {
   return name;
 }
 
+std::string ConstructFilename(std::string command_name) {
+  // Get a random uint64_t.
+  // It helps maintain unique profraw filenames.
+  // Previously, we were using instrumented binary's signature,
+  // daemon name, and PID. But it doesn't always guarantee
+  // uniqueness (i.e. PIDs might be same in different namespaces).
+  std::string random_int = std::to_string(base::RandUint64());
+
+  // Build the entire string.
+  const base::FilePath filename =
+      base::FilePath(kProfileFileDir)
+          .Append(base::FilePath(command_name + "-" + random_int +
+                                 kProfileFileSuffix));
+
+  return filename.value();
+}
+
 }  // namespace
 
 void SetUpProfiling() {
   std::string command_name = GetProcessCommandName().value_or(kDefaultPrefix);
+
   if (command_name == kDefaultPrefix) {
     LOG(WARNING) << ": Cannot fetch command name; use '" << kDefaultPrefix
                  << "' instead.";
   }
-
   const char* current_profile_path = __llvm_profile_get_filename();
+
   if (current_profile_path != nullptr && strlen(current_profile_path) != 0) {
     LOG(WARNING) << __func__ << ": Overriding the current profile path: "
                  << current_profile_path;
   }
+  std::string profile_file_path = ConstructFilename(command_name);
 
-  // Build the entire string.
-  const base::FilePath profile_file_path =
-      base::FilePath(kProfileFileDir)
-          .Append(base::FilePath(command_name + kProfileFileSuffix));
   // Set the destination filename for profraws.
-  __llvm_profile_set_filename(profile_file_path.value().c_str());
+  __llvm_profile_set_filename(profile_file_path.c_str());
 }
 
 #else
