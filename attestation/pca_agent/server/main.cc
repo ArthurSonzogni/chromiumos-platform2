162a7dd861ab8b6249f80896f31e5e09bed830af
Md Sadman Sakib <sadmansakib@google.com>
2022-11-02 04:26:26 +0800

    attestation: Implement code profiling for pca_agent.
    BUG=b:238843558
    TEST=(chroot) USE=profiling emerge-nami attestation
    TEST=(dut) restart pca_agentd
    
    Cq-Depend: chromium:3997732
    Change-Id: Ifedb35309262cb779bc3f19916b5a56aaa7cd58c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3997771
    Reviewed-by: Leo Lai <cylai@google.com>
    Commit-Queue: Md Sadman Sakib <sadmansakib@google.com>
    Tested-by: Md Sadman Sakib <sadmansakib@google.com>

--- attestation/pca_agent/server/main.cc
+++ attestation/pca_agent/server/main.cc
@@ -4,12 +4,17 @@
 
 #include <base/command_line.h>
 #include <brillo/syslog_logging.h>
+#include <libhwsec-foundation/profiling/profiling.h>
 
 #include "attestation/pca_agent/server/pca_agent_daemon.h"
 
 int main(int argc, char* argv[]) {
   base::CommandLine::Init(argc, argv);
   base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
+
+  // Start profiling.
+  hwsec_foundation::SetUpProfiling();
+
   int flags = brillo::kLogToSyslog;
   if (cl->HasSwitch("log_to_stderr")) {
     flags |= brillo::kLogToStderr;
