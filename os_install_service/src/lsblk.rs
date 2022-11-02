bb2faeb2218ab8da9bd1d7de59c296fbca83d619
Nicholas Bishop <nicholasbishop@google.com>
2022-11-02 14:35:19 -0400

    os_install_service: Remove support for old lsblk version
    At the time this code was written, the lsblk version on CrOS was
    2.32. In that version, JSON fields got encoded as strings even for data
    that should be a bool or a number. CrOS now has lsblk 2.36.2, which
    encodes bools and numbers in the appropriate type instead of a string,
    so we can drop support for the old version of lsblk.
    
    Also add an `Eq` derive to make clippy happy.
    
    BUG=b:257078794
    TEST=cros_workon_make --install --test os_install_service
    TEST=Test OS install in a VM, make sure it succeeds and result boots
    
    Change-Id: I36418f6fb800800e89fe880fe5987c91715fe25c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4000241
    Reviewed-by: Steve Kendall <skend@google.com>
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>

--- os_install_service/src/lsblk.rs
+++ os_install_service/src/lsblk.rs
@@ -3,17 +3,15 @@
 // found in the LICENSE file.
 
 use std::path::Path;
-use std::str::FromStr;
-use std::{fmt, process};
+use std::process;
 
 use anyhow::Error;
-use serde::de::{self, Visitor};
-use serde::{Deserialize, Deserializer};
+use serde::Deserialize;
 
 use crate::util::get_command_output;
 
 /// Struct for deserializing the JSON output of `lsblk`.
-#[derive(Clone, Debug, Deserialize, PartialEq)]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
 pub struct LsBlkDevice {
     /// Device name.
     ///
@@ -26,11 +24,11 @@ pub struct LsBlkDevice {
 
     /// Whether the device is removable. This is not completely
     /// reliable, for example USB SSDs may not show as removable.
-    #[serde(rename = "rm", deserialize_with = "deserialize_rm")]
+    #[serde(rename = "rm")]
     pub is_removable: bool,
 
     /// Size in bytes.
-    #[serde(rename = "size", deserialize_with = "deserialize_size")]
+    #[serde(rename = "size")]
     pub size_in_bytes: u64,
 
     /// Device type.
@@ -52,82 +50,6 @@ impl LsBlkDevice {
         let num_part = &self.name[index + 1..];
         num_part.parse().ok()
     }
-}
-
-/// Deserialize the "rm" field of an lsblk device.
-///
-/// This is a boolean field, but the representation depends on the
-/// version of lsblk. In 2.32 it uses "0" for false and "1" for
-/// true, in 2.36.1 it uses proper true and false values.
-fn deserialize_rm<'de, D>(deserializer: D) -> Result<bool, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    struct StringOrBool;
-
-    impl<'de> Visitor<'de> for StringOrBool {
-        type Value = bool;
-
-        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-            formatter.write_str("string or bool")
-        }
-
-        fn visit_str<E>(self, value: &str) -> Result<bool, E>
-        where
-            E: de::Error,
-        {
-            match value {
-                "0" => Ok(false),
-                "1" => Ok(true),
-                _ => Err(de::Error::custom("expected either '0' or '1'")),
-            }
-        }
-
-        fn visit_bool<E>(self, value: bool) -> Result<bool, E>
-        where
-            E: de::Error,
-        {
-            Ok(value)
-        }
-    }
-
-    deserializer.deserialize_any(StringOrBool)
-}
-
-/// Deserialize the "size" field of an lsblk device.
-///
-/// This is a numeric field, but the representation depends on the
-/// version of lsblk. In 2.32 it wraps the number in a string, in
-/// 2.36.1 it uses an actual number.
-fn deserialize_size<'de, D>(deserializer: D) -> Result<u64, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    struct StringOrU64;
-
-    impl<'de> Visitor<'de> for StringOrU64 {
-        type Value = u64;
-
-        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-            formatter.write_str("string or u64")
-        }
-
-        fn visit_str<E>(self, value: &str) -> Result<u64, E>
-        where
-            E: de::Error,
-        {
-            FromStr::from_str(value).map_err(|_| de::Error::custom("cannot parse string as u64"))
-        }
-
-        fn visit_u64<E>(self, value: u64) -> Result<u64, E>
-        where
-            E: de::Error,
-        {
-            Ok(value)
-        }
-    }
-
-    deserializer.deserialize_any(StringOrU64)
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
@@ -272,84 +194,28 @@ mod tests {
 
         #[rustfmt::skip]
         let expected = vec![
-            mkdev("/dev/sda", "/dev/sda", false, 6807435776, "disk"),
-            mkdev("/dev/sda12", "/dev/sda12", false, 134217728, "part"),
-            mkdev("/dev/sda11", "/dev/sda11", false, 8388608, "part"),
+            mkdev("/dev/sda", "/dev/sda", false, 6939566592, "disk"),
+            mkdev("/dev/sda12", "/dev/sda12", false, 67108864, "part"),
+            mkdev("/dev/sda11", "/dev/sda11", false, 512, "part"),
             mkdev("/dev/sda10", "/dev/sda10", false, 512, "part"),
             mkdev("/dev/sda9", "/dev/sda9", false, 512, "part"),
             mkdev("/dev/sda8", "/dev/sda8", false, 16777216, "part"),
             mkdev("/dev/sda7", "/dev/sda7", false, 512, "part"),
             mkdev("/dev/sda6", "/dev/sda6", false, 512, "part"),
             mkdev("/dev/sda5", "/dev/sda5", false, 2097152, "part"),
-            mkdev("/dev/sda4", "/dev/sda4", false, 67108864, "part"),
-            mkdev("/dev/sda3", "/dev/sda3", false, 2147483648, "part"),
-            mkdev("/dev/sda2", "/dev/sda2", false, 67108864, "part"),
-            mkdev("/dev/sda1", "/dev/sda1", false, 4295023104, "part"),
-            mkdev("/dev/loop1", "/dev/loop1", false, 113876992, "loop"),
-            mkdev("/dev/loop0", "/dev/loop0", false, 1248124928, "loop"),
-            mkdev("/dev/dm-0", "/dev/mapper/encstateful", false, 1248124928, "dm"),
+            mkdev("/dev/sda4", "/dev/sda4", false, 16777216, "part"),
+            mkdev("/dev/sda3", "/dev/sda3", false, 2516582400, "part"),
+            mkdev("/dev/sda2", "/dev/sda2", false, 16777216, "part"),
+            mkdev("/dev/sda1", "/dev/sda1", false, 4301324800, "part"),
+            mkdev("/dev/loop4", "/dev/loop4", false, 6475776, "loop"),
+            mkdev("/dev/loop3", "/dev/loop3", false, 9670656, "loop"),
+            mkdev("/dev/loop2", "/dev/loop2", false, 102133760, "loop"),
+            mkdev("/dev/loop1", "/dev/loop1", false, 1243103232, "loop"),
+            mkdev("/dev/dm-1", "/dev/mapper/encstateful", false, 1243103232, "dm"),
+            mkdev("/dev/loop0", "/dev/loop0", false, 4096, "loop"),
         ];
 
         let output = LsBlkOutput::parse(input).unwrap();
         assert_eq!(output.flattened(), expected);
-    }
-
-    fn mkdev2(
-        kname: &str,
-        name: &str,
-        is_removable: bool,
-        size_in_bytes: u64,
-        dtype: &str,
-    ) -> LsBlkDevice {
-        LsBlkDevice {
-            kernel_name: kname.into(),
-            name: name.into(),
-            is_removable,
-            size_in_bytes,
-            device_type: dtype.into(),
-        }
-    }
-
-    #[test]
-    fn test_lsblk_deserialization_v2_36_1() {
-        // This test input was generated by running this command on a
-        // machine with lsblk v2.36.1 (newer than what CrOS currently
-        // has).
-        //
-        //     lsblk --bytes --output KNAME,NAME,RM,SIZE,TYPE \
-        //         --json --paths --exclude 2,11,253
-        let input = include_bytes!("test_lsblk_output_v2_36_1.json");
-
-        #[rustfmt::skip]
-        let expected = vec![
-            mkdev2("/dev/nvme0n1", "/dev/nvme0n1", false, 1024209543168, "disk"),
-            mkdev2("/dev/nvme0n1p3", "/dev/nvme0n1p3", false, 1020208477696, "part"),
-            mkdev2("/dev/nvme0n1p2", "/dev/nvme0n1p2", false, 2000000000, "part"),
-            mkdev2("/dev/nvme0n1p1", "/dev/nvme0n1p1", false, 2000000000, "part"),
-            mkdev2("/dev/nvme1n1", "/dev/nvme1n1", false, 1000204886016, "disk"),
-        ];
-
-        let output = LsBlkOutput::parse(input).unwrap();
-        assert_eq!(output.flattened(), expected);
-    }
-
-    #[test]
-    fn test_lsblk_removable_deserialization() {
-        // The "rm" field can be either a string or a bool depending
-        // on the version of lsblk, verify that both work.
-        let device: Vec<LsBlkDevice> = serde_json::from_value(serde_json::json!(
-            [
-                {"name":"a", "kname": "b", "rm":false, "size":"1", "type":"part"},
-                {"name":"a", "kname": "b", "rm":true, "size":"1", "type":"part"},
-
-                {"name":"a", "kname": "b", "rm":"0", "size":"1", "type":"part"},
-                {"name":"a", "kname": "b", "rm":"1", "size":"1", "type":"part"}
-            ]
-        ))
-        .unwrap();
-        assert_eq!(
-            device.iter().map(|d| d.is_removable).collect::<Vec<_>>(),
-            vec![false, true, false, true]
-        );
     }
 }
