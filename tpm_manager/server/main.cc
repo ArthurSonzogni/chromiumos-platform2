be8521e282c182ebbfcc07293bc6c505153f091b
Sam Chen <chensa@google.com>
2022-11-08 12:07:12 +0800

    tpm_manager: Move auto init TPM checking outside main program
    Use an new program "check_tpm_preinit_condition" to check if is it
    running from installer or booting from removable devices and touch
    "/run/tpm_manager/no_preinit" if true. Run this at the pre-start phase
    of tpm_managerd.
    
    This is a prior change for launching tpm_managerd using minijail,
    since it's complicated to make it work in minijail.
    
    BUG=b:208780592
    TEST=unittests of tpm_manager
    TEST=tast run $DUT hwsec.Login
    
    Change-Id: Ib9805d1c40c10ba32710b842fe93b2feeac34716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4012183
    Tested-by: Sam Chen <chensa@google.com>
    Reviewed-by: Yi Chou <yich@google.com>
    Reviewed-by: Leo Lai <cylai@google.com>
    Reviewed-by: Betul Soysal <betuls@google.com>
    Commit-Queue: Sam Chen <chensa@google.com>

--- tpm_manager/server/main.cc
+++ tpm_manager/server/main.cc
@@ -9,16 +9,12 @@
 #include <base/command_line.h>
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
-#include <base/strings/string_number_conversions.h>
-#include <base/strings/string_util.h>
 #include <base/logging.h>
-#include <base/process/launch.h>
 #include <brillo/syslog_logging.h>
 #include <libhwsec-foundation/tpm/tpm_version.h>
 #include <libhwsec-foundation/tpm_error/tpm_error_uma_reporter.h>
 #include <libhwsec-foundation/profiling/profiling.h>
 #include <linux/limits.h>
-#include <rootdev/rootdev.h>
 #include <tpm_manager-client/tpm_manager/dbus-constants.h>
 #if USE_TPM2
 #include <trunks/trunks_factory_impl.h>
@@ -33,83 +29,6 @@ namespace {
 
 constexpr char kLogToStderrSwitch[] = "log_to_stderr";
 constexpr char kNoPreinitFlagFile[] = "/run/tpm_manager/no_preinit";
-
-constexpr char kIsRunningFromInstaller[] = "is_running_from_installer";
-constexpr char kInstallerYes[] = "yes\n";
-
-constexpr char kDevDir[] = "/dev/";
-constexpr char kSysBlock[] = "/sys/block/";
-constexpr char kRemovable[] = "removable";
-
-std::string GetBootDeviceName() {
-  char path[PATH_MAX];
-  int ret = rootdev(path, sizeof(path), /* full resolution = */ true,
-                    /* remove partition = */ true);
-  if (ret != 0) {
-    LOG(WARNING) << "rootdev failed with error code: " << ret;
-    return "";
-  }
-
-  std::string boot_path(path);
-  if (boot_path.substr(0, sizeof(kDevDir) - 1) != kDevDir) {
-    LOG(WARNING) << "Unknown device prefix: " << boot_path;
-    return "";
-  }
-
-  return boot_path.substr(sizeof(kDevDir) - 1);
-}
-
-bool IsBootFromRemoveableDevice() {
-  base::FilePath file =
-      base::FilePath(kSysBlock).Append(GetBootDeviceName()).Append(kRemovable);
-
-  std::string file_content;
-
-  if (!base::ReadFileToString(file, &file_content)) {
-    return false;
-  }
-
-  std::string removable_str;
-  base::TrimWhitespaceASCII(file_content, base::TRIM_ALL, &removable_str);
-
-  int removable = 0;
-  if (!base::StringToInt(removable_str, &removable)) {
-    LOG(WARNING) << "removable is not a number: " << removable_str;
-    return false;
-  }
-
-  return removable;
-}
-
-bool PerformPreinit() {
-  if (base::PathExists(base::FilePath(kNoPreinitFlagFile))) {
-    return false;
-  }
-
-  if (USE_OS_INSTALL_SERVICE) {
-    // We should not preinit the TPM if we are running the OS from the
-    // installer.
-    std::string output;
-    if (!base::GetAppOutput({kIsRunningFromInstaller}, &output)) {
-      LOG(ERROR) << "Failed to run is_running_from_installer";
-    }
-
-    if (output == kInstallerYes) {
-      return false;
-    }
-
-    return true;
-  }
-
-  // Normal ChromeOS case.
-  if (IsBootFromRemoveableDevice()) {
-    // Don't perform preinit when we are booting from removable device.
-    // Because we may not store the data at correct location.
-    return false;
-  }
-
-  return true;
-}
 
 }  // namespace
 
@@ -131,7 +50,7 @@ int main(int argc, char* argv[]) {
   brillo::InitLog(flags);
 
   tpm_manager::LocalDataStoreImpl local_data_store;
-  bool perform_preinit = PerformPreinit();
+  bool perform_preinit = !base::PathExists(base::FilePath(kNoPreinitFlagFile));
 
   std::unique_ptr<tpm_manager::TpmManagerService> tpm_manager_service{
       new tpm_manager::TpmManagerService(perform_preinit, &local_data_store)};
