428410af7b6598036796ae8101237a3ad5434fdf
Md Sadman Sakib <sadmansakib@google.com>
2022-11-08 18:22:31 +0800

    bootlockbox: Implement code profiling.
    BUG=b:238843558
    TEST=(chroot) USE=profiling emerge-${BOARD} bootlockbox
    TEST=(dut) restart bootlockbox
    
    Cq-Depend: chromium:4011612
    Change-Id: I747fd02f4c30a419901e53a9c8b593fe0a68dd38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4011611
    Tested-by: Md Sadman Sakib <sadmansakib@google.com>
    Reviewed-by: Yi Chou <yich@google.com>
    Commit-Queue: Md Sadman Sakib <sadmansakib@google.com>

--- bootlockbox/boot_lockboxd.cc
+++ bootlockbox/boot_lockboxd.cc
@@ -3,11 +3,15 @@
 // found in the LICENSE file.
 
 #include <brillo/syslog_logging.h>
+#include <libhwsec-foundation/profiling/profiling.h>
 
 #include "bootlockbox/boot_lockbox_service.h"
 
 int main(int argc, char** argv) {
   brillo::InitLog(brillo::kLogToSyslog | brillo::kLogToStderr);
+
+  // Start profiling.
+  hwsec_foundation::SetUpProfiling();
 
   bootlockbox::BootLockboxService service;
   service.Run();
