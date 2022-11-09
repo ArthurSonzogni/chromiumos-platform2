d4189fd9187017b6457984b97d325166c64f1fb5
Grace Cham <hscham@google.com>
2022-11-09 11:12:19 +0800

    nnapi: migrate to base::ImmediateCrash
    IMMEDIATE_CRASH is removed in libchrome r1061911.
    
    BUG=b:258269774
    TEST=CQ
    
    Change-Id: I964810cb234bb1e8c39ef39a57508e01fb05745c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4016079
    Tested-by: Grace Cham <hscham@chromium.org>
    Commit-Queue: Jim Pollock <jmpollock@chromium.org>
    Reviewed-by: Brett Brotherton <bbrotherton@google.com>
    Commit-Queue: Jack Shen <jackshen@google.com>
    Auto-Submit: Grace Cham <hscham@chromium.org>
    Reviewed-by: Jack Shen <jackshen@google.com>
    Reviewed-by: Jim Pollock <jmpollock@chromium.org>

--- nnapi/native/binder_stubs.cpp
+++ nnapi/native/binder_stubs.cpp
@@ -27,40 +27,34 @@ AStatus* AStatus_newOk() {
 
 bool AStatus_isOk(const AStatus*) {
   NOTIMPLEMENTED() << "AStatus_isOk shouldn't get called";
-  IMMEDIATE_CRASH();
-  return false;
+  base::ImmediateCrash();
 }
 
 AStatus* AStatus_fromStatus(binder_status_t) {
   NOTIMPLEMENTED() << "AStatus_fromStatus shouldn't get called";
-  IMMEDIATE_CRASH();
-  return nullptr;
+  base::ImmediateCrash();
 }
 
 int32_t AParcel_getDataPosition(const AParcel*) {
   NOTIMPLEMENTED() << "AParcel_getDataPosition shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readByteArray(const AParcel*,
                                       void*,
                                       AParcel_byteArrayAllocator) {
   NOTIMPLEMENTED() << "AParcel_readByteArray shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeByteArray(AParcel*, const int8_t*, int32_t) {
   NOTIMPLEMENTED() << "AParcel_writeByteArray shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeFloatArray(AParcel*, const float*, int32_t) {
   NOTIMPLEMENTED() << "AParcel_writeFloatArray shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readBoolArray(const AParcel*,
@@ -68,72 +62,61 @@ binder_status_t AParcel_readBoolArray(const AParcel*,
                                       AParcel_boolArrayAllocator,
                                       AParcel_boolArraySetter) {
   NOTIMPLEMENTED() << "AParcel_readBoolArray shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readFloatArray(const AParcel*,
                                        void*,
                                        AParcel_floatArrayAllocator) {
   NOTIMPLEMENTED() << "AParcel_readFloatArray shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readParcelFileDescriptor(const AParcel*, int*) {
   NOTIMPLEMENTED() << "AParcel_readParcelFileDescriptor shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeParcelFileDescriptor(AParcel*, int) {
   NOTIMPLEMENTED() << "AParcel_writeParcelFileDescriptor shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readFloat(const AParcel*, float*) {
   NOTIMPLEMENTED() << "AParcel_readFloat shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeFloat(AParcel*, float) {
   NOTIMPLEMENTED() << "AParcel_writeFloat shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeStrongBinder(AParcel*, AIBinder*) {
   NOTIMPLEMENTED() << "AParcel_writeStrongBinder shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readStrongBinder(const AParcel*, AIBinder**) {
   NOTIMPLEMENTED() << "AParcel_readStrongBinder shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readInt32Array(const AParcel*,
                                        void*,
                                        AParcel_int32ArrayAllocator) {
   NOTIMPLEMENTED() << "AParcel_readInt32Array shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeInt32Array(AParcel*, const int32_t*, int32_t) {
   NOTIMPLEMENTED() << "AParcel_writeInt32Array shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readStatusHeader(const AParcel*, AStatus**) {
   NOTIMPLEMENTED() << "AParcel_readStatusHeader shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readParcelableArray(const AParcel*,
@@ -141,26 +124,22 @@ binder_status_t AParcel_readParcelableArray(const AParcel*,
                                             AParcel_parcelableArrayAllocator,
                                             AParcel_readParcelableElement) {
   NOTIMPLEMENTED() << "AParcel_readParcelableArray shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_setDataPosition(const AParcel*, int32_t) {
   NOTIMPLEMENTED() << "AParcel_setDataPosition shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readChar(const AParcel*, char16_t*) {
   NOTIMPLEMENTED() << "AParcel_readChar shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeChar(AParcel*, char16_t) {
   NOTIMPLEMENTED() << "AParcel_writeChar shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeParcelableArray(AParcel*,
@@ -168,135 +147,117 @@ binder_status_t AParcel_writeParcelableArray(AParcel*,
                                              int32_t,
                                              AParcel_writeParcelableElement) {
   NOTIMPLEMENTED() << "AParcel_writeParcelableArray shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeStatusHeader(AParcel*, const AStatus*) {
   NOTIMPLEMENTED() << "AParcel_writeStatusHeader shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeInt32(AParcel*, int32_t) {
   NOTIMPLEMENTED() << "AParcel_writeInt32 shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeInt64(AParcel*, int64_t) {
   NOTIMPLEMENTED() << "AParcel_writeInt64 shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readString(const AParcel*,
                                    void*,
                                    AParcel_stringAllocator) {
   NOTIMPLEMENTED() << "AParcel_readString shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readInt64(const AParcel*, int64_t*) {
   NOTIMPLEMENTED() << "AParcel_readInt64 shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readInt32(const AParcel*, int32_t*) {
   NOTIMPLEMENTED() << "AParcel_readInt32 shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readBool(const AParcel*, bool*) {
   NOTIMPLEMENTED() << "AParcel_readBool shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeString(AParcel*, const char*, int32_t) {
   NOTIMPLEMENTED() << "AParcel_writeString shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_readInt64Array(const AParcel*,
                                        void*,
                                        AParcel_int64ArrayAllocator) {
   NOTIMPLEMENTED() << "AParcel_readInt64Array shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeInt64Array(AParcel*, const int64_t*, int32_t) {
   NOTIMPLEMENTED() << "AParcel_writeInt64Array shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 binder_status_t AParcel_writeBool(AParcel*, bool) {
   NOTIMPLEMENTED() << "AParcel_writeBool shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 void AParcel_delete(AParcel*) {
   NOTIMPLEMENTED() << "AParcel_delete shouldn't get called";
-  IMMEDIATE_CRASH();
+  base::ImmediateCrash();
 }
 
 binder_status_t AIBinder_prepareTransaction(AIBinder*, AParcel**) {
   NOTIMPLEMENTED() << "AIBinder_prepareTransaction shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 void AIBinder_decStrong(AIBinder*) {
   NOTIMPLEMENTED() << "AIBinder_decStrong shouldn't get called";
-  IMMEDIATE_CRASH();
+  base::ImmediateCrash();
 }
 
 binder_status_t AIBinder_transact(
     AIBinder*, transaction_code_t, AParcel**, AParcel**, binder_flags_t) {
   NOTIMPLEMENTED() << "AIBinder_transact shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 void AIBinder_Weak_delete(AIBinder_Weak*) {
   NOTIMPLEMENTED() << "AIBinder_Weak_delete shouldn't get called";
-  IMMEDIATE_CRASH();
+  base::ImmediateCrash();
 }
 
 AIBinder* AIBinder_new(const AIBinder_Class*, void*) {
   NOTIMPLEMENTED() << "AIBinder_new shouldn't get called";
-  IMMEDIATE_CRASH();
-  return nullptr;
+  base::ImmediateCrash();
 }
 
 bool AIBinder_associateClass(AIBinder*, const AIBinder_Class*) {
   NOTIMPLEMENTED() << "AIBinder_associateClass shouldn't get called";
-  IMMEDIATE_CRASH();
-  return false;
+  base::ImmediateCrash();
 }
 
 bool AIBinder_isRemote(const AIBinder*) {
   NOTIMPLEMENTED() << "AIBinder_isRemote shouldn't get called";
-  IMMEDIATE_CRASH();
-  return false;
+  base::ImmediateCrash();
 }
 
 binder_status_t AIBinder_dump(AIBinder*, int, const char**, uint32_t) {
   NOTIMPLEMENTED() << "AIBinder_dump shouldn't get called";
-  IMMEDIATE_CRASH();
-  return 0;
+  base::ImmediateCrash();
 }
 
 void AIBinder_incStrong(AIBinder*) {
   NOTIMPLEMENTED() << "AIBinder_incStrong shouldn't get called";
-  IMMEDIATE_CRASH();
+  base::ImmediateCrash();
 }
 
 AIBinder_Class* AIBinder_Class_define(const char*,
@@ -304,13 +265,12 @@ AIBinder_Class* AIBinder_Class_define(const char*,
                                       AIBinder_Class_onDestroy,
                                       AIBinder_Class_onTransact) {
   NOTIMPLEMENTED() << "AIBinder_Class_define shouldn't get called";
-  IMMEDIATE_CRASH();
-  return nullptr;
+  base::ImmediateCrash();
 }
 
 void AIBinder_Class_setOnDump(AIBinder_Class*, AIBinder_onDump) {
   NOTIMPLEMENTED() << "AIBinder_Class_setOnDump shouldn't get called";
-  IMMEDIATE_CRASH();
+  base::ImmediateCrash();
 }
 
 binder_status_t AIBinder_ping(AIBinder* /*binder*/) {
