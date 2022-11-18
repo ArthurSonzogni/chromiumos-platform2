b1f9953c8ef5dbb400f235a22225e5476d610b74
David Greenaway <dgreenaway@google.com>
2022-11-18 15:35:57 +1100

    faced: Add internal locking to FakeCameraService
    We have been seeing some flakes of the camera tests in CQ (b/255460130),
    where the camera tests fail with a bad pointer dereference:
    
        [ RUN      ] CameraClientTest.CaptureFrames
        camera_unittests: [camera_client.cc(157)] Starting capture: device = 0, fourcc = NV12, width = 1920, height = 1080, fps = 30
        camera_unittests: [camera_client.cc(224)] Stopping capture on camera: 0
        Error: /var/cache/portage/chromeos-base/faced/out/Default/camera_unittests: failed with signal SIGSEGV(11)
    
    Though I haven't been able to reproduce the same bug as in CQ, I have
    been able to trigger TSAN failures by stress testing the camera unit
    tests (in particular, running large numbers of the test in parallel
    repeatedly).
    
    This CL adds locks to the FakeCameraService class to avoid racey
    accesses on its internal members. After this CL, stress testing the
    `camera_unittests` binary no longer produces TSAN warnings.
    
    BUG=b:255460130
    TEST=Described above.
    
    Change-Id: Iaef41f6e4d80be8e5470486a5285dd2427503ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4037368
    Reviewed-by: Ben Kersten <bkersten@google.com>
    Commit-Queue: David Greenaway <dgreenaway@google.com>
    Tested-by: David Greenaway <dgreenaway@google.com>

--- faced/camera/fake_camera_service.cc
+++ faced/camera/fake_camera_service.cc
@@ -6,33 +6,80 @@
 
 #include <vector>
 
+#include <base/logging.h>
 #include <base/task/thread_pool.h>
+#include <base/time/time.h>
 
 namespace faced::testing {
 
+namespace {
+
+// Time between frames being sent.
+//
+// We schedule the frames to be sent at 30 FPS (i.e., 33ms), though we expect
+// tests to be using a mock clock in practice.
+constexpr base::TimeDelta kFrameInterval = base::Milliseconds(33);
+
+}  // namespace
+
+FakeCameraService::FakeCameraService()
+    : ops_runner_(
+          base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()})) {}
+
+FakeCameraService::~FakeCameraService() {
+  base::AutoLock lock(mutex_);
+  CHECK_NE(state_, State::kCapturing)
+      << "Attempted to destroy FakeCameraService while a capture was in "
+         "progress.";
+}
+
 void FakeCameraService::AddCameraInfo(cros_cam_info_t cam_info,
                                       bool is_removed) {
+  base::AutoLock lock(mutex_);
   camera_infos_.push_back(cam_info);
   camera_is_removed_.push_back(is_removed);
 }
 
 void FakeCameraService::AddResult(cros_cam_capture_result_t result) {
+  base::AutoLock lock(mutex_);
   results_.push_back(result);
 }
 
 int FakeCameraService::Init() {
-  ops_runner_ = base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()});
+  base::AutoLock lock(mutex_);
+
+  CHECK_EQ(state_, State::kCreated)
+      << "Attempted to call Init() in incorrect state.";
+  state_ = State::kIdle;
+
   return 0;
 }
 
 int FakeCameraService::Exit() {
+  base::AutoLock lock(mutex_);
+
+  CHECK_EQ(state_, State::kIdle)
+      << "Attempted to call Exit() in incorrect state.";
+  state_ = State::kExit;
+
   return 0;
 }
 
 int FakeCameraService::GetCameraInfo(cros_cam_get_cam_info_cb_t callback,
                                      void* context) {
-  for (int i = 0; i < camera_infos_.size(); i++) {
-    if ((*callback)(context, &camera_infos_[i], camera_is_removed_[i]) != 0) {
+  // Make a copy of our camera information data structures, so we can issue
+  // callbacks below without holding a lock.
+  std::vector<cros_cam_info_t> camera_infos;
+  std::vector<bool> camera_is_removed;
+  {
+    base::AutoLock lock(mutex_);
+    camera_infos = camera_infos_;
+    camera_is_removed = camera_is_removed_;
+  }
+
+  // Call the user's callback once for each camera info.
+  for (int i = 0; i < camera_infos.size(); i++) {
+    if ((*callback)(context, &camera_infos[i], camera_is_removed[i]) != 0) {
       return 1;
     }
   }
@@ -43,52 +90,74 @@ int FakeCameraService::GetCameraInfo(cros_cam_get_cam_info_cb_t callback,
 int FakeCameraService::StartCapture(const cros_cam_capture_request_t* request,
                                     cros_cam_capture_cb_t callback,
                                     void* context) {
+  base::AutoLock lock(mutex_);
+
+  // Update current state.
+  CHECK_EQ(state_, State::kIdle)
+      << "Called StartCapture() when the camera service was not in an idle "
+         "state.";
+  state_ = State::kCapturing;
   camera_id_ = request->id;
-  ops_runner_->PostTask(
+
+  // Schedule the first frame.
+  ops_runner_->PostDelayedTask(
       FROM_HERE,
-      base::BindOnce(&FakeCameraService::StartCaptureCallback,
-                     base::Unretained(this), request, callback, context));
+      base::BindOnce(&FakeCameraService::DispatchNextFrame,
+                     base::Unretained(this), request, callback, context),
+      kFrameInterval);
   return 0;
 }
 
 int FakeCameraService::StopCapture(int id) {
-  ops_runner_->PostTask(FROM_HERE,
-                        base::BindOnce(&FakeCameraService::StopCaptureCallback,
-                                       base::Unretained(this)));
+  base::AutoLock lock(mutex_);
+
+  CHECK_EQ(state_, State::kCapturing)
+      << "Attempted to call StopCapture when no capture was in progress.";
+  CHECK_EQ(id, camera_id_)
+      << "Called StopCapture() with the incorrect camera ID.";
+
+  // The camera service API guarantees that when StopCapture() returns,
+  // no new callsback will be issued.
+  state_ = State::kIdle;
+
   return 0;
 }
 
-void FakeCameraService::StartCaptureCallback(
+void FakeCameraService::DispatchNextFrame(
     const cros_cam_capture_request_t* request,
     cros_cam_capture_cb_t callback,
     void* context) {
-  DCHECK(ops_runner_->RunsTasksInCurrentSequence());
+  CHECK(ops_runner_->RunsTasksInCurrentSequence());
 
+  base::AutoLock lock(mutex_);
+
+  // If StopCapture() has been called, don't dispatch any more frames.
+  if (state_ != State::kCapturing) {
+    return;
+  }
+
+  // If we have run out of frames, just stall the stream.
   if (results_.empty()) {
     return;
   }
 
+  // Fetch the next frame, and send it to the user's callback.
+  //
+  // If a non-zero value is returned, we stop the current capture.
   cros_cam_capture_result_t result = results_.front();
   results_.pop_front();
-
   bool should_continue = ((*callback)(context, &result) == 0);
-
   if (!should_continue) {
-    StopCapture(camera_id_);
+    state_ = State::kIdle;
     return;
   }
 
-  // Simulate 30fps
+  // Schedule the next frame.
   ops_runner_->PostDelayedTask(
       FROM_HERE,
-      base::BindOnce(&FakeCameraService::StartCaptureCallback,
+      base::BindOnce(&FakeCameraService::DispatchNextFrame,
                      base::Unretained(this), request, callback, context),
-      base::Milliseconds(33));
-}
-
-void FakeCameraService::StopCaptureCallback() {
-  DCHECK(ops_runner_->RunsTasksInCurrentSequence());
-  results_.clear();
+      kFrameInterval);
 }
 
 }  // namespace faced::testing
--- faced/camera/fake_camera_service.h
+++ faced/camera/fake_camera_service.h
@@ -9,7 +9,9 @@
 #include <string>
 #include <vector>
 
+#include <base/synchronization/lock.h>
 #include <base/task/thread_pool.h>
+#include <base/thread_annotations.h>
 
 #include "faced/camera/camera_service.h"
 
@@ -18,7 +20,12 @@ namespace faced::testing {
 // FakeCameraService provides fake data for tests
 class FakeCameraService : public CameraService {
  public:
-  FakeCameraService() = default;
+  FakeCameraService();
+  ~FakeCameraService() override;
+
+  // Disallow copy and move.
+  FakeCameraService(const FakeCameraService&) = delete;
+  FakeCameraService operator=(const FakeCameraService&) = delete;
 
   // Helper function to add test camera infos
   void AddCameraInfo(cros_cam_info_t cam_info, bool is_removed);
@@ -45,21 +52,35 @@ class FakeCameraService : public CameraService {
   int StopCapture(int id) override;
 
  private:
-  // Callback for continually feeding test results on sequenced task runner
-  void StartCaptureCallback(const cros_cam_capture_request_t* request,
-                            cros_cam_capture_cb_t callback,
-                            void* context);
+  // Send the next frame to our client.
+  void DispatchNextFrame(const cros_cam_capture_request_t* request,
+                         cros_cam_capture_cb_t callback,
+                         void* context);
 
-  void StopCaptureCallback();
+  base::Lock mutex_;
+
+  // Current state of the camera service.
+  enum class State {
+    kCreated,    // Created, but Init() not yet called.
+    kIdle,       // Ready to capture.
+    kCapturing,  // Actively capturing
+    kExit,       // Exit() called. The instance cannot be used again.
+  };
+  State state_ GUARDED_BY(mutex_) = State::kCreated;
 
   // Data for tests
-  std::vector<cros_cam_info_t> camera_infos_;
-  std::vector<bool> camera_is_removed_;
+  std::vector<cros_cam_info_t> camera_infos_ GUARDED_BY(mutex_);
+  std::vector<bool> camera_is_removed_ GUARDED_BY(mutex_);
+  std::deque<cros_cam_capture_result_t> results_ GUARDED_BY(mutex_);
 
-  std::deque<cros_cam_capture_result_t> results_;
-  int camera_id_;
+  // The ID of the camera currently being captured from.
+  int camera_id_ GUARDED_BY(mutex_) = -1;
 
-  // Runner for getting camera images
+  // Runner for calling the user's capture callback on.
+  //
+  // The real camera service calls the callback from a separate thread, so we do
+  // the same in the fake camera to emulate it (and ideally trigger the same
+  // races the real camera service would trigger).
   scoped_refptr<base::SequencedTaskRunner> ops_runner_;
 };
 
