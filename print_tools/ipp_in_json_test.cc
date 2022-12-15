f16a23b057b7430febe852aa8691296480393dfb
Piotr Pawliczek <pawliczek@google.com>
2022-12-15 20:08:06 +0000

    print_tools: Add missing unit tests
    Add new unit tests for the file ipp_in_json.cc.
    
    BUG=b:248023854
    TEST=FEATURES=test emerge-amd64-generic print_tools
    
    Change-Id: Ia461e10fd5ec3252204b8c3b956dfb2745b44316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4112823
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Reviewed-by: Mason Wilde <masonwilde@google.com>
    Tested-by: Piotr Pawliczek <pawliczek@chromium.org>

--- print_tools/ipp_in_json_test.cc
+++ print_tools/ipp_in_json_test.cc
@@ -23,14 +23,37 @@ TEST(IppToJson, StringAttribute) {
   ASSERT_EQ(frame.AddGroup(ipp::GroupTag::document_attributes, &grp),
             ipp::Code::kOK);
   ASSERT_NE(grp, nullptr);
-  EXPECT_EQ(grp->AddAttr("test-attr", ipp::ValueTag::mimeMediaType, "value"),
+  EXPECT_EQ(
+      grp->AddAttr("test-attr", ipp::ValueTag::textWithoutLanguage, "value"),
+      ipp::Code::kOK);
+
+  std::string json;
+  EXPECT_TRUE(ConvertToJson(frame, log, true, &json));
+  EXPECT_EQ(json,
+            R"({"response":{"document-attributes":{)"
+            R"("test-attr":{"type":"textWithoutLanguage","value":"value"})"
+            R"(}},"status":"client-error-gone"})");
+}
+
+TEST(IppToJson, StringWithLanguageAttribute) {
+  ipp::Collection* grp = nullptr;
+  ipp::ParsingResults log{.whole_buffer_was_parsed = true};
+
+  ipp::Frame frame(ipp::Version::_2_0, ipp::Status::client_error_gone, 1,
+                   false);
+  ASSERT_EQ(frame.AddGroup(ipp::GroupTag::document_attributes, &grp),
+            ipp::Code::kOK);
+  ASSERT_NE(grp, nullptr);
+  EXPECT_EQ(grp->AddAttr("test-attr", ipp::ValueTag::textWithLanguage,
+                         ipp::StringWithLanguage("Value", "Language")),
             ipp::Code::kOK);
 
   std::string json;
   EXPECT_TRUE(ConvertToJson(frame, log, true, &json));
   EXPECT_EQ(json, R"({"response":{"document-attributes":{)"
-                  R"("test-attr":{"type":"mimeMediaType","value":"value"})"
-                  R"(}},"status":"client-error-gone"})");
+                  R"("test-attr":{"type":"textWithLanguage",)"
+                  R"("value":{"language":"Language","value":"Value"})"
+                  R"(}}},"status":"client-error-gone"})");
 }
 
 TEST(IppToJson, IntegerAttribute) {
@@ -50,6 +73,23 @@ TEST(IppToJson, IntegerAttribute) {
                   R"(}},"status":"successful-ok"})");
 }
 
+TEST(IppToJson, EnumAttribute) {
+  ipp::Collection* grp = nullptr;
+  ipp::ParsingResults log{.whole_buffer_was_parsed = true};
+
+  ipp::Frame frame(ipp::Version::_1_1, ipp::Status::successful_ok, 1, false);
+  ASSERT_EQ(frame.AddGroup(ipp::GroupTag::job_attributes, &grp),
+            ipp::Code::kOK);
+  ASSERT_NE(grp, nullptr);
+  EXPECT_EQ(grp->AddAttr("abcd", ipp::ValueTag::enum_, 1234), ipp::Code::kOK);
+
+  std::string json;
+  EXPECT_TRUE(ConvertToJson(frame, log, true, &json));
+  EXPECT_EQ(json, R"({"response":{"job-attributes":{)"
+                  R"("abcd":{"type":"enum","value":1234})"
+                  R"(}},"status":"successful-ok"})");
+}
+
 TEST(IppToJson, BooleanAttribute) {
   ipp::Collection* grp = nullptr;
   ipp::ParsingResults log{.whole_buffer_was_parsed = true};
@@ -66,6 +106,23 @@ TEST(IppToJson, BooleanAttribute) {
   EXPECT_EQ(json, R"({"response":{"job-attributes":{)"
                   R"("attr1":{"type":"boolean","value":true},)"
                   R"("attr2":{"type":"boolean","value":false})"
+                  R"(}},"status":"successful-ok"})");
+}
+
+TEST(IppToJson, OutOfBandAttribute) {
+  ipp::Collection* grp = nullptr;
+  ipp::ParsingResults log{.whole_buffer_was_parsed = true};
+
+  ipp::Frame frame(ipp::Version::_1_1, ipp::Status::successful_ok, 1, false);
+  ASSERT_EQ(frame.AddGroup(ipp::GroupTag::printer_attributes, &grp),
+            ipp::Code::kOK);
+  ASSERT_NE(grp, nullptr);
+  EXPECT_EQ(grp->AddAttr("attr", ipp::ValueTag::not_settable), ipp::Code::kOK);
+
+  std::string json;
+  EXPECT_TRUE(ConvertToJson(frame, log, true, &json));
+  EXPECT_EQ(json, R"({"response":{"printer-attributes":{)"
+                  R"("attr":"not-settable")"
                   R"(}},"status":"successful-ok"})");
 }
 
@@ -105,6 +162,39 @@ TEST(IppToJson, CollectionAttribute) {
                   R"("attr":{"type":"collection","value":)"
                   R"({"attr":{"type":"boolean","value":true}})"
                   R"(}}},"status":"successful-ok"})");
+}
+
+TEST(IppToJson, TwoEmptyGroups) {
+  ipp::Collection* grp = nullptr;
+  ipp::ParsingResults log{.whole_buffer_was_parsed = true};
+
+  ipp::Frame frame(ipp::Version::_1_1, ipp::Status::successful_ok, 1, false);
+  ASSERT_EQ(frame.AddGroup(ipp::GroupTag::job_attributes, &grp),
+            ipp::Code::kOK);
+  ASSERT_NE(grp, nullptr);
+  ASSERT_EQ(frame.AddGroup(ipp::GroupTag::job_attributes, &grp),
+            ipp::Code::kOK);
+  ASSERT_NE(grp, nullptr);
+
+  std::string json;
+  EXPECT_TRUE(ConvertToJson(frame, log, true, &json));
+  EXPECT_EQ(json, R"({"response":{"job-attributes":[{},{}]},)"
+                  R"("status":"successful-ok"})");
+}
+
+TEST(IppToJson, ParsingLogs) {
+  ipp::ParsingResults log{.whole_buffer_was_parsed = true};
+  log.errors.push_back(ipp::Log{
+      .message = "msg", .frame_context = "fc", .parser_context = "pc"});
+
+  ipp::Frame frame(ipp::Version::_2_0, ipp::Status::client_error_gone, 1,
+                   false);
+
+  std::string json;
+  EXPECT_TRUE(ConvertToJson(frame, log, true, &json));
+  EXPECT_EQ(json, R"({"parsing_logs":[{"frame_context":"fc","message":"msg",)"
+                  R"("parser_context":"pc"}],"response":{},)"
+                  R"("status":"client-error-gone"})");
 }
 
 }  // namespace
