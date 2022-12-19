153ca116635db67c18b38d73452334a263f0f216
Ethan Cheng <yycheng@google.com>
2022-12-19 06:28:14 +0000

    diagnostics: Implemented |OnStart|, |Run| and |UpdateProgress| function to memory routine v2
    Implemented two functions |Run| and |UpdateProgress| in memory routine
    v2.
    
    BUG=b:260193241
    TEST=(cr) FEATURES=test USE="-cros-debug" emerge-zork diagnostics
    
    Change-Id: I32cf7e65328de733f1f70c7a795c52b20fe33359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4055133
    Reviewed-by: Wei-Luan Wang <weiluanwang@google.com>
    Tested-by: Ethan Cheng <yycheng@google.com>
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Auto-Submit: Ethan Cheng <yycheng@google.com>
    Commit-Queue: Ethan Cheng <yycheng@google.com>
    Reviewed-by: Chung-sheng Wu <chungsheng@google.com>

--- diagnostics/cros_healthd/routines/memory/memory_v2.h
+++ diagnostics/cros_healthd/routines/memory/memory_v2.h
@@ -7,7 +7,9 @@
 
 #include <memory>
 #include <string>
+#include <vector>
 
+#include <base/files/file.h>
 #include <base/functional/callback_forward.h>
 #include <base/functional/callback_helpers.h>
 #include <base/memory/weak_ptr.h>
@@ -34,11 +36,48 @@ class MemoryRoutineV2 final : public BaseRoutineControl {
   void OnStart() override;
 
  private:
+  // The |Run| function is added to the memory resource queue as a callback and
+  // will be called when memory resource is available.
+  void Run(base::ScopedClosureRunner notify_resource_queue_finished);
+
+  // Initialize variables needed to read stdout.
+  void SetUpStdout(mojo::ScopedHandle handle);
+
+  // Read memtester return code and parses memtester output.
+  void DetermineRoutineResult();
+
+  // Accepts a return code and store it inside a class variable.
+  void GetReturnCode(int return_code);
+
+  // Update the percentage progress of the routine.
+  void UpdatePercentage();
+
+  // Read and parse the memtester stdout from read_stdout_size_ to
+  // current_stdout_size.
+  void ReadNewMemtesterResult();
+
+  // Parse the memtester output to determine the results.
+  ash::cros_healthd::mojom::MemoryRoutineDetailPtr ParseMemtesterResult();
+
+  // Calculate the percentage progress based on the current parsed output.
+  std::optional<int8_t> CalculatePercentage();
+
   // Unowned. Should outlive this instance.
   Context* const context_ = nullptr;
   // Once the memory resource is finished (when memtester finish running), run
   // this callback to notify the resource queue of resource availability.
   base::ScopedClosureRunner notify_resource_queue_finished_;
+  // A remote to manage lifetime of the memtester process.
+  mojo::Remote<ash::cros_healthd::mojom::ProcessControl> process_control_;
+  // The return code of memtester process.
+  int memtester_return_code_;
+  // A file descriptor that points to memtester stdout to allow for real time
+  // output capturing.
+  base::File stdout_file_;
+  // Stores the number of bytes the stdout file has been read so far.
+  size_t read_stdout_size_;
+  // Stores the parsed stdout result.
+  std::vector<std::vector<std::string>> parsed_memtester_result_;
 
   // Must be the last class member.
   base::WeakPtrFactory<MemoryRoutineV2> weak_ptr_factory_{this};
