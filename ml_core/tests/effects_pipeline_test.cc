5f6a10ff59557c97eb3ebb87054c19cfce025e6d
Tom Shafron <shafron@google.com>
2023-01-09 23:59:41 +0000

    ml_core: Reinstating some effects_pipeline tests
    EffectsPipeline tests were disabled while we reevaluate if they are still
    needed once the EffectsStreamManipulator tests were written.
    
    Remove redundant EffectsPipeline tests and modify other tests after
    consideration of EffectsStreamManipulator tests
    
    BUG=b:264947489
    TEST=Deployed and ran ml_core_effects_pipeline_test on DUT
    
    Change-Id: I1a970c1539f25c97b199ce0af47ddb72d4a23335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4150590
    Reviewed-by: Michael Olney <mpolney@google.com>
    Tested-by: Tom Shafron <shafron@google.com>
    Reviewed-by: Tom Shafron <shafron@google.com>
    Commit-Queue: Tom Shafron <shafron@google.com>
    Reviewed-by: Jun Sheng <shengjun@chromium.org>

--- ml_core/tests/effects_pipeline_test.cc
+++ ml_core/tests/effects_pipeline_test.cc
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <EGL/egl.h>
+#include <GLES2/gl2ext.h>
+#include <GLES3/gl3.h>
+
 #include <stdlib.h>
 
 #include <base/command_line.h>
@@ -22,17 +26,26 @@ base::WaitableEvent waitable(base::WaitableEvent::ResetPolicy::MANUAL,
                              base::WaitableEvent::InitialState::NOT_SIGNALED);
 
 base::FilePath DlcPath;
+EGLContext context;
 
 class EffectsPipelineTest : public ::testing::Test {
  protected:
   void SetUp() override {
     waitable.Reset();
     effect_set_success = false;
-    pipeline_ = cros::EffectsPipeline::Create(DlcPath);
+    pipeline_ = cros::EffectsPipeline::Create(DlcPath, context);
   }
 
   std::unique_ptr<cros::EffectsPipeline> pipeline_;
-  ImageObserver observer_();
+
+  cros::EffectsConfig config_{
+      .relight_enabled = false,
+      .blur_enabled = false,
+      .replace_enabled = false,
+      .blur_level = cros::mojom::BlurLevel::kHeavy,
+      .segmentation_gpu_api = cros::mojom::GpuApi::kOpenCL,
+      .relighting_gpu_api = cros::mojom::GpuApi::kOpenCL,
+      .graph_max_frames_in_flight = 2};
 };
 
 void SetEffectCallback(bool success) {
@@ -50,211 +63,38 @@ bool WaitForEffectSetAndReset() {
 }
 
 TEST_F(EffectsPipelineTest, SetEffectWithCallback) {
-  cros::EffectsConfig config{.effect =
-                                 cros::mojom::CameraEffect::kBackgroundBlur};
-  pipeline_->SetEffect(&config, SetEffectCallback);
+  config_.blur_enabled = true;
+  pipeline_->SetEffect(&config_, SetEffectCallback);
 
   EXPECT_TRUE(WaitForEffectSetAndReset());
 }
 
 TEST_F(EffectsPipelineTest, RotateThroughAllEffects) {
-  cros::EffectsConfig effects_config;
-
-  for (int i = 0; i < static_cast<int>(cros::mojom::CameraEffect::kCount);
-       ++i) {
-    effects_config.effect = static_cast<cros::mojom::CameraEffect>(i);
-    pipeline_->SetEffect(&effects_config, SetEffectCallback);
-    ASSERT_TRUE(WaitForEffectSetAndReset());
-  }
-}
-
-TEST_F(EffectsPipelineTest, NoEffectLeavesFrameUnaltered) {
-  const base::FilePath input_file(
-      "/usr/local/share/ml_core/tom_sample_720.png");
-
-  PngImageIO pngio_ = PngImageIO();
-  auto info = pngio_.ReadPngFile(input_file).value();
-
-  std::unique_ptr<uint8_t> frame_data_buf(
-      new uint8_t[info.num_row_bytes * info.height]);
-  ImageFrame frame{.frame_data = frame_data_buf.get(),
-                   .frame_width = 0,
-                   .frame_height = 0,
-                   .stride = 0};
-  pipeline_->SetRenderedImageObserver(std::make_unique<ImageObserver>(&frame));
-
-  std::unique_ptr<uint8_t> raw_data_buf(
-      new uint8_t[info.num_row_bytes * info.height]);
-  ASSERT_TRUE(
-      info.GetRawData(raw_data_buf.get(), info.num_row_bytes * info.height));
-
-  pipeline_->ProcessFrame(1, raw_data_buf.get(), info.width, info.height,
-                          info.num_row_bytes);
-  pipeline_->Wait();
-
-  EXPECT_EQ(frame.frame_height, info.height);
-  EXPECT_EQ(frame.frame_width, info.width);
-  EXPECT_EQ(frame.stride, info.num_row_bytes);
-  EXPECT_TRUE(FuzzyBufferComparison(frame.frame_data, raw_data_buf.get(),
-                                    info.num_row_bytes * info.height));
-}
-
-TEST_F(EffectsPipelineTest, BlurEffectBlursImage) {
-  const base::FilePath input_file(
-      "/usr/local/share/ml_core/tom_sample_720.png");
-  const base::FilePath reference_file(
-      "/usr/local/share/ml_core/tom_blur_720_hd.png");
-
-  PngImageIO pngio_ = PngImageIO();
-  auto input_info = pngio_.ReadPngFile(input_file).value();
-
-  std::unique_ptr<uint8_t> frame_data_buf(
-      new uint8_t[input_info.num_row_bytes * input_info.height]);
-  ImageFrame frame{.frame_data = frame_data_buf.get(),
-                   .frame_width = 0,
-                   .frame_height = 0,
-                   .stride = 0};
-  pipeline_->SetRenderedImageObserver(std::make_unique<ImageObserver>(&frame));
-
-  cros::EffectsConfig effects_config;
-  effects_config.effect = cros::mojom::CameraEffect::kBackgroundBlur;
-  pipeline_->SetEffect(&effects_config, SetEffectCallback);
+  // enabling background blur effect
+  config_.blur_enabled = true;
+  pipeline_->SetEffect(&config_, SetEffectCallback);
   ASSERT_TRUE(WaitForEffectSetAndReset());
 
-  std::unique_ptr<uint8_t> raw_data_buf(
-      new uint8_t[input_info.num_row_bytes * input_info.height]);
-  ASSERT_TRUE(input_info.GetRawData(
-      raw_data_buf.get(), input_info.num_row_bytes * input_info.height));
-
-  pipeline_->ProcessFrame(1, raw_data_buf.get(), input_info.width,
-                          input_info.height, input_info.num_row_bytes);
-  pipeline_->Wait();
-
-  auto ref_info = pngio_.ReadPngFile(reference_file).value();
-  ASSERT_TRUE(ref_info.GetRawData(raw_data_buf.get(),
-                                  ref_info.num_row_bytes * ref_info.height));
-
-  EXPECT_EQ(frame.frame_height, ref_info.height);
-  EXPECT_EQ(frame.frame_width, ref_info.width);
-  EXPECT_EQ(frame.stride, ref_info.num_row_bytes);
-
-  const int kAcceptablePixelDelta = 5;
-  const int kNumAcceptOutsideDelta = 2000;
-  // This comparison allows for bytes to differ by 5 in their channel
-  // values and allow up to 2000 channel bytes to be higher
-  // 2000 bytes is 0.05% of images total pixels
-  EXPECT_TRUE(FuzzyBufferComparison(frame.frame_data, raw_data_buf.get(),
-                                    ref_info.num_row_bytes * ref_info.height,
-                                    kAcceptablePixelDelta,
-                                    kNumAcceptOutsideDelta));
-}
-
-TEST_F(EffectsPipelineTest, RelightEffectRelightsImage) {
-  const base::FilePath input_file(
-      "/usr/local/share/ml_core/tom_sample_720.png");
-  const base::FilePath reference_file(
-      "/usr/local/share/ml_core/tom_relight_auto_720_hd.png");
-
-  PngImageIO pngio_ = PngImageIO();
-  auto input_info = pngio_.ReadPngFile(input_file).value();
-
-  std::unique_ptr<uint8_t> frame_data_buf(
-      new uint8_t[input_info.num_row_bytes * input_info.height]);
-  ImageFrame frame{.frame_data = frame_data_buf.get(),
-                   .frame_width = 0,
-                   .frame_height = 0,
-                   .stride = 0};
-  pipeline_->SetRenderedImageObserver(std::make_unique<ImageObserver>(&frame));
-
-  cros::EffectsConfig effects_config;
-  effects_config.effect = cros::mojom::CameraEffect::kPortraitRelight;
-  pipeline_->SetEffect(&effects_config, SetEffectCallback);
+  // enabling relight here so the effect should be
+  // blur+relight
+  config_.relight_enabled = true;
+  pipeline_->SetEffect(&config_, SetEffectCallback);
   ASSERT_TRUE(WaitForEffectSetAndReset());
 
-  std::unique_ptr<uint8_t> raw_data_buf(
-      new uint8_t[input_info.num_row_bytes * input_info.height]);
-  ASSERT_TRUE(input_info.GetRawData(
-      raw_data_buf.get(), input_info.num_row_bytes * input_info.height));
-
-  // Need to send two packets as relight uses information
-  // from the packets N and N-1 to create the effect
-  pipeline_->ProcessFrame(1, raw_data_buf.get(), input_info.width,
-                          input_info.height, input_info.num_row_bytes);
-  pipeline_->Wait();
-  pipeline_->ProcessFrame(2, raw_data_buf.get(), input_info.width,
-                          input_info.height, input_info.num_row_bytes);
-  pipeline_->Wait();
-
-  auto ref_info = pngio_.ReadPngFile(reference_file).value();
-  ASSERT_TRUE(ref_info.GetRawData(raw_data_buf.get(),
-                                  ref_info.num_row_bytes * ref_info.height));
-
-  EXPECT_EQ(frame.frame_height, ref_info.height);
-  EXPECT_EQ(frame.frame_width, ref_info.width);
-  EXPECT_EQ(frame.stride, ref_info.num_row_bytes);
-
-  const int kAcceptablePixelDelta = 5;
-  const int kNumAcceptOutsideDelta = 2000;
-  EXPECT_TRUE(FuzzyBufferComparison(frame.frame_data, raw_data_buf.get(),
-                                    ref_info.num_row_bytes * ref_info.height,
-                                    kAcceptablePixelDelta,
-                                    kNumAcceptOutsideDelta));
-}
-
-TEST_F(EffectsPipelineTest, BlurEffectWithExtraBlurLevel) {
-  const base::FilePath input_file(
-      "/usr/local/share/ml_core/tom_sample_720.png");
-  const base::FilePath reference_file(
-      "/usr/local/share/ml_core/tom_maximum_blur_720_hd.png");
-
-  PngImageIO pngio_ = PngImageIO();
-  auto input_info = pngio_.ReadPngFile(input_file).value();
-
-  std::unique_ptr<uint8_t> frame_data_buf(
-      new uint8_t[input_info.num_row_bytes * input_info.height]);
-  ImageFrame frame{.frame_data = frame_data_buf.get(),
-                   .frame_width = 0,
-                   .frame_height = 0,
-                   .stride = 0};
-  pipeline_->SetRenderedImageObserver(std::make_unique<ImageObserver>(&frame));
-
-  cros::EffectsConfig effects_config;
-  effects_config.effect = cros::mojom::CameraEffect::kBackgroundBlur;
-  pipeline_->SetEffect(&effects_config, SetEffectCallback);
-  EXPECT_TRUE(WaitForEffectSetAndReset());
-
-  std::unique_ptr<uint8_t> raw_data_buf(
-      new uint8_t[input_info.num_row_bytes * input_info.height]);
-  ASSERT_TRUE(input_info.GetRawData(
-      raw_data_buf.get(), input_info.num_row_bytes * input_info.height));
-
-  pipeline_->ProcessFrame(1, raw_data_buf.get(), input_info.width,
-                          input_info.height, input_info.num_row_bytes);
-  pipeline_->Wait();
-
-  effects_config.blur_level = cros::mojom::BlurLevel::kMaximum;
-
-  pipeline_->SetEffect(&effects_config, SetEffectCallback);
+  // disabling blur so the effect is just relight
+  config_.blur_enabled = false;
+  pipeline_->SetEffect(&config_, SetEffectCallback);
   ASSERT_TRUE(WaitForEffectSetAndReset());
 
-  pipeline_->ProcessFrame(2, raw_data_buf.get(), input_info.width,
-                          input_info.height, input_info.num_row_bytes);
-  pipeline_->Wait();
+  // all effects disabled
+  config_.relight_enabled = false;
+  pipeline_->SetEffect(&config_, SetEffectCallback);
+  ASSERT_TRUE(WaitForEffectSetAndReset());
 
-  auto ref_info = pngio_.ReadPngFile(reference_file).value();
-  ASSERT_TRUE(ref_info.GetRawData(raw_data_buf.get(),
-                                  ref_info.num_row_bytes * ref_info.height));
-
-  EXPECT_EQ(frame.frame_height, ref_info.height);
-  EXPECT_EQ(frame.frame_width, ref_info.width);
-  EXPECT_EQ(frame.stride, ref_info.num_row_bytes);
-
-  const int kAcceptablePixelDelta = 5;
-  const int kNumAcceptOutsideDelta = 2500;
-  EXPECT_TRUE(FuzzyBufferComparison(frame.frame_data, raw_data_buf.get(),
-                                    ref_info.num_row_bytes * ref_info.height,
-                                    kAcceptablePixelDelta,
-                                    kNumAcceptOutsideDelta));
+  // enabling background replace effect
+  config_.replace_enabled = true;
+  pipeline_->SetEffect(&config_, SetEffectCallback);
+  ASSERT_TRUE(WaitForEffectSetAndReset());
 }
 
 }  // namespace
