e6a684b73143a50b7ee8f829e7ed0f30d6bd2738
Yi Chou <yich@google.com>
2023-01-13 09:14:48 +0800

    libhwsec-foundation: Add more overloaded operator for NoDefault
    BUG=b:265164258
    TEST=Unittest of libhwsec-foundation
    
    Change-Id: Ie812e5bd7f4f79a76026216474939f66889c7f57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4160366
    Reviewed-by: John Admanski <jadmanski@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Yi Chou <yich@google.com>
    Tested-by: Yi Chou <yich@google.com>

--- libhwsec-foundation/utility/no_default_init.h
+++ libhwsec-foundation/utility/no_default_init.h
@@ -8,22 +8,31 @@
 #include <type_traits>
 #include <utility>
 
-// A helper to remove the default constructor for objects.
+// Helper type to create variable of an arbitrary type which does not allow for
+// default construction. This is most useful in contexts like structs where you
+// want force specific fields to be explicitly initialized.
 //
 // Example usage:
 //
-// struct StructName {
-//   NoDefault<int> val;
-//   NoDefault<std::string> str;
-// };
+//   struct StructName {
+//     NoDefault<int> val;
+//     NoDefault<std::string> str;
+//   };
 //
-// These would prevent the following code being compileable:
-// StructName{};
-// StructName{.val = 123};
-// StructName{.str = "456"};
+// This would prevent the following code from compiling:
 //
-// But the following code is still compileable:
-// StructName{.val = 123, .str = "456"};
+//   StructName{};
+//   StructName{.val = 123};
+//   StructName{.str = "456"};
+//
+// But this code still works:
+//
+//   StructName{.val = 123, .str = "456"};
+//
+// The helper is trying to become a drop-in replacement for the original type,
+// but it would not work if the original type is a "final" object or union.
+// If some operations(e.g. std::move, or implicit casting) don't work, you can
+// try to static_cast the helper to the reference of the original type.
 
 namespace hwsec_foundation {
 
@@ -33,8 +42,8 @@ class NoDefault : public T {
   using T::T;
   using T::operator=;
 
-  NoDefault(T&& t) : T(std::move(t)) {}  // NOLINT(runtime/explicit)
-  NoDefault(const T& t) : T(t) {}        // NOLINT(runtime/explicit)
+  constexpr NoDefault(T&& t) : T(std::move(t)) {}  // NOLINT(runtime/explicit)
+  constexpr NoDefault(const T& t) : T(t) {}        // NOLINT(runtime/explicit)
   NoDefault() = delete;
 
   ~NoDefault() = default;
@@ -48,7 +57,22 @@ class NoDefault<T, std::enable_if_t<std::is_scalar_v<T>>> {
 
   ~NoDefault() = default;
 
-  operator T() const { return value_; }
+  constexpr operator T() const { return value_; }
+  constexpr operator T&() { return value_; }
+
+  template <int&... ExplicitArgumentBarrier,
+            typename U = T,
+            typename = std::void_t<decltype(*std::declval<U>())>>
+  constexpr auto& operator*() const {
+    return *value_;
+  }
+
+  template <int&... ExplicitArgumentBarrier,
+            typename U = T,
+            typename = std::void_t<decltype(*std::declval<U>())>>
+  constexpr T operator->() const {
+    return value_;
+  }
 
  private:
   T value_;
