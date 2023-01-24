523f5817525116a8eccd84428a4781454b6a8caf
Andrew Lassalle <andrewlassalle@chromium.org>
2023-01-24 15:32:52 -0800

    shill: cellular: Add test to catch regressions on main carriers
    Add unit test to validate the service_providers database for the main
    carriers. Start with AT&T and US cellular.
    
    BUG=b:223669451
    TEST=Run unit tests
    
    Change-Id: Ia4dcc3ec812d40132c9ab1b216dd0b5a59e4b5f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4193067
    Commit-Queue: Andrew Lassalle <andrewlassalle@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Tested-by: Andrew Lassalle <andrewlassalle@chromium.org>

---
+++ shill/cellular/mobile_operator_info_carriers_test.cc
@@ -0,0 +1,376 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
+#include "shill/cellular/mobile_operator_info.h"
+
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <base/check_op.h>
+#include <base/files/file_path.h>
+#include <base/rand_util.h>
+#include <base/strings/strcat.h>
+#include <gtest/gtest.h>
+
+#include "shill/cellular/apn_list.h"
+#include "shill/cellular/mobile_operator_mapper.h"
+#include "shill/dbus-constants.h"
+#include "shill/ipconfig.h"
+#include "shill/logging.h"
+#include "shill/test_event_dispatcher.h"
+
+using std::string;
+using testing::Test;
+
+// The tests in this file are meant to ensure there are no regressions on the
+// most popular carriers. These test will be validated using
+// service_providers.prototxt and will use the real MobileOperatorMappers.
+
+namespace shill {
+
+class ApnBuilder {
+ public:
+  ApnBuilder() {
+    // Set ip_type to ipv4 by default to match the behavior of the proto
+    apn_.ip_type = kApnIpTypeV4;
+  }
+  explicit ApnBuilder(std::string apn_name) : ApnBuilder() { Name(apn_name); }
+
+  const MobileOperatorMapper::MobileAPN& apn() { return apn_; }
+  ApnBuilder& Name(const string value) {
+    apn_.apn = value;
+    return *this;
+  }
+  ApnBuilder& Username(const string value) {
+    apn_.username = value;
+    return *this;
+  }
+  ApnBuilder& Password(const string value) {
+    apn_.password = value;
+    return *this;
+  }
+  ApnBuilder& Auth(const string value) {
+    apn_.authentication = value;
+    return *this;
+  }
+  ApnBuilder& ApnTypes(const std::set<std::string> value) {
+    apn_.apn_types = value;
+    return *this;
+  }
+  ApnBuilder& IpType(const string value) {
+    apn_.ip_type = value;
+    return *this;
+  }
+  ApnBuilder& OperatorNameList(
+      const std::vector<MobileOperatorMapper::LocalizedName> value) {
+    apn_.operator_name_list = value;
+    return *this;
+  }
+
+ private:
+  MobileOperatorMapper::MobileAPN apn_;
+};
+
+class MobileOperatorInfoCarriersTest : public Test {
+ public:
+  MobileOperatorInfoCarriersTest()
+      :  // TODO(b/266738838): validate the list of MCCMNCs for AT&T.
+        kAttUsMccmncs({"310030", "310038", "310070", "310090", "310150",
+                       "310170", "310280", "310380", "310410", "310560",
+                       "310670", "310680", "310950", "310980", "311180",
+                       "312670", "313100", "313110", "313120", "313130",
+                       "313140", "313790"}),
+        kVerizonUsMccmncs({"310995", "311270", "311480"}),
+        kTmobileUsMccmncs({"310026", "310160", "310200", "310210", "310220",
+                           "310230", "310240", "310250", "310260", "310270",
+                           "310310", "310490", "310580", "310660", "310800",
+                           "311882", "312250"}) {
+    operator_info_ = std::make_unique<MobileOperatorInfo>(&dispatcher_, "test");
+  }
+  MobileOperatorInfoCarriersTest(const MobileOperatorInfoCarriersTest&) =
+      delete;
+  MobileOperatorInfoCarriersTest& operator=(
+      const MobileOperatorInfoCarriersTest&) = delete;
+
+  void SetUp() override {
+    shill::ScopeLogger::GetInstance()->set_verbose_level(1);
+    shill::ScopeLogger::GetInstance()->EnableScopesByName("cellular");
+    operator_info_->ClearDatabasePaths();
+    const char* out_dir = getenv("OUT");
+    CHECK_NE(out_dir, nullptr);
+    base::FilePath database_path =
+        base::FilePath(out_dir).Append("serviceproviders.pbf");
+
+    operator_info_->AddDatabasePath(database_path);
+    operator_info_->Init();
+  }
+
+ protected:
+  MobileOperatorMapper::MobileAPN PrepareModbApnForComparison(
+      MobileOperatorMapper::MobileAPN apn) {
+    // Ignore operator name list
+    apn.operator_name_list.clear();
+    return apn;
+  }
+  void CheckIfApnExists(ApnBuilder apn_builder) {
+    for (const auto& apn_info : operator_info_->apn_list()) {
+      if (PrepareModbApnForComparison(apn_info) == apn_builder.apn())
+        return;
+    }
+    FAIL() << "APN: " << apn_builder.apn().apn << " not found";
+  }
+  string ApnToString(MobileOperatorMapper::MobileAPN apn) {
+    return base::StringPrintf(
+        "{apn: %s, username: %s, password: %s, authentication: %s, ip_type: %s "
+        ", apn_types: %s , operator_name_list.size(): %s}",
+        apn.apn.c_str(), apn.username.c_str(), apn.password.c_str(),
+        apn.authentication.c_str(), apn.ip_type.c_str(),
+        ApnList::JoinApnTypes(
+            std::vector<string>(apn.apn_types.begin(), apn.apn_types.end()))
+            .c_str(),
+        base::NumberToString(apn.operator_name_list.size()).c_str());
+  }
+
+  void CheckFirstApn(ApnBuilder apn_builder) {
+    EXPECT_GT(operator_info_->apn_list().size(), 0);
+    EXPECT_EQ(PrepareModbApnForComparison(operator_info_->apn_list()[0]),
+              apn_builder.apn())
+        << " operator_info_->apn_list()[0]:"
+        << ApnToString(operator_info_->apn_list()[0])
+        << " \napn_builder.apn():" << ApnToString(apn_builder.apn());
+  }
+
+  std::string CreateRandomGid1NotInSet(std::vector<int> gid1s) {
+    std::sort(gid1s.begin(), gid1s.end());
+    int gid1 = base::RandInt(0, 0xFFFF - gid1s.size());
+    for (auto i : gid1s) {
+      if (gid1 >= i)
+        gid1++;
+    }
+    return base::HexEncode(&gid1, 2);
+  }
+
+  std::string CreateRandomMccmnc(std::string prefix, int len) {
+    std::string value =
+        base::StrCat({prefix, base::NumberToString(base::RandInt(0, 999999))});
+    return value.substr(0, len);
+  }
+
+  EventDispatcherForTest dispatcher_;
+  std::unique_ptr<MobileOperatorInfo> operator_info_;
+
+  static constexpr char kUnknownMccmnc[] = "000000";
+  const std::set<string> kAttUsMccmncs;
+  const std::set<string> kVerizonUsMccmncs;
+  const std::set<string> kTmobileUsMccmncs;
+};
+
+class MobileOperatorInfoCarriersAttTest
+    : public MobileOperatorInfoCarriersTest {
+ public:
+  MobileOperatorInfoCarriersAttTest()
+      : kAttApn4G(ApnBuilder("broadband")
+                      .IpType(kApnIpTypeV4V6)
+                      .ApnTypes({kApnTypeDefault, kApnTypeIA})),
+        kAttApn5G(ApnBuilder("nrbroadband")
+                      .IpType(kApnIpTypeV4V6)
+                      .ApnTypes({kApnTypeDefault, kApnTypeIA})),
+        kAtt5gMccmncs({"310410", "310280", "311180", "310950"}),
+        kAttMxMccmncs({"334050", "334090"}),
+        kCricketMccmncs({"310150"}),
+        kFirstnetMccmncs(
+            {"312670", "313100", "313110", "313120", "313130", "313140"}) {}
+
+ protected:
+  std::vector<string> GetUsMnoOnlyMccmncs() {
+    std::vector<string> value;
+    for (auto& mccmnc : kAttUsMccmncs) {
+      if (!kCricketMccmncs.count(mccmnc) && !kFirstnetMccmncs.count(mccmnc))
+        value.push_back(mccmnc);
+    }
+    return value;
+  }
+
+  const ApnBuilder kAttApn4G;
+  const ApnBuilder kAttApn5G;
+
+  const std::set<string> kAtt5gMccmncs;
+  const std::set<string> kAttMxMccmncs;
+  const std::set<string> kCricketMccmncs;
+  const std::set<string> kFirstnetMccmncs;
+
+  static constexpr int kAttMtu = 1430;
+  static constexpr char kAttOperatorName[] = "AT&T";
+};
+
+TEST_F(MobileOperatorInfoCarriersAttTest, AttUsHome_AttServing) {
+  for (const auto& mccmnc : GetUsMnoOnlyMccmncs()) {
+    operator_info_->UpdateMCCMNC(mccmnc);
+    operator_info_->UpdateServingMCCMNC(mccmnc);
+    EXPECT_EQ(operator_info_->operator_name(), kAttOperatorName);
+    EXPECT_EQ(operator_info_->serving_operator_name(), kAttOperatorName);
+    EXPECT_EQ(operator_info_->mtu(), kAttMtu);
+    operator_info_->UpdateGID1(CreateRandomGid1NotInSet({0x53FF}));
+    CheckFirstApn(kAttApn4G);
+  }
+}
+
+TEST_F(MobileOperatorInfoCarriersAttTest, UnknownHome_AttServing) {
+  operator_info_->UpdateMCCMNC(kUnknownMccmnc);
+  operator_info_->UpdateServingMCCMNC(GetUsMnoOnlyMccmncs()[0]);
+  EXPECT_EQ(operator_info_->operator_name(), "");
+  EXPECT_EQ(operator_info_->serving_operator_name(), kAttOperatorName);
+  EXPECT_EQ(operator_info_->mtu(), kAttMtu);
+  EXPECT_EQ(operator_info_->apn_list().size(), 0);
+}
+
+TEST_F(MobileOperatorInfoCarriersAttTest, AttHome_UnknownServing) {
+  operator_info_->UpdateMCCMNC(*kAttUsMccmncs.begin());
+  operator_info_->UpdateServingMCCMNC(kUnknownMccmnc);
+  EXPECT_EQ(operator_info_->operator_name(), kAttOperatorName);
+  EXPECT_EQ(operator_info_->serving_operator_name(), "");
+  EXPECT_EQ(operator_info_->mtu(), kAttMtu);
+  CheckFirstApn(kAttApn4G);
+}
+
+TEST_F(MobileOperatorInfoCarriersAttTest, Att5gUsHome_AttServing) {
+  for (const auto& home : kAtt5gMccmncs) {
+    for (const auto& serving : GetUsMnoOnlyMccmncs()) {
+      operator_info_->UpdateMCCMNC(home);
+      operator_info_->UpdateServingMCCMNC(serving);
+      // ATT 5G has an GID1 filter
+      operator_info_->UpdateGID1("53FF");
+      EXPECT_EQ(operator_info_->operator_name(), kAttOperatorName);
+      EXPECT_EQ(operator_info_->serving_operator_name(), kAttOperatorName);
+      EXPECT_EQ(operator_info_->mtu(), kAttMtu);
+      CheckFirstApn(kAttApn5G);
+      CheckIfApnExists(kAttApn4G);
+    }
+  }
+}
+
+TEST_F(MobileOperatorInfoCarriersAttTest, CricketHome_AttServing) {
+  for (const auto& home : kCricketMccmncs) {
+    for (const auto& serving : GetUsMnoOnlyMccmncs()) {
+      operator_info_->UpdateMCCMNC(home);
+      operator_info_->UpdateServingMCCMNC(serving);
+      // Cricket has an OPERATOR_NAME filter
+      operator_info_->UpdateOperatorName("Cricket");
+      EXPECT_EQ(operator_info_->operator_name(), "Cricket");
+      EXPECT_EQ(operator_info_->serving_operator_name(), kAttOperatorName);
+      EXPECT_EQ(operator_info_->mtu(), 1430 /* Cricket mtu */);
+      CheckFirstApn(ApnBuilder("mht").ApnTypes({kApnTypeDefault, kApnTypeIA}));
+    }
+  }
+}
+
+TEST_F(MobileOperatorInfoCarriersAttTest, FirstnetHome_AttServing) {
+  for (const auto& home : kFirstnetMccmncs) {
+    for (const auto& serving : GetUsMnoOnlyMccmncs()) {
+      // Firstnet has an MCCMNC filter
+      operator_info_->UpdateMCCMNC(home);
+      operator_info_->UpdateServingMCCMNC(serving);
+      EXPECT_EQ(operator_info_->operator_name(), "firstnet");
+      EXPECT_EQ(operator_info_->serving_operator_name(), kAttOperatorName);
+      // Even though firstnet might use AT&T as serving operator, the firstnet
+      // mtu is smaller than AT&T's, so it'a always chosen.
+      EXPECT_EQ(operator_info_->mtu(), 1342);
+      CheckFirstApn(ApnBuilder("firstnet-broadband")
+                        .ApnTypes({kApnTypeDefault, kApnTypeIA}));
+    }
+  }
+}
+
+TEST_F(MobileOperatorInfoCarriersAttTest, AttMxHome_AttMxServing) {
+  for (const auto& mccmnc : kAttMxMccmncs) {
+    operator_info_->UpdateMCCMNC(mccmnc);
+    operator_info_->UpdateServingMCCMNC(mccmnc);
+    EXPECT_EQ(operator_info_->operator_name(), kAttOperatorName);
+    EXPECT_EQ(operator_info_->serving_operator_name(), kAttOperatorName);
+    EXPECT_EQ(operator_info_->mtu(), IPConfig::kUndefinedMTU);
+    CheckFirstApn(kAttApn4G);
+  }
+}
+
+class MobileOperatorInfoCarriersUsCellularTest
+    : public MobileOperatorInfoCarriersTest {
+ public:
+  MobileOperatorInfoCarriersUsCellularTest()
+      : kUsCellularApn(ApnBuilder("usccinternet").ApnTypes({kApnTypeDefault})),
+        kUsCellularMccmncs({"311580", "311581", "311582", "311583", "311584",
+                            "311585", "311586", "311587", "311588", "311589"}) {
+  }
+
+ protected:
+  std::vector<string> GetMccmncsFromAllUsCarriers() {
+    std::vector<string> value;
+    for (auto& mccmnc : kAttUsMccmncs)
+      value.push_back(mccmnc);
+    for (auto& mccmnc : kVerizonUsMccmncs)
+      value.push_back(mccmnc);
+    for (auto& mccmnc : kTmobileUsMccmncs)
+      value.push_back(mccmnc);
+    return value;
+  }
+
+  const ApnBuilder kUsCellularApn;
+  const std::set<string> kUsCellularMccmncs;
+  static constexpr char kUsCellularOperatorName[] = "US Cellular";
+};
+
+TEST_F(MobileOperatorInfoCarriersUsCellularTest, RoamOnUsCarriers) {
+  std::vector<string> serving_mccmncs = GetMccmncsFromAllUsCarriers();
+  // Append random MCCMNC's in the range `31[0-6]...` to test unknown US
+  // MCCMNCs.
+  for (int i = 0; i < 7; i++) {
+    serving_mccmncs.push_back(
+        CreateRandomMccmnc(base::StrCat({"31", base::NumberToString(i)}), 6));
+  }
+  for (const auto& serving_mccmnc : serving_mccmncs) {
+    for (const auto& mccmnc : kUsCellularMccmncs) {
+      operator_info_->UpdateMCCMNC(mccmnc);
+      operator_info_->UpdateServingMCCMNC(serving_mccmnc);
+      EXPECT_EQ(operator_info_->operator_name(), kUsCellularOperatorName);
+      EXPECT_EQ(operator_info_->requires_roaming(), true)
+          << " serving_mccmnc: " << serving_mccmnc;
+      CheckFirstApn(kUsCellularApn);
+    }
+  }
+}
+
+TEST_F(MobileOperatorInfoCarriersUsCellularTest, RoamOutsideTheUs) {
+  std::vector<string> serving_mccmncs;
+  // US MCC is "31[0-6]...". Add random MCCMNCs that don't match that prefix.
+  // Add upper boundary MCCMNC
+  serving_mccmncs.push_back("317000");
+  while (serving_mccmncs.size() < 10) {
+    std::string mccmnc = CreateRandomMccmnc("", 5);
+    if (!base::StartsWith(mccmnc, "31"))
+      serving_mccmncs.push_back(mccmnc);
+  }
+  while (serving_mccmncs.size() < 20) {
+    std::string mccmnc = CreateRandomMccmnc("", 6);
+    if (!base::StartsWith(mccmnc, "31"))
+      serving_mccmncs.push_back(mccmnc);
+  }
+
+  for (const auto& serving_mccmnc : serving_mccmncs) {
+    for (const auto& mccmnc : kUsCellularMccmncs) {
+      operator_info_->UpdateMCCMNC(mccmnc);
+      operator_info_->UpdateServingMCCMNC(serving_mccmnc);
+      EXPECT_EQ(operator_info_->operator_name(), kUsCellularOperatorName);
+      EXPECT_EQ(operator_info_->requires_roaming(), false)
+          << " serving_mccmnc: " << serving_mccmnc;
+      CheckFirstApn(kUsCellularApn);
+    }
+  }
+}
+}  // namespace shill
