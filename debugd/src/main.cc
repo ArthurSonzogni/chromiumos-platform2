ba118479ce108d82d87d02147070b149184e0b30
Mike Frysinger <vapier@chromium.org>
2023-02-09 01:47:51 -0500

    debugd: log minijail errors explicitly
    When a minijail call fails, log details about it rather than only
    blow up.
    
    BUG=b:256739303
    TEST=CQ passes
    
    Change-Id: Ic6ed3249e0d9af1b91555bae73170e02d13a9f59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4234179
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- debugd/src/main.cc
+++ debugd/src/main.cc
@@ -22,6 +22,13 @@ namespace {
 // For TPM 1.2 only: Directory to mount for access to tcsd socket.
 constexpr char kTcsdDir[] = "/run/tcsd";
 
+// Minijail APIs tend to return the errno value directly, so pull that out and
+// then return success based on it.
+static inline bool mj_call(int ret) {
+  errno = -ret;
+  return errno == 0;
+}
+
 // @brief Enter a VFS namespace.
 //
 // We don't want anyone other than our descendants to see our tmpfs.
@@ -30,27 +37,20 @@ void enter_vfs_namespace() {
 
   // Create a minimalistic mount namespace with just the bare minimum required.
   minijail_namespace_vfs(j.get());
-  if (minijail_enter_pivot_root(j.get(), "/mnt/empty"))
-    LOG(FATAL) << "minijail_enter_pivot_root() failed";
-  if (minijail_bind(j.get(), "/", "/", 0))
-    LOG(FATAL) << "minijail_bind(\"/\") failed";
-  if (minijail_mount_with_data(j.get(), "none", "/proc", "proc",
-                               MS_NOSUID | MS_NOEXEC | MS_NODEV, nullptr)) {
-    LOG(FATAL) << "minijail_mount_with_data(\"/proc\") failed";
-  }
-  if (minijail_bind(j.get(), "/var", "/var", 1))
-    LOG(FATAL) << "minijail_bind(\"/var\") failed";
+  PCHECK(mj_call(minijail_enter_pivot_root(j.get(), "/mnt/empty")));
+  PCHECK(mj_call(minijail_bind(j.get(), "/", "/", 0)));
+  PCHECK(mj_call(minijail_mount_with_data(j.get(), "none", "/proc", "proc",
+                                          MS_NOSUID | MS_NOEXEC | MS_NODEV,
+                                          nullptr)));
+  PCHECK(mj_call(minijail_bind(j.get(), "/var", "/var", 1)));
 
   // Hack a path for vpd until it can migrate to /var.
   // https://crbug.com/876838
-  if (minijail_mount_with_data(j.get(), "tmpfs", "/mnt", "tmpfs",
-                               MS_NOSUID | MS_NOEXEC | MS_NODEV,
-                               "mode=0755,size=10M")) {
-    LOG(FATAL) << "minijail_mount_with_data(\"/mnt\") failed";
-  }
+  PCHECK(mj_call(minijail_mount_with_data(j.get(), "tmpfs", "/mnt", "tmpfs",
+                                          MS_NOSUID | MS_NOEXEC | MS_NODEV,
+                                          "mode=0755,size=10M")));
   const char kVpdPath[] = "/mnt/stateful_partition/unencrypted/cache/vpd";
-  if (minijail_bind(j.get(), kVpdPath, kVpdPath, 1))
-    LOG(FATAL) << "minijail_bind(\"" << kVpdPath << "\") failed";
+  PCHECK(mj_call(minijail_bind(j.get(), kVpdPath, kVpdPath, 1)));
 
   // debugd needs access to the stateful swap directory.
   const base::FilePath kSwapWbDir(
@@ -62,31 +62,24 @@ void enter_vfs_namespace() {
                  << base::File::ErrorToString(error);
     }
   }
-  if (minijail_bind(j.get(), kSwapWbDir.MaybeAsASCII().c_str(),
-                    kSwapWbDir.MaybeAsASCII().c_str(), 1)) {
-    LOG(FATAL) << "minijail_bind(\"" << kSwapWbDir << "\") failed";
-  }
+  PCHECK(mj_call(minijail_bind(j.get(), kSwapWbDir.MaybeAsASCII().c_str(),
+                               kSwapWbDir.MaybeAsASCII().c_str(), 1)));
 
   minijail_remount_mode(j.get(), MS_SLAVE);
 
-  if (minijail_mount_with_data(j.get(), "tmpfs", "/run", "tmpfs",
-                               MS_NOSUID | MS_NOEXEC | MS_NODEV, nullptr)) {
-    LOG(FATAL) << "minijail_mount_with_data(\"/run\") failed";
-  }
+  PCHECK(mj_call(minijail_mount_with_data(j.get(), "tmpfs", "/run", "tmpfs",
+                                          MS_NOSUID | MS_NOEXEC | MS_NODEV,
+                                          nullptr)));
 
-  if (minijail_mount(j.get(), "/run/daemon-store/debugd",
-                     "/run/daemon-store/debugd", "none",
-                     MS_BIND | MS_REC) != 0) {
-    LOG(FATAL) << "minijail_mount(\"/run/daemon-store/debugd\") failed";
-  }
+  PCHECK(mj_call(minijail_mount(j.get(), "/run/daemon-store/debugd",
+                                "/run/daemon-store/debugd", "none",
+                                MS_BIND | MS_REC)));
 
   // Mount /run/debugd for a shared place for runtime data.
-  if (minijail_bind(j.get(), "/run/debugd", "/run/debugd", 1))
-    LOG(FATAL) << "minijail_bind(\"/run/debugd\") failed";
+  PCHECK(mj_call(minijail_bind(j.get(), "/run/debugd", "/run/debugd", 1)));
 
   // Mount /run/dbus to be able to communicate with D-Bus.
-  if (minijail_bind(j.get(), "/run/dbus", "/run/dbus", 0))
-    LOG(FATAL) << "minijail_bind(\"/run/dbus\") failed";
+  PCHECK(mj_call(minijail_bind(j.get(), "/run/dbus", "/run/dbus", 0)));
 
   // Mount /tmp, /run/cups, and /run/ippusb to be able to communicate with CUPS.
   // /tmp must be at least 3 * kernel partition size plus a little extra. This
@@ -97,75 +90,60 @@ void enter_vfs_namespace() {
   // often leads to a smaller blob), then copies the old blob to a new blob and
   // overwrites the repacked kernel onto the new blob.
   minijail_mount_tmp_size(j.get(), 100 * 1024 * 1024);
-  if (minijail_bind(j.get(), "/run/cups", "/run/cups", 0))
-    LOG(FATAL) << "minijail_bind(\"/run/cups\") failed";
+  PCHECK(mj_call(minijail_bind(j.get(), "/run/cups", "/run/cups", 0)));
   // Mount /run/ippusb to be able to communicate with CUPS.
-  if (minijail_bind(j.get(), "/run/ippusb", "/run/ippusb", 0))
-    LOG(FATAL) << "minijail_bind(\"/run/ippusb\") failed";
+  PCHECK(mj_call(minijail_bind(j.get(), "/run/ippusb", "/run/ippusb", 0)));
 
   // Mount writable debug directories for cups, ippusb, and lorgnette for use
   // with printscan_tool.
-  if (minijail_bind(j.get(), "/run/cups/debug", "/run/cups/debug", 1))
-    LOG(FATAL) << "minijail_bind(\"/run/cups/debug\" failed";
-  if (minijail_bind(j.get(), "/run/ippusb/debug", "/run/ippusb/debug", 1))
-    LOG(FATAL) << "minijail_bind(\"/run/ippusb/debug\" failed";
-  if (minijail_bind(j.get(), "/run/lorgnette/debug", "/run/lorgnette/debug", 1))
-    LOG(FATAL) << "minijail_bind(\"/run/lorgnette/debug\" failed";
+  PCHECK(
+      mj_call(minijail_bind(j.get(), "/run/cups/debug", "/run/cups/debug", 1)));
+  PCHECK(mj_call(
+      minijail_bind(j.get(), "/run/ippusb/debug", "/run/ippusb/debug", 1)));
+  PCHECK(mj_call(minijail_bind(j.get(), "/run/lorgnette/debug",
+                               "/run/lorgnette/debug", 1)));
 
   // In case we start before avahi-daemon, make sure the path exists.
   mkdir("/var/run/avahi-daemon", 0755);
   // Mount /run/avahi-daemon in order to perform mdns name resolution.
-  if (minijail_bind(j.get(), "/run/avahi-daemon", "/run/avahi-daemon", 0))
-    LOG(FATAL) << "minijail_bind(\"/run/avahi-daemon\") failed";
+  PCHECK(mj_call(
+      minijail_bind(j.get(), "/run/avahi-daemon", "/run/avahi-daemon", 0)));
 
   // Since shill provides network resolution settings, bind mount it.
   // In case we start before shill, make sure the path exists.
   mkdir("/run/shill", 0755);
-  if (minijail_bind(j.get(), "/run/shill", "/run/shill", 0))
-    LOG(FATAL) << "minijail_bind(\"/run/shill\") failed";
+  PCHECK(mj_call(minijail_bind(j.get(), "/run/shill", "/run/shill", 0)));
 
   // Bind mount /run/lockbox and /var/lib/devicesettings to be able to read
   // policy files and check device policies.
   // In case we start before, make sure the path exists.
   mkdir("/run/lockbox", 0755);
-  if (minijail_bind(j.get(), "/run/lockbox", "/run/lockbox", 0))
-    LOG(FATAL) << "minijail_bind(\"/run/lockbox\") failed";
+  PCHECK(mj_call(minijail_bind(j.get(), "/run/lockbox", "/run/lockbox", 0)));
   // In case we start before, make sure the path exists.
   mkdir("/var/lib/devicesettings", 0755);
-  if (minijail_bind(j.get(), "/var/lib/devicesettings",
-                    "/var/lib/devicesettings", 0))
-    LOG(FATAL) << "minijail_bind(\"/var/lib/devicesettings\") failed";
+  PCHECK(mj_call(minijail_bind(j.get(), "/var/lib/devicesettings",
+                               "/var/lib/devicesettings", 0)));
 
   // Mount /dev to be able to inspect devices.
-  if (minijail_mount_with_data(j.get(), "/dev", "/dev", "bind",
-                               MS_BIND | MS_REC, nullptr)) {
-    LOG(FATAL) << "minijail_mount_with_data(\"/dev\") failed";
-  }
+  PCHECK(mj_call(minijail_mount_with_data(j.get(), "/dev", "/dev", "bind",
+                                          MS_BIND | MS_REC, nullptr)));
 
   // Mount /sys to access some logs.
-  if (minijail_mount_with_data(j.get(), "/sys", "/sys", "bind",
-                               MS_BIND | MS_REC, nullptr)) {
-    LOG(FATAL) << "minijail_mount_with_data(\"/sys\") failed";
-  }
+  PCHECK(mj_call(minijail_mount_with_data(j.get(), "/sys", "/sys", "bind",
+                                          MS_BIND | MS_REC, nullptr)));
 
   // Mount /run/chromeos-config/v1 to access chromeos-config.
-  if (minijail_bind(j.get(), "/run/chromeos-config/v1",
-                    "/run/chromeos-config/v1", 0)) {
-    LOG(FATAL) << "minijail_bind(\"/run/chromeos-config/v1\") failed";
-  }
+  PCHECK(mj_call(minijail_bind(j.get(), "/run/chromeos-config/v1",
+                               "/run/chromeos-config/v1", 0)));
 
   // Mount /run/lock so that lock file for crossystem is shared.
-  if (minijail_bind(j.get(), "/run/lock", "/run/lock", 1)) {
-    LOG(FATAL) << "minijail_bind(\"/run/lock\") failed";
-  }
+  PCHECK(mj_call(minijail_bind(j.get(), "/run/lock", "/run/lock", 1)));
 
   if (USE_TPM) {
     // For TPM 1.2 only: Enable utilities that communicate with TPM via tcsd -
     // mount directory containing tcsd socket.
     mkdir(kTcsdDir, 0755);
-    if (minijail_bind(j.get(), kTcsdDir, kTcsdDir, 0)) {
-      LOG(FATAL) << "minijail_bind(\"" << kTcsdDir << "\") failed";
-    }
+    PCHECK(mj_call(minijail_bind(j.get(), kTcsdDir, kTcsdDir, 0)));
   }
 
   minijail_enter(j.get());
