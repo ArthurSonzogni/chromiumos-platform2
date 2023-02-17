5c49d050646703c45807424b5c1036c723a99e5e
Junichi Uekawa <uekawa@google.com>
2023-02-17 14:25:40 +0900

    vm_tools: concierge: Add a case for privileged_quota_uids.
    BUG=b:269184617
    TEST=unit test.
    
    Change-Id: Ie54116332595a72ad3806235e16db8b7d685275d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4263807
    Commit-Queue: Junichi Uekawa <uekawa@chromium.org>
    Reviewed-by: Abhishek Bhardwaj <abhishekbh@chromium.org>
    Tested-by: Junichi Uekawa <uekawa@chromium.org>
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>

--- vm_tools/concierge/vm_util_test.cc
+++ vm_tools/concierge/vm_util_test.cc
@@ -510,5 +510,21 @@ TEST(VMUtilTest, SharedDataParamSimple) {
             "false:writeback=true:posix_acl=true");
 }
 
+// privileged_quota_uids is passed in.
+TEST(VMUtilTest, SharedDataParamWithPrivilegedQuotaUids) {
+  SharedDataParam param{.data_dir = base::FilePath("/usr/local/bin"),
+                        .tag = "usr_local_bin",
+                        .enable_caches = true,
+                        .ascii_casefold = false,
+                        .posix_acl = true,
+                        .privileged_quota_uids = {0}};
+  ASSERT_EQ(param.to_string(),
+            "/usr/local/bin:usr_local_bin:type=fs:cache=always:uidmap=0 655360 "
+            "5000,5000 600 50,5050 660410 1994950:gidmap=0 655360 1065,1065 "
+            "20119 1,1066 656426 3934,5000 600 50,5050 660410 "
+            "1994950:timeout=3600:rewrite-security-xattrs=true:ascii_casefold="
+            "false:writeback=true:posix_acl=true:privileged_quota_uids=0");
+}
+
 }  // namespace concierge
 }  // namespace vm_tools
