6c7c16b142b2828f53c9a2e5e66eadf2e5d7aadc
Wei-Luan Wang <weiluanwang@google.com>
2023-02-25 16:33:56 +0000

    shill: Remove unused callback types
    BUG=chromium:1205291
    TEST=CQ
    
    Change-Id: Ifd3ddd5a1e1cd4547e5e47f5afcb39e9c2b9139b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4292954
    Tested-by: Wei-Luan Wang <weiluanwang@google.com>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Auto-Submit: Wei-Luan Wang <weiluanwang@google.com>
    Commit-Queue: Hugo Benichi <hugobenichi@google.com>

--- shill/callbacks.h
+++ shill/callbacks.h
@@ -22,7 +22,6 @@ class Error;
 // Convenient typedefs for some commonly used callbacks.
 using ResultCallback = base::Callback<void(const Error&)>;
 using ResultOnceCallback = base::OnceCallback<void(const Error&)>;
-using ResultBoolCallback = base::Callback<void(const Error&, bool)>;
 using ResultVariantDictionariesCallback =
     base::Callback<void(const Error&, const VariantDictionaries&)>;
 using ResultVariantDictionariesOnceCallback =
@@ -32,14 +31,10 @@ using KeyValueStoreCallback =
     base::OnceCallback<void(const KeyValueStore&, const Error&)>;
 using KeyValueStoresCallback =
     base::OnceCallback<void(const std::vector<KeyValueStore>&, const Error&)>;
-using KeyValueStoresOnceCallback =
-    base::OnceCallback<void(const std::vector<KeyValueStore>&, const Error&)>;
 using RpcIdentifierCallback =
     base::OnceCallback<void(const RpcIdentifier&, const Error&)>;
 using StringCallback =
     base::OnceCallback<void(const std::string&, const Error&)>;
-using ActivationStateSignalCallback =
-    base::RepeatingCallback<void(uint32_t, uint32_t, const KeyValueStore&)>;
 using ResultStringmapsCallback =
     base::OnceCallback<void(const Stringmaps&, const Error&)>;
 using BrilloAnyCallback = base::OnceCallback<void(
