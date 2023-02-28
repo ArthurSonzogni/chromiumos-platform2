dd10a5ae3618bb9dc5fb47ac415ebef6e9a3827d
Shah Hossain <shahadath@google.com>
2023-02-27 16:41:34 -0800

    resourced: add method to pull borealis CID
    Call concierge_client with cryptohome to get borealis CID.
    
    BUG=b:209841376
    TEST=cargo build --features vm_grpc
    TEST=(on DUT) dbus-send --system --type=signal /org/chromium/VmConcierge
    org.chromium.VmConcierge.VmStartedSignal
    
    Change-Id: I3f1f6a4af6805799a1dc8a86559e391856d166c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4296816
    Reviewed-by: Syed Faaiz Hussain <syedfaaiz@google.com>
    Commit-Queue: Shah Hossain <shahadath@google.com>
    Tested-by: Shah Hossain <shahadath@google.com>

--- resourced/src/vm_grpc/vm_grpc_util.rs
+++ resourced/src/vm_grpc/vm_grpc_util.rs
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use dbus::message::Message;
+use glob::glob;
 use std::path::Path;
+use std::process::Command;
 
 use crate::vm_grpc::vm_grpc_client::VmGrpcClient;
 use crate::vm_grpc::vm_grpc_server::VmGrpcServer;
@@ -17,19 +19,19 @@ const RESOURCED_GRPC_SERVER_PORT: u16 = 5551;
 // VSOCK port where resourced can connect a client.
 const GUEST_VM_GRPC_SERVER_PORT: u16 = 5553;
 
-// Default CID to use for borealis.
-// TODO: remove compile-time hardcode and retrieve from dbus info packet.
-const DEFAULT_GUEST_VM_CID: i16 = 34;
-
 // VSOCK CID for accepting any serverside connection.
 const CID_ANY: i16 = -1;
 
-#[cfg(feature = "vm_grpc")]
 pub(crate) fn vm_grpc_init(msg: &Message) -> Result<()> {
     use std::sync::Arc;
 
     info!("VmStartedSignal RX => {:?}", msg);
-    // TODO: parse msg and retrieve CID. (using hardcoded default temporarily).
+
+    // TODO (b/266499318): remove this when dbus payload decoding is available
+    // resourced run as a daemon should not call an executable.  Use dbus/IPC
+    // calls instead for cryptohome and CID.
+    let borealis_cid = get_vm_cid("borealis")?;
+    info!("Borealis CID: {borealis_cid}");
 
     let root = Path::new("/");
     let pkt_tx_interval = Arc::new(AtomicI64::new(-1));
@@ -37,11 +39,49 @@ pub(crate) fn vm_grpc_init(msg: &Message) -> Result<()> {
 
     let _server = VmGrpcServer::run(CID_ANY, RESOURCED_GRPC_SERVER_PORT, root, pkt_tx_interval)?;
     VmGrpcClient::run(
-        DEFAULT_GUEST_VM_CID,
+        borealis_cid,
         GUEST_VM_GRPC_SERVER_PORT,
         root,
         pkt_tx_interval_clone,
     )?;
 
     Ok(())
+}
+
+// TODO (b/266499318): Remove this once dbus message decoding is implemented.
+pub(crate) fn get_vm_cid(vm_name: &str) -> Result<i16> {
+    let crypto_home = get_crypto_home()?;
+
+    // Blocking call.  concierge_client fails fast.
+    let concierge_out = Command::new("/usr/bin/concierge_client")
+        .arg("--get_vm_cid")
+        .arg(format!("--cryptohome_id={crypto_home}"))
+        .arg(format!("--name={}", vm_name))
+        .output()?;
+
+    Ok(String::from_utf8_lossy(&concierge_out.stdout)
+        .to_string()
+        .strip_suffix('\n')
+        .unwrap_or_default()
+        .parse::<i16>()?)
+}
+
+// TODO (b/266499318): Remove this once dbus message decoding is implemented.
+pub(crate) fn get_crypto_home() -> Result<String> {
+    let crypto_home: String;
+    let mut home_hashes = glob("/home/root/*")?;
+
+    // Only getting the first path match here, which should be the primary user.
+    if let Some(home_hash) = home_hashes.next() {
+        crypto_home = home_hash?
+            .file_name()
+            .and_then(|x| x.to_str())
+            .unwrap_or_default()
+            .to_string();
+        info!("cryptohome: {:?}", crypto_home);
+    } else {
+        bail!("Could not read cryptohome");
+    }
+
+    Ok(crypto_home)
 }
