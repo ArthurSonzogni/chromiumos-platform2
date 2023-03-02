afcfff0f5fda99b21f85b6d8c2da80d162893062
Allen Shih <allenshihmc@google.com>
2023-03-02 03:20:37 +0000

    runtime_probe: Deprecate base::Value::ExtractKey()
    libchrome r1110011 uprev removes base::Value::ExtractKey(). Migrate
    base::Value::ExtractKey() to base::Value::Dict::Extract().
    
    BUG=b:271356665
    TEST=FEATURES=test emerge-${BOARD} runtime_probe
    TEST=Manually test on DUT
    
    Change-Id: Ia99a26791275dc8801b755ae5415693a4a194d26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4302369
    Reviewed-by: Kevin Lin <kevinptt@chromium.org>
    Commit-Queue: Allen Shih <allenshihmc@chromium.org>
    Reviewed-by: Byron Lee <byronlee@chromium.org>
    Tested-by: Allen Shih <allenshihmc@chromium.org>

--- runtime_probe/utils/value_utils.cc
+++ runtime_probe/utils/value_utils.cc
@@ -23,7 +23,7 @@ void PrependToDVKey(base::Value* dict_value, const std::string& prefix) {
     original_keys.push_back(entry.first);
   }
   for (const auto& key : original_keys) {
-    auto value = dict_value->ExtractKey(key);
+    auto value = dict_value->GetDict().Extract(key);
     dict_value->SetKey(prefix + key, std::move(*value));
   }
 }
@@ -33,7 +33,7 @@ bool RenameKey(base::Value* dv,
                const std::string& new_key) {
   if (!dv->is_dict())
     return false;
-  auto value = dv->ExtractKey(old_key);
+  auto value = dv->GetDict().Extract(old_key);
   if (!value)
     return false;
   dv->SetKey(new_key, std::move(*value));
