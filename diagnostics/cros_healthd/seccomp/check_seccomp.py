17fbb574cb4e5f3af8bfaccefcf60eafb27364cf
Chung-Sheng Wu <chungsheng@google.com>
2023-03-02 08:03:46 +0000

    diagnostics: Use GN to control seccomp checker
    Pass the required / denied syscalls as argument to the seccomp
    checker so we can have different list for different sourse set.
    
    BUG=b:270292621
    TEST=emerge-{zork,jacuzzi,jacuzzi64} diagnostics
    
    Change-Id: I80a67a825613f14f962f33fb45926788d643f799
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4302491
    Tested-by: Chung-sheng Wu <chungsheng@google.com>
    Commit-Queue: Chung-sheng Wu <chungsheng@google.com>
    Reviewed-by: Wei-Luan Wang <weiluanwang@google.com>
    Reviewed-by: Kerker Yang <kerker@chromium.org>

--- diagnostics/cros_healthd/seccomp/check_seccomp.py
+++ diagnostics/cros_healthd/seccomp/check_seccomp.py
@@ -12,24 +12,12 @@ import sys
 from chromite.lib import commandline
 
 
-# Lists of syscall which should be in all the policy files.
-REQUIRED_SYSCALL = {
-    "amd64": [],
-    "arm": [],
-    "arm64": [],
-}
-
-# Lists of syscall which should not be in all the policy files.
-DENIED_SYSCALL = {
-    "amd64": [],
-    "arm": [],
-    "arm64": [],
-}
-
 SeccompLine = namedtuple("SeccompLine", ["line_number", "line", "value"])
 
 
-def ParseSeccompAndReturnParseError(arch, filename):
+def ParseSeccompAndReturnParseError(
+    filename, required_syscalls, denied_syscalls
+):
     seccomp = {}
     parse_error = ""
     with open(filename, "r") as f:
@@ -65,7 +53,7 @@ def ParseSeccompAndReturnParseError(arch, filename):
             parse_error += f'{filename}: unexpected termination "\\"\n'
 
     missing_syscalls = ""
-    for syscall in REQUIRED_SYSCALL[arch]:
+    for syscall in required_syscalls:
         if syscall not in seccomp:
             missing_syscalls += f"{syscall}: 1\n"
             continue
@@ -82,7 +70,7 @@ def ParseSeccompAndReturnParseError(arch, filename):
             f"{missing_syscalls}"
         )
 
-    for syscall in DENIED_SYSCALL[arch]:
+    for syscall in denied_syscalls:
         if syscall in seccomp:
             parse_error += (
                 f"{filename}:{seccomp[syscall].line_number}: "
@@ -97,16 +85,23 @@ def ParseSeccompAndReturnParseError(arch, filename):
 def GetParser():
     """Returns an argument parser."""
     parser = commandline.ArgumentParser(description=__doc__)
-    parser.add_argument(
-        "--arch",
-        required=True,
-        help="The architecture of the seccomp files.",
-    )
     parser.add_argument("--seccomp", required=True, help="Seccomp filename.")
     parser.add_argument(
         "--output",
         required=True,
         help="Output filename for the check result (for gn to check timestamp).",
+    )
+    parser.add_argument(
+        "--required-syscalls",
+        default=[],
+        action="append",
+        help="Syscalls which are required.",
+    )
+    parser.add_argument(
+        "--denied-syscalls",
+        default=[],
+        action="append",
+        help="Syscalls which are denied.",
     )
     return parser
 
@@ -116,7 +111,9 @@ def main(argv):
     opts = parser.parse_args(argv)
     opts.Freeze()
 
-    parse_error = ParseSeccompAndReturnParseError(opts.arch, opts.seccomp)
+    parse_error = ParseSeccompAndReturnParseError(
+        opts.seccomp, opts.required_syscalls, opts.denied_syscalls
+    )
     if parse_error:
         sys.stderr.write(parse_error)
         return 1
