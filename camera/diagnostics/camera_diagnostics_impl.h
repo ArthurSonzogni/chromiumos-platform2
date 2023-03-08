a56c0cdefeb254d12718090e7f41055e55a2ea4e
Rabbi Mollah <rabbim@chromium.org>
2023-03-08 12:11:28 +0800

    camera: Implement and expose Camera Diagnostics Service APIs
    In this CL we implement 4 camera diagnostics service APIs and
    expose them through mojo. These APIs to be used by
    camera service and diagnostics service backend for
    sending frames and receiving frame analysis result.
    
    BUG=b:253545976
    TEST=emerge-brya cros-camera-diagnostics
    
    Change-Id: I8ee8c13e7c2397df5a79a4cb67b7defac28aeacd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4318527
    Tested-by: Rabbi Mollah <rabbim@chromium.org>
    Commit-Queue: Rabbi Mollah <rabbim@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>

--- camera/diagnostics/camera_diagnostics_impl.h
+++ camera/diagnostics/camera_diagnostics_impl.h
@@ -5,11 +5,31 @@
 #ifndef CAMERA_DIAGNOSTICS_CAMERA_DIAGNOSTICS_IMPL_H_
 #define CAMERA_DIAGNOSTICS_CAMERA_DIAGNOSTICS_IMPL_H_
 
-#include "camera/diagnostics/mojo/camera_diagnostics.mojom.h"
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+#include "camera/mojo/camera_diagnostics.mojom.h"
+#include "common/privacy_shutter_detector_impl.h"
 
 namespace cros {
 
-class CameraDiagnosticsImpl : public cros::mojom::CameraDiagnostics {};
+class CameraDiagnosticsImpl final : public cros::mojom::CameraDiagnostics {
+ public:
+  CameraDiagnosticsImpl();
+  CameraDiagnosticsImpl(CameraDiagnosticsImpl&) = delete;
+  CameraDiagnosticsImpl& operator=(const CameraDiagnosticsImpl&) = delete;
+  void SetYuvAnalysisEnabled(bool state) final;
+  void GetYuvAnalysisEnabled(GetYuvAnalysisEnabledCallback callback) final;
+  void AnalyzeYuvFrame(mojom::CameraDiagnosticsFramePtr buffer,
+                       AnalyzeYuvFrameCallback callback) final;
+  void GetDiagnosticsResult(GetDiagnosticsResultCallback callback) final;
+
+ private:
+  uint32_t analysis_result_;
+  bool yuv_analysis_enabled_;
+  std::unique_ptr<PrivacyShutterDetector> privacy_shutter_detector_;
+};
 
 }  // namespace cros
 
