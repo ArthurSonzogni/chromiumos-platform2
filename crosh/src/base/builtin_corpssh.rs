e4640cfeab5053c797e2a5d14e77a261d196eb99
Howard Yang <hcyang@google.com>
2023-03-10 11:33:32 +0800

    crosh: Deprecate builtin_corpssh command
    Now that the corpssh feature is enabled by default and not reading this
    flag, we can remove the implementation. However, for user experience,
    keep this command there for a longer while, and modify the help message
    to tell the users the deprecation of the command.
    
    BUG=b:232334144
    TEST=FEATURES=test emerge crosh
    TEST=test on DUT that the command message is clear and correct
    
    Change-Id: I0cdce8a1d6f2bc6958898c7a69c96d767d505450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4328110
    Tested-by: Howard Yang <hcyang@google.com>
    Commit-Queue: Howard Yang <hcyang@google.com>
    Reviewed-by: Yi Chou <yich@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- crosh/src/base/builtin_corpssh.rs
+++ crosh/src/base/builtin_corpssh.rs
@@ -4,76 +4,28 @@
 
 // Provides the command "builtin_corpssh" for crosh which enables/disables
 // built-in gnubby's CorpSSH support.
-
-use dbus::blocking::Connection;
-use system_api::client::OrgChromiumDebugd;
+// This command is deprecated and will be removed in M116.
 
 use crate::dispatcher::{self, Arguments, Command, Dispatcher};
-use crate::util::DEFAULT_DBUS_TIMEOUT;
-
-const INVALID_ARGUMENTS_MSG: &str = "Please pass in a single argument 'enable' or 'disable'.";
-const CORP_PROTOCOL: &str = "corp_protocol";
 
 pub fn register(dispatcher: &mut Dispatcher) {
     dispatcher.register_command(
         Command::new(
             "builtin_corpssh".to_string(),
-            "<enable|disable>".to_string(),
-            "Enable or disable CorpSSH support for the built-in gnubby.".to_string(),
+            "".to_string(),
+            "[Deprecated] Enable or disable CorpSSH support for the \
+             built-in gnubby.\n  This is now always enabled, and the \
+             command will be removed soon."
+                .to_string(),
         )
         .set_command_callback(Some(execute_builtin_corpssh)),
     );
 }
 
-fn execute_builtin_corpssh(_cmd: &Command, args: &Arguments) -> Result<(), dispatcher::Error> {
-    let args = args.get_args();
-    if args.len() != 1 {
-        return Err(dispatcher::Error::CommandInvalidArguments(
-            INVALID_ARGUMENTS_MSG.to_string(),
-        ));
-    }
-
-    let enabled = match args[0].as_str() {
-        "enable" => true,
-        "disable" => false,
-        _ => {
-            return Err(dispatcher::Error::CommandInvalidArguments(
-                INVALID_ARGUMENTS_MSG.to_string(),
-            ))
-        }
-    };
-
-    let connection = Connection::new_system().map_err(|err| {
-        eprintln!("ERROR: Failed to get D-Bus connection: {}", err);
-        dispatcher::Error::CommandReturnedError
-    })?;
-    let conn_path = connection.with_proxy(
-        "org.chromium.debugd",
-        "/org/chromium/debugd",
-        DEFAULT_DBUS_TIMEOUT,
+fn execute_builtin_corpssh(_cmd: &Command, _args: &Arguments) -> Result<(), dispatcher::Error> {
+    eprintln!(
+        "This command is deprecated. CorpSSH support is now \
+     always enabled,\nand the command will be removed soon."
     );
-
-    let flags = conn_path.get_u2f_flags().map_err(|err| {
-        eprintln!("ERROR: Got unexpected result: {}", err);
-        dispatcher::Error::CommandReturnedError
-    })?;
-
-    let mut flags: Vec<&str> = flags.split(',').collect();
-    flags.retain(|&flag| flag != CORP_PROTOCOL);
-    if enabled {
-        flags.push(CORP_PROTOCOL);
-    }
-
-    let flags = flags.join(",");
-    let response = conn_path.set_u2f_flags(&flags).map_err(|err| {
-        eprintln!("ERROR: Got unexpected result: {}", err);
-        dispatcher::Error::CommandReturnedError
-    })?;
-
-    if !response.is_empty() {
-        eprintln!("ERROR: {}", response);
-        return Err(dispatcher::Error::CommandReturnedError);
-    }
-
-    Ok(())
+    Err(dispatcher::Error::CommandReturnedError)
 }
