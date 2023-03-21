7f673093db3fae2d044eeb7ed5f9705b576f0dcd
Justin Huang <justinhuang@google.com>
2023-03-21 03:52:24 +0000

    Revert "Reland "Reland "vm_tools: Change output positioning in sommelier to not overlap"""
    This reverts commit f398dca18b6994f663e634227cc3e337287572f2.
    
    Reason for revert: b/274540666
    
    Original change's description:
    > Reland "Reland "vm_tools: Change output positioning in sommelier to not overlap""
    >
    > Previously windows were being centered using unscaled output dimensions,
    > this change scales the width/height of the output before it gets used to
    > position windows.
    >
    > BUG=b:269053427
    >
    > TEST=Checked that everything still appears to work fine after changing the scaling percentages including this bug not replicating with VSCode
    >
    > This is a reland of commit 2e05b8c30343a760e5253c929dc5c0d8a04a3470
    >
    > Original change's description:
    > > Reland "vm_tools: Change output positioning in sommelier to not overlap"
    > >
    > > This is a reland of commit d52d2c986614b0f85c8b8890cd57cf18633cdd79
    > >
    > > The change was breaking scaling of apps in Crostini due to the physical
    > > dimensions not being scaled before being sent. This CL is now amended
    > > to fix this.
    > >
    > > Original change's description:
    > > > vm_tools: Change output positioning in sommelier to not overlap
    > > >
    > > > Outputs used to all be at (0,0). This CL changes it so that outputs are
    > > > positioned in a row horizontally in order of their x position in exo
    > > > space.
    > > >
    > > > BUG=b:246485823, b:208720580
    > > > TEST=None
    > > >
    > > > Change-Id: I29dd90b4fa9a5b4f4f1d0c1aa1b06483ecc5d449
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4063628
    > > > Commit-Queue: Lucy Qu <lqu@google.com>
    > > > Reviewed-by: Chloe Pelling <cpelling@google.com>
    > > > Tested-by: Lucy Qu <lqu@google.com>
    > >
    > > Bug: b:246485823, b:208720580
    > > Change-Id: I9bbe824a74948aa616ed1c90aa9f3f8e2280633c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4127481
    > > Reviewed-by: David Munro <davidmunro@google.com>
    > > Tested-by: Lucy Qu <lqu@google.com>
    > > Commit-Queue: Lucy Qu <lqu@google.com>
    > > Reviewed-by: Chloe Pelling <cpelling@google.com>
    >
    > Bug: b:246485823, b:208720580
    > Change-Id: I11fc660564e20b94cc0b0a4d1873fd8fac1945e6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4247962
    > Commit-Queue: Lucy Qu <lqu@google.com>
    > Reviewed-by: David Munro <davidmunro@google.com>
    > Tested-by: Lucy Qu <lqu@google.com>
    > Reviewed-by: Chloe Pelling <cpelling@google.com>
    
    Bug: b:269053427
    Bug: b:246485823, b:208720580
    Change-Id: Id162f3987de67a0b00a731baa76bd626e4516cc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4357597
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Justin Huang <justinhuang@google.com>
    Reviewed-by: Lucy Qu <lqu@google.com>
    Reviewed-by: Chloe Pelling <cpelling@google.com>

--- vm_tools/sommelier/sommelier-window.cc
+++ vm_tools/sommelier/sommelier-window.cc
@@ -279,20 +279,10 @@ static void sl_internal_toplevel_configure(struct sl_window* window,
       window->next_config.values[i++] = guest_y;
     } else if (!(window->size_flags & (US_POSITION | P_POSITION))) {
       window->next_config.mask |= XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y;
-      if (window->paired_surface && window->paired_surface->output) {
-        window->next_config.values[i++] =
-            window->paired_surface->output->virt_x +
-            (window->paired_surface->output->virt_width - width_in_pixels) / 2;
-        window->next_config.values[i++] =
-            window->paired_surface->output->virt_y +
-            (window->paired_surface->output->virt_height - height_in_pixels) /
-                2;
-      } else {
-        window->next_config.values[i++] =
-            window->ctx->screen->width_in_pixels / 2 - width_in_pixels / 2;
-        window->next_config.values[i++] =
-            window->ctx->screen->height_in_pixels / 2 - height_in_pixels / 2;
-      }
+      window->next_config.values[i++] =
+          window->ctx->screen->width_in_pixels / 2 - width_in_pixels / 2;
+      window->next_config.values[i++] =
+          window->ctx->screen->height_in_pixels / 2 - height_in_pixels / 2;
     }
     window->next_config.values[i++] = width_in_pixels;
     window->next_config.values[i++] = height_in_pixels;
