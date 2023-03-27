188d55ea88e2c7417644bb7e191e07d422c953c5
Nicholas Bishop <nicholasbishop@google.com>
2023-03-27 17:46:47 -0400

    installer: Add reven partition migration documentation
    BUG=b:249809975
    TEST=FEATURES=test USE=reven_partition_migration emerge-reven chromeos-installer
    
    Change-Id: I68594cc57dddda692e85067a50ca1bcdeb71e33a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4375078
    Reviewed-by: Jeffery Miller <jefferymiller@google.com>
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>
    Reviewed-by: Ted Brandston <tbrandston@google.com>

--- installer/reven_partition_migration.h
+++ installer/reven_partition_migration.h
@@ -14,6 +14,8 @@
 // on the reven board. This is a no-op if the
 // `reven_partition_migration` USE flag is not enabled.
 //
+// See `docs/reven_partition_migration.md` for details of the migration.
+//
 // Returns true if postinstall should be allowed to continue, or false
 // on a fatal failure. Note that returning true does not necessarily
 // indicate that the migration succeeded.
