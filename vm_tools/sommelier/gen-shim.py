f0357ffc02fdde7c8dacd697fb5bd7b39cbc4ea2
Justin Huang <justinhuang@google.com>
2023-03-28 13:32:40 +1100

    vm_tools: Addresses Mike's comments on gen-shim.py
    Ref: crrev.com/c/4349388
    
    BUG=b:255439799
    TEST=CQ
    
    Change-Id: I35f711493aace0b5e5e6b6c7f5420cf8e640ce76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4374961
    Auto-Submit: Justin Huang <justinhuang@google.com>
    Commit-Queue: Justin Huang <justinhuang@google.com>
    Tested-by: Justin Huang <justinhuang@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Chloe Pelling <cpelling@google.com>

--- vm_tools/sommelier/gen-shim.py
+++ vm_tools/sommelier/gen-shim.py
@@ -5,25 +5,50 @@
 
 """A C++ code generator of Wayland protocol shims."""
 
-# pylint: enable=import-error
 import os
+from pathlib import Path
 import sys
+from typing import List, Optional
 from xml.etree import ElementTree
 
 # pylint: disable=import-error
-from jinja2 import Template
+import jinja2
 
 
-def CppTypeForWaylandEventType(xml_type_string, interface):
-    """Generates the type for a wayland event argument."""
+def CppTypeForWaylandEventType(xml_type_string: str, interface: str) -> str:
+    """Generates the type for a wayland event argument.
+
+    Converts a wayland type to a CPP type for an event.
+
+    Args:
+        xml_type_string: The string describing the type of the wayland type
+            that's present inside the wayland protocol XML.
+        interface: The string describing the interface of the object.
+
+    Returns:
+        A string which contains the CPP type which will be inserted into the
+        generated shim.
+    """
     if xml_type_string == "new_id" or xml_type_string == "object":
         return "struct wl_resource *"
     else:
         return CppTypeForWaylandType(xml_type_string, interface)
 
 
-def CppTypeForWaylandType(xml_type_string, interface):
-    """Generates the type for generic wayland type."""
+def CppTypeForWaylandType(xml_type_string: str, interface: str) -> str:
+    """Generates the type for generic wayland type.
+
+    Converts a wayland type to a CPP type in a generalized fashion.
+
+    Args:
+        xml_type_string: The string describing the type of the wayland type
+            that's present inside the wayland protocol XML.
+        interface: The string describing the interface of the object.
+
+    Returns:
+        A string which contains the CPP type which will be inserted into the
+        generated shim.
+    """
     if xml_type_string == "int" or xml_type_string == "fd":
         return "int32_t"
     elif xml_type_string == "uint" or xml_type_string == "new_id":
@@ -33,26 +58,55 @@ def CppTypeForWaylandType(xml_type_string, interface):
     elif xml_type_string == "string":
         return "const char *"
     elif xml_type_string == "object":
-        return "struct %s *" % interface
+        return f"struct {interface} *"
     elif xml_type_string == "array":
         return "struct wl_array *"
     else:
-        raise ValueError("Invalid Type conversion: %s" % xml_type_string)
+        raise ValueError(f"Invalid Type conversion: {xml_type_string}")
 
 
-def GetRequestReturnType(args):
-    """Gets the return type of a Wayland request."""
-    for arg in args:
+def GetRequestReturnType(arguments: List[object]) -> str:
+    """Gets the return type of a Wayland request.
+
+    Args:
+        arguments: A list of XML tag objects which contains the information
+            about all arguments that will be provided to the request
+
+    Returns:
+        A string which contains the CPP return type of the function.
+    """
+    for arg in arguments:
         if arg.attrib["type"] == "new_id":
             if "interface" in arg.attrib:
-                return "struct %s *" % arg.attrib["interface"]
+                return f"struct {arg.attrib['interface']} *"
             else:
                 return "void *"
     return "void"
 
 
-def RequestXmlToJinjaInput(request):
-    """Parses a <request> element into dictionary form for use of jinja template."""
+def RequestXmlToJinjaInput(request: object) -> dict:
+    """Parses a |request| element into dictionary form for use of jinja \
+    template.
+
+    Args:
+        request: An XML tag object which contains the information about a
+            request.
+
+    Returns:
+        A dictionary of format
+        {
+            "name": Method name of the request (e.g. set_anchor),
+            "args": [
+                {
+                    "name": Name of the argument (e.g. x),
+                    "type": CPP Type of the argument (e.g. int)
+                }
+            ],
+            "ret": Return value of the request
+        }
+        which is used by the jinja template to generate the shim contents for a
+        request.
+    """
     method = {"name": request.attrib["name"], "args": [], "ret": ""}
     method["ret"] = GetRequestReturnType(request.findall("arg"))
 
@@ -78,8 +132,26 @@ def RequestXmlToJinjaInput(request):
     return method
 
 
-def EventXmlToJinjaInput(event):
-    """Parses an <event> element into dictionary for for use of jinja template."""
+def EventXmlToJinjaInput(event: object) -> dict:
+    """Parses an |event| element into dictionary for use of jinja template.
+
+    Args:
+        event: An XML tag object which contains information about an event.
+
+    Returns:
+        A dictionary of format
+        {
+            "name": Name of the event (e.g. ping)
+            "args": [
+                {
+                    "name": Name of the argument (e.g. serial)
+                    "type": CPP Type of the argument (e.g. uint_32t)
+                }
+            ]
+        }
+        which is used by the jinja template to generate shim contents for an
+        event.
+    """
     return {
         "name": event.attrib["name"],
         "args": [
@@ -94,42 +166,64 @@ def EventXmlToJinjaInput(event):
     }
 
 
-def InterfaceXmlToJinjaInput(interface):
-    """Creates an interface dict for XML interface input."""
+def InterfaceXmlToJinjaInput(interface: object) -> dict:
+    """Creates an interface dict for XML interface input.
+
+    Args:
+        interface: An XML tag object which contains the information about an
+            interface.
+
+    Returns:
+        A dictionary of format
+        {
+            "name": The name of the interface,
+            "name_underscore": Name of the interface in snake_case,
+            "methods": [<Request>] # Where request dicts are defined above.
+                Naming dfference due to wayland-scanner patterns.
+            "events": [<Event>] # Where event dicts are defined above.
+        }
+        which is used by the jinja template to generate shim contents for the
+        interface.
+    """
     interf = {
         "name": "".join(
-            [i.capitalize() for i in interface.attrib["name"].split("_")]
+            [x.capitalize() for x in interface.attrib["name"].split("_")]
         )
         + "Shim",
         "name_underscore": interface.attrib["name"],
         "methods": [
-            RequestXmlToJinjaInput(i) for i in interface.findall("request")
+            RequestXmlToJinjaInput(x) for x in interface.findall("request")
         ],
-        "events": [EventXmlToJinjaInput(i) for i in interface.findall("event")],
+        "events": [EventXmlToJinjaInput(x) for x in interface.findall("event")],
     }
     return interf
 
 
-def GenerateShims(in_xml, out_directory):
-    """Generates shims for Wayland Protocols."""
-    with open(
-        os.path.dirname(os.path.abspath(__file__))
-        + "/gen/protocol-shim.h.jinja2",
-        encoding="utf-8",
-    ) as f:
-        shim_template = Template(f.read())
-    with open(
-        os.path.dirname(os.path.abspath(__file__))
-        + "/gen/protocol-shim.cc.jinja2",
-        encoding="utf-8",
-    ) as f:
-        shim_impl_template = Template(f.read())
-    with open(
-        os.path.dirname(os.path.abspath(__file__))
-        + "/gen/mock-protocol-shim.h.jinja2",
-        encoding="utf-8",
-    ) as f:
-        mock_template = Template(f.read())
+def GenerateShims(in_xml: str, out_directory: str) -> None:
+    """Generates shims for Wayland Protocols.
+
+    Args:
+        in_xml: The location of the .xml definition of a wayland protocol.
+        out_directory: The directory to output the generated shim files.
+    """
+    template_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "gen"
+    )
+    template_dir = Path(__file__).resolve().parent / "gen"
+    h_data = (template_dir / "protocol-shim.h.jinja2").read_text(
+        encoding="utf-8"
+    )
+    shim_template = jinja2.Template(h_data)
+
+    cc_data = (template_dir / "protocol-shim.cc.jinja2").read_text(
+        encoding="utf-8"
+    )
+    shim_impl_template = jinja2.Template(cc_data)
+
+    mock_data = (template_dir / "protocol-shim.cc.jinja2").read_text(
+        encoding="utf-8"
+    )
+    mock_template = jinja2.Template(mock_data)
 
     tree = ElementTree.parse(in_xml)
     root = tree.getroot()
@@ -137,39 +231,32 @@ def GenerateShims(in_xml, out_directory):
     filename = os.path.basename(in_xml).split(".")[0]
 
     # Because some protocol files don't have the protocol name == file name, we
-    # have to infer the name from the file name instead (gtk-shell :eyes:)
+    # have to infer the name from the file name instead (gtk-shell :eyes:).
     protocol = {
         "interfaces": [
-            InterfaceXmlToJinjaInput(i) for i in root.findall("interface")
+            InterfaceXmlToJinjaInput(x) for x in root.findall("interface")
         ],
         "name_hyphen": filename,
         "name_underscore": filename.replace("-", "_"),
     }
 
-    with open(
-        out_directory + "/" + protocol["name_hyphen"] + "-shim.h",
-        "w",
-        encoding="utf-8",
-    ) as f:
-        f.write(shim_template.render(protocol=protocol))
+    (out_directory / (protocol["name_hyphen"] + "-shim.h")).write_text(
+        shim_template.render(protocol=protocol), encoding="utf-8"
+    )
+    (out_directory / (protocol["name_hyphen"] + "-shim.cc")).write_text(
+        shim_impl_template.render(protocol=protocol), encoding="utf-8"
+    )
+    (
+        out_directory / ("mock-" + protocol["name_hyphen"] + "-shim.h")
+    ).write_text(mock_template.render(protocol=protocol), encoding="utf-8")
 
-    with open(
-        out_directory + "/" + protocol["name_hyphen"] + "-shim.cc",
-        "w",
-        encoding="utf-8",
-    ) as f:
-        f.write(shim_impl_template.render(protocol=protocol))
 
-    with open(
-        out_directory + "/mock-" + protocol["name_hyphen"] + "-shim.h",
-        "w",
-        encoding="utf-8",
-    ) as f:
-        f.write(mock_template.render(protocol=protocol))
+def main(argv: Optional[List[str]]):
+    assert len(argv) == 2
+    source_xml = Path(argv[0])
+    out_dir = Path(argv[1])
+    GenerateShims(source_xml, out_dir)
 
 
 if __name__ == "__main__":
-    source_xml = sys.argv[1]
-    out_dir = sys.argv[2]
-
-    GenerateShims(source_xml, out_dir)
+    sys.exit(main(sys.argv[1:]))
