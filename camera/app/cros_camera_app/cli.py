48efe00466fdb38b7ca2bc18cbfe276245f54c3f
Shik Chen <shik@chromium.org>
2023-03-31 18:59:20 +0800

    camera: app: Refactor arguments parsing with decorators
    This CL adds two decorators @cli.command and @cli.option to simplify
    command line arguments declaration and parsing. The parsing logic is now
    co-located with the command, and the cmd_* function can take the
    expected parameters precisely instead of using the loosely-typed
    arguments.Namespace.
    
    The decorator API is a minimal version of the popular "Click" package
    for CLI.
    
    BUG=b:194991677
    TEST=tast run <dut> camera.CCACLI
    
    Change-Id: I24c7c7497d931dd7d08cef4598e80603adf3ad2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4387229
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Auto-Submit: Shik Chen <shik@chromium.org>

--- camera/app/cros_camera_app/cli.py
+++ camera/app/cros_camera_app/cli.py
@@ -10,12 +10,15 @@ Run `cca help` for more information about the supported subcommands.
 
 import argparse
 import codecs
+import collections
 import enum
+import functools
+import inspect
 import logging
 import pathlib
 import shutil
 import sys
-from typing import List, Optional, Type
+from typing import Callable, List, Optional, Type
 
 from cros_camera_app import app
 from cros_camera_app import device
@@ -64,135 +67,6 @@ class EnumAction(argparse.Action):
         setattr(namespace, self.dest, enum_value)
 
 
-def cmd_setup(args: argparse.Namespace):
-    del args
-    device.setup()
-
-
-# TODO(shik): Wake up the display if it's sleeping.
-def cmd_open(args: argparse.Namespace):
-    cca = app.CameraApp()
-    cca.open(facing=args.facing, mode=args.mode)
-
-
-def cmd_close(args: argparse.Namespace):
-    del args  # unused
-    cca = app.CameraApp()
-    cca.close()
-
-
-# TODO(shik): Provide an option to reuse the existing CCA session and not to
-# close the app afterward.
-def cmd_take_photo(args: argparse.Namespace):
-    cca = app.CameraApp()
-    path = cca.take_photo(facing=args.facing)
-    if args.output:
-        shutil.copy2(path, args.output)
-        logging.info("Copied photo from %s to %s", path, args.output)
-    else:
-        logging.info("Saved photo at %s", path)
-
-
-def cmd_record_video(args: argparse.Namespace):
-    cca = app.CameraApp()
-    path = cca.record_video(facing=args.facing, duration=args.duration)
-    if args.output:
-        shutil.copy2(path, args.output)
-        logging.info("Copied video from %s to %s", path, args.output)
-    else:
-        logging.info("Saved video at %s", path)
-
-
-def parse_args(argv: Optional[List[str]]) -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        description="ChromeOS Camera App (CCA) CLI."
-    )
-    parser.add_argument(
-        "--debug",
-        action="store_true",
-        help="enable debug logging",
-    )
-    parser.set_defaults(func=lambda _: parser.print_help())
-    subparsers = parser.add_subparsers()
-
-    setup_parser = subparsers.add_parser(
-        "setup",
-        help="Setup the DUT",
-        description="Setup the DUT to make it ready to be controlled remotely.",
-    )
-    setup_parser.set_defaults(func=cmd_setup)
-
-    open_parser = subparsers.add_parser(
-        "open",
-        help="Open CCA",
-        description="Open CCA.",
-    )
-    open_parser.add_argument(
-        "--facing",
-        help="facing of the camera to be opened",
-        action=EnumAction,
-        enum_type=app.Facing,
-    )
-    open_parser.add_argument(
-        "--mode",
-        help="target capture mode in app",
-        action=EnumAction,
-        enum_type=app.Mode,
-    )
-    open_parser.set_defaults(func=cmd_open)
-
-    close_parser = subparsers.add_parser(
-        "close",
-        help="Close CCA",
-        description="Close CCA if it's open.",
-    )
-    close_parser.set_defaults(func=cmd_close)
-
-    take_photo_parser = subparsers.add_parser(
-        "take-photo",
-        help="Take a photo",
-        description="Take a photo using CCA.",
-    )
-    take_photo_parser.add_argument(
-        "--facing",
-        help="facing of the camera to be captured",
-        action=EnumAction,
-        enum_type=app.Facing,
-    )
-    take_photo_parser.add_argument(
-        "--output",
-        help="output path to save the photo",
-        type=pathlib.Path,
-    )
-    take_photo_parser.set_defaults(func=cmd_take_photo)
-
-    record_video_parser = subparsers.add_parser(
-        "record-video",
-        help="Record a video",
-        description="Record a video using CCA.",
-    )
-    record_video_parser.add_argument(
-        "--facing",
-        help="facing of the camera to be recorded",
-        action=EnumAction,
-        enum_type=app.Facing,
-    )
-    record_video_parser.add_argument(
-        "--duration",
-        help="duration in seconds to be recorded",
-        type=float,
-        default=3,
-    )
-    record_video_parser.add_argument(
-        "--output",
-        help="output path to save the video",
-        type=pathlib.Path,
-    )
-    record_video_parser.set_defaults(func=cmd_record_video)
-
-    return parser.parse_args(argv)
-
-
 def setup_logging(debug: bool):
     # ChromeOS shell might use C locale instead of UTF-8, which may trigger
     # encoding error when printing non-ASCII characters. Here we enforce stdout
@@ -214,11 +88,189 @@ def setup_logging(debug: bool):
     logging.basicConfig(level=log_level, format=log_format)
 
 
+class CLIRunner:
+    """Runner to parse command line arguments and dispatch commands."""
+
+    def __init__(self):
+        """Initializes the instance."""
+
+        parser = argparse.ArgumentParser(
+            description="ChromeOS Camera App (CCA) CLI."
+        )
+        parser.add_argument(
+            "--debug",
+            action="store_true",
+            help="enable debug logging",
+        )
+        parser.set_defaults(func=lambda _: parser.print_help())
+
+        self._parser = parser
+        self._subparsers = self._parser.add_subparsers(
+            title="supported commands"
+        )
+        self._options = collections.defaultdict(list)
+
+    def command(self, *args, **kwargs):
+        """Decorator to register a new command.
+
+        Args:
+            *args: The arguments to be forwarded to add_parser().
+            **kwargs: The keyword arguments to be forwarded to add_parser().
+        """
+        cmd_parser = self._subparsers.add_parser(*args, **kwargs)
+
+        def decorator(func: Callable):
+            for args, kwargs in self._options[func]:
+                cmd_parser.add_argument(*args, **kwargs)
+
+            @functools.wraps(func)
+            def wrapper(args):
+                # Extract the function parameters from parsed arguments.
+                params = inspect.signature(func).parameters
+                unwrapped_args = {k: getattr(args, k) for k in params}
+                func(**unwrapped_args)
+
+            cmd_parser.set_defaults(func=wrapper)
+
+            return func
+
+        return decorator
+
+    def option(self, *args, **kwargs):
+        """Decorator to register an option for the current command.
+
+        Args:
+            *args: The arguments to be forwarded to add_argument().
+            **kwargs: The keyword arguments to be forwarded to add_argument().
+        """
+
+        def decorator(func: Callable):
+            self._options[func].append((args, kwargs))
+            return func
+
+        return decorator
+
+    def run(self, argv: Optional[List[str]] = None) -> Optional[int]:
+        """Parses the arguments and runs the target command.
+
+        Args:
+            argv: The command line arguments.
+
+        Returns:
+            An optional return code that is suitable to be used with sys.exit().
+        """
+        args = self._parser.parse_args(argv)
+        setup_logging(args.debug)
+        return args.func(args)
+
+
+cli = CLIRunner()
+
+
+@cli.command(
+    "setup",
+    help="Setup the DUT",
+    description="Setup the DUT to make it ready to be controlled remotely.",
+)
+def cmd_setup():
+    device.setup()
+
+
+@cli.command(
+    "open",
+    help="Open CCA",
+    description="Open CCA.",
+)
+@cli.option(
+    "--facing",
+    help="facing of the camera to be opened",
+    action=EnumAction,
+    enum_type=app.Facing,
+)
+@cli.option(
+    "--mode",
+    help="target capture mode in app",
+    action=EnumAction,
+    enum_type=app.Mode,
+)
+def cmd_open(facing: app.Facing, mode: app.Mode):
+    # TODO(shik): Wake up the display if it's sleeping.
+    cca = app.CameraApp()
+    cca.open(facing=facing, mode=mode)
+
+
+@cli.command(
+    "close",
+    help="Close CCA",
+    description="Close CCA if it's open.",
+)
+def cmd_close():
+    cca = app.CameraApp()
+    cca.close()
+
+
+@cli.command(
+    "take-photo",
+    help="Take a photo",
+    description="Take a photo using CCA.",
+)
+@cli.option(
+    "--facing",
+    help="facing of the camera to be captured",
+    action=EnumAction,
+    enum_type=app.Facing,
+)
+@cli.option(
+    "--output",
+    help="output path to save the photo",
+    type=pathlib.Path,
+)
+def cmd_take_photo(facing: app.Facing, output: pathlib.Path):
+    # TODO(shik): Provide an option to reuse the existing CCA session and not to
+    # close the app afterward.
+    cca = app.CameraApp()
+    path = cca.take_photo(facing=facing)
+    if output:
+        shutil.copy2(path, output)
+        logging.info("Copied photo from %s to %s", path, output)
+    else:
+        logging.info("Saved photo at %s", path)
+
+
+@cli.command(
+    "record-video",
+    help="Record a video",
+    description="Record a video using CCA.",
+)
+@cli.option(
+    "--facing",
+    help="facing of the camera to be recorded",
+    action=EnumAction,
+    enum_type=app.Facing,
+)
+@cli.option(
+    "--duration",
+    help="duration in seconds to be recorded",
+    type=float,
+    default=3,
+)
+@cli.option(
+    "--output",
+    help="output path to save the video",
+    type=pathlib.Path,
+)
+def cmd_record_video(facing: app.Facing, duration: float, output: pathlib.Path):
+    cca = app.CameraApp()
+    path = cca.record_video(facing=facing, duration=duration)
+    if output:
+        shutil.copy2(path, output)
+        logging.info("Copied video from %s to %s", path, output)
+    else:
+        logging.info("Saved video at %s", path)
+
+
 def main(argv: Optional[List[str]] = None) -> Optional[int]:
-    args = parse_args(argv)
-    setup_logging(args.debug)
-    logging.debug("args = %s", args)
-    return args.func(args)
+    return cli.run(argv)
 
 
 if __name__ == "__main__":
