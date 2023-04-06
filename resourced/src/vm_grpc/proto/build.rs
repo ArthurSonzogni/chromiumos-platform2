e18350a566a95d2bfa0d02dd6489250bd71cd5fc
Syed Faaiz Hussain <syedfaaiz@google.com>
2023-04-06 12:28:24 -0700

    resourced: Use concierge's D-Bus API to support dynamic CIDs
    BUG=b:274378849
    BUG=b:209841376
    TEST=cargo build --features vm_grpc
    TEST=(on DUT) vmc launch borealis shows new CID after enabling
    resourced.
    
    Change-Id: Id04fcd1eca5b8ef02b13f96422af73e06c1d8788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4407864
    Auto-Submit: Syed Faaiz Hussain <syedfaaiz@google.com>
    Reviewed-by: Abhishek Bhardwaj <abhishekbh@chromium.org>
    Tested-by: Syed Faaiz Hussain <syedfaaiz@google.com>
    Commit-Queue: Syed Faaiz Hussain <syedfaaiz@google.com>
    Reviewed-by: Vovo Yang <vovoy@chromium.org>

--- resourced/src/vm_grpc/proto/build.rs
+++ resourced/src/vm_grpc/proto/build.rs
@@ -3,7 +3,55 @@
 // found in the LICENSE file.
 
 #[cfg(feature = "vm_grpc")]
+use anyhow::Result;
+#[cfg(feature = "vm_grpc")]
+use std::env;
+#[cfg(feature = "vm_grpc")]
+use std::fs::{self, File};
+#[cfg(feature = "vm_grpc")]
+use std::io::Write;
+#[cfg(feature = "vm_grpc")]
+use std::path::{Path, PathBuf};
+#[cfg(feature = "vm_grpc")]
 extern crate protoc_grpcio;
+
+// TODO(b:277383885): Share proto building code with crostini_client
+#[cfg(feature = "vm_grpc")]
+const PROTOS_TO_GENERATE: &[(&str, &str)] = &[
+    ("arc", "dbus/arc/arc.proto"),
+    ("vm_concierge", "dbus/vm_concierge/concierge_service.proto"),
+];
+
+#[cfg(feature = "vm_grpc")]
+fn generate_protos(source_dir: &Path, protos: &[(&str, &str)]) -> Result<()> {
+    let out_dir = PathBuf::from("src/vm_grpc/vm_grpc_util/");
+    if out_dir.exists() {
+        // If CROS_RUST is set, skip generation.
+        if env::var("CROS_RUST") == Ok(String::from("1")) {
+            return Ok(());
+        }
+        fs::remove_dir_all(&out_dir)?;
+    }
+    fs::create_dir_all(&out_dir)?;
+    let mut out = File::create(out_dir.join("include_protos.rs"))?;
+    for (module, input_path) in protos {
+        let input_path = source_dir.join(input_path);
+        let input_dir = input_path.parent().unwrap();
+        let parent_input_dir = source_dir.join("dbus");
+        // Invoke protobuf compiler.
+        protoc_rust::Codegen::new()
+            .input(input_path.as_os_str().to_str().unwrap())
+            .include(input_dir.as_os_str().to_str().unwrap())
+            .include(parent_input_dir)
+            .out_dir(&out_dir)
+            .run()
+            .expect("protoc could not compile concierge_service proto");
+
+        // Write out a `mod` that refers to the generated module.
+        writeln!(out, "pub mod {};", module)?;
+    }
+    Ok(())
+}
 
 fn main() {
     #[cfg(feature = "vm_grpc")]
@@ -18,5 +66,11 @@ fn main() {
             None,
         )
         .expect("Failed to compile gRPC definitions!");
+        let source_dir = match env::var("SYSROOT") {
+            Ok(path) => PathBuf::from(path).join("usr/include/chromeos"),
+            // Make this work when typing "cargo build" in platform2/vm_tools/crostini_client
+            Err(_) => PathBuf::from("../system_api"),
+        };
+        generate_protos(&source_dir, PROTOS_TO_GENERATE).unwrap();
     }
 }
--- resourced/src/vm_grpc/vm_grpc_util.rs
+++ resourced/src/vm_grpc/vm_grpc_util.rs
@@ -2,16 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::{bail, Result};
-use dbus::message::Message;
-use glob::glob;
-use std::path::Path;
-use std::process::Command;
-
 use crate::vm_grpc::vm_grpc_client::VmGrpcClient;
 use crate::vm_grpc::vm_grpc_server::VmGrpcServer;
-use libchromeos::sys::info;
+use anyhow::{bail, Result};
+use dbus::message::Message;
+use protobuf::CodedInputStream;
+use protobuf::Message as protoMessage;
+use std::path::Path;
 use std::sync::atomic::AtomicI64;
+mod arc;
+mod concierge_service;
 
 // VSOCK port to use for accepting GRPC socket connections.
 const RESOURCED_GRPC_SERVER_PORT: u16 = 5551;
@@ -22,17 +22,26 @@ const GUEST_VM_GRPC_SERVER_PORT: u16 = 5553;
 // VSOCK CID for accepting any serverside connection.
 const CID_ANY: i16 = -1;
 
+// Name for the borealis VM.
+const BOREALIS_VM_NAME: &str = "borealis";
+
+// Extracts the CID for the borealis VM from the D-bus message.
+fn get_borealis_cid(msg: &Message) -> Result<i16> {
+    let raw_buffer: Vec<u8> = msg.read1()?;
+    let input = &mut CodedInputStream::from_bytes(&raw_buffer);
+    let mut borealis_vm = concierge_service::VmStartedSignal::new();
+    borealis_vm.merge_from(input)?;
+    let name_vm = borealis_vm.get_name();
+    if name_vm != BOREALIS_VM_NAME {
+        bail!("ignoring VmStartedSIgnal for {}.", name_vm)
+    }
+    let borealis_cid = borealis_vm.get_vm_info().get_cid() as i16;
+    Ok(borealis_cid)
+}
+
 pub(crate) fn vm_grpc_init(msg: &Message) -> Result<()> {
     use std::sync::Arc;
-
-    info!("VmStartedSignal RX => {:?}", msg);
-
-    // TODO (b/266499318): remove this when dbus payload decoding is available
-    // resourced run as a daemon should not call an executable.  Use dbus/IPC
-    // calls instead for cryptohome and CID.
-    let borealis_cid = get_vm_cid("borealis")?;
-    info!("Borealis CID: {borealis_cid}");
-
+    let borealis_cid = get_borealis_cid(msg)?;
     let root = Path::new("/");
     let pkt_tx_interval = Arc::new(AtomicI64::new(-1));
     let pkt_tx_interval_clone = pkt_tx_interval.clone();
@@ -46,42 +55,4 @@ pub(crate) fn vm_grpc_init(msg: &Message) -> Result<()> {
     )?;
 
     Ok(())
-}
-
-// TODO (b/266499318): Remove this once dbus message decoding is implemented.
-pub(crate) fn get_vm_cid(vm_name: &str) -> Result<i16> {
-    let crypto_home = get_crypto_home()?;
-
-    // Blocking call.  concierge_client fails fast.
-    let concierge_out = Command::new("/usr/bin/concierge_client")
-        .arg("--get_vm_cid")
-        .arg(format!("--cryptohome_id={crypto_home}"))
-        .arg(format!("--name={}", vm_name))
-        .output()?;
-
-    Ok(String::from_utf8_lossy(&concierge_out.stdout)
-        .to_string()
-        .strip_suffix('\n')
-        .unwrap_or_default()
-        .parse::<i16>()?)
-}
-
-// TODO (b/266499318): Remove this once dbus message decoding is implemented.
-pub(crate) fn get_crypto_home() -> Result<String> {
-    let crypto_home: String;
-    let mut home_hashes = glob("/home/root/*")?;
-
-    // Only getting the first path match here, which should be the primary user.
-    if let Some(home_hash) = home_hashes.next() {
-        crypto_home = home_hash?
-            .file_name()
-            .and_then(|x| x.to_str())
-            .unwrap_or_default()
-            .to_string();
-        info!("cryptohome: {:?}", crypto_home);
-    } else {
-        bail!("Could not read cryptohome");
-    }
-
-    Ok(crypto_home)
 }
