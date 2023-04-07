3940219cf59d3d551c7c199f72941102d8894b20
Firas Sammoura <fsammoura@google.com>
2023-04-07 22:22:40 +0000

    libec: Create Factory Command for EC_CMD_FLASH_PROTECT
    BUG=b:116396469
    TEST=FEATURES="test" emerge-hatch libec
    
    Change-Id: I57b1c07c04f7028e12501069f0dbd3dbc5b68b7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4409081
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>

---
+++ libec/flash_protect_command_factory.cc
@@ -0,0 +1,21 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "libec/flash_protect_command_factory.h"
+
+namespace ec {
+
+std::unique_ptr<EcCommandInterface> FlashProtectCommandFactory::Create(
+    CrosFpDeviceInterface* cros_fp,
+    flash_protect::Flags flags,
+    flash_protect::Flags mask) {
+  if (cros_fp->EcCmdVersionSupported(EC_CMD_FLASH_PROTECT, 2) ==
+      EcCmdVersionSupportStatus::SUPPORTED) {
+    return std::make_unique<ec::FlashProtectCommand_v2>(flags, mask);
+  }
+
+  return std::make_unique<ec::FlashProtectCommand>(flags, mask);
+}
+
+}  // namespace ec
