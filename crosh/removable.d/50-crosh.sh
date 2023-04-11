ff11b9d5a7c5a5d3cdb8ec10266565ff41661b3f
Jae Hoon Kim <kimjae@chromium.org>
2023-04-11 22:03:47 +0000

    crosh: Typo fix
    BUG=none
    TEST=none
    
    Change-Id: I3c478d44962917096602b9bfe48ebae8eada9e47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4415093
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>
    Auto-Submit: Jae Hoon Kim <kimjae@chromium.org>
    Tested-by: Jae Hoon Kim <kimjae@chromium.org>

--- crosh/removable.d/50-crosh.sh
+++ crosh/removable.d/50-crosh.sh
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Chrosh commands that are only loaded when we're booting from removable
+# Crosh commands that are only loaded when we're booting from removable
 # media.
 
 do_install() {
