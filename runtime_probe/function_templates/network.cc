8b712d3db8bf68f8af2e1bfdce1bacf1f57fbbdd
Chung-Sheng Wu <chungsheng@google.com>
2023-04-14 04:32:38 +0000

    runtime_probe: Split bus probing logic
    Move bus probing logic from network function to another file. This is to
    reuse the logic in other probe functions. Also refactor related tests.
    
    BUG=b:277876413,b:194903437
    TEST=FEATURES=test emerge-zork runtime_probe
    
    Change-Id: I5c8b69607370d941bf7a1a403562380666a29a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4421376
    Reviewed-by: Kevin Lin <kevinptt@chromium.org>
    Commit-Queue: Chung-sheng Wu <chungsheng@google.com>
    Tested-by: Chung-sheng Wu <chungsheng@google.com>

--- runtime_probe/function_templates/network.cc
+++ runtime_probe/function_templates/network.cc
@@ -11,12 +11,8 @@
 #include <vector>
 
 #include <base/containers/contains.h>
-#include <base/containers/fixed_flat_map.h>
-#include <base/containers/span.h>
 #include <base/files/file_path.h>
-#include <base/files/file_util.h>
 #include <base/logging.h>
-#include <base/strings/string_piece.h>
 #include <base/values.h>
 #include <brillo/dbus/dbus_connection.h>
 #include <brillo/variant_dictionary.h>
@@ -25,9 +21,8 @@
 #include <shill/dbus-proxies.h>
 
 #include "runtime_probe/system/context.h"
+#include "runtime_probe/utils/bus_utils.h"
 #include "runtime_probe/utils/file_utils.h"
-#include "runtime_probe/utils/type_utils.h"
-#include "runtime_probe/utils/value_utils.h"
 
 namespace runtime_probe {
 namespace {
@@ -38,48 +33,6 @@ constexpr const char* kValidNetworkTypes[] = {
     shill::kTypeCellular,
     shill::kTypeEthernet,
 };
-
-constexpr char kBusTypePci[] = "pci";
-constexpr char kBusTypeSdio[] = "sdio";
-constexpr char kBusTypeUsb[] = "usb";
-
-constexpr auto kPciFields =
-    base::MakeFixedFlatMap<base::StringPiece, base::StringPiece>(
-        {{"vendor_id", "vendor"}, {"device_id", "device"}});
-constexpr auto kPciOptionalFields =
-    base::MakeFixedFlatMap<base::StringPiece, base::StringPiece>(
-        {{"revision", "revision"}, {"subsystem", "subsystem_device"}});
-constexpr auto kSdioFields =
-    base::MakeFixedFlatMap<base::StringPiece, base::StringPiece>(
-        {{"vendor_id", "vendor"}, {"device_id", "device"}});
-constexpr auto kUsbFields =
-    base::MakeFixedFlatMap<base::StringPiece, base::StringPiece>(
-        {{"vendor_id", "idVendor"}, {"product_id", "idProduct"}});
-constexpr auto kUsbOptionalFields =
-    base::MakeFixedFlatMap<base::StringPiece, base::StringPiece>(
-        {{"bcd_device", "bcdDevice"}});
-
-constexpr int PCI_REVISION_ID_OFFSET = 0x08;
-
-// For linux kernels of versions before 4.10-rc1, there is no standalone file
-// `revision` describing the revision id of the PCI component.  The revision is
-// still available at offset 8 of the binary file `config`.
-std::optional<uint8_t> GetPciRevisionIdFromConfig(base::FilePath node_path) {
-  const auto file_path = node_path.Append("config");
-  if (!base::PathExists(file_path)) {
-    LOG(ERROR) << file_path.value() << " doesn't exist.";
-    return std::nullopt;
-  }
-  base::File config{file_path, base::File::FLAG_OPEN | base::File::FLAG_READ};
-  uint8_t revision_array[1];
-  base::span<uint8_t> revision_span(revision_array);
-  if (!config.ReadAndCheck(PCI_REVISION_ID_OFFSET, revision_span)) {
-    LOG(ERROR) << "Cannot read file " << file_path << " at offset "
-               << PCI_REVISION_ID_OFFSET;
-    return std::nullopt;
-  }
-  return revision_array[0];
-}
 
 std::map<std::string, std::string> GetDevicesType() {
   std::map<std::string, std::string> result;
@@ -114,46 +67,6 @@ std::map<std::string, std::string> GetDevicesType() {
   return result;
 }
 
-std::optional<base::Value> GetNetworkData(const base::FilePath& node_path) {
-  const auto dev_path = node_path.Append("device");
-  const auto dev_subsystem_path = dev_path.Append("subsystem");
-  base::FilePath dev_subsystem_link_path;
-  if (!base::ReadSymbolicLink(dev_subsystem_path, &dev_subsystem_link_path)) {
-    VLOG(2) << "Cannot get real path of " << dev_subsystem_path;
-    return std::nullopt;
-  }
-  std::string bus_type = dev_subsystem_link_path.BaseName().value();
-
-  std::optional<base::Value> res;
-  if (bus_type == kBusTypePci) {
-    res = MapFilesToDict(dev_path, kPciFields, kPciOptionalFields);
-    if (res && !res->GetDict().FindString("revision")) {
-      auto revision_id = GetPciRevisionIdFromConfig(dev_path);
-      if (revision_id) {
-        res->GetDict().Set("revision", ByteToHexString(*revision_id));
-      }
-    }
-  } else if (bus_type == kBusTypeSdio) {
-    res = MapFilesToDict(dev_path, kSdioFields);
-  } else if (bus_type == kBusTypeUsb) {
-    auto field_path = base::MakeAbsoluteFilePath(dev_path.Append(".."));
-    res = MapFilesToDict(field_path, kUsbFields, kUsbOptionalFields);
-  } else {
-    LOG(ERROR) << "Unknown bus_type " << bus_type;
-    return std::nullopt;
-  }
-
-  if (!res) {
-    LOG(ERROR) << "Cannot find " << bus_type << "-specific fields on network \""
-               << dev_path << "\"";
-    return std::nullopt;
-  }
-  PrependToDVKey(&*res, bus_type + "_");
-  res->GetDict().Set("bus_type", bus_type);
-
-  return res;
-}
-
 }  // namespace
 
 namespace internal {
@@ -169,13 +82,10 @@ NetworkFunction::DataType NetworkFunction::EvalImpl() const {
   base::FilePath net_dev_pattern =
       Context::Get()->root_dir().Append("sys/class/net/*");
   for (const auto& net_dev_path : Glob(net_dev_pattern)) {
-    auto node_res = GetNetworkData(net_dev_path);
-    if (!node_res) {
-      continue;
+    auto node_res = GetDeviceBusDataFromSysfsNode(net_dev_path);
+    if (node_res) {
+      results.Append(std::move(*node_res));
     }
-    CHECK(!node_res->GetDict().FindString("path"));
-    node_res->GetDict().Set("path", net_dev_path.value());
-    results.Append(std::move(*node_res));
   }
 
   return results;
--- runtime_probe/function_templates/network_test.cc
+++ runtime_probe/function_templates/network_test.cc
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <map>
 #include <optional>
+#include <set>
 #include <string>
 #include <utility>
 #include <vector>
@@ -12,6 +14,7 @@
 #include <base/strings/stringprintf.h>
 #include <brillo/variant_dictionary.h>
 #include <dbus/shill/dbus-constants.h>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "runtime_probe/function_templates/network.h"
@@ -22,17 +25,9 @@ namespace runtime_probe {
 namespace {
 
 using ::testing::_;
-using ::testing::AnyNumber;
-using ::testing::ByMove;
-using ::testing::DoAll;
+using ::testing::Eq;
+using ::testing::Pointee;
 using ::testing::Return;
-using ::testing::SetArgPointee;
-
-constexpr auto kNetworkDirPath("/sys/class/net");
-
-constexpr auto kBusTypePci("pci");
-constexpr auto kBusTypeSdio("sdio");
-constexpr auto kBusTypeUsb("usb");
 
 class MockNetworkFunction : public NetworkFunction {
   using NetworkFunction::NetworkFunction;
@@ -48,315 +43,73 @@ class MockNetworkFunction : public NetworkFunction {
 
 class NetworkFunctionTest : public BaseFunctionTest {
  protected:
-  // Set up |network_fields| for network device.
-  // For example:
-  //   SetNetworkDevice("pci", "wlan0",
-  //                  {{"device", "0x1111"}, {"vendor", "0x2222"}});
-  // The function will set "0x1111" to file /sys/class/net/wlan0/device
-  // and "0x2222" to file /sys/class/net/wlan0/vendor.
-  //
-  // @param bus_type: Bus type of the network device.
-  // @param interface: Interface name under /sys/class/net/ .
-  // @param network_fields: A vector of <field name, field value> indicates
-  // information to be probed.
-  // @param usb_id: Set only when |bus_type| is "usb". Path name of the
-  // directory under sys/bus/usb/devices/ where |network_fields| is put at.
-  void SetNetworkDevice(
-      const std::string& bus_type,
-      const std::string& interface,
-      const std::vector<std::pair<std::string, std::string>>& network_fields,
-      const std::string& usb_id = "") {
-    std::string field_path;
-    if (bus_type == kBusTypeUsb) {
-      SetDirectory({"sys/bus/usb/devices", usb_id, "0:0"});
-      SetSymbolicLink({"/sys/bus/usb/devices", usb_id, "0:0"},
-                      {kNetworkDirPath, interface, "device"});
-      field_path = base::StringPrintf("sys/bus/usb/devices/%s", usb_id.c_str());
-    } else {
-      field_path = base::StringPrintf("%s/%s/device", kNetworkDirPath,
-                                      interface.c_str());
-    }
+  void SetNetworkDevice(const std::string& dev_name,
+                        const std::string& network_type) {
+    const std::string bus_dev =
+        "/sys/devices/pci0000:00/0000:00:08.1" + dev_name;
+    const std::string bus_dev_relative_to_sys = "../../../";
+    SetSymbolicLink(bus_dev, {"/sys/class/net", dev_name, "device"});
+    // The symbolic link is for getting the bus type.
+    SetSymbolicLink({bus_dev_relative_to_sys, "bus", "pci"},
+                    {bus_dev, "subsystem"});
+    SetFile({bus_dev, "device"}, "0x1111");
+    SetFile({bus_dev, "vendor"}, "0x2222");
 
-    // The symbolic link is for the probe function to get the bus type.
-    SetDirectory({"sys/bus", bus_type});
-    SetSymbolicLink({"/sys/bus", bus_type},
-                    {kNetworkDirPath, interface, "device/subsystem"});
-
-    // Set up network information that is going to be probed.
-    for (const auto& [field, value] : network_fields) {
-      SetFile({field_path, field}, value);
-    }
+    shill_devices_["/dev/" + dev_name] = {{shill::kInterfaceProperty, dev_name},
+                                          {shill::kTypeProperty, network_type}};
+    mock_context()->SetShillProxies(shill_devices_);
   }
+
+  std::map<std::string, brillo::VariantDictionary> shill_devices_;
 };
 
-TEST_F(NetworkFunctionTest, ProbeNetworkPci) {
-  const base::Value probe_statement(base::Value::Type::DICT);
+TEST_F(NetworkFunctionTest, ProbeNetwork) {
+  SetNetworkDevice("wlan0", shill::kTypeWifi);
+  base::Value probe_statement(base::Value::Type::DICT);
   auto probe_function = CreateProbeFunction<NetworkFunction>(probe_statement);
 
-  SetNetworkDevice(kBusTypePci, "wlan0",
-                   {{"device", "0x1111"}, {"vendor", "0x2222"}});
-
-  brillo::VariantDictionary device0_props = {
-      {shill::kInterfaceProperty, std::string("wlan0")},
-      {shill::kTypeProperty, std::string(shill::kTypeWifi)}};
-  mock_context()->SetShillProxies({{"/dev/0", device0_props}});
-
   auto result = probe_function->Eval();
-  auto ans = CreateProbeResultFromJson(base::StringPrintf(
-      R"JSON(
-    [
-      {
-        "bus_type": "pci",
-        "path": "%s",
-        "pci_device_id": "0x1111",
-        "pci_vendor_id": "0x2222",
-        "type": "wifi"
-      }
-    ]
-  )JSON",
-      GetPathUnderRoot({kNetworkDirPath, "wlan0"}).value().c_str()));
-  EXPECT_EQ(result, ans);
-}
-
-TEST_F(NetworkFunctionTest, GetPciRevisionIdFromConfig) {
-  const base::Value probe_statement(base::Value::Type::DICT);
-  auto probe_function = CreateProbeFunction<NetworkFunction>(probe_statement);
-
-  SetNetworkDevice(kBusTypePci, "wlan0",
-                   {{"device", "0x1111"}, {"vendor", "0x2222"}});
-
-  brillo::VariantDictionary device0_props = {
-      {shill::kInterfaceProperty, std::string("wlan0")},
-      {shill::kTypeProperty, std::string(shill::kTypeWifi)}};
-  mock_context()->SetShillProxies({{"/dev/0", device0_props}});
-
-  // The revision is at offset 8 of the binary file.
-  std::vector<uint8_t> config_buffer{0x00, 0x01, 0x02, 0x03, 0x04,
-                                     0x05, 0x06, 0x07, 0x08, 0x09};
-  SetFile({kNetworkDirPath, "wlan0/device/config"},
-          base::span<uint8_t>(config_buffer));
-
-  auto result = probe_function->Eval();
-  auto ans = CreateProbeResultFromJson(base::StringPrintf(
-      R"JSON(
-    [
-      {
-        "bus_type": "pci",
-        "path": "%s",
-        "pci_device_id": "0x1111",
-        "pci_vendor_id": "0x2222",
-        "pci_revision": "0x08",
-        "type": "wifi"
-      }
-    ]
-  )JSON",
-      GetPathUnderRoot({kNetworkDirPath, "wlan0"}).value().c_str()));
-  EXPECT_EQ(result, ans);
-}
-
-TEST_F(NetworkFunctionTest, GetPciRevisionIdFromConfigFailed) {
-  const base::Value probe_statement(base::Value::Type::DICT);
-  auto probe_function = CreateProbeFunction<NetworkFunction>(probe_statement);
-
-  SetNetworkDevice(kBusTypePci, "wlan0",
-                   {{"device", "0x1111"}, {"vendor", "0x2222"}});
-
-  brillo::VariantDictionary device0_props = {
-      {shill::kInterfaceProperty, std::string("wlan0")},
-      {shill::kTypeProperty, std::string(shill::kTypeWifi)}};
-  mock_context()->SetShillProxies({{"/dev/0", device0_props}});
-
-  // Fail to read the binary file at offset 8.
-  std::vector<uint8_t> config_buffer{0x00, 0x01, 0x02, 0x03, 0x04};
-  SetFile({kNetworkDirPath, "wlan0/device/config"},
-          base::span<uint8_t>(config_buffer));
-
-  auto result = probe_function->Eval();
-  auto ans = CreateProbeResultFromJson(base::StringPrintf(
-      R"JSON(
-    [
-      {
-        "bus_type": "pci",
-        "path": "%s",
-        "pci_device_id": "0x1111",
-        "pci_vendor_id": "0x2222",
-        "type": "wifi"
-      }
-    ]
-  )JSON",
-      GetPathUnderRoot({kNetworkDirPath, "wlan0"}).value().c_str()));
-  EXPECT_EQ(result, ans);
-}
-
-TEST_F(NetworkFunctionTest, ProbeNetworkSdio) {
-  const base::Value probe_statement(base::Value::Type::DICT);
-  auto probe_function = CreateProbeFunction<NetworkFunction>(probe_statement);
-
-  SetNetworkDevice(kBusTypeSdio, "wlan0",
-                   {{"device", "0x1111"}, {"vendor", "0x2222"}});
-
-  brillo::VariantDictionary device0_props = {
-      {shill::kInterfaceProperty, std::string("wlan0")},
-      {shill::kTypeProperty, std::string(shill::kTypeWifi)}};
-  mock_context()->SetShillProxies({{"/dev/0", device0_props}});
-
-  auto result = probe_function->Eval();
-  auto ans = CreateProbeResultFromJson(base::StringPrintf(
-      R"JSON(
-    [
-      {
-        "bus_type": "sdio",
-        "path": "%s",
-        "sdio_device_id": "0x1111",
-        "sdio_vendor_id": "0x2222",
-        "type": "wifi"
-      }
-    ]
-  )JSON",
-      GetPathUnderRoot({kNetworkDirPath, "wlan0"}).value().c_str()));
-  EXPECT_EQ(result, ans);
-}
-
-TEST_F(NetworkFunctionTest, ProbeNetworkUsb) {
-  const base::Value probe_statement(base::Value::Type::DICT);
-  auto probe_function = CreateProbeFunction<NetworkFunction>(probe_statement);
-
-  SetNetworkDevice(kBusTypeUsb, "wlan0",
-                   {{"idProduct", "0x1111"}, {"idVendor", "0x2222"}}, "0");
-
-  brillo::VariantDictionary device0_props = {
-      {shill::kInterfaceProperty, std::string("wlan0")},
-      {shill::kTypeProperty, std::string(shill::kTypeWifi)}};
-  mock_context()->SetShillProxies({{"/dev/0", device0_props}});
-
-  auto result = probe_function->Eval();
-  auto ans = CreateProbeResultFromJson(base::StringPrintf(
-      R"JSON(
-    [
-      {
-        "bus_type": "usb",
-        "path": "%s",
-        "usb_product_id": "0x1111",
-        "usb_vendor_id": "0x2222",
-        "type": "wifi"
-      }
-    ]
-  )JSON",
-      GetPathUnderRoot({kNetworkDirPath, "wlan0"}).value().c_str()));
-  EXPECT_EQ(result, ans);
-}
-
-TEST_F(NetworkFunctionTest, UnknownBusType) {
-  const base::Value probe_statement(base::Value::Type::DICT);
-  auto probe_function = CreateProbeFunction<NetworkFunction>(probe_statement);
-
-  // The bus type is "unknown".
-  SetNetworkDevice("unknown", "wlan0",
-                   {{"device", "0x1111"}, {"vendor", "0x2222"}});
-
-  brillo::VariantDictionary device0_props = {
-      {shill::kInterfaceProperty, std::string("wlan0")},
-      {shill::kTypeProperty, std::string(shill::kTypeWifi)}};
-  mock_context()->SetShillProxies({{"/dev/0", device0_props}});
-
-  auto result = probe_function->Eval();
-  auto ans = CreateProbeResultFromJson(R"JSON(
-    []
-  )JSON");
-  EXPECT_EQ(result, ans);
-}
-
-TEST_F(NetworkFunctionTest, NoRequiredFields) {
-  const base::Value probe_statement(base::Value::Type::DICT);
-  auto probe_function = CreateProbeFunction<NetworkFunction>(probe_statement);
-
-  // No required field "vendor".
-  SetNetworkDevice(kBusTypePci, "wlan0", {{"device", "0x1111"}});
-
-  brillo::VariantDictionary device0_props = {
-      {shill::kInterfaceProperty, std::string("wlan0")},
-      {shill::kTypeProperty, std::string(shill::kTypeWifi)}};
-  mock_context()->SetShillProxies({{"/dev/0", device0_props}});
-
-  auto result = probe_function->Eval();
-  auto ans = CreateProbeResultFromJson(R"JSON(
-    []
-  )JSON");
-  EXPECT_EQ(result, ans);
+  EXPECT_EQ(result.size(), 1);
+  EXPECT_TRUE(result[0].GetDict().FindString("path"));
+  EXPECT_TRUE(result[0].GetDict().FindString("bus_type"));
 }
 
 TEST_F(NetworkFunctionTest, ProbeNetworkByType) {
-  SetNetworkDevice(kBusTypePci, "wlan0",
-                   {{"device", "0x1111"}, {"vendor", "0x2222"}});
-  SetNetworkDevice(kBusTypePci, "eth0",
-                   {{"device", "0x3333"}, {"vendor", "0x4444"}});
-  SetNetworkDevice(kBusTypePci, "wwan0",
-                   {{"device", "0x5555"}, {"vendor", "0x6666"}});
-
-  brillo::VariantDictionary device0_props = {
-      {shill::kInterfaceProperty, std::string("wlan0")},
-      {shill::kTypeProperty, std::string(shill::kTypeWifi)}};
-  brillo::VariantDictionary device1_props = {
-      {shill::kInterfaceProperty, std::string("eth0")},
-      {shill::kTypeProperty, std::string(shill::kTypeEthernet)}};
-  brillo::VariantDictionary device2_props = {
-      {shill::kInterfaceProperty, std::string("wwan0")},
-      {shill::kTypeProperty, std::string(shill::kTypeCellular)}};
-  mock_context()->SetShillProxies({{"/dev/0", device0_props},
-                                   {"/dev/1", device1_props},
-                                   {"/dev/2", device2_props}});
-
-  auto ans = CreateProbeResultFromJson(base::StringPrintf(
-      R"JSON(
-    [
-      {
-        "bus_type": "pci",
-        "path": "%s",
-        "pci_device_id": "0x1111",
-        "pci_vendor_id": "0x2222",
-        "type": "wifi"
-      },
-      {
-        "bus_type": "pci",
-        "path": "%s",
-        "pci_device_id": "0x3333",
-        "pci_vendor_id": "0x4444",
-        "type": "ethernet"
-      },
-      {
-        "bus_type": "pci",
-        "path": "%s",
-        "pci_device_id": "0x5555",
-        "pci_vendor_id": "0x6666",
-        "type": "cellular"
-      }
-    ]
-  )JSON",
-      GetPathUnderRoot({kNetworkDirPath, "wlan0"}).value().c_str(),
-      GetPathUnderRoot({kNetworkDirPath, "eth0"}).value().c_str(),
-      GetPathUnderRoot({kNetworkDirPath, "wwan0"}).value().c_str()));
+  SetNetworkDevice("wlan0", shill::kTypeWifi);
+  SetNetworkDevice("eth0", shill::kTypeEthernet);
+  SetNetworkDevice("wwan0", shill::kTypeCellular);
+  const std::set<std::string> expected_types{
+      shill::kTypeWifi, shill::kTypeEthernet, shill::kTypeCellular};
 
   // Probe all.
   {
     const base::Value probe_statement(base::Value::Type::DICT);
     auto probe_function = CreateProbeFunction<NetworkFunction>(probe_statement);
     auto result = probe_function->Eval();
-    ExpectUnorderedListEqual(result, ans);
+    std::set<std::string> result_types;
+    for (const auto& each_result : result) {
+      auto* type = each_result.GetDict().FindString("type");
+      if (type) {
+        result_types.insert(*type);
+      }
+    }
+    EXPECT_EQ(result_types, expected_types);
   }
   // Filter by each type.
-  for (const auto& each_ans : ans) {
-    const std::string expected_type = *each_ans.GetDict().FindString("type");
+  for (const auto& expected_type : expected_types) {
     base::Value probe_statement(base::Value::Type::DICT);
     probe_statement.GetDict().Set("device_type", expected_type);
     auto probe_function = CreateProbeFunction<NetworkFunction>(probe_statement);
     auto result = probe_function->Eval();
     EXPECT_EQ(result.size(), 1);
-    EXPECT_EQ(result[0], each_ans);
+    EXPECT_THAT(result[0].GetDict().FindString("type"),
+                Pointee(Eq(expected_type)));
   }
 
   // TODO(b/269822306): The below two do the same thing as the above, but use
   // the old interface to pass the type. Remove this after we done the
   // migration.
+  // Probe all.
   {
     const base::Value probe_statement(base::Value::Type::DICT);
     auto probe_function =
@@ -364,10 +117,17 @@ TEST_F(NetworkFunctionTest, ProbeNetworkByType) {
     EXPECT_CALL(*probe_function, GetNetworkType())
         .WillOnce(Return(std::nullopt));
     auto result = probe_function->Eval();
-    ExpectUnorderedListEqual(result, ans);
+    std::set<std::string> result_types;
+    for (const auto& each_result : result) {
+      auto* type = each_result.GetDict().FindString("type");
+      if (type) {
+        result_types.insert(*type);
+      }
+    }
+    EXPECT_EQ(result_types, expected_types);
   }
-  for (const auto& each_ans : ans) {
-    const std::string expected_type = *each_ans.GetDict().FindString("type");
+  // Filter by each type.
+  for (const auto& expected_type : expected_types) {
     base::Value probe_statement(base::Value::Type::DICT);
     auto probe_function =
         CreateProbeFunction<MockNetworkFunction>(probe_statement);
@@ -375,7 +135,8 @@ TEST_F(NetworkFunctionTest, ProbeNetworkByType) {
         .WillOnce(Return(expected_type));
     auto result = probe_function->Eval();
     EXPECT_EQ(result.size(), 1);
-    EXPECT_EQ(result[0], each_ans);
+    EXPECT_THAT(result[0].GetDict().FindString("type"),
+                Pointee(Eq(expected_type)));
   }
 }
 
