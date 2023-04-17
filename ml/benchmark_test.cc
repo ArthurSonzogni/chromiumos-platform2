04a32bb7679c756da6530cd54f661cf7f231c58b
Yi Chou <yich@google.com>
2023-04-17 14:52:42 +0800

    ml: Fix the percentile percentile comparing in unittest
    The percentile may be "equal" to the other percentile.
    
    BUG=b:278490569
    TEST=FEATURES=test emerge-brya ml
    
    Change-Id: Ia5e355476b1aca342feefb686e3368215613305c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4432372
    Tested-by: Yi Chou <yich@google.com>
    Auto-Submit: Yi Chou <yich@google.com>
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Commit-Queue: Yi Chou <yich@google.com>

--- ml/benchmark_test.cc
+++ ml/benchmark_test.cc
@@ -152,8 +152,8 @@ TEST_F(MlBenchmarkTest, TfliteModelMatchedValueTest) {
 
   auto latencies = results.percentile_latencies_in_us();
   EXPECT_EQ(latencies.size(), 4);
-  EXPECT_TRUE(0 < latencies[50] && latencies[50] < latencies[90] &&
-              latencies[90] < latencies[95] && latencies[95] < latencies[99]);
+  EXPECT_TRUE(0 <= latencies[50] && latencies[50] <= latencies[90] &&
+              latencies[90] <= latencies[95] && latencies[95] <= latencies[99]);
 
   auto& metrics = results.metrics();
   EXPECT_EQ(metrics[0].name(), "average_error");
@@ -168,14 +168,14 @@ TEST_F(MlBenchmarkTest, TfliteModelMatchedValueTest) {
   EXPECT_EQ(metrics[1].direction(),
             chrome::ml_benchmark::Metric::SMALLER_IS_BETTER);
   EXPECT_EQ(metrics[1].cardinality(), chrome::ml_benchmark::Metric::SINGLE);
-  EXPECT_GT(metrics[1].values()[0], 0.0f);
+  EXPECT_GE(metrics[1].values()[0], 0.0f);
 
   EXPECT_EQ(metrics[2].name(), "90th_perc_cpu_time");
-  EXPECT_GT(metrics[2].values()[0], metrics[1].values()[0]);
+  EXPECT_GE(metrics[2].values()[0], metrics[1].values()[0]);
   EXPECT_EQ(metrics[3].name(), "95th_perc_cpu_time");
-  EXPECT_GT(metrics[3].values()[0], metrics[2].values()[0]);
+  EXPECT_GE(metrics[3].values()[0], metrics[2].values()[0]);
   EXPECT_EQ(metrics[4].name(), "99th_perc_cpu_time");
-  EXPECT_GT(metrics[4].values()[0], metrics[3].values()[0]);
+  EXPECT_GE(metrics[4].values()[0], metrics[3].values()[0]);
 }
 
 TEST_F(MlBenchmarkTest, TfliteModelUnmatchedValueTest) {
