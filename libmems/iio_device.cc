ArthurSonzogni/chromiumos-platform2

commit 7fcf97a500144fd2ff441733bd4204ae4379a8d0

libmems: Add IioDevice::GetLocation
To support the label attribute that contains the location, adds
IioDevice::GetLocation to check both device attributes in order to
determine the location.

BUG=b:202781843
TEST=unit tests

Change-Id: I6ae9a0b1f8162d2a6a404ae02d67231d2e2fb6c1
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3711420
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Cheng-Hao Yang <chenghaoyang@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>

libmems/iio_device.cc
@@ -39,8 +39,21 @@ std::optional<base::FilePath> IioDevice::GetAbsoluteSysPath() const {
   return std::nullopt;
 }
 
+std::optional<std::string> IioDevice::GetLocation() const {
+  auto label = ReadStringAttribute(kLabelAttr);
+  if (label.has_value()) {
+    if (label->find("-base") != std::string::npos)
+      return "base";
+
+    if (label->find("-display") != std::string::npos)
+      return "lid";
+  }
+
+  return ReadStringAttribute(kLocationAttr);
+}
+
 bool IioDevice::IsSingleSensor() const {
-  return ReadStringAttribute("location").has_value();
+  return ReadStringAttribute(kLocationAttr).has_value();
 }
 
 // static

libmems/iio_device_test.cc
@@ -151,6 +151,43 @@ INSTANTIATE_TEST_SUITE_P(
         std::make_tuple("0.0 1.0 100.0 ", true, 1.0, 100.0),
         std::make_tuple("0.0 2.0 a b c 100.0 ", true, 2.0, 100.0)));
 
+class IioDeviceTestOnLocationWithParam
+    : public ::testing::TestWithParam<std::tuple<std::optional<std::string>,
+                                                 std::optional<std::string>,
+                                                 std::optional<std::string>>> {
+ protected:
+  void SetUp() override {
+    device_ = std::make_unique<libmems::fakes::FakeIioDevice>(
+        nullptr, kFakeDeviceName, kFakeDeviceId);
+
+    if (std::get<0>(GetParam()).has_value()) {
+      EXPECT_TRUE(device_->WriteStringAttribute(
+          kLabelAttr, std::get<0>(GetParam()).value()));
+    }
+    if (std::get<1>(GetParam()).has_value()) {
+      EXPECT_TRUE(device_->WriteStringAttribute(
+          kLocationAttr, std::get<1>(GetParam()).value()));
+    }
+  }
+
+  std::unique_ptr<libmems::fakes::FakeIioDevice> device_;
+};
+
+TEST_P(IioDeviceTestOnLocationWithParam, GetLocation) {
+  EXPECT_EQ(device_->GetLocation(), std::get<2>(GetParam()));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    IioDeviceTestOnLocationWithParamRun,
+    IioDeviceTestOnLocationWithParam,
+    ::testing::Values(std::make_tuple(std::nullopt, std::nullopt, std::nullopt),
+                      std::make_tuple(std::nullopt, "base", "base"),
+                      std::make_tuple(std::nullopt, "lid", "lid"),
+                      std::make_tuple(std::nullopt, "camera", "camera"),
+                      std::make_tuple("accel-base", std::nullopt, "base"),
+                      std::make_tuple("accel-display", "base", "lid"),
+                      std::make_tuple("accel-camera", "lid", "lid")));
+
 }  // namespace
 
 }  // namespace libmems

libmems/common_types.h
@@ -34,6 +34,8 @@ constexpr char kFrequencyAttr[] = "frequency";
 constexpr char kSamplingFrequencyAttr[] = "sampling_frequency";
 constexpr char kHWFifoTimeoutAttr[] = "buffer/hwfifo_timeout";
 constexpr char kSamplingFrequencyAvailable[] = "sampling_frequency_available";
+constexpr char kLabelAttr[] = "label";
+constexpr char kLocationAttr[] = "location";
 
 constexpr double kFrequencyEpsilon = 0.001;  // Hz
 

libmems/iio_device.h
@@ -55,6 +55,8 @@ class LIBMEMS_EXPORT IioDevice {
 
   std::optional<base::FilePath> GetAbsoluteSysPath() const;
 
+  std::optional<std::string> GetLocation() const;
+
   // Reads the |name| attribute of this device and returns the value
   // as a string. It will return std::nullopt if the attribute cannot
   // be read.

commit a5f4e8db20698d96720098e9818ea17f05fb9417

libmems: Add camera location
To be compatible with all possible locations, support "accel-camera"
label, return "camera" like |location| attribute would have done.

Fixes: 7fcf97a50014 ("libmems: Add IioDevice::GetLocation")

BUG=b:202781843
TEST=unit tests

Change-Id: I0d88bb090729947e34635f03aa7e7d9f3ad21cdf
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4174465
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Cheng-Hao Yang <chenghaoyang@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>

libmems/iio_device.cc
@@ -47,6 +47,9 @@ std::optional<std::string> IioDevice::GetLocation() const {
 
     if (label->find("-display") != std::string::npos)
       return "lid";
+
+    if (label->find("-camera") != std::string::npos)
+      return "camera";
   }
 
   return ReadStringAttribute(kLocationAttr);

libmems/iio_device_test.cc
@@ -186,7 +186,7 @@ INSTANTIATE_TEST_SUITE_P(
                       std::make_tuple(std::nullopt, "camera", "camera"),
                       std::make_tuple("accel-base", std::nullopt, "base"),
                       std::make_tuple("accel-display", "base", "lid"),
-                      std::make_tuple("accel-camera", "lid", "lid")));
+                      std::make_tuple("accel-camera", "lid", "camera")));
 
 }  // namespace
 