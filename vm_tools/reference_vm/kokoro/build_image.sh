dee9e05c6c2e052b333006f4dfbb443b5fda1d14
James Ye <jamesye@google.com>
2023-05-05 11:13:12 +1000

    vm_tools: refvm: Add year and month to output directory
    Use a custom shorter output path instead of the default.
    See cl/529571866 for the other half of this change.
    
    BUG=b:279686117
    TEST=kokoro
    
    Change-Id: I347be14357391296848164e90efb758888d4fcb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4505715
    Reviewed-by: David Munro <davidmunro@google.com>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Commit-Queue: James Ye <jamesye@google.com>

--- vm_tools/reference_vm/kokoro/build_image.sh
+++ vm_tools/reference_vm/kokoro/build_image.sh
@@ -10,13 +10,16 @@ main() {
 
   # shellcheck disable=SC2154
   local src_root="${KOKORO_ARTIFACTS_DIR}/git/platform2/vm_tools/reference_vm"
-  local result_dir="${src_root}/out"
 
+  timestamp=$(date +%s)
+  prefix="$(date -d @"${timestamp}" --utc +%Y-%m)"
+  suffix="$(date -d @"${timestamp}" --utc +%Y%m%d_%H%M%S)"
+
+  local result_dir="${src_root}/out/${prefix}"
   mkdir -p "${result_dir}"
   cd "${result_dir}"
 
-  timestamp="$(date --utc +%Y%m%d_%H%M%S)"
-  image_path="refvm-${timestamp}.img"
+  image_path="refvm-${suffix}.img"
   sudo "${src_root}/build.py" \
     --debian-release=bookworm \
     -o "${image_path}"
