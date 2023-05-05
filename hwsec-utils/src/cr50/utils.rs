9b738e8a82ac8c7f784afbd366715935932e9f6e
Sam Chen <chensa@google.com>
2023-05-05 12:26:36 +0800

    hwsec-utils: add utilities for parsing gsctool outputs
    Added helper functions for retrieving value from gsctool output, since
    it will be frequently used in cr50_verify_ro.
    
    BUG=b:279698573
    TEST=hwsec-utils unit tests
    
    Change-Id: I3401ee4e117abcb4bc07b895c857f7990cbd5077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4505195
    Commit-Queue: Sam Chen <chensa@google.com>
    Tested-by: Sam Chen <chensa@google.com>
    Reviewed-by: Howard Yang <hcyang@google.com>

--- hwsec-utils/src/cr50/utils.rs
+++ hwsec-utils/src/cr50/utils.rs
@@ -15,6 +15,51 @@ use crate::output::HwsecOutput;
 use crate::tpm2::BoardID;
 use crate::tpm2::FactoryConfig;
 
+/// Convert string version representation <epoch>.<major>.<minor> into Version struct.
+pub fn parse_version(version_string: &str) -> Option<Version> {
+    let mut parts = version_string.split('.');
+    if parts.clone().count() != 3 {
+        return None;
+    }
+    let epoch = parts.next().unwrap().parse::<u8>().ok()?;
+    let major = parts.next().unwrap().parse::<u8>().ok()?;
+    let minor = parts.next().unwrap().parse::<u8>().ok()?;
+
+    Some(Version {
+        epoch,
+        major,
+        minor,
+    })
+}
+
+/// 'gsctool -M [options]' output has format
+///
+/// ...
+/// <INDEX1>=<VALUE1>\n
+/// <INDEX2>=<VALUE2>\n
+/// ...
+///
+/// This function finds the line that starts with the given index, and outputs its value.
+pub fn get_value_from_gsctool_output<'a>(
+    gsctool_output: &'a str,
+    index: &'a str,
+) -> Result<&'a str, HwsecError> {
+    let prefix = index.to_owned() + "=";
+    let Some(line) = gsctool_output
+    .lines()
+    .find(|line| line.starts_with(&prefix)) else {
+        eprintln!("Cannot find a line starts with {}", index);
+        return Err(HwsecError::InternalError);
+    };
+    match line.split('=').nth(1) {
+        Some(value) => Ok(value),
+        None => {
+            eprintln!("Failed when retrieving value after '='");
+            Err(HwsecError::InternalError)
+        }
+    }
+}
+
 pub fn run_gsctool_cmd(
     ctx: &mut impl Context,
     mut options: Vec<&str>,
@@ -217,6 +262,8 @@ pub fn get_factory_config_with_gsctool(
 #[cfg(test)]
 mod tests {
     use super::extract_rw_fw_version_from_gsctool_response;
+    use super::get_value_from_gsctool_output;
+    use super::parse_version;
     use crate::cr50::Version;
     use crate::error::HwsecError;
 
@@ -249,5 +296,54 @@ mod tests {
     fn test_extract_rw_fw_version_from_gsctool_response_not_valid_version() {
         let result = extract_rw_fw_version_from_gsctool_response("RW_FW_VER=123");
         assert_eq!(result, Err(HwsecError::GsctoolResponseBadFormatError));
+    }
+
+    #[test]
+    fn test_parse_version_ok() {
+        let result = parse_version("1.2.3");
+        assert_eq!(
+            result,
+            Some(Version {
+                epoch: 1,
+                major: 2,
+                minor: 3,
+            })
+        );
+    }
+
+    #[test]
+    fn test_parse_version_fail_1() {
+        let result = parse_version("a.b.c");
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_parse_version_fail_2() {
+        let result = parse_version("1.23");
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_get_value_from_gsctool_output_ok() {
+        let result = get_value_from_gsctool_output("INDEX=VALUE", "INDEX");
+        assert_eq!(result, Ok("VALUE"));
+    }
+
+    #[test]
+    fn test_get_value_from_gsctool_output_ok_multiple_lines() {
+        let result = get_value_from_gsctool_output("INDEX1=VALUE1\nINDEX2=VALUE2", "INDEX2");
+        assert_eq!(result, Ok("VALUE2"));
+    }
+
+    #[test]
+    fn test_get_value_from_gsctool_output_empty_value() {
+        let result = get_value_from_gsctool_output("INDEX=", "INDEX");
+        assert_eq!(result, Ok(""));
+    }
+
+    #[test]
+    fn test_get_value_from_gsctool_output_fail_index_not_found() {
+        let result = get_value_from_gsctool_output("ABC=", "INDEX");
+        assert_eq!(result, Err(HwsecError::InternalError));
     }
 }
