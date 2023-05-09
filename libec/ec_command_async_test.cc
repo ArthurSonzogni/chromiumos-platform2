f49536986bfe4cba9168d21377f7a994f4d09c6b
Firas Sammoura <fsammoura@google.com>
2023-05-09 16:10:34 +0000

    libec: Create Parametrized tests for ec_command_async_test
    Add MockFlashProtectCommand async command with non-empty response.
    Create type-parametric test to repeat all unit tests with the two async
    command types: MockAddEntropyCommand and MockFlashProtectCommand.
    
    BUG=b:116396469
    TEST=FEATURES="test" emerge-hatch libec
    
    Change-Id: Ic6c9065899776cb99cb131f0990c264a85e59757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4518022
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>

--- libec/ec_command_async_test.cc
+++ libec/ec_command_async_test.cc
@@ -36,27 +36,44 @@ class MockAddEntropyCommand
             EC_CMD_ADD_ENTROPY, ADD_ENTROPY_GET_RESULT, options) {}
 };
 
+class MockFlashProtectCommand
+    : public MockEcCommandAsync<struct ec_params_flash_protect_v2,
+                                struct ec_response_flash_protect> {
+ public:
+  explicit MockFlashProtectCommand(const Options& options)
+      : MockEcCommandAsync(
+            EC_CMD_FLASH_PROTECT, FLASH_PROTECT_GET_RESULT, options) {}
+};
+
+template <typename T>
+class EcCommandAsyncTest : public testing::Test {};
+
+using EcCommandAsyncTestTypes =
+    ::testing::Types<MockAddEntropyCommand, MockFlashProtectCommand>;
+
+TYPED_TEST_SUITE(EcCommandAsyncTest, EcCommandAsyncTestTypes);
+
 // ioctl behavior for EC commands:
 //   returns sizeof(EC response) (>=0) on success, -1 on failure
 //   cmd.result is error code from EC (EC_RES_SUCCESS, etc)
 
-TEST(EcCommandAsync, Run_Success) {
-  MockAddEntropyCommand mock_cmd({.poll_for_result_num_attempts = 2,
-                                  .poll_interval = base::Milliseconds(1)});
+TYPED_TEST(EcCommandAsyncTest, Run_Success) {
+  TypeParam mock_cmd({.poll_for_result_num_attempts = 2,
+                      .poll_interval = base::Milliseconds(1)});
   EXPECT_CALL(mock_cmd, ioctl)
       .Times(3)
       // First call to ioctl() to start the command; EC returns success.
-      .WillOnce([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillOnce([](int, uint32_t, typename TypeParam::Data* data) {
         data->cmd.result = EC_RES_SUCCESS;
         return data->cmd.insize;
       })
       // Second call to ioctl() to get the result; EC returns busy.
-      .WillOnce([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillOnce([](int, uint32_t, typename TypeParam::Data* data) {
         data->cmd.result = EC_RES_BUSY;
         return data->cmd.insize;
       })
       // Third call to ioctl() to get the result; EC returns success.
-      .WillOnce([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillOnce([](int, uint32_t, typename TypeParam::Data* data) {
         data->cmd.result = EC_RES_SUCCESS;
         return data->cmd.insize;
       });
@@ -65,19 +82,19 @@ TEST(EcCommandAsync, Run_Success) {
   EXPECT_EQ(mock_cmd.Result(), EC_RES_SUCCESS);
 }
 
-TEST(EcCommandAsync, Run_TimeoutFailure) {
-  MockAddEntropyCommand mock_cmd({.poll_for_result_num_attempts = 2,
-                                  .poll_interval = base::Milliseconds(1)});
+TYPED_TEST(EcCommandAsyncTest, Run_TimeoutFailure) {
+  TypeParam mock_cmd({.poll_for_result_num_attempts = 2,
+                      .poll_interval = base::Milliseconds(1)});
 
   EXPECT_CALL(mock_cmd, ioctl)
       .Times(3)
       // First call to ioctl() to start the command; EC returns success.
-      .WillOnce([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillOnce([](int, uint32_t, typename TypeParam::Data* data) {
         data->cmd.result = EC_RES_SUCCESS;
         return data->cmd.insize;
       })
       // All remaining ioctl() calls; EC returns busy.
-      .WillRepeatedly([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillRepeatedly([](int, uint32_t, typename TypeParam::Data* data) {
         data->cmd.result = EC_RES_BUSY;
         return data->cmd.insize;
       });
@@ -86,24 +103,23 @@ TEST(EcCommandAsync, Run_TimeoutFailure) {
   EXPECT_EQ(mock_cmd.Result(), EC_RES_BUSY);
 }
 
-TEST(EcCommandAsync, Run_Failure) {
-  MockAddEntropyCommand mock_cmd(
-      {// With the number of attempts set to 2, there will be at most
-       // 3 ioctl calls (the extra one starts the command). In this
-       // test case, we're validating that the last ioctl() call will
-       // not be performed because we got an error on the second
-       // ioctl() call.
-       .poll_for_result_num_attempts = 2,
-       .poll_interval = base::Milliseconds(1)});
+TYPED_TEST(EcCommandAsyncTest, Run_Failure) {
+  TypeParam mock_cmd({// With the number of attempts set to 2, there will be at
+                      // most 3 ioctl calls (the extra one starts the command).
+                      // In this test case, we're validating that the last
+                      // ioctl() call will not be performed because we got an
+                      // error on the second ioctl() call.
+                      .poll_for_result_num_attempts = 2,
+                      .poll_interval = base::Milliseconds(1)});
   EXPECT_CALL(mock_cmd, ioctl)
       .Times(2)
       // First call to ioctl() to start the command; EC returns success.
-      .WillOnce([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillOnce([](int, uint32_t, typename TypeParam::Data* data) {
         data->cmd.result = EC_RES_SUCCESS;
         return data->cmd.insize;
       })
       // Second call to ioctl() to get the result; EC returns error.
-      .WillOnce([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillOnce([](int, uint32_t, typename TypeParam::Data* data) {
         data->cmd.result = EC_RES_ERROR;
         return data->cmd.insize;
       });
@@ -112,8 +128,8 @@ TEST(EcCommandAsync, Run_Failure) {
   EXPECT_EQ(mock_cmd.Result(), EC_RES_ERROR);
 }
 
-TEST(EcCommandAsync, Run_IoctlTimesOut) {
-  MockAddEntropyCommand mock({
+TYPED_TEST(EcCommandAsyncTest, Run_IoctlTimesOut) {
+  TypeParam mock({
       // With the number of attempts set to 2, there will be at
       // most 3 ioctl calls (the extra one starts the command). In
       // this test case, we're validating that the last ioctl()
@@ -125,13 +141,13 @@ TEST(EcCommandAsync, Run_IoctlTimesOut) {
   EXPECT_CALL(mock, ioctl)
       .Times(2)
       // First call to ioctl() to start the command; EC returns success.
-      .WillOnce([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillOnce([](int, uint32_t, typename TypeParam::Data* data) {
         data->cmd.result = EC_RES_SUCCESS;
         return data->cmd.insize;
       })
       // Second call to ioctl() to get the result returns error (EC not
       // responding).
-      .WillOnce([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillOnce([](int, uint32_t, typename TypeParam::Data* data) {
         errno = ETIMEDOUT;
         return kIoctlFailureRetVal;
       });
@@ -140,25 +156,25 @@ TEST(EcCommandAsync, Run_IoctlTimesOut) {
   EXPECT_EQ(mock.Result(), kEcCommandUninitializedResult);
 }
 
-TEST(EcCommandAsync, Run_IoctlTimesOut_IgnoreFailure) {
-  MockAddEntropyCommand mock({.poll_for_result_num_attempts = 2,
-                              .poll_interval = base::Milliseconds(1),
-                              .validate_poll_result = false});
+TYPED_TEST(EcCommandAsyncTest, Run_IoctlTimesOut_IgnoreFailure) {
+  TypeParam mock({.poll_for_result_num_attempts = 2,
+                  .poll_interval = base::Milliseconds(1),
+                  .validate_poll_result = false});
   EXPECT_CALL(mock, ioctl)
       .Times(3)
       // First call to ioctl() to start the command; EC returns success.
-      .WillOnce([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillOnce([](int, uint32_t, typename TypeParam::Data* data) {
         data->cmd.result = EC_RES_SUCCESS;
         return data->cmd.insize;
       })
       // Second call to ioctl() to get the result returns error; EC not
       // responding.
-      .WillOnce([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillOnce([](int, uint32_t, typename TypeParam::Data* data) {
         errno = ETIMEDOUT;
         return kIoctlFailureRetVal;
       })
       // Third call to ioctl() to get the result; EC returns success.
-      .WillOnce([](int, uint32_t, MockAddEntropyCommand::Data* data) {
+      .WillOnce([](int, uint32_t, typename TypeParam::Data* data) {
         data->cmd.result = EC_RES_SUCCESS;
         return data->cmd.insize;
       });
@@ -167,18 +183,18 @@ TEST(EcCommandAsync, Run_IoctlTimesOut_IgnoreFailure) {
   EXPECT_EQ(mock.Result(), EC_RES_SUCCESS);
 }
 
-TEST(EcCommandAsync, Run_InvalidOptions_ZeroPollAttempts) {
-  MockAddEntropyCommand mock({.poll_for_result_num_attempts = 0});
+TYPED_TEST(EcCommandAsyncTest, Run_InvalidOptions_ZeroPollAttempts) {
+  TypeParam mock({.poll_for_result_num_attempts = 0});
   EXPECT_DEATH(mock.Run(kDummyFd), "poll_for_result_num_attempts > 0");
 }
 
-TEST(EcCommandAsync, Run_InvalidOptions_NegativePollAttempts) {
-  MockAddEntropyCommand mock({.poll_for_result_num_attempts = -1});
+TYPED_TEST(EcCommandAsyncTest, Run_InvalidOptions_NegativePollAttempts) {
+  TypeParam mock({.poll_for_result_num_attempts = -1});
   EXPECT_DEATH(mock.Run(kDummyFd), "poll_for_result_num_attempts > 0");
 }
 
-TEST(EcCommandAsync, DefaultOptions) {
-  MockAddEntropyCommand::Options options;
+TYPED_TEST(EcCommandAsyncTest, DefaultOptions) {
+  typename TypeParam::Options options;
   EXPECT_EQ(options.validate_poll_result, true);
   EXPECT_EQ(options.poll_for_result_num_attempts, 20);
   EXPECT_EQ(options.poll_interval, base::Milliseconds(100));
