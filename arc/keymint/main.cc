ce9eb8b35b573f4503acbad50d5a26cd365159d2
Yao Li <yaohuali@google.com>
2023-05-11 11:10:34 -0700

    arc: keymint: Add KeyMintLogger
    Add KeyMintLogger, such that the logs can be forwarded to CrOS' logging
    system.
    
    keymint_logger.(cc|h) is derived from
    arc/keymaster/keymaster_logger.(cc|h).
    
    Currently arc-keymintd likely can't run yet, due to lack of
    configuration info.
    
    BUG=b:247941366
    TEST=emerge-hatch-arc-t arc-keymint
    
    Change-Id: If2b999678575ee7f3255a1ca7d0fc10f80c29176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4522484
    Tested-by: Yao Li <yaohuali@google.com>
    Reviewed-by: Vaibhav Raheja <vraheja@chromium.org>
    Commit-Queue: Yao Li <yaohuali@google.com>

--- arc/keymint/main.cc
+++ arc/keymint/main.cc
@@ -3,14 +3,18 @@
 // found in the LICENSE file.
 
 #include <base/command_line.h>
+#include <brillo/syslog_logging.h>
 
 #include "arc/keymint/daemon.h"
+#include "arc/keymint/keymint_logger.h"
 
 int main(int argc, char** argv) {
   // arc-keymintd takes no command line arguments.
   base::CommandLine::Init(argc, argv);
-
-  // TODO(b/247941366): Add logging support.
+  // Logging to system logs in /var/log/arc.log.
+  brillo::InitLog(brillo::kLogToSyslog | brillo::kLogToStderr);
+  // Setup keymint logger.
+  arc::keymint::KeyMintLogger();
 
   LOG(INFO) << "Running Daemon";
   arc::keymint::Daemon daemon;
