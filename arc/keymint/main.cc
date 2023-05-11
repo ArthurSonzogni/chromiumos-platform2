5d9ee223dd9fa72e972f327fb211f006ace94789
Yao Li <yaohuali@google.com>
2023-05-10 18:17:04 -0700

    arc: keymint: Add Daemon class & run it from main function
    Adds Daemon class. Create and run it in main function.
    
    The daemon.(cc|h) code is derived from arc/keymaster.
    
    It likely won't be able to run and communicate to ARC side as of now, as
    certain things are still missing (dbus_permission, seccomp, etc.)
    
    BUG=b:247941366
    TEST=emerge-hatch-arc-t arc-keymint
    
    Change-Id: Icf737247d9640a200a7a626859cbfc82c5c292c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4519370
    Commit-Queue: Yao Li <yaohuali@google.com>
    Reviewed-by: Vaibhav Raheja <vraheja@chromium.org>
    Tested-by: Yao Li <yaohuali@google.com>

--- arc/keymint/main.cc
+++ arc/keymint/main.cc
@@ -2,6 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <base/command_line.h>
+
+#include "arc/keymint/daemon.h"
+
 int main(int argc, char** argv) {
-  return 0;
+  // arc-keymintd takes no command line arguments.
+  base::CommandLine::Init(argc, argv);
+
+  // TODO(b/247941366): Add logging support.
+
+  LOG(INFO) << "Running Daemon";
+  arc::keymint::Daemon daemon;
+  return daemon.Run();
 }
