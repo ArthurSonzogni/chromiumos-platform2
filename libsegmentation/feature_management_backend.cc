ArthurSonzogni/chromiumos-platform2

commit 4618643e3456c353e802b20c4841440f0886541b

libsegmentation: Write state to VPD and read from VPD sysfs
This change moves libsegmentation persistence logic by writing to the
"vpd" binary and reading from the vpd sysfs file. This is done to
minimize delay on the splash screen.

BUG=b:273339247
TEST=Unit tests.

Change-Id: I107473f8a6b54fe1848b883053ab5bb60e0748aa
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4521909
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Auto-Submit: Abhishek Bhardwaj <abhishekbh@chromium.org>
Tested-by: Abhishek Bhardwaj <abhishekbh@chromium.org>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Abhishek Bhardwaj <abhishekbh@chromium.org>

libsegmentation/feature_management_backend.cc
@@ -6,6 +6,7 @@
 #include <optional>
 #include <string>
 
+#include <base/base64.h>
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
 #include <base/json/json_reader.h>
@@ -165,6 +166,32 @@ std::optional<libsegmentation::DeviceInfo> GetDeviceInfoFromGSC() {
   return device_info;
 }
 
+// Write |device_info| as base64 to the "vpd" binary by spawning a new process.
+bool WriteToVpd(const libsegmentation::DeviceInfo& device_info) {
+  std::string serialized = device_info.SerializeAsString();
+  std::string base64_encoded;
+  base::Base64Encode(serialized, &base64_encoded);
+
+  brillo::ProcessImpl process;
+  process.AddArg("/usr/sbin/vpd");
+  process.AddArg("-i");
+  process.AddArg("RW_VPD");
+  process.AddArg("-s");
+  process.AddArg("feature_device_info=" + base64_encoded);
+
+  if (!process.Start()) {
+    LOG(ERROR) << "Failed to start VPD process";
+    return false;
+  }
+
+  int return_code = process.Wait();
+  if (return_code != 0) {
+    LOG(ERROR) << "VPD process exited with return code: " << return_code;
+    return false;
+  }
+  return true;
+}
+
 }  // namespace
 
 FeatureManagementInterface::FeatureLevel
@@ -211,13 +238,19 @@ bool FeatureManagementImpl::CacheDeviceInfo() {
       return false;
     }
 
-    // Persist the device info read from the hardware id to the stateful
-    // partition. If we fail to write it then don't cache it. We only cache it
-    // if we have the same device info on the stateful partition.
-    if (!FeatureManagementUtil::WriteDeviceInfoToFile(
-            device_info_result.value(), device_info_file)) {
-      LOG(WARNING) << "Failed to persist device info";
-      return false;
+    // Persist the device info read from "gsctool" via "vpd" or to a regular
+    // file for testing. If we fail to write it then don't cache it.
+    if (persist_via_vpd_) {
+      if (!WriteToVpd(device_info_result.value())) {
+        LOG(ERROR) << "Failed to persist device info via vpd";
+        return false;
+      }
+    } else {
+      if (!FeatureManagementUtil::WriteDeviceInfoToFile(
+              device_info_result.value(), device_info_file)) {
+        LOG(ERROR) << "Failed to persist device info";
+        return false;
+      }
     }
   }
 

libsegmentation/feature_management_impl.cc
@@ -23,11 +23,15 @@
 
 namespace segmentation {
 
-#if USE_FEATURE_MANAGEMENT
-// File that will house device info on the stateful partition.
-constexpr char kDeviceInfoFilePath[] =
-    "/mnt/stateful_partition/unencrypted/cache/libsegmentation/property.json";
+// Sysfs file corresponding to VPD state. This will be used to persist device
+// info state and read cache device info state.
+constexpr const char kVpdSysfsFilePath[] =
+    "/sys/firmware/vpd/rw/feature_device_info";
 
+#if USE_FEATURE_MANAGEMENT
+// Sysfs file corresponding to VPD state. This will be used to persist device
+// info state and read cache device info state.
+const char* kDeviceInfoFilePath = kVpdSysfsFilePath;
 #else
 constexpr char kDeviceInfoFilePath[] = "";
 
@@ -44,6 +48,13 @@ FeatureManagementInterface::ScopeLevel FeatureManagementImpl::GetScopeLevel() {
 
 FeatureManagementImpl::FeatureManagementImpl()
     : FeatureManagementImpl(base::FilePath(kDeviceInfoFilePath)) {}
+
+FeatureManagementImpl::FeatureManagementImpl(
+    const base::FilePath& device_info_file_path)
+    : device_info_file_path_(device_info_file_path) {
+  persist_via_vpd_ =
+      device_info_file_path_ == base::FilePath(kVpdSysfsFilePath);
+}
 
 bool FeatureManagementImpl::IsFeatureEnabled(const std::string& name) const {
   // To be continued.

libsegmentation/feature_management_util.cc
@@ -4,9 +4,11 @@
 
 #include <optional>
 
+#include <base/base64.h>
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
 #include <base/logging.h>
+#include <brillo/process/process.h>
 #include <google/protobuf/util/json_util.h>
 
 #include "libsegmentation/device_info.pb.h"
@@ -19,42 +21,30 @@ namespace segmentation {
 // successful.
 std::optional<libsegmentation::DeviceInfo>
 FeatureManagementUtil::ReadDeviceInfoFromFile(const base::FilePath& file_path) {
-  std::string json_input;
-  if (!base::ReadFileToString(file_path, &json_input)) {
-    LOG(ERROR) << "Failed to read JSON string from file: " << file_path;
+  std::string encoded;
+  if (!base::ReadFileToString(file_path, &encoded)) {
+    LOG(ERROR) << "Failed to read protobuf string from file: " << file_path;
     return std::nullopt;
   }
 
+  // The value is expected to be in the base64 format.
+  std::string decoded;
+  base::Base64Decode(encoded, &decoded);
   libsegmentation::DeviceInfo device_info;
-  google::protobuf::util::JsonParseOptions options;
-  options.ignore_unknown_fields = false;
-  auto status = google::protobuf::util::JsonStringToMessage(
-      json_input, &device_info, options);
-  if (!status.ok()) {
-    LOG(ERROR) << "Failed to parse JSON string into DeviceInfo message: "
-               << status.message();
+  if (!device_info.ParseFromString(decoded)) {
+    LOG(ERROR) << "Failed to parse device info from the protobuf";
     return std::nullopt;
   }
-
   return device_info;
 }
 
-// Writes |device_info| as JSON to |file_path|. Returns false if the write isn't
-// successful.
 bool FeatureManagementUtil::WriteDeviceInfoToFile(
-    const libsegmentation::DeviceInfo device_info,
+    const libsegmentation::DeviceInfo& device_info,
     const base::FilePath& file_path) {
-  std::string json_output;
-  google::protobuf::util::JsonPrintOptions options;
-  options.add_whitespace = true;
-  options.always_print_primitive_fields = true;
-  auto status = google::protobuf::util::MessageToJsonString(
-      device_info, &json_output, options);
-  if (!status.ok()) {
-    return false;
-  }
-
-  return base::WriteFile(file_path, json_output);
+  std::string serialized = device_info.SerializeAsString();
+  std::string base64_encoded;
+  base::Base64Encode(serialized, &base64_encoded);
+  return base::WriteFile(file_path, base64_encoded);
 }
 
 FeatureManagementInterface::FeatureLevel

libsegmentation/feature_management_impl.h
@@ -24,8 +24,7 @@ class BRILLO_EXPORT FeatureManagementImpl : public FeatureManagementInterface {
   // feature-management-data.
   FeatureManagementImpl();
 
-  explicit FeatureManagementImpl(const base::FilePath& device_info_file_path)
-      : device_info_file_path_(device_info_file_path) {}
+  explicit FeatureManagementImpl(const base::FilePath& device_info_file_path);
 
   // FeatureManagementInterface overrides.
   bool IsFeatureEnabled(const std::string& name) const override;
@@ -33,9 +32,16 @@ class BRILLO_EXPORT FeatureManagementImpl : public FeatureManagementInterface {
   ScopeLevel GetScopeLevel() override;
 
  private:
-  // Represents the file on the stateful partition that houses the device info.
-  // This will be read to populate |cached_device_info|.
+  // Represents the file that houses the device info. This will be read to
+  // populate |cached_device_info|.
+  //
+  // In production we will write to this path via the "vpd" binary and read it
+  // as a regular file. For testing, we read and write from a test file stored
+  // in this variable.
   base::FilePath device_info_file_path_;
+
+  // Use the "vpd" binary to persist the state.
+  bool persist_via_vpd_ = false;
 
 #if USE_FEATURE_MANAGEMENT
   // Reads device info from the stateful partition, if not present reads it from

libsegmentation/feature_management_util.h
@@ -10,6 +10,7 @@
 #include <base/files/file_path.h>
 #include <brillo/brillo_export.h>
 
+#include "base/files/file.h"
 #include "libsegmentation/device_info.pb.h"
 #include "libsegmentation/feature_management_interface.h"
 
@@ -24,10 +25,10 @@ class BRILLO_EXPORT FeatureManagementUtil {
   static std::optional<libsegmentation::DeviceInfo> ReadDeviceInfoFromFile(
       const base::FilePath& file_path);
 
-  // Writes |device_info| as JSON to |file_path|. Returns false if the write
+  // Writes |device_info| as base64 to |file_path|. Returns false if the write
   // isn't successful.
   static bool WriteDeviceInfoToFile(
-      const libsegmentation::DeviceInfo device_info,
+      const libsegmentation::DeviceInfo& device_info,
       const base::FilePath& file_path);
 
   // Converts feature level from the internal proto to the external API.

commit a2ba62f111af30e615cbe61f663492c434990b62

libsegmentation: Remove extra includes
Remove unnecessary includes that were not listed as dependencies.

BUG=b:273339247
TEST=Compile image in CQ.

fixes: 4618643e34 ("libsegmentation: Write state to VPD and read from VPD sysfs")
Change-Id: Ie3f85a2c5e95db99f2c46fa540cc41559426be5c
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4521845
Tested-by: Abhishek Bhardwaj <abhishekbh@chromium.org>
Reviewed-by: Abhishek Bhardwaj <abhishekbh@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>

libsegmentation/feature_management_backend.cc
@@ -9,14 +9,9 @@
 #include <base/base64.h>
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
-#include <base/json/json_reader.h>
-#include <base/json/json_writer.h>
 #include <base/logging.h>
 #include <base/values.h>
 #include <brillo/process/process.h>
-#include <google/protobuf/util/json_util.h>
-#include <rootdev/rootdev.h>
-#include <vboot/vboot_host.h>
 
 #include "libsegmentation/device_info.pb.h"
 #include "libsegmentation/feature_management_impl.h"

libsegmentation/feature_management_fake.h
@@ -13,7 +13,6 @@
 
 #include "libsegmentation/feature_management.h"
 #include "libsegmentation/feature_management_interface.h"
-#include "proto/feature_management.pb.h"
 
 namespace segmentation {
 

libsegmentation/feature_management_impl.cc
@@ -10,14 +10,9 @@
 
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
-#include <base/json/json_reader.h>
-#include <base/json/json_writer.h>
 #include <base/logging.h>
 #include <base/values.h>
 #include <brillo/data_encoding.h>
-#include <google/protobuf/util/json_util.h>
-#include <rootdev/rootdev.h>
-#include <vboot/vboot_host.h>
 
 #include "libsegmentation/device_info.pb.h"
 #include "libsegmentation/feature_management.h"

libsegmentation/feature_management_util.cc
@@ -9,7 +9,6 @@
 #include <base/files/file_util.h>
 #include <base/logging.h>
 #include <brillo/process/process.h>
-#include <google/protobuf/util/json_util.h>
 
 #include "libsegmentation/device_info.pb.h"
 #include "libsegmentation/feature_management_interface.h"
@@ -17,8 +16,8 @@
 
 namespace segmentation {
 
-// Writes |device_info| as JSON to |file_path|. Returns false if the write isn't
-// successful.
+// Writes |device_info| as base64 to |file_path|. Returns false if the write
+// isn't successful.
 std::optional<libsegmentation::DeviceInfo>
 FeatureManagementUtil::ReadDeviceInfoFromFile(const base::FilePath& file_path) {
   std::string encoded;