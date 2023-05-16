cf99e26073a76aae206524d979892350ff491dba
Chung-Sheng Wu <chungsheng@google.com>
2023-05-16 11:12:40 +0000

    runtime_probe: Implement ArgumentParser
    Implement ArgumentParser to support parsing several types. The different
    from the original parsing function are:
    * Support std::optional, so we can get nullopt when arg is miss. (So for
      example we don't need to use empty string as default value).
    * Support vector of all possible types (int, bool, ProbeFunction...).
    * Support ProbeFunction with default value.
    
    Note that this doesn't mean we will have those new types in arguments.
    This only add the possibility to the framework.
    
    BUG=b:279873258
    TEST=FEATURES=test emerge-zork runtime_probe
    
    Change-Id: I58f344c500e06516f1fdf33658e9d98178bc2ea0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4498175
    Commit-Queue: Chung-sheng Wu <chungsheng@google.com>
    Reviewed-by: Allen Shih <allenshihmc@chromium.org>
    Reviewed-by: Kevin Lin <kevinptt@chromium.org>
    Tested-by: Chung-sheng Wu <chungsheng@google.com>

--- runtime_probe/probe_function_argument_test.cc
+++ runtime_probe/probe_function_argument_test.cc
@@ -8,79 +8,198 @@
 #include <gtest/gtest.h>
 
 #include "runtime_probe/probe_function.h"
+#include "runtime_probe/probe_function_argument.h"
 #include "runtime_probe/probe_function_argument_legacy.h"
 
 namespace runtime_probe {
+namespace {
 
-class Mock1ProbeFunction : public ProbeFunction {
+class BaseProbeFunction : public ProbeFunction {
   using ProbeFunction::ProbeFunction;
 
  public:
-  NAME_PROBE_FUNCTION("mock1");
-
   DataType EvalImpl() const override { return {}; }
 };
 
-class Mock2ProbeFunction : public ProbeFunction {
-  using ProbeFunction::ProbeFunction;
+class NoArgProbeFunction : public BaseProbeFunction {
+  using BaseProbeFunction::BaseProbeFunction;
 
  public:
-  NAME_PROBE_FUNCTION("mock2");
+  NAME_PROBE_FUNCTION("no_arg");
+};
 
-  template <typename T>
-  static auto FromKwargsValue(const base::Value& dict_value) {
-    PARSE_BEGIN();
-    PARSE_ARGUMENT(a_str);
-    PARSE_ARGUMENT(a_int);
-    PARSE_ARGUMENT(a_bool);
-    PARSE_ARGUMENT(default_int, 1);
-    PARSE_END();
+class NoArg2ProbeFunction : public BaseProbeFunction {
+  using BaseProbeFunction::BaseProbeFunction;
+
+ public:
+  NAME_PROBE_FUNCTION("no_arg2");
+};
+
+class ArgProbeFunction : public BaseProbeFunction {
+  using BaseProbeFunction::BaseProbeFunction;
+
+ public:
+  NAME_PROBE_FUNCTION("arg");
+
+  bool PostParseArguments() override { return post_parse_arg_result_; }
+
+  PROBE_FUNCTION_ARG_DEF(std::string, a_str);
+  PROBE_FUNCTION_ARG_DEF(int, a_int);
+  PROBE_FUNCTION_ARG_DEF(bool, a_bool);
+  PROBE_FUNCTION_ARG_DEF(int, default_int, 42);
+  PROBE_FUNCTION_ARG_DEF(std::optional<int>, opt_int);
+  PROBE_FUNCTION_ARG_DEF(std::vector<int>, a_vec_int);
+  PROBE_FUNCTION_ARG_DEF(std::vector<int>,
+                         default_vec_int,
+                         std::vector<int>{1, 2, 3});
+  PROBE_FUNCTION_ARG_DEF(std::unique_ptr<ProbeFunction>, a_probe_fun);
+  PROBE_FUNCTION_ARG_DEF(std::unique_ptr<ProbeFunction>,
+                         default_probe_fun,
+                         CreateProbeFunction<NoArgProbeFunction>(
+                             base::Value(base::Value::Type::DICT)));
+
+  static bool post_parse_arg_result_;
+};
+
+bool ArgProbeFunction::post_parse_arg_result_ = true;
+
+class ProbeFunctionArgumentTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    original_function_table_ = std::move(ProbeFunction::registered_functions_);
+    ProbeFunction::registered_functions_ =
+        ProbeFunctions<NoArgProbeFunction,
+                       NoArg2ProbeFunction>::ConstructRegisteredFunctionTable();
+
+    ArgProbeFunction::post_parse_arg_result_ = true;
+
+    arg_.Set("a_str", "");
+    arg_.Set("a_int", 0);
+    arg_.Set("a_bool", false);
+    arg_.Set("a_vec_int", base::Value::List{});
+    base::Value::Dict probe_fun;
+    probe_fun.Set("no_arg", base::Value::Dict{});
+    arg_.Set("a_probe_fun", std::move(probe_fun));
   }
-  DataType EvalImpl() const override { return {}; }
 
-  std::string a_str_;
-  int a_int_;
-  bool a_bool_;
-  int default_int_;
+  void TearDown() override {
+    ProbeFunction::registered_functions_ = std::move(original_function_table_);
+    ArgProbeFunction::post_parse_arg_result_ = true;
+  }
+
+  base::Value::Dict arg_;
+  ProbeFunction::RegisteredFunctionTableType original_function_table_;
 };
 
-TEST(ProbeFunctionArgumentTest, EmptyArgument) {
-  const base::Value empty_value(base::Value::Type::DICT);
-  base::Value arg_value(base::Value::Type::DICT);
-  arg_value.SetKey("a_str", base::Value("a_str"));
+template <typename T>
+constexpr auto CreateProbeFunctionTmp = &T::template FromKwargsValueTmp<T>;
 
-  auto mock_func1 = CreateProbeFunction<Mock1ProbeFunction>(empty_value);
-  EXPECT_NE(mock_func1, nullptr);
+TEST_F(ProbeFunctionArgumentTest, Required) {
+  arg_.Set("a_str", "str");
+  arg_.Set("a_int", 42);
+  arg_.Set("a_bool", true);
+  {
+    base::Value::List tmp;
+    tmp.Append(1);
+    tmp.Append(2);
+    tmp.Append(3);
+    arg_.Set("a_vec_int", std::move(tmp));
+  }
+  {
+    base::Value::Dict probe_fun;
+    probe_fun.Set("no_arg", base::Value::Dict{});
+    arg_.Set("a_probe_fun", std::move(probe_fun));
+  }
 
-  auto mock_func2 = CreateProbeFunction<Mock1ProbeFunction>(arg_value);
-  EXPECT_EQ(mock_func2, nullptr);
+  auto fun = CreateProbeFunctionTmp<ArgProbeFunction>(arg_);
+  ASSERT_TRUE(fun);
+  EXPECT_EQ(fun->a_str_, "str");
+  EXPECT_EQ(fun->a_int_, 42);
+  EXPECT_EQ(fun->a_bool_, true);
+  EXPECT_EQ(fun->a_vec_int_, (std::vector<int>{1, 2, 3}));
+  ASSERT_TRUE(fun->a_probe_fun_);
+  EXPECT_EQ(fun->a_probe_fun_->GetFunctionName(), "no_arg");
+
+  // Default value
+  EXPECT_EQ(fun->default_int_, 42);
+  EXPECT_EQ(fun->opt_int_, std::nullopt);
+  EXPECT_EQ(fun->default_vec_int_, (std::vector<int>{1, 2, 3}));
+  ASSERT_TRUE(fun->default_probe_fun_);
+  EXPECT_EQ(fun->default_probe_fun_->GetFunctionName(), "no_arg");
 }
 
-TEST(ProbeFunctionArgumentTest, WithArguments) {
-  const base::Value empty_value(base::Value::Type::DICT);
-  base::Value::Dict arg_value = base::Value::Dict()
-                                    .Set("a_str", "a_str")
-                                    .Set("a_int", 1)
-                                    .Set("a_bool", true);
+TEST_F(ProbeFunctionArgumentTest, Optional) {
+  arg_.Set("default_int", 1);
+  arg_.Set("opt_int", 2);
+  {
+    base::Value::List tmp;
+    tmp.Append(4);
+    tmp.Append(5);
+    tmp.Append(6);
+    arg_.Set("default_vec_int", std::move(tmp));
+  }
+  {
+    base::Value::Dict probe_fun;
+    probe_fun.Set("no_arg2", base::Value::Dict{});
+    arg_.Set("default_probe_fun", std::move(probe_fun));
+  }
 
-  auto mock_func1 = CreateProbeFunction<Mock2ProbeFunction>(empty_value);
-  EXPECT_EQ(mock_func1, nullptr);
-
-  auto mock_func2 =
-      CreateProbeFunction<Mock2ProbeFunction>(base::Value(arg_value.Clone()));
-  EXPECT_NE(mock_func2, nullptr);
-  EXPECT_EQ(mock_func2->default_int_, 1);
-
-  arg_value.Set("default_int", base::Value(2));
-  auto mock_func3 =
-      CreateProbeFunction<Mock2ProbeFunction>(base::Value(arg_value.Clone()));
-  EXPECT_NE(mock_func3, nullptr);
-  EXPECT_EQ(mock_func3->default_int_, 2);
-
-  arg_value.Set("invalid_field", base::Value("invalid_field"));
-  auto mock_func4 = CreateProbeFunction<Mock2ProbeFunction>(
-      base::Value(std::move(arg_value)));
-  EXPECT_EQ(mock_func4, nullptr);
+  auto fun = CreateProbeFunctionTmp<ArgProbeFunction>(arg_);
+  ASSERT_TRUE(fun);
+  EXPECT_EQ(fun->default_int_, 1);
+  EXPECT_EQ(fun->opt_int_, 2);
+  EXPECT_EQ(fun->default_vec_int_, (std::vector<int>{4, 5, 6}));
+  ASSERT_TRUE(fun->default_probe_fun_);
+  EXPECT_EQ(fun->default_probe_fun_->GetFunctionName(), "no_arg2");
 }
 
+TEST_F(ProbeFunctionArgumentTest, Empty) {
+  arg_.clear();
+  auto fun = CreateProbeFunctionTmp<ArgProbeFunction>(arg_);
+  ASSERT_FALSE(fun);
+}
+
+TEST_F(ProbeFunctionArgumentTest, WrongType) {
+  arg_.Set("a_int", "str");
+  auto fun = CreateProbeFunctionTmp<ArgProbeFunction>(arg_);
+  ASSERT_FALSE(fun);
+}
+
+TEST_F(ProbeFunctionArgumentTest, WrongOptionalType) {
+  arg_.Set("opt_int", "str");
+  auto fun = CreateProbeFunctionTmp<ArgProbeFunction>(arg_);
+  ASSERT_FALSE(fun);
+}
+
+TEST_F(ProbeFunctionArgumentTest, WrongDefaultType) {
+  arg_.Set("default_int", "str");
+  auto fun = CreateProbeFunctionTmp<ArgProbeFunction>(arg_);
+  ASSERT_FALSE(fun);
+}
+
+TEST_F(ProbeFunctionArgumentTest, WrongVectorType) {
+  {
+    base::Value::List tmp;
+    tmp.Append(4);
+    tmp.Append("str");
+    tmp.Append(6);
+    arg_.Set("default_vec_int", std::move(tmp));
+  }
+  auto fun = CreateProbeFunctionTmp<ArgProbeFunction>(arg_);
+  ASSERT_FALSE(fun);
+}
+
+TEST_F(ProbeFunctionArgumentTest, Unexpected) {
+  arg_.Set("unexpected", "str");
+  auto fun = CreateProbeFunctionTmp<ArgProbeFunction>(arg_);
+  ASSERT_FALSE(fun);
+}
+
+TEST_F(ProbeFunctionArgumentTest, PostParseArgumentsFailed) {
+  ArgProbeFunction::post_parse_arg_result_ = false;
+  auto fun = CreateProbeFunctionTmp<ArgProbeFunction>(arg_);
+  ASSERT_FALSE(fun);
+}
+
+}  // namespace
 }  // namespace runtime_probe
