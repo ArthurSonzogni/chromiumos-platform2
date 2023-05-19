03d0ba482750ed54a5fde106eecb4efe7b28bc44
Wei-Luan Wang <weiluanwang@google.com>
2023-05-19 11:39:14 +0000

    diagnostics: Refactor unit tests of lid event with test future
    BUG=b:275056694
    TEST=USE="-cros-debug" FEATURES=test emerge-volteer diagnostics
    
    Change-Id: Ifa92a0315ef680fd42953162529bd7d43e9eeac2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4549041
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Commit-Queue: Wei-Luan Wang <weiluanwang@google.com>
    Tested-by: Wei-Luan Wang <weiluanwang@google.com>

--- diagnostics/cros_healthd/events/lid_events_impl_test.cc
+++ diagnostics/cros_healthd/events/lid_events_impl_test.cc
@@ -3,17 +3,14 @@
 // found in the LICENSE file.
 
 #include <memory>
-#include <utility>
 
-#include <base/check.h>
-#include <base/run_loop.h>
 #include <base/test/task_environment.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <power_manager/dbus-proxy-mocks.h>
 
+#include "diagnostics/cros_healthd/events/event_observer_test_future.h"
 #include "diagnostics/cros_healthd/events/lid_events_impl.h"
-#include "diagnostics/cros_healthd/events/mock_event_observer.h"
 #include "diagnostics/cros_healthd/system/mock_context.h"
 #include "diagnostics/mojom/public/cros_healthd_events.mojom.h"
 
@@ -23,9 +20,7 @@ namespace {
 namespace mojom = ::ash::cros_healthd::mojom;
 
 using ::testing::_;
-using ::testing::Invoke;
 using ::testing::SaveArg;
-using ::testing::StrictMock;
 
 // Tests for the LidEventsImpl class.
 class LidEventsImplTest : public testing::Test {
@@ -42,20 +37,14 @@ class LidEventsImplTest : public testing::Test {
                 DoRegisterLidOpenedSignalHandler(_, _))
         .WillOnce(SaveArg<0>(&lid_opened_callback_));
     lid_events_impl_ = std::make_unique<LidEventsImpl>(&mock_context_);
-
-    mojo::PendingRemote<mojom::EventObserver> observer;
-    mojo::PendingReceiver<mojom::EventObserver> observer_receiver(
-        observer.InitWithNewPipeAndPassReceiver());
-    observer_ = std::make_unique<StrictMock<MockEventObserver>>(
-        std::move(observer_receiver));
-    lid_events_impl_->AddObserver(std::move(observer));
+    lid_events_impl_->AddObserver(event_observer_.BindNewPendingRemote());
   }
 
   org::chromium::PowerManagerProxyMock* mock_power_manager_proxy() {
     return mock_context_.mock_power_manager_proxy();
   }
 
-  MockEventObserver* mock_observer() { return observer_.get(); }
+  EventObserverTestFuture& event_observer() { return event_observer_; }
 
   void EmitLidClosedSignal() { lid_closed_callback_.Run(); }
 
@@ -64,7 +53,7 @@ class LidEventsImplTest : public testing::Test {
  private:
   base::test::TaskEnvironment task_environment_;
   MockContext mock_context_;
-  std::unique_ptr<StrictMock<MockEventObserver>> observer_;
+  EventObserverTestFuture event_observer_;
   std::unique_ptr<LidEventsImpl> lid_events_impl_;
   base::RepeatingClosure lid_closed_callback_;
   base::RepeatingClosure lid_opened_callback_;
@@ -72,34 +61,22 @@ class LidEventsImplTest : public testing::Test {
 
 // Test that we can receive lid closed events.
 TEST_F(LidEventsImplTest, ReceiveLidClosedEvent) {
-  base::RunLoop run_loop;
-  EXPECT_CALL(*mock_observer(), OnEvent(_))
-      .WillOnce(Invoke([&](mojom::EventInfoPtr info) {
-        EXPECT_TRUE(info->is_lid_event_info());
-        const auto& lid_event_info = info->get_lid_event_info();
-        EXPECT_EQ(lid_event_info->state, mojom::LidEventInfo::State::kClosed);
-        run_loop.Quit();
-      }));
-
   EmitLidClosedSignal();
 
-  run_loop.Run();
+  auto info = event_observer().WaitForEvent();
+  ASSERT_TRUE(info->is_lid_event_info());
+  const auto& lid_event_info = info->get_lid_event_info();
+  EXPECT_EQ(lid_event_info->state, mojom::LidEventInfo::State::kClosed);
 }
 
 // Test that we can receive lid opened events.
 TEST_F(LidEventsImplTest, ReceiveLidOpenedEvent) {
-  base::RunLoop run_loop;
-  EXPECT_CALL(*mock_observer(), OnEvent(_))
-      .WillOnce(Invoke([&](mojom::EventInfoPtr info) {
-        EXPECT_TRUE(info->is_lid_event_info());
-        const auto& lid_event_info = info->get_lid_event_info();
-        EXPECT_EQ(lid_event_info->state, mojom::LidEventInfo::State::kOpened);
-        run_loop.Quit();
-      }));
-
   EmitLidOpenedSignal();
 
-  run_loop.Run();
+  auto info = event_observer().WaitForEvent();
+  ASSERT_TRUE(info->is_lid_event_info());
+  const auto& lid_event_info = info->get_lid_event_info();
+  EXPECT_EQ(lid_event_info->state, mojom::LidEventInfo::State::kOpened);
 }
 
 }  // namespace
