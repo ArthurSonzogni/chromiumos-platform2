dd2a17ffeeec7be5176695d2b1928d64b7ca41ad
Wei-Luan Wang <weiluanwang@google.com>
2023-05-19 12:01:35 +0000

    diagnostics: Refactor unit test of stylus event with test future
    BUG=b:275056694
    TEST=USE="-cros-debug" FEATURES=test emerge-volteer diagnostics
    
    Change-Id: Idb48c60d0077e3a191f50296d85a2c7456294483
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4546815
    Tested-by: Wei-Luan Wang <weiluanwang@google.com>
    Commit-Queue: Wei-Luan Wang <weiluanwang@google.com>
    Reviewed-by: Kerker Yang <kerker@chromium.org>

--- diagnostics/cros_healthd/events/stylus_events_impl_test.cc
+++ diagnostics/cros_healthd/events/stylus_events_impl_test.cc
@@ -2,17 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <memory>
 #include <utility>
 
-#include <base/check.h>
-#include <base/run_loop.h>
 #include <base/test/task_environment.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <mojo/public/cpp/bindings/pending_remote.h>
 
-#include "diagnostics/cros_healthd/events/mock_event_observer.h"
+#include "diagnostics/cros_healthd/events/event_observer_test_future.h"
 #include "diagnostics/cros_healthd/events/stylus_events_impl.h"
 #include "diagnostics/cros_healthd/system/mock_context.h"
 #include "diagnostics/mojom/public/cros_healthd_events.mojom.h"
@@ -23,8 +20,6 @@ namespace {
 namespace mojom = ::ash::cros_healthd::mojom;
 
 using ::testing::_;
-using ::testing::Invoke;
-using ::testing::StrictMock;
 using ::testing::WithArg;
 
 // Tests for the StylusEventsImpl class.
@@ -35,30 +30,16 @@ class StylusEventsImplTest : public testing::Test {
   StylusEventsImplTest& operator=(const StylusEventsImplTest&) = delete;
 
   void SetUp() override {
-    stylus_events_impl_ = std::make_unique<StylusEventsImpl>(&mock_context_);
-
-    SetExecutorMonitorStylus();
-    event_observer_ = CreateAndAddMockObserver();
+    EXPECT_CALL(*mock_executor(), MonitorStylus(_, _))
+        .WillOnce(WithArg<0>([=](auto stylus_observer) {
+          stylus_observer_.Bind(std::move(stylus_observer));
+        }));
   }
 
-  MockEventObserver* mock_event_observer() { return event_observer_.get(); }
   MockExecutor* mock_executor() { return mock_context_.mock_executor(); }
 
-  std::unique_ptr<StrictMock<MockEventObserver>> CreateAndAddMockObserver() {
-    mojo::PendingRemote<mojom::EventObserver> stylus_observer;
-    mojo::PendingReceiver<mojom::EventObserver> observer_receiver(
-        stylus_observer.InitWithNewPipeAndPassReceiver());
-    stylus_events_impl_->AddObserver(std::move(stylus_observer));
-    return std::make_unique<StrictMock<MockEventObserver>>(
-        std::move(observer_receiver));
-  }
-
-  void SetExecutorMonitorStylus() {
-    EXPECT_CALL(*mock_executor(), MonitorStylus(_, _))
-        .WillOnce(WithArg<0>(
-            [=](mojo::PendingRemote<mojom::StylusObserver> stylus_observer) {
-              stylus_observer_.Bind(std::move(stylus_observer));
-            }));
+  void AddEventObserver(mojo::PendingRemote<mojom::EventObserver> observer) {
+    stylus_events_impl_.AddObserver(std::move(observer));
   }
 
   void EmitStylusConnectedEvent(const mojom::StylusConnectedEventPtr& event) {
@@ -71,8 +52,7 @@ class StylusEventsImplTest : public testing::Test {
  private:
   base::test::TaskEnvironment task_environment_;
   MockContext mock_context_;
-  std::unique_ptr<StrictMock<MockEventObserver>> event_observer_;
-  std::unique_ptr<StylusEventsImpl> stylus_events_impl_;
+  StylusEventsImpl stylus_events_impl_{&mock_context_};
   mojo::Remote<mojom::StylusObserver> stylus_observer_;
 };
 
@@ -81,19 +61,16 @@ TEST_F(StylusEventsImplTest, StylusTouchEvent) {
   mojom::StylusTouchEvent fake_touch_event;
   fake_touch_event.touch_point = mojom::StylusTouchPointInfo::New();
 
-  base::RunLoop run_loop;
-  EXPECT_CALL(*mock_event_observer(), OnEvent(_))
-      .WillOnce(Invoke([&](mojom::EventInfoPtr info) {
-        EXPECT_TRUE(info->is_stylus_event_info());
-        const auto& stylus_event_info = info->get_stylus_event_info();
-        EXPECT_TRUE(stylus_event_info->is_touch_event());
-        EXPECT_EQ(fake_touch_event, *stylus_event_info->get_touch_event());
-        run_loop.Quit();
-      }));
+  EventObserverTestFuture event_observer;
+  AddEventObserver(event_observer.BindNewPendingRemote());
 
   EmitStylusTouchEvent(fake_touch_event.Clone());
 
-  run_loop.Run();
+  auto info = event_observer.WaitForEvent();
+  ASSERT_TRUE(info->is_stylus_event_info());
+  const auto& stylus_event_info = info->get_stylus_event_info();
+  ASSERT_TRUE(stylus_event_info->is_touch_event());
+  EXPECT_EQ(fake_touch_event, *stylus_event_info->get_touch_event());
 }
 
 // Test that we can receive stylus connected events.
@@ -102,20 +79,16 @@ TEST_F(StylusEventsImplTest, StylusConnectedEvent) {
   fake_connected_event.max_x = 1;
   fake_connected_event.max_y = 2;
 
-  base::RunLoop run_loop;
-  EXPECT_CALL(*mock_event_observer(), OnEvent(_))
-      .WillOnce(Invoke([&](mojom::EventInfoPtr info) {
-        EXPECT_TRUE(info->is_stylus_event_info());
-        const auto& stylus_event_info = info->get_stylus_event_info();
-        EXPECT_TRUE(stylus_event_info->is_connected_event());
-        EXPECT_EQ(fake_connected_event,
-                  *stylus_event_info->get_connected_event());
-        run_loop.Quit();
-      }));
+  EventObserverTestFuture event_observer;
+  AddEventObserver(event_observer.BindNewPendingRemote());
 
   EmitStylusConnectedEvent(fake_connected_event.Clone());
 
-  run_loop.Run();
+  auto info = event_observer.WaitForEvent();
+  ASSERT_TRUE(info->is_stylus_event_info());
+  const auto& stylus_event_info = info->get_stylus_event_info();
+  ASSERT_TRUE(stylus_event_info->is_connected_event());
+  EXPECT_EQ(fake_connected_event, *stylus_event_info->get_connected_event());
 }
 
 // Test that we can receive stylus connected events by multiple observers.
@@ -124,26 +97,21 @@ TEST_F(StylusEventsImplTest, StylusConnectedEventWithMultipleObservers) {
   fake_connected_event.max_x = 1;
   fake_connected_event.max_y = 2;
 
-  auto second_event_observer = CreateAndAddMockObserver();
-
-  int counter = 2;
-  base::RunLoop run_loop;
-  auto on_event = [&](mojom::EventInfoPtr info) {
-    EXPECT_TRUE(info->is_stylus_event_info());
-    const auto& stylus_event_info = info->get_stylus_event_info();
-    EXPECT_TRUE(stylus_event_info->is_connected_event());
-    EXPECT_EQ(fake_connected_event, *stylus_event_info->get_connected_event());
-    counter--;
-    if (counter == 0) {
-      run_loop.Quit();
-    }
-  };
-  EXPECT_CALL(*mock_event_observer(), OnEvent(_)).WillOnce(Invoke(on_event));
-  EXPECT_CALL(*second_event_observer, OnEvent(_)).WillOnce(Invoke(on_event));
+  EventObserverTestFuture event_observer, event_observer2;
+  AddEventObserver(event_observer.BindNewPendingRemote());
+  AddEventObserver(event_observer2.BindNewPendingRemote());
 
   EmitStylusConnectedEvent(fake_connected_event.Clone());
 
-  run_loop.Run();
+  auto check_result = [&fake_connected_event](mojom::EventInfoPtr info) {
+    ASSERT_TRUE(info->is_stylus_event_info());
+    const auto& stylus_event_info = info->get_stylus_event_info();
+    ASSERT_TRUE(stylus_event_info->is_connected_event());
+    EXPECT_EQ(fake_connected_event, *stylus_event_info->get_connected_event());
+  };
+
+  check_result(event_observer.WaitForEvent());
+  check_result(event_observer2.WaitForEvent());
 }
 
 }  // namespace
