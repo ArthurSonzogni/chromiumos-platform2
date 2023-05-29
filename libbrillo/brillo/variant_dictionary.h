9814889050463876fa2a9e5e9d99c43d670f8433
Jie Jiang <jiejiang@chromium.org>
2023-05-29 11:09:30 +0000

    libbrillo: Change signature of GetVariantValueOrDefault()
    - Replace `const std::string&` with `base::StringPiece`, to avoid
      unnecessary temp string objects at the call sites;
    - Change return type from `const T` to `T`, since const value disables
      some optimizations can be done by the compiler when move semantics
      can be used at the call site (also see [1]).
    
    [1] go/c-readability-advice#const-value-return-type
    
    BUG=b:284259970
    TEST=FEATURES="test" emerge-$BOARD libbrillo shill
    
    Change-Id: Ice04c5f877227a6d7e13c0e4f59d427816936cc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4573033
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Jie Jiang <jiejiang@chromium.org>
    Commit-Queue: Jie Jiang <jiejiang@chromium.org>

--- libbrillo/brillo/variant_dictionary.h
+++ libbrillo/brillo/variant_dictionary.h
@@ -9,6 +9,7 @@
 #include <map>
 #include <string>
 
+#include <base/strings/string_piece.h>
 #include <brillo/any.h>
 #include <brillo/brillo_export.h>
 
@@ -20,8 +21,8 @@ using VariantDictionary = std::map<std::string, brillo::Any, std::less<>>;
 // and convert it to the type T.  If the value does not exist, or the type
 // conversion fails, the default value of type T is returned.
 template <typename T>
-const T GetVariantValueOrDefault(const VariantDictionary& dictionary,
-                                 const std::string& key) {
+T GetVariantValueOrDefault(const VariantDictionary& dictionary,
+                           base::StringPiece key) {
   VariantDictionary::const_iterator it = dictionary.find(key);
   if (it == dictionary.end()) {
     return T();
