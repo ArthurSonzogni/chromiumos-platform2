a140945a141aa45f35219913fe958a3a6922dbc2
Byron Lee <byronlee@google.com>
2023-05-30 08:28:46 +0000

    printscanmgr: Update {bind,callback,callback_forward,callback_helpers}.h includes
    These headers are now in base/functional/.
    
    BUG=b:266161904
    TEST=CQ
    
    Change-Id: I54843c9a976e9f92249085bcbe29b984954bce9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4574304
    Tested-by: Byron Lee <byronlee@chromium.org>
    Reviewed-by: Paul Moy <pmoy@chromium.org>
    Auto-Submit: Byron Lee <byronlee@chromium.org>
    Commit-Queue: Byron Lee <byronlee@chromium.org>

--- printscanmgr/executor/mojo_adaptor.h
+++ printscanmgr/executor/mojo_adaptor.h
@@ -7,7 +7,7 @@
 
 #include <memory>
 
-#include <base/bind.h>
+#include <base/functional/bind.h>
 #include <base/memory/weak_ptr.h>
 #include <base/task/single_thread_task_runner.h>
 #include <dbus/bus.h>
