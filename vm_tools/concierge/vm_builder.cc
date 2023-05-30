ArthurSonzogni/chromiumos-platform2

commit 6f3a202b047fe0152401d5ccea68fd7e95806bad

vm_tools: decouple cross domain enablement
Decouple cross domain enablement from virtgpu-native-context
enablement. virtgpu-native-context enablement is moved to an
inner condition of cross domain enablement.

BUG=b:267565578
TEST=emerge build vm_host_tools, make sure both of the flags are true. Deploy package vm_host_tools and strongbad device works fine with a couple of apps from Google Play Store.

Change-Id: Iee6b0d9d8744e5bc9454a77692bac09f5b4f052e
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4234483
Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
Reviewed-by: Ryan Neph <ryanneph@google.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Commit-Queue: Dawn Han <dawnhan@google.com>
Tested-by: Dawn Han <dawnhan@google.com>

vm_tools/concierge/vm_builder.cc
@@ -462,7 +462,7 @@ base::StringPairs VmBuilder::BuildRunParams() const {
   if (enable_gpu_) {
     std::string gpu_arg = "vulkan=";
     gpu_arg += enable_vulkan_ ? "true" : "false";
-    if (enable_virtgpu_native_context_ || enable_cross_domain_context_) {
+    if (enable_cross_domain_context_) {
       gpu_arg += ",context-types=cross-domain";
       if (enable_vulkan_) {
         gpu_arg += ":venus";

commit e6c7e9ae318200d8910e616cc3ccff512506e84c

vm_tools: Don't require cross-domain for native-context
The dependency on cross-domain is only applicable for arcvm (ie.
gralloc), but not for crostini.

BUG=None
TEST=vmc start --enable-virtgpu-native-context termina

Fixes: 6f3a202b04 ("vm_tools: decouple cross domain enablement")
Change-Id: I9220fd32a1b58a83828a4a6fd939403b95bccb14
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4485158
Reviewed-by: Ryan Neph <ryanneph@google.com>
Commit-Queue: Rob Clark <robdclark@chromium.org>
Tested-by: Rob Clark <robdclark@chromium.org>
Auto-Submit: Rob Clark <robdclark@chromium.org>
Reviewed-by: Nic Hollingum <hollingum@google.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>

vm_tools/concierge/vm_builder.cc
@@ -405,8 +405,11 @@ base::StringPairs VmBuilder::BuildRunParams() const {
   if (enable_gpu_) {
     std::string gpu_arg = "vulkan=";
     gpu_arg += enable_vulkan_ ? "true" : "false";
-    if (enable_cross_domain_context_) {
-      gpu_arg += ",context-types=cross-domain";
+    if (enable_virtgpu_native_context_ || enable_cross_domain_context_) {
+      gpu_arg += ",context-types=";
+      if (enable_cross_domain_context_) {
+        gpu_arg += ":cross-domain";
+      }
       if (enable_vulkan_) {
         gpu_arg += ":venus";
       }