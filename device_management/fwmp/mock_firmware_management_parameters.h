caef4ab93008d3878c2bd7763c604d1b37cb5dda
Md Sadman Sakib <sadmansakib@google.com>
2023-06-01 23:04:17 +0800

    device_management: migrate fwmp related unittests
    BUG=b:271826562
    TEST=FEATURES=test emerge-${BOARD} device_management
    
    Cq-Depend: chromium:4576765
    Change-Id: I6461b35757b81cf7c9deb7bcf5e0f388906890db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4580687
    Tested-by: Md Sadman Sakib <sadmansakib@google.com>
    Reviewed-by: Yi Chou <yich@google.com>
    Commit-Queue: Md Sadman Sakib <sadmansakib@google.com>

---
+++ device_management/fwmp/mock_firmware_management_parameters.h
@@ -0,0 +1,28 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVICE_MANAGEMENT_FWMP_MOCK_FIRMWARE_MANAGEMENT_PARAMETERS_H_
+#define DEVICE_MANAGEMENT_FWMP_MOCK_FIRMWARE_MANAGEMENT_PARAMETERS_H_
+
+#include "device_management/fwmp/firmware_management_parameters.h"
+#include <gmock/gmock.h>
+
+namespace device_management {
+class MockFirmwareManagementParameters
+    : public fwmp::FirmwareManagementParameters {
+ public:
+  MockFirmwareManagementParameters();
+  virtual ~MockFirmwareManagementParameters();
+
+  MOCK_METHOD(bool, Create, (), (override));
+  MOCK_METHOD(bool, Destroy, (), (override));
+  MOCK_METHOD(bool, Load, (), (override));
+  MOCK_METHOD(bool, Store, (uint32_t, const brillo::Blob*), (override));
+  MOCK_METHOD(bool, GetFlags, (uint32_t*), (override));
+  MOCK_METHOD(bool, GetDeveloperKeyHash, (brillo::Blob*), (override));
+  MOCK_METHOD(bool, IsLoaded, (), (const, override));
+};
+}  // namespace device_management
+
+#endif  // DEVICE_MANAGEMENT_FWMP_MOCK_FIRMWARE_MANAGEMENT_PARAMETERS_H_
