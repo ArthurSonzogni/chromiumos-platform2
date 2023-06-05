0c8a5002affbcc4bd50a874fccbea719e712a2bf
Yi Chou <yich@google.com>
2023-06-05 11:55:55 +0800

    libhwsec: Add fuzzed protobuf generator
    BUG=b:284913581
    TEST=CQ
    
    Change-Id: If4de9389cab307bfd762cd610efb9ba6d4ae9069
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4583200
    Commit-Queue: Yi Chou <yich@google.com>
    Reviewed-by: Ching-Kang Yen <chingkang@chromium.org>
    Tested-by: Yi Chou <yich@google.com>

--- libhwsec/middleware/middleware_owner.h
+++ libhwsec/middleware/middleware_owner.h
@@ -26,13 +26,13 @@
 
 #if USE_FUZZER
 #include <fuzzer/FuzzedDataProvider.h>
-#include "libhwsec/fuzzed/attestation.h"
 #include "libhwsec/fuzzed/basic_objects.h"
 #include "libhwsec/fuzzed/config.h"
 #include "libhwsec/fuzzed/hwsec_objects.h"
 #include "libhwsec/fuzzed/key_management.h"
 #include "libhwsec/fuzzed/middleware.h"
 #include "libhwsec/fuzzed/pinweaver.h"
+#include "libhwsec/fuzzed/protobuf.h"
 #include "libhwsec/fuzzed/recovery_crypto.h"
 #include "libhwsec/fuzzed/signature_sealing.h"
 #include "libhwsec/fuzzed/storage.h"
