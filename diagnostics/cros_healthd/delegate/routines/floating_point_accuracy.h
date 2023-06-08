e64e7cf153c6d9913abdb799937b3dce020026ae
Ethan Cheng <yycheng@google.com>
2023-06-08 05:38:02 +0000

    diagnostics: Copy Floating Point Accuracy into delegate
    Copy the functions needed to run Floating Point Accuracy routine into
    the delegate folder.
    
    BUG=b:182964589
    TEST=FEATURES=test P2_TEST_FILTER='*' emerge-volteer diagnostics
    
    Change-Id: I083a8c9b7d4bb62eaf0be09c7204a317b9931e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4598552
    Tested-by: Ethan Cheng <yycheng@google.com>
    Commit-Queue: Ethan Cheng <yycheng@google.com>
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Reviewed-by: Byron Lee <byronlee@chromium.org>

---
+++ diagnostics/cros_healthd/delegate/routines/floating_point_accuracy.h
@@ -0,0 +1,29 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DIAGNOSTICS_CROS_HEALTHD_DELEGATE_ROUTINES_FLOATING_POINT_ACCURACY_H_
+#define DIAGNOSTICS_CROS_HEALTHD_DELEGATE_ROUTINES_FLOATING_POINT_ACCURACY_H_
+
+#include <bitset>
+#include <cstdint>
+#include "base/time/time.h"
+
+namespace diagnostics {
+
+class FloatingPointAccuracyDelegate {
+ public:
+  FloatingPointAccuracyDelegate() = default;
+  FloatingPointAccuracyDelegate(const FloatingPointAccuracyDelegate&) = delete;
+  FloatingPointAccuracyDelegate& operator=(
+      const FloatingPointAccuracyDelegate&) = delete;
+  virtual ~FloatingPointAccuracyDelegate() = default;
+
+  // Executes floating point accuracy task. Returns true if test is completed
+  // without any error, false otherwise.
+  bool Run();
+};
+
+}  // namespace diagnostics
+
+#endif  // DIAGNOSTICS_CROS_HEALTHD_DELEGATE_ROUTINES_FLOATING_POINT_ACCURACY_H_
