19dbe79adfa578776c3c60e97bc97141e54fd9dc
Oleh Lamzin <lamzin@google.com>
2023-06-12 15:41:42 +0000

    Revert "vm_tools: maitred: Set up /etc overlay mount"
    This reverts commit 48ab68b48a01bb6aee6ff42af66acb8f384ef722.
    
    Reason for revert: CL prevents sludge VM to start
    
    BUG=b:286771697
    TEST=manually built sludge image with reverted CL and verified that 
         wilco.APIGetConfigurationDataEmpty test passes
         sludge VM build instruction in go/sludge-care
    
    Original change's description:
    > vm_tools: maitred: Set up /etc overlay mount
    >
    > Maitred doesn't correctly handle timezone changes in systemd distros.
    > This requires /etc/localtime to be a symlink to the desired zoneinfo. To
    > standardise the implementation of timezone updates across VMs, introduce
    > a writable /etc overlay.
    >
    > This is disabled on Borealis until the Borealis kernel has overlayfs
    > support.
    >
    > BUG=b:268424708
    > TEST=Build and deploy tatl
    >
    > Change-Id: I6295292987d3e47d155c40ad2edb5b16f17b64a4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4597218
    > Tested-by: James Ye <jamesye@google.com>
    > Commit-Queue: James Ye <jamesye@google.com>
    > Reviewed-by: David Munro <davidmunro@google.com>
    
    BUG=b:268424708
    
    Change-Id: I5e96c5387a1633db8069ec03a1cd520f3afbde30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4604237
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Pi-Hsun Shih <pihsun@chromium.org>
    Auto-Submit: Oleh Lamzin <lamzin@google.com>
    Tested-by: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>

--- vm_tools/maitred/init.cc
+++ vm_tools/maitred/init.cc
@@ -56,7 +56,6 @@
 #include <base/strings/string_util.h>
 #include <base/strings/stringprintf.h>
 #include <base/time/time.h>
-#include <brillo/file_utils.h>
 #include <chromeos/constants/vm_tools.h>
 #include <grpcpp/grpcpp.h>
 #include <vm_protos/proto_bindings/vm_crash.grpc.pb.h>
@@ -356,24 +355,6 @@ constexpr struct {
     {
         .path = "/var/lib/misc",
         .mode = 0755,
-    },
-#endif
-};
-
-// Overlay mounts to be created on boot.
-constexpr struct {
-  const char* target;
-  const char* lower_dir;
-  const char* upper_dir;
-  const char* work_dir;
-} overlays[] = {
-// TODO(b/286177860): remove this once the Borealis kernel has overlayfs
-#if !USE_VM_BOREALIS
-    {
-        .target = "/etc",
-        .lower_dir = "/etc",
-        .upper_dir = "/run/etc/upper",
-        .work_dir = "/run/etc/work",
     },
 #endif
 };
@@ -1314,28 +1295,6 @@ bool Init::Setup() {
     for (const auto& dir : boot_dirs) {
       if (mkdir(dir.path, dir.mode) != 0 && errno != EEXIST) {
         PLOG(ERROR) << "Failed to create " << dir.path;
-        return false;
-      }
-    }
-
-    for (const auto& overlay : overlays) {
-      if (!brillo::MkdirRecursively(base::FilePath(overlay.upper_dir), 0755)
-               .is_valid()) {
-        PLOG(ERROR) << "Failed to create " << overlay.upper_dir;
-        return false;
-      }
-
-      if (!brillo::MkdirRecursively(base::FilePath(overlay.work_dir), 0755)
-               .is_valid()) {
-        PLOG(ERROR) << "Failed to create " << overlay.work_dir;
-        return false;
-      }
-
-      string options = base::StringPrintf("lowerdir=%s,upperdir=%s,workdir=%s",
-                                          overlay.lower_dir, overlay.upper_dir,
-                                          overlay.work_dir);
-      if (mount("overlay", overlay.target, "overlay", 0, options.c_str())) {
-        PLOG(ERROR) << "Failed to mount overlay " << overlay.target;
         return false;
       }
     }
