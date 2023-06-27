ArthurSonzogni/chromiumos-platform2

commit 88384aabf9de6114b026be650faa63bab17cb286

Reland "init: DoMountVarAndHomeChronos"
This is a reland of commit 123ddd0718a387c5c4c594c126026ccafc5b3e6a

Fixed the mount-encrypted set call to properly propagate the backup
file for the system key as an arg.

Also fixed the crash reporter args.

Original change's description:
> init: DoMountVarAndHomeChronos
>
> Implement the different mount helper classes, which implement the
> different functionality for DoMountVarAndHomeChronos for devices
> with test images or in factory mode.
> This functionality comes from the test_utils.sh and
> factory_utils.sh scripts, links for reference:
> https://source.corp.google.com/chromeos_public/src/platform2/init/upstart/test-init/test_utils.sh
> https://source.corp.google.com/chromeos_public/src/platform2/init/upstart/test-init/factory_utils.sh
>
> BUG=b:172210918
> TEST=FEATURES=test emerge-brya chromeos-init, installed and booted test image on brya,
> built a factory_install image and booted on hatch
>
> Cq-Depend: chromium:4083809
> Change-Id: I73c11257fae80843980d59fb0baf463d95dff9b8
> Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2999420
> Tested-by: Alexis Savery <asavery@chromium.org>
> Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
> Commit-Queue: Alexis Savery <asavery@chromium.org>

BUG=b:172210918, b:262690740
TEST=FEATURES=FEATURES=test emerge-asurada, deployed to DUT, used
echo 'clobber' > /mnt/stateful_partition/.update_available ; crossystem clear_tpm_owner_request=1; reboot
run test_that suite:bvt-tast-informational, confirmed no longer seeing Unexpected TPM status failures
which triggered the revert.

Cq-Depend: chromium:4121233
Change-Id: Ib0bcbf5bfa1898a8bef95f55693bc5d441ab0645
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4119228
Commit-Queue: Alexis Savery <asavery@chromium.org>
Reviewed-by: Sarthak Kukreti <sarthakkukreti@google.com>
Tested-by: Alexis Savery <asavery@chromium.org>
Reviewed-by: Mike Frysinger <vapier@chromium.org>

init/startup/mount_helper.cc
@@ -5,6 +5,7 @@
 #include <sys/mount.h>
 
 #include <memory>
+#include <optional>
 #include <stack>
 #include <string>
 #include <utility>
@@ -26,6 +27,7 @@ namespace {
 
 constexpr char kVar[] = "var";
 constexpr char kHomeChronos[] = "home/chronos";
+constexpr char kMountEncryptedLog[] = "run/mount_encrypted/mount-encrypted.log";
 
 }  // namespace
 
@@ -34,11 +36,13 @@ namespace startup {
 MountHelper::MountHelper(std::unique_ptr<Platform> platform,
                          const Flags& flags,
                          const base::FilePath& root,
-                         const base::FilePath& stateful)
+                         const base::FilePath& stateful,
+                         const bool dev_mode)
     : platform_(std::move(platform)),
       flags_(flags),
       root_(root),
-      stateful_(stateful) {}
+      stateful_(stateful),
+      dev_mode_(dev_mode) {}
 
 // Adds mounts to undo_mount stack.
 void MountHelper::RememberMount(const base::FilePath& mount) {
@@ -118,6 +122,25 @@ void MountHelper::MountOrFail(const base::FilePath& source,
   CleanupMounts(msg);
 }
 
+// Create, possibly migrate from, the unencrypted stateful partition, and bind
+// mount the /var and /home/chronos mounts from the encrypted filesystem
+// /mnt/stateful_partition/encrypted, all managed by the "mount-encrypted"
+// helper. Takes the same arguments as mount-encrypted. Since /var is managed by
+// mount-encrypted, it should not be created in the unencrypted stateful
+// partition. Its mount point in the root filesystem exists already from the
+// rootfs image. Since /home is still mounted from the unencrypted stateful
+// partition, having /home/chronos already doesn't matter. It will be created by
+// mount-encrypted if it is missing. These mounts inherit nodev,noexec,nosuid
+// from the encrypted filesystem /mnt/stateful_partition/encrypted.
+bool MountHelper::MountVarAndHomeChronosEncrypted() {
+  base::FilePath mount_enc_log = root_.Append(kMountEncryptedLog);
+  std::string output;
+  std::vector<std::string> mnt_enc_args;
+  int status = platform_->MountEncrypted(mnt_enc_args, &output);
+  base::AppendToFile(mount_enc_log, output);
+  return status == 0;
+}
+
 // Give mount-encrypted umount 10 times to retry, otherwise
 // it will fail with "device is busy" because lazy umount does not finish
 // clearing all reference points yet. Check crbug.com/p/21345.
@@ -146,7 +169,25 @@ bool MountHelper::UmountVarAndHomeChronosEncrypted() {
     }
     base::PlatformThread::Sleep(base::Milliseconds(100));
   }
-  return !ret;
+  return ret == 0;
+}
+
+// Bind mount /var and /home/chronos. All function arguments are ignored.
+bool MountHelper::MountVarAndHomeChronosUnencrypted() {
+  if (!base::CreateDirectory(stateful_.Append(kVar))) {
+    return false;
+  }
+
+  if (!platform_->Mount(stateful_.Append("var"), root_.Append(kVar), "",
+                        MS_BIND, "")) {
+    return false;
+  }
+  if (!platform_->Mount(stateful_.Append(kHomeChronos),
+                        root_.Append(kHomeChronos), "", MS_BIND, "")) {
+    platform_->Umount(root_.Append(kVar));
+    return false;
+  }
+  return true;
 }
 
 // Unmount bind mounts for /var and /home/chronos.
@@ -159,6 +200,15 @@ bool MountHelper::UmountVarAndHomeChronosUnencrypted() {
     ret = true;
   }
   return ret;
+}
+
+bool MountHelper::MountVarAndHomeChronos() {
+  std::optional<bool> encrypted = flags_.encstateful;
+  bool encrypted_state = encrypted.value_or(false);
+  if (encrypted_state) {
+    return MountVarAndHomeChronosEncrypted();
+  }
+  return MountVarAndHomeChronosUnencrypted();
 }
 
 bool MountHelper::DoUmountVarAndHomeChronos() {
@@ -177,6 +227,18 @@ void MountHelper::SetMountStackForTest(
 
 Flags MountHelper::GetFlags() {
   return flags_;
+}
+
+base::FilePath MountHelper::GetRoot() {
+  return root_;
+}
+
+base::FilePath MountHelper::GetStateful() {
+  return stateful_;
+}
+
+Platform* MountHelper::GetPlatform() {
+  return platform_.get();
 }
 
 }  // namespace startup

init/startup/platform_impl.cc
@@ -16,6 +16,10 @@
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
 #include <base/logging.h>
+#include <base/strings/string_split.h>
+#include <base/strings/string_util.h>
+#include <brillo/blkdev_utils/lvm.h>
+#include <brillo/key_value_store.h>
 #include <brillo/process/process.h>
 
 #include "init/clobber_state.h"
@@ -23,6 +27,14 @@
 #include "init/crossystem_impl.h"
 #include "init/startup/platform_impl.h"
 #include "init/utils.h"
+
+namespace {
+
+constexpr char kProcCmdLine[] = "proc/cmdline";
+constexpr char kFactoryDir[] = "mnt/stateful_partition/dev_image/factory";
+const size_t kMaxReadSize = 4 * 1024;
+
+}  // namespace
 
 namespace startup {
 
@@ -59,6 +71,24 @@ base::ScopedFD Platform::Open(const base::FilePath& pathname, int flags) {
 // NOLINTNEXTLINE(runtime/int)
 int Platform::Ioctl(int fd, unsigned long request, int* arg1) {
   return ioctl(fd, request, arg1);
+}
+
+int Platform::MountEncrypted(const std::vector<std::string>& args,
+                             std::string* output) {
+  brillo::ProcessImpl mount_enc;
+  base::FilePath file;
+  base::CreateTemporaryFile(&file);
+  mount_enc.AddArg("/usr/sbin/mount-encrypted");
+  for (auto arg : args) {
+    mount_enc.AddArg(arg);
+  }
+  mount_enc.RedirectOutput(file.value());
+  int status = mount_enc.Run();
+  if (status == 0) {
+    base::ReadFileToString(file, output);
+  }
+  base::DeleteFile(file);
+  return status;
 }
 
 void Platform::BootAlert(const std::string& arg) {
@@ -218,6 +248,68 @@ bool Platform::InDevMode(CrosSystem* cros_system) {
   // a developer image.
   int debug;
   return (cros_system->GetInt("cros_debug", &debug) && debug == 1);
+}
+
+// Determine if the device is using a test image.
+bool IsTestImage(const base::FilePath& lsb_file) {
+  brillo::KeyValueStore store;
+  if (!store.Load(lsb_file)) {
+    PLOG(ERROR) << "Problem parsing " << lsb_file.value();
+    return false;
+  }
+  std::string value;
+  if (!store.GetString("CHROMEOS_RELEASE_TRACK", &value)) {
+    PLOG(ERROR) << "CHROMEOS_RELEASE_TRACK not found in " << lsb_file.value();
+    return false;
+  }
+  return base::StartsWith(value, "test", base::CompareCase::SENSITIVE);
+}
+
+// Return if the device is in factory test mode.
+bool IsFactoryTestMode(CrosSystem* cros_system,
+                       const base::FilePath& base_dir) {
+  // The path to factory enabled tag. If this path exists in a debug build,
+  // we assume factory test mode.
+  base::FilePath factory_dir = base_dir.Append(kFactoryDir);
+  base::FilePath factory_tag = factory_dir.Append("enabled");
+  struct stat statbuf;
+  int res;
+  if (cros_system->GetInt("debug_build", &res) && res == 1 &&
+      stat(factory_tag.value().c_str(), &statbuf) == 0 &&
+      S_ISREG(statbuf.st_mode)) {
+    return true;
+  }
+  return false;
+}
+
+// Return if the device is in factory installer mode.
+bool IsFactoryInstallerMode(const base::FilePath& base_dir) {
+  std::string cmdline;
+
+  if (!base::ReadFileToStringWithMaxSize(base_dir.Append(kProcCmdLine),
+                                         &cmdline, kMaxReadSize)) {
+    PLOG(ERROR) << "Failed to read proc command line";
+    return false;
+  }
+
+  if (cmdline.find("cros_factory_install") != std::string::npos) {
+    return true;
+  }
+
+  struct stat statbuf;
+  base::FilePath installer = base_dir.Append("root/.factory_installer");
+  if (stat(installer.value().c_str(), &statbuf) == 0 &&
+      S_ISREG(statbuf.st_mode)) {
+    return true;
+  }
+  return false;
+}
+
+// Return if the device is in either in factory test mode or in factory
+// installer mode.
+bool IsFactoryMode(CrosSystem* cros_system, const base::FilePath& base_dir) {
+  return (IsFactoryTestMode(cros_system, base_dir) ||
+          IsFactoryInstallerMode(base_dir));
 }
 
 }  // namespace startup

init/BUILD.gn
@@ -71,10 +71,15 @@ static_library("utils") {
 static_library("libcrosstartup") {
   sources = [
     "startup/chromeos_startup.cc",
+    "startup/factory_mode_mount_helper.cc",
+    "startup/flags.h",
     "startup/mount_helper.cc",
+    "startup/mount_helper_factory.cc",
     "startup/platform_impl.cc",
     "startup/security_manager.cc",
+    "startup/standard_mount_helper.cc",
     "startup/stateful_mount.cc",
+    "startup/test_mode_mount_helper.cc",
   ]
   configs += [ ":target_defaults" ]
   deps = [

init/chromeos_startup.sh
@@ -125,7 +125,6 @@ dev_update_stateful_partition() { true; }
 
 # do_* are wrapper functions that may be redefined in developer mode or test
 # images. Find more implementation in {dev,test,factory}_utils.sh.
-do_mount_var_and_home_chronos() { mount_var_and_home_chronos; }
 do_umount_var_and_home_chronos() { umount_var_and_home_chronos; }
 
 if [ "${CROS_DEBUG}" -eq 1 ]; then
@@ -268,18 +267,6 @@ force_clean_file_attrs() {
     exec clobber-state "keepimg"
   fi
 }
-
-if ! do_mount_var_and_home_chronos; then
-  if [ ! -O "${MOUNT_ENCRYPTED_FAILED_FILE}" ]; then
-    touch "${MOUNT_ENCRYPTED_FAILED_FILE}"
-  else
-    crossystem recovery_request=1
-  fi
-  reboot
-  exit 0
-fi
-rm -f "${MOUNT_ENCRYPTED_FAILED_FILE}"
-remember_mount "${ENCRYPTED_STATEFUL_MNT}"
 
 # Setup the encrypted reboot vault once the encrypted stateful partition
 # is available. If unlocking the encrypted reboot vault failed (due to power

init/encrypted_stateful/startup_utils.sh
@@ -2,20 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Create, possibly migrate from, the unencrypted stateful partition, and bind
-# mount the /var and /home/chronos mounts from the encrypted filesystem
-# /mnt/stateful_partition/encrypted, all managed by the "mount-encrypted"
-# helper. Takes the same arguments as mount-encrypted. Since /var is managed by
-# mount-encrypted, it should not be created in the unencrypted stateful
-# partition. Its mount point in the root filesystem exists already from the
-# rootfs image.  Since /home is still mounted from the unencrypted stateful
-# partition, having /home/chronos already doesn't matter. It will be created by
-# mount-encrypted if it is missing. These mounts inherit nodev,noexec,nosuid
-# from the encrypted filesystem /mnt/stateful_partition/encrypted.
-mount_var_and_home_chronos() {
-  mount-encrypted "$@" >>/run/mount_encrypted/mount-encrypted.log 2>&1
-}
-
 # Give mount-encrypted umount 10 times to retry, otherwise
 # it will fail with 'device is busy' because lazy umount does not finish
 # clearing all reference points yet. Check crosbug.com/p/21345.

init/startup/chromeos_startup.cc
@@ -23,11 +23,15 @@
 #include "init/file_attrs_cleaner.h"
 #include "init/startup/chromeos_startup.h"
 #include "init/startup/constants.h"
+#include "init/startup/factory_mode_mount_helper.h"
 #include "init/startup/flags.h"
 #include "init/startup/mount_helper.h"
+#include "init/startup/mount_helper_factory.h"
 #include "init/startup/platform_impl.h"
 #include "init/startup/security_manager.h"
+#include "init/startup/standard_mount_helper.h"
 #include "init/startup/stateful_mount.h"
+#include "init/startup/test_mode_mount_helper.h"
 #include "init/utils.h"
 
 namespace {
@@ -61,6 +65,13 @@ constexpr char kInstallAttributesFile[] = "home/.shadow/install_attributes.pb";
 // operate by creating this file with the necessary arguments and then
 // rebooting.
 constexpr char kResetFile[] = "factory_install_reset";
+// Flag file indicating that mount encrypted stateful failed last time.
+// If the file is present and mount_encrypted failed again, machine would
+// enter self-repair mode.
+constexpr char kMountEncryptedFailedFile[] = "mount_encrypted_failed";
+// kEncryptedStatefulMnt stores the path to the initial mount point for
+// the encrypted stateful partition
+constexpr char kEncryptedStatefulMnt[] = "encrypted";
 
 constexpr char kDisableStatefulSecurityHard[] =
     "usr/share/cros/startup/disable_stateful_security_hardening";
@@ -491,6 +502,24 @@ int ChromeosStartup::Run() {
   StartTpm2Simulator();
 
   CleanupTpm();
+
+  base::FilePath encrypted_failed = stateful_.Append(kMountEncryptedFailedFile);
+  struct stat stbuf;
+  if (!mount_helper_->DoMountVarAndHomeChronos()) {
+    if (!platform_->Stat(encrypted_failed, &stbuf) ||
+        stbuf.st_uid != getuid()) {
+      base::WriteFile(encrypted_failed, "");
+    } else {
+      cros_system_->SetInt("recovery_request", 1);
+    }
+
+    utils::Reboot();
+    return 0;
+  }
+
+  base::DeleteFile(encrypted_failed);
+  base::FilePath encrypted_state_mnt = stateful_.Append(kEncryptedStatefulMnt);
+  mount_helper_->RememberMount(encrypted_state_mnt);
 
   int ret = RunChromeosStartupScript();
   if (ret) {

commit 069535be493e2ee93ac49bd517e2081b619c1efa

init: startup: Combine stderr/stdout when running subcommands
Use WriteFile() when log file does not exist, AppendToFile() when it
does.

Previous change (crrev.com/c/4548220) was only collecting stdout when
|mount-encrypted| is called but the interesting output is in stderr.

Use RedirectOutputToMemory(true); to make sure both files are collected.

BUG=b:288639511
TEST=On brya,
Check /run/mount_encrypted/mount-encrypted.log and
/var/log/mount-encrypted.log exits.

Fixes: commit 88384aabf9 ("Reland "init: DoMountVarAndHomeChronos")
Fixes: commit 870dacbf45 ("init: startup: switch to RedirectOutputToMemory for output")
Change-Id: I056f4207c016f2a13d323a6a8a527c65e502ce72
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4646503
Reviewed-by: Sarthak Kukreti <sarthakkukreti@google.com>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>

init/startup/mount_helper.cc
@@ -133,9 +133,12 @@ void MountHelper::MountOrFail(const base::FilePath& source,
 bool MountHelper::MountVarAndHomeChronosEncrypted() {
   base::FilePath mount_enc_log = root_.Append(kMountEncryptedLog);
   std::string output;
-  std::vector<std::string> mnt_enc_args;
-  int status = platform_->MountEncrypted(mnt_enc_args, &output);
-  base::AppendToFile(mount_enc_log, output);
+  int status = platform_->MountEncrypted(std::vector<std::string>(), &output);
+  if (base::PathExists(mount_enc_log)) {
+    base::AppendToFile(mount_enc_log, output);
+  } else {
+    base::WriteFile(mount_enc_log, output);
+  }
   return status == 0;
 }
 

init/startup/platform_impl.cc
@@ -95,7 +95,7 @@ int Platform::MountEncrypted(const std::vector<std::string>& args,
     mount_enc.AddArg(arg);
   }
   if (output) {
-    mount_enc.RedirectUsingMemory(STDOUT_FILENO);
+    mount_enc.RedirectOutputToMemory(true);
   }
 
   int status = mount_enc.Run();