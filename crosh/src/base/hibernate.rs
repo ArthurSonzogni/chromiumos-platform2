b975910a684e246a29afe68b2cc47187a0c6fb0d
Matthias Kaehlcke <mka@chromium.org>
2023-07-17 17:44:11 +0000

    crosh: Add informational message to hibernate command
    Tell the user to not press any keys or use the touchpad/mouse
    while the system is preparing for hibernation.
    
    Add support for option parsing and an option to omit the delay.
    
    BUG=b:289415578
    TEST=crosh hibernate
    
    Change-Id: I178379b46841e58498f8e54306af13730ec98c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4690807
    Commit-Queue: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Tested-by: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Allen Webb <allenwebb@google.com>

--- crosh/src/base/hibernate.rs
+++ crosh/src/base/hibernate.rs
@@ -5,13 +5,29 @@
 // Provides the command "hibernate" for crosh which tries to hibernate the
 // system (if hibernate is available and enabled).
 
+use std::io::Write;
+use std::thread;
+use std::time;
+
 use dbus::blocking::Connection;
+use getopts::{Matches, Options};
 use libchromeos::sys::error;
 
 use crate::dispatcher::{self, Arguments, Command, Dispatcher};
 use crate::util::DEFAULT_DBUS_TIMEOUT;
 
 const SUSPEND_FLAVOR_HIBERNATE: u32 = 2;
+
+// short name, long name, description, hint
+const FLAGS: [(&str, &str, &str, &str); 1] = [(
+    "n",
+    "no-delay",
+    "skip the delay before initiating the hibernation.",
+    "",
+)];
+
+#[derive(Debug, Clone)]
+struct InvalidArgumentError;
 
 pub fn register(dispatcher: &mut Dispatcher) {
     dispatcher.register_command(
@@ -22,21 +38,39 @@ pub fn register(dispatcher: &mut Dispatcher) {
   hibernate is enabled."#
                 .to_string(),
         )
-        .set_command_callback(Some(hibernate)),
+        .set_command_callback(Some(hibernate))
+        .set_help_callback(hibernate_help),
     );
 }
 
 fn hibernate(_cmd: &Command, args: &Arguments) -> Result<(), dispatcher::Error> {
-    if !args.get_args().is_empty() {
-        return Err(dispatcher::Error::CommandInvalidArguments(
-            "too many arguments".to_string(),
-        ));
-    }
+    let matches = parse_options(args).map_err(|_err| {
+        println!("{}", hibernate_usage());
+        dispatcher::Error::CommandReturnedError
+    })?;
+
+    let skip_delay = matches.opt_present("n");
 
     let connection = Connection::new_system().map_err(|err| {
         error!("ERROR: Failed to get D-Bus connection: {}", err);
         dispatcher::Error::CommandReturnedError
     })?;
+
+    // The abortion of hibernate as a result of user input is a byproduct of
+    // how suspend works on Chrome OS. When a suspend request is received
+    // powerd records the current number of wakeup events. It then prepares
+    // the system for suspend. The wakeup count is checked again before
+    // actually suspending the system, if it has increased the suspend
+    // attempt is aborted.
+    println!(
+        r#"  System will now hibernate. Please do not press any keys or use the
+  touchpad/mouse until the system has fully shutdown, otherwise the
+  system might abort hibernate."#
+    );
+
+    if !skip_delay {
+        thread::sleep(time::Duration::from_secs(2));
+    }
 
     let proxy = connection.with_proxy(
         "org.chromium.PowerManager",
@@ -56,4 +90,43 @@ fn hibernate(_cmd: &Command, args: &Arguments) -> Result<(), dispatcher::Error> 
         })?;
 
     Ok(())
+}
+
+fn parse_options(args: &Arguments) -> Result<Matches, InvalidArgumentError> {
+    let opts = get_options();
+
+    let matches = opts
+        .parse(args.get_tokens())
+        .map_err(|_err_| InvalidArgumentError)?;
+
+    if matches.free.len() != 1 {
+        return Err(InvalidArgumentError);
+    }
+
+    Ok(matches)
+}
+
+fn get_options() -> Options {
+    let mut opts = Options::new();
+
+    for flag in FLAGS.iter() {
+        if flag.3.is_empty() {
+            opts.optflag(flag.0, flag.1, flag.2);
+        } else {
+            opts.optopt(flag.0, flag.1, flag.2, flag.3);
+        }
+    }
+
+    opts
+}
+
+fn hibernate_usage() -> String {
+    let opts = get_options();
+    opts.usage("Usage: hibernate [options]")
+}
+
+fn hibernate_help(_cmd: &Command, w: &mut dyn Write, _level: usize) {
+    let usage = hibernate_usage();
+    w.write_all(usage.as_bytes()).unwrap();
+    w.flush().unwrap();
 }
