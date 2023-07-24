dee846125bfb3cec8de22ade6d3057d165c5d025
Ben Scarlato <akhna@google.com>
2023-07-24 18:07:39 +0000

    crosh: add no_new_privs message for Dev mode
    This message will display in Dev mode, and explains that sudo commands
    won't work unless the build flag login_enable_crosh_sudo is used.
    
    BUG=b:292565730
    TEST=login to crosh shell in Dev mode
    
    Change-Id: I5a05c43c960a64c8e414f64a0b83bcddb8bf2190
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4714145
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Ben Scarlato <akhna@google.com>
    Auto-Submit: Ben Scarlato <akhna@google.com>
    Tested-by: Ben Scarlato <akhna@google.com>

--- crosh/src/dev/shell.rs
+++ crosh/src/dev/shell.rs
@@ -25,6 +25,17 @@ pub fn register(dispatcher: &mut Dispatcher) {
 }
 
 fn execute_shell(_cmd: &Command, _args: &Arguments) -> Result<(), dispatcher::Error> {
+    println!(
+        r#"Sudo commands will not succeed by default.
+If you want to use sudo commands, use the VT-2 shell
+(Ctrl-Alt-{{F2/Right arrow/Refresh}}) or build the image with the
+login_enable_crosh_sudo USE flag:
+
+$ USE=login_enable_crosh_sudo emerge-$BOARD chromeos-login
+or
+$ USE=login_enable_crosh_sudo cros build-packages --board=$BOARD
+    "#
+    );
     wait_for_result(
         process::Command::new(get_shell())
             .arg("-l")
