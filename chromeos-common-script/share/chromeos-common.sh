ArthurSonzogni/chromiumos-platform2

commit 1cdc61e16312b8ac01595da47751b60ffa578084

chromeos-common-script: Reject bad rootdev in get_fixed_dst_drive()
We've repeatedly (b/292925780, b/258046147, ...) discovered that these
rootdev specifications are incorrect, and only happen to work because we
don't validate them -- we just blindly use their 'basename' to construct
a /dev/ path.

Reject bad rootdev_base specifications proactively at runtime. This will
prevent accidentally utilizing future bad rootdev_base entries.

BUG=b:217452568, b:258725140
TEST=platform.PartitionSizes

Change-Id: I59bbd5ce09c3440ac57651d5ceca63074e840d7d
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4717852
Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Brian Norris <briannorris@chromium.org>
Tested-by: Brian Norris <briannorris@chromium.org>

chromeos-common-script/share/chromeos-common.sh
@@ -365,6 +365,7 @@ get_fixed_dst_drive() {
     esac
     # No " here, the variable may contain wildcards.
     for rootdev in ${DEFAULT_ROOTDEV}; do
+      [ -f "${rootdev}" ] || continue
       dev="/dev/$(basename "${rootdev}")"
       if [ -b "${dev}" ]; then
         case "${dev}" in

commit cc6ae6750d1609fb51ce5b8535f5c94a5bf16c03

chromeos-common-script: Fix Reject bad rootdev in get_fixed_dst_drive()
Alter the test to check for directory, not regular file.

BUG=b:217452568, b:258725140, b:292925780
TEST=Check fail log, we would have:
...`
+ exp_rootdev_type=NVME
+ [ -f /sys/devices/pci0000:00/0000:00:02.2/0000:02:00.0/nvme/nvme0/nvme0n1 ]
+ continue
...
That file is a directory, so we should use `-d` to check if it exists.

Fixes: commit 1cdc61e1631 ("chromeos-common-script: Reject bad rootdev in get_fixed_dst_drive()")
Change-Id: I0d6e7fb2875d94b8b6c055b49ec8cb3cc29d14a4
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4724561
Reviewed-by: Brian Norris <briannorris@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Junichi Uekawa <uekawa@chromium.org>

chromeos-common-script/share/chromeos-common.sh
@@ -365,7 +365,7 @@ get_fixed_dst_drive() {
     esac
     # No " here, the variable may contain wildcards.
     for rootdev in ${DEFAULT_ROOTDEV}; do
-      [ -f "${rootdev}" ] || continue
+      [ -d "${rootdev}" ] || continue
       dev="/dev/$(basename "${rootdev}")"
       if [ -b "${dev}" ]; then
         case "${dev}" in