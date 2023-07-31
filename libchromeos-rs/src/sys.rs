36ff16a94788345b75fe9752c970fa24cc840117
Dennis Kempin <denniskempin@google.com>
2023-07-31 13:06:07 -0700

    Remove crosvm_base types only used by sirenia
    Sirenia has been removed, so we can remove those utilities as well.
    This reduces the amount of code we import from crosvm_base, getting
    us closer to the goal of cutting those ties.
    
    BUG=b:293488266
    TEST=cros build_packages --board=amd64-generic
    
    Change-Id: I4f8ec02c6e2d6fe8d148ebc9ed804beb86fc261b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4738743
    Commit-Queue: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Tested-by: Dennis Kempin <denniskempin@google.com>

--- libchromeos-rs/src/sys.rs
+++ libchromeos-rs/src/sys.rs
@@ -6,7 +6,6 @@
 // Note: This list is supposed to shrink over time as crosvm_base functionality is replaced with
 // third_party crates or ChromeOS specific implementations.
 // Do not add to this list.
-pub use crosvm_base::add_fd_flags;
 pub use crosvm_base::block_signal;
 pub use crosvm_base::errno_result;
 pub use crosvm_base::getpid;
@@ -21,11 +20,8 @@ pub use crosvm_base::ioctl_iowr_nr;
 pub use crosvm_base::ioctl_with_mut_ptr;
 pub use crosvm_base::ioctl_with_ptr;
 pub use crosvm_base::ioctl_with_val;
-pub use crosvm_base::net;
 pub use crosvm_base::pagesize;
 pub use crosvm_base::pipe;
-pub use crosvm_base::reap_child;
-pub use crosvm_base::round_up_to_page_size;
 pub use crosvm_base::set_rt_prio_limit;
 pub use crosvm_base::set_rt_round_robin;
 pub use crosvm_base::signal::Error as SignalError;
@@ -33,25 +29,20 @@ pub use crosvm_base::syscall;
 pub use crosvm_base::unblock_signal;
 pub use crosvm_base::unix::clear_signal_handler;
 pub use crosvm_base::unix::duration_to_timespec;
-pub use crosvm_base::unix::getsid;
 pub use crosvm_base::unix::panic_handler;
 pub use crosvm_base::unix::register_signal_handler;
-pub use crosvm_base::unix::setsid;
 pub use crosvm_base::unix::vsock;
-pub use crosvm_base::unix::wait_for_interrupt;
-pub use crosvm_base::unix::KillOnDrop;
-pub use crosvm_base::unix::Pid;
 pub use crosvm_base::unix::SharedMemory;
 pub use crosvm_base::AsRawDescriptor;
 pub use crosvm_base::Error;
 pub use crosvm_base::FromRawDescriptor;
 pub use crosvm_base::IntoRawDescriptor;
-pub use crosvm_base::MappedRegion;
-pub use crosvm_base::MemfdSeals;
-pub use crosvm_base::MemoryMapping;
-pub use crosvm_base::MemoryMappingBuilder;
 pub use crosvm_base::RawDescriptor;
 pub use crosvm_base::Result;
 pub use crosvm_base::SafeDescriptor;
 pub use crosvm_base::ScmSocket;
-pub use crosvm_base::Terminal;
+
+pub mod net {
+    // TODO: b/293488266 - still used by libcras, remove after refactoring.
+    pub use crosvm_base::net::UnixSeqpacket;
+}
