dd2ecf6caaa6061553218639c1d12bb62a14aa50
Md Sadman Sakib <sadmansakib@google.com>
2023-08-08 18:15:03 +0800

    cryptohome: use lockbox-cache script to migrate install-attributes file
    This CL migrates existing (if any) install_attributes.pb file from
    /home/.shadow/ dir to /var/lib/device_management/. This operation
    happens at boot time and before creating the verified cache copy
    of install_attributes.
    
    BUG=b:294167091
    TEST=works fine with a device with existing users
    TEST=works fine with a fresh installation
    
    Cq-Depend: chromium:4827803
    Change-Id: I8f782b157ee5cb4426f8d2c0355111cc1be2a1e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4758607
    Reviewed-by: Yi Chou <yich@google.com>
    Tested-by: Md Sadman Sakib <sadmansakib@google.com>
    Commit-Queue: Md Sadman Sakib <sadmansakib@google.com>

--- cryptohome/init/lockbox-cache.sh
+++ cryptohome/init/lockbox-cache.sh
@@ -5,13 +5,32 @@
 
 umask 022
 mkdir -p -m 0711 $LOCKBOX_CACHE_DIR
+
+# Look for the existing install attributes.
+# If there's any, move them to new path.
+# Note: this whole process is kept as much fault-tolerant as possible.
+if [ -f $OLD_INSTALL_ATTRS_FILE ]; then
+  if [ ! -f $NEW_INSTALL_ATTRS_FILE ]; then
+    # First, create a copy to the new location, then rename it.
+    # If the copy/rename operation somehow gets interrupted (sudden
+    # reboot), the old install_attributes.pb file will still be there at
+    # the next reboot.
+    # So, it will reach this step again and eventually continue from here.
+    mkdir -p $INSTALL_ATTRS_NEW_PATH
+    cp $OLD_INSTALL_ATTRS_FILE $COPY_INSTALL_ATTRS_FILE
+    mv $COPY_INSTALL_ATTRS_FILE $NEW_INSTALL_ATTRS_FILE
+  fi
+  # It's time for removal.
+  rm $OLD_INSTALL_ATTRS_FILE
+fi
+
 # /sbin/mount-encrypted emits the TPM NVRAM contents, if they exist, to a
 # file on tmpfs which is used to authenticate the lockbox during cache
 # creation.
 if [ -O $LOCKBOX_NVRAM_FILE ]; then
   lockbox-cache --cache=$INSTALL_ATTRS_CACHE \
                 --nvram=$LOCKBOX_NVRAM_FILE \
-                --lockbox=$INSTALL_ATTRS_FILE
+                --lockbox=$NEW_INSTALL_ATTRS_FILE
   # There are no other consumers; remove the nvram data
   rm $LOCKBOX_NVRAM_FILE
 fi
