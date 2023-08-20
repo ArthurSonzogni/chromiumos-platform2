8328e6d4103a9edfde7d9449e6b8501d147302a7
Darren Wu <darrenwu@google.com>
2023-08-20 08:31:10 +0800

    resourced: Add Offline SMT CPUs and offline small core by default
    The battery saver mode was offlining the small cores ans half CPUs, here to add offline
    SMT cores, and default will offline small cores in BSM.
    
    BUG=b:294161473
    TEST=cargo fmt; carge clippy --all-targets; cargo test
    TEST=Tested on rammus
    
    Change-Id: Ic209f932291ed07b8a9db5ddcb8cbf64dd6301a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4796688
    Reviewed-by: Vovo Yang <vovoy@chromium.org>
    Auto-Submit: Darren Wu <darrenwu@chromium.org>
    Commit-Queue: Darren Wu <darrenwu@chromium.org>
    Reviewed-by: Darren Wu <darrenwu@chromium.org>
    Commit-Queue: Hsin-Yi Wang <hsinyi@chromium.org>
    Tested-by: Darren Wu <darrenwu@chromium.org>
    Reviewed-by: Hsin-Yi Wang <hsinyi@chromium.org>

--- resourced/src/cpu_utils.rs
+++ resourced/src/cpu_utils.rs
@@ -2,19 +2,26 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use glob::glob;
+use std::fs::read_to_string;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
+use std::str::FromStr;
 
 use crate::common;
 
-#[derive(PartialEq, Eq)]
+const SMT_CONTROL_PATH: &str = "sys/devices/system/cpu/smt/control";
+
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub enum HotplugCpuAction {
     // Set all CPUs to online.
     OnlineAll,
-    // Offline small CPUs if the device has big/little clusters. Otherwise
-    // offline half of the CPUs if the device has >= 4 CPUs.
+    // Offline small CPUs if the device has big/little clusters.
+    OfflineSmallCore,
+    // Offline SMT cores if available.
+    OfflineSMT,
+    // Offline half of the CPUs if the device has >= 4 CPUs.
     OfflineHalf,
 }
 
@@ -131,31 +138,105 @@ fn update_cpu_online_status(root: &Path, cpus_fmt: &str, online: bool) -> Result
 
     Ok(())
 }
+// Simultaneous Multithreading(SMT) control is sysfs control interface
+// in /sys/devices/system/cpu/smt/control
+#[derive(Debug, PartialEq)]
+enum SmtControlStatus {
+    // SMT is enabled
+    On,
+    // SMT is disabled
+    Off,
+    // SMT is force disabled. Cannot be changed.
+    ForceOff,
+    // SMT is not supported by the CPU
+    NotSupported,
+    // SMT runtime toggling is not implemented for the architecture
+    NotImplemented,
+}
+
+impl FromStr for SmtControlStatus {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim_end() {
+            "on" => Ok(SmtControlStatus::On),
+            "off" => Ok(SmtControlStatus::Off),
+            "forceoff" => Ok(SmtControlStatus::ForceOff),
+            "notsupported" => Ok(SmtControlStatus::NotSupported),
+            "notimplemented" => Ok(SmtControlStatus::NotImplemented),
+            _ => bail!("Unknown Smt Control Status: '{}'", s),
+        }
+    }
+}
+
+// Checks the SMT sysfs /sys/devices/system/cpu/smt/control is able to be controlled.
+// The "on" and "off" are the two state can be controlled.
+// Returns Ok(true) when smt control is "on" or "off"
+fn is_smt_contrallable(root: &Path) -> Result<bool> {
+    let control_path = root.join(SMT_CONTROL_PATH);
+
+    if !control_path.exists() {
+        return Ok(false);
+    }
+
+    let control_string = read_to_string(&control_path)?;
+
+    match SmtControlStatus::from_str(&control_string)? {
+        SmtControlStatus::On | SmtControlStatus::Off => Ok(true),
+        _ => Ok(false),
+    }
+}
+
+// Change the SMT control state through sysfs.
+// Reference: https://docs.kernel.org/admin-guide/hw-vuln/l1tf.html#smt-control
+// * `enable`:
+//    Set true to enable SMT, which to online all CPUs. Try to access cpu online in sysfs has
+//    no restrictions.
+//    Set false to disable SMT, which to offline the non-primary CPUs. Try to set non-primary
+//    CPUs to online state in sysfs will be rejected.
+fn update_cpu_smt_control(root: &Path, enable: bool) -> Result<()> {
+    let control = root.join(SMT_CONTROL_PATH);
+
+    if is_smt_contrallable(root)? {
+        if enable {
+            std::fs::write(control, "on")?;
+        } else {
+            std::fs::write(control, "off")?;
+        }
+    }
+
+    Ok(())
+}
 
 fn hotplug_cpus_impl(root: &Path, action: HotplugCpuAction) -> Result<()> {
     match action {
         HotplugCpuAction::OnlineAll => {
             let all_cores: String = get_cpuset_all_cpus(root)?;
+            update_cpu_smt_control(root, true)?;
             update_cpu_online_status(root, &all_cores, true)?;
         }
-        HotplugCpuAction::OfflineHalf => {
+        HotplugCpuAction::OfflineSmallCore => {
             if is_big_little_supported(root)? {
                 let little_cores: String = get_little_cores(root)?;
                 update_cpu_online_status(root, &little_cores, false)?;
-            } else {
-                let num_cores: i32 = get_cpuset_all_cpus(root)?
-                    .split('-')
-                    .last()
-                    .context("can't get number of cores")?
-                    .trim()
-                    .parse()?;
-                if num_cores >= 3 {
-                    update_cpu_online_status(
-                        root,
-                        &format!("{}-{}", (num_cores / 2) + 1, num_cores),
-                        false,
-                    )?;
-                }
+            }
+        }
+        HotplugCpuAction::OfflineSMT => {
+            update_cpu_smt_control(root, false)?;
+        }
+        HotplugCpuAction::OfflineHalf => {
+            let num_cores: i32 = get_cpuset_all_cpus(root)?
+                .split('-')
+                .last()
+                .context("can't get number of cores")?
+                .trim()
+                .parse()?;
+            if num_cores >= 3 {
+                update_cpu_online_status(
+                    root,
+                    &format!("{}-{}", (num_cores / 2) + 1, num_cores),
+                    false,
+                )?;
             }
         }
     }
@@ -179,7 +260,6 @@ mod tests {
         let root_online_cpu = root.join(format!("sys/devices/system/cpu/cpu{}/online", cpu));
         test_create_parent_dir(&root_online_cpu);
         std::fs::write(root_online_cpu, value).unwrap();
-        println!("create {}", cpu)
     }
 
     fn test_check_online_cpu(root: &Path, cpu: u32, expected: &str) {
@@ -189,31 +269,188 @@ mod tests {
         assert_eq!(value, expected);
     }
 
+    fn test_check_smt_control_state(root: &Path, expected: &str) {
+        let root_smt_control = root.join(SMT_CONTROL_PATH);
+        test_create_parent_dir(&root_smt_control);
+        let value = std::fs::read_to_string(root_smt_control).unwrap();
+        assert_eq!(value, expected);
+    }
+
+    fn test_write_smt_control(root: &Path, status: &str) {
+        let smt_control = root.join(SMT_CONTROL_PATH);
+        test_create_parent_dir(&smt_control);
+        std::fs::write(smt_control, status).unwrap();
+    }
+
+    fn test_check_smt_control(root: &Path, expected: &str) {
+        let smt_control = root.join(SMT_CONTROL_PATH);
+        test_create_parent_dir(&smt_control);
+        let value = std::fs::read_to_string(smt_control).unwrap();
+        assert_eq!(value, expected);
+    }
+
     #[test]
     fn test_hotplug_cpus() {
-        // Setup.
+        struct Test<'a> {
+            cpus: &'a str,
+            big_little: bool,
+            cluster1_freq: [u32; 2],
+            cluster2_freq: [u32; 2],
+            hotplug: &'a HotplugCpuAction,
+            smt: &'a str,
+            cluster1_expected_state: [&'a str; 2],
+            cluster2_expected_state: [&'a str; 2],
+            smt_expected_state: &'a str,
+        }
+
+        let tests = [
+            Test {
+                cpus: "0-3",
+                big_little: true,
+                cluster1_freq: [2400000; 2],
+                cluster2_freq: [1800000; 2],
+                hotplug: &HotplugCpuAction::OfflineSmallCore,
+                smt: "on",
+                cluster1_expected_state: ["1"; 2],
+                cluster2_expected_state: ["0"; 2],
+                smt_expected_state: "",
+            },
+            Test {
+                cpus: "0-3",
+                big_little: false,
+                cluster1_freq: [2400000; 2],
+                cluster2_freq: [2400000; 2],
+                hotplug: &HotplugCpuAction::OfflineHalf,
+                smt: "on",
+                cluster1_expected_state: ["1"; 2],
+                cluster2_expected_state: ["0"; 2],
+                smt_expected_state: "",
+            },
+            Test {
+                cpus: "0-3",
+                big_little: false,
+                cluster1_freq: [2400000; 2],
+                cluster2_freq: [2400000; 2],
+                hotplug: &HotplugCpuAction::OfflineSMT,
+                smt: "on",
+                cluster1_expected_state: ["1"; 2],
+                cluster2_expected_state: ["0"; 2],
+                smt_expected_state: "off",
+            },
+        ];
+
+        for test in tests {
+            // Setup.
+            let root = TempDir::new().unwrap();
+            test_write_cpuset_root_cpus(root.path(), test.cpus);
+            test_write_smt_control(root.path(), test.smt);
+            if test.big_little {
+                test_write_ui_use_flags(root.path(), "big_little");
+            }
+            for (i, freq) in test.cluster1_freq.iter().enumerate() {
+                test_write_online_cpu(root.path(), i.try_into().unwrap(), "1");
+                test_write_cpu_max_freq(root.path(), i.try_into().unwrap(), *freq);
+            }
+            for (i, freq) in test.cluster2_freq.iter().enumerate() {
+                test_write_online_cpu(
+                    root.path(),
+                    (test.cluster1_freq.len() + i).try_into().unwrap(),
+                    "1",
+                );
+                test_write_cpu_max_freq(
+                    root.path(),
+                    (test.cluster1_freq.len() + i).try_into().unwrap(),
+                    *freq,
+                );
+            }
+
+            // Call function to test.
+            hotplug_cpus_impl(root.path(), *test.hotplug).unwrap();
+
+            // Check result.
+            if test.hotplug == &HotplugCpuAction::OfflineSMT {
+                // The mock sysfs cannot offline the SMT CPUs, here to check the smt control state
+                test_check_smt_control_state(root.path(), test.smt_expected_state);
+                continue;
+            }
+
+            for (i, state) in test.cluster1_expected_state.iter().enumerate() {
+                test_check_online_cpu(root.path(), i.try_into().unwrap(), state);
+            }
+
+            for (i, state) in test.cluster2_expected_state.iter().enumerate() {
+                test_check_online_cpu(
+                    root.path(),
+                    (test.cluster1_expected_state.len() + i).try_into().unwrap(),
+                    state,
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_smt_control_status() {
+        assert_eq!(
+            SmtControlStatus::from_str("on\n").unwrap(),
+            SmtControlStatus::On
+        );
+        assert_eq!(
+            SmtControlStatus::from_str("off\n").unwrap(),
+            SmtControlStatus::Off
+        );
+        assert_eq!(
+            SmtControlStatus::from_str("forceoff\n").unwrap(),
+            SmtControlStatus::ForceOff
+        );
+        assert_eq!(
+            SmtControlStatus::from_str("notsupported\n").unwrap(),
+            SmtControlStatus::NotSupported
+        );
+        assert_eq!(
+            SmtControlStatus::from_str("notimplemented\n").unwrap(),
+            SmtControlStatus::NotImplemented
+        );
+
+        assert!(SmtControlStatus::from_str("").is_err());
+        assert!(SmtControlStatus::from_str("abc").is_err());
+    }
+
+    #[test]
+    fn test_is_smt_controllable() {
         let root = TempDir::new().unwrap();
-        test_write_cpuset_root_cpus(root.path(), "0-11");
-        test_write_ui_use_flags(root.path(), "big_little");
-        for i in 0..8 {
-            test_write_online_cpu(root.path(), i, "1");
-            test_write_cpu_max_freq(root.path(), i, 2400000);
-        }
-        for i in 8..12 {
-            test_write_online_cpu(root.path(), i, "1");
-            test_write_cpu_max_freq(root.path(), i, 1800000);
-        }
 
-        // Call function to test.
-        hotplug_cpus_impl(root.path(), HotplugCpuAction::OfflineHalf).unwrap();
+        let tests = [
+            ("on", true),
+            ("off", true),
+            ("forceoff", false),
+            ("notsupported", false),
+            ("notimplemented", false),
+        ];
 
-        // Check result.
-        for i in 0..8 {
-            test_check_online_cpu(root.path(), i, "1");
+        for test in tests {
+            test_write_smt_control(root.path(), test.0);
+            let result = is_smt_contrallable(root.path()).unwrap();
+            assert_eq!(result, test.1);
         }
+    }
 
-        for i in 8..12 {
-            test_check_online_cpu(root.path(), i, "0");
+    #[test]
+    fn test_update_cpu_smt_control() {
+        let root = TempDir::new().unwrap();
+
+        let tests = [
+            ("on", true, "on"),
+            ("on", false, "off"),
+            ("off", true, "on"),
+            ("off", false, "off"),
+            ("forceoff", true, "forceoff"),
+            ("notsupported", true, "notsupported"),
+            ("notimplemented", true, "notimplemented"),
+        ];
+        for test in tests {
+            test_write_smt_control(root.path(), test.0);
+            let _ = update_cpu_smt_control(root.path(), test.1);
+            test_check_smt_control(root.path(), test.2);
         }
     }
 }
