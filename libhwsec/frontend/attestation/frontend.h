9374ca20c8d19e2966f0082377660ffe006ee836
Yi Chou <yich@google.com>
2023-08-30 17:27:16 +0800

    libhwsec: Add missing attestation description
    BUG=None
    TEST=CQ
    
    Change-Id: I9e7685116889a1d7af8b96e89ede520f30a52299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4825048
    Tested-by: Yi Chou <yich@google.com>
    Reviewed-by: Ching-Kang Yen <chingkang@chromium.org>
    Commit-Queue: Yi Chou <yich@google.com>

--- libhwsec/frontend/attestation/frontend.h
+++ libhwsec/frontend/attestation/frontend.h
@@ -25,18 +25,36 @@ class AttestationFrontend : public Frontend {
  public:
   using CreateIdentityResult = Attestation::CreateIdentityResult;
   ~AttestationFrontend() override = default;
+
+  // Unseals the |sealed_data| with current boot mode.
   virtual StatusOr<brillo::SecureBlob> Unseal(
       const brillo::Blob& sealed_data) const = 0;
+
+  // Seal the |unsealed_data| with current boot mode.
   virtual StatusOr<brillo::Blob> Seal(
       const brillo::SecureBlob& unsealed_data) const = 0;
+
+  // Quotes the |device_configs| with |key_blob|.
   virtual StatusOr<attestation::Quote> Quote(
       DeviceConfig device_config, const brillo::Blob& key_blob) const = 0;
+
+  // Checks if |quote| is valid for a single device config specified by
+  // |device_configs|.
   virtual StatusOr<bool> IsQuoted(DeviceConfig device_config,
                                   const attestation::Quote& quote) const = 0;
+
+  // Returns the current boot mode if it is valid.
   virtual StatusOr<DeviceConfigSettings::BootModeSetting::Mode>
   GetCurrentBootMode() const = 0;
+
+  // Certifies data of the |space| with the |key_blob|.
   virtual StatusOr<attestation::Quote> CertifyNV(
       RoSpace space, const brillo::Blob& key_blob) const = 0;
+
+  // Create a key with |key_type|, |key_usage|, and |restriction|, and
+  // certifies it by |identity_key| with |external_data|. When
+  // |endorsement_auth| is kEndorsementAuth, the key is created as a virtual
+  // endorsement key (vEK).
   virtual StatusOr<attestation::CertifiedKey> CreateCertifiedKey(
       const brillo::Blob& identity_key_blob,
       attestation::KeyType key_type,
@@ -44,6 +62,9 @@ class AttestationFrontend : public Frontend {
       KeyRestriction restriction,
       EndorsementAuth endorsement_auth,
       const std::string& external_data) const = 0;
+
+  // Creates identity of |key_type| type, which contains
+  // attestation::IdentityKey and attestation::IdentityBinding.
   virtual StatusOr<Attestation::CreateIdentityResult> CreateIdentity(
       attestation::KeyType key_type) const = 0;
 };
