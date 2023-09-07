ArthurSonzogni/chromiumos-platform2

commit 98c9e711cd193cb6815684fab0f4d68f05ef4c4f

libsegmentation: Add Feature requirement checks
Check if a device meets hardware requirement before setting its feature
level.

BUG=b:285392690
TEST=Check on various brya.

Change-Id: Iead7b8b0669e5cbb80e6ae7eabbe148cda6ca173
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4640455
Reviewed-by: Vovo Yang <vovoy@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>

libsegmentation/feature_management_backend.cc
@@ -7,12 +7,6 @@
 #include <optional>
 #include <string>
 
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <sys/stat.h>
-
-#include <linux/fs.h>
-
 #include <base/base64.h>
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
@@ -283,7 +277,30 @@ bool FeatureManagementImpl::Check_HW_Requirement(
     return false;
   }
 
-  // TODO(b:273339247)
+  // Feature level 1:
+  // DRAM > 8GB
+  // Obtain the size of the physical memory of the system.
+  const size_t k8GB = 8 * 1024 * 1024 * 1024ULL;
+  if (base::SysInfo::AmountOfPhysicalMemory() < k8GB)
+    return false;
+
+  // SSD > 128GB
+  // sysinfo AmountOfTotalDiskSpace can not be used, it returns the size of the
+  // underlying filesystem.
+  std::optional<base::FilePath> root_device =
+      FeatureManagementUtil::GetDefaultRoot(base::FilePath("/"));
+  if (!root_device)
+    return false;
+
+  std::optional<int64_t> size =
+      FeatureManagementUtil::GetDiskSpace(*root_device);
+  if (!size)
+    return false;
+
+  const size_t k128GB = 128 * 1024 * 1024 * 1024ULL;
+  if (*size < k128GB)
+    return false;
+
   return true;
 }
 

libsegmentation/feature_management_impl.h
@@ -86,6 +86,10 @@ class BRILLO_EXPORT FeatureManagementImpl : public FeatureManagementInterface {
   bool CacheDeviceInfo();
 
   // Check hardware requirement based on feature level
+  // Currently for feature level 1 device, we need:
+  // - 8GB of RAM
+  // - 128GB SSD
+  // See go/cros-tiering-prd for reference.
   bool Check_HW_Requirement(const DeviceSelection& selection);
 
   // Cache valid device information read from the stateful partition.

libsegmentation/feature_management_util.cc
@@ -3,15 +3,24 @@
 // found in the LICENSE file.
 
 #include <map>
+#include <glob.h>
 #include <optional>
+#include <utility>
 #include <vector>
+
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <linux/fs.h>
 
 #include <base/base64.h>
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
+#include <base/json/json_reader.h>
 #include <base/logging.h>
 #include <base/strings/string_piece.h>
 #include <base/strings/string_split.h>
+#include <base/values.h>
 #include <brillo/process/process.h>
 
 #include "libsegmentation/device_info.pb.h"
@@ -127,6 +136,102 @@ std::optional<std::string> FeatureManagementUtil::DecodeHWID(
     return std::nullopt;
 
   return decoded_bit_string;
+}
+
+std::optional<int64_t> FeatureManagementUtil::GetDiskSpace(
+    const base::FilePath& dev) {
+  base::ScopedFD fd(HANDLE_EINTR(
+      open(dev.value().c_str(), O_RDONLY | O_NOFOLLOW | O_CLOEXEC)));
+  if (!fd.is_valid()) {
+    PLOG(ERROR) << "open failed";
+    return std::nullopt;
+  }
+
+  int64_t size;
+  if (ioctl(fd.get(), BLKGETSIZE64, &size)) {
+    PLOG(ERROR) << "ioctl(BLKGETSIZE) failed";
+    return std::nullopt;
+  }
+  return size;
+}
+
+// TODO(b:176492189): Move to a common library
+// Return a dictionary of the image variables defining the partitions
+// sizes and offsets on the storage as well as globs to where it
+// can be found.
+// - root: usually '/'.unless we are unit testing.
+// - entry:
+//   `load_base_vars`: for fixed storage information
+//   'load_partition_vars': for removable [usb install image] information.
+//
+//
+std::optional<base::Value> GetPartitionVars(const base::FilePath& root,
+                                            const std::string entry) {
+  std::string json_string;
+  if (!base::ReadFileToString(root.Append("usr/sbin/partition_vars.json"),
+                              &json_string)) {
+    PLOG(ERROR) << "Unable to read json file";
+    return std::nullopt;
+  }
+  // JSON_PARSE_RFC (no override) is acceptable for produciong json,
+  // but for testing, it is more readable to add '\n' for readability.
+  auto part_vars = base::JSONReader::ReadAndReturnValueWithError(
+      json_string, base::JSON_ALLOW_CONTROL_CHARS);
+  if (!part_vars.has_value()) {
+    PLOG(ERROR) << "Failed to parse image variables.";
+    return std::nullopt;
+  }
+  if (!part_vars->is_dict()) {
+    LOG(ERROR) << "Failed to read json file as a dictionary";
+    return std::nullopt;
+  }
+
+  base::Value::Dict* image_vars = part_vars->GetDict().FindDict(entry);
+  if (image_vars == nullptr) {
+    PLOG(ERROR) << "Failed to parse dictionary from partition_vars.json";
+    return std::nullopt;
+  }
+  return base::Value(std::move(*image_vars));
+}
+
+// TODO(b:176492189): Move to a common library
+std::optional<base::FilePath> FeatureManagementUtil::GetDefaultRoot(
+    const base::FilePath& root) {
+  std::optional<base::Value> image_vars =
+      GetPartitionVars(root, "load_base_vars");
+
+  if (!image_vars) {
+    LOG(ERROR) << "Unable to find installation dictionary.";
+    return std::nullopt;
+  }
+
+  const auto& image_vars_dict = image_vars->GetDict();
+  const std::string* default_root_globs =
+      image_vars_dict.FindString("DEFAULT_ROOTDEV");
+
+  base::FilePath root_path = root.Append("dev");
+  for (const std::string& pattern :
+       base::SplitString(*default_root_globs, " ", base::TRIM_WHITESPACE,
+                         base::SPLIT_WANT_NONEMPTY)) {
+    glob_t glob_result;
+    std::optional<base::FilePath> path;
+
+    // Append root for testing purposes. The |pattern| are absolute,
+    // so we can not use Append().
+    base::FilePath pattern_under_test(root.value() + pattern);
+
+    int ret = glob(pattern_under_test.value().c_str(), GLOB_ONLYDIR,
+                   /*errfunc=*/nullptr, &glob_result);
+    if (ret == EXIT_SUCCESS && glob_result.gl_pathc == 1) {
+      base::FilePath system_root_path(glob_result.gl_pathv[0]);
+      path = root_path.Append(system_root_path.BaseName());
+    }
+
+    globfree(&glob_result);
+    if (path)
+      return path;
+  }
+  return std::nullopt;
 }
 
 }  // namespace segmentation

libsegmentation/feature_management_util.h
@@ -11,6 +11,7 @@
 #include <brillo/brillo_export.h>
 
 #include "base/files/file.h"
+#include <base/values.h>
 #include "libsegmentation/device_info.pb.h"
 #include "libsegmentation/feature_management_interface.h"
 
@@ -42,6 +43,21 @@ class BRILLO_EXPORT FeatureManagementUtil {
   // Implement base8192 decoding used by hwid.
   // Returns a string of '0' and '1' suitable for decoding.
   static std::optional<std::string> DecodeHWID(const std::string& hwid);
+
+  // Return the size of a block device.
+  // dev format is '/dev/sda', '/dev/nvme0n1', '/dev/mmcblk0', ...
+  // TODO(b:176492189): Move to a common library
+  static std::optional<int64_t> GetDiskSpace(const base::FilePath& dev);
+
+  // Find the fixed block device on the device.
+  // It may not be the device the rootfs is when we run ChromeOS from a
+  // removable device.
+  // The block device will be for example /dev/sda, /dev/mmcblk1, ...
+  // TODO(b:176492189): Move to a common library
+  // root is the usual '/', unless we are unit testing. In that case.
+  // root points to a temporary directory set up for testing.
+  static std::optional<base::FilePath> GetDefaultRoot(
+      const base::FilePath& root);
 };
 
 }  // namespace segmentation

libsegmentation/feature_management_util_test.cc
@@ -5,6 +5,9 @@
 #include <memory>
 #include <utility>
 
+#include <brillo/file_utils.h>
+#include <base/files/scoped_temp_dir.h>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
@@ -35,6 +38,55 @@ TEST_F(FeatureManagementUtilTest, DecodeHwidValid) {
   EXPECT_EQ(
       FeatureManagementUtil::DecodeHWID("REDRIX-ZZCR D3A-39F-27K-E6B").value(),
       "0001100100000110111110010111010101010100010010000001");
+}
+
+class FeatureManagementUtilRootDevTest : public ::testing::Test {
+ public:
+  FeatureManagementUtilRootDevTest() = default;
+  ~FeatureManagementUtilRootDevTest() override = default;
+
+  void SetUp() override {
+    EXPECT_TRUE(_root.CreateUniqueTempDir());
+    base::FilePath vars =
+        _root.GetPath().Append("usr/sbin/partition_vars.json");
+
+    std::string vars_content = R"""(
+{ "load_base_vars": {
+   "DEFAULT_ROOTDEV": "
+      /sys/devices/pci0000:00/0000:00:17.0/ata*/host*/target*/*/block/sd*
+      /sys/devices/pci0000:00/0000:00:1c.*/0000:*:00.0/nvme/nvme*/nvme*n1
+      /sys/devices/pci0000:00/0000:00:1a.0/mmc_host/mmc*/mmc*:000*/block/mmcblk*
+      /sys/devices/pci0000:00/0000:00:1d.*/0000:*:00.0/nvme/nvme*/nvme*n1
+      /sys/devices/pci0000:00/0000:00:06.*/0000:*:00.0/nvme/nvme*/nvme*n1
+      /sys/devices/pci0000:00/0000:00:12.7/host*/target*/*/block/sd*"
+    }
+})""";
+    ASSERT_TRUE(brillo::WriteStringToFile(vars, vars_content));
+  }
+
+ protected:
+  base::ScopedTempDir _root;
+};
+
+TEST_F(FeatureManagementUtilRootDevTest, NoDefaultRoot) {
+  // No path defined.
+  EXPECT_FALSE(FeatureManagementUtil::GetDefaultRoot(_root.GetPath()));
+}
+
+TEST_F(FeatureManagementUtilRootDevTest, OneDefaultRoot) {
+  // One good path defined.
+  EXPECT_TRUE(brillo::TouchFile(
+      _root.GetPath().Append("sys/devices/pci0000:00/0000:00:06.1/0000:03:00.0/"
+                             "nvme/nvme0/nvme0n1/size")));
+  EXPECT_TRUE(FeatureManagementUtil::GetDefaultRoot(_root.GetPath()));
+}
+
+TEST_F(FeatureManagementUtilRootDevTest, OneWrongRoot) {
+  // One path defined, but does not match any globs.
+  EXPECT_TRUE(brillo::TouchFile(
+      _root.GetPath().Append("sys/devices/pci0000:00/0000:00:06.1/0000:03:00.0/"
+                             "nvme/nvme0/nvme0n2/size")));
+  EXPECT_FALSE(FeatureManagementUtil::GetDefaultRoot(_root.GetPath()));
 }
 
 }  // namespace segmentation

commit 0adaca6d444b090d7602d4f502e45f48cf30ddc1

libsegmentation: Relax HW requirement
Current Requirements are too strict:
- 8GiB of RAM: ot all RAM is available - due to the PCI hole.
  settle for 7GiB
- 128GB of SSD: SSD controller use power of 10, and some (eMMC) can
  take a larger share. Settle for 110GiB.

Fixes: commit 98c9e711cd19 ("libsegmentation: Add Feature requirement checks")

BUG=b:285392690
TEST=Check on Taeko (8GB/128GB) and ensure the device complies.

Change-Id: I632766ffa5e87df29bbf6ce51a5dda443a6e5f6f
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4844655
Reviewed-by: Kevin Lin <kevinptt@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>

libsegmentation/feature_management_backend.cc
@@ -278,13 +278,16 @@ bool FeatureManagementImpl::Check_HW_Requirement(
   }
 
   // Feature level 1:
-  // DRAM > 8GB
+  // DRAM >= 8GiB. But since not all the physical RAM is available (PCI hole),
+  // settle for 7GiB.
   // Obtain the size of the physical memory of the system.
-  const size_t k8GB = 8 * 1024 * 1024 * 1024ULL;
-  if (base::SysInfo::AmountOfPhysicalMemory() < k8GB)
+  const size_t k7GiB = 7 * 1024 * 1024 * 1024ULL;
+  if (base::SysInfo::AmountOfPhysicalMemory() < k7GiB)
     return false;
 
-  // SSD > 128GB
+  // SSD >= 128GB
+  // But since SSD counts in power of 10 and controller may even take a bigger
+  // share, settle for 110GiB.
   // sysinfo AmountOfTotalDiskSpace can not be used, it returns the size of the
   // underlying filesystem.
   std::optional<base::FilePath> root_device =
@@ -297,8 +300,8 @@ bool FeatureManagementImpl::Check_HW_Requirement(
   if (!size)
     return false;
 
-  const size_t k128GB = 128 * 1024 * 1024 * 1024ULL;
-  if (*size < k128GB)
+  const size_t k110GiB = 110 * 1024 * 1024 * 1024ULL;
+  if (*size < k110GiB)
     return false;
 
   return true;