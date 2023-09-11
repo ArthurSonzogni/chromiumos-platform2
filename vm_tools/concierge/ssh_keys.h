514665a8ebd8e1d8514c393b2a290288b47ac8b8
Junichi Uekawa <uekawa@google.com>
2023-09-11 16:46:50 +0900

    vm_tools: concierge: Remove unused functions.
    BUG=b:267949754
    TEST=cros_workon_make --test --board=brya vm_host_tools
    
    Change-Id: Id4b957827fc168f439f5b3b610873c4d37eace08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4855869
    Reviewed-by: Takaya Saeki <takayas@chromium.org>
    Commit-Queue: Junichi Uekawa <uekawa@chromium.org>
    Reviewed-by: James Ye <jamesye@google.com>
    Tested-by: Junichi Uekawa <uekawa@chromium.org>

--- vm_tools/concierge/ssh_keys.h
+++ vm_tools/concierge/ssh_keys.h
@@ -9,32 +9,6 @@
 
 namespace vm_tools::concierge {
 
-// Gets the public key for the host (Chrome OS) for usage in the authorized
-// hosts file in a container. If the key does not exist yet it will be generated
-// as part of this call. Returns the empty string if there is any kind of
-// failure.
-std::string GetHostSshPublicKey(const std::string& cryptohome_id);
-
-// Gets the private key for the host (Chrome OS) for usage by SSH in SFTP
-// mounting. If the key does not exist yet it will be generated as part of this
-// call. Returns the empty string if there is any kind of failure.
-std::string GetHostSshPrivateKey(const std::string& cryptohome_id);
-
-// Gets the public key for the guest (container) for usage by Chrome OS in its
-// known hosts. If the key does not exist yet it will be generated as part of
-// this call. Returns the empty string if there is any kind of failure.
-std::string GetGuestSshPublicKey(const std::string& cryptohome_id,
-                                 const std::string& vm_name,
-                                 const std::string& container_name);
-
-// Gets the private key for the guest (container) for usage by the container as
-// its identity in SSH for SFTP mounting. If the key does not exist yet it will
-// be generated as part of this call. Returns the empty string if there is any
-// kind of failure.
-std::string GetGuestSshPrivateKey(const std::string& cryptohome_id,
-                                  const std::string& vm_name,
-                                  const std::string& container_name);
-
 // Erases all of the SSH keys generated for the specified |vm_name|. Should be
 // called when a VM disk image is destroyed. Returns false if there were any
 // failures deleting the keys, true otherwise.
