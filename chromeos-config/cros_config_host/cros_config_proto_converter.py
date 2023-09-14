7fda9190a6e63cdc001370c74ec0ee11ecc4a55e
Madhav <madhavadas@google.com>
2023-09-14 21:12:04 +0000

    chromeos-config: Change modem wedge timeout to string
    Fix wedge-reboot-delay-ms schema validation error.
    
    BUG=b:300160679
    TEST=./regen.sh && ./run_tests.sh
    
    Cq-Depend: chromium:4867590
    Change-Id: I100d2e36c79527d77328cfc327aad006584e310c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4867104
    Tested-by: Madhav <madhavadas@google.com>
    Commit-Queue: Madhav <madhavadas@google.com>
    Reviewed-by: Jesse McGuire <jessemcguire@google.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- chromeos-config/cros_config_host/cros_config_proto_converter.py
+++ chromeos-config/cros_config_host/cros_config_proto_converter.py
@@ -2623,9 +2623,9 @@ def _build_modem(config):
     result = {}
     result["firmware-variant"] = firmware_variant
     if hw_features.cellular.wedge_timeout_in_ms:
-        result[
-            "wedge-reboot-delay-ms"
-        ] = hw_features.cellular.wedge_timeout_in_ms
+        result["wedge-reboot-delay-ms"] = str(
+            hw_features.cellular.wedge_timeout_in_ms
+        )
 
     return result
 
