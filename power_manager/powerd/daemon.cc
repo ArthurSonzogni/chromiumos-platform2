e3be0bc48d227982e5917b2fda8529b4600409af
Hardik Goyal <hardikgoyal@google.com>
2023-09-25 11:54:56 +0100

    power: Replace base::Delete* with brillo::Delete*
    This is part of hardening work, and I'm using it as a warm up task to
    get to know powerd to implement topological resume.
    
    BUG=b:272503861
    BUG=b:293163215
    TEST=FEATURES=test emerge-dedede powerd
    
    Change-Id: I13c3a7e2095fbb0de6bf230245ca7775fcf0ad31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4887259
    Reviewed-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Auto-Submit: Hardik Goyal <hardikgoyal@chromium.org>
    Tested-by: Hardik Goyal <hardikgoyal@chromium.org>
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>

--- power_manager/powerd/daemon.cc
+++ power_manager/powerd/daemon.cc
@@ -27,6 +27,7 @@
 #include <base/task/sequenced_task_runner.h>
 #include <base/threading/platform_thread.h>
 #include <base/time/time.h>
+#include <brillo/files/file_util.h>
 #include <chromeos/dbus/service_constants.h>
 #include <chromeos/mojo/service_constants.h>
 #include <chromeos/ec/ec_commands.h>
@@ -839,7 +840,7 @@ void Daemon::SetSuspendAnnounced(bool announced) {
     if (base::WriteFile(suspend_announced_path_, nullptr, 0) < 0)
       PLOG(ERROR) << "Couldn't create " << suspend_announced_path_.value();
   } else {
-    if (!base::DeleteFile(suspend_announced_path_))
+    if (!brillo::DeleteFile(suspend_announced_path_))
       PLOG(ERROR) << "Couldn't delete " << suspend_announced_path_.value();
   }
 }
@@ -956,7 +957,7 @@ policy::Suspender::Delegate::SuspendResult Daemon::DoSuspend(
     RunSetuidHelper("eventlog_add", "--eventlog_code=0xa8", false);
 
   if (created_suspended_state_file_) {
-    if (!base::DeleteFile(base::FilePath(suspended_state_path)))
+    if (!brillo::DeleteFile(base::FilePath(suspended_state_path)))
       PLOG(ERROR) << "Failed to delete " << suspended_state_path.value();
   }
 
