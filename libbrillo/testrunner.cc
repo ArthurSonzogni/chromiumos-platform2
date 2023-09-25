e2f7adc39a15e2a836de962edaf2e8c7212f54bf
Sami Kyöstilä <skyostil@chromium.org>
2023-09-25 16:45:16 +1000

    libbrillo: Enable stack traces in unit tests
    BUG=b:298303538
    TEST=FEATURES=test emerge-brya libbrillo
    
    Change-Id: I4fdf3bb11f56c304171d3a2f9ec93bd255d0cc93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4886425
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Tested-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>

--- libbrillo/testrunner.cc
+++ libbrillo/testrunner.cc
@@ -7,11 +7,13 @@
 #include <gtest/gtest.h>
 
 #include <base/at_exit.h>
+#include <base/debug/stack_trace.h>
 #include <base/test/test_timeouts.h>
 #include <brillo/test_helpers.h>
 
 int main(int argc, char** argv) {
   base::AtExitManager at_exit_manager;
+  CHECK(base::debug::EnableInProcessStackDumping());
   SetUpTests(&argc, argv, true);
   TestTimeouts::Initialize();
   return RUN_ALL_TESTS();
