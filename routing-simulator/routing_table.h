cc90ce60f9f7dfaca634ae0cc46ef6692d42c5db
Jie Jiang <jiejiang@chromium.org>
2023-10-02 13:41:50 +0900

    routing-simulator: Add skeleton files
    Add BUILD.gn and an example class with the unit tests file.
    
    BUG=b:302635459
    TEST=FEATURES="test" emerge-$BOARD routing-simulator
    
    Cq-Depend: chromium:4905151
    Change-Id: I59362d3e70b000cb97ab42f5383a7e8f82d9c31f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4902415
    Tested-by: Jie Jiang <jiejiang@chromium.org>
    Reviewed-by: Taoyu Li <taoyl@chromium.org>
    Commit-Queue: Jie Jiang <jiejiang@chromium.org>

---
+++ routing-simulator/routing_table.h
@@ -0,0 +1,14 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ROUTING_SIMULATOR_ROUTING_TABLE_H_
+#define ROUTING_SIMULATOR_ROUTING_TABLE_H_
+
+namespace routing_simulator {
+
+class RoutingTable {};
+
+}  // namespace routing_simulator
+
+#endif  // ROUTING_SIMULATOR_ROUTING_TABLE_H_
---
+++ routing-simulator/routing_table_test.cc
@@ -0,0 +1,17 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "routing-simulator/routing_table.h"
+
+#include <gtest/gtest.h>
+
+namespace routing_simulator {
+namespace {
+
+TEST(RoutingSimulatorTest, Example) {
+  EXPECT_EQ(1, 1);
+}
+
+}  // namespace
+}  // namespace routing_simulator
