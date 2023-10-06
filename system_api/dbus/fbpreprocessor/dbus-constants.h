a14209b2c371fbe177100a229540bb0785ede1dc
Nicolas Norvez <norvez@chromium.org>
2023-10-06 23:30:38 +0000

    system_api: add D-Bus API for fbpreprocessord
    To communicate with crash-reporter and debugd, fbpreprocessord uses a
    protobuf that describes the content of a firmware dump. debugd can
    request the list of firmware dumps available on disk by querying
    fbpreprocessord's D-Bus interface, which will return that protobuf.
    
    See design docs at:
    go/cros-wifi-fw-dumps-architecture
    go/cros-fbpreprocessord-dd
    
    BUG=b:306784815
    TEST=cros-workon-${BOARD} start chromeos-base/system_api
    TEST=FEATURES=test emerge-${BOARD} chromeos-base/system_api
    
    Change-Id: Ia0812b7c67cd10ca780f4f380fd8203ec74c1052
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4919944
    Reviewed-by: David Ruth <druth@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Nicolas Norvez <norvez@chromium.org>
    Tested-by: Nicolas Norvez <norvez@chromium.org>

---
+++ system_api/dbus/fbpreprocessor/dbus-constants.h
@@ -0,0 +1,15 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SYSTEM_API_DBUS_FBPREPROCESSOR_DBUS_CONSTANTS_H_
+#define SYSTEM_API_DBUS_FBPREPROCESSOR_DBUS_CONSTANTS_H_
+
+namespace fbpreprocessor {
+
+constexpr char kFbPreprocessorServiceName[] = "org.chromium.FbPreprocessor";
+constexpr char kFbPreprocessorServicePath[] = "/org/chromium/FbPreprocessor";
+
+}  // namespace fbpreprocessor
+
+#endif  // SYSTEM_API_DBUS_FBPREPROCESSOR_DBUS_CONSTANTS_H_
