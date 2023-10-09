a11192802d7239d0bcae40def819e395d750e035
Yi Chou <yich@google.com>
2023-10-09 12:41:20 +0000

    Revert "cryptohome: Fix shell warnings in lockbox scripts"
    This reverts commit 7c43b2a8abd5daa6cd90782e9ad44ad9a15ae592.
    
    Reason for revert: b/303839666
    We should not rename the variables without changing the conf file.
    
    Original change's description:
    > cryptohome: Fix shell warnings in lockbox scripts
    >
    > BUG=b:301035575
    > TEST=works as expected after reboot
    >
    > Change-Id: I97d47ca653392d2b9ff577e074b5f1c3f489c67c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4874758
    > Tested-by: Md Sadman Sakib <sadmansakib@google.com>
    > Reviewed-by: Yi Chou <yich@google.com>
    > Commit-Queue: Md Sadman Sakib <sadmansakib@google.com>
    
    BUG=b:301035575, b:303839666
    
    Change-Id: I84240a79039a77be3faa0ad41fe3d45e306a3c6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4919004
    Tested-by: Yi Chou <yich@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Miriam Polzer <mpolzer@google.com>
    Commit-Queue: Yi Chou <yich@google.com>

--- cryptohome/init/lockbox-cache.sh
+++ cryptohome/init/lockbox-cache.sh
@@ -4,34 +4,33 @@
 # found in the LICENSE file.
 
 umask 022
-mkdir -p "${LOCKBOX_CACHE_DIR}"
-chmod 0711 "${LOCKBOX_CACHE_DIR}"
+mkdir -p -m 0711 $LOCKBOX_CACHE_DIR
 
 # Look for the existing install attributes.
 # If there's any, move them to new path.
 # Note: this whole process is kept as much fault-tolerant as possible.
-if [ -f "${OLD_INSTALL_ATTRS_FILE}" ]; then
-  if [ ! -f "${NEW_INSTALL_ATTRS_FILE}" ]; then
+if [ -f $OLD_INSTALL_ATTRS_FILE ]; then
+  if [ ! -f $NEW_INSTALL_ATTRS_FILE ]; then
     # First, create a copy to the new location, then rename it.
     # If the copy/rename operation somehow gets interrupted (sudden
     # reboot), the old install_attributes.pb file will still be there at
     # the next reboot.
     # So, it will reach this step again and eventually continue from here.
-    mkdir -p "${INSTALL_ATTRS_NEW_DIR}"
-    cp "${OLD_INSTALL_ATTRS_FILE}" "${DUP_INSTALL_ATTRS_FILE}"
-    mv "${DUP_INSTALL_ATTRS_FILE}" "${NEW_INSTALL_ATTRS_FILE}"
+    mkdir -p $INSTALL_ATTRS_NEW_PATH
+    cp $OLD_INSTALL_ATTRS_FILE $COPY_INSTALL_ATTRS_FILE
+    mv $COPY_INSTALL_ATTRS_FILE $NEW_INSTALL_ATTRS_FILE
   fi
   # It's time for removal.
-  rm "${OLD_INSTALL_ATTRS_FILE}"
+  rm $OLD_INSTALL_ATTRS_FILE
 fi
 
 # /sbin/mount-encrypted emits the TPM NVRAM contents, if they exist, to a
 # file on tmpfs which is used to authenticate the lockbox during cache
 # creation.
-if [ -O "${LOCKBOX_NVRAM_FILE}" ]; then
-  lockbox-cache --cache="${INSTALL_ATTRS_CACHE}" \
-                --nvram="${LOCKBOX_NVRAM_FILE}" \
-                --lockbox="${NEW_INSTALL_ATTRS_FILE}"
+if [ -O $LOCKBOX_NVRAM_FILE ]; then
+  lockbox-cache --cache=$INSTALL_ATTRS_CACHE \
+                --nvram=$LOCKBOX_NVRAM_FILE \
+                --lockbox=$NEW_INSTALL_ATTRS_FILE
   # There are no other consumers; remove the nvram data
-  rm "${LOCKBOX_NVRAM_FILE}"
+  rm $LOCKBOX_NVRAM_FILE
 fi
