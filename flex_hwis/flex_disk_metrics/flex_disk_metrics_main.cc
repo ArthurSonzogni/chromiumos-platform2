05d216c647aa8c5ecab10b06b20af3c704ff67ad
Nicholas Bishop <nicholasbishop@google.com>
2023-10-11 17:32:52 -0400

    flex_hwis: Implement the main binary for flex_disk_metrics
    This code delegates to the previously-implemented `GetPartitionSizeMap`
    and `SendDiskLayoutMetrics` for most of the work.
    
    The new `GetRootDiskDeviceName` is implemented via the rootdev library,
    which is added as a dependency (see also the linked chromiumos-overlay
    CL).
    
    The main function now includes the actual list of partitions for which
    to send metrics.
    
    Also enabled logging to stderr/syslog.
    
    Corresponding browser CL to define the histogram: crrev.com/c/4936033
    
    BUG=b:303112627
    TEST=emerge-reven chromeos-base/flex_hwis
    TEST=cros deploy vm chromeos-base/flex_hwis
    TEST=Run flex_disk_metrics on VM, verify it exits 0
    TEST=Check that the new metrics appear under chrome://histograms
    
    Cq-Depend: chromium:4916525
    Change-Id: Id936035d387e1e8ce50fafed1b8418b538231703
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4932845
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>
    Reviewed-by: Ting-Hao Lin <tinghaolin@google.com>

--- flex_hwis/flex_disk_metrics/flex_disk_metrics_main.cc
+++ flex_hwis/flex_disk_metrics/flex_disk_metrics_main.cc
@@ -4,6 +4,50 @@
 
 #include "flex_hwis/flex_disk_metrics/flex_disk_metrics.h"
 
+#include <base/logging.h>
+#include <brillo/syslog_logging.h>
+#include <rootdev/rootdev.h>
+
+namespace {
+
+// Get the name of the disk device the OS is running on (e.g. "sda").
+std::optional<std::string> GetRootDiskDeviceName() {
+  char path[PATH_MAX];
+  const auto ret = rootdev(path, PATH_MAX, /*full=*/true, /*strip=*/true);
+  if (ret == 0) {
+    const base::FilePath disk_path(path);
+    return disk_path.BaseName().value();
+  } else {
+    LOG(ERROR) << "Failed to get root device, error=" << ret;
+    return std::nullopt;
+  }
+}
+
+}  // namespace
+
 int main() {
-  return 1;
+  brillo::InitLog(brillo::kLogToStderr | brillo::kLogToSyslog);
+
+  const auto root_disk_device_name = GetRootDiskDeviceName();
+  if (!root_disk_device_name) {
+    return EXIT_FAILURE;
+  }
+
+  // The list of partition labels must match the variants of the
+  // `Platform.FlexPartitionSize.{Partition}` histogram:
+  // https://source.chromium.org/chromium/chromium/src/+/HEAD:tools/metrics/histograms/metadata/platform/histograms.xml
+  std::vector<std::string> partition_labels = {
+      "EFI-SYSTEM", "KERN-A", "KERN-B", "ROOT-A", "ROOT-B",
+  };
+
+  MetricsLibrary metrics;
+
+  const MapPartitionLabelToMiBSize label_to_size_map =
+      GetPartitionSizeMap(base::FilePath("/"), root_disk_device_name.value());
+
+  if (SendDiskMetrics(metrics, label_to_size_map, partition_labels)) {
+    return EXIT_SUCCESS;
+  } else {
+    return EXIT_FAILURE;
+  }
 }
