7dfcf7c9af73cd89e040a7eea320ba51742bf6b5
Nicholas Bishop <nicholasbishop@google.com>
2023-10-11 17:00:50 -0400

    flex_hwis: Stub in flex_disk_metrics source set
    This adds an empty pair of flex_disk_metrics source/header files. This
    is where most of the implementation will go, and is in a source set so
    that it can be shared between the main binary and the test binary.
    
    Update BUILD.gn to add this source set, and include the new header file
    in each of the source files.
    
    BUG=b:303112627
    TEST=cros_workon_make -breven --test chromeos-base/flex_hwis
    
    Change-Id: I9d7e33a97108e2c19a16b85f028892a09192c20f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4930665
    Reviewed-by: Ting-Hao Lin <tinghaolin@google.com>
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>

---
+++ flex_hwis/flex_disk_metrics/flex_disk_metrics.h
@@ -0,0 +1,8 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLEX_HWIS_FLEX_DISK_METRICS_FLEX_DISK_METRICS_H_
+#define FLEX_HWIS_FLEX_DISK_METRICS_FLEX_DISK_METRICS_H_
+
+#endif  // FLEX_HWIS_FLEX_DISK_METRICS_FLEX_DISK_METRICS_H_
--- flex_hwis/flex_disk_metrics/flex_disk_metrics_main.cc
+++ flex_hwis/flex_disk_metrics/flex_disk_metrics_main.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "flex_hwis/flex_disk_metrics/flex_disk_metrics.h"
+
 int main() {
   return 1;
 }
