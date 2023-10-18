a3db7d6ec560ff166e99d6a4c7e2479a27f706d0
Sam Chen <chensa@google.com>
2023-10-18 14:23:59 +0800

    hwsec-utils: Add cr50_get_name binary
    The cr50_get_name function is actually not only used by other
    cr50-scripts, so we should probably make it a stand-alone binary.
    
    BUG=b:306082740
    TEST=build hwsec-utils ok
    TEST=restart cr50-update and check /var/log/messages that it works
    
    Change-Id: I80f7e5cd4a50c6583ae1ffb77d3ee21305d86c22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4951303
    Reviewed-by: Yi Chou <yich@google.com>
    Tested-by: Sam Chen <chensa@google.com>
    Commit-Queue: Sam Chen <chensa@google.com>

---
+++ hwsec-utils/src/bin/cr50_get_name.rs
@@ -0,0 +1,16 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use hwsec_utils::context::RealContext;
+use hwsec_utils::cr50::cr50_get_name;
+
+fn main() {
+    let mut real_ctx = RealContext::new();
+    match cr50_get_name(&mut real_ctx, &["--any"]) {
+        Ok(image) => println!("{}", image),
+        Err(_) => {
+            eprintln!("Failed to find out which Cr50 imaged should be used")
+        }
+    }
+}
