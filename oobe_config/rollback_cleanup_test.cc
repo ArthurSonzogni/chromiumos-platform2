ed329db304e8cda1bfbef086fa59817979397a2a
Miriam Polzer <mpolzer@google.com>
2023-10-19 10:50:24 +0000

    oobe_config: Test hwsec rollback_cleanup
    Make sure that we zero the TPM space for rollback upon cleanup if and
    only if OOBE is completed.
    
    BUG=b:304917918
    TEST=FEATURES=test emerge-amd64-generic oobe_config
    
    Change-Id: I22ad3b1cdaf0926b36e7907ab636d88349edc3f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4952517
    Reviewed-by: Yi Chou <yich@google.com>
    Commit-Queue: Miriam Polzer <mpolzer@google.com>
    Tested-by: Miriam Polzer <mpolzer@google.com>
    Reviewed-by: Igor <igorcov@chromium.org>

--- oobe_config/rollback_cleanup_test.cc
+++ oobe_config/rollback_cleanup_test.cc
@@ -8,7 +8,9 @@
 #include <string>
 
 #include "gtest/gtest.h"
+#include "libhwsec/backend/mock_backend.h"
 #include "libhwsec/factory/tpm2_simulator_factory_for_test.h"
+#include "libhwsec/structures/space.h"
 #include "oobe_config/filesystem/file_handler_for_testing.h"
 #include "oobe_config/metrics/enterprise_rollback_metrics_handler_for_testing.h"
 
@@ -17,17 +19,30 @@ namespace {
 const base::Version kDeviceVersionM108("15183.1.2");
 const base::Version kTargetVersionM107("15117.0.0");
 
+constexpr uint32_t kRollbackSpaceIndex = 0x100e;
+constexpr uint32_t kRollbackSpaceSize = 32;
+
 // Random data to write to rollback files.
 const std::string data(128, 0x66);
 }  // namespace
 
 class RollbackCleanupTest : public testing::Test {
  protected:
-  void SetUp() override { file_handler_.CreateDefaultExistingPaths(); }
+  void SetUp() override {
+    file_handler_.CreateDefaultExistingPaths();
+    hwsec_oobe_config_ = hwsec_factory_.GetOobeConfigFrontend();
+  }
+
+  bool CreateRollbackSpace() {
+    return hwsec_factory_.GetFakeTpmNvramForTest().DefinePlatformCreateSpace(
+               kRollbackSpaceIndex, kRollbackSpaceSize) &&
+           hwsec_oobe_config_->IsRollbackSpaceReady().ok();
+  }
 
   FileHandlerForTesting file_handler_;
   EnterpriseRollbackMetricsHandlerForTesting metrics_handler_;
   hwsec::Tpm2SimulatorFactoryForTest hwsec_factory_;
+  std::unique_ptr<const hwsec::OobeConfigFrontend> hwsec_oobe_config_;
 };
 
 TEST_F(RollbackCleanupTest, DoNotRemoveAnyRollbackDataIfOobeIsNotCompleted) {
@@ -53,6 +68,29 @@ TEST_F(RollbackCleanupTest, RemoveRollbackDataIfOobeIsCompleted) {
   EXPECT_FALSE(file_handler_.HasOpensslEncryptedRollbackData());
   EXPECT_FALSE(file_handler_.HasTpmEncryptedRollbackData());
   EXPECT_FALSE(file_handler_.HasDecryptedRollbackData());
+}
+
+TEST_F(RollbackCleanupTest, ClearTpmSpaceIfOobeIsCompleted) {
+  EXPECT_TRUE(CreateRollbackSpace());
+  EXPECT_TRUE(file_handler_.CreateOobeCompletedFlag());
+
+  const brillo::Blob zero(kRollbackSpaceSize);
+  EXPECT_CALL(hwsec_factory_.GetMockBackend().GetMock().storage,
+              Store(hwsec::Space::kEnterpriseRollback, zero))
+      .Times(1);
+
+  RollbackCleanup(&file_handler_, &metrics_handler_, &hwsec_factory_);
+}
+
+TEST_F(RollbackCleanupTest, DoNotClearTpmSpaceIfOobeIsNotCompleted) {
+  EXPECT_TRUE(CreateRollbackSpace());
+
+  const brillo::Blob zero(kRollbackSpaceSize);
+  EXPECT_CALL(hwsec_factory_.GetMockBackend().GetMock().storage,
+              Store(hwsec::Space::kEnterpriseRollback, zero))
+      .Times(0);
+
+  RollbackCleanup(&file_handler_, &metrics_handler_, &hwsec_factory_);
 }
 
 TEST_F(RollbackCleanupTest,
