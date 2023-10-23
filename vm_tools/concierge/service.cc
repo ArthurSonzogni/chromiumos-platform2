d4b3f02bb10f0baab63ef095c02b00ca0b84ecd8
Momoko Hattori <momohatt@google.com>
2023-10-23 13:01:50 +0900

    vm_tools: concierge: Allow empty owner_id in StopVm D-Bus method
    This CL changes StopVm to allow empty owner_id in StopVmRequest so that
    session_manager can call it without passing owner_id in CL:4863022.
    The owner_id filed will be removed from StopVmRequest in b/305120263.
    
    BUG=b:290194650
    TEST=With CL:4863022, check that ARCVM stops (by looking at
    TEST=/var/log/messages) after StopVm is called by login_manager.
    
    Change-Id: Ibf99bbe311512860dbc94acb659685b33968fbfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4964042
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
    Tested-by: Momoko Hattori <momohatt@chromium.org>
    Commit-Queue: Momoko Hattori <momohatt@chromium.org>

--- vm_tools/concierge/service.cc
+++ vm_tools/concierge/service.cc
@@ -2399,9 +2399,19 @@ StartVmResponse Service::StartVmInternal(
   return response;
 }
 
-StopVmResponse Service::StopVm(const StopVmRequest& request) {
+StopVmResponse Service::StopVm(const StopVmRequest& request_original) {
   LOG(INFO) << "Received request: " << __func__;
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  // TODO(b/307732864): Support generating mutable arguments in D-Bus bindings
+  // and remove this copying.
+  StopVmRequest request(request_original);
+
+  // Allow accepting empty owner_id until its deprecation is done.
+  // TODO(b/305120263): Remove owner_id from StopVmRequest.
+  if (request_original.owner_id().empty()) {
+    request.set_owner_id(primary_owner_id_);
+  }
 
   StopVmResponse response;
 
