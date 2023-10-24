7d6f28f0f67cbc291cdcbb7488abff6e9c317d53
snamika <snamika@google.com>
2023-10-24 06:13:36 +0000

    routing-simulator: Add an executable for the project
    Add main.cc to log test texts to check if it works on DUT.
    Implementation will be added later.
    Add an executable section in BUILD.gn to create an executable for the project.
    
    BUG=b:307460180
    TEST=FEATURES=”test” emerge-octopus routing-simulator
    
    Change-Id: Id023d1f7c19916e8974aef4dc8e3bcc676aa0f8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4970917
    Commit-Queue: Namika Sakaban <snamika@google.com>
    Tested-by: Namika Sakaban <snamika@google.com>
    Reviewed-by: Jie Jiang <jiejiang@chromium.org>

---
+++ routing-simulator/main.cc
@@ -0,0 +1,11 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <base/logging.h>
+
+int main() {
+  LOG(INFO) << "hello";
+  // TODO(b/307460180): Add implementation later.
+  return 0;
+}
