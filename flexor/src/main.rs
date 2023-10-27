fca370bf5c64538ee7c98603d78af659d59e9d2d
Bastian Kersting <bkersting@google.com>
2023-10-27 08:34:16 +0000

    flexor: Add logging and switch to anyhow
    This change adds logging via the stderrlog crate and
    replaces the errorhandling with thiserror with anyhow.
    
    BUG=b:307486255
    TEST=emerge-reven flexor
    
    Cq-Depend: chromium:4979967
    Change-Id: I4b535a27c13d500f8b80efa8bda1283765404d06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4983828
    Reviewed-by: Nicholas Bishop <nicholasbishop@google.com>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Tested-by: Bastian Kersting <bkersting@google.com>

--- flexor/src/main.rs
+++ flexor/src/main.rs
@@ -2,15 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use error::Result;
+use anyhow::Result;
 use libchromeos::panic_handler;
+use log::info;
 
-mod error;
+static FLEXOR_TAG: &'static str = "flexor";
 
 fn main() -> Result<()> {
-    // Setup the panic handler and logging.
+    // Setup the panic handler.
     panic_handler::install_memfd_handler();
-    println!("Hello from Flexor!");
+    // For now log everything.
+    log::set_max_level(log::LevelFilter::Info);
+    if let Err(err) = stderrlog::new()
+        .module(FLEXOR_TAG)
+        .show_module_names(true)
+        .verbosity(log::max_level())
+        .init()
+    {
+        eprintln!("Failed to initialize logger: {err}");
+    }
+
+    info!("Hello from Flexor!");
 
     Ok(())
 }
