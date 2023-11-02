6447da1c94f547539a1be459f10d9f0fb228f83d
Ting-Hao Lin <tinghaolin@google.com>
2023-11-02 19:14:24 +0000

    flex_hwis: handle optional data from cros_healthd
    The hardware data in flex_hwis is retrieved from
    the cros_healthd service. Some data fields from
    cors_healthd are optional and should be handled
    to avoid causing errors. For example, the reven-vmtest
    board does not have did_vid data under the tpm
    information. In this case, the field will not be
    assigned.
    
    BUG=b:308981024
    TEST=emerge-reven-vmtest chromeos-base/flex_hwis
    TEST=cros deploy vm chromeos-base/flex_hwis
    TEST=tast -verbose run vm platform.FlexHWIS
    
    Change-Id: I1fea288374aa44910124bb7ce1185145da28c1fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5002255
    Tested-by: Ting-Hao Lin <tinghaolin@google.com>
    Commit-Queue: Ting-Hao Lin <tinghaolin@google.com>
    Reviewed-by: Nicholas Bishop <nicholasbishop@google.com>

--- flex_hwis/flex_hwis_mojo.cc
+++ flex_hwis/flex_hwis_mojo.cc
@@ -100,71 +100,103 @@ std::string int_to_hex(uint16_t i) {
 }  // namespace
 
 void FlexHwisMojo::SetSystemInfo(hwis_proto::Device* data) {
+  LOG(INFO) << "Set system telemetry info";
   const auto& system_result = telemetry_info_->system_result;
-  if (system_result && system_result->is_system_info()) {
-    const auto& system_info = system_result->get_system_info();
+  if (!system_result || !system_result->is_system_info()) {
+    LOG(WARNING) << "No system telemetry info from cros_healthd service.";
+    return;
+  }
+
+  const auto& system_info = system_result->get_system_info();
+  if (system_info->os_info) {
     const mojom::BootMode boot_mode = system_info->os_info->boot_mode;
-    data->mutable_dmi_info()->set_vendor(
-        system_info->dmi_info->sys_vendor.value());
-    data->mutable_dmi_info()->set_product_name(
-        system_info->dmi_info->product_name.value());
-    data->mutable_dmi_info()->set_product_version(
-        system_info->dmi_info->product_version.value());
-    data->mutable_bios()->set_bios_version(
-        system_info->dmi_info->bios_version.value());
     data->mutable_bios()->set_secureboot(boot_mode ==
                                          mojom::BootMode::kCrosEfiSecure);
     data->mutable_bios()->set_uefi(boot_mode ==
                                        mojom::BootMode::kCrosEfiSecure ||
                                    boot_mode == mojom::BootMode::kCrosEfi);
-  } else {
-    LOG(INFO) << "No system telemetry info from cros_healthd service.";
+  }
+  if (system_info->dmi_info) {
+    if (system_info->dmi_info->sys_vendor.has_value()) {
+      data->mutable_dmi_info()->set_vendor(
+          system_info->dmi_info->sys_vendor.value());
+    }
+    if (system_info->dmi_info->product_name.has_value()) {
+      data->mutable_dmi_info()->set_product_name(
+          system_info->dmi_info->product_name.value());
+    }
+    if (system_info->dmi_info->product_version.has_value()) {
+      data->mutable_dmi_info()->set_product_version(
+          system_info->dmi_info->product_version.value());
+    }
+    if (system_info->dmi_info->bios_version.has_value()) {
+      data->mutable_bios()->set_bios_version(
+          system_info->dmi_info->bios_version.value());
+    }
   }
 }
 
 void FlexHwisMojo::SetCpuInfo(hwis_proto::Device* data) {
+  LOG(INFO) << "Set cpu telemetry info.";
   const auto& cpu_result = telemetry_info_->cpu_result;
-  if (cpu_result && cpu_result->is_cpu_info()) {
-    const auto& cpu_info = cpu_result->get_cpu_info();
-    for (const auto& physical_cpu : cpu_info->physical_cpus) {
-      hwis_proto::Device_Cpu* cpu = data->add_cpu();
+  if (!cpu_result || !cpu_result->is_cpu_info()) {
+    LOG(WARNING) << "No cpu telemetry info from cros_healthd service.";
+    return;
+  }
+
+  const auto& cpu_info = cpu_result->get_cpu_info();
+  for (const auto& physical_cpu : cpu_info->physical_cpus) {
+    hwis_proto::Device_Cpu* cpu = data->add_cpu();
+    if (physical_cpu->model_name.has_value()) {
       cpu->set_name(physical_cpu->model_name.value());
     }
-  } else {
-    LOG(INFO) << "No CPU telemetry info from cros_healthd service.";
   }
 }
 
 void FlexHwisMojo::SetMemoryInfo(hwis_proto::Device* data) {
+  LOG(INFO) << "Set memory telemetry info.";
   const auto& memory_result = telemetry_info_->memory_result;
-  if (memory_result && memory_result->is_memory_info()) {
-    const auto& memory_info = memory_result->get_memory_info();
-    data->mutable_memory()->set_total_kib(memory_info->total_memory_kib);
-  } else {
-    LOG(INFO) << "No memory telemetry info from cros_healthd service.";
+  if (!memory_result || !memory_result->is_memory_info()) {
+    LOG(WARNING) << "No memory telemetry info from cros_healthd service.";
+    return;
   }
+
+  const auto& memory_info = memory_result->get_memory_info();
+  data->mutable_memory()->set_total_kib(memory_info->total_memory_kib);
 }
 
 template <class T>
 void FlexHwisMojo::SetDeviceInfo(const mojom::BusDevicePtr& device,
                                  const T& controller) {
   controller->set_name(device->vendor_name + " " + device->product_name);
+  if (!device->bus_info) {
+    return;
+  }
+
   switch (device->bus_info->which()) {
     case mojom::BusInfo::Tag::kPciBusInfo: {
       const auto& pci_info = device->bus_info->get_pci_bus_info();
-      controller->set_id("pci:" + int_to_hex(pci_info->vendor_id) + ":" +
-                         int_to_hex(pci_info->device_id));
       controller->set_bus(hwis_proto::Device::PCI);
-      controller->add_driver(pci_info->driver.value());
+      if (pci_info) {
+        controller->set_id("pci:" + int_to_hex(pci_info->vendor_id) + ":" +
+                           int_to_hex(pci_info->device_id));
+        if (pci_info->driver.has_value()) {
+          controller->add_driver(pci_info->driver.value());
+        }
+      }
       break;
     }
     case mojom::BusInfo::Tag::kUsbBusInfo: {
       const auto& usb_info = device->bus_info->get_usb_bus_info();
-      controller->set_id("usb:" + int_to_hex(usb_info->vendor_id) + ":" +
-                         int_to_hex(usb_info->product_id));
       controller->set_bus(hwis_proto::Device::USB);
-      for (const auto& interface : usb_info->interfaces) {
-        controller->add_driver(interface->driver.value());
+      if (usb_info) {
+        controller->set_id("usb:" + int_to_hex(usb_info->vendor_id) + ":" +
+                           int_to_hex(usb_info->product_id));
+        for (const auto& interface : usb_info->interfaces) {
+          if (interface->driver.has_value()) {
+            controller->add_driver(interface->driver.value());
+          }
+        }
       }
       break;
     }
@@ -178,40 +210,52 @@ void FlexHwisMojo::SetDeviceInfo(const mojom::BusDevicePtr& device,
 }
 
 void FlexHwisMojo::SetBusInfo(hwis_proto::Device* data) {
+  LOG(INFO) << "Set bus telemetry info.";
   const auto& bus_result = telemetry_info_->bus_result;
-  if (bus_result && bus_result->is_bus_devices()) {
-    const auto& devices = bus_result->get_bus_devices();
-    for (const auto& device : devices) {
-      switch (device->device_class) {
-        case mojom::BusDeviceClass::kEthernetController: {
-          SetDeviceInfo(device, data->add_ethernet_adapter());
-          break;
-        }
-        case mojom::BusDeviceClass::kWirelessController: {
-          SetDeviceInfo(device, data->add_wireless_adapter());
-          break;
-        }
-        case mojom::BusDeviceClass::kBluetoothAdapter: {
-          SetDeviceInfo(device, data->add_bluetooth_adapter());
-          break;
-        }
-        case mojom::BusDeviceClass::kDisplayController: {
-          SetDeviceInfo(device, data->add_gpu());
-          break;
-        }
-        default:
-          break;
-      }
+  if (!bus_result || !bus_result->is_bus_devices()) {
+    LOG(WARNING) << "No bus telemetry info from cros_healthd service.";
+    return;
+  }
+
+  const auto& devices = bus_result->get_bus_devices();
+  for (const auto& device : devices) {
+    if (!device) {
+      continue;
     }
-  } else {
-    LOG(INFO) << "No BUS telemetry info from cros_healthd service.";
+
+    switch (device->device_class) {
+      case mojom::BusDeviceClass::kEthernetController: {
+        SetDeviceInfo(device, data->add_ethernet_adapter());
+        break;
+      }
+      case mojom::BusDeviceClass::kWirelessController: {
+        SetDeviceInfo(device, data->add_wireless_adapter());
+        break;
+      }
+      case mojom::BusDeviceClass::kBluetoothAdapter: {
+        SetDeviceInfo(device, data->add_bluetooth_adapter());
+        break;
+      }
+      case mojom::BusDeviceClass::kDisplayController: {
+        SetDeviceInfo(device, data->add_gpu());
+        break;
+      }
+      default:
+        break;
+    }
   }
 }
 
 void FlexHwisMojo::SetGraphicInfo(hwis_proto::Device* data) {
+  LOG(INFO) << "Set graphics telemetry info.";
   const auto& graphics_result = telemetry_info_->graphics_result;
-  if (graphics_result && graphics_result->is_graphics_info()) {
-    const auto& graphics_info = graphics_result->get_graphics_info();
+  if (!graphics_result || !graphics_result->is_graphics_info()) {
+    LOG(WARNING) << "No graphics telemetry info from cros_healthd service.";
+    return;
+  }
+
+  const auto& graphics_info = graphics_result->get_graphics_info();
+  if (graphics_info->gles_info) {
     data->mutable_graphics_info()->set_gl_version(
         graphics_info->gles_info->version);
     data->mutable_graphics_info()->set_gl_shading_version(
@@ -224,43 +268,52 @@ void FlexHwisMojo::SetGraphicInfo(hwis_proto::Device* data) {
     for (const auto& extension : graphics_info->gles_info->extensions) {
       data->mutable_graphics_info()->add_gl_extensions(extension);
     }
-  } else {
-    LOG(INFO) << "No Graphics telemetry info from cros_healthd service.";
   }
 }
 
 void FlexHwisMojo::SetInputInfo(hwis_proto::Device* data) {
+  LOG(INFO) << "Set input telemetry info.";
   const auto& input_result = telemetry_info_->input_result;
-  if (input_result && input_result->is_input_info()) {
-    const auto& input_info = input_result->get_input_info();
-    data->mutable_touchpad()->set_stack(input_info->touchpad_library_name);
-  } else {
-    LOG(INFO) << "No input telemetry info from cros_healthd service.";
+  if (!input_result || !input_result->is_input_info()) {
+    LOG(WARNING) << "No input telemetry info from cros_healthd service.";
+    return;
   }
+
+  const auto& input_info = input_result->get_input_info();
+  data->mutable_touchpad()->set_stack(input_info->touchpad_library_name);
 }
 
 void FlexHwisMojo::SetTpmInfo(hwis_proto::Device* data) {
+  LOG(INFO) << "Set tpm telemetry info.";
   const auto& tpm_result = telemetry_info_->tpm_result;
+  if (!tpm_result || !tpm_result->is_tpm_info()) {
+    LOG(WARNING) << "No tpm telemetry info from cros_healthd service.";
+    return;
+  }
+
   // ChromeOS Flex supports only certain TPM 1.2 and TPM 2.0 chipsets.
   std::map<int, std::string> tpm_version_map = {{0x312e3200, "1.2"},
                                                 {0x322e3000, "2.0"}};
-  if (tpm_result && tpm_result->is_tpm_info()) {
-    const auto& tpm_info = tpm_result->get_tpm_info();
-
-    std::string version = "Unknown";
+  std::string version = "Unknown";
+  const auto& tpm_info = tpm_result->get_tpm_info();
+  if (tpm_info->version) {
     if (tpm_version_map.find(tpm_info->version->family) !=
         tpm_version_map.end()) {
       version = tpm_version_map[tpm_info->version->family];
     }
-    data->mutable_tpm()->set_tpm_version(version);
     data->mutable_tpm()->set_spec_level(tpm_info->version->spec_level);
     data->mutable_tpm()->set_manufacturer(tpm_info->version->manufacturer);
-    data->mutable_tpm()->set_did_vid(tpm_info->did_vid.value());
+  }
+  data->mutable_tpm()->set_tpm_version(version);
+  if (tpm_info->supported_features) {
     data->mutable_tpm()->set_tpm_allow_listed(
         tpm_info->supported_features->is_allowed);
+  }
+  if (tpm_info->status) {
     data->mutable_tpm()->set_tpm_owned(tpm_info->status->owned);
-  } else {
-    LOG(INFO) << "No tpm telemetry info from cros_healthd service.";
+  }
+  if (tpm_info->did_vid.has_value()) {
+    data->mutable_tpm()->set_did_vid(tpm_info->did_vid.value());
   }
 }
 
