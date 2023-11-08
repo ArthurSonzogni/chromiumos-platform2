d61975367be04b6ce1f7ee115e03a4793ce9cb54
George Burgess IV <gbiv@google.com>
2023-11-08 11:56:29 -0700

    net-base: add default values to Options struct
    Clang will soon start warning about code like:
    
    ```
    auto options = Options {
      .timeout = 2,
      .number_of_tries = 1,
    };
    ```
    
    because named field initializers are missing, and
    `-Wmissing-field-initializers` is on by default. Having explicit
    defaults in the `Options` struct itself suppresses these warnings, while
    offering no functional change.
    
    BUG=b:305723283
    TEST=FEATURES=test emerge-hana net-base
    
    Change-Id: I35042f7046481bee7777cb583109b5c29109e757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5014385
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Tested-by: George Burgess <gbiv@chromium.org>
    Reviewed-by: Taoyu Li <taoyl@chromium.org>

--- net-base/dns_client.h
+++ net-base/dns_client.h
@@ -98,23 +98,23 @@ class NET_BASE_EXPORT DNSClient {
 
     // Maximum number of attempts to each name server. The value set in
     // resolv.conf will be used if not set.
-    std::optional<int> number_of_tries;
+    std::optional<int> number_of_tries = std::nullopt;
 
     // The timeout value for the first try to each name server. The value set in
     // resolv.conf will be used if not set. The timeout for the following tries
     // will be controlled by the c-ares library. For more details, see comments
     // for ARES_OPT_TIMEOUTMS in https://c-ares.org/ares_init_options.html .
-    std::optional<base::TimeDelta> per_query_initial_timeout;
+    std::optional<base::TimeDelta> per_query_initial_timeout = std::nullopt;
 
     // If not empty, the query will be bound to this interface. Note that the
     // program needs CAP_NET_RAW to set this option.
-    std::string interface;
+    std::string interface = "";
 
     // The name server used for the query. The name servers in resolv.conf will
     // be used if this option in empty. Only support one name server here by
     // intention. The caller should create one DNSClient for each name server to
     // query multiple server.
-    std::optional<IPAddress> name_server;
+    std::optional<IPAddress> name_server = std::nullopt;
   };
 
   DNSClient() = default;
