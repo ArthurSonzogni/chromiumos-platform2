558a6a67dae2762cff597603d996c5f191d1d9d0
Raul Rangel <rrangel@chromium.org>
2023-11-08 18:05:13 +0000

    Revert "common-mk: Make meson_test.py a standalone script"
    This reverts commit 11b95eadc716bf326c5f65ceccefe9364c2f99d6.
    
    Reason for revert: b/309793528
    
    Original change's description:
    > common-mk: Make meson_test.py a standalone script
    >
    > meson_test.py is specified as the exe_wrapper for meson-based packages,
    > which lets Meson invoke target binaries always via the script, including
    > the configure phase and the test phase.
    >
    > Since some basic third-party packages depended on by primordial packages
    > use meson (e.g. sys-libs/libcxx -> dev-util/cmake -> dev-libs/jsoncpp),
    > anything meson_test.py depends on will be placed at the bottom of the
    > dependency graph, which will trigger massive rebuilds under Bazel when
    > they are updated. So it is important to minimize the dependencies of
    > meson_test.py.
    >
    > This patch converts meson_test.py to a simple stand-alone Python script
    > that calls proot(1) for foreign-architecture binary execution.
    >
    > BUG=b:272420127
    > TEST=CQ
    >
    > Change-Id: Ia50e1239bdd45a22ae28330244a7d7e32d608f25
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4991402
    > Tested-by: Shuhei Takahashi <nya@chromium.org>
    > Reviewed-by: Raul Rangel <rrangel@chromium.org>
    > Reviewed-by: Tim Bain <tbain@google.com>
    > Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    
    BUG=b:272420127, b:309793528
    
    Change-Id: I1c4a4f9ef9635bf544a748fb734da0cb9f25d076
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5013229
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Commit-Queue: Raul Rangel <rrangel@chromium.org>
    Commit-Queue: Allen Webb <allenwebb@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Tim Bain <tbain@google.com>
    Auto-Submit: Raul Rangel <rrangel@chromium.org>
    Tested-by: Raul Rangel <rrangel@chromium.org>

--- common-mk/meson_test.py
+++ common-mk/meson_test.py
@@ -3,118 +3,72 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Meson's exe_wrapper to execute foreign-architecture binaries."""
+"""Wrapper for platform2_test for meson packages.
+
+This mostly sets some environment variables that can't easily be set
+in meson.eclass.
+"""
 
 import os
+from pathlib import Path
 import sys
-from typing import Dict, List, Mapping, NoReturn
 
 
-# Several meson packages pass the path to the build or source
-# directory into their tests using an environment variable so they
-# can read data files or execute other test programs. These paths
-# will be from the perspective of the cros chroot, not from the
-# build sysroot that the test is actually going to run from. We
-# can translate these paths by just stripping the ${SYSROOT}
-# prefix from the start.
-#
-# Unfortunately there doesn't seem to be any standard approach to
-# this, so we just have to look at all the env variables we have
-# observed being used in this way.
-_PATH_ENV_VARS = (
-    # Used by dev-libs/wayland
-    "TEST_BUILD_DIR",
-    "TEST_SRC_DIR",
-    # Used by media-libs/harfbuzz, dev-libs/json-glib, app-arch/gcab,
-    # dev-libs/glib
-    "G_TEST_BUILDDIR",
-    "G_TEST_SRCDIR",
-    # Used by x11-libs/libxkbcommon
-    "top_builddir",
-    "top_srcdir",
-)
+def translate_path(sysroot, path):
+    """Remove the SYSROOT prefix from paths that have it"""
 
-
-def _maybe_escape_sandbox(argv: List[str]) -> None:
-    """Re-executes itself if necessary to escape from the Portage sandbox."""
-    if os.environ.get("SANDBOX_ON") != "1":
-        return
-
-    os.environ["SANDBOX_ON"] = "0"
-    os.environ.pop("LD_PRELOAD")
-    os.execvp(argv[0], argv)
-
-
-def _detect_qemu_arch(sysroot: str) -> str:
-    """Detects the architecture of the sysroot."""
-    if os.path.lexists(os.path.join(sysroot, "lib64/ld-linux-x86-64.so.2")):
-        return "x86_64"
-    if os.path.lexists(os.path.join(sysroot, "lib64/ld-linux-aarch64.so.1")):
-        return "aarch64"
-    if os.path.lexists(os.path.join(sysroot, "lib/ld-linux-armhf.so.3")):
-        return "arm"
-    raise RuntimeError(f"Unsupported arch: {sysroot}")
-
-
-def _translate_path(path: str, sysroot: str) -> str:
-    """Remove the SYSROOT prefix from paths that have it."""
-    if path == sysroot:
-        return "/"
     if path.startswith(sysroot):
         return path[len(sysroot) :]
     return path
 
 
-def _translate_paths_in_env(
-    orig_env: Mapping[str, str], sysroot: str
-) -> Dict[str, str]:
-    """Translates paths contained in environment variables."""
-    new_env = {}
-    for key, value in orig_env.items():
-        if key in _PATH_ENV_VARS:
-            value = _translate_path(value, sysroot)
-        new_env[key] = value
-    return new_env
+def append_env_path(real_argv, sysroot, env_name):
+    """Translate the path in |env_name| and add it to |real_argv|"""
+
+    if env_name in os.environ:
+        translated_path = translate_path(sysroot, os.environ[env_name])
+        real_argv += ["--env", f"{env_name}={translated_path}"]
 
 
-def main(argv: List[str]) -> NoReturn:
-    # This script MUST NOT depend on chromite as it's used to build primordial
-    # third-party packages and thus introduces deep dependencies to chromite.
-    assert "chromite" not in sys.modules
+def main(argv):
+    DIR = Path(__file__).resolve().parent
+    real_argv = [DIR / "platform2_test.py"]
 
-    # TODO: Don't escape from the sandbox. We do it for a historical reason:
-    # this script used to call into platform2_test.py which unconditionally
-    # escaped from the sandbox. We should instead add necessary file paths to
-    # the sandbox allowlist and re-enable the sandbox.
-    _maybe_escape_sandbox(argv)
+    real_argv += ["--strategy=unprivileged", "--user=root", "--bind-mount-dev"]
 
-    sysroot = os.environ.get("SYSROOT")
-    if not sysroot:
-        raise RuntimeError("$SYSROOT is not set")
-    if not sysroot.startswith("/"):
-        raise RuntimeError("$SYSROOT must be absolute")
-    if sysroot == "/":
-        raise RuntimeError("$SYSROOT must not be /")
-    sysroot = sysroot.rstrip("/")
+    sysroot = os.environ["SYSROOT"]
+    real_argv += ["--sysroot", sysroot]
 
-    qemu_arch = _detect_qemu_arch(sysroot)
+    # Several meson packages pass the path to the build or source
+    # directory into their tests using an environment variable so they
+    # can read data files or execute other test programs. These paths
+    # will be from the perspective of the cros chroot, not from the
+    # build sysroot that the test is actually going to run from. We
+    # can translate these paths by just stripping the ${SYSROOT}
+    # prefix from the start.
+    #
+    # Unfortunately there doesn't seem to be any standard approach to
+    # this, so we just have to look at all the env variables we have
+    # observed being used in this way.
 
-    new_args = argv[1:]
-    if not new_args:
-        raise RuntimeError("Needs arguments")
-    new_args[0] = _translate_path(new_args[0], sysroot)
-    new_cwd = _translate_path(os.getcwd(), sysroot)
+    # Used by dev-libs/wayland
+    append_env_path(real_argv, sysroot, "TEST_BUILD_DIR")
+    append_env_path(real_argv, sysroot, "TEST_SRC_DIR")
 
-    exec_args = [
-        "proot",
-        f"--rootfs={sysroot}",
-        f"--qemu=qemu-{qemu_arch}",
-        f"--cwd={new_cwd}",
-    ] + new_args
-    exec_env = _translate_paths_in_env(os.environ, sysroot)
+    # Used by media-libs/harfbuzz, dev-libs/json-glib, app-arch/gcab,
+    # dev-libs/glib
+    append_env_path(real_argv, sysroot, "G_TEST_BUILDDIR")
+    append_env_path(real_argv, sysroot, "G_TEST_SRCDIR")
 
-    os.execvpe(exec_args[0], exec_args, exec_env)
+    # Used by x11-libs/libxkbcommon
+    append_env_path(real_argv, sysroot, "top_builddir")
+    append_env_path(real_argv, sysroot, "top_srcdir")
+
+    real_argv += ["--"]
+    real_argv += argv
+
+    os.execv(real_argv[0], real_argv)
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    sys.exit(main(sys.argv[1:]))
