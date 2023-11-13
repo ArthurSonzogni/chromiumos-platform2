6f3b7a98f54b6ddaef9a0599bab0de27d1a1c701
Yichen Yu <yichenyu@google.com>
2023-11-13 20:48:49 +0000

    fbpreprocessor: make log level configurable through start
    We are going to add the VLOG mechanism that allows us to do local
    debugging. This CL makes the log level configurable through an env
    variable from the start command.
    
    BUG=b:307593542,b:308219814
    TEST=emerge-${BOARD} fbpreprocessord
    TEST=cros deploy ${DUT} fbpreprocessord
    TEST=start fbpreprocessord LOG_LEVEL=-4 (on DUT)
    
    Change-Id: Iafe4d69fe229004006fae42ab108a06f8fd49ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5026245
    Tested-by: Yichen Yu <yichenyu@google.com>
    Reviewed-by: Nicolas Norvez <norvez@chromium.org>
    Auto-Submit: Yichen Yu <yichenyu@google.com>
    Commit-Queue: Yichen Yu <yichenyu@google.com>

--- fbpreprocessor/main.cc
+++ fbpreprocessor/main.cc
@@ -10,8 +10,30 @@
 #include "fbpreprocessor/configuration.h"
 #include "fbpreprocessor/fbpreprocessor_daemon.h"
 
+namespace {
+constexpr int kMaxLogLevel = 4;
+constexpr int kMinLogLevel = -4;
+
+void ValidateAndSetMinLogLevel(int log_level) {
+  if (log_level > kMaxLogLevel || log_level < kMinLogLevel) {
+    LOG(ERROR) << "Log level (" << log_level << ") exceeds the valid range. "
+               << "Keep as the system default level.";
+    return;
+  }
+  LOG(INFO) << "Setting log level as " << log_level;
+  if (log_level > logging::LOGGING_INFO) {
+    LOG(WARNING) << "Log level larger than INFO. This is unusual and please "
+                    "make sure the correct log_level is configured.";
+  }
+  logging::SetMinLogLevel(log_level);
+}
+}  // namespace
+
 int main(int argc, char* argv[]) {
   DEFINE_string(log_dir, "/var/log/", "Directory where logs are written.");
+  DEFINE_int32(log_level, logging::LOGGING_INFO,
+               "Logging level - 0: LOG(INFO), 1: LOG(WARNING), 2: LOG(ERROR), "
+               "-1: VLOG(1), -2: VLOG(2), ...");
   DEFINE_uint32(file_expiration,
                 fbpreprocessor::Configuration::kDefaultExpirationSeconds,
                 "Default expiration period of processed files, in seconds.");
@@ -29,6 +51,7 @@ int main(int argc, char* argv[]) {
   logging::InitLogging(logging_settings);
 
   LOG(INFO) << "Starting fbpreprocessord.";
+  ValidateAndSetMinLogLevel(FLAGS_log_level);
 
   fbpreprocessor::Configuration config;
   int file_expiration = FLAGS_file_expiration;
