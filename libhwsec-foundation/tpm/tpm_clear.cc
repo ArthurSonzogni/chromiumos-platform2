6b236174721c0e022ef9155297bc60a5c6227a78
Yi Chou <yich@google.com>
2023-11-20 14:58:55 +0800

    libhwsec-foundation: Add TPM clear library
    These functions support clearing TPM across different system.
    
    BUG=b:311323718
    TEST=CQ
    
    Cq-Depend: chromium:5039697
    Change-Id: I5b83668852611a0c8639892478c0aa35f17537f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5043666
    Tested-by: Yi Chou <yich@google.com>
    Reviewed-by: Ching-Kang Yen <chingkang@chromium.org>
    Commit-Queue: Yi Chou <yich@google.com>

---
+++ libhwsec-foundation/tpm/tpm_clear.cc
@@ -0,0 +1,171 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "libhwsec-foundation/tpm/tpm_clear.h"
+
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
+#include <base/logging.h>
+#include <base/strings/string_split.h>
+#include <base/strings/string_util.h>
+#include <brillo/files/file_util.h>
+#include <libcrossystem/crossystem.h>
+
+namespace {
+
+#if USE_TPM2_SIMULATOR
+
+constexpr char kNVChipPath[] =
+    "/mnt/stateful_partition/unencrypted/tpm2-simulator/NVChip";
+
+#elif USE_TPM_DYNAMIC
+
+constexpr char kTpmPpiPath[] = "/sys/class/tpm/tpm0/ppi/request";
+constexpr char kTpmTcgOpPath[] = "/sys/class/tpm/tpm0/ppi/tcg_operations";
+constexpr int kTpmPpiNothingId = 0;
+constexpr int kTpmPpiClearId = 22;
+
+bool WriteStringToFile(const base::FilePath& filename,
+                       const std::string& data) {
+  int result = base::WriteFile(filename, data.data(), data.size());
+  return (result != -1 && static_cast<size_t>(result) == data.size());
+}
+
+std::map<int, int> GetTpmTcgOpMap() {
+  std::string data;
+  if (!base::ReadFileToString(base::FilePath(kTpmTcgOpPath), &data)) {
+    return {};
+  }
+
+  std::map<int, int> result;
+  std::vector<std::string> lines = base::SplitString(
+      data, "\n", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
+  for (const std::string& line : lines) {
+    int id = 0;
+    int value = 0;
+    if (sscanf(line.c_str(), "%d %d: ", &id, &value) != 2) {
+      continue;
+    }
+    result.insert({id, value});
+  }
+
+  return result;
+}
+
+#endif
+
+}  // namespace
+
+namespace hwsec_foundation::tpm {
+
+#if USE_TPM2_SIMULATOR
+
+bool SupportClearRequest() {
+  return true;
+}
+
+bool SupportClearWithoutPrompt() {
+  return true;
+}
+
+bool SetClearTpmRequest(bool value) {
+  // We don't support to set "Clear TPM Request" back to false on VM.
+  if (value == false) {
+    return false;
+  }
+
+  return brillo::DeleteFile(base::FilePath(kNVChipPath));
+}
+
+std::optional<bool> GetClearTpmRequest() {
+  return !base::PathExists(base::FilePath(kNVChipPath));
+}
+
+#elif USE_TPM_DYNAMIC
+
+bool SupportClearRequest() {
+  std::map<int, int> op_map = GetTpmTcgOpMap();
+  int op_value = op_map[kTpmPpiClearId];
+  return op_value == 3 || op_value == 4;
+}
+
+bool SupportClearWithoutPrompt() {
+  std::map<int, int> op_map = GetTpmTcgOpMap();
+  int op_value = op_map[kTpmPpiClearId];
+  return op_value == 4;
+}
+
+bool SetClearTpmRequest(bool value) {
+  if (value == true && !SupportClearWithoutPrompt()) {
+    return false;
+  }
+
+  std::string ppi_id =
+      value ? std::to_string(kTpmPpiClearId) : std::to_string(kTpmPpiNothingId);
+
+  if (!WriteStringToFile(base::FilePath(kTpmPpiPath), ppi_id)) {
+    return false;
+  }
+
+  return true;
+}
+
+std::optional<bool> GetClearTpmRequest() {
+  if (!SupportClearWithoutPrompt()) {
+    return std::nullopt;
+  }
+
+  std::string data;
+  if (!base::ReadFileToString(base::FilePath(kTpmPpiPath), &data)) {
+    return std::nullopt;
+  }
+
+  auto request = base::TrimWhitespaceASCII(data, base::TRIM_ALL);
+
+  if (request == std::to_string(kTpmPpiClearId)) {
+    return true;
+  }
+
+  if (request == std::to_string(kTpmPpiNothingId)) {
+    return false;
+  }
+
+  return std::nullopt;
+}
+
+#else
+
+bool SupportClearRequest() {
+  return true;
+}
+
+bool SupportClearWithoutPrompt() {
+  return true;
+}
+
+bool SetClearTpmRequest(bool value) {
+  auto crossystem = std::make_unique<crossystem::Crossystem>();
+  return crossystem->VbSetSystemPropertyInt(
+      crossystem::Crossystem::kClearTpmOwnerRequest, static_cast<int>(value));
+}
+
+std::optional<bool> GetClearTpmRequest() {
+  auto crossystem = std::make_unique<crossystem::Crossystem>();
+  std::optional<int> result = crossystem->VbGetSystemPropertyInt(
+      crossystem::Crossystem::kClearTpmOwnerRequest);
+  if (!result.has_value()) {
+    return std::nullopt;
+  }
+  return *result;
+}
+
+#endif
+
+}  // namespace hwsec_foundation::tpm
---
+++ libhwsec-foundation/tpm/tpm_clear.h
@@ -0,0 +1,30 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIBHWSEC_FOUNDATION_TPM_TPM_CLEAR_H_
+#define LIBHWSEC_FOUNDATION_TPM_TPM_CLEAR_H_
+
+#include <optional>
+
+#include "libhwsec-foundation/hwsec-foundation_export.h"
+
+// These functions support clearing TPM across different system.
+
+namespace hwsec_foundation::tpm {
+
+// Checks the device support TPM clear request or not.
+HWSEC_FOUNDATION_EXPORT bool SupportClearRequest();
+
+// Checks the device support TPM clear request without user prompt or not.
+HWSEC_FOUNDATION_EXPORT bool SupportClearWithoutPrompt();
+
+// Sets the value of "Clear TPM Request" to |value|.
+HWSEC_FOUNDATION_EXPORT bool SetClearTpmRequest(bool value);
+
+// Gets the current value of "Clear TPM Request".
+HWSEC_FOUNDATION_EXPORT std::optional<bool> GetClearTpmRequest();
+
+}  // namespace hwsec_foundation::tpm
+
+#endif  // LIBHWSEC_FOUNDATION_TPM_TPM_CLEAR_H_
