dddba29f1f35f55cf9692ef9c5ae282508ffe8cc
Ryo Hashimoto <hashimoto@google.com>
2023-11-21 13:54:44 +0900

    secagentd: Stop including string_piece_forward.h
    This header is being removed.
    
    BUG=b:309658031
    TEST=emerge-amd64-generic secagentd
    
    Change-Id: I943918739a725ccbb6d2a69484249bcc453845ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5048551
    Tested-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Jason Ling <jasonling@chromium.org>

--- secagentd/process_cache.cc
+++ secagentd/process_cache.cc
@@ -30,7 +30,7 @@
 #include "base/memory/scoped_refptr.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/strings/string_piece_forward.h"
+#include "base/strings/string_piece.h"
 #include "base/strings/string_tokenizer.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
