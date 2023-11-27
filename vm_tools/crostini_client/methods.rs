ArthurSonzogni/chromiumos-platform2

commit 2923eb8b7dc4f6a4bd66cbd334a859c66e3d55af

vm_tools: Replace usages to INSTALL_METHOD for DLC
.. as INSTALL_DLC_METHOD is removed now.

BUG=b:218396905
TEST=CQ

Change-Id: I1dfe623810c97be94d527b545b24341e4fb243c9
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4972304
Reviewed-by: Joel Hockey <joelhockey@chromium.org>
Tested-by: Jae Hoon Kim <kimjae@chromium.org>
Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>
Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>

vm_tools/crostini_client/methods.rs
@@ -602,7 +602,7 @@ impl Methods {
             DLC_SERVICE_SERVICE_NAME,
             DLC_SERVICE_SERVICE_PATH,
             DLC_SERVICE_INTERFACE,
-            INSTALL_DLC_METHOD,
+            INSTALL_METHOD,
         )?
         .append1(name);
 

commit da59a6fb1ad6a8e81d36cb18529bdaaa8d8e38f2

vm_tools: crostini_client: fix DLC Install message
The dlcservice Install method accepts an InstallRequest proto message,
but the vmc code was sending it a string.

This was broken when migrating from InstallDlc to Install in
crrev.com/c/4972304

BUG=b:312768614
TEST=Start bru with vmc when termina-dlc is not yet installed

Fixes: 2923eb8b7dc4 ("vm_tools: Replace usages to INSTALL_METHOD for DLC")
Change-Id: Ib158f50ed740abe1ce224a6e6ef363b6fc7bd057
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5055608
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Joel Hockey <joelhockey@chromium.org>
Tested-by: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Jae Hoon Kim <kimjae@chromium.org>

vm_tools/crostini_client/methods.rs
@@ -598,13 +598,16 @@ impl Methods {
     }
 
     fn init_dlc_install(&mut self, name: &str) -> Result<(), Box<dyn Error>> {
+        let mut install_request = dlcservice::InstallRequest::new();
+        install_request.id = name.to_owned();
+
         let method = Message::new_method_call(
             DLC_SERVICE_SERVICE_NAME,
             DLC_SERVICE_SERVICE_PATH,
             DLC_SERVICE_INTERFACE,
             INSTALL_METHOD,
         )?
-        .append1(name);
+        .append1(install_request.write_to_bytes()?);
 
         self.connection
             .send_with_reply_and_block(method, DEFAULT_TIMEOUT)