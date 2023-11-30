a215f342eef771d59f5d71d32e2558fd4d132832
Yi Chou <yich@google.com>
2023-11-30 12:54:12 +0800

    cryprohome: Improve log and stability for lockbox-cache
    Add sync after every file system operations to make sure the file had
    been write to the disk correctly. This might slow down the boot time,
    but this migration will only happen once after the OS update.
    
    And add more log into the syslog, so we will be able to investigate the
    error further.
    
    BUG=b:313661664
    TEST=tast run $DUT hwsec.CrossVersionInstallAttributes.*
    TEST=tast run $DUT hwsec.InstallAttributes
    
    Change-Id: I29a25bc5c00e3ca9c8f46206b1f751bd7845172c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5075896
    Tested-by: Yi Chou <yich@google.com>
    Reviewed-by: Ching-Kang Yen <chingkang@chromium.org>
    Reviewed-by: Md Sadman Sakib <sadmansakib@google.com>

--- cryptohome/init/lockbox-cache.sh
+++ cryptohome/init/lockbox-cache.sh
@@ -3,31 +3,45 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+echo "Lockbox cache started"
+
 umask 022
 mkdir -p -m 0711 $LOCKBOX_CACHE_DIR
+
+# Exit the script if any command fails.
+set -e
 
 # Look for the existing install attributes.
 # If there's any, move them to new path.
 # Note: this whole process is kept as much fault-tolerant as possible.
-if [ -f $OLD_INSTALL_ATTRS_FILE ]; then
-  if [ ! -f $NEW_INSTALL_ATTRS_FILE ]; then
+if [ -s $OLD_INSTALL_ATTRS_FILE ]; then
+  if [ ! -s $NEW_INSTALL_ATTRS_FILE ]; then
+    echo "Migrating install attributes"
     # First, create a copy to the new location, then rename it.
     # If the copy/rename operation somehow gets interrupted (sudden
     # reboot), the old install_attributes.pb file will still be there at
     # the next reboot.
     # So, it will reach this step again and eventually continue from here.
     mkdir -p $INSTALL_ATTRS_NEW_PATH
+    sync
     cp $OLD_INSTALL_ATTRS_FILE $COPY_INSTALL_ATTRS_FILE
+    sync
     mv $COPY_INSTALL_ATTRS_FILE $NEW_INSTALL_ATTRS_FILE
+    sync
   fi
-  # It's time for removal.
-  rm $OLD_INSTALL_ATTRS_FILE
+  if [ -s $NEW_INSTALL_ATTRS_FILE ]; then
+    echo "Removing stale install attributes"
+    # It's time for removal.
+    rm $OLD_INSTALL_ATTRS_FILE
+    sync
+  fi
 fi
 
 # /sbin/mount-encrypted emits the TPM NVRAM contents, if they exist, to a
 # file on tmpfs which is used to authenticate the lockbox during cache
 # creation.
 if [ -O $LOCKBOX_NVRAM_FILE ]; then
+  echo "Validating lockbox"
   lockbox-cache --cache=$INSTALL_ATTRS_CACHE \
                 --nvram=$LOCKBOX_NVRAM_FILE \
                 --lockbox=$NEW_INSTALL_ATTRS_FILE
