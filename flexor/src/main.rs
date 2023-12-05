f2b1bc00b76e4f243e0c19796a77b9467d06799d
Bastian Kersting <bkersting@google.com>
2023-12-05 20:23:41 +0000

    flexor: Switch logging to syslog
    This change switches the current `stderrlog` logging
    to the ChromeOS official libchromeos::syslog behaviour.
    This means flexor's logs will now end up in the usual
    /var/log/messages folder.
    
    Furthermore, this adds behaviour for an installation
    error and tries to safe the logs to disk.
    
    BUG=b:315010092
    TEST=use build_flexor to build and image and deploy it to a device,
    connect via serial console. Create an error case and then either mount
    the data partition (e.g. /dev/nvme0n1p4) or the flex deploy partition
    (e.g. /dev/nvme0n1p13), depending on when your produced error happened,
    now check for a file "messages" on the mounted partition and verify
    that it is equivalent to /var/log/messages of the running Flexor.
    
    Change-Id: I73dfc4608465487b32c4b910196db750ab83bf1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5088871
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Reviewed-by: Nicholas Bishop <nicholasbishop@google.com>
    Tested-by: Bastian Kersting <bkersting@google.com>

--- flexor/src/main.rs
+++ flexor/src/main.rs
@@ -6,8 +6,8 @@ use std::path::Path;
 
 use anyhow::{Context, Result};
 use gpt_disk_types::BlockSize;
-use libchromeos::panic_handler;
-use log::info;
+use libchromeos::{panic_handler, syslog};
+use log::{error, info};
 use nix::sys::reboot::reboot;
 
 mod cgpt;
@@ -20,6 +20,7 @@ mod util;
 
 const FLEXOR_TAG: &str = "flexor";
 const FLEX_IMAGE_FILENAME: &str = "flex_image.tar.xz";
+const FLEXOR_LOG_FILE: &str = "/var/log/messages";
 const DATA_PART_NUM: u32 = 4;
 const FLEX_DEPLOY_PART_NUM: u32 = 13;
 
@@ -82,37 +83,85 @@ fn setup_flex_deploy_partition_and_install(device_path: &Path) -> Result<()> {
     )
 }
 
-fn main() -> Result<()> {
-    // Setup the panic handler.
-    panic_handler::install_memfd_handler();
-    // For now log everything.
-    log::set_max_level(log::LevelFilter::Info);
-    if let Err(err) = stderrlog::new()
-        .module(FLEXOR_TAG)
-        .show_module_names(true)
-        .verbosity(log::max_level())
-        .init()
+// Performs the actual installation of ChromeOS.
+fn perform_installation(device_path: &Path) -> Result<()> {
+    info!("Start Flex-ing");
+    copy_image_to_rootfs(device_path)?;
+
+    info!("Setting up the disk");
+    // Assuming a block size of 512, like all of ChromeOS.
+    setup_disk(device_path, BlockSize::BS_512)?;
+
+    info!("Setting up the new partition and installing ChromeOS Flex");
+    setup_flex_deploy_partition_and_install(device_path)
+}
+
+/// Tries to save logs to the disk depending on what state the installation fails in.
+/// We basically have two option:
+/// 1. Either we are in the state before the disk was reformatted, in that case we write
+///    the logs back to the partition that also has the installation payload.
+/// 2. Otherwise we hope to already have the Flex layout including the FLEX_DEPLOY partition
+///    in that case we write the logs to that partition (may need to create a filesystem on that
+///    partition though).
+fn try_safe_logs(device_path: &Path) -> Result<()> {
+    // Case 1: The data partition still exists, so we write the logs to it.
+    if let Some(data_partition_path) =
+        libchromeos::disk::get_partition_device(device_path, DATA_PART_NUM)
     {
+        if matches!(data_partition_path.try_exists(), Ok(true)) {
+            let data_mount = mount::Mount::mount_by_path(data_partition_path, mount::FsType::Vfat)?;
+            std::fs::copy(FLEXOR_LOG_FILE, data_mount.mount_path())?;
+            return Ok(());
+        }
+    }
+
+    let flex_depl_partition_path =
+        libchromeos::disk::get_partition_device(device_path, FLEX_DEPLOY_PART_NUM)
+            .context("Error finding a place to write logs to")?;
+
+    // Case 2: We already have the Flex layout and can try to write to the FLEX_DEPLOY partition.
+    if let Ok(true) = flex_depl_partition_path.try_exists() {
+        match mount::Mount::mount_by_path(&flex_depl_partition_path, mount::FsType::EXT4) {
+            Ok(flex_depl_mount) => {
+                std::fs::copy(FLEXOR_LOG_FILE, flex_depl_mount.mount_path())?;
+            }
+            Err(_) => {
+                // The partition seems to exist, but we can't mount it as ext4,
+                // so we try to create a file system and retry.
+                util::mkfs_ext4(&flex_depl_partition_path)?;
+                let flex_depl_mount =
+                    mount::Mount::mount_by_path(&flex_depl_partition_path, mount::FsType::EXT4)?;
+                std::fs::copy(FLEXOR_LOG_FILE, flex_depl_mount.mount_path())?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    // Setup the panic handler and logging.
+    panic_handler::install_memfd_handler();
+    if let Err(err) = syslog::init(FLEXOR_TAG.to_owned(), /*log_to_stderr=*/ true) {
         eprintln!("Failed to initialize logger: {err}");
     }
 
     info!("Hello from Flexor!");
-    let device_path = disk::get_target_device()?;
+    let device_path = disk::get_target_device().context("Error getting the target disk")?;
 
-    // Assuming a block size of 512, like all of ChromeOS.
-    let block_size = BlockSize::BS_512;
-
-    info!("Start Flex-ing");
-    copy_image_to_rootfs(&device_path)?;
-
-    info!("Setting up the disk");
-    setup_disk(&device_path, block_size)?;
-
-    info!("Setting up the new partition and installing ChromeOS Flex");
-    setup_flex_deploy_partition_and_install(&device_path)?;
-
-    info!("Rebooting into ChromeOS Flex, keep fingers crossed");
-    reboot(nix::sys::reboot::RebootMode::RB_AUTOBOOT)?;
-
-    Ok(())
+    match perform_installation(&device_path) {
+        Ok(_) => {
+            info!("Rebooting into ChromeOS Flex, keep fingers crossed");
+            reboot(nix::sys::reboot::RebootMode::RB_AUTOBOOT)
+                .context("Unable to reboot after successful installation")?;
+            Ok(())
+        }
+        Err(err) => {
+            error!("Flexor couldn't complete due to error: {err}");
+            // Try to save logs if possible, otherwise just be stuck.
+            let _ = try_safe_logs(&device_path);
+            // TODO(b/314965086): Add an error screen displaying the log.
+            Ok(())
+        }
+    }
 }
