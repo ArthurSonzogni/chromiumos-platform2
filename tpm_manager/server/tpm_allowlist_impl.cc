4414b094fd8bb2f27b330fd9ce72383fa1b05595
Jeffery Miller <jefferymiller@google.com>
2023-12-08 17:36:11 -0600

    tpm_manager: Extend TPM allow list
    Add Latitude 5510 to the list.
    
    BUG=b:311270384
    TEST=tast run dut hwsec.* u2fd.* cryptohome.*
    
    Change-Id: Ifbaa069fd0f8faebe2c3c179a66c30190aa602d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5106623
    Tested-by: Jeffery Miller <jefferymiller@google.com>
    Reviewed-by: Yi Chou <yich@google.com>
    Auto-Submit: Jeffery Miller <jefferymiller@google.com>
    Commit-Queue: Jeffery Miller <jefferymiller@google.com>

--- tpm_manager/server/tpm_allowlist_impl.cc
+++ tpm_manager/server/tpm_allowlist_impl.cc
@@ -82,6 +82,7 @@ constexpr DeviceName kTpm2DeviceNameAllowlist[] = {
     DeviceName{"HP", "HP EliteBook 640 14 inch G10 Notebook PC", kVendorIdNtc},
     DeviceName{"HP", "HP EliteBook 645 14 inch G10 Notebook PC", kVendorIdNtc},
     DeviceName{"Dell Inc.", "Latitude 7490", kVendorIdWinbond},
+    DeviceName{"Dell Inc.", "Latitude 5510", kVendorIdStm},
 };
 
 std::optional<bool> IsTpmFileEnabled() {
