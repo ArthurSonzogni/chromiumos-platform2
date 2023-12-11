4bea328d4b34533b26bdfe3bf67431dd5933f766
Yi Chou <yich@google.com>
2023-12-11 17:22:08 +0800

    vtpm: Daemonize the vtpmd
    The will prevent the upstart sending the SIGTERM signal before we spawn
    the task.
    
    BUG=b:315742907
    TEST=CQ
    
    Change-Id: I331e50ecda7ac1ec3a2f7ca2d040b9cab6ba22a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5109415
    Tested-by: Yi Chou <yich@google.com>
    Reviewed-by: Howard Yang <hcyang@google.com>
    Commit-Queue: Yi Chou <yich@google.com>

--- vtpm/main.cc
+++ vtpm/main.cc
@@ -1,8 +1,10 @@
 // Copyright 2022 The ChromiumOS Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #include <signal.h>
 #include <sysexits.h>
+#include <unistd.h>
 
 #include <base/command_line.h>
 #include <brillo/syslog_logging.h>
@@ -23,6 +25,8 @@ void MaskSignals() {
 }
 
 int main(int argc, char* argv[]) {
+  PLOG_IF(FATAL, daemon(0, 0) == -1) << "Failed to daemonize";
+
   base::CommandLine::Init(argc, argv);
   base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
   int flags = brillo::kLogToSyslog;
