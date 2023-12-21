c4a6a9b3bf8fc92addebb7a3171735026a9eb5a4
Byron Lee <byronlee@google.com>
2023-12-21 10:11:35 +0000

    diagnostics: Remove usages of base::RunLoop for executor utils
    BUG=b:275056694
    TEST=FEATURES=test emerge-volteer diagnostics
    TEST=CQ
    
    Change-Id: Ie1348bab75d86a05b25c8ea8625c79f8f217eded
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5144707
    Reviewed-by: Wei-Luan Wang <weiluanwang@google.com>
    Tested-by: Byron Lee <byronlee@chromium.org>
    Commit-Queue: Byron Lee <byronlee@chromium.org>

--- diagnostics/cros_healthd/executor/utils/scoped_process_control_test.cc
+++ diagnostics/cros_healthd/executor/utils/scoped_process_control_test.cc
@@ -5,9 +5,8 @@
 #include "diagnostics/cros_healthd/executor/utils/scoped_process_control.h"
 
 #include <base/functional/callback_helpers.h>
-#include <base/run_loop.h>
-#include <base/test/bind.h>
 #include <base/test/task_environment.h>
+#include <base/test/test_future.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <mojo/public/cpp/bindings/callback_helpers.h>
@@ -36,27 +35,24 @@ class ScopedProcessControlTest : public testing::Test {
 };
 
 TEST_F(ScopedProcessControlTest, RunOneCallbackOnOutOfScope) {
-  base::RunLoop run_loop;
+  base::test::TestFuture<void> future;
   {
     ScopedProcessControl scoped_process_control;
     fake_process_control_.BindReceiver(
         scoped_process_control.BindNewPipeAndPassReceiver());
     scoped_process_control.AddOnTerminateCallback(
-        base::ScopedClosureRunner(run_loop.QuitClosure()));
+        base::ScopedClosureRunner(future.GetCallback()));
   }
-  run_loop.Run();
+  EXPECT_TRUE(future.Wait());
   EXPECT_EQ(fake_process_control_.return_code(), 143);
   fake_process_control_.receiver().FlushForTesting();
   EXPECT_FALSE(fake_process_control_.IsConnected());
 }
 
 TEST_F(ScopedProcessControlTest, RunMultipleCallbacksOnOutOfScope) {
-  base::RunLoop run_loop;
+  base::test::TestFuture<bool> future;
   {
-    CallbackBarrier barrier(base::BindLambdaForTesting([&](bool success) {
-      run_loop.Quit();
-      EXPECT_TRUE(success);
-    }));
+    CallbackBarrier barrier(future.GetCallback());
     ScopedProcessControl scoped_process_control;
     fake_process_control_.BindReceiver(
         scoped_process_control.BindNewPipeAndPassReceiver());
@@ -67,33 +63,33 @@ TEST_F(ScopedProcessControlTest, RunMultipleCallbacksOnOutOfScope) {
     scoped_process_control.AddOnTerminateCallback(
         base::ScopedClosureRunner(barrier.CreateDependencyClosure()));
   }
-  run_loop.Run();
+  EXPECT_TRUE(future.Get());
   EXPECT_EQ(fake_process_control_.return_code(), 143);
   fake_process_control_.receiver().FlushForTesting();
   EXPECT_FALSE(fake_process_control_.IsConnected());
 }
 
 TEST_F(ScopedProcessControlTest, RunAllCallbacksOnReset) {
-  base::RunLoop run_loop;
+  base::test::TestFuture<void> future;
   ScopedProcessControl scoped_process_control;
   fake_process_control_.BindReceiver(
       scoped_process_control.BindNewPipeAndPassReceiver());
   scoped_process_control.AddOnTerminateCallback(
-      base::ScopedClosureRunner(run_loop.QuitClosure()));
+      base::ScopedClosureRunner(future.GetCallback()));
   scoped_process_control.Reset();
-  run_loop.Run();
+  EXPECT_TRUE(future.Wait());
   EXPECT_EQ(fake_process_control_.return_code(), 143);
   fake_process_control_.receiver().FlushForTesting();
   EXPECT_FALSE(fake_process_control_.IsConnected());
 }
 
 TEST_F(ScopedProcessControlTest, ResetSuccessfullyIfNoRemoteBound) {
-  base::RunLoop run_loop;
+  base::test::TestFuture<void> future;
   ScopedProcessControl scoped_process_control;
   scoped_process_control.AddOnTerminateCallback(
-      base::ScopedClosureRunner(run_loop.QuitClosure()));
+      base::ScopedClosureRunner(future.GetCallback()));
   scoped_process_control.Reset();
-  run_loop.Run();
+  EXPECT_TRUE(future.Wait());
 }
 
 TEST_F(ScopedProcessControlTest, AddCallbackAfterCallbacksCalled) {
@@ -101,19 +97,19 @@ TEST_F(ScopedProcessControlTest, AddCallbackAfterCallbacksCalled) {
   fake_process_control_.BindReceiver(
       scoped_process_control.BindNewPipeAndPassReceiver());
   {
-    base::RunLoop run_loop;
+    base::test::TestFuture<void> future;
     scoped_process_control.AddOnTerminateCallback(
-        base::ScopedClosureRunner(run_loop.QuitClosure()));
+        base::ScopedClosureRunner(future.GetCallback()));
     fake_process_control_.SetReturnCode(0);
-    run_loop.Run();
+    EXPECT_TRUE(future.Wait());
   }
   fake_process_control_.receiver().FlushForTesting();
   EXPECT_TRUE(fake_process_control_.IsConnected());
   {
-    base::RunLoop run_loop;
+    base::test::TestFuture<void> future;
     scoped_process_control.AddOnTerminateCallback(
-        base::ScopedClosureRunner(run_loop.QuitClosure()));
-    run_loop.Run();
+        base::ScopedClosureRunner(future.GetCallback()));
+    EXPECT_TRUE(future.Wait());
   }
   fake_process_control_.receiver().FlushForTesting();
   EXPECT_TRUE(fake_process_control_.IsConnected());
