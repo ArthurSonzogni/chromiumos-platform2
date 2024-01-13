6eea1be947ac7d52021e241225d40c570da080b4
Andrew Moylan <amoylan@chromium.org>
2024-01-13 23:09:48 +1100

    croslog: logging::LOG_FATAL -> LOGGING_FATAL
    Old name logging::LOG_FATAL is deprecated in libchrome.
    
    BUG=b:319987566
    TEST=None
    
    Change-Id: I342cdcf9d670bf938ddaa1a455b60b0c76a6c6de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5196248
    Auto-Submit: Andrew Moylan <amoylan@chromium.org>
    Tested-by: Andrew Moylan <amoylan@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>

--- croslog/log_parser_fuzzer.cc
+++ croslog/log_parser_fuzzer.cc
@@ -17,7 +17,7 @@ namespace croslog {
 namespace {
 
 struct Environment {
-  Environment() { logging::SetMinLogLevel(logging::LOG_FATAL); }
+  Environment() { logging::SetMinLogLevel(logging::LOGGING_FATAL); }
 };
 
 }  // namespace
