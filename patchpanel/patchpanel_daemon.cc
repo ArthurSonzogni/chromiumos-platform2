cff655e04490cf7a020877e3c70f72246a83306b
Jie Jiang <jiejiang@chromium.org>
2024-01-26 14:10:54 +0000

    patchpanel: Remove ShouldEnableFeature()
    Not used.
    
    BUG=None
    TEST=FEATURES="test" emerge-$BOARD patchpanel
    
    Change-Id: I84455a3b91d5a97dad26ddc56f952d226f6794c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5241017
    Commit-Queue: Jie Jiang <jiejiang@chromium.org>
    Tested-by: Jie Jiang <jiejiang@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>

--- patchpanel/patchpanel_daemon.cc
+++ patchpanel/patchpanel_daemon.cc
@@ -4,34 +4,19 @@
 
 #include "patchpanel/patchpanel_daemon.h"
 
-#include <arpa/inet.h>
-#include <net/if.h>
-#include <netinet/in.h>
-#include <stdint.h>
 #include <sys/prctl.h>
-#include <sys/socket.h>
-#include <sys/un.h>
 
-#include <algorithm>
-#include <set>
 #include <utility>
 
-#include <base/files/scoped_file.h>
-#include <base/functional/bind.h>
 #include <base/logging.h>
-#include <base/notreached.h>
-#include <base/strings/string_number_conversions.h>
-#include <base/strings/string_split.h>
-#include <base/strings/string_util.h>
-#include <base/task/single_thread_task_runner.h>
-#include <brillo/key_value_store.h>
+#include <chromeos/dbus/service_constants.h>
 #include <metrics/metrics_library.h>
 #include <net-base/process_manager.h>
 
-#include "patchpanel/ipc.h"
 #include "patchpanel/metrics.h"
-#include "patchpanel/proto_utils.h"
-#include "patchpanel/shill_client.h"
+#include "patchpanel/patchpanel_adaptor.h"
+#include "patchpanel/rtnl_client.h"
+#include "patchpanel/system.h"
 
 namespace patchpanel {
 
@@ -41,76 +26,6 @@ PatchpanelDaemon::PatchpanelDaemon(const base::FilePath& cmd_path)
       system_(std::make_unique<System>()),
       process_manager_(net_base::ProcessManager::GetInstance()),
       metrics_(std::make_unique<MetricsLibrary>()) {}
-
-std::map<const std::string, bool> PatchpanelDaemon::cached_feature_enabled_ =
-    {};
-
-bool PatchpanelDaemon::ShouldEnableFeature(
-    int min_android_sdk_version,
-    int min_chrome_milestone,
-    const std::vector<std::string>& supported_boards,
-    const std::string& feature_name) {
-  static const char kLsbReleasePath[] = "/etc/lsb-release";
-
-  const auto& cached_result = cached_feature_enabled_.find(feature_name);
-  if (cached_result != cached_feature_enabled_.end())
-    return cached_result->second;
-
-  auto check = [min_android_sdk_version, min_chrome_milestone,
-                &supported_boards, &feature_name]() {
-    brillo::KeyValueStore store;
-    if (!store.Load(base::FilePath(kLsbReleasePath))) {
-      LOG(ERROR) << "Could not read lsb-release";
-      return false;
-    }
-
-    std::string value;
-    if (!store.GetString("CHROMEOS_ARC_ANDROID_SDK_VERSION", &value)) {
-      LOG(ERROR) << feature_name
-                 << " disabled - cannot determine Android SDK version";
-      return false;
-    }
-    int ver = 0;
-    if (!base::StringToInt(value.c_str(), &ver)) {
-      LOG(ERROR) << feature_name << " disabled - invalid Android SDK version";
-      return false;
-    }
-    if (ver < min_android_sdk_version) {
-      LOG(INFO) << feature_name << " disabled for Android SDK " << value;
-      return false;
-    }
-
-    if (!store.GetString("CHROMEOS_RELEASE_CHROME_MILESTONE", &value)) {
-      LOG(ERROR) << feature_name
-                 << " disabled - cannot determine ChromeOS milestone";
-      return false;
-    }
-    if (!base::StringToInt(value.c_str(), &ver)) {
-      LOG(ERROR) << feature_name << " disabled - invalid ChromeOS milestone";
-      return false;
-    }
-    if (ver < min_chrome_milestone) {
-      LOG(INFO) << feature_name << " disabled for ChromeOS milestone " << value;
-      return false;
-    }
-
-    if (!store.GetString("CHROMEOS_RELEASE_BOARD", &value)) {
-      LOG(ERROR) << feature_name << " disabled - cannot determine board";
-      return false;
-    }
-    if (!supported_boards.empty() &&
-        std::find(supported_boards.begin(), supported_boards.end(), value) ==
-            supported_boards.end()) {
-      LOG(INFO) << feature_name << " disabled for board " << value;
-      return false;
-    }
-    return true;
-  };
-
-  bool result = check();
-  cached_feature_enabled_.emplace(feature_name, result);
-  return result;
-}
 
 void PatchpanelDaemon::RegisterDBusObjectsAsync(
     brillo::dbus_utils::AsyncEventSequencer* sequencer) {
--- patchpanel/patchpanel_daemon.h
+++ patchpanel/patchpanel_daemon.h
@@ -5,23 +5,13 @@
 #ifndef PATCHPANEL_PATCHPANEL_DAEMON_H_
 #define PATCHPANEL_PATCHPANEL_DAEMON_H_
 
-#include <iostream>
-#include <map>
 #include <memory>
-#include <string>
-#include <utility>
-#include <vector>
 
-#include <base/memory/weak_ptr.h>
+#include <base/files/file_path.h>
 #include <brillo/daemons/dbus_daemon.h>
-#include <brillo/process/process_reaper.h>
-#include <chromeos/dbus/service_constants.h>
 #include <metrics/metrics_library.h>
 #include <net-base/process_manager.h>
-#include <patchpanel/proto_bindings/patchpanel_service.pb.h>
 
-#include "patchpanel/manager.h"
-#include "patchpanel/metrics.h"
 #include "patchpanel/patchpanel_adaptor.h"
 #include "patchpanel/system.h"
 
@@ -36,16 +26,6 @@ class PatchpanelDaemon final : public brillo::DBusServiceDaemon {
 
   ~PatchpanelDaemon() = default;
 
-  // This function is used to enable specific features only on selected
-  // combination of Android version, Chrome version, and boards.
-  // Empty |supportedBoards| means that the feature should be enabled on all
-  // board.
-  static bool ShouldEnableFeature(
-      int min_android_sdk_version,
-      int min_chrome_milestone,
-      const std::vector<std::string>& supported_boards,
-      const std::string& feature_name);
-
  protected:
   // Implements brillo::DBusServiceDaemon.
   void RegisterDBusObjectsAsync(
@@ -55,10 +35,6 @@ class PatchpanelDaemon final : public brillo::DBusServiceDaemon {
   void OnShutdown(int* exit_code) override;
 
  private:
-  // |cached_feature_enabled| stores the cached result of if a feature should be
-  // enabled.
-  static std::map<const std::string, bool> cached_feature_enabled_;
-
   // The file path of the patchpanel daemon binary.
   base::FilePath cmd_path_;
 
