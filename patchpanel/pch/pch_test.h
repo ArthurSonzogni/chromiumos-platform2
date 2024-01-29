65ed569bfca1341474e8c0198cadaf820213cf21
Jie Jiang <jiejiang@chromium.org>
2024-01-29 05:39:03 +0000

    shill, patchpanel: Move PCH to a separate folder
    cpplint config cannot be applied to the whole file but can be applied to
    a folder. Move the precompiled headers to a seperate folder so that the
    header guard check can be skipped on them.
    
    BUG=None
    TEST=FEATURES="test" emerge-$BOARD shill patchpanel
    
    Change-Id: I646bc4fb46553a821846ab90deb14f25546dbaa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5237794
    Commit-Queue: Jie Jiang <jiejiang@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Tested-by: Jie Jiang <jiejiang@chromium.org>

--- patchpanel/pch/pch_test.h
+++ patchpanel/pch/pch_test.h
@@ -2,19 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#if defined(SHILL_PCH_TEST_H_)
+#if defined(PATCHPANEL_PCH_TEST_H_)
 #error You should not include this header directly in the code.
 #endif
 
-#define SHILL_PCH_TEST_H_
+#define PATCHPANEL_PCH_TEST_H_
 
-// This is the precompiled header for building shill unit test.
+// This is the precompiled header for building patchpanel unit test.
 // - This header will be prepend to each cc file directly by the compiler, so
 //   the code should not include this header directly.
-// - It's better not to include any shill headers here, since any change to the
-//   included header would trigger a full rebuild, which is not desired.
+// - It's better not to include any patchpanel headers here, since any change to
+//   the included header would trigger a full rebuild, which is not desired.
 
-#include "shill/pch.h"
+#include "patchpanel/pch/pch.h"
 
 #include <base/test/task_environment.h>
 #include <gmock/gmock.h>
