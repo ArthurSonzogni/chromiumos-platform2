9db570c2c7cb40b9efd9e7fbfd42ebe9ade680f9
Nicolas Norvez <norvez@chromium.org>
2024-02-23 02:16:11 +0000

    fbpreprocessor: log to syslog
    Rather than using a dedicated log file, it's better to use syslog for
    consistency with the rest of the platform and also to avoid maintaining
    different log files.
    
    BUG=b:326501037
    TEST=cros workon --board=${BOARD} start fbpreprocessord
    TEST=FEATURES=test emerge-${BOARD} fbpreprocessord
    TEST=create tryjob that includes the CL, flash on DUT, observe that
    fbpreprocessord is WAI and the log messages are in /var/log/messages
    
    Change-Id: I98d0e57c9e10d3e00f5826fb1f34af385119020d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5320490
    Commit-Queue: Nicolas Norvez <norvez@chromium.org>
    Reviewed-by: Yichen Yu <yichenyu@chromium.org>
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Tested-by: Nicolas Norvez <norvez@chromium.org>

--- fbpreprocessor/main.cc
+++ fbpreprocessor/main.cc
@@ -2,10 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <base/command_line.h>
-#include <base/files/file_path.h>
 #include <base/logging.h>
 #include <brillo/flag_helper.h>
+#include <brillo/syslog_logging.h>
 
 #include "fbpreprocessor/configuration.h"
 #include "fbpreprocessor/fbpreprocessor_daemon.h"
@@ -30,7 +29,6 @@ void ValidateAndSetMinLogLevel(int log_level) {
 }  // namespace
 
 int main(int argc, char* argv[]) {
-  DEFINE_string(log_dir, "/var/log/", "Directory where logs are written.");
   DEFINE_int32(log_level, logging::LOGGING_INFO,
                "Logging level - 0: LOG(INFO), 1: LOG(WARNING), 2: LOG(ERROR), "
                "-1: VLOG(1), -2: VLOG(2), ...");
@@ -41,14 +39,7 @@ int main(int argc, char* argv[]) {
   brillo::FlagHelper::Init(
       argc, argv, "fbpreprocessord, the debug data preprocessing daemon.");
 
-  const base::FilePath log_file =
-      base::FilePath(FLAGS_log_dir).Append("fbpreprocessord.log");
-
-  logging::LoggingSettings logging_settings;
-  logging_settings.logging_dest = logging::LOG_TO_FILE;
-  logging_settings.log_file_path = log_file.value().c_str();
-  logging_settings.lock_log = logging::DONT_LOCK_LOG_FILE;
-  logging::InitLogging(logging_settings);
+  brillo::InitLog(brillo::kLogToSyslog | brillo::kLogHeader);
 
   LOG(INFO) << "Starting fbpreprocessord.";
   ValidateAndSetMinLogLevel(FLAGS_log_level);
