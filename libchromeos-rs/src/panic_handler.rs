953a95b2280a7f7c5bc604a9fab37b1982e6826e
Daniel Verkamp <dverkamp@chromium.org>
2024-02-26 13:58:41 -0800

    libchromeos-rs: write!() directly to panic memfd
    This avoids a heap allocation for the String, which is probably a good
    idea if the panic was caused by heap corruption.
    
    BUG=b:309651697
    TEST=manually verify panic message in /var/spool/crash/*.meta
    
    Change-Id: I5d916eb811614d5d4d04ca3667d7413c5e27bb3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5325206
    Tested-by: Daniel Verkamp <dverkamp@chromium.org>
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Commit-Queue: Allen Webb <allenwebb@google.com>

--- libchromeos-rs/src/panic_handler.rs
+++ libchromeos-rs/src/panic_handler.rs
@@ -7,9 +7,9 @@
 use nix::sys::memfd::memfd_create;
 use nix::sys::memfd::MemFdCreateFlag;
 use std::ffi::CString;
-use std::fmt::Write;
 use std::fs::File;
-use std::io;
+use std::io::Result;
+use std::io::Write;
 use std::mem;
 use std::os::unix::prelude::FromRawFd;
 use std::panic;
@@ -28,27 +28,25 @@ fn create_panic_memfd() -> nix::Result<File> {
 
 // TODO(b/309651697): This was written to be compatible with existing PanicInfo formatting, but it
 // should probably be made more stable.
-fn format_panic_info(panic_info: &panic::PanicInfo<'_>) -> String {
-    // 128 is arbitrary, but should be enough to cover most cases.
-    let mut result = String::with_capacity(128);
-    result += "panicked at '";
+fn format_panic_info<W: Write>(w: &mut W, panic_info: &panic::PanicInfo<'_>) -> Result<()> {
+    write!(w, "panicked at '")?;
     if let Some(message) = panic_info.message() {
-        let _ = write!(&mut result, "{}", message);
+        write!(w, "{}", message)?;
     } else if let Some(message) = panic_info.payload().downcast_ref::<&'static str>() {
-        result += message;
+        write!(w, "{}", message)?;
     }
-    result += "', ";
+    write!(w, "', ")?;
 
     // At the time of writing, `PanicInfo::location()` cannot return `None`.
     match panic_info.location() {
         Some(location) => {
-            let _ = write!(&mut result, "{}", location);
+            write!(w, "{}", location)?;
         }
         None => {
-            result += "no location info";
+            write!(w, "no location info")?;
         }
     }
-    result
+    Ok(())
 }
 
 /// Inserts a panic handler that writes the panic info to a memfd called
@@ -59,7 +57,7 @@ pub fn install_memfd_handler() {
     panic::set_hook(Box::new(move |p| {
         // On failure, ignore the error and call the original handler.
         if let Ok(mut panic_memfd) = create_panic_memfd() {
-            let _ = io::Write::write_all(&mut panic_memfd, format_panic_info(p).as_bytes());
+            let _ = format_panic_info(&mut panic_memfd, p);
             // Intentionally leak panic_memfd so it is picked up by the crash handler.
             mem::forget(panic_memfd);
         }
