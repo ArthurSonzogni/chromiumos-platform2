8c9e94c2341fa4bd591f1f11964fd3a27292c50b
Nicholas Bishop <nicholasbishop@google.com>
2024-03-08 17:39:22 -0500

    mini_udisks: Add panic and logging boilerplate
    BUG=b:328780545
    TEST=cros_workon start chromeos-base/mini_udisks
    TEST=emerge-reven chromeos-base/mini_udisks
    
    Change-Id: I75979b2d81a8d7077ee73cf36396e00dd8930409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5353432
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>
    Reviewed-by: Jeffery Miller <jefferymiller@google.com>

--- mini_udisks/src/main.rs
+++ mini_udisks/src/main.rs
@@ -2,6 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-fn main() {
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    libchromeos::panic_handler::install_memfd_handler();
+
+    libchromeos::syslog::init("mini-udisks".to_string(), /*log_to_stderr=*/ true)
+        .expect("failed to initialize logger");
+
     todo!()
 }
