ArthurSonzogni/chromiumos-platform2

commit 8de12dc867bef9bb1b1354eddf3f02620c4621e8

init: Move early mount calls to C++
Move the early mount calls from chromeos_startup.sh into C++.
Create the lib_impl and file_system_impl classes to implement
library functions and functions that interface with the filesystem.

BUG=b:172210918
TEST=build_image --board=hatch

Change-Id: I11863a80c48cd9f39291b93f20af12127ed5a895
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2999417
Commit-Queue: Alexis Savery <asavery@chromium.org>
Reviewed-by: Sarthak Kukreti <sarthakkukreti@chromium.org>
Tested-by: Alexis Savery <asavery@chromium.org>
Reviewed-by: Mike Frysinger <vapier@chromium.org>

init/startup/chromeos_startup.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <sys/mount.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
 
@@ -12,12 +14,26 @@
 #include <base/files/file_util.h>
 #include <base/logging.h>
 #include <brillo/process/process.h>
+#include <brillo/userdb_utils.h>
 
 #include "init/crossystem.h"
 #include "init/crossystem_impl.h"
 #include "init/startup/chromeos_startup.h"
 #include "init/startup/constants.h"
-#include "init/startup/lib.h"
+#include "init/startup/platform_impl.h"
+
+namespace {
+
+constexpr char kTracingOn[] = "sys/kernel/debug/tracing/tracing_on";
+
+constexpr char kSysKernelDebug[] = "sys/kernel/debug";
+constexpr char kSysKernelConfig[] = "sys/kernel/config";
+constexpr char kSysKernelSecurity[] = "sys/kernel/security";
+constexpr char kDisableStatefulSecurityHard[] =
+    "usr/share/cros/startup/disable_stateful_security_hardening";
+constexpr char kDebugfsAccessGrp[] = "debugfs-access";
+
+}  // namespace
 
 namespace startup {
 
@@ -50,6 +66,7 @@ void ChromeosStartup::CheckClock() {
     stime.tv_sec = kBaseSecs;
     stime.tv_nsec = 0;
     if (clock_settime(CLOCK_REALTIME, &stime) != 0) {
+      // TODO(b/232901639): Improve failure reporting.
       PLOG(WARNING) << "Unable to set time.";
     }
   }
@@ -60,12 +77,69 @@ ChromeosStartup::ChromeosStartup(std::unique_ptr<CrosSystem> cros_system,
                                  const base::FilePath& root,
                                  const base::FilePath& stateful,
                                  const base::FilePath& lsb_file,
-                                 const base::FilePath& proc_file)
+                                 const base::FilePath& proc_file,
+                                 std::unique_ptr<Platform> platform)
     : cros_system_(std::move(cros_system)),
       lsb_file_(lsb_file),
       proc_(proc_file),
       root_(root),
-      stateful_(stateful) {}
+      stateful_(stateful),
+      platform_(std::move(platform)) {}
+
+void ChromeosStartup::EarlySetup() {
+  gid_t debugfs_grp;
+  if (!brillo::userdb::GetGroupInfo(kDebugfsAccessGrp, &debugfs_grp)) {
+    PLOG(WARNING) << "Can't get gid for " << kDebugfsAccessGrp;
+  } else {
+    char data[25];
+    snprintf(data, sizeof(data), "mode=0750,uid=0,gid=%d", debugfs_grp);
+    const base::FilePath debug = root_.Append(kSysKernelDebug);
+    if (!platform_->Mount("debugfs", debug, "debugfs", kCommonMountFlags,
+                          data)) {
+      // TODO(b/232901639): Improve failure reporting
+      PLOG(WARNING) << "Unable to mount " << debug.value();
+    }
+  }
+
+  // /sys/kernel/debug/tracing/tracing_on is 1 right after tracefs is
+  // initialized in the kernel. Set it to a reasonable initial state of 0 after
+  // debugfs is mounted. This needs to be done early during boot to avoid
+  // interference with ureadahead that uses ftrace to build the list of files to
+  // preload in the block cache. Android's init running in the ARC++ container
+  // sets this file to 0, and we set it to 0 here so the the initial state of
+  // tracing_on is always 0 regardless of ARC++.
+  struct stat st;
+  const base::FilePath tracing = root_.Append(kTracingOn);
+  if (platform_->Stat(tracing, &st) && S_ISREG(st.st_mode)) {
+    if (!base::WriteFile(tracing, "0")) {
+      PLOG(WARNING) << "Failed to write to " << tracing.value();
+    }
+  }
+
+  // Mount configfs, if present.
+  const base::FilePath sys_config = root_.Append(kSysKernelConfig);
+  if (base::DirectoryExists(sys_config)) {
+    if (!platform_->Mount("configfs", sys_config, "configfs", kCommonMountFlags,
+                          "")) {
+      // TODO(b/232901639): Improve failure reporting.
+      PLOG(WARNING) << "Unable to mount " << sys_config.value();
+    }
+  }
+
+  const base::FilePath disable_sec_hard =
+      root_.Append(kDisableStatefulSecurityHard);
+  disable_stateful_security_hardening_ = base::PathExists(disable_sec_hard);
+
+  // Mount securityfs as it is used to configure inode security policies below.
+  if (!disable_stateful_security_hardening_) {
+    const base::FilePath sys_security = root_.Append(kSysKernelSecurity);
+    if (!platform_->Mount("securityfs", sys_security, "securityfs",
+                          kCommonMountFlags, "")) {
+      // TODO(b/232901639): Improve failure reporting.
+      PLOG(WARNING) << "Unable to mount " << sys_security.value();
+    }
+  }
+}
 
 // Main function to run chromeos_startup.
 int ChromeosStartup::Run() {
@@ -76,9 +150,12 @@ int ChromeosStartup::Run() {
   // bootstat writes timings to tmpfs.
   bootstat_.LogEvent("pre-startup");
 
+  EarlySetup();
+
   int ret = RunChromeosStartupScript();
   if (ret) {
-    LOG(WARNING) << "chromeos_startup.sh returned with code " << ret;
+    // TODO(b/232901639): Improve failure reporting.
+    PLOG(WARNING) << "chromeos_startup.sh returned with code " << ret;
   }
 
   bootstat_.LogEvent("post-startup");

init/BUILD.gn
@@ -18,6 +18,7 @@ group("all") {
   ]
   if (use.test) {
     deps += [
+      ":chromeos_startup_test",
       ":clobber_state_test",
       ":file_attrs_cleaner_test",
       ":periodic_scheduler_test",
@@ -68,7 +69,10 @@ static_library("utils") {
 }
 
 static_library("libcrosstartup") {
-  sources = [ "startup/chromeos_startup.cc" ]
+  sources = [
+    "startup/chromeos_startup.cc",
+    "startup/platform_impl.cc",
+  ]
   configs += [ ":target_defaults" ]
   deps = [
     ":libclobber",
@@ -169,6 +173,22 @@ executable("usermode-helper") {
 if (use.test) {
   pkg_config("test_config") {
     pkg_deps = [ "libchrome-test" ]
+  }
+
+  executable("chromeos_startup_test") {
+    sources = [
+      "startup/chromeos_startup_test.cc",
+      "startup/fake_platform_impl.cc",
+    ]
+    configs += [
+      "//common-mk:test",
+      ":target_defaults",
+      ":test_config",
+    ]
+    deps = [
+      ":libcrosstartup",
+      "//common-mk/testrunner",
+    ]
   }
 
   executable("clobber_state_test") {

init/chromeos_startup.sh
@@ -117,41 +117,14 @@ add_clobber_crash_report() {
   sync
 }
 
-mount -n -i -c -t debugfs \
-  -o nodev,noexec,nosuid,mode=0750,uid=0,gid=debugfs-access \
-  debugfs /sys/kernel/debug
-
-# /sys/kernel/debug/tracing/tracing_on is 1 right after tracefs is initialized
-# in the kernel. Set it to a reasonable initial state of 0 after debugfs is
-# mounted. This needs to be done early during boot to avoid interference with
-# ureadahead that uses ftrace to build the list of files to preload in the block
-# cache. Android's init running in the ARC++ container sets this file to 0, and
-# we set it to 0 here so the the initial state of tracing_on is always 0
-# regardless of ARC++.
-if [ -e /sys/kernel/debug/tracing/tracing_on ]; then
-  echo 0 > /sys/kernel/debug/tracing/tracing_on
-fi
-
 # Some startup functions are split into a separate library which may be
 # different for different targets (e.g., regular Chrome OS vs. embedded).
 . /usr/share/cros/startup_utils.sh
-
-# Mount configfs, if present.
-if [ -d /sys/kernel/config ]; then
-  mount -n -i -c -t configfs -o nodev,nosuid,noexec configfs \
-    /sys/kernel/config
-fi
 
 if [ -e /usr/share/cros/startup/disable_stateful_security_hardening ]; then
   DISABLE_STATEFUL_SECURITY_HARDENING="true"
 else
   DISABLE_STATEFUL_SECURITY_HARDENING="false"
-fi
-
-if [ "${DISABLE_STATEFUL_SECURITY_HARDENING}" = "false" ]; then
-  # Mount securityfs as it is used to configure inode security policies below.
-  mount -n -i -c -t securityfs -o nodev,noexec,nosuid securityfs \
-    /sys/kernel/security
 fi
 
 # Initialize kernel sysctl settings early so that they take effect for boot

init/startup/chromeos_startup.h
@@ -15,9 +15,35 @@
 #include <metrics/bootstat.h>
 
 #include "init/crossystem.h"
-#include "init/startup/lib.h"
+#include "init/startup/platform_impl.h"
 
 namespace startup {
+
+// Define a struct to contain the flags we set when parsing USE flags.
+struct Flags {
+  // Indicates built with USE=encrypted_reboot_vault. Used to determine
+  // if we need to setup the encrypted reboot vault.
+  bool encrypted_reboot_vault;
+  // Indicates built with USE=encrypted_stateful, used to determine which
+  // mount function to use
+  bool encstateful;
+  // Indicates built with USE=direncryption. Used when mounting the stateful
+  // partition to determine if we should enable directory encryption.
+  bool direncryption;
+  // Indicates built with USE=fsverity. Used when mounting the stateful
+  // partition.
+  bool fsverity;
+  // Indicates built with USE=lvm_stateful_partition. Used when mounting the
+  // stateful partition.
+  bool lvm_stateful;
+  // Indicates built with USE=prjquota. Used when mounting the stateful
+  // partition.
+  bool prjquota;
+  // Not a USE flag, but indicates if built with both USE=tpm2 and
+  // USE=encrypted_stateful. Used to determine if we will try to create
+  // a system key.
+  bool sys_key_util;
+};
 
 // This is the primary class for the startup functionality, making use of the
 // other classes in the startup directory. chromeos_startup sets up different
@@ -35,8 +61,13 @@ class ChromeosStartup {
                   const base::FilePath& root,
                   const base::FilePath& stateful,
                   const base::FilePath& lsb_file,
-                  const base::FilePath& proc_file);
+                  const base::FilePath& proc_file,
+                  std::unique_ptr<Platform> platform);
   virtual ~ChromeosStartup() = default;
+
+  // EarlySetup contains the early mount calls of chromeos_startup. This
+  // function exists to help break up the Run function into smaller functions.
+  void EarlySetup();
 
   // Run the chromeos startup routine.
   int Run();
@@ -52,8 +83,9 @@ class ChromeosStartup {
   const base::FilePath proc_;
   const base::FilePath root_;
   const base::FilePath stateful_;
-  std::stack<base::FilePath> mount_stack_;
   bootstat::BootStat bootstat_;
+  std::unique_ptr<Platform> platform_;
+  bool disable_stateful_security_hardening_;
 };
 
 }  // namespace startup

init/startup/chromeos_startup_test.cc
@@ -1,0 +1,71 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdlib.h>
+#include <sys/sysmacros.h>
+#include <sys/types.h>
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <base/files/file_util.h>
+#include <base/files/scoped_temp_dir.h>
+#include <base/logging.h>
+#include <base/strings/string_split.h>
+#include <base/strings/string_util.h>
+#include <base/values.h>
+#include <gtest/gtest.h>
+
+#include "init/crossystem.h"
+#include "init/crossystem_fake.h"
+#include "init/startup/chromeos_startup.h"
+#include "init/startup/fake_platform_impl.h"
+#include "init/startup/platform_impl.h"
+
+class EarlySetupTest : public ::testing::Test {
+ protected:
+  EarlySetupTest() : cros_system_(new CrosSystemFake()) {}
+
+  void SetUp() override {
+    ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
+    base_dir_ = temp_dir_.GetPath();
+    kernel_debug_ = base_dir_.Append("sys/kernel/debug");
+    kernel_config_ = base_dir_.Append("sys/kernel/config");
+    tracing_ = kernel_debug_.Append("tracing/tracing_on");
+    security_hardening_ = base_dir_.Append(
+        "usr/share/cros/startup/disable_stateful_security_hardening");
+    kernel_security_ = base_dir_.Append("sys/kernel/security");
+    namespaces_ = base_dir_.Append("run/namespaces");
+    platform_ = new startup::FakePlatform();
+    startup_ = std::make_unique<startup::ChromeosStartup>(
+        std::unique_ptr<CrosSystem>(cros_system_), flags_, base_dir_, base_dir_,
+        base_dir_, base_dir_,
+        std::unique_ptr<startup::FakePlatform>(platform_));
+  }
+
+  CrosSystemFake* cros_system_;
+  startup::Flags flags_;
+  startup::FakePlatform* platform_;
+  std::unique_ptr<startup::ChromeosStartup> startup_;
+  base::ScopedTempDir temp_dir_;
+  base::FilePath base_dir_;
+  base::FilePath kernel_debug_;
+  base::FilePath dev_pts_;
+  base::FilePath dev_shm_;
+  base::FilePath kernel_config_;
+  base::FilePath tracing_;
+  base::FilePath security_hardening_;
+  base::FilePath kernel_security_;
+  base::FilePath namespaces_;
+};
+
+TEST_F(EarlySetupTest, NoTracing) {
+  platform_->SetMountResultForPath(kernel_debug_, "debugfs");
+  platform_->SetMountResultForPath(kernel_config_, "configfs");
+  platform_->SetMountResultForPath(kernel_security_, "securityfs");
+  platform_->SetMountResultForPath(namespaces_, "");
+
+  startup_->EarlySetup();
+}

init/startup/constants.h
@@ -5,6 +5,8 @@
 #ifndef INIT_STARTUP_CONSTANTS_H_
 #define INIT_STARTUP_CONSTANTS_H_
 
+#include <sys/mount.h>
+
 namespace startup {
 
 // These constants are used to check the clock. Since they need to be
@@ -14,6 +16,12 @@ constexpr int kYear = 2022;
 // This isn't exactly correct as it doesn't handle leap years, but it's
 // good enough for our purposes (pulling clock to the ~last year).
 constexpr uint64_t kBaseSecs = (kYear - 1970) * (365 * 24 * 60 * 60);
+
+// Many of the mount calls in chromeos_startup utilize these flags.
+// Making this a constant to simplify those mount calls, but this
+// should only be used in cases where these specific mount flags are
+// needed.
+constexpr int kCommonMountFlags = MS_NOSUID | MS_NODEV | MS_NOEXEC;
 
 }  // namespace startup
 

init/startup/fake_platform_impl.cc
@@ -1,0 +1,66 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdlib.h>
+#include <string>
+#include <sys/types.h>
+
+#include <base/files/file_util.h>
+
+#include "init/startup/fake_platform_impl.h"
+
+namespace startup {
+
+FakePlatform::FakePlatform() : Platform() {}
+
+void FakePlatform::SetStatResultForPath(const base::FilePath& path,
+                                        const struct stat& st) {
+  result_map_[path.value()] = st;
+}
+
+void FakePlatform::SetMountResultForPath(const base::FilePath& path,
+                                         const std::string& output) {
+  mount_result_map_[path.value()] = output;
+}
+
+bool FakePlatform::Stat(const base::FilePath& path, struct stat* st) {
+  std::unordered_map<std::string, struct stat>::iterator it;
+  it = result_map_.find(path.value());
+  if (st == nullptr || it == result_map_.end()) {
+    return false;
+  }
+
+  *st = it->second;
+  return true;
+}
+
+bool FakePlatform::Mount(const base::FilePath& src,
+                         const base::FilePath& dst,
+                         const std::string& type,
+                         unsigned long flags,
+                         const std::string& data) {
+  std::unordered_map<std::string, std::string>::iterator it;
+  it = mount_result_map_.find(dst.value());
+  if (it == mount_result_map_.end()) {
+    return false;
+  }
+
+  return src.value().compare(it->second) == 0;
+}
+
+bool FakePlatform::Mount(const std::string& src,
+                         const base::FilePath& dst,
+                         const std::string& type,
+                         unsigned long flags,
+                         const std::string& data) {
+  std::unordered_map<std::string, std::string>::iterator it;
+  it = mount_result_map_.find(dst.value());
+  if (it == mount_result_map_.end()) {
+    return false;
+  }
+
+  return src.compare(it->second) == 0;
+}
+
+}  // namespace startup

init/startup/fake_platform_impl.h
@@ -1,0 +1,54 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef INIT_STARTUP_FAKE_PLATFORM_IMPL_H_
+#define INIT_STARTUP_FAKE_PLATFORM_IMPL_H_
+
+#include <stdlib.h>
+#include <string>
+#include <sys/types.h>
+#include <unordered_map>
+#include <vector>
+
+#include <base/files/file_util.h>
+
+#include "init/startup/platform_impl.h"
+
+namespace startup {
+
+// This class is utilized by tests to override functions that
+// use of system calls or other functionality that utilizes
+// system command outputs.
+
+class FakePlatform : public Platform {
+ public:
+  FakePlatform();
+
+  void SetStatResultForPath(const base::FilePath& path, const struct stat& st);
+
+  void SetMountResultForPath(const base::FilePath& path,
+                             const std::string& output);
+  // Wrapper around stat(2).
+  bool Stat(const base::FilePath& path, struct stat* st) override;
+
+  // Wrapper around mount(2).
+  bool Mount(const base::FilePath& src,
+             const base::FilePath& dst,
+             const std::string& type,
+             unsigned long flags,
+             const std::string& data) override;
+  bool Mount(const std::string& src,
+             const base::FilePath& dst,
+             const std::string& type,
+             unsigned long flags,
+             const std::string& data) override;
+
+ private:
+  std::unordered_map<std::string, struct stat> result_map_;
+  std::unordered_map<std::string, std::string> mount_result_map_;
+};
+
+}  // namespace startup
+
+#endif  // INIT_STARTUP_FAKE_PLATFORM_IMPL_H_

commit c4684d73c43c9c48c389ed6e11c00a64f10781e5

init: startup: Use empty src when mounting special fs
When calling mount() for in-kernel filesystem (configfs,...), the |src|
argument is moot, so use an empty path, add the filesystem type before
calling mount syscall.

We can remove one version of mount.

BUG=b:172210918
TEST=unit tests, CQ.
Check the mount output is consistent with the current behavior:
> mount | grep security
securityfs on /sys/kernel/security type securityfs ...

Fixes: commit 8de12dc867 ("init: Move early mount calls to C++")
Change-Id: I9ff72149c4fe878a338e2c55052e38df45928c6c
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5354052
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Mike Frysinger <vapier@chromium.org>
Auto-Submit: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Mike Frysinger <vapier@chromium.org>

init/startup/chromeos_startup.cc
@@ -322,6 +322,7 @@ ChromeosStartup::ChromeosStartup(
 
 void ChromeosStartup::EarlySetup() {
   const base::FilePath sysfs = root_.Append(kSysfs);
+  const base::FilePath empty;
   gid_t debugfs_grp;
   if (!brillo::userdb::GetGroupInfo(kDebugfsAccessGrp, &debugfs_grp)) {
     PLOG(WARNING) << "Can't get gid for " << kDebugfsAccessGrp;
@@ -329,7 +330,7 @@ void ChromeosStartup::EarlySetup() {
     char data[25];
     snprintf(data, sizeof(data), "mode=0750,uid=0,gid=%d", debugfs_grp);
     const base::FilePath debug = sysfs.Append(kKernelDebug);
-    if (!startup_dep_->Mount("debugfs", debug, "debugfs", kCommonMountFlags,
+    if (!startup_dep_->Mount(empty, debug, "debugfs", kCommonMountFlags,
                              data)) {
       // TODO(b/232901639): Improve failure reporting.
       PLOG(WARNING) << "Unable to mount " << debug.value();
@@ -343,7 +344,7 @@ void ChromeosStartup::EarlySetup() {
   // dependence on debugfs.
   const base::FilePath tracefs = sysfs.Append(kKernelTracing);
   // All users may need to access the tracing directory.
-  if (!startup_dep_->Mount("tracefs", tracefs, "tracefs", kCommonMountFlags,
+  if (!startup_dep_->Mount(empty, tracefs, "tracefs", kCommonMountFlags,
                            "mode=0755")) {
     // TODO(b/232901639): Improve failure reporting.
     PLOG(WARNING) << "Unable to mount " << tracefs.value();
@@ -352,8 +353,8 @@ void ChromeosStartup::EarlySetup() {
   // Mount configfs, if present.
   const base::FilePath configfs = sysfs.Append(kKernelConfig);
   if (base::DirectoryExists(configfs)) {
-    if (!startup_dep_->Mount("configfs", configfs, "configfs",
-                             kCommonMountFlags, "")) {
+    if (!startup_dep_->Mount(empty, configfs, "configfs", kCommonMountFlags,
+                             "")) {
       // TODO(b/232901639): Improve failure reporting.
       PLOG(WARNING) << "Unable to mount " << configfs.value();
     }
@@ -367,7 +368,7 @@ void ChromeosStartup::EarlySetup() {
     const std::string data =
         base::StrCat({"mode=0770,gid=", std::to_string(bpffs_grp)});
     const base::FilePath bpffs = sysfs.Append(kBpf);
-    if (!startup_dep_->Mount("bpffs", bpffs, "bpf", kCommonMountFlags,
+    if (!startup_dep_->Mount(empty, bpffs, "bpf", kCommonMountFlags,
                              data.c_str())) {
       // TODO(b/232901639): Improve failure reporting.
       PLOG(WARNING) << "Unable to mount " << bpffs.value();
@@ -376,8 +377,8 @@ void ChromeosStartup::EarlySetup() {
 
   // Mount securityfs as it is used to configure inode security policies below.
   const base::FilePath securityfs = sysfs.Append(kKernelSecurity);
-  if (!startup_dep_->Mount("securityfs", securityfs, "securityfs",
-                           kCommonMountFlags, "")) {
+  if (!startup_dep_->Mount(empty, securityfs, "securityfs", kCommonMountFlags,
+                           "")) {
     // TODO(b/232901639): Improve failure reporting.
     PLOG(WARNING) << "Unable to mount " << securityfs.value();
   }
@@ -948,7 +949,7 @@ int ChromeosStartup::Run() {
   // still possible.
   const base::FilePath kernel_sec =
       root_.Append(kSysfs).Append(kKernelSecurity);
-  if (!startup_dep_->Mount("securityfs", kernel_sec, "securityfs",
+  if (!startup_dep_->Mount(base::FilePath(), kernel_sec, "securityfs",
                            MS_REMOUNT | MS_RDONLY | kCommonMountFlags, "")) {
     PLOG(WARNING) << "Failed to remount " << kernel_sec << " as readonly.";
   }

init/startup/fake_startup_dep_impl.cc
@@ -103,20 +103,6 @@ bool FakeStartupDep::Mount(const base::FilePath& src,
   return src.value().compare(it->second) == 0;
 }
 
-bool FakeStartupDep::Mount(const std::string& src,
-                           const base::FilePath& dst,
-                           const std::string& type,
-                           unsigned long flags,  // NOLINT(runtime/int)
-                           const std::string& data) {
-  std::unordered_map<std::string, std::string>::iterator it;
-  it = mount_result_map_.find(dst.value());
-  if (it == mount_result_map_.end()) {
-    return false;
-  }
-
-  return src.compare(it->second) == 0;
-}
-
 bool FakeStartupDep::Umount(const base::FilePath& path) {
   umount_vector_.push_back(path.value());
   return true;

init/startup/fake_startup_dep_impl.h
@@ -55,11 +55,6 @@ class FakeStartupDep : public StartupDep {
              const std::string& type,
              unsigned long flags,  // NOLINT(runtime/int)
              const std::string& data) override;
-  bool Mount(const std::string& src,
-             const base::FilePath& dst,
-             const std::string& type,
-             unsigned long flags,  // NOLINT(runtime/int)
-             const std::string& data) override;
   bool Umount(const base::FilePath& path) override;
   base::ScopedFD Open(const base::FilePath& pathname, int flags) override;
   // NOLINTNEXTLINE(runtime/int)

init/startup/mock_startup_dep_impl.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef INIT_STARTUP_MOCK_PLATFORM_IMPL_H_
-#define INIT_STARTUP_MOCK_PLATFORM_IMPL_H_
+#ifndef INIT_STARTUP_MOCK_STARTUP_DEP_IMPL_H_
+#define INIT_STARTUP_MOCK_STARTUP_DEP_IMPL_H_
 
 #include <string>
 
@@ -27,14 +27,6 @@ class MockStartupDep : public StartupDep {
   MOCK_METHOD(bool,
               Mount,
               (const base::FilePath& src,
-               const base::FilePath& dst,
-               const std::string& type,
-               unsigned long flags,  // NOLINT(runtime/int)
-               const std::string& data),
-              (override));
-  MOCK_METHOD(bool,
-              Mount,
-              (const std::string& src,
                const base::FilePath& dst,
                const std::string& type,
                unsigned long flags,  // NOLINT(runtime/int)
@@ -64,4 +56,4 @@ class MockStartupDep : public StartupDep {
 
 }  // namespace startup
 
-#endif  // INIT_STARTUP_MOCK_PLATFORM_IMPL_H_
+#endif  // INIT_STARTUP_MOCK_STARTUP_DEP_IMPL_H_

init/startup/startup_dep_impl.cc
@@ -60,16 +60,8 @@ bool StartupDep::Mount(const base::FilePath& src,
                        const std::string& type,
                        const unsigned long flags,  // NOLINT(runtime/int)
                        const std::string& data) {
-  return mount(src.value().c_str(), dst.value().c_str(), type.c_str(), flags,
-               data.c_str()) == 0;
-}
-
-bool StartupDep::Mount(const std::string& src,
-                       const base::FilePath& dst,
-                       const std::string& type,
-                       const unsigned long flags,  // NOLINT(runtime/int)
-                       const std::string& data) {
-  return mount(src.c_str(), dst.value().c_str(), type.c_str(), flags,
+  std::string src_path = !src.value().empty() ? src.value() : type;
+  return mount(src_path.c_str(), dst.value().c_str(), type.c_str(), flags,
                data.c_str()) == 0;
 }
 

init/startup/startup_dep_impl.h
@@ -57,11 +57,6 @@ class StartupDep {
                      const std::string& type,
                      unsigned long flags,  // NOLINT(runtime/int)
                      const std::string& data);
-  virtual bool Mount(const std::string& src,
-                     const base::FilePath& dst,
-                     const std::string& type,
-                     unsigned long flags,  // NOLINT(runtime/int)
-                     const std::string& data);
 
   // Wrapper around umount(2).
   virtual bool Umount(const base::FilePath& path);

init/startup/uefi_startup.cc
@@ -54,7 +54,7 @@ bool UefiDelegateImpl::MountEfivarfs(const UserAndGroup& fwupd) {
   const base::FilePath efivars_dir = root_dir_.Append(kEfivarsDir);
   const std::string data =
       "uid=" + std::to_string(fwupd.uid) + ",gid=" + std::to_string(fwupd.gid);
-  if (!startup_dep_.Mount(/*src=*/kFsTypeEfivarfs,
+  if (!startup_dep_.Mount(/*src=*/base::FilePath(),
                           /*dst=*/efivars_dir,
                           /*type=*/kFsTypeEfivarfs,
                           /*flags=*/kCommonMountFlags,

init/startup/uefi_startup_test.cc
@@ -135,7 +135,7 @@ TEST_F(UefiDelegateTest, MountEfivarfs) {
   ASSERT_TRUE(base::CreateDirectory(efivars_dir));
 
   EXPECT_CALL(mock_startup_dep_,
-              Mount(kFsTypeEfivarfs, efivars_dir, kFsTypeEfivarfs,
+              Mount(base::FilePath(), efivars_dir, kFsTypeEfivarfs,
                     kCommonMountFlags, "uid=123,gid=456"))
       .WillOnce(Return(true));
 
@@ -207,12 +207,13 @@ TEST_F(UefiDelegateTest, MountEfiSystemPartition) {
   ASSERT_TRUE(base::CreateDirectory(root_dir_.Append("run")));
 
   const base::FilePath esp_dir = root_dir_.Append(kEspDir);
+  const base::FilePath root_dev = base::FilePath("/dev/sda12");
 
   EXPECT_CALL(mock_startup_dep_, GetRootDevicePartitionPath(kEspLabel))
-      .WillOnce(Return(base::FilePath("/dev/sda12")));
+      .WillOnce(Return(root_dev));
   EXPECT_CALL(mock_startup_dep_,
-              Mount(base::FilePath("/dev/sda12"), esp_dir, kFsTypeVfat,
-                    kCommonMountFlags, "uid=123,gid=456,umask=007"))
+              Mount(root_dev, esp_dir, kFsTypeVfat, kCommonMountFlags,
+                    "uid=123,gid=456,umask=007"))
       .WillOnce(Return(true));
 
   EXPECT_TRUE(uefi_delegate_->MountEfiSystemPartition(