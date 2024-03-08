cb8d894b08cd19e230f0384a7800e7d625905f6c
John L Chen <zuan@chromium.org>
2024-03-09 01:19:23 +0800

    arc: Enable provisioning of Device Key in keymint
    This enables libarc-attestation to start preparing the attestation
    credentials when arc-keymint is launched.
    
    BUG=b:327036688
    TEST=emerge-$BOARD arc-keymint
    
    Change-Id: Iffa4d3f6400f298bbaf7edb1ce1e27df9c3d8817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5354798
    Reviewed-by: Vaibhav Raheja <vraheja@chromium.org>
    Reviewed-by: Yao Li <yaohuali@google.com>
    Commit-Queue: John L Chen <zuan@chromium.org>
    Tested-by: John L Chen <zuan@chromium.org>

--- arc/keymint/context/arc_remote_provisioning_context.cc
+++ arc/keymint/context/arc_remote_provisioning_context.cc
@@ -4,13 +4,19 @@
 
 #include "arc/keymint/context/arc_remote_provisioning_context.h"
 
+#include <libarc-attestation/lib/interface.h>
+
 namespace arc::keymint::context {
 
 namespace {}  // namespace
 
 ArcRemoteProvisioningContext::ArcRemoteProvisioningContext(
     keymaster_security_level_t security_level)
-    : PureSoftRemoteProvisioningContext(security_level) {}
+    : PureSoftRemoteProvisioningContext(security_level) {
+  // Signal to libarc-attestation to start provisioning the certificate for
+  // attestation.
+  arc_attestation::ProvisionDkCert(false /* blocking */);
+}
 
 ArcRemoteProvisioningContext::~ArcRemoteProvisioningContext() = default;
 }  // namespace arc::keymint::context
