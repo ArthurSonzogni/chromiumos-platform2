7088177f0c535b6bbc4b5df3c0cabb31944baadf
Ben Scarlato <akhna@google.com>
2024-03-11 01:37:38 +0000

    crosh: write to eventfd on shell window resize
    The --isolated crosh shell needs this to handle resize events.
    
    BUG=b:330373462
    TEST=run `shell --isolated`, run `top` and resize the window
    
    Change-Id: I2de29f6e2414f5432bcf35bb5dcc21b51785ff2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5383371
    Tested-by: Ben Scarlato <akhna@google.com>
    Auto-Submit: Ben Scarlato <akhna@google.com>
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Commit-Queue: Ben Scarlato <akhna@google.com>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Allen Webb <allenwebb@google.com>

--- crosh/src/dev/shell.rs
+++ crosh/src/dev/shell.rs
@@ -5,7 +5,6 @@
 // Provides the command "shell" for crosh which gives developers access to bash if it is available,
 // or dash otherwise.
 
-use std::os::fd::OwnedFd;
 use std::os::unix::io::FromRawFd;
 use std::os::unix::io::IntoRawFd;
 use std::path::Path;
@@ -13,14 +12,26 @@ use std::process;
 
 use crate::dispatcher::{self, wait_for_result, Arguments, Command, Dispatcher};
 use crate::util::{add_epoll_for_fd, epoll_wait, is_no_new_privs_set, DEFAULT_DBUS_TIMEOUT};
+use dbus::arg::OwnedFd;
 use dbus::blocking::Connection;
-use libc::dup;
+use libc::{dup, SIGWINCH};
+use nix::sys::eventfd::{eventfd, EfdFlags};
+use nix::unistd::write;
+use std::sync::atomic::{AtomicBool, Ordering};
+
 use libchromeos::pipe;
 use system_api::client::OrgChromiumDebugd;
+
+use std::os::unix::io::AsRawFd;
+use std::thread::{self, sleep};
+use std::time::Duration;
 
 static DEFAULT_SHELL: &str = "/bin/sh";
 static BASH_SHELL: &str = "/bin/bash";
 static ISOLATED_SHELL: &str = "--isolated";
+static RESIZE_MSG_VAL: u64 = 1u64;
+
+static RESIZE_TRIGGERED: AtomicBool = AtomicBool::new(true);
 
 pub fn register(dispatcher: &mut Dispatcher) {
     dispatcher.register_command(
@@ -33,6 +44,19 @@ pub fn register(dispatcher: &mut Dispatcher) {
     );
 }
 
+fn resize_monitor_thread(event_fd: i32) {
+    loop {
+        if RESIZE_TRIGGERED.swap(false, Ordering::Release) {
+            let _ = write(event_fd.as_raw_fd(), &RESIZE_MSG_VAL.to_le_bytes());
+        }
+        sleep(Duration::from_millis(100));
+    }
+}
+
+fn sigwinch_handler() {
+    RESIZE_TRIGGERED.store(true, Ordering::Release);
+}
+
 fn execute_shell(_cmd: &Command, args: &Arguments) -> Result<(), dispatcher::Error> {
     let tokens = args.get_args();
     if tokens.len() > 1 {
@@ -41,6 +65,16 @@ fn execute_shell(_cmd: &Command, args: &Arguments) -> Result<(), dispatcher::Err
     }
 
     if tokens.contains(&ISOLATED_SHELL.to_owned()) {
+        let event_fd = eventfd(0, EfdFlags::empty()).unwrap();
+        // Safe because the signal handler only updates the value of an AtomicBool.
+        unsafe {
+            let _ = signal_hook_registry::register(SIGWINCH, sigwinch_handler);
+        };
+        // TODO(b/330734519): restore previous SIGWINCH handler on shell exit.
+        let _ = Some(thread::spawn(move || {
+            resize_monitor_thread(event_fd.as_raw_fd())
+        }));
+
         // Set up D-Bus connection for creating a shell in separate process tree.
         let connection = Connection::new_system().map_err(|err| {
             eprintln!("ERROR: Failed to get D-Bus connection: {}", err);
@@ -61,7 +95,7 @@ fn execute_shell(_cmd: &Command, args: &Arguments) -> Result<(), dispatcher::Err
                 // Safe because this will always be the STDIN file descriptor.
                 unsafe { OwnedFd::from_raw_fd(dup(libc::STDIN_FILENO)) },
                 // Safe because this will always be the STDOUT file descriptor.
-                unsafe { OwnedFd::from_raw_fd(dup(libc::STDOUT_FILENO)) },
+                unsafe { OwnedFd::from_raw_fd(event_fd.as_raw_fd()) },
             )
             .map_err(|err| {
                 eprintln!("ERROR: Got unexpected result: {}", err);
