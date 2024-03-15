edf90568bf8131bdfcb4d213f103dbff22df387b
Junichi Uekawa <uekawa@google.com>
2024-03-15 11:09:29 +0900

    vm_tools: vm_concierge: Revert: StartVm becomes StartVm2.
    tast SlimVm tests broke.
    
    BUG=b:324960184
    BUG=b:329763717
    TEST=cq
    TEST=cros_sdk tast run localhost:2232 vm.*Slim*
    
    This reverts commit 2c46fa9bad321a8afb8d3f791db04f760033461e.
    
    Change-Id: Ia1e4949562b09aa3dd1b3ec2aba99957344a66dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5366876
    Commit-Queue: Junichi Uekawa <uekawa@chromium.org>
    Tested-by: Junichi Uekawa <uekawa@chromium.org>
    Reviewed-by: Ryuichiro Chiba <chibar@chromium.org>

--- vm_tools/concierge/service.cc
+++ vm_tools/concierge/service.cc
@@ -283,6 +283,43 @@ constexpr int kVmMemoryManagementHostKillDecisionMsTimeoutDefault = 300;
 const VariationsFeature kVmMemoryManagementServiceFeature{
     kVmMemoryManagementServiceFeatureName, FEATURE_ENABLED_BY_DEFAULT};
 
+std::optional<internal::VmStartImageFds> GetVmStartImageFds(
+    const std::unique_ptr<dbus::MessageReader>& reader,
+    const google::protobuf::RepeatedField<int>& fds) {
+  struct internal::VmStartImageFds result;
+  for (const auto& fdType : fds) {
+    base::ScopedFD fd;
+    if (!reader->PopFileDescriptor(&fd)) {
+      LOG(ERROR) << "Failed to pop VM start image file descriptor";
+      return std::nullopt;
+    }
+    switch (fdType) {
+      case StartVmRequest_FdType_KERNEL:
+        result.kernel_fd = std::move(fd);
+        break;
+      case StartVmRequest_FdType_ROOTFS:
+        result.rootfs_fd = std::move(fd);
+        break;
+      case StartVmRequest_FdType_INITRD:
+        result.initrd_fd = std::move(fd);
+        break;
+      case StartVmRequest_FdType_STORAGE:
+        result.storage_fd = std::move(fd);
+        break;
+      case StartVmRequest_FdType_BIOS:
+        result.bios_fd = std::move(fd);
+        break;
+      case StartVmRequest_FdType_PFLASH:
+        result.pflash_fd = std::move(fd);
+        break;
+      default:
+        LOG(WARNING) << "received request with unknown FD type " << fdType
+                     << ". Ignoring.";
+    }
+  }
+  return result;
+}
+
 std::optional<internal::VmStartImageFds> GetVmStartImageFds2(
     const google::protobuf::RepeatedField<int>& fds,
     const std::vector<base::ScopedFD>& file_handles) {
@@ -1465,15 +1502,44 @@ void Service::Stop(base::OnceClosure on_stopped) {
       FROM_HERE, std::move(on_stopped));
 }
 
-void Service::StartVm2(
-    std::unique_ptr<brillo::dbus_utils::DBusMethodResponse<StartVmResponse>>
-        response_cb,
-    const StartVmRequest& request,
-    const std::vector<base::ScopedFD>& file_handles) {
-  StartVm(std::move(response_cb), request, file_handles);
+void Service::StartVm(dbus::MethodCall* method_call,
+                      dbus::ExportedObject::ResponseSender response_sender) {
+  RAW_SERVICE_METHOD();
+
+  auto reader = std::make_unique<dbus::MessageReader>(method_call);
+
+  StartVmRequest request;
+  StartVmResponse response;
+  // We change to a success status later if necessary.
+  response.set_status(VM_STATUS_FAILURE);
+
+  if (!reader->PopArrayOfBytesAsProto(&request)) {
+    LOG(ERROR) << "Unable to parse StartVmRequest from message";
+    response.set_failure_reason("Unable to parse protobuf");
+    SendDbusResponse(std::move(response_sender), method_call, response);
+    return;
+  }
+
+  if (!CheckStartVmPreconditions(request, &response)) {
+    SendDbusResponse(std::move(response_sender), method_call, response);
+    return;
+  }
+
+  std::optional<internal::VmStartImageFds> vm_start_image_fds =
+      GetVmStartImageFds(reader, request.fds());
+  if (!vm_start_image_fds) {
+    response.set_failure_reason("failed to get a VmStartImage fd");
+    SendDbusResponse(std::move(response_sender), method_call, response);
+    return;
+  }
+
+  response =
+      StartVmInternal(std::move(request), std::move(*vm_start_image_fds));
+  SendDbusResponse(std::move(response_sender), method_call, response);
+  return;
 }
 
-void Service::StartVm(
+void Service::StartVm2(
     std::unique_ptr<brillo::dbus_utils::DBusMethodResponse<StartVmResponse>>
         response_cb,
     const StartVmRequest& request,
