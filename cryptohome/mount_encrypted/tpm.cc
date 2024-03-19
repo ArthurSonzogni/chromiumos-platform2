colemickens/platform2-sommelier

commit e11a3946521b447108fdf1288c3108d040089422

cryptohome: Add encstateful NVRAM space for TPM1
This adds support for the TPM1 system key loading code to get the system key
from a dedicated NVRAM space (as opposed to using the system salt in the
lockbox space). This has a number of benefits, including the ability to
configure a TPM-backed system key when the lockbox space doesn't exist or is
still locked after TPM clear.

BUG=None
TEST=compiles and passes tests

Change-Id: Ib1bb292221b4a2776e31a0a767b26eb9fad46cb5
Reviewed-on: https://chromium-review.googlesource.com/937708
Commit-Ready: Mattias Nissler <mnissler@chromium.org>
Tested-by: Mattias Nissler <mnissler@chromium.org>
Reviewed-by: Andrey Pronin <apronin@chromium.org>

cryptohome/mount_encrypted/tpm.cc
@@ -7,6 +7,11 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <memory>
+#include <utility>
+
+#include <openssl/rsa.h>
+
 #include <base/logging.h>
 #include <base/strings/string_number_conversions.h>
 
@@ -14,26 +19,39 @@
 
 #include <openssl/rand.h>
 
+#include "cryptohome/cryptolib.h"
 #include "cryptohome/mount_encrypted.h"
+
+namespace {
+
+#if !USE_TPM2
+
+// A delegation family label identifying the delegation family we create as a
+// flag that persists until the next TPM clear, at which point it gets cleared
+// automatically. This is by the system key handling logic to determine whether
+// a fresh system key has been generated after the last TPM clear.
+uint8_t kSystemKeyInitializedDummyDelegationFamilyLabel = 0xff;
+
+// Maximum TPM delegation table size.
+const uint32_t kDelegationTableSize = 8;
+
+#endif  // !USE_TPM2
+
+// Initial auth policy buffer size that's expected to be large enough across TPM
+// 1.2 and TPM 2.0 hardware. The code uses this for retrieving auth policies.
+// Note that if the buffer is too small, it retries with the size indicated by
+// the failing function.
+const size_t kInitialAuthPolicySize = 128;
+
+}  // namespace
 
 NvramSpace::NvramSpace(Tpm* tpm, uint32_t index)
   : tpm_(tpm), index_(index) {}
 
 result_code NvramSpace::GetAttributes(uint32_t* attributes) {
-  if (attributes_ != 0) {
-    *attributes = attributes_;
-    return RESULT_SUCCESS;
-  }
-
-  if (!tpm_->available()) {
-    return RESULT_FAIL_FATAL;
-  }
-
-  uint32_t result = TlclGetPermissions(index_, &attributes_);
-  if (result != TPM_SUCCESS) {
-    LOG(ERROR) << "Failed to read NVRAM space attributes for index" << index_
-               << ": " << result;
-    return RESULT_FAIL_FATAL;
+  result_code rc = GetSpaceInfo();
+  if (rc != RESULT_SUCCESS) {
+    return rc;
   }
 
   *attributes = attributes_;
@@ -149,6 +167,104 @@ result_code NvramSpace::Define(uint32_t attributes, uint32_t size) {
   return RESULT_SUCCESS;
 }
 
+result_code NvramSpace::CheckPCRBinding(uint32_t pcr_selection, bool* match) {
+  *match = false;
+
+  std::vector<uint8_t> policy;
+  result_code rc = GetSpaceInfo();
+  if (rc != RESULT_SUCCESS) {
+    return rc;
+  }
+
+  rc = GetPCRBindingPolicy(pcr_selection, &policy);
+  if (rc != RESULT_SUCCESS) {
+    return rc;
+  }
+
+  *match = auth_policy_ == policy;
+  return RESULT_SUCCESS;
+}
+
+result_code NvramSpace::GetSpaceInfo() {
+  if (attributes_ != 0) {
+    return RESULT_SUCCESS;
+  }
+
+  if (!tpm_->available()) {
+    return RESULT_FAIL_FATAL;
+  }
+
+  uint32_t auth_policy_size = kInitialAuthPolicySize;
+  auth_policy_.resize(auth_policy_size);
+  uint32_t size;
+  uint32_t result = TlclGetSpaceInfo(index_, &attributes_, &size,
+                                     auth_policy_.data(), &auth_policy_size);
+  if (result == TPM_E_BUFFER_SIZE && auth_policy_size > 0) {
+    auth_policy_.resize(auth_policy_size);
+    result = TlclGetSpaceInfo(index_, &attributes_, &size, auth_policy_.data(),
+                              &auth_policy_size);
+  }
+  if (result != TPM_SUCCESS) {
+    attributes_ = 0;
+    auth_policy_.clear();
+    LOG(ERROR) << "Failed to read NVRAM space info for index " << index_ << ": "
+               << result;
+    return RESULT_FAIL_FATAL;
+  }
+
+  CHECK_LE(auth_policy_size, auth_policy_.size());
+  auth_policy_.resize(auth_policy_size);
+
+  return RESULT_SUCCESS;
+}
+
+result_code NvramSpace::GetPCRBindingPolicy(uint32_t pcr_selection,
+                                            std::vector<uint8_t>* policy) {
+  if (!tpm_->available()) {
+    return RESULT_FAIL_FATAL;
+  }
+
+  if (pcr_selection == 0) {
+    policy->clear();
+    return RESULT_SUCCESS;
+  }
+
+  int value_index = 0;
+  uint8_t pcr_values[32][TPM_PCR_DIGEST] = {};
+  for (int index = 0; index < 32; ++index) {
+    if (((1 << index) & pcr_selection) != 0) {
+      std::vector<uint8_t> pcr_value;
+      result_code rc = tpm_->ReadPCR(index, &pcr_value);
+      if (rc != RESULT_SUCCESS) {
+        return rc;
+      }
+      CHECK_EQ(TPM_PCR_DIGEST, pcr_value.size());
+      memcpy(pcr_values[value_index++], pcr_value.data(), TPM_PCR_DIGEST);
+    }
+  }
+
+  uint32_t auth_policy_size = kInitialAuthPolicySize;
+  policy->resize(auth_policy_size);
+  uint32_t result = TlclInitNvAuthPolicy(pcr_selection, pcr_values,
+                                         policy->data(), &auth_policy_size);
+  if (result == TPM_E_BUFFER_SIZE && auth_policy_size > 0) {
+    policy->resize(auth_policy_size);
+    result = TlclInitNvAuthPolicy(pcr_selection, pcr_values, policy->data(),
+                                  &auth_policy_size);
+  }
+
+  if (result != TPM_SUCCESS) {
+    policy->clear();
+    LOG(ERROR) << "Failed to get NV policy " << result;
+    return RESULT_FAIL_FATAL;
+  }
+
+  CHECK_LE(auth_policy_size, policy->size());
+  policy->resize(auth_policy_size);
+
+  return RESULT_SUCCESS;
+}
+
 Tpm::Tpm() {
 #if USE_TPM2
   is_tpm2_ = true;
@@ -222,6 +338,32 @@ result_code Tpm::GetRandomBytes(uint8_t* buffer, int wanted) {
   return RESULT_FAIL_FATAL;
 }
 
+result_code Tpm::ReadPCR(uint32_t index, std::vector<uint8_t>* value) {
+  // See whether the PCR is available in the cache. Note that we currently
+  // assume PCR values remain constant during the lifetime of the process, so we
+  // only ever read once.
+  auto entry = pcr_values_.find(index);
+  if (entry != pcr_values_.end()) {
+    *value = entry->second;
+    return RESULT_SUCCESS;
+  }
+
+  if (!available()) {
+    return RESULT_FAIL_FATAL;
+  }
+
+  std::vector<uint8_t> temp_value(TPM_PCR_DIGEST);
+  uint32_t result = TlclPCRRead(index, temp_value.data(), temp_value.size());
+  if (result != TPM_SUCCESS) {
+    LOG(ERROR) << "TPM PCR " << index << " read failed: " << result;
+    return RESULT_FAIL_FATAL;
+  }
+
+  pcr_values_[index] = temp_value;
+  *value = std::move(temp_value);
+  return RESULT_SUCCESS;
+}
+
 NvramSpace* Tpm::GetLockboxSpace() {
   if (lockbox_space_) {
     return lockbox_space_.get();
@@ -267,3 +409,87 @@ NvramSpace* Tpm::GetEncStatefulSpace() {
 
   return encstateful_space_.get();
 }
+
+#if USE_TPM2
+
+result_code Tpm::SetSystemKeyInitializedFlag() {
+  return RESULT_FAIL_FATAL;
+}
+
+result_code Tpm::HasSystemKeyInitializedFlag(bool* flag_value) {
+  return RESULT_FAIL_FATAL;
+}
+
+#else
+
+result_code Tpm::SetSystemKeyInitializedFlag() {
+  bool flag_value = false;
+  result_code rc = HasSystemKeyInitializedFlag(&flag_value);
+  if (rc != TPM_SUCCESS) {
+    return RESULT_FAIL_FATAL;
+  }
+
+  if (flag_value) {
+    return RESULT_SUCCESS;
+  }
+
+  uint32_t result = TlclCreateDelegationFamily(
+      kSystemKeyInitializedDummyDelegationFamilyLabel);
+  if (result != TPM_SUCCESS) {
+    LOG(ERROR) << "Failed to create dummy delegation family: " << result;
+    return RESULT_FAIL_FATAL;
+  }
+
+  initialized_flag_ = true;
+  initialized_flag_checked_ = true;
+
+  return RESULT_SUCCESS;
+}
+
+result_code Tpm::HasSystemKeyInitializedFlag(bool* flag_value) {
+  if (!available()) {
+    return RESULT_FAIL_FATAL;
+  }
+
+  if (initialized_flag_checked_) {
+    *flag_value = initialized_flag_;
+    return RESULT_SUCCESS;
+  }
+
+  // The dummy delegation family is only relevant for unowned TPMs. Pretend the
+  // flag is present if the TPM is owned.
+  bool owned = false;
+  result_code rc = IsOwned(&owned);
+  if (rc != RESULT_SUCCESS) {
+    LOG(ERROR) << "Failed to determine ownership.";
+    return rc;
+  }
+  if (owned) {
+    initialized_flag_checked_ = true;
+    initialized_flag_ = true;
+    *flag_value = initialized_flag_;
+    return RESULT_SUCCESS;
+  }
+
+  TPM_FAMILY_TABLE_ENTRY table[kDelegationTableSize];
+  uint32_t table_size = kDelegationTableSize;
+  uint32_t result = TlclReadDelegationFamilyTable(table, &table_size);
+  if (result != TPM_SUCCESS) {
+    LOG(ERROR) << "Failed to read delegation family table: " << result;
+    return RESULT_FAIL_FATAL;
+  }
+
+  for (uint32_t i = 0; i < table_size; ++i) {
+    if (table[i].familyLabel ==
+        kSystemKeyInitializedDummyDelegationFamilyLabel) {
+      initialized_flag_ = true;
+      break;
+    }
+  }
+
+  initialized_flag_checked_ = true;
+  *flag_value = initialized_flag_;
+  return RESULT_SUCCESS;
+}
+
+#endif

cryptohome/cryptohome.gyp
@@ -202,6 +202,9 @@
           'vboot_host',
         ],
       },
+      'defines': [
+        'CHROMEOS_ENVIRONMENT=1',
+      ],
       'conditions': [
         ['USE_tpm2 == 1', {
           'defines': [

cryptohome/mount_encrypted/encryption_key_unittest.cc
@@ -11,6 +11,8 @@
 #include <base/files/scoped_temp_dir.h>
 #include <base/logging.h>
 
+#include <openssl/sha.h>
+
 #include <vboot/tlcl.h>
 
 #include <gtest/gtest.h>
@@ -36,6 +38,12 @@ const size_t kWrappedKeySize = 48;
 const uint32_t kEncStatefulAttributesTpm2 =
     TPMA_NV_AUTHWRITE | TPMA_NV_AUTHREAD | TPMA_NV_WRITEDEFINE |
     TPMA_NV_READ_STCLEAR | TPMA_NV_WRITTEN;
+
+const uint8_t kPCRBootModeValue[] = {
+    0x89, 0xea, 0xf3, 0x51, 0x34, 0xb4, 0xb3, 0xc6, 0x49, 0xf4, 0x4c,
+    0x0c, 0x76, 0x5b, 0x96, 0xae, 0xab, 0x8b, 0xb3, 0x4e, 0xe8, 0x3c,
+    0xc7, 0xa6, 0x83, 0xc4, 0xe5, 0x3d, 0x15, 0x81, 0xc8, 0xc7,
+};
 
 // NVRAM space contents used in tests that exercise behavior with existing
 // valid NVRAM space contents. Contains a random system key.
@@ -64,7 +72,14 @@ const uint8_t kWrappedKeyEncStatefulTpm2[] = {
 
 #else  // USE_TPM2
 
+const uint32_t kEncStatefulAttributesTpm1 =
+    TPM_NV_PER_WRITE_STCLEAR | TPM_NV_PER_READ_STCLEAR;
 const uint32_t kLockboxAttributesTpm1 = TPM_NV_PER_WRITEDEFINE;
+
+const uint8_t kPCRBootModeValue[] = {
+    0x06, 0x4a, 0xec, 0x9b, 0xbd, 0x94, 0xde, 0xa1, 0x23, 0x1a,
+    0xe7, 0x57, 0x67, 0x64, 0x7f, 0x09, 0x8c, 0x39, 0x8e, 0x79,
+};
 
 // NVRAM space contents used in tests that exercise behavior with existing
 // valid NVRAM space contents. This contains a random "lockbox salt", which
@@ -111,6 +126,34 @@ const uint8_t kWrappedKeyNeedsFinalization[] = {
     0x3d, 0xef, 0x01, 0x92, 0x9c, 0x89, 0x15, 0x85, 0xc5, 0xe5, 0x78, 0x10,
 };
 
+// Contents of the encstateful TPM NVRAM space used in tests that set up
+// existing valid NVRAM space contents. Contains random system key material.
+const uint8_t kEncStatefulTpm1Contents[] = {
+    0x31, 0x4D, 0x50, 0x54, 0x01, 0x00, 0x00, 0x00, 0xa3, 0xea, 0xd7, 0x78,
+    0xa6, 0xb4, 0x74, 0xd7, 0x8f, 0xa1, 0x9a, 0xbd, 0x04, 0x6a, 0xc5, 0x6c,
+    0x21, 0xc7, 0x60, 0x1c, 0x45, 0xe3, 0x06, 0xe2, 0x6a, 0x68, 0x94, 0x96,
+    0x8b, 0x1a, 0xf3, 0x67, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+// A random encryption key used in tests that exercise the situation where the
+// encstateful NVRAM space already exists.
+const uint8_t kEncryptionKeyEncStatefulTpm1[] = {
+    0x7c, 0xdd, 0x2f, 0xba, 0x4b, 0x6d, 0x28, 0x5b, 0xa0, 0x5a, 0xa5,
+    0x84, 0x82, 0x41, 0x02, 0x36, 0x7a, 0x17, 0xc6, 0xe4, 0x78, 0x0e,
+    0x86, 0x50, 0x6c, 0x09, 0x50, 0x5c, 0x33, 0x57, 0x19, 0xae,
+};
+
+// This is kEncryptionKeyEncStatefulTpm1, encrypted with the system key from
+// kEncStatefulTpm1Contents.
+const uint8_t kWrappedKeyEncStatefulTpm1[] = {
+    0x7d, 0x10, 0x2a, 0x73, 0x20, 0xd2, 0x29, 0xe8, 0x27, 0xeb, 0xfd, 0xc0,
+    0x57, 0xd8, 0x03, 0x16, 0x3c, 0xb7, 0x00, 0x80, 0x56, 0xf9, 0x93, 0x84,
+    0xb6, 0xb7, 0xcb, 0xfb, 0x42, 0x59, 0x2b, 0x07, 0xd5, 0x00, 0xa4, 0x8d,
+    0x9c, 0x70, 0x9d, 0x15, 0x80, 0xe3, 0x75, 0xea, 0x7b, 0x72, 0x9c, 0xe8,
+};
+
 #endif  // !USE_TPM2
 
 }  // namespace
@@ -124,6 +167,8 @@ class EncryptionKeyTest : public testing::Test {
     tpm_ = std::make_unique<Tpm>();
     loader_ = SystemKeyLoader::Create(tpm_.get());
     key_ = std::make_unique<EncryptionKey>(loader_.get(), tmpdir_.GetPath());
+
+    tlcl_.SetPCRValue(kPCRBootMode, kPCRBootModeValue);
   }
 
   void SetOwned() {
@@ -132,11 +177,22 @@ class EncryptionKeyTest : public testing::Test {
 
   void SetupSpace(uint32_t index,
                   uint32_t attributes,
+                  bool bind_to_pcr0,
                   const uint8_t* data,
                   size_t size) {
     TlclStub::NvramSpaceData* space = tlcl_.GetSpace(index);
     space->contents.assign(data, data + size);
     space->attributes = attributes;
+
+    if (bind_to_pcr0) {
+      uint32_t policy_size = SHA256_DIGEST_LENGTH;
+      space->policy.resize(policy_size);
+      uint8_t pcr_values[1][TPM_PCR_DIGEST] = {};
+      memcpy(pcr_values[0], kPCRBootModeValue, TPM_PCR_DIGEST);
+      ASSERT_EQ(TPM_SUCCESS,
+                TlclInitNvAuthPolicy((1 << kPCRBootMode), pcr_values,
+                                     space->policy.data(), &policy_size));
+    }
   }
 
   void WriteWrappedKey(const base::FilePath& path, const uint8_t* key) {
@@ -210,7 +266,7 @@ TEST_F(EncryptionKeyTest, TpmOwnedNoSpaces) {
 }
 
 TEST_F(EncryptionKeyTest, TpmExistingSpaceNoKeyFile) {
-  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm2,
+  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm2, false,
              kEncStatefulTpm2Contents, sizeof(kEncStatefulTpm2Contents));
 
   ExpectFreshKey();
@@ -220,7 +276,7 @@ TEST_F(EncryptionKeyTest, TpmExistingSpaceNoKeyFile) {
 }
 
 TEST_F(EncryptionKeyTest, TpmExistingSpaceBadKey) {
-  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm2,
+  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm2, false,
              kEncStatefulTpm2Contents, sizeof(kEncStatefulTpm2Contents));
   std::vector<uint8_t> wrapped_key(sizeof(kWrappedKeyEncStatefulTpm2), 0xa5);
   WriteWrappedKey(key_->key_path(), wrapped_key.data());
@@ -234,7 +290,7 @@ TEST_F(EncryptionKeyTest, TpmExistingSpaceBadKey) {
 TEST_F(EncryptionKeyTest, TpmExistingSpaceBadAttributes) {
   // TODO(crbug.com/840361): See bug description.
   uint32_t attributes = kEncStatefulAttributesTpm2 | TPMA_NV_PLATFORMCREATE;
-  SetupSpace(kEncStatefulIndex, attributes,
+  SetupSpace(kEncStatefulIndex, attributes, false,
              kEncStatefulTpm2Contents, sizeof(kEncStatefulTpm2Contents));
 
   ExpectFreshKey();
@@ -245,7 +301,7 @@ TEST_F(EncryptionKeyTest, TpmExistingSpaceBadAttributes) {
 
 TEST_F(EncryptionKeyTest, TpmExistingSpaceNotYetWritten) {
   SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm2 & ~TPMA_NV_WRITTEN,
-             kEncStatefulTpm2Contents, sizeof(kEncStatefulTpm2Contents));
+             false, kEncStatefulTpm2Contents, sizeof(kEncStatefulTpm2Contents));
 
   ExpectFreshKey();
   EXPECT_FALSE(key_->is_migration_allowed());
@@ -255,7 +311,7 @@ TEST_F(EncryptionKeyTest, TpmExistingSpaceNotYetWritten) {
 
 TEST_F(EncryptionKeyTest, TpmExistingSpaceBadContents) {
   std::vector<uint8_t> bad_contents(sizeof(kEncStatefulTpm2Contents), 0xa5);
-  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm2,
+  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm2, false,
              bad_contents.data(), bad_contents.size());
 
   ExpectFreshKey();
@@ -265,7 +321,7 @@ TEST_F(EncryptionKeyTest, TpmExistingSpaceBadContents) {
 }
 
 TEST_F(EncryptionKeyTest, TpmExistingSpaceValid) {
-  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm2,
+  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm2, false,
              kEncStatefulTpm2Contents, sizeof(kEncStatefulTpm2Contents));
   WriteWrappedKey(key_->key_path(), kWrappedKeyEncStatefulTpm2);
 
@@ -292,7 +348,7 @@ TEST_F(EncryptionKeyTest, TpmOwnedNoSpaces) {
 }
 
 TEST_F(EncryptionKeyTest, TpmClearExistingLockboxV2Unowned) {
-  SetupSpace(kLockboxIndex, kLockboxAttributesTpm1, kLockboxV2Contents,
+  SetupSpace(kLockboxIndex, kLockboxAttributesTpm1, true, kLockboxV2Contents,
              sizeof(kLockboxV2Contents));
 
   ExpectFreshKey();
@@ -301,7 +357,7 @@ TEST_F(EncryptionKeyTest, TpmClearExistingLockboxV2Unowned) {
 }
 
 TEST_F(EncryptionKeyTest, TpmOwnedExistingLockboxV2Finalize) {
-  SetupSpace(kLockboxIndex, kLockboxAttributesTpm1, kLockboxV2Contents,
+  SetupSpace(kLockboxIndex, kLockboxAttributesTpm1, true, kLockboxV2Contents,
              sizeof(kLockboxV2Contents));
   SetOwned();
 
@@ -311,7 +367,7 @@ TEST_F(EncryptionKeyTest, TpmOwnedExistingLockboxV2Finalize) {
 }
 
 TEST_F(EncryptionKeyTest, TpmOwnedExistingLockboxV2Finalized) {
-  SetupSpace(kLockboxIndex, kLockboxAttributesTpm1, kLockboxV2Contents,
+  SetupSpace(kLockboxIndex, kLockboxAttributesTpm1, true, kLockboxV2Contents,
              sizeof(kLockboxV2Contents));
   SetOwned();
   WriteWrappedKey(key_->key_path(), kWrappedKeyLockboxV2);
@@ -322,7 +378,7 @@ TEST_F(EncryptionKeyTest, TpmOwnedExistingLockboxV2Finalized) {
 }
 
 TEST_F(EncryptionKeyTest, TpmOwnedExistingLockboxV2BadDecrypt) {
-  SetupSpace(kLockboxIndex, kLockboxAttributesTpm1, kLockboxV2Contents,
+  SetupSpace(kLockboxIndex, kLockboxAttributesTpm1, true, kLockboxV2Contents,
              sizeof(kLockboxV2Contents));
   SetOwned();
   std::vector<uint8_t> wrapped_key(sizeof(kWrappedKeyLockboxV2), 0xa5);
@@ -350,6 +406,104 @@ TEST_F(EncryptionKeyTest, TpmOwnedNeedsFinalization) {
   ExpectExistingKey(kEncryptionKeyNeedsFinalization);
   EXPECT_TRUE(key_->is_migration_allowed());
   ExpectNeedsFinalization();
+}
+
+TEST_F(EncryptionKeyTest, EncStatefulTpmClearExisting) {
+  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm1, true,
+             kEncStatefulTpm1Contents, sizeof(kEncStatefulTpm1Contents));
+  SetupSpace(kLockboxIndex, kLockboxAttributesTpm1, true, kLockboxV2Contents,
+             sizeof(kLockboxV2Contents));
+
+  ExpectFreshKey();
+  EXPECT_TRUE(key_->is_migration_allowed());
+  ExpectFinalized(true);
+  bool initialized = false;
+  EXPECT_EQ(RESULT_SUCCESS, tpm_->HasSystemKeyInitializedFlag(&initialized));
+  EXPECT_TRUE(initialized);
+  CheckSpace(kEncStatefulIndex, kEncStatefulAttributesTpm1, kEncStatefulSize);
+
+  TlclStub::NvramSpaceData* space = tlcl_.GetSpace(kEncStatefulIndex);
+  EXPECT_NE(space->contents,
+            std::vector<uint8_t>(
+                kEncStatefulTpm1Contents,
+                kEncStatefulTpm1Contents + sizeof(kEncStatefulTpm1Contents)));
+}
+
+TEST_F(EncryptionKeyTest, EncStatefulTpmClearInitialized) {
+  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm1, true,
+             kEncStatefulTpm1Contents, sizeof(kEncStatefulTpm1Contents));
+  tpm_->SetSystemKeyInitializedFlag();
+  WriteWrappedKey(key_->key_path(), kWrappedKeyEncStatefulTpm1);
+
+  ExpectExistingKey(kEncryptionKeyEncStatefulTpm1);
+  EXPECT_FALSE(key_->is_migration_allowed());
+  ExpectFinalized(false);
+  CheckSpace(kEncStatefulIndex, kEncStatefulAttributesTpm1, kEncStatefulSize);
+}
+
+TEST_F(EncryptionKeyTest, EncStatefulTpmOwnedExisting) {
+  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm1, true,
+             kEncStatefulTpm1Contents, sizeof(kEncStatefulTpm1Contents));
+  SetupSpace(kLockboxIndex, kLockboxAttributesTpm1, true, kLockboxV2Contents,
+             sizeof(kLockboxV2Contents));
+  SetOwned();
+  WriteWrappedKey(key_->key_path(), kWrappedKeyEncStatefulTpm1);
+
+  ExpectExistingKey(kEncryptionKeyEncStatefulTpm1);
+  EXPECT_FALSE(key_->is_migration_allowed());
+  ExpectFinalized(false);
+  CheckSpace(kEncStatefulIndex, kEncStatefulAttributesTpm1, kEncStatefulSize);
+}
+
+TEST_F(EncryptionKeyTest, EncStatefulTpmClearBadPCRBinding) {
+  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm1, false,
+             kEncStatefulTpm1Contents, sizeof(kEncStatefulTpm1Contents));
+
+  ExpectFreshKey();
+  EXPECT_TRUE(key_->is_migration_allowed());
+  ExpectNeedsFinalization();
+}
+
+TEST_F(EncryptionKeyTest, EncStatefulTpmClearBadSize) {
+  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm1, true,
+             kEncStatefulTpm1Contents, sizeof(kEncStatefulTpm1Contents) - 1);
+
+  ExpectFreshKey();
+  EXPECT_TRUE(key_->is_migration_allowed());
+  ExpectNeedsFinalization();
+}
+
+TEST_F(EncryptionKeyTest, EncStatefulTpmClearBadAttributes) {
+  SetupSpace(kEncStatefulIndex, 0, true, kEncStatefulTpm1Contents,
+             sizeof(kEncStatefulTpm1Contents));
+
+  ExpectFreshKey();
+  EXPECT_TRUE(key_->is_migration_allowed());
+  ExpectNeedsFinalization();
+}
+
+TEST_F(EncryptionKeyTest, EncStatefulTpmClearBadContents) {
+  std::vector<uint8_t> bad_contents(sizeof(kEncStatefulTpm1Contents), 0xa5);
+  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm1, true,
+             bad_contents.data(), bad_contents.size());
+
+  ExpectFreshKey();
+  EXPECT_TRUE(key_->is_migration_allowed());
+  ExpectFinalized(true);
+  CheckSpace(kEncStatefulIndex, kEncStatefulAttributesTpm1, kEncStatefulSize);
+}
+
+TEST_F(EncryptionKeyTest, EncStatefulTpmOwnedBadSpaceLockboxFallback) {
+  SetupSpace(kEncStatefulIndex, kEncStatefulAttributesTpm1, false,
+             kEncStatefulTpm1Contents, sizeof(kEncStatefulTpm1Contents));
+  SetupSpace(kLockboxIndex, kLockboxAttributesTpm1, true, kLockboxV2Contents,
+             sizeof(kLockboxV2Contents));
+  SetOwned();
+  WriteWrappedKey(key_->key_path(), kWrappedKeyLockboxV2);
+
+  ExpectExistingKey(kEncryptionKeyLockboxV2);
+  EXPECT_FALSE(key_->is_migration_allowed());
+  ExpectFinalized(false);
 }
 
 #endif  // !USE_TPM2

cryptohome/mount_encrypted/tlcl_stub.cc
@@ -8,7 +8,23 @@
 
 #include <base/logging.h>
 
+#include <openssl/sha.h>
+
 #include <vboot/tlcl.h>
+
+#include <brillo/secure_blob.h>
+
+#include "cryptohome/cryptolib.h"
+
+namespace {
+
+#if !USE_TPM2
+
+const size_t kMaxDelegationFamilyTableSize = 8;
+
+#endif  // !USE_TPM2
+
+}  // namespace
 
 TlclStub* TlclStub::g_instance = nullptr;
 
@@ -26,6 +42,11 @@ TlclStub::NvramSpaceData* TlclStub::GetSpace(uint32_t index) {
 
 void TlclStub::SetOwned(const std::vector<uint8_t>& owner_auth) {
   owner_auth_ = owner_auth;
+}
+
+void TlclStub::SetPCRValue(uint32_t index,
+                           const uint8_t value[TPM_PCR_DIGEST]) {
+  memcpy(pcr_values_[index], value, TPM_PCR_DIGEST);
 }
 
 TlclStub* TlclStub::Get() {
@@ -66,6 +87,25 @@ uint32_t TlclStub::DefineSpace(uint32_t index, uint32_t perm, uint32_t size) {
 uint32_t TlclStub::GetPermissions(uint32_t index, uint32_t* permissions) {
   return WithSpace(index, [=](NvramSpaceData* space) {
     *permissions = space->attributes;
+    return TPM_SUCCESS;
+  });
+}
+
+uint32_t TlclStub::GetSpaceInfo(uint32_t index,
+                                uint32_t* permissions,
+                                uint32_t* size,
+                                void* auth_policy,
+                                uint32_t* auth_policy_size) {
+  return WithSpace(index, [=](NvramSpaceData* space) {
+    if (space->policy.size() > *auth_policy_size) {
+      *auth_policy_size = space->policy.size();
+      return TPM_E_BUFFER_SIZE;
+    }
+
+    *permissions = space->attributes;
+    *size = space->contents.size();
+    memcpy(auth_policy, space->policy.data(), space->policy.size());
+    *auth_policy_size = space->policy.size();
     return TPM_SUCCESS;
   });
 }
@@ -125,6 +165,53 @@ uint32_t TlclStub::ReadLock(uint32_t index) {
   });
 }
 
+uint32_t TlclStub::PCRRead(uint32_t index, void *data, uint32_t length) {
+  if (length < TPM_PCR_DIGEST) {
+    return TPM_E_BUFFER_SIZE;
+  }
+
+  auto entry = pcr_values_.find(index);
+  if (entry != pcr_values_.end()) {
+    memcpy(data, entry->second, TPM_PCR_DIGEST);
+  } else {
+    memset(data, 0, TPM_PCR_DIGEST);
+  }
+
+  return TPM_SUCCESS;
+}
+
+#if !USE_TPM2
+
+uint32_t TlclStub::CreateDelegationFamily(uint8_t family_label) {
+  if (is_owned()) {
+    return TPM_E_OWNER_SET;
+  }
+
+  if (delegation_family_table_.size() >= kMaxDelegationFamilyTableSize) {
+    return TPM_E_INTERNAL_ERROR;
+  }
+
+  delegation_family_table_.push_back(
+      {0, family_label, ++delegation_family_id_, 1, 0});
+
+  return TPM_SUCCESS;
+}
+
+uint32_t TlclStub::ReadDelegationFamilyTable(TPM_FAMILY_TABLE_ENTRY* table,
+                                             uint32_t* table_size) {
+  if (*table_size < delegation_family_table_.size()) {
+    return TPM_E_BUFFER_SIZE;
+  }
+
+  *table_size = delegation_family_table_.size();
+  std::copy(delegation_family_table_.begin(), delegation_family_table_.end(),
+            table);
+
+  return TPM_SUCCESS;
+}
+
+#endif  // !USE_TPM2
+
 template<typename Action>
 uint32_t TlclStub::WithSpace(uint32_t index, Action action) {
   auto entry = nvram_spaces_.find(index);
@@ -163,6 +250,15 @@ uint32_t TlclGetPermissions(uint32_t index, uint32_t *permissions) {
   return TlclStub::Get()->GetPermissions(index, permissions);
 }
 
+uint32_t TlclGetSpaceInfo(uint32_t index,
+                          uint32_t* attributes,
+                          uint32_t* size,
+                          void* auth_policy,
+                          uint32_t* auth_policy_size) {
+  return TlclStub::Get()->GetSpaceInfo(index, attributes, size, auth_policy,
+                                       auth_policy_size);
+}
+
 uint32_t TlclWrite(uint32_t index, const void *data, uint32_t length) {
   return TlclStub::Get()->Write(index, data, length);
 }
@@ -178,5 +274,46 @@ uint32_t TlclWriteLock(uint32_t index) {
 uint32_t TlclReadLock(uint32_t index) {
   return TlclStub::Get()->ReadLock(index);
 }
+
+uint32_t TlclPCRRead(uint32_t index, void *data, uint32_t length) {
+  return TlclStub::Get()->PCRRead(index, data, length);
+}
+
+uint32_t TlclInitNvAuthPolicy(uint32_t pcr_selection_bitmap,
+                              const uint8_t pcr_values[][TPM_PCR_DIGEST],
+                              void* auth_policy,
+                              uint32_t* auth_policy_size) {
+  int buffer_size = *auth_policy_size;
+  *auth_policy_size = SHA256_DIGEST_LENGTH;
+  if (buffer_size < SHA256_DIGEST_LENGTH) {
+    return TPM_E_BUFFER_SIZE;
+  }
+
+  std::vector<uint8_t> input(32);
+  for (int index = 0; index < 32; ++index) {
+    input[index] = (pcr_selection_bitmap & (1 << index)) != 0;
+    if (input[index]) {
+      input.insert(input.end(), *pcr_values, *pcr_values + TPM_PCR_DIGEST);
+      ++pcr_values;
+    }
+  }
+
+  brillo::SecureBlob digest = cryptohome::CryptoLib::Sha256(input);
+  memcpy(auth_policy, digest.data(), digest.size());
+  return TPM_SUCCESS;
+}
+
+#if !USE_TPM2
+
+uint32_t TlclCreateDelegationFamily(uint8_t family_label) {
+  return TlclStub::Get()->CreateDelegationFamily(family_label);
+}
+
+uint32_t TlclReadDelegationFamilyTable(TPM_FAMILY_TABLE_ENTRY *table,
+                                       uint32_t* table_size) {
+  return TlclStub::Get()->ReadDelegationFamilyTable(table, table_size);
+}
+
+#endif  // !USE_TPM2
 
 }  // extern "C"

ArthurSonzogni/chromiumos-platform2

commit cc1d335712613bca832ee1439dffaef0382a9a16

mount-encrypted: Fix invalid constant check
Use proper constant when checking result of
HasSystemKeyInitializedFlag().

BUG=none
TEST=Unit test of arm-generic.

Fixes: commit e11a394652 ("cryptohome: Add encstateful NVRAM space for TPM1")
Change-Id: Iffebd54e4994184c2b359d597c08cbd7445ba3b7
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5379064
Reviewed-by: Andrey Pronin <apronin@chromium.org>
Auto-Submit: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Andrey Pronin <apronin@chromium.org>

cryptohome/mount_encrypted/tpm.cc
@@ -538,7 +538,7 @@ result_code Tpm::TakeOwnership() {
 result_code Tpm::SetSystemKeyInitializedFlag() {
   bool flag_value = false;
   result_code rc = HasSystemKeyInitializedFlag(&flag_value);
-  if (rc != TPM_SUCCESS) {
+  if (rc != RESULT_SUCCESS) {
     return RESULT_FAIL_FATAL;
   }
 