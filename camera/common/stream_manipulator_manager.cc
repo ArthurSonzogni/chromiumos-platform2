5a88155dbf38145dce8eda377794f306cf49ec4d
Juliana Chang <julianachang@chromium.org>
2024-03-22 06:24:25 +0000

    Revert "camera: Move EffectsSM before FramingSM"
    This reverts commit 0e9127684a88d55b35243017f590340c48112351.
    
    Reason for revert: The BLOB from FramingSM will be discarded, we will need some refactoring for this issue. 
    
    Original change's description:
    > camera: Move EffectsSM before FramingSM
    >
    > Activate the EffectsStreamManipulator before
    > FramingStreamManipulator in order to avoid processing
    > large YUV buffers added by the FramingStreamManipulator.
    > These large frames are removed once they are consumed by
    > the FramingSM, thus EffectsSM doesn't need to apply
    > effects on those.
    >
    > BUG=None
    > TEST=CCA and tast.camera.CCAUIRecordVideoPerf
    >
    > Disallow-Recycled-Builds: test-failures
    > Change-Id: Ide8b618fcca502cda989a90cb59a52636d624b1e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5377417
    > Auto-Submit: Imran Ziad <imranziad@chromium.org>
    > Reviewed-by: Ren-Pei Zeng <kamesan@chromium.org>
    > Tested-by: Imran Ziad <imranziad@chromium.org>
    > Commit-Queue: Ren-Pei Zeng <kamesan@chromium.org>
    
    BUG=None
    
    Change-Id: Id8e22b7f37cf16e638fda9f8816d8ff7b00223b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5380133
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Imran Ziad <imranziad@chromium.org>
    Commit-Queue: Juliana Chang <julianachang@chromium.org>

--- camera/common/stream_manipulator_manager.cc
+++ camera/common/stream_manipulator_manager.cc
@@ -224,6 +224,9 @@ StreamManipulatorManager::StreamManipulatorManager(
                   CameraMojoChannelManager::GetInstance()))));
   LOGF(INFO) << "RotateAndCropStreamManipulator enabled";
 
+  MaybeEnableFramingStreamManipulator(feature_profile, runtime_options,
+                                      gpu_resources, &stream_manipulators_);
+
 #if USE_CAMERA_FEATURE_EFFECTS
   LOGF(INFO) << "Service built with effects support";
   if (feature_profile.IsEnabled(FeatureProfile::FeatureType::kEffects)) {
@@ -242,9 +245,6 @@ StreamManipulatorManager::StreamManipulatorManager(
 #else
   LOGF(INFO) << "Service built without effects support";
 #endif
-
-  MaybeEnableFramingStreamManipulator(feature_profile, runtime_options,
-                                      gpu_resources, &stream_manipulators_);
 
 #if USE_CAMERA_FEATURE_DIAGNOSTICS
   if (create_options.diagnostics_config != nullptr) {
