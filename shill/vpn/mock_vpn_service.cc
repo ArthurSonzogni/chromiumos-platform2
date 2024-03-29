581bbb06a981e5d336b143ef7022c286e3c1eb92
Jie Jiang <jiejiang@chromium.org>
2024-03-29 07:52:06 +0000

    shill: vpn: VPNService ctor no longer checks driver
    Remove the test-only code.
    
    BUG=None
    TEST=FEATURES="test" emerge-$BOARD shill
    
    Change-Id: If67b933cf6484c1889cda31689a3ae2201cca045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5401645
    Tested-by: Jie Jiang <jiejiang@chromium.org>
    Commit-Queue: Jie Jiang <jiejiang@chromium.org>
    Reviewed-by: Taoyu Li <taoyl@chromium.org>

--- shill/vpn/mock_vpn_service.cc
+++ shill/vpn/mock_vpn_service.cc
@@ -6,13 +6,16 @@
 
 #include <utility>
 
+#include "shill/vpn/mock_vpn_driver.h"
 #include "shill/vpn/vpn_driver.h"
 
 namespace shill {
 
 MockVPNService::MockVPNService(Manager* manager,
                                std::unique_ptr<VPNDriver> driver)
-    : VPNService(manager, std::move(driver)) {}
+    : VPNService(
+          manager,
+          driver ? std::move(driver) : std::make_unique<MockVPNDriver>()) {}
 
 MockVPNService::~MockVPNService() = default;
 
--- shill/vpn/vpn_service.cc
+++ shill/vpn/vpn_service.cc
@@ -85,13 +85,9 @@ VPNService::VPNService(Manager* manager, std::unique_ptr<VPNDriver> driver)
     : Service(manager, Technology::kVPN),
       driver_(std::move(driver)),
       last_default_physical_service_online_(manager->IsOnline()) {
-  if (driver_) {
-    log_name_ = "vpn_" + VPNTypeEnumToString(driver_->vpn_type()) + "_" +
-                base::NumberToString(serial_number());
-  } else {
-    // |driver| may be null in tests.
-    log_name_ = "vpn_" + base::NumberToString(serial_number());
-  }
+  DCHECK(driver_);
+  log_name_ = base::StrCat({"vpn_", VPNTypeEnumToString(driver_->vpn_type()),
+                            "_", base::NumberToString(serial_number())});
   SetConnectable(true);
   set_save_credentials(false);
   mutable_store()->RegisterDerivedString(
