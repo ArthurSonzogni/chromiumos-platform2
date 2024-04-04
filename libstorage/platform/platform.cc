ArthurSonzogni/chromiumos-platform2

commit 33c1ae48d5138f631b6ec329a9a177d784c92971

libstorage: platform: Mount: Use fs type when the source is empty
When calling Mount() for in kernel filesystem, |from| is meaningless; we
use the filesystem type as a convention.
But platform Mount() requires |from| to be absolute, preventing setting
the filesystem type.
Allow empty |from|, use |type| as |from| when the caller send an empty path.

BUG=b:172221451
TEST=Unit tests. Check `mount` output on DUTs:

Change-Id: I3b270d70a26a9f5684f042eb1094242e23dced8e
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5387035
Reviewed-by: Daniil Lunev <dlunev@chromium.org>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>

libstorage/platform/platform.cc
@@ -288,10 +288,11 @@ bool Platform::Mount(const FilePath& from,
                      const std::string& type,
                      uint32_t mount_flags,
                      const std::string& mount_options) {
-  DCHECK(from.IsAbsolute()) << "from=" << from;
+  DCHECK(from.IsAbsolute() || from.value().empty()) << "from=" << from;
   DCHECK(to.IsAbsolute()) << "to=" << to;
 
-  if (mount(from.value().c_str(), to.value().c_str(), type.c_str(), mount_flags,
+  std::string from_path = !from.value().empty() ? from.value() : type;
+  if (mount(from_path.c_str(), to.value().c_str(), type.c_str(), mount_flags,
             mount_options.c_str())) {
     return false;
   }

commit 886db390d480c8f30b13a44195f4ca46d111c601

libstorage: allow relative from path for tpmfs mount
Relax DCHECK for Mount: the "from" argument can be a relative path when
the type of mount is "tmpfs":

For instance, /media is mount as tmpfs media:
/media rw,nosuid,nodev,noexec,relatime shared:43 - tmpfs media rw,seclabel

BUG=b:172221451
TEST=Check chromeos_startup does not abort on brya.

Fixes: 33c1ae48d5138 ("libstorage: platform: Mount: Use fs type when the source is empty")
Change-Id: I2b234ae8ed5af42c59745eb01d67312c75cde5c5
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5414894
Reviewed-by: Alexis Savery <asavery@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>

libstorage/platform/platform.cc
@@ -104,6 +104,7 @@ constexpr char kPathDumpe2fs[] = "/sbin/dumpe2fs";
 constexpr char kPathE2fsck[] = "/sbin/e2fsck";
 constexpr char kPathTune2fs[] = "/sbin/tune2fs";
 constexpr char kEcryptFS[] = "ecryptfs";
+constexpr char kTmpFS[] = "tmpfs";
 
 bool IsDirectory(const base::stat_wrapper_t& file_info) {
   return !!S_ISDIR(file_info.st_mode);
@@ -288,7 +289,8 @@ bool Platform::Mount(const FilePath& from,
                      const std::string& type,
                      uint32_t mount_flags,
                      const std::string& mount_options) {
-  DCHECK(from.IsAbsolute() || from.value().empty()) << "from=" << from;
+  DCHECK(from.IsAbsolute() || from.value().empty() || type == kTmpFS)
+      << "from=" << from;
   DCHECK(to.IsAbsolute()) << "to=" << to;
 
   std::string from_path = !from.value().empty() ? from.value() : type;