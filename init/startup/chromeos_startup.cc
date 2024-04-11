ArthurSonzogni/chromiumos-platform2

commit c236c66dfff22035fe0e265eb68aff6231b9054a

init: Preserve logical volumes during select clobber
.. this feature preserves logical volumes during select clobber
triggering flows. However, we do not want to enable it as the default
because there are cases where thinpool or other transitions might be
happening that do not make logical sense to use the `preserve_lvs`
options.

BUG=b:222344877
TEST=emerge + CQ

Change-Id: I22c5d1ca227acfa9a21d537b1c757738ae29beb8
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4903511
Reviewed-by: Sarthak Kukreti <sarthakkukreti@google.com>
Tested-by: Jae Hoon Kim <kimjae@chromium.org>
Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>

init/startup/chromeos_startup.cc
@@ -230,7 +230,7 @@ void ChromeosStartup::ForceCleanFileAttrs(const base::FilePath& path) {
   bool status = file_attrs_cleaner::ScanDir(path.value(), skip);
 
   if (!status) {
-    std::vector<std::string> args = {"keepimg"};
+    std::vector<std::string> args = {"keepimg", "preserve_lvs"};
     platform_->Clobber(
         "self-repair", args,
         std::string("Bad file attrs under ").append(path.value()));
@@ -399,6 +399,7 @@ void ChromeosStartup::CheckForStatefulWipe() {
     // If it's not a plain file owned by us, force a powerwash.
     if (stbuf.st_uid != getuid() || !S_ISREG(stbuf.st_mode)) {
       clobber_args.push_back("keepimg");
+      clobber_args.push_back("preserve_lvs");
     } else {
       std::string str;
       if (!base::ReadFileToString(reset_file, &str)) {

init/startup/mount_helper.cc
@@ -95,7 +95,7 @@ void MountHelper::CleanupMounts(const std::string& msg) {
   std::vector<std::string> crash_args{"--clobber_state"};
   platform_->AddClobberCrashReport(crash_args);
 
-  std::vector<std::string> argv{"fast", "keepimg"};
+  std::vector<std::string> argv{"fast", "keepimg", "preserve_lvs"};
   platform_->Clobber(argv);
 }
 

init/startup/stateful_mount.cc
@@ -465,7 +465,7 @@ void StatefulMount::MountStateful() {
               LOG(ERROR) << "Failed to activate unencrypted stateful logical "
                          << "volume.";
 
-              ClobberStateful({"fast", "keepimg"},
+              ClobberStateful({"fast", "keepimg", "preserve_lvs"},
                               "'Invalid unencrypted logical volume'");
             }
             should_mount_lvm = true;
@@ -512,7 +512,7 @@ void StatefulMount::MountStateful() {
                     << kDumpe2fsStatefulLog;
       }
 
-      ClobberStateful({"keepimg"},
+      ClobberStateful({"keepimg", "preserve_lvs"},
                       "'Self-repair corrupted stateful partition'");
     }
 

commit d7184a52dfb0b2910bd185834fa2d7c4889ba8ea

init: startup: Use default option when reset file is incorrect
When we can not read `factory_install_reset` clobber file, use the
default argument, potentially recreating LVM topology.

BUG=b:222344877
TEST=Create a directory `/mnt/stateful_partition/factory_install_reset`,
unexpected, as we expect a file, observe clobber.

Fixes: c236c66dfff ("init: Preserve logical volumes during select clobber")
Change-Id: I4d13687cce5743f9e28a1efb5b21327d636fc010
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5444768
Reviewed-by: Alexis Savery <asavery@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>

init/startup/chromeos_startup.cc
@@ -457,8 +457,6 @@ void ChromeosStartup::CheckForStatefulWipe() {
     platform_->GetOwnership(reset_file, &uid, NULL, false /* follow_links */);
     // If it's not a plain file owned by us, force a powerwash.
     if (uid != getuid() || platform_->IsLink(reset_file)) {
-      clobber_args.push_back("keepimg");
-      clobber_args.push_back("preserve_lvs");
       clobber_log_msg =
           "Powerwash initiated by Reset file presence, but invalid";
     } else {

init/startup/chromeos_startup_test.cc
@@ -440,7 +440,7 @@ TEST_F(StatefulWipeTest, PowerwashForced) {
   std::string res;
   ASSERT_TRUE(base::ReadFileToString(clobber_test_log_, &res));
   EXPECT_EQ(res, "Powerwash initiated by Reset file presence, but invalid");
-  std::set<std::string> expected = {"keepimg", "preserve_lvs"};
+  std::set<std::string> expected = {"keepimg"};
   EXPECT_EQ(startup_dep_->GetClobberArgs(), expected);
 }
 