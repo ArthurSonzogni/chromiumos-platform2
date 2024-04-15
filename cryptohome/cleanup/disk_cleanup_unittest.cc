ArthurSonzogni/chromiumos-platform2

commit 6e648e10b20ba25ac5d41a0baefc1a92dd86335e

cryptohome: clean up daemon-store-cache for mounted users
This is a simple implementation of clearing daemon-store-cache
for mounted users, repeatedly (attempt to delete files if it
failed previously).

Alternative implementation is to explicitly get mounted home dirs
by GetHomeDirs, but I could not see the benfit of doing so.

BUG=b:281083614
TEST=Run the unit tests. Create a big file under
/run/daemon-store-cache/shadercached/<hash>/ then obeserve
crytphome deleting the files.

Cq-Depend: chromium:4560428, chromium:4564773
Change-Id: I3413302377e0a084c8a8e8e48645774c8b31fa37
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4560809
Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
Tested-by: Max Lee <endlesspring@google.com>
Commit-Queue: Max Lee <endlesspring@google.com>
Reviewed-by: Daniil Lunev <dlunev@chromium.org>

cryptohome/cleanup/disk_cleanup.cc
@@ -359,7 +359,7 @@ bool DiskCleanup::FreeDiskSpaceInternal() {
 
   bool early_stop = false;
 
-  // Purge up daemon store cache.
+  // Purge up daemon store cache
   old_free_disk_space = free_disk_space;
   for (auto dir = normal_cleanup_homedirs.rbegin();
        dir != normal_cleanup_homedirs.rend(); dir++) {
@@ -406,6 +406,41 @@ bool DiskCleanup::FreeDiskSpaceInternal() {
       return false;
   }
 
+  // Purge up daemon store cache for the mounted (logged in) users.
+  if (!routines_->DeleteDaemonStoreCacheMountedUsers()) {
+    result = false;
+  }
+  old_free_disk_space = free_disk_space;
+  free_disk_space = AmountOfFreeDiskSpace();
+  const int64_t freed_daemon_store_cache_logged_in_space =
+      free_disk_space.value() - old_free_disk_space.value();
+  if (freed_daemon_store_cache_logged_in_space > 0) {
+    ReportFreedDaemonStoreCacheMountedUsersDiskSpaceInMb(
+        freed_daemon_store_cache_logged_in_space / 1024 / 1024);
+  }
+
+  switch (GetFreeDiskSpaceState(free_disk_space)) {
+    case DiskCleanup::FreeSpaceState::kAboveTarget:
+      ReportDiskCleanupProgress(
+          DiskCleanupProgress::kDaemonStoreCacheMountedUsersCleanedAboveTarget);
+      return result;
+    case DiskCleanup::FreeSpaceState::kAboveThreshold:
+    case DiskCleanup::FreeSpaceState::kNeedNormalCleanup:
+      cleaned_over_minimum = true;
+      ReportDiskCleanupProgress(
+          DiskCleanupProgress::
+              kDaemonStoreCacheMountedUsersCleanedAboveMinimum);
+      // Continue cleanup.
+      break;
+    case DiskCleanup::FreeSpaceState::kNeedAggressiveCleanup:
+    case DiskCleanup::FreeSpaceState::kNeedCriticalCleanup:
+      // Continue cleanup.
+      break;
+    case DiskCleanup::FreeSpaceState::kError:
+      LOG(ERROR) << "Failed to get the amount of free space";
+      return false;
+  }
+
   // Purge Dmcrypt cache vaults.
   for (auto dir = normal_cleanup_homedirs.rbegin();
        dir != normal_cleanup_homedirs.rend(); dir++) {
@@ -442,8 +477,13 @@ bool DiskCleanup::FreeDiskSpaceInternal() {
       return result;
     case DiskCleanup::FreeSpaceState::kAboveThreshold:
     case DiskCleanup::FreeSpaceState::kNeedNormalCleanup:
-      ReportDiskCleanupProgress(
-          DiskCleanupProgress::kCacheVaultsCleanedAboveMinimum);
+      // Do not call ReportDiskCleanupProgress if cleaned_over_minimum was set
+      // by previous clean up routine (i.e. daemon-store-cache cleanup for
+      // mounted users).
+      if (!cleaned_over_minimum) {
+        ReportDiskCleanupProgress(
+            DiskCleanupProgress::kCacheVaultsCleanedAboveMinimum);
+      }
       return result;
     case DiskCleanup::FreeSpaceState::kNeedAggressiveCleanup:
     case DiskCleanup::FreeSpaceState::kNeedCriticalCleanup:

commit 9a21665d597211597e1b5489c8e7a263958a7db9

cryptohome: Remove Dmcrypt cache on normal path
Since Cache Vault contains Cache/Gcache when LVM is used,
remove the other users ones on the normal cleanup path, before removing
user daemon stores.

BUG=b:281083614
TEST=unit test

Fixes: 6e648e10b20 ("cryptohome: clean up daemon-store-cache for mounted users")
Change-Id: I0f2efd5e697154928467f989acf21d49ebec7c6a
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5438354
Reviewed-by: Sarthak Kukreti <sarthakkukreti@google.com>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Max Lee <endlesspring@google.com>
Tested-by: Gwendal Grignou <gwendal@chromium.org>

cryptohome/cleanup/disk_cleanup.cc
@@ -274,6 +274,12 @@ bool DiskCleanup::FreeDiskSpaceInternal() {
     return result.success;
   }
 
+  result |= RemoveDmCryptCacheVaults(normal_cleanup_homedirs,
+                                     result.cleaned_over_minimum);
+  if (result.should_stop) {
+    return result.success;
+  }
+
   // Normal cleanup processed all folders. Move the cutoff forward.
   last_normal_disk_cleanup_complete_ = platform_->GetCurrentTime();
 
@@ -299,7 +305,6 @@ bool DiskCleanup::FreeDiskSpaceInternal() {
   }
 
   bool return_result = result.success;
-  bool cleaned_over_minimum = result.cleaned_over_minimum;
   bool early_stop = result.should_stop;
 
   // Purge up daemon store cache for the mounted (logged in) users.
@@ -322,64 +327,9 @@ bool DiskCleanup::FreeDiskSpaceInternal() {
       return return_result;
     case DiskCleanup::FreeSpaceState::kAboveThreshold:
     case DiskCleanup::FreeSpaceState::kNeedNormalCleanup:
-      cleaned_over_minimum = true;
       ReportDiskCleanupProgress(
           DiskCleanupProgress::
               kDaemonStoreCacheMountedUsersCleanedAboveMinimum);
-      // Continue cleanup.
-      break;
-    case DiskCleanup::FreeSpaceState::kNeedAggressiveCleanup:
-    case DiskCleanup::FreeSpaceState::kNeedCriticalCleanup:
-      // Continue cleanup.
-      break;
-    case DiskCleanup::FreeSpaceState::kError:
-      LOG(ERROR) << "Failed to get the amount of free space";
-      return false;
-  }
-
-  // Purge Dmcrypt cache vaults.
-  for (const auto& normal_cleanup_homedir :
-       base::Reversed(normal_cleanup_homedirs)) {
-    if (!routines_->DeleteCacheVault(normal_cleanup_homedir.obfuscated))
-      return_result = false;
-
-    if (HasTargetFreeSpace()) {
-      early_stop = true;
-      break;
-    }
-  }
-
-  old_free_disk_space = free_disk_space;
-  free_disk_space = AmountOfFreeDiskSpace();
-  if (!free_disk_space) {
-    LOG(ERROR) << "Failed to get the amount of free space";
-    return false;
-  }
-
-  const int64_t freed_vault_cache_space =
-      free_disk_space.value() - old_free_disk_space.value();
-  // Report only if something was deleted.
-  if (freed_vault_cache_space > 0) {
-    ReportFreedCacheVaultDiskSpaceInMb(freed_vault_cache_space / 1024 / 1024);
-  }
-
-  if (!early_stop)
-    last_normal_disk_cleanup_complete_ = platform_->GetCurrentTime();
-
-  switch (GetFreeDiskSpaceState(free_disk_space)) {
-    case DiskCleanup::FreeSpaceState::kAboveTarget:
-      ReportDiskCleanupProgress(
-          DiskCleanupProgress::kCacheVaultsCleanedAboveTarget);
-      return return_result;
-    case DiskCleanup::FreeSpaceState::kAboveThreshold:
-    case DiskCleanup::FreeSpaceState::kNeedNormalCleanup:
-      // Do not call ReportDiskCleanupProgress if cleaned_over_minimum was set
-      // by previous clean up routine (i.e. daemon-store-cache cleanup for
-      // mounted users).
-      if (!cleaned_over_minimum) {
-        ReportDiskCleanupProgress(
-            DiskCleanupProgress::kCacheVaultsCleanedAboveMinimum);
-      }
       return return_result;
     case DiskCleanup::FreeSpaceState::kNeedAggressiveCleanup:
     case DiskCleanup::FreeSpaceState::kNeedCriticalCleanup:
@@ -697,6 +647,76 @@ DiskCleanup::DiskCleanupActionResult DiskCleanup::RemoveDaemonStoreCache(
         ReportDiskCleanupProgress(
             DiskCleanupProgress::kDaemonStoreCacheCleanedAboveMinimum);
       }
+      result.cleaned_over_minimum = true;
+      break;
+    case DiskCleanup::FreeSpaceState::kNeedAggressiveCleanup:
+    case DiskCleanup::FreeSpaceState::kNeedCriticalCleanup:
+      // Continue cleanup.
+      break;
+    case DiskCleanup::FreeSpaceState::kError:
+      LOG(ERROR) << "Failed to get the amount of free space";
+      result.success = false;
+      break;
+  }
+
+  return result;
+}
+
+// Purge up cache dmcrypt volumes for every unmounted user that has logged out
+// after the last normal cleanup happened.
+DiskCleanup::DiskCleanupActionResult DiskCleanup::RemoveDmCryptCacheVaults(
+    const std::vector<HomeDirs::HomeDir>& homedirs, bool cleaned_over_minimum) {
+  DiskCleanupActionResult result;
+
+  auto free_disk_space = AmountOfFreeDiskSpace();
+  if (!free_disk_space) {
+    LOG(ERROR) << "Failed to get the amount of free space";
+    result.success = false;
+    return result;
+  }
+
+  auto old_free_disk_space = free_disk_space;
+  for (const auto& homedir : base::Reversed(homedirs)) {
+    if (!routines_->DeleteCacheVault(homedir.obfuscated))
+      result.success = false;
+
+    if (HasTargetFreeSpace()) {
+      ReportDiskCleanupProgress(
+          DiskCleanupProgress::kCacheVaultsCleanedAboveTarget);
+      result.should_stop = true;
+      break;
+    }
+  }
+
+  free_disk_space = AmountOfFreeDiskSpace();
+  if (!free_disk_space) {
+    LOG(ERROR) << "Failed to get the amount of free space";
+    result.success = false;
+    return result;
+  }
+
+  const int64_t freed_daemon_store_cache_space =
+      free_disk_space.value() - old_free_disk_space.value();
+  // Report only if something was deleted.
+  if (freed_daemon_store_cache_space > 0) {
+    ReportFreedCacheVaultDiskSpaceInMb(freed_daemon_store_cache_space / 1024 /
+                                       1024);
+  }
+
+  switch (GetFreeDiskSpaceState(free_disk_space)) {
+    case DiskCleanup::FreeSpaceState::kAboveTarget:
+      ReportDiskCleanupProgress(
+          DiskCleanupProgress::kCacheVaultsCleanedAboveTarget);
+      break;
+    case DiskCleanup::FreeSpaceState::kAboveThreshold:
+    case DiskCleanup::FreeSpaceState::kNeedNormalCleanup:
+      // Do not call ReportDiskCleanupProgress if cleaned_over_minimum was set
+      // by previous clean up routine (i.e. gcache cleanup).
+      if (!cleaned_over_minimum) {
+        ReportDiskCleanupProgress(
+            DiskCleanupProgress::kCacheVaultsCleanedAboveMinimum);
+      }
+      result.cleaned_over_minimum = true;
       break;
     case DiskCleanup::FreeSpaceState::kNeedAggressiveCleanup:
     case DiskCleanup::FreeSpaceState::kNeedCriticalCleanup:

cryptohome/cleanup/disk_cleanup.h
@@ -138,6 +138,9 @@ class DiskCleanup {
   DiskCleanupActionResult RemoveDaemonStoreCache(
       const std::vector<HomeDirs::HomeDir>& homedirs,
       bool cleaned_over_minimum);
+  DiskCleanupActionResult RemoveDmCryptCacheVaults(
+      const std::vector<HomeDirs::HomeDir>& homedirs,
+      bool cleaned_over_minimum);
 
   // Actually performs disk cleanup. Called by FreeDiskSpaceDuringLogin.
   bool FreeDiskSpaceDuringLoginInternal(const ObfuscatedUsername& obfuscated);

cryptohome/cleanup/disk_cleanup_unittest.cc
@@ -139,6 +139,9 @@ class DiskCleanupTest : public ::testing::Test {
 
     if (cleanups > 2) {
       queries++;  // Daemon cache cleanup reports UMA stats.
+    }
+    if (cleanups > 3) {
+      queries++;  // Cache vault cleanup reports UMA stats.
       queries++;  // Disk cleanup decides if it continues.
     }
 
@@ -471,11 +474,15 @@ TEST_F(DiskCleanupTest, AllCacheCleanup) {
       EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCache(hd.obfuscated))
           .WillOnce(Return(true));
     }
+
+    for (const auto& hd : GetTestHomedirs()) {
+      EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(hd.obfuscated))
+          .WillOnce(Return(true));
+    }
   }
 
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
       .Times(0);
-  EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_, DeleteUserAndroidCache(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_, DeleteUserProfile(_)).Times(0);
 
@@ -500,10 +507,10 @@ TEST_F(DiskCleanupTest, AndroidCacheStopAfterOneUser) {
       .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCache(_))
       .Times(GetTestHomedirs().size());
-  EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
-      .Times(1);
   EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_))
       .Times(GetTestHomedirs().size());
+  EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
+      .Times(1);
   // Only clean up the first user.
   EXPECT_CALL(*cleanup_routines_, DeleteUserAndroidCache(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_,
@@ -601,10 +608,10 @@ TEST_F(DiskCleanupTest, RemoveOneProfile) {
       .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCache(_))
       .Times(GetTestHomedirs().size());
-  EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
-      .Times(1);
   EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_))
       .Times(GetTestHomedirs().size());
+  EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
+      .Times(1);
   EXPECT_CALL(*cleanup_routines_, DeleteUserAndroidCache(_))
       .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteUserProfile(_)).Times(0);
@@ -764,9 +771,10 @@ TEST_F(DiskCleanupTest, RepeatNormalCleanup) {
       .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCache(_))
       .Times(GetTestHomedirs().size());
+  EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_))
+      .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
       .Times(0);
-  EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_, DeleteUserAndroidCache(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_, DeleteUserProfile(_)).Times(0);
 
@@ -788,6 +796,8 @@ TEST_F(DiskCleanupTest, RepeatNormalCleanup) {
               DeleteUserGCache(GetTestHomedirs()[2].obfuscated));
   EXPECT_CALL(*cleanup_routines_,
               DeleteDaemonStoreCache(GetTestHomedirs()[2].obfuscated));
+  EXPECT_CALL(*cleanup_routines_,
+              DeleteCacheVault(GetTestHomedirs()[2].obfuscated));
 
   ASSERT_TRUE(base::Time::FromUTCExploded({2022, 4, 5, 1}, &cleanup_time));
   EXPECT_CALL(platform_, GetCurrentTime).WillRepeatedly(Return(cleanup_time));
@@ -866,9 +876,10 @@ TEST_F(DiskCleanupTest, FullAggressiveCleanupAfterNormal) {
       .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCache(_))
       .Times(GetTestHomedirs().size());
+  EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_))
+      .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
       .Times(0);
-  EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_, DeleteUserAndroidCache(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_, DeleteUserProfile(_)).Times(0);
 
@@ -897,10 +908,10 @@ TEST_F(DiskCleanupTest, FullAggressiveCleanupAfterNormal) {
               DeleteUserGCache(GetTestHomedirs()[2].obfuscated));
   EXPECT_CALL(*cleanup_routines_,
               DeleteDaemonStoreCache(GetTestHomedirs()[2].obfuscated));
-  EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
-      .Times(1);
   EXPECT_CALL(*cleanup_routines_,
               DeleteCacheVault(GetTestHomedirs()[2].obfuscated));
+  EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
+      .Times(1);
 
   ASSERT_TRUE(base::Time::FromUTCExploded({2022, 4, 5, 1}, &cleanup_time));
   EXPECT_CALL(platform_, GetCurrentTime).WillRepeatedly(Return(cleanup_time));
@@ -926,9 +937,9 @@ TEST_F(DiskCleanupTest, RepeatNormalCleanupAfterEarlyStop) {
       .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteUserGCache(_)).Times(2);
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCache(_)).Times(0);
+  EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
       .Times(0);
-  EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_, DeleteUserAndroidCache(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_, DeleteUserProfile(_)).Times(0);
 
@@ -946,6 +957,8 @@ TEST_F(DiskCleanupTest, RepeatNormalCleanupAfterEarlyStop) {
   EXPECT_CALL(*cleanup_routines_, DeleteUserGCache(_))
       .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCache(_))
+      .Times(GetTestHomedirs().size());
+  EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_))
       .Times(GetTestHomedirs().size());
 
   ASSERT_TRUE(base::Time::FromUTCExploded({2022, 4, 5, 1}, &cleanup_time));
@@ -974,10 +987,10 @@ TEST_F(DiskCleanupTest, RepeatAggressiveCleanupAfterEarlyStop) {
       .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCache(_))
       .Times(GetTestHomedirs().size());
-  EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
-      .Times(1);
   EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_))
       .Times(GetTestHomedirs().size());
+  EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
+      .Times(1);
   EXPECT_CALL(*cleanup_routines_, DeleteUserAndroidCache(_)).Times(2);
   EXPECT_CALL(*cleanup_routines_, DeleteUserProfile(_)).Times(0);
 
@@ -1149,7 +1162,7 @@ TEST_F(DiskCleanupTest, DaemonStoreCacheMountedUserCleanupEarlyStop) {
       .WillRepeatedly(Return(kTargetFreeSpaceAfterCleanup + 1));
 
   EXPECT_CALL(platform_, AmountOfFreeDiskSpace(ShadowRoot()))
-      .Times(disk_space_queries(3) + 2)
+      .Times(disk_space_queries(4) + 2)
       .WillRepeatedly(Return(kFreeSpaceThresholdToTriggerAggressiveCleanup - 1))
       .RetiresOnSaturation();
 
@@ -1160,12 +1173,13 @@ TEST_F(DiskCleanupTest, DaemonStoreCacheMountedUserCleanupEarlyStop) {
       .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteUserGCache(_))
       .Times(GetTestHomedirs().size());
+  EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_))
+      .Times(GetTestHomedirs().size());
   // Only clean up the first user.
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCache(_))
       .Times(GetTestHomedirs().size());
   EXPECT_CALL(*cleanup_routines_, DeleteDaemonStoreCacheMountedUsers())
       .Times(1);
-  EXPECT_CALL(*cleanup_routines_, DeleteCacheVault(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_, DeleteUserAndroidCache(_)).Times(0);
   EXPECT_CALL(*cleanup_routines_, DeleteUserProfile(_)).Times(0);
 