40fba0028baa37a5f729c1b4ac97688877284078
Jeffery Miller <jefferymiller@google.com>
2024-04-19 17:11:22 -0500

    tpm_manager: Extend TPM allow list
    Add ZBook Firefly 14 G7 Mobile Workstation
    
    BUG=b:335902901
    TEST=tast run dutf hwsec.* u2fd.* cryptophome.*
    TEST=tpm_manager_client clear_tpm
    
    Change-Id: If7413189ce261d5345a751a2b3236bbc2648c344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5762522
    Commit-Queue: Jeffery Miller <jefferymiller@google.com>
    Reviewed-by: Yi Chou <yich@google.com>
    Tested-by: Jeffery Miller <jefferymiller@google.com>
    Auto-Submit: Jeffery Miller <jefferymiller@google.com>

--- tpm_manager/server/tpm_allowlist_impl.cc
+++ tpm_manager/server/tpm_allowlist_impl.cc
@@ -87,6 +87,7 @@ constexpr DeviceName kTpm2DeviceNameAllowlist[] = {
     DeviceName{"HP", "HP ProDesk 400 G5 Desktop Mini", kVendorIdIfx},
     DeviceName{"HP", "HP EliteBook 840 G6", kVendorIdIfx},
     DeviceName{"Intel(R) Client Systems", "NUC11TNKv5", kVendorIdIfx},
+    DeviceName{"HP", "HP ZBook Firefly 14 G7 Mobile Workstation", kVendorIdIfx},
 };
 
 std::optional<bool> IsTpmFileEnabled() {
