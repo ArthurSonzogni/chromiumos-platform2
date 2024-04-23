27b3e37443ad3021470b964cae35d31ff820568f
Jae Hoon Kim <kimjae@chromium.org>
2024-04-23 00:21:20 +0000

    dlcservice: Add deshelled `--unpack`
    .. purely golang based, rather than invoking via shell.
    
    BUG=b:335722339
    TEST=FEATURES=test emerge-$B <PKG>
    
    Change-Id: Ia7cc206085c1ac35657c0376cbfd8e02915397eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5474357
    Reviewed-by: Yuanpeng Ni‎ <yuanpengni@chromium.org>
    Tested-by: Jae Hoon Kim <kimjae@chromium.org>
    Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>

--- dlcservice/src/go.chromium.org/chromiumos/dlctool/dlctool.go
+++ dlcservice/src/go.chromium.org/chromiumos/dlctool/dlctool.go
@@ -6,13 +6,24 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"path"
 )
 
-const dlctoolShellPath = "/usr/local/bin/dlctool-shell"
+const (
+	dlcPreloadPath             = "/var/cache/dlc-images"
+	dlcserviceMetadataUtilPath = "/usr/bin/dlc_metadata_util"
+	dlcserviceUtilPath         = "/usr/bin/dlcservice_util"
+	dlctoolShellPath           = "/usr/local/bin/dlctool-shell"
+	dlcImageFile               = "dlc.img"
+
+	unsquashfsPath = "/usr/bin/unsquashfs"
+)
 
 var (
 	flagID     *string = flag.String("id", "", "ID of the DLC to [un]pack.")
@@ -54,8 +65,180 @@ func dlctoolShell(args []string) {
 	}
 }
 
+func readState(id *string) ([]byte, error) {
+	cmd := &exec.Cmd{
+		Path: dlcserviceUtilPath,
+		Args: append([]string{dlcserviceUtilPath}, "--dlc_state", "--id="+*id),
+	}
+	return cmd.Output()
+}
+
+func readMetadata(id *string) ([]byte, error) {
+	cmd := &exec.Cmd{
+		Path: dlcserviceMetadataUtilPath,
+		Args: append([]string{dlcserviceMetadataUtilPath}, "--get", "--id="+*id),
+	}
+	return cmd.Output()
+}
+
+func isDlcInstalled(id *string) bool {
+	out, err := readState(id)
+	if err != nil {
+		log.Fatalf("Failed to read state: %v", err)
+	}
+
+	state := struct {
+		State int `json:"state"`
+	}{}
+	err = json.Unmarshal(out, &state)
+
+	if err != nil {
+		log.Fatalf("Failed to unmarshal DLC (%s) state", *id)
+	}
+
+	return state.State == 2
+}
+
+func isDlcPreloadable(id *string) bool {
+	_, err := os.Stat(path.Join(dlcPreloadPath, *id, "package", dlcImageFile))
+	return !os.IsNotExist(err)
+}
+
+func isDlcScaled(id *string) bool {
+	out, err := readMetadata(id)
+	if err != nil {
+		log.Fatalf("Failed to read metadata: %v", err)
+	}
+
+	metadata := struct {
+		Manifest struct {
+			Scaled bool `json:"scaled"`
+		} `json:"manifest"`
+	}{}
+	err = json.Unmarshal(out, &metadata)
+
+	if err != nil {
+		log.Fatalf("Failed to unmarshal DLC (%s)", *id)
+	}
+
+	return metadata.Manifest.Scaled
+}
+
+func isDlcForceOTA(id *string) bool {
+	out, err := readMetadata(id)
+	if err != nil {
+		log.Fatalf("Failed to read metadata: %v", err)
+	}
+
+	metadata := struct {
+		Manifest struct {
+			ForceOTA bool `json:"force-ota"`
+		} `json:"manifest"`
+	}{}
+	err = json.Unmarshal(out, &metadata)
+
+	if err != nil {
+		log.Fatalf("Failed to unmarshal DLC (%s)", *id)
+	}
+
+	return metadata.Manifest.ForceOTA
+}
+
+func getDlcImagePath(id *string) string {
+	out, err := readState(id)
+	if err != nil {
+		log.Fatalf("dlcImagePath: Failed to read state: %v", err)
+	}
+
+	state := struct {
+		ImagePath string `json:"image_path"`
+	}{}
+	err = json.Unmarshal(out, &state)
+
+	if err != nil {
+		log.Fatalf("dlcImagePath: Failed to unmarshal DLC (%s)", *id)
+	}
+
+	return state.ImagePath
+}
+
+func installDlc(id *string) error {
+	cmd := &exec.Cmd{
+		Path: dlcserviceUtilPath,
+		Args: append([]string{dlcserviceUtilPath}, "--install", "--id="+*id),
+	}
+	return cmd.Run()
+}
+
+func tryInstallingDlc(id *string) error {
+	if isDlcInstalled(id) {
+		log.Printf("DLC (%s) is already installed, continuing...\n", *id)
+		return nil
+	}
+
+	if isDlcPreloadable(id) {
+		log.Printf("Trying to install DLC (%s) because it's preloaded.\n", *id)
+	} else if isDlcScaled(id) {
+		log.Printf("Trying to install DLC (%s) because it's scaled.\n", *id)
+	} else if isDlcForceOTA(id) {
+		log.Printf("Trying to install DLC (%s) because it's force-ota.\n", *id)
+	} else {
+		return fmt.Errorf("tryInstallingDlc failed: Can't install DLC (%s)", *id)
+	}
+
+	if err := installDlc(id); err != nil {
+		return fmt.Errorf("tryInstallingDlc failed: %w", err)
+	}
+	log.Printf("Installed DLC (%s)\n", *id)
+	return nil
+}
+
+func extractDlc(id, path *string) error {
+	// TODO(b/335722339): Add support for other filesystems based on image type.
+	cmd := &exec.Cmd{
+		Path: unsquashfsPath,
+		Args: []string{unsquashfsPath, "-d", *path, getDlcImagePath(id)},
+	}
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("extractDlc: failed to decompress: %w", err)
+	}
+
+	return nil
+}
+
+func unpackDlc(id, path *string) error {
+	if _, err := os.Stat(*path); !os.IsNotExist(err) {
+		return fmt.Errorf("%s is a path which already exists", *path)
+	}
+
+	if err := tryInstallingDlc(id); err != nil {
+		return fmt.Errorf("unpackDlc: failed installing DLC: %w", err)
+	}
+
+	if err := extractDlc(id, path); err != nil {
+		return fmt.Errorf("unpackDlc: failed extracting: %w", err)
+	}
+
+	return nil
+}
+
 func main() {
 	initFlags()
+
+	args := flag.Args()
+	if len(args) != 1 {
+		log.Fatal("Please only pass in <path>.")
+	}
+	path := path.Clean(args[0])
+
+	if *flagUnpack {
+		log.Printf("Unpacking DLC (%s) to: %s\n", *flagID, path)
+		if err := unpackDlc(flagID, &path); err != nil {
+			log.Fatalf("Unpacking DLC (%s) failed: %v", *flagID, err)
+		}
+		return
+	}
 
 	if *flagShell {
 		dlctoolShell(os.Args[1:])
