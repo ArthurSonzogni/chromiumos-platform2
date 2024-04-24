283500b735ed25519cdc37107c46e434999a24b0
Vaibhav Raheja <vraheja@google.com>
2024-04-24 14:16:46 -0700

    arc: Add unit-test file for ARC RPC
    This CL adds the boilerplate code for
    unit-tests of Arc Remote Provisioning Context.
    
    BUG=b:327492676
    TEST=emerge-brya chromeos-base/arc-keymint
    TEST=cros_workon_make --board=brya chromeos-base/arc-keymint --test
    
    Change-Id: Id4b15cd2de76db1f3309216a992610dde9f1424a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5485260
    Tested-by: Vaibhav Raheja <vraheja@chromium.org>
    Reviewed-by: Yao Li <yaohuali@google.com>
    Commit-Queue: Vaibhav Raheja <vraheja@chromium.org>

---
+++ arc/keymint/context/arc_remote_provisioning_context_test.cc
@@ -0,0 +1,11 @@
+// Copyright 2024 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "arc/keymint/context/arc_remote_provisioning_context.h"
+
+namespace arc::keymint::context {
+
+namespace {}
+
+}  // namespace arc::keymint::context
