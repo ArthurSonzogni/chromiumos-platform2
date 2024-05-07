ffcac44ae0e00904279140147f8a013774e7dda2
maciek swiech <drmasquatch@google.com>
2024-05-07 10:02:37 -0600

    primary_io_manager: exit early if not running on chromebox
    PrimaryIoManager is only needed to run on chromebox-format devices, so
    it should exit cleanly (preventing upstart respawn) if it detects it is
    not running on such a device.
    
    BUG=b:308434587
    TEST=unit tests
    TEST=deploy to DUT with hardcoded test values
    
    Change-Id: Ic5e73ed1e2c44df209bcade44e9201b8dd00e6ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5526753
    Tested-by: maciek swiech <drmasquatch@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: maciek swiech <drmasquatch@google.com>

--- primary_io_manager/primary_io_manager_main.cc
+++ primary_io_manager/primary_io_manager_main.cc
@@ -12,6 +12,7 @@
 #include <brillo/syslog_logging.h>
 #include <chromeos/dbus/service_constants.h>
 
+#include "chromeos-config/libcros_config/cros_config.h"
 #include "primary_io_manager/primary_io_manager.h"
 
 namespace primary_io_manager {
@@ -37,12 +38,28 @@ class Daemon : public brillo::DBusServiceDaemon {
 
 }  // namespace primary_io_manager
 
+bool RunningOnChromebox() {
+  brillo::CrosConfig cros_config;
+  std::string form_factor;
+
+  if (!cros_config.GetString("/hardware-properties", "form-factor",
+                             &form_factor)) {
+    LOG(ERROR) << "Unable to ascertain form-factor from CrosConfig.";
+  }
+
+  return (form_factor.empty() || form_factor == "CHROMEBOX");
+}
+
 int main(int argc, char** argv) {
   brillo::FlagHelper::Init(argc, argv, "Chromium OS Primary IO Manager");
   brillo::InitLog(brillo::kLogToSyslog);
 
+  if (!RunningOnChromebox()) {
+    LOG(INFO) << "Not running on chromebox-format device, exiting.";
+    return 0;
+  }
+
   primary_io_manager::Daemon daemon{};
 
-  // TODO(b/308434587): only run on chromebox
   return daemon.Run();
 }
