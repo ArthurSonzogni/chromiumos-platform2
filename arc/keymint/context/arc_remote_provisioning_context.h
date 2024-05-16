66fae3da8c8bd5c957569af411602a3ec992dee5
Vaibhav Raheja <vraheja@google.com>
2024-05-16 15:16:04 -0700

    arc: Add BccPayload Labels to ARC RPC
    This CL adds BccPayload labels to ARC
    Remote Provisioning Context.
    
    These are based directly on |ProtectedData.aidl| -
    https://bit.ly/4bDXTdg.
    
    BUG=b:341128453
    TEST=emerge-brya chromeos-base/arc-keymint
    TEST=cros_workon_make --board=brya chromeos-base/arc-keymint --test
    
    Change-Id: Ief41c78e00d61e9722151ef30c57ad90c576ae00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5545964
    Commit-Queue: Vaibhav Raheja <vraheja@chromium.org>
    Reviewed-by: Yao Li <yaohuali@google.com>
    Tested-by: Vaibhav Raheja <vraheja@chromium.org>

--- arc/keymint/context/arc_remote_provisioning_context.h
+++ arc/keymint/context/arc_remote_provisioning_context.h
@@ -9,6 +9,24 @@
 
 namespace arc::keymint::context {
 
+// BCCPayload labels are based on |ProtectedData.aidl|.
+enum BccPayloadLabel : int {
+  ISSUER = 1,
+  SUBJECT = 2,
+  SUBJECT_PUBLIC_KEY = -4670552,
+  KEY_USAGE = -4670553,
+  CODE_HASH = -4670545,
+  CODE_DESCRIPTOR = -4670546,
+  CONFIG_HASH = -4670547,
+  CONFIG_DESCRIPTOR = -4670548,
+  CONFIG_COMPONENT_NAME = -70002,
+  CONFIG_FIRMWARE_VERSION = -70003,
+  CONFIG_RESETTABLE = -70004,
+  AUTHORITY_HASH = -4670549,
+  AUTHORITY_DESCRIPTOR = -4670550,
+  MODE = -4670551,
+};
+
 // Defines specific behavior for ARC Remote Provisioning Context in ChromeOS.
 class ArcRemoteProvisioningContext
     : public ::keymaster::PureSoftRemoteProvisioningContext {
