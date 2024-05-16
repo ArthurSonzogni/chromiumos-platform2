307f57052f047ec2ec0cec393fd7d3f74c0062a4
Wei-Luan Wang <weiluanwang@google.com>
2024-05-16 07:15:41 +0000

    diagnostics: Improve test coverage for fwupd version format parsing
    Add unit tests for all version format. In addition, cover an edge case
    for out-of-range values.
    
    BUG=none
    TEST=FEATURES=test emerge-volteer diagnostics
    
    Change-Id: Ida4a9d994e1f8c840fd10a353c754b2574cf0dec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5541541
    Commit-Queue: Wei-Luan Wang <weiluanwang@google.com>
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Tested-by: Wei-Luan Wang <weiluanwang@google.com>

--- diagnostics/cros_healthd/utils/fwupd_utils_test.cc
+++ diagnostics/cros_healthd/utils/fwupd_utils_test.cc
@@ -13,11 +13,10 @@
 
 #include "diagnostics/mojom/public/cros_healthd_probe.mojom.h"
 
-namespace diagnostics {
-namespace fwupd_utils {
+namespace diagnostics::fwupd_utils {
 namespace {
 
-using FwupdVersionFormat = ::ash::cros_healthd::mojom::FwupdVersionFormat;
+namespace mojom = ::ash::cros_healthd::mojom;
 
 TEST(FwupdUtilsTest, DeviceContainsVendorId) {
   auto device_info = DeviceInfo();
@@ -79,7 +78,7 @@ TEST(FwupdUtilsTest, ParseDbusFwupdDeviceInfo) {
   EXPECT_EQ(device_info.instance_ids, instance_ids);
   EXPECT_EQ(device_info.serial, serial);
   EXPECT_EQ(device_info.version, version);
-  EXPECT_EQ(device_info.version_format, FwupdVersionFormat::kPlain);
+  EXPECT_EQ(device_info.version_format, mojom::FwupdVersionFormat::kPlain);
   EXPECT_EQ(device_info.joined_vendor_id, joined_vendor_id);
 }
 
@@ -97,8 +96,51 @@ TEST(FwupdUtilsTest, ParseDbusFwupdDeviceInfoMissingKeys) {
   EXPECT_EQ(device_info.instance_ids.size(), 0);
   EXPECT_EQ(device_info.serial, std::nullopt);
   EXPECT_EQ(device_info.version, std::nullopt);
-  EXPECT_EQ(device_info.version_format, FwupdVersionFormat::kUnknown);
+  EXPECT_EQ(device_info.version_format, mojom::FwupdVersionFormat::kUnknown);
   EXPECT_EQ(device_info.joined_vendor_id, std::nullopt);
+}
+
+TEST(FwupdUtilsTest, ParseVersionFormat) {
+  auto parse_helper = [](FwupdVersionFormat dbus_value) {
+    brillo::VariantDictionary dbus_response_entry;
+    dbus_response_entry.emplace(kFwupdResultKeyVersionFormat,
+                                static_cast<uint32_t>(dbus_value));
+    return ParseDbusFwupdDeviceInfo(dbus_response_entry).version_format;
+  };
+
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_PLAIN),
+            mojom::FwupdVersionFormat::kPlain);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_NUMBER),
+            mojom::FwupdVersionFormat::kNumber);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_PAIR),
+            mojom::FwupdVersionFormat::kPair);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_TRIPLET),
+            mojom::FwupdVersionFormat::kTriplet);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_QUAD),
+            mojom::FwupdVersionFormat::kQuad);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_BCD),
+            mojom::FwupdVersionFormat::kBcd);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_INTEL_ME),
+            mojom::FwupdVersionFormat::kIntelMe);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_INTEL_ME2),
+            mojom::FwupdVersionFormat::kIntelMe2);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_SURFACE_LEGACY),
+            mojom::FwupdVersionFormat::kSurfaceLegacy);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_SURFACE),
+            mojom::FwupdVersionFormat::kSurface);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_DELL_BIOS),
+            mojom::FwupdVersionFormat::kDellBios);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_HEX),
+            mojom::FwupdVersionFormat::kHex);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_LAST),
+            mojom::FwupdVersionFormat::kUnknown);
+  EXPECT_EQ(parse_helper(FWUPD_VERSION_FORMAT_UNKNOWN),
+            mojom::FwupdVersionFormat::kUnknown);
+  // Test for unexpected values since the values are from external D-Bus
+  // services.
+  EXPECT_EQ(parse_helper(
+                static_cast<FwupdVersionFormat>(FWUPD_VERSION_FORMAT_LAST + 1)),
+            mojom::FwupdVersionFormat::kUnknown);
 }
 
 TEST(FwupdUtilsTest, MatchUsbBySerials) {
@@ -107,7 +149,7 @@ TEST(FwupdUtilsTest, MatchUsbBySerials) {
       .instance_ids = std::vector<std::string>{"USB\\VID_1234&PID_5678"},
       .serial = "serial1",
       .version = "version1",
-      .version_format = FwupdVersionFormat::kPlain,
+      .version_format = mojom::FwupdVersionFormat::kPlain,
       .joined_vendor_id = "USB:0x1234",
   };
   const auto& guid = InstanceIdToGuid("USB\\VID_1234&PID_5678");
@@ -128,7 +170,7 @@ TEST(FwupdUtilsTest, MatchUsbBySerials) {
 
   auto res = FetchUsbFirmwareVersion(device_infos, usb_device_filter);
   EXPECT_EQ(res->version, "version1");
-  EXPECT_EQ(res->version_format, FwupdVersionFormat::kPlain);
+  EXPECT_EQ(res->version_format, mojom::FwupdVersionFormat::kPlain);
 }
 
 TEST(FwupdUtilsTest, UsbProductNotMatched) {
@@ -136,7 +178,7 @@ TEST(FwupdUtilsTest, UsbProductNotMatched) {
   // no serial. This kind of matchingness is too weak.
   DeviceInfo device{
       .version = "version",
-      .version_format = FwupdVersionFormat::kPlain,
+      .version_format = mojom::FwupdVersionFormat::kPlain,
       .joined_vendor_id = "USB:0x1234",
   };
 
@@ -157,7 +199,7 @@ TEST(FwupdUtilsTest, MultipleUsbMatchedWithACommonVersion) {
   DeviceInfo device{
       .instance_ids = std::vector<std::string>{"USB\\VID_1234&PID_5678"},
       .version = "version",
-      .version_format = FwupdVersionFormat::kPlain,
+      .version_format = mojom::FwupdVersionFormat::kPlain,
       .joined_vendor_id = "USB:0x1234",
   };
   const auto& guid = InstanceIdToGuid("USB\\VID_1234&PID_5678");
@@ -173,7 +215,7 @@ TEST(FwupdUtilsTest, MultipleUsbMatchedWithACommonVersion) {
 
   auto res = FetchUsbFirmwareVersion(device_infos, usb_device_filter);
   EXPECT_EQ(res->version, "version");
-  EXPECT_EQ(res->version_format, FwupdVersionFormat::kPlain);
+  EXPECT_EQ(res->version_format, mojom::FwupdVersionFormat::kPlain);
 }
 
 TEST(FwupdUtilsTest, MultipleUsbMatchedButDifferentVersions) {
@@ -181,7 +223,7 @@ TEST(FwupdUtilsTest, MultipleUsbMatchedButDifferentVersions) {
   DeviceInfo device1{
       .instance_ids = std::vector<std::string>{"USB\\VID_1234&PID_5678"},
       .version = "version",
-      .version_format = FwupdVersionFormat::kPlain,
+      .version_format = mojom::FwupdVersionFormat::kPlain,
       .joined_vendor_id = "USB:0x1234",
   };
   const auto& guid = InstanceIdToGuid("USB\\VID_1234&PID_5678");
@@ -203,5 +245,4 @@ TEST(FwupdUtilsTest, MultipleUsbMatchedButDifferentVersions) {
 }
 
 }  // namespace
-}  // namespace fwupd_utils
-}  // namespace diagnostics
+}  // namespace diagnostics::fwupd_utils
