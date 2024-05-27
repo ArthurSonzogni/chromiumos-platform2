d6aafb05412715192945822ea9abddb790034d64
Chih-Yu Huang <akahuang@google.com>
2024-05-27 17:18:47 +0900

    shill: copy url/mojom/url.mojom from Chromium
    Originally we duplicate the URL Mojo struct at portal.mojom. However, it
    increases the maintenance burden to duplicate the URL struct when
    copying back the portal.mojom API back to Chromium.
    
    This CL copies the url/mojom/url.mojom from Chromium project, and makes
    the portal Mojo API use url.mojom.Url directly. In order to keep the
    mojo include path the same as Chromium, this CL changes the mojo_root to
    platform2/shill/mojom, then moves the url mojom file to
    platform2/shill/mojom/url/mojom/url.mojom.
    
    BUG=b:340406014
    TEST=FEATURES=test emerge-brya shill
    
    Change-Id: I05aab7ee90317a8545df44e33ac4f80265e05d8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5569013
    Tested-by: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>

--- shill/mojom/url/mojom/url_mojom_traits.h
+++ shill/mojom/url/mojom/url_mojom_traits.h
@@ -2,21 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SHILL_MOJOM_URL_MOJOM_TRAITS_H_
-#define SHILL_MOJOM_URL_MOJOM_TRAITS_H_
+#ifndef SHILL_MOJOM_URL_MOJOM_URL_MOJOM_TRAITS_H_
+#define SHILL_MOJOM_URL_MOJOM_URL_MOJOM_TRAITS_H_
 
 #include <string>
 
 #include <mojo/public/cpp/bindings/struct_traits.h>
 #include <net-base/http_url.h>
 
-#include "shill/mojom/portal.mojom.h"
+#include "url/mojom/url.mojom.h"
 
 namespace mojo {
 
 template <>
-struct StructTraits<chromeos::connectivity::mojom::URLDataView,
-                    net_base::HttpUrl> {
+struct StructTraits<url::mojom::UrlDataView, net_base::HttpUrl> {
   static bool IsNull(const net_base::HttpUrl& url) {
     return url.protocol() == net_base::HttpUrl::Protocol::kUnknown;
   }
@@ -27,8 +26,7 @@ struct StructTraits<chromeos::connectivity::mojom::URLDataView,
     return url.ToString();
   }
 
-  static bool Read(chromeos::connectivity::mojom::URLDataView data,
-                   net_base::HttpUrl* out_url) {
+  static bool Read(url::mojom::UrlDataView data, net_base::HttpUrl* out_url) {
     mojo::StringDataView string_view;
     data.GetUrlDataView(&string_view);
 
@@ -45,4 +43,4 @@ struct StructTraits<chromeos::connectivity::mojom::URLDataView,
 };
 
 }  // namespace mojo
-#endif  // SHILL_MOJOM_URL_MOJOM_TRAITS_H_
+#endif  // SHILL_MOJOM_URL_MOJOM_URL_MOJOM_TRAITS_H_
