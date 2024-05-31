ArthurSonzogni/chromiumos-platform2

commit 672226c39b84fb536905a0844789497ab52e1f2b

init: startup: Use libstorage::platform
Finally use the platform object to access filesystem data (
mount points, files, ...)

BUG=b:172221451
TEST=Unit tests.

Change-Id: I59ee5bac36571eae77186cf2fd6b892a759d188f
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5332179
Reviewed-by: Alexis Savery <asavery@chromium.org>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>

init/startup/mount_helper_factory.cc
@@ -44,8 +44,8 @@ MountHelperFactory::MountHelperFactory(libstorage::Platform* platform,
 std::unique_ptr<MountHelper> MountHelperFactory::Generate(
     crossystem::Crossystem* crossystem) {
   bool dev_mode = InDevMode(crossystem);
-  bool is_test_image = IsTestImage(lsb_file_);
-  bool is_factory_mode = IsFactoryMode(crossystem, root_);
+  bool is_test_image = IsTestImage(platform_, lsb_file_);
+  bool is_factory_mode = IsFactoryMode(platform_, crossystem, root_);
 
   // Use factory mount helper.
   if (dev_mode && is_test_image && is_factory_mode) {

init/startup/startup_dep_impl.cc
@@ -20,7 +20,6 @@
 #include <base/logging.h>
 #include <base/strings/string_split.h>
 #include <base/strings/string_util.h>
-#include <brillo/blkdev_utils/lvm.h>
 #include <brillo/blkdev_utils/storage_utils.h>
 #include <brillo/files/file_util.h>
 #include <brillo/key_value_store.h>
@@ -34,8 +33,6 @@ namespace {
 
 constexpr char kProcCmdLine[] = "proc/cmdline";
 constexpr char kFactoryDir[] = "mnt/stateful_partition/dev_image/factory";
-
-const size_t kMaxReadSize = 4 * 1024;
 
 }  // namespace
 
@@ -57,10 +54,6 @@ bool StartupDep::Mount(const base::FilePath& src,
   std::string src_path = !src.value().empty() ? src.value() : type;
   return mount(src_path.c_str(), dst.value().c_str(), type.c_str(), flags,
                data.c_str()) == 0;
-}
-
-bool StartupDep::Umount(const base::FilePath& path) {
-  return !umount(path.value().c_str());
 }
 
 base::ScopedFD StartupDep::Open(const base::FilePath& pathname, int flags) {
@@ -262,9 +255,15 @@ bool InDevMode(crossystem::Crossystem* crossystem) {
 }
 
 // Determine if the device is using a test image.
-bool IsTestImage(const base::FilePath& lsb_file) {
+bool IsTestImage(libstorage::Platform* platform,
+                 const base::FilePath& lsb_file) {
   brillo::KeyValueStore store;
-  if (!store.Load(lsb_file)) {
+  std::string lsb_content;
+  if (!platform->ReadFileToString(lsb_file, &lsb_content)) {
+    PLOG(ERROR) << "Problem reading " << lsb_file.value();
+    return false;
+  }
+  if (!store.LoadFromString(lsb_content)) {
     PLOG(ERROR) << "Problem parsing " << lsb_file.value();
     return false;
   }
@@ -277,32 +276,31 @@ bool IsTestImage(const base::FilePath& lsb_file) {
 }
 
 // Return if the device is in factory test mode.
-bool IsFactoryTestMode(crossystem::Crossystem* crossystem,
+bool IsFactoryTestMode(libstorage::Platform* platform,
+                       crossystem::Crossystem* crossystem,
                        const base::FilePath& base_dir) {
   // The path to factory enabled tag. If this path exists in a debug build,
   // we assume factory test mode.
   base::FilePath factory_dir = base_dir.Append(kFactoryDir);
   base::FilePath factory_tag = factory_dir.Append("enabled");
-  struct stat statbuf;
   std::optional<int> res =
       crossystem->VbGetSystemPropertyInt(crossystem::Crossystem::kDebugBuild);
-  if (res == 1 && stat(factory_tag.value().c_str(), &statbuf) == 0 &&
-      S_ISREG(statbuf.st_mode)) {
+  if (res == 1 && platform->FileExists(factory_tag))
     return true;
-  }
+
   return false;
 }
 
 // Return if the device is in either in factory test mode or in factory
 // installer mode.
-bool IsFactoryMode(crossystem::Crossystem* crossystem,
+bool IsFactoryMode(libstorage::Platform* platform,
+                   crossystem::Crossystem* crossystem,
                    const base::FilePath& base_dir) {
-  if (IsFactoryTestMode(crossystem, base_dir))
+  if (IsFactoryTestMode(platform, crossystem, base_dir))
     return true;
 
   std::string cmdline;
-  if (!base::ReadFileToStringWithMaxSize(base_dir.Append(kProcCmdLine),
-                                         &cmdline, kMaxReadSize)) {
+  if (!platform->ReadFileToString(base_dir.Append(kProcCmdLine), &cmdline)) {
     PLOG(ERROR) << "Failed to read proc command line";
     return false;
   }
@@ -311,13 +309,8 @@ bool IsFactoryMode(crossystem::Crossystem* crossystem,
     return true;
   }
 
-  struct stat statbuf;
   base::FilePath installer = base_dir.Append("root/.factory_installer");
-  if (stat(installer.value().c_str(), &statbuf) == 0 &&
-      S_ISREG(statbuf.st_mode)) {
-    return true;
-  }
-  return false;
+  return platform->FileExists(installer);
 }
 
 }  // namespace startup

init/startup/startup_dep_impl.h
@@ -15,14 +15,17 @@
 #include <base/files/file_path.h>
 #include <base/files/scoped_file.h>
 #include <libcrossystem/crossystem.h>
+#include <libstorage/platform/platform.h>
 
 namespace startup {
 
 // Determine if the device is using a test image.
-bool IsTestImage(const base::FilePath& lsb_file);
+bool IsTestImage(libstorage::Platform* platform,
+                 const base::FilePath& lsb_file);
 
 // Determines if the device is in factory test mode.
-bool IsFactoryTestMode(crossystem::Crossystem* crossystem,
+bool IsFactoryTestMode(libstorage::Platform* platform,
+                       crossystem::Crossystem* crossystem,
                        const base::FilePath& base_dir);
 
 // Determine if the device is in dev mode.
@@ -31,7 +34,8 @@ bool IsDebugBuild(crossystem::Crossystem* crossystem);
 
 // Determines if the device is in either factory test mode or in factory
 // installer mode.
-bool IsFactoryMode(crossystem::Crossystem* crossystem,
+bool IsFactoryMode(libstorage::Platform* platform,
+                   crossystem::Crossystem* crossystem,
                    const base::FilePath& base_dir);
 
 // StartupDep defines functions that interface with the filesystem and
@@ -54,9 +58,6 @@ class StartupDep {
                      const std::string& type,
                      unsigned long flags,  // NOLINT(runtime/int)
                      const std::string& data);
-
-  // Wrapper around umount(2).
-  virtual bool Umount(const base::FilePath& path);
 
   // Wrapper around open(2).
   virtual base::ScopedFD Open(const base::FilePath& pathname, int flags);

init/startup/chromeos_startup.h
@@ -80,7 +80,8 @@ class ChromeosStartup {
   void RemoveVarEmpty();
   void CheckVarLog();
   void RestoreContextsForVar(
-      void (*restorecon_func)(const base::FilePath& path,
+      void (*restorecon_func)(libstorage::Platform* platform_,
+                              const base::FilePath& path,
                               const std::vector<base::FilePath>& exclude,
                               bool is_recursive,
                               bool set_digests));

init/startup/factory_mode_mount_helper.cc
@@ -44,8 +44,8 @@ FactoryModeMountHelper::FactoryModeMountHelper(libstorage::Platform* platform,
 bool FactoryModeMountHelper::DoMountVarAndHomeChronos() {
   base::FilePath option_file = stateful_.Append(kOptionsFile);
   std::string option = "";
-  if (base::PathExists(option_file)) {
-    base::ReadFileToString(option_file, &option);
+  if (platform_->FileExists(option_file)) {
+    platform_->ReadFileToString(option_file, &option);
   }
   if (option == "tmpfs") {
     // Mount tmpfs to /var/. When booting from USB disk, writing to /var/
@@ -54,14 +54,13 @@ bool FactoryModeMountHelper::DoMountVarAndHomeChronos() {
     // tmpfs on /var to improve performance. (especially when running
     // tests like touchpad, touchscreen).
     base::FilePath var = root_.Append(kVar);
-    if (!startup_dep_->Mount(base::FilePath("tmpfs_var"), var, "tmpfs", 0,
-                             "")) {
+    if (!platform_->Mount(base::FilePath("tmpfs_var"), var, "tmpfs", 0, "")) {
       return false;
     }
     base::FilePath stateful_home_chronos = stateful_.Append(kHomeChronos);
     base::FilePath home_chronos = root_.Append(kHomeChronos);
-    if (!startup_dep_->Mount(stateful_home_chronos, home_chronos, "", MS_BIND,
-                             "")) {
+    if (!platform_->Mount(stateful_home_chronos, home_chronos, "", MS_BIND,
+                          "")) {
       return false;
     }
     return true;

init/startup/fake_startup_dep_impl.cc
@@ -11,13 +11,14 @@
 
 #include <base/files/file_util.h>
 #include <base/logging.h>
-#include <brillo/files/file_util.h>
+#include <libstorage/platform/platform.h>
 
 #include "init/startup/fake_startup_dep_impl.h"
 
 namespace startup {
 
-FakeStartupDep::FakeStartupDep() : StartupDep() {}
+FakeStartupDep::FakeStartupDep(libstorage::Platform* platform)
+    : StartupDep(), platform_(platform) {}
 
 void FakeStartupDep::SetStatResultForPath(const base::FilePath& path,
                                           const struct stat& st) {
@@ -27,11 +28,6 @@ void FakeStartupDep::SetStatResultForPath(const base::FilePath& path,
 void FakeStartupDep::SetStatvfsResultForPath(const base::FilePath& path,
                                              const struct statvfs& st) {
   result_statvfs_map_[path.value()] = st;
-}
-
-void FakeStartupDep::SetMountResultForPath(const base::FilePath& path,
-                                           const std::string& output) {
-  mount_result_map_[path.value()] = output;
 }
 
 void FakeStartupDep::SetMountEncOutputForArg(const std::string& arg,
@@ -78,18 +74,6 @@ bool FakeStartupDep::Mount(const base::FilePath& src,
                            const std::string& type,
                            unsigned long flags,  // NOLINT(runtime/int)
                            const std::string& data) {
-  std::unordered_map<std::string, std::string>::iterator it;
-  it = mount_result_map_.find(dst.value());
-
-  if (it == mount_result_map_.end()) {
-    return false;
-  }
-
-  return src.value().compare(it->second) == 0;
-}
-
-bool FakeStartupDep::Umount(const base::FilePath& path) {
-  umount_vector_.push_back(path.value());
   return true;
 }
 
@@ -123,12 +107,12 @@ void FakeStartupDep::BootAlert(const std::string& arg) {
 void FakeStartupDep::RemoveInBackground(
     const std::vector<base::FilePath>& paths) {
   for (auto path : paths) {
-    brillo::DeletePathRecursively(path);
+    platform_->DeletePathRecursively(path);
   }
 }
 
 void FakeStartupDep::ClobberLog(const std::string& msg) {
-  WriteFile(clobber_log_, msg);
+  platform_->WriteStringToFile(clobber_log_, msg);
 }
 
 void FakeStartupDep::Clobber(const std::string& boot_alert_msg,

commit a0d4c9cdd1c6af7dfcf5c8030745682a48719b2d

init: startup: Remove crossystem from IsFactoryMode
crossystem is already in platform object, no need to add to the argument
list.

BUG=b:172221451
TEST=Unit test.

Fixes: 672226c39b ("init: startup: Use libstorage::platform")
Change-Id: I864783d64dabe8df53be6b8cc1d50116923c8c89
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5552521
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Alexis Savery <asavery@chromium.org>

init/startup/mount_helper_factory.cc
@@ -45,7 +45,7 @@ std::unique_ptr<MountHelper> MountHelperFactory::Generate(
     crossystem::Crossystem* crossystem) {
   bool dev_mode = InDevMode(crossystem);
   bool is_test_image = IsTestImage(platform_, lsb_file_);
-  bool is_factory_mode = IsFactoryMode(platform_, crossystem, root_);
+  bool is_factory_mode = IsFactoryMode(platform_, root_);
 
   // Use factory mount helper.
   if (dev_mode && is_test_image && is_factory_mode) {

init/startup/startup_dep_impl.cc
@@ -218,14 +218,13 @@ bool IsTestImage(libstorage::Platform* platform,
 // Return if the device is in either in factory test mode or in factory
 // installer mode.
 bool IsFactoryMode(libstorage::Platform* platform,
-                   crossystem::Crossystem* crossystem,
                    const base::FilePath& base_dir) {
   // The path to factory enabled tag. If this path exists in a debug build,
   // we assume factory test mode.
   base::FilePath factory_dir = base_dir.Append(kFactoryDir);
   base::FilePath factory_tag = factory_dir.Append("enabled");
-  std::optional<int> res =
-      crossystem->VbGetSystemPropertyInt(crossystem::Crossystem::kDebugBuild);
+  std::optional<int> res = platform->GetCrosssystem()->VbGetSystemPropertyInt(
+      crossystem::Crossystem::kDebugBuild);
   if (res == 1 && platform->FileExists(factory_tag))
     return true;
 

init/startup/startup_dep_impl.h
@@ -29,7 +29,6 @@ bool IsDebugBuild(crossystem::Crossystem* crossystem);
 // Determines if the device is in either factory test mode or in factory
 // installer mode.
 bool IsFactoryMode(libstorage::Platform* platform,
-                   crossystem::Crossystem* crossystem,
                    const base::FilePath& base_dir);
 
 // StartupDep defines functions that interface with the filesystem and