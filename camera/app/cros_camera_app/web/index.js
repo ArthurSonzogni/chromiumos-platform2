fb45b16daf1eaa8fd29a3fa7340a8b8e99561d4c
Shik Chen <shik@chromium.org>
2024-06-07 12:20:42 +0800

    camera: app: Show FPS counter in web server side panel
    BUG=b:343626256
    TEST=Run `cca web` and check manually
    
    Change-Id: I134f322b3b57b1458dccfe07436fbbf54ddaa938
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5606105
    Tested-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>

--- camera/app/cros_camera_app/web/index.js
+++ camera/app/cros_camera_app/web/index.js
@@ -2,16 +2,65 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-async function init() {
-  const videoEl = document.querySelector("video");
+class FpsCounter {
+  /**
+   * @param {HTMLVideoElement} video
+   * @param {number} len
+   */
+  constructor(video, len) {
+    this.video = video;
+    this.len = len;
 
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: {
-      width: { ideal: 1280 },
-      frameRate: { ideal: 30 },
-    },
-  });
-  videoEl.srcObject = stream;
+    /** @type {number[]} */
+    this.timestamps = [];
+
+    /** @type {HTMLDivElement} */
+    this.el = document.getElementById("fps-counter");
+  }
+
+  /**
+   * @param {DOMHighResTimeStamp} now
+   */
+  update(now) {
+    this.timestamps.push(now);
+    if (this.timestamps.length >= this.len * 2) {
+      this.timestamps = this.timestamps.slice(-this.len);
+    }
+
+    const count = Math.min(this.len, this.timestamps.length);
+    const start = this.timestamps.at(-count);
+    const fps = count === 0 ? 0 : ((count - 1) * 1000) / (now - start);
+    this.el.textContent = `FPS: ${fps.toFixed(0)}`;
+
+    this.video.requestVideoFrameCallback(this.update.bind(this));
+  }
+
+  start() {
+    this.video.requestVideoFrameCallback(this.update.bind(this));
+  }
+}
+
+class CameraApp {
+  constructor() {
+    this.video = document.querySelector("video");
+    this.fpsCounter = new FpsCounter(this.video, 100);
+  }
+
+  async start() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        width: { ideal: 1280 },
+        frameRate: { ideal: 30 },
+      },
+    });
+    this.video.srcObject = stream;
+    this.fpsCounter.start();
+  }
+}
+
+async function init() {
+  const app = new CameraApp();
+  await app.start();
 }
 
 init();
