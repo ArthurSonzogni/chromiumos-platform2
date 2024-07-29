d518421b3c5356b175c61e7c2cb2f488fc8c4de3
Byron Lee <byronlee@google.com>
2024-07-29 02:28:29 +0000

    diagnostics: Refactor MeminfoReaderTest
    Check different memory metrics in separate unit tests.
    
    BUG=None
    TEST=FEATURES=test emerge-volteer diagnostics
    
    Change-Id: I4539eb1cec92da4bf08e1af8842cd81cb258bb48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5741910
    Reviewed-by: Wei-Luan Wang <weiluanwang@google.com>
    Tested-by: Byron Lee <byronlee@chromium.org>
    Commit-Queue: Byron Lee <byronlee@chromium.org>

--- diagnostics/cros_healthd/system/meminfo_reader_test.cc
+++ diagnostics/cros_healthd/system/meminfo_reader_test.cc
@@ -6,12 +6,19 @@
 
 #include <string>
 
+#include <base/strings/strcat.h>
 #include <gtest/gtest.h>
 
 #include "diagnostics/base/file_test_utils.h"
 
 namespace diagnostics {
 namespace {
+
+struct FakeMeminfoContent {
+  std::string mem_total = "MemTotal:        0 kB\n";
+  std::string mem_free = "MemFree:         0 kB\n";
+  std::string mem_available = "MemAvailable:    0 kB\n";
+};
 
 class MeminfoReaderTest : public BaseFileTest {
  public:
@@ -21,94 +28,77 @@ class MeminfoReaderTest : public BaseFileTest {
  protected:
   MeminfoReaderTest() = default;
 
-  void SetMockMemoryInfo(const std::string& info) {
-    SetFile({"proc", "meminfo"}, info);
+  void SetFakeMemoryInfo(const FakeMeminfoContent& fake_meminfo) {
+    SetFile({"proc", "meminfo"}, base::StrCat({
+                                     fake_meminfo.mem_total,
+                                     fake_meminfo.mem_free,
+                                     fake_meminfo.mem_available,
+                                 }));
   }
 
   MeminfoReader meminfo_reader_;
 };
 
-TEST_F(MeminfoReaderTest, MeminfoSuccess) {
-  SetMockMemoryInfo(
-      "MemTotal:        3906320 kB\n"
-      "MemFree:         873180 kB\n"
-      "MemAvailable:    87980 kB\n");
-
+TEST_F(MeminfoReaderTest, TotalMemory) {
+  SetFakeMemoryInfo({.mem_total = "MemTotal:        3906320 kB\n"});
   auto memory_info = meminfo_reader_.GetInfo();
-  EXPECT_TRUE(memory_info.has_value());
+  ASSERT_TRUE(memory_info.has_value());
   EXPECT_EQ(memory_info.value().total_memory_kib, 3906320);
+}
+
+TEST_F(MeminfoReaderTest, NoTotalMemory) {
+  SetFakeMemoryInfo({.mem_total = ""});
+  EXPECT_FALSE(meminfo_reader_.GetInfo().has_value());
+}
+
+TEST_F(MeminfoReaderTest, FreeMemory) {
+  SetFakeMemoryInfo({.mem_free = "MemFree:         873180 kB\n"});
+  auto memory_info = meminfo_reader_.GetInfo();
+  ASSERT_TRUE(memory_info.has_value());
   EXPECT_EQ(memory_info.value().free_memory_kib, 873180);
+}
+
+TEST_F(MeminfoReaderTest, NoFreeMemory) {
+  SetFakeMemoryInfo({.mem_free = ""});
+  EXPECT_FALSE(meminfo_reader_.GetInfo().has_value());
+}
+
+TEST_F(MeminfoReaderTest, AvailableMemory) {
+  SetFakeMemoryInfo({.mem_available = "MemAvailable:    87980 kB\n"});
+  auto memory_info = meminfo_reader_.GetInfo();
+  ASSERT_TRUE(memory_info.has_value());
   EXPECT_EQ(memory_info.value().available_memory_kib, 87980);
 }
 
-TEST_F(MeminfoReaderTest, MeminfoNoFile) {
-  auto memory_info = meminfo_reader_.GetInfo();
-  EXPECT_FALSE(memory_info.has_value());
+TEST_F(MeminfoReaderTest, NoAvailableMemory) {
+  SetFakeMemoryInfo({.mem_available = ""});
+  EXPECT_FALSE(meminfo_reader_.GetInfo().has_value());
+}
+
+TEST_F(MeminfoReaderTest, MissingMeminfoFile) {
+  UnsetPath({"proc", "meminfo"});
+  EXPECT_FALSE(meminfo_reader_.GetInfo().has_value());
 }
 
 TEST_F(MeminfoReaderTest, MeminfoFormattedIncorrectly) {
-  SetMockMemoryInfo("Incorrectly formatted meminfo contents.\n");
-
-  auto memory_info = meminfo_reader_.GetInfo();
-  EXPECT_FALSE(memory_info.has_value());
+  SetFile({"proc", "meminfo"}, "Incorrectly formatted meminfo contents.\n");
+  EXPECT_FALSE(meminfo_reader_.GetInfo().has_value());
 }
 
-TEST_F(MeminfoReaderTest, MeminfoNoMemTotal) {
-  SetMockMemoryInfo(
-      "MemFree:         873180 kB\n"
-      "MemAvailable:    87980 kB\n");
-
-  auto memory_info = meminfo_reader_.GetInfo();
-  EXPECT_FALSE(memory_info.has_value());
-}
-
-TEST_F(MeminfoReaderTest, MeminfoNoMemFree) {
-  SetMockMemoryInfo(
-      "MemTotal:        3906320 kB\n"
-      "MemAvailable:    87980 kB\n");
-
-  auto memory_info = meminfo_reader_.GetInfo();
-  EXPECT_FALSE(memory_info.has_value());
-}
-
-TEST_F(MeminfoReaderTest, MeminfoNoMemAvailable) {
-  SetMockMemoryInfo(
-      "MemTotal:        3906320 kB\n"
-      "MemFree:         873180 kB\n");
-
-  auto memory_info = meminfo_reader_.GetInfo();
-  EXPECT_FALSE(memory_info.has_value());
-}
-
-TEST_F(MeminfoReaderTest, MeminfoIncorrectlyFormattedMemTotal) {
+TEST_F(MeminfoReaderTest, IncorrectFormat) {
   // No space between memory amount and unit.
-  SetMockMemoryInfo(
-      "MemTotal:        3906320kB\n"
-      "MemFree:         873180 kB\n"
-      "MemAvailable:    87980 kB\n");
-
-  auto memory_info = meminfo_reader_.GetInfo();
-  EXPECT_FALSE(memory_info.has_value());
+  SetFakeMemoryInfo({.mem_total = "MemTotal:        3906320kB\n"});
+  EXPECT_FALSE(meminfo_reader_.GetInfo().has_value());
 }
 
-TEST_F(MeminfoReaderTest, MeminfoIncorrectlyFormattedMemFree) {
-  SetMockMemoryInfo(
-      "MemTotal:        3906320 kB\n"
-      "MemFree:         873180 WrongUnit\n"
-      "MemAvailable:    87980 kB\n");
-
-  auto memory_info = meminfo_reader_.GetInfo();
-  EXPECT_FALSE(memory_info.has_value());
+TEST_F(MeminfoReaderTest, WrongUnit) {
+  SetFakeMemoryInfo({.mem_free = "MemFree:         873180 WrongUnit\n"});
+  EXPECT_FALSE(meminfo_reader_.GetInfo().has_value());
 }
 
-TEST_F(MeminfoReaderTest, MeminfoIncorrectlyFormattedMemAvailable) {
-  SetMockMemoryInfo(
-      "MemTotal:        3906320 kB\n"
-      "MemFree:         873180 kB\n"
-      "MemAvailable:    NotAnInteger kB\n");
-
-  auto memory_info = meminfo_reader_.GetInfo();
-  EXPECT_FALSE(memory_info.has_value());
+TEST_F(MeminfoReaderTest, InvalidInteger) {
+  SetFakeMemoryInfo({.mem_available = "MemAvailable:    NotAnInteger kB\n"});
+  EXPECT_FALSE(meminfo_reader_.GetInfo().has_value());
 }
 
 }  // namespace
