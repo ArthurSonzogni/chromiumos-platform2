2a8b6e700288777748bea3477df8b42dbd890a7d
Craig Hesling <hesling@chromium.org>
2024-08-16 00:11:11 -0700

    fpstudy: Reduce linter warnings in collection
    * Remove shebang for non-executable file
    * Add doc strings and fix others
    
    BUG=b:343809394
    TEST=python -m unittest discover -v -p '*_test.py'
    TEST=cros lint collection.py
    
    Change-Id: Ia5942ddf0838c85664d3f8398e9b58d2c859e447
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5795372
    Reviewed-by: Firas Sammoura <fsammoura@google.com>
    Commit-Queue: Craig Hesling <hesling@chromium.org>
    Tested-by: Craig Hesling <hesling@chromium.org>

--- biod/study/analysis-tool/collection.py
+++ biod/study/analysis-tool/collection.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
 # Copyright 2022 The ChromiumOS Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+"""House the classes for working with fpstudy collection directories."""
 
 from __future__ import annotations
 
