7bb087e9043666a576036e0234dbd870eba6a02d
Jeffery Miller <jefferymiller@google.com>
2024-08-29 14:05:47 -0500

    tpm_manager: Extend TPM allow list
    Add Latitude 3420
    
    BUG=b:357914302
    TEST=tast run dutf hwsec.* u2fd.* cryptophome.*
    TEST=tpm_manager_client clear_tpm
    
    Change-Id: I0307042c663002b95b1e9f160f7dd6002189109d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5916039
    Commit-Queue: Jeffery Miller <jefferymiller@google.com>
    Reviewed-by: Yi Chou <yich@google.com>
    Tested-by: Jeffery Miller <jefferymiller@google.com>

--- tpm_manager/server/tpm_allowlist_impl.cc
+++ tpm_manager/server/tpm_allowlist_impl.cc
@@ -91,6 +91,7 @@ constexpr DeviceName kTpm2DeviceNameAllowlist[] = {
     DeviceName{"Dell Inc.", "Latitude 5420", kVendorIdStm},
     DeviceName{"HP", "HP EliteBook 840 G8 Notebook PC", kVendorIdIfx},
     DeviceName{"HP", "HP ProDesk 600 G3 SFF", kVendorIdIfx},
+    DeviceName{"Dell Inc.", "Latitude 3420", kVendorIdStm},
 };
 
 std::optional<bool> IsTpmFileEnabled() {
