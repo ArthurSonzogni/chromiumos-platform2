ArthurSonzogni/chromiumos-platform2

commit 55f76cba1df882339e72668b46c38c8182a6d06e

libmems: Check sys path before creating IioDevice instances
As libiio doesn't support device removal, libmems needs this patch to
avoid creating IioDevice instances when the devices are not available.

BUG=b:287255834
TEST=run on geralt

Change-Id: I7e53336310f3f2b92b10687331e48bcd1f0e5729
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4614787
Commit-Queue: Harvey Yang <chenghaoyang@chromium.org>
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Harvey Yang <chenghaoyang@chromium.org>

libmems/iio_context_impl.cc
@@ -8,6 +8,7 @@
 #include <utility>
 
 #include <base/check.h>
+#include <base/files/file_util.h>
 #include <base/strings/stringprintf.h>
 #include <base/logging.h>
 
@@ -154,7 +155,7 @@ std::vector<IioDevice*> IioContextImpl::GetAll(
 
   for (uint32_t i = 0; i < dev_count; ++i) {
     iio_device* dev = iio_context_get_device(ctx, i);
-    if (!dev) {
+    if (!dev || !base::DirectoryExists(T::GetPathById(i))) {
       LOG(WARNING) << "Unable to get " << i << "th device";
       continue;
     }

libmems/iio_device_impl.cc
@@ -47,6 +47,11 @@ std::string IioDeviceImpl::GetStringFromId(int id) {
   return base::StringPrintf("%s%d", kDeviceIdPrefix, id);
 }
 
+// static
+base::FilePath IioDeviceImpl::GetPathById(int id) {
+  return base::FilePath(kSysDevString).Append(GetStringFromId(id));
+}
+
 IioDeviceImpl::IioDeviceImpl(IioContextImpl* ctx, iio_device* dev)
     : IioDevice(),
       context_(ctx),
@@ -113,8 +118,7 @@ int IioDeviceImpl::GetId() const {
 }
 
 base::FilePath IioDeviceImpl::GetPath() const {
-  std::string id_str = GetStringFromId(GetId());
-  auto path = base::FilePath(kSysDevString).Append(id_str);
+  auto path = GetPathById(GetId());
   CHECK(base::DirectoryExists(path));
   return path;
 }

libmems/iio_device_impl.h
@@ -26,6 +26,7 @@ class LIBMEMS_EXPORT IioDeviceImpl : public IioDevice {
  public:
   static std::optional<int> GetIdFromString(const char* id_str);
   static std::string GetStringFromId(int id);
+  static base::FilePath GetPathById(int id);
 
   // iio_device objects are kept alive by the IioContextImpl.
   IioDeviceImpl(IioContextImpl* ctx, iio_device* dev);

libmems/iio_device_trigger_impl.cc
@@ -38,6 +38,10 @@ std::string IioDeviceTriggerImpl::GetStringFromId(int id) {
   return base::StringPrintf("trigger%d", id);
 }
 
+base::FilePath IioDeviceTriggerImpl::GetPathById(int id) {
+  return base::FilePath(kSysDevString).Append(GetStringFromId(id));
+}
+
 IioDeviceTriggerImpl::IioDeviceTriggerImpl(IioContextImpl* ctx, iio_device* dev)
     : IioDevice(), context_(ctx), trigger_(dev) {
   CHECK(context_);
@@ -64,12 +68,12 @@ int IioDeviceTriggerImpl::GetId() const {
 }
 
 base::FilePath IioDeviceTriggerImpl::GetPath() const {
-  int id = GetId();
-  std::string id_str = std::string(kIioSysfsTrigger);
-  if (id >= 0)
-    id_str = GetStringFromId(id);
+  base::FilePath path;
+  if (GetId() >= 0)
+    path = GetPathById(GetId());
+  else
+    path = base::FilePath(kSysDevString).Append(std::string(kIioSysfsTrigger));
 
-  auto path = base::FilePath(kSysDevString).Append(id_str);
   CHECK(base::DirectoryExists(path));
   return path;
 }

libmems/iio_device_trigger_impl.h
@@ -22,6 +22,8 @@ class IioContextImpl;
 
 class LIBMEMS_EXPORT IioDeviceTriggerImpl : public IioDevice {
  public:
+  static base::FilePath GetPathById(int id);
+
   // Return -1 for iio_sysfs_trigger
   static std::optional<int> GetIdFromString(const char* id_str);
   // Return iio_sysfs_trigger for -1

commit ff7e878ef8d2695534b372fa168c68e165a6f84e

libmems: Check sys path using device id
Since libiio now supports hwmon, the index in the device array is not
the device id anymore. Retrieve the device id (4 from iio:device4 for
instance), and then check is /sys/bus/iio/devices/iio:device4 exists.

Fixes: commit 55f76cba1df8 ("libmems: Check sys path before creating IioDevice instances")

BUG=b:287255834,b:366363916
TEST=Manually test on octopus with Shimless RMA
TEST=tast run DUT shimlessrma.Calibration.gyro

Change-Id: I6d09ac5535b0866b31656cf0a095e6d1605e7525
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5871725
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Harvey Yang <chenghaoyang@chromium.org>
Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>

libmems/iio_context_impl.cc
@@ -151,7 +151,7 @@ std::vector<IioDevice*> IioContextImpl::GetAll(
   iio_context* ctx = GetCurrentContext();
   for (uint32_t i = 0; i < iio_context_get_devices_count(ctx); ++i) {
     iio_device* dev = iio_context_get_device(ctx, i);
-    if (!dev || !base::DirectoryExists(T::GetPathById(i))) {
+    if (!dev) {
       continue;
     }
 
@@ -159,9 +159,15 @@ std::vector<IioDevice*> IioContextImpl::GetAll(
     if (!id_str)
       continue;
 
-    auto id = T::GetIdFromString(id_str);
+    std::optional<int> id = T::GetIdFromString(id_str);
     if (!id.has_value())
       continue;
+
+    // libiio does not support removal, make sure the device has not been removed
+    // by checking if its directory in sysfs still exists.
+    if (!base::DirectoryExists(T::GetPathById(id.value()))) {
+      continue;
+    }
 
     devices.push_back(GetById(id.value(), devices_map));
   }