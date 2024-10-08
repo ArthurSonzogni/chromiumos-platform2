edde19742c5f1c850193f3824e399087f08b817b
Soichiro Fujii <soichirof@google.com>
2024-10-08 06:21:26 +0000

    featured: Look for /build/lib for linking against test library in 32bit target
    Current configuration in build.rs that looks for test library
    deployed in /build directory only accounts for a 64bit target,
    which causes failure when trying to test for a 32bit target.
    
    This CL adds /build/lib into one of the location to look for
    so any 32bit target can compile the test without failing
    to locate the library to link to.
    
    BUG=b:242920490
    TEST=After emerging the library to /build/lib,
    `FEATURES=test emerge-hana dev-rust/featured` compiles correctly.
    TEST=CQ passses.
    
    Change-Id: Ib5129a70db26085cb94d5366804920f16e3308c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5914994
    Tested-by: Soichiro Fujii <soichirof@google.com>
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Commit-Queue: Soichiro Fujii <soichirof@google.com>
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>

--- featured/rust-client/build.rs
+++ featured/rust-client/build.rs
@@ -11,11 +11,12 @@ fn main() {
     // details: b/242920490
     if cfg!(feature = "fake_backend") {
         if let Ok(sysroot) = std::env::var("SYSROOT") {
-            // '/build/lib64' is required to link to the c_fake_feature_library.
+            // '/build/lib' or '/build/lib64' is required to link to the c_fake_feature_library.
             // this rpath is the same as the one that gets added in BUILD.gn
             // if you include the '//common-mk:test' config.
             println!("cargo:rustc-link-arg=-Wl,-rpath=\
             $ORIGIN/:$ORIGIN/lib:/build/lib:/build/lib64:/usr/local/lib:/usr/local/lib64");
+            println!("cargo:rustc-link-search=native={}/build/lib", sysroot);
             println!("cargo:rustc-link-search=native={}/build/lib64", sysroot);
             println!("cargo:rustc-link-lib=dylib=c_fake_feature_library");
         }
