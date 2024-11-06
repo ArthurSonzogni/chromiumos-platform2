415091f60bc235bbb8d933cfa1fd83c53cc40c86
Cheng-Yu Lee <cylee@google.com>
2024-11-06 11:56:09 +0800

    odml: [CORAL] remove the line break at the end of embedding prompt
    The line breaking doesn't change the embedding vector at all, so remove it.
    
    BUG=b:361429567
    TEST=unittest
    
    Change-Id: Ieb0064f207c7d3859dd5a6a1b8e5acc777c9fc3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5997731
    Commit-Queue: ChromeOS Auto Runner <chromeos-auto-runner@chromeos-bot.iam.gserviceaccount.com>
    Commit-Queue: Cheng-Yu Lee <cylee@google.com>
    Tested-by: Cheng-Yu Lee <cylee@google.com>
    Reviewed-by: Howard Yang <hcyang@google.com>

--- odml/coral/embedding/engine.cc
+++ odml/coral/embedding/engine.cc
@@ -77,11 +77,11 @@ namespace internal {
 // TODO(b/361429567): Switch to the final prompt for the feature.
 std::string EntityToEmbeddingPrompt(const mojom::Entity& entity) {
   if (entity.is_app()) {
-    return base::StringPrintf("A page with title: \"%s\"\n",
+    return base::StringPrintf("A page with title: \"%s\"",
                               entity.get_app()->title.c_str());
   } else if (entity.is_tab()) {
     const mojom::Tab& tab = *entity.get_tab();
-    return base::StringPrintf("A page with title: \"%s\" and URL: \"%s\"\n",
+    return base::StringPrintf("A page with title: \"%s\" and URL: \"%s\"",
                               tab.title.c_str(), tab.url->url.c_str());
   }
   return "";
