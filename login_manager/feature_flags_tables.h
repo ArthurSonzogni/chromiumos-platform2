2a6c3452b01715e4d1ea1483e6a77a7be761d044
Morten Stenshorne <mstensho@chromium.org>
2024-11-07 09:39:14 +0000

    Remove references to NearbySharingDeviceContacts.
    Removed from Chromium here:
    https://chromium-review.googlesource.com/c/chromium/src/+/5999924
    
    Bug: 356235459
    Change-Id: I38434bf08a27a536c1332f06f4665ce91c27d7cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5999740
    Tested-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>

--- login_manager/feature_flags_tables.h
+++ login_manager/feature_flags_tables.h
@@ -923,8 +923,6 @@ constexpr FeatureMappingEntry kFeaturesMap[] = {
      "enable-navigation-predictor-renderer-warmup", 1},
     {"NearbySharing", "0", "nearby-sharing", 2},
     {"NearbySharing", "1", "nearby-sharing", 1},
-    {"NearbySharingDeviceContacts", "0", "nearby-sharing-device-contacts", 2},
-    {"NearbySharingDeviceContacts", "1", "nearby-sharing-device-contacts", 1},
     {"NearbySharingWebRtc", "0", "nearby-sharing-webrtc", 2},
     {"NearbySharingWebRtc", "1", "nearby-sharing-webrtc", 1},
     {"NewDragSpecInLauncher", "0", "enable-launcher-app-paging", 2},
