02980af264b8252fb0f0ad995699d543de0e483c
John L Chen <zuan@chromium.org>
2024-11-14 23:53:46 +0800

    odml: Add benchmark option for embedding_model_console
    BUG=b:379041430
    TEST=Deploy and run
    
    Change-Id: I9d02551e29a856db66f44353099caa40db924762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6022361
    Reviewed-by: Howard Yang <hcyang@google.com>
    Tested-by: John L Chen <zuan@chromium.org>
    Commit-Queue: John L Chen <zuan@chromium.org>

--- odml/embedding_model_console.cc
+++ odml/embedding_model_console.cc
@@ -5,10 +5,14 @@
 #include <stdio.h>
 
 #include <algorithm>
+#include <iostream>
+#include <memory>
+#include <string>
 
 #include <base/command_line.h>
 #include <base/files/file_util.h>
 #include <base/functional/bind.h>
+#include <base/memory/raw_ref.h>
 #include <base/run_loop.h>
 #include <base/task/single_thread_task_executor.h>
 #include <base/task/single_thread_task_runner.h>
@@ -43,6 +47,38 @@ constexpr const char kContent[] = "content";
 constexpr const char kTaskType[] = "task_type";
 constexpr const char kTruncateInput[] = "truncate_input";
 constexpr const char kBinaryOutput[] = "binary_output";
+// --benchmark specifies benchmark mode.
+constexpr const char kBenchmark[] = "benchmark";
+// --benchmark_run_count is the number of inference we'll make.
+constexpr const char kBenchmarkRunCount[] = "benchmark_run_count";
+// --benchmark_max_seconds is the approximate maximum time we'll run. If we
+// cannot finish --benchmark_run_count within the specified time, we'll cut it
+// short.
+constexpr const char kBenchmarkMaxSeconds[] = "benchmark_max_seconds";
+
+const constexpr char* kBenchmarkPrompts[] = {
+    "Tokyo City Guide - What to do in Tokyo",
+    "numpy.stack() in Python",
+    "Mountain View, CA - Google Map",
+    "Best Hiking Trails near San Francisco",
+    "Understanding Quantum Mechanics",
+    "Top 10 Restaurants in Paris",
+    "How to Bake a Perfect Chocolate Cake",
+    "Learning to Play the Guitar for Beginners",
+    "The History of the Roman Empire",
+    "The Benefits of Meditation",
+    "Effective Time Management Strategies",
+    "The Art of Public Speaking",
+    "Climate Change and its Impact",
+    "The Future of Artificial Intelligence",
+    "Space Exploration and the Search for Life",
+    "The Wonders of the Natural World",
+    "Investing in the Stock Market",
+    "The Psychology of Human Behavior",
+    "The Power of Positive Thinking",
+    "How to Write a Compelling Story"};
+constexpr size_t kBenchmarkPromptsSize =
+    sizeof(kBenchmarkPrompts) / sizeof(kBenchmarkPrompts[0]);
 
 TaskType GetTaskTypeFromString(std::string s) {
   if (s == "clustering") {
@@ -76,6 +112,137 @@ void WriteEmbedding(const std::vector<float>& embedding,
 
   bool ret = base::WriteFile(base::FilePath(path), embedding_content);
   CHECK(ret) << "Failed to write file.";
+}
+
+class EmbeddingBenchmark {
+ public:
+  EmbeddingBenchmark(raw_ref<mojo::Remote<OnDeviceEmbeddingModel>> model,
+                     TaskType task_type)
+      : model_(model), task_type_(task_type) {}
+
+  void Run(int run_count, int max_seconds);
+
+ private:
+  // How many times do we want to run during the benchmark?
+  int target_run_count_ = -1;
+  // If we hit this amount of seconds, we'll cut the benchmark short.
+  // This is needed because certain devices (such as CPU) is much slower and we
+  // want to benchmark with a maximum time instead of specified number of tries.
+  int max_seconds_;
+  // How many times has we called GenerateEmbedding()?
+  int launched_count_;
+  // How many times has GenerateEmbedding() returned a result?
+  int finish_count_;
+  // How many GenerateEmbedding() calls are invoked but not yet finished?
+  int in_flight_count_;
+  // Has max_seconds_ been exceeded?
+  bool has_been_cut_short_;
+  // The start of benchmarking.
+  base::TimeTicks start_;
+  // End of benchmarking.
+  base::TimeTicks end_;
+  raw_ref<mojo::Remote<OnDeviceEmbeddingModel>> model_;
+  TaskType task_type_;
+  std::unique_ptr<base::RunLoop> run_loop_;
+
+  // This is the maximum number of concurrent request send to the embedding
+  // service backend at the same time. A value of 2 is picked at the moment
+  // because we want to ensure it's larger than 1 so that the roundtrip IPC
+  // latency doesn't skew the benchmark result and it's not too large so we can
+  // efficiently cut-off the benchmarking due to time limit. Also note that
+  // we've a maximum in-flight request count because we're measuring throughput
+  // and not latency. If we decide to measure latency in the future then we'll
+  // need to remove such cap so as to not distort the percentile latency figures
+  // contributed by queueing delay.
+  static constexpr int kMaxInflightCount = 2;
+
+  std::string GetContent(int idx);
+  void LaunchOne();
+  void OnFinish(OnDeviceEmbeddingModelInferenceError error,
+                const std::vector<float>& embeddings);
+  void PrintStats();
+};
+
+void EmbeddingBenchmark::Run(int run_count, int max_seconds) {
+  CHECK_EQ(target_run_count_, -1);  // run() should only be called once.
+  target_run_count_ = run_count;
+  max_seconds_ = max_seconds;
+  launched_count_ = 0;
+  finish_count_ = 0;
+  in_flight_count_ = 0;
+  has_been_cut_short_ = false;
+  run_loop_ = std::make_unique<base::RunLoop>();
+  start_ = base::TimeTicks::Now();
+  LaunchOne();
+  run_loop_->Run();
+  PrintStats();
+}
+
+std::string EmbeddingBenchmark::GetContent(int idx) {
+  idx = idx % kBenchmarkPromptsSize;
+  return std::string(kBenchmarkPrompts[idx]);
+}
+
+void EmbeddingBenchmark::LaunchOne() {
+  GenerateEmbeddingRequest generate_embedding_request;
+  generate_embedding_request.content = GetContent(launched_count_);
+  generate_embedding_request.task_type = task_type_;
+  generate_embedding_request.truncate_input = true;
+
+  in_flight_count_++;
+  launched_count_++;
+  (*model_)->GenerateEmbedding(
+      generate_embedding_request.Clone(),
+      base::BindOnce(&EmbeddingBenchmark::OnFinish, base::Unretained(this)));
+}
+
+void EmbeddingBenchmark::OnFinish(OnDeviceEmbeddingModelInferenceError error,
+                                  const std::vector<float>& embeddings) {
+  base::TimeTicks current_time = base::TimeTicks::Now();
+  in_flight_count_--;
+  finish_count_++;
+  CHECK_EQ(error, OnDeviceEmbeddingModelInferenceError::kSuccess);
+
+  if (!has_been_cut_short_ &&
+      ((current_time - start_) > base::Seconds(max_seconds_))) {
+    // We've not finished yet, we need to cut it short and let all in-flight
+    // requests finish.
+    target_run_count_ = launched_count_;
+    has_been_cut_short_ = true;
+  }
+
+  if (finish_count_ >= target_run_count_) {
+    CHECK_EQ(finish_count_, target_run_count_);
+    end_ = base::TimeTicks::Now();
+    run_loop_->Quit();
+    return;
+  }
+
+  if (launched_count_ < target_run_count_ &&
+      in_flight_count_ < kMaxInflightCount) {
+    LaunchOne();
+  }
+}
+
+void EmbeddingBenchmark::PrintStats() {
+  base::TimeDelta run_time = end_ - start_;
+  std::cout << "Embedding benchmark result: " << run_time.InMilliseconds()
+            << " ms for " << target_run_count_ << " invocations" << std::endl;
+}
+
+void RunBenchmark(raw_ref<mojo::Remote<OnDeviceEmbeddingModel>> model,
+                  base::CommandLine* cl) {
+  EmbeddingBenchmark bench(
+      model, GetTaskTypeFromString(cl->GetSwitchValueASCII(kTaskType)));
+  int run_count = 16;
+  int max_seconds = 20;
+  if (!cl->GetSwitchValueASCII(kBenchmarkRunCount).empty()) {
+    run_count = std::stoi(cl->GetSwitchValueASCII(kBenchmarkRunCount));
+  }
+  if (!cl->GetSwitchValueASCII(kBenchmarkMaxSeconds).empty()) {
+    max_seconds = std::stoi(cl->GetSwitchValueASCII(kBenchmarkMaxSeconds));
+  }
+  bench.Run(run_count, max_seconds);
 }
 
 }  // namespace
@@ -181,6 +348,8 @@ int main(int argc, char** argv) {
 
       run_loop.Run();
     }
+  } else if (cl->HasSwitch(kBenchmark)) {
+    RunBenchmark(raw_ref(model), cl);
   }
 
   return 0;
