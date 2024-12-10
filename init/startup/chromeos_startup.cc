ArthurSonzogni/chromiumos-platform2

commit 3f052bc4d63d43e7159542b6a564f37fadeb1d73

init: startup: Remove .developer_mode manually when clobbering in dev mode
When transitioning from developper mode to normal mode in a test build,
we need to make sure that all the conditions are met to reach a stable
state:
Normal mode:
- mainfw_type = normal
- /mnt/stateful_partition/.developer_mode absent

Test mode:
- mainfw_type = developer
- /mnt/stateful_partition/.developer_mode present

BUG=b:377822591
TEST=Check the device is not trying to clobber at every boot.

Change-Id: I21965adb594d09f2d83b2121e16f256a064efd69
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6075605
Reviewed-by: Alexis Savery <asavery@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Sarthak Kukreti <sarthakkukreti@google.com>
Auto-Submit: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>

init/startup/chromeos_startup.cc
@@ -527,6 +527,7 @@ void ChromeosStartup::CheckForStatefulWipe() {
         // stateful manually.
         clobber_log_msg = "Leave developer mode on a debug build";
         DevUpdateStatefulPartition("clobber");
+        platform_->DeleteFile(dev_mode_allowed_file_);
       }
     }
   } else if (IsDevToVerifiedModeTransition(1)) {

commit 9b462f005819118c38dcbd3b4c7209132ace8529

init: startup: Fix .developer_mode handling
The automatic merge completed but added invalid code.

Fixes: 3f052bc4d63 ("init: startup: Remove .developer_mode manually when clobbering in dev mode")

BUG=b:377822591
TEST=Check chromeos-init compiles.

Change-Id: I3bae15ccf1fc509441420bb16510fab2712356a8
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6082755
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Pin-yen Lin <treapking@chromium.org>
Reviewed-by: Sarthak Kukreti <sarthakkukreti@google.com>
Reviewed-by: Pin-yen Lin <treapking@chromium.org>
Auto-Submit: Gwendal Grignou <gwendal@chromium.org>

init/startup/chromeos_startup.cc
@@ -527,7 +527,7 @@ void ChromeosStartup::CheckForStatefulWipe() {
         // stateful manually.
         clobber_log_msg = "Leave developer mode on a debug build";
         DevUpdateStatefulPartition("clobber");
-        platform_->DeleteFile(dev_mode_allowed_file_);
+        platform_->DeleteFile(dev_mode_allowed_file);
       }
     }
   } else if (IsDevToVerifiedModeTransition(1)) {