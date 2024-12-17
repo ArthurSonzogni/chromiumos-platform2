19252a437e052b3449e2bb7c1afcda0ec4a0bc94
Ted Brandston <tbrandston@google.com>
2024-12-17 11:33:32 -0500

    flexor: Add run-test-disk options to update image
    To save having to make a new flexor_disk.img each time, when mostly
    what I want to test is whether changes to the installer break flexor,
    allow just swapping files into an existing flexor_disk.img
    
    BUG=b:383794194
    TEST=cargo xtask run-test-disk \
              --flexor-vmlinuz flexor_vmlinuz flexor_disk.img
         cargo xtask run-test-disk \
              --install-image chromiumos_test_image.bin \
              flexor_disk.img
         cargo xtask run-test-disk \
              --flexor-vmlinuz flexor_vmlinuz \
              --install-image chromiumos_test_image.tar.xz \
              flexor_disk.img
         In each case, confirm by manual inspection that your altered flexor_vmlinuz is used and/or your new flexor_image is installed
    TEST=emerge-reven flexor
    
    Change-Id: I331c1b2583003f848d44233211b19e72fc93cd98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6147729
    Tested-by: Ted Brandston <tbrandston@google.com>
    Reviewed-by: Jonathan Denose <jdenose@google.com>
    Commit-Queue: Ted Brandston <tbrandston@google.com>

--- flexor/xtask/src/main.rs
+++ flexor/xtask/src/main.rs
@@ -17,7 +17,8 @@ struct Opt {
 #[derive(Subcommand)]
 enum Action {
     /// Takes a flexor image (like what the create_flexor_disk script produces) and runs it in a VM
-    /// to test installation.
+    /// to test installation, optionally updating some of the contained files.
+    /// Ctrl+C to exit.
     RunTestDisk(RunTestDiskArgs),
 }
 
@@ -25,12 +26,26 @@ enum Action {
 struct RunTestDiskArgs {
     /// The disk image to test (e.g. flexor_disk.img).
     flexor_disk: PathBuf,
+
+    /// Optionally update the flexor_vmlinuz before running.
+    /// This contains the install script that will be run.
+    #[arg(long)]
+    flexor_vmlinuz: Option<PathBuf>,
+
+    /// Optionally update the flex_image.tar.xz before running.
+    /// This is the image that will be installed by flexor.
+    // TODO(tbrandston): Optional improvement: detect non-xz input and use tar+flate2 to compress.
+    #[arg(long)]
+    install_image: Option<PathBuf>,
 }
 
 fn main() -> Result<()> {
     let opt = Opt::parse();
 
     match &opt.action {
-        Action::RunTestDisk(args) => test_disk::run(&args.flexor_disk),
+        Action::RunTestDisk(args) => {
+            test_disk::update(&args.flexor_disk, &args.flexor_vmlinuz, &args.install_image)?;
+            test_disk::run(&args.flexor_disk)
+        }
     }
 }
--- flexor/xtask/src/test_disk.rs
+++ flexor/xtask/src/test_disk.rs
@@ -2,9 +2,121 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::{bail, Result};
-use std::path::Path;
+use anyhow::{anyhow, bail, Result};
+use fatfs::{FileSystem, FsOptions, ReadWriteSeek};
+use gpt_disk_types::{BlockSize, GptPartitionType, Lba, LbaRangeInclusive};
+use gptman::{GPTPartitionEntry, GPT};
+use std::fs::{self, File, OpenOptions};
+use std::io::{Cursor, Read, Seek, SeekFrom, Write};
+use std::ops::RangeInclusive;
+use std::path::{Path, PathBuf};
 use std::process::Command;
+
+const FLEX_IMAGE_FILENAME: &str = "flex_image.tar.xz";
+const FLEXOR_VMLINUZ_FILENAME: &str = "flexor_vmlinuz";
+
+const SECTOR_SIZE: BlockSize = BlockSize::BS_512;
+
+// Copied from crdyboot
+struct PartitionDataRange(LbaRangeInclusive);
+
+impl PartitionDataRange {
+    fn new(partition: &GPTPartitionEntry) -> Self {
+        Self(
+            LbaRangeInclusive::new(Lba(partition.starting_lba), Lba(partition.ending_lba)).unwrap(),
+        )
+    }
+
+    fn to_byte_range(&self) -> RangeInclusive<u64> {
+        self.0.to_byte_range(SECTOR_SIZE).unwrap()
+    }
+
+    fn num_bytes(&self) -> usize {
+        self.0.num_bytes(SECTOR_SIZE).unwrap().try_into().unwrap()
+    }
+
+    fn read_bytes_from_file(&self, f: &mut File) -> Result<Vec<u8>> {
+        let mut v = vec![0; self.num_bytes()];
+        f.seek(SeekFrom::Start(*self.to_byte_range().start()))?;
+        f.read_exact(&mut v)?;
+        Ok(v)
+    }
+
+    fn write_bytes_to_file(&self, f: &mut File, data: &[u8]) -> Result<()> {
+        assert!(data.len() <= self.num_bytes());
+        f.seek(SeekFrom::Start(*self.to_byte_range().start()))?;
+        Ok(f.write_all(data)?)
+    }
+}
+
+// Find the data partition on disk (assume there's only one) and return its location & size.
+fn get_data_partition_range(disk_file: &mut File) -> Result<PartitionDataRange> {
+    let gpt = GPT::read_from(disk_file, SECTOR_SIZE.to_u64())?;
+
+    let data_guid = GptPartitionType::BASIC_DATA.0.to_bytes();
+    let data_partition = gpt
+        .iter()
+        .find(|(_, part)| part.partition_type_guid == data_guid)
+        .ok_or(anyhow!("Couldn't find a data partition."))?
+        .1;
+
+    Ok(PartitionDataRange::new(data_partition))
+}
+
+fn write_to_fatfs<T: ReadWriteSeek>(
+    root_dir: &fatfs::Dir<T>,
+    path: &str,
+    input: &Path,
+) -> std::io::Result<()> {
+    let data = fs::read(input)?;
+
+    // Remove if it already exists.
+    let _ = root_dir.remove(path);
+    // Write out the new version.
+    let mut output_file = root_dir.create_file(path)?;
+    output_file.write_all(&data)
+}
+
+/// Updates a flexor test disk image, inserting a new flexor_vmlinuz and/or install_image
+/// if either is passed.
+pub fn update(
+    flexor_disk: &Path,
+    flexor_vmlinuz: &Option<PathBuf>,
+    install_image: &Option<PathBuf>,
+) -> Result<()> {
+    if flexor_vmlinuz.is_none() && install_image.is_none() {
+        // Nothing to do.
+        return Ok(());
+    }
+
+    println!("Updating disk...");
+
+    let mut disk_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .truncate(false)
+        .open(flexor_disk)?;
+
+    let data_partition_range = get_data_partition_range(&mut disk_file)?;
+    let mut data_partition_data = data_partition_range.read_bytes_from_file(&mut disk_file)?;
+
+    {
+        // Load the data as a FAT filesystem, and grab the root dir.
+        let cursor = Cursor::new(&mut data_partition_data);
+        let data_fs = FileSystem::new(cursor, FsOptions::new())?;
+        let root_dir = data_fs.root_dir();
+
+        if let Some(vmlinuz_path) = flexor_vmlinuz {
+            write_to_fatfs(&root_dir, FLEXOR_VMLINUZ_FILENAME, vmlinuz_path)?;
+        }
+
+        if let Some(image_path) = install_image {
+            write_to_fatfs(&root_dir, FLEX_IMAGE_FILENAME, image_path)?;
+        }
+    }
+
+    data_partition_range.write_bytes_to_file(&mut disk_file, &data_partition_data)
+}
 
 /// Runs a flexor test disk image.
 pub fn run(flexor_disk: &Path) -> Result<()> {
