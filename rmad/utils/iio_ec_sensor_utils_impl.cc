a31173ebc88fe51295f78a3e73581b30712884dc
Byron Lee <byronlee@chromium.org>
2025-01-14 07:02:03 +0000

    rmad: Add missing header
    Add base/strings/string_util.h header to fix implicit include.
    
    BUG=b:389810207
    TEST=emerge-endeavour chromeos-base/rmad
    
    Change-Id: I9bda4a7d5225ead7bd34f8c1f5bbb6e91cbe8cd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6172897
    Tested-by: Byron Lee <byronlee@chromium.org>
    Reviewed-by: Jeff Lin <jeffulin@google.com>
    Commit-Queue: Byron Lee <byronlee@chromium.org>

--- rmad/utils/iio_ec_sensor_utils_impl.cc
+++ rmad/utils/iio_ec_sensor_utils_impl.cc
@@ -13,6 +13,7 @@
 #include <base/logging.h>
 #include <base/process/launch.h>
 #include <base/strings/string_number_conversions.h>
+#include <base/strings/string_util.h>
 #include <libmems/iio_context_impl.h>
 
 namespace {
