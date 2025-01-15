1c7976f28124fa9297fb5e83f2a80c36f298ad7c
Zihan Chen <zihanchen@google.com>
2025-01-14 19:59:15 -0800

    vm_tools/baguette_image: Fix docker build script
    Add missing mkdir line.
    
    TEST=fixes recipe error
    
    BUG=b:345312465
    
    Change-Id: Ib23c420b1e0dae1b6cf22e0dd279eb18ada718ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6176102
    Auto-Submit: Zihan Chen <zihanchen@google.com>
    Tested-by: Zihan Chen <zihanchen@google.com>
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

--- vm_tools/baguette_image/src/docker-build.sh
+++ vm_tools/baguette_image/src/docker-build.sh
@@ -4,5 +4,6 @@
 # found in the LICENSE file.
 
 docker buildx create --use --name insecure-builder --buildkitd-flags '--allow-insecure-entitlement security.insecure'
+mkdir -p docker_export
 docker buildx build -f src/build.Dockerfile --platform linux/amd64,linux/arm64 --allow security.insecure --output type=local,dest=docker_export .
 docker buildx build -f src/convert.Dockerfile --output type=local,dest=docker_export .
