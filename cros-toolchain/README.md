09cd69315d3d8f82d4a4c1dc2b5a1ac3c75c7974
George Burgess IV <gbiv@google.com>
2025-01-16 10:50:05 -0700

    cros-toolchain: add FORTIFY tests
    These are copied bit-for-bit from their prior location
    (autotest/files/client/site_tests/platform_ToolchainTests/src), except:
    - the old test driver has been removed in favor of invoking the test
      binary directly (by the changes in crrev.com/c/6180187),
    - the Makefile was replaced with a more-standard-for-platform2 BUILD.gn,
      and
    - the diagnostic-checking functionality from the Makefile was replaced
      with the new 'verify-fortify-diags.sh' script.
    
    I didn't edit the C++ code itself, since as much as I might like to
    clean it up a bit, this CL is big enough already.
    
    BUG=b:388925046
    TEST=FEATURES=test with the new ebuild
    
    Change-Id: I5b28f5bdd7db88f7a25a52d6e1db568f866fac6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6180339
    Reviewed-by: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Tested-by: George Burgess <gbiv@chromium.org>

--- cros-toolchain/README.md
+++ cros-toolchain/README.md
@@ -6,3 +6,8 @@ made available to the ChromeOS CQ.
 Some of our code is also located in the `src/third_party/toolchain-utils`
 directory. That repo is designed to be shared with other projects (specifically,
 Android), and the CQ there has no overlap with ChromeOS' "regular" CQ.
+
+Subdirectories:
+
+- `fortify-tests/` -- used by the chromeos-base/toolchain-tests ebuild to verify
+  that FORTIFY crashes in expected cases.
---
+++ cros-toolchain/fortify-tests/verify-fortify-diags.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -eux
+# Copyright 2025 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Verifies FORTIFY diagnostics WAI for these tests. Making `gn` do this is
+# relatively hacky, so a simple shell script is used instead.
+
+my_dir="$(dirname "$(readlink -m "$0")")"
+cd "${my_dir}"
+
+# SC complains about CXX/CPPFLAGS/CXXFLAGS not being quoted below. That's
+# intended.
+# shellcheck disable=SC2206
+verify_cmd=(
+  # CXX should be exported by the ebuild; add'l flags are optional.
+  ${CXX:?}
+  ${CPPFLAGS:-}
+  ${CXXFLAGS:-}
+  # This tests GNU extensions
+  -D_GNU_SOURCE
+  # FORTIFY checks require optimizations to be enabled above -O0.
+  -O2
+  -Wformat-nonliteral
+  # clang-fortify-tests has checks for getwd, which was removed in c++14.
+  -std=c++11
+  # All users will override -D_FORTIFY_SOURCE
+  -U_FORTIFY_SOURCE
+  # `-Xclang -verify` has Clang verify diagnostics that it emits internally.
+  -Xclang
+  -verify
+  # -fsyntax-only has clnag skip everything after parsing.
+  -fsyntax-only
+  -DCOMPILATION_TESTS
+  "${my_dir}/clang-fortify-tests.cpp"
+)
+
+"${verify_cmd[@]}" "-D_FORTIFY_SOURCE=1"
+"${verify_cmd[@]}" "-D_FORTIFY_SOURCE=2"
