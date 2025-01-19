cdbd99ad5414cb5323ab026d0b9267701ae65b9b
Craig Hesling <hesling@chromium.org>
2025-01-18 19:23:12 -0800

    fpstudy: Re-re-introduce TestCase
    In alignment with https://crrev.com/c/5664058, TestCase will
    now be the combination of a TestCaseDescriptor and an
    Experiment. We introduce the ability to simply parse a
    TestCase from a directory.
    
    BUG=b:388931275
    TEST=cros format .
    TEST=cros lint .
    TEST=python3 -m unittest discover -v -p '*_test.py'
    
    Change-Id: Id9115b8202654a011170f723139092b1f3483349
    
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6184153
    Commit-Queue: Craig Hesling <hesling@chromium.org>
    Reviewed-by: Firas Sammoura <fsammoura@google.com>
    Tested-by: Craig Hesling <hesling@chromium.org>

---
+++ biod/study/analysis-tool/test_case.py
@@ -0,0 +1,62 @@
+# Copyright 2025 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Represent the results from one particular testing condition."""
+
+from __future__ import annotations
+
+import pathlib
+
+import experiment
+import test_case_descriptor
+
+
+class TestCase:
+    """A TestCase represents the results from one particular testing condition.
+
+    It contains the `name`, `description`, and `Experiment` results from that
+    testing condition.
+    """
+
+    _descriptor: test_case_descriptor.TestCaseDescriptor
+    _exp: experiment.Experiment
+
+    def __init__(
+        self,
+        descriptor: test_case_descriptor.TestCaseDescriptor,
+        exp: experiment.Experiment,
+    ) -> None:
+        self._descriptor = descriptor
+        self._exp = exp
+
+    def name(self) -> str:
+        return self._descriptor.name
+
+    def description(self) -> str:
+        return self._descriptor.description
+
+    def experiment(self) -> experiment.Experiment:
+        return self._exp
+
+
+def test_case_from_dir(dir_path: pathlib.Path) -> TestCase:
+    """Parse a TestCase from a directory with all the prerequisite files.
+
+    This function expects there to be a test_case.toml in addition to the files
+    needed to parse an `Experiment`.
+    """
+    if not dir_path.is_dir():
+        raise NotADirectoryError(f"{dir_path} is not a directory.")
+
+    test_case_descriptor_toml = dir_path / "test_case.toml"
+    if not test_case_descriptor_toml.is_file():
+        raise FileNotFoundError(
+            f"test case descriptor {test_case_descriptor_toml} does not exist."
+        )
+
+    tcd = test_case_descriptor.test_case_descriptor_from_toml(
+        test_case_descriptor_toml
+    )
+    exp = experiment.experiment_from_decisions_dir(dir_path)
+    return TestCase(tcd, exp)
---
+++ biod/study/analysis-tool/test_case_test.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# Copyright 2025 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Test the test_case module."""
+
+import pathlib
+import tempfile
+import unittest
+
+import simulate_fpstudy
+import test_case
+
+
+class Test_TestCase(unittest.TestCase):
+    """Test TestCase."""
+
+    def setUp(self) -> None:
+        self.temp_dir_context = tempfile.TemporaryDirectory()
+        self.addCleanup(self.temp_dir_context.cleanup)
+        self.temp_dir = pathlib.Path(self.temp_dir_context.name)
+        self.simulation = simulate_fpstudy.SimulateEvalResults(
+            num_users=10,
+            num_fingers=2,
+            num_verify_samples=3,
+        )
+        self.simulation.GenerateFARResults()
+        self.simulation.GenerateFRRResults()
+
+    def test_load(self):
+        """Manually save a full test case to a dir and try to load it."""
+
+        self.simulation.Experiment().far_decisions_to_csv(
+            self.temp_dir / "FAR_decisions.csv"
+        )
+        self.simulation.Experiment().frr_decisions_to_csv(
+            self.temp_dir / "FRR_decisions.csv"
+        )
+        self.simulation.TestCaseDescriptor().to_toml(
+            self.temp_dir / "test_case.toml"
+        )
+
+        tc = test_case.test_case_from_dir(self.temp_dir)
+        self.assertEqual(tc.name(), self.simulation.TestCaseDescriptor().name)
+        self.assertEqual(
+            tc.description(), self.simulation.TestCaseDescriptor().description
+        )
+
+        self.assertTrue(
+            tc.experiment()
+            .far_decisions()
+            .equals(self.simulation.Experiment().far_decisions())
+        )
+        self.assertTrue(
+            tc.experiment()
+            .frr_decisions()
+            .equals(self.simulation.Experiment().frr_decisions())
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
