ArthurSonzogni/chromiumos-platform2

commit 2988096ec4984ee41d58896ccf432350ed969466

thinpool_migrator: Add stateful migrator class
The stateful migrator class corrals all the operations
required to migrate the stateful partition to use
an lvm2 thinpool. In order to do so, we need the
first 1M of the partition for lvm2 metadata. So, we

- resize the filesystem down the require amount, copy
over the partition header to the end of filesystem.

- generate thinpool metadata and write it out to the
offset in the newly created metadata space.

- generate lvm2 metadata and write it to the beginning
of the stateful partition.

- ... profit

BUG=b:232006113
TEST=unittests

Change-Id: I6f606599bf4190539b6917ce08a042381cd68fc3
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4307565
Commit-Queue: Sarthak Kukreti <sarthakkukreti@google.com>
Tested-by: Sarthak Kukreti <sarthakkukreti@google.com>
Reviewed-by: Daniil Lunev <dlunev@chromium.org>

thinpool_migrator/thinpool_migrator.cc
@@ -1,0 +1,346 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "thinpool_migrator/thinpool_migrator.h"
+
+#include <string>
+#include <utility>
+
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
+#include <base/functional/callback_helpers.h>
+#include <base/logging.h>
+#include <base/strings/string_number_conversions.h>
+#include <brillo/blkdev_utils/device_mapper.h>
+#include <brillo/process/process.h>
+#include <brillo/syslog_logging.h>
+
+#include <thinpool_migrator/stateful_metadata.h>
+
+namespace thinpool_migrator {
+namespace {
+constexpr const char kThinpoolSuperblockMetadataPath[] = "/tmp/thinpool.xml";
+constexpr const char kVgcfgRestoreFile[] = "/tmp/vgcfgrestore.txt";
+
+constexpr const uint64_t kPartitionHeaderSize = 1ULL * 1024 * 1024;
+constexpr const uint64_t kSectorSize = 512;
+
+// Device mapper target name.
+constexpr const char kMetadataDeviceMapperTarget[] = "thinpool-metadata-dev";
+constexpr const char kDeviceMapperPrefix[] = "/dev/mapper";
+
+brillo::DevmapperTable GetMetadataDeviceTable(uint64_t offset,
+                                              uint64_t size,
+                                              const base::FilePath& device) {
+  return brillo::DevmapperTable(
+      0, size / kSectorSize, "linear",
+      brillo::SecureBlob(base::StringPrintf(
+          "%s %" PRIu64, device.value().c_str(), offset / kSectorSize)));
+}
+
+}  // namespace
+
+ThinpoolMigrator::ThinpoolMigrator(
+    const base::FilePath& device_path,
+    uint64_t size,
+    std::unique_ptr<brillo::DeviceMapper> device_mapper)
+    : current_state_(MigrationState::kNotStarted),
+      block_device_(device_path),
+      stateful_metadata_(std::make_unique<StatefulMetadata>(device_path, size)),
+      device_mapper_(std::move(device_mapper)),
+      partition_size_(size),
+
+      resized_filesystem_size_(stateful_metadata_->GetResizedFilesystemSize()),
+      relocated_header_offset_(resized_filesystem_size_),
+      thinpool_metadata_offset_(
+          stateful_metadata_->GetThinpoolMetadataOffset()),
+      thinpool_metadata_size_(stateful_metadata_->GetThinpoolMetadataSize()) {}
+
+bool ThinpoolMigrator::Migrate(bool dry_run) {
+  // For a dry run, dump the generated metadata.
+  if (dry_run) {
+    LOG(INFO) << "Volume group configuration:";
+    stateful_metadata_->DumpVolumeGroupConfiguration();
+    LOG(INFO) << "Thinpool metadata:";
+    stateful_metadata_->DumpThinpoolMetadataMappings();
+  }
+
+  // Migration cleanup will attempt to reverse the migration if any one of the
+  // below steps fails.
+  base::ScopedClosureRunner migration_cleanup;
+  if (!dry_run) {
+    migration_cleanup.ReplaceClosure(
+        base::BindOnce(base::IgnoreResult(&ThinpoolMigrator::RevertMigration),
+                       base::Unretained(this)));
+  }
+
+  switch (current_state_) {
+    case MigrationState::kNotStarted:
+      // Attempt to shrink the filesystem.
+      LOG(INFO) << "Shrinking filesystem to " << resized_filesystem_size_;
+      if (!dry_run && !ShrinkStatefulFilesystem()) {
+        LOG(ERROR) << "Failed to shrink filesystem";
+        return false;
+      }
+      SetState(MigrationState::kFilesystemResized);
+      [[fallthrough]];
+
+    case MigrationState::kFilesystemResized:
+      // Now that the filesystem has space, copy over the filesystem superblock
+      // to the end of the filesystem.
+      LOG(INFO) << "Duplicating filesystem header at "
+                << relocated_header_offset_;
+      if (!dry_run && !DuplicatePartitionHeader()) {
+        LOG(ERROR) << "Failed to copy filesystem header";
+        return false;
+      }
+      SetState(MigrationState::kPartitionHeaderCopied);
+      [[fallthrough]];
+
+    case MigrationState::kPartitionHeaderCopied:
+      // Now attempt to write the thinpool metadata partition in the remaining
+      // space.
+      LOG(INFO) << "Attempting to persist thinpool metadata at "
+                << thinpool_metadata_offset_;
+      if (!dry_run && !PersistThinpoolMetadata()) {
+        LOG(ERROR) << "Failed to persist thinpool metadata";
+        return false;
+      }
+      SetState(MigrationState::kThinpoolMetadataPersisted);
+      [[fallthrough]];
+
+    case MigrationState::kThinpoolMetadataPersisted:
+      // The end game: generate and persist LVM metadata at the beginning of the
+      // partition.
+      LOG(INFO) << "Persisting LVM2 metadata at beginning of partition";
+      if (!dry_run && !PersistLvmMetadata()) {
+        LOG(ERROR) << "Failed to persist LVM metadata";
+        return false;
+      }
+
+      SetState(MigrationState::kCompleted);
+      [[fallthrough]];
+
+    case MigrationState::kCompleted:
+      migration_cleanup.ReplaceClosure(base::DoNothing());
+      LOG(INFO) << "Migration complete";
+      return true;
+  }
+}
+
+bool ThinpoolMigrator::ShrinkStatefulFilesystem() {
+  if (!ResizeStatefulFilesystem(resized_filesystem_size_)) {
+    LOG(ERROR) << "Failed to resize filesystem";
+    return false;
+  }
+  return true;
+}
+
+bool ThinpoolMigrator::ExpandStatefulFilesystem() {
+  if (!ResizeStatefulFilesystem(0)) {
+    LOG(ERROR) << "Failed to resize filesystem";
+    return false;
+  }
+  return true;
+}
+
+bool ThinpoolMigrator::DuplicatePartitionHeader() {
+  if (!DuplicateHeader(0, relocated_header_offset_, kPartitionHeaderSize)) {
+    LOG(ERROR) << "Failed to duplicate superblock at the end of device";
+    return false;
+  }
+
+  return true;
+}
+
+bool ThinpoolMigrator::RestorePartitionHeader() {
+  if (!DuplicateHeader(relocated_header_offset_, 0, kPartitionHeaderSize)) {
+    LOG(ERROR) << "Failed to duplicate superblock at the beginning of device";
+    return false;
+  }
+
+  return true;
+}
+
+bool ThinpoolMigrator::ConvertThinpoolMetadataToBinary(
+    const base::FilePath& path) {
+  brillo::ProcessImpl thin_restore;
+  thin_restore.AddArg("/sbin/thin_restore");
+  thin_restore.AddArg("-i");
+  thin_restore.AddArg(kThinpoolSuperblockMetadataPath);
+  thin_restore.AddArg("-o");
+  thin_restore.AddArg(path.value().c_str());
+
+  thin_restore.SetCloseUnusedFileDescriptors(true);
+  if (thin_restore.Run() != 0) {
+    LOG(ERROR) << "Failed to convert thinpool metadata";
+    return false;
+  }
+
+  return true;
+}
+
+bool ThinpoolMigrator::PersistThinpoolMetadata() {
+  if (!stateful_metadata_->DumpThinpoolMetadataMappings(
+          base::FilePath(kThinpoolSuperblockMetadataPath))) {
+    LOG(ERROR) << "Failed to generate metadata for device";
+    return false;
+  }
+
+  // Set up a dm-linear device on top of the thinpool's metadata section.
+  if (!device_mapper_->Setup(
+          kMetadataDeviceMapperTarget,
+          GetMetadataDeviceTable(thinpool_metadata_offset_,
+                                 thinpool_metadata_size_, block_device_))) {
+    LOG(ERROR) << "Failed to set up metadata dm-linear device";
+    return false;
+  }
+
+  base::FilePath metadata_device =
+      base::FilePath(kDeviceMapperPrefix)
+          .AppendASCII(kMetadataDeviceMapperTarget);
+
+  // Use thin_restore to convert from the generated XML format.
+  bool ret = true;
+  if (!ConvertThinpoolMetadataToBinary(metadata_device)) {
+    LOG(ERROR) << "Failed to persist thinpool metadata";
+    ret = false;
+  }
+
+  sync();
+  base::IgnoreResult(device_mapper_->Remove(kMetadataDeviceMapperTarget, true));
+  return ret;
+}
+
+bool ThinpoolMigrator::InitializePhysicalVolume(const std::string& uuid) {
+  brillo::ProcessImpl pvcreate;
+  pvcreate.AddArg("/sbin/pvcreate");
+  pvcreate.AddArg("--force");
+  pvcreate.AddArg("--uuid");
+  pvcreate.AddArg(uuid);
+  pvcreate.AddArg("--restorefile");
+  pvcreate.AddArg(kVgcfgRestoreFile);
+  pvcreate.AddArg(block_device_.value());
+
+  pvcreate.SetCloseUnusedFileDescriptors(true);
+  if (pvcreate.Run() != 0) {
+    LOG(ERROR) << "Failed to instantiate physical volume";
+    return false;
+  }
+
+  return true;
+}
+bool ThinpoolMigrator::RestoreVolumeGroupConfiguration(
+    const std::string& vgname) {
+  brillo::ProcessImpl vgcfgrestore;
+  vgcfgrestore.AddArg("/sbin/vgcfgrestore");
+  vgcfgrestore.AddArg(vgname);
+  vgcfgrestore.AddArg("--force");
+  vgcfgrestore.AddArg("-f");
+  vgcfgrestore.AddArg(kVgcfgRestoreFile);
+
+  vgcfgrestore.SetCloseUnusedFileDescriptors(true);
+  if (vgcfgrestore.Run() != 0) {
+    LOG(ERROR) << "Failed to restore volume group";
+    return false;
+  }
+
+  return true;
+}
+
+bool ThinpoolMigrator::PersistLvmMetadata() {
+  if (!stateful_metadata_->DumpVolumeGroupConfiguration(
+          base::FilePath(kVgcfgRestoreFile))) {
+    LOG(ERROR) << "Failed to dump volume group configuration";
+    return false;
+  }
+
+  if (!InitializePhysicalVolume(stateful_metadata_->GetPvUuid())) {
+    LOG(ERROR) << "Failed to initialize physical volume "
+               << stateful_metadata_->GetPvUuid();
+    return false;
+  }
+
+  if (!RestoreVolumeGroupConfiguration(
+          stateful_metadata_->GetVolumeGroupName())) {
+    LOG(ERROR) << "Failed to restore volume group";
+    return false;
+  }
+
+  return true;
+}
+
+// 'Tis a sad day, but it must be done.
+bool ThinpoolMigrator::RevertMigration() {
+  switch (current_state_) {
+    case MigrationState::kCompleted:
+      LOG(ERROR) << "Reverting a completed migration is not allowed as it will "
+                    "corrupt the filesystem";
+      return false;
+    case MigrationState::kNotStarted:
+      LOG(ERROR) << "No revert needed, migration not started yet";
+      return false;
+    // It is possible that we failed to completely write out the LVM2 header.
+    case MigrationState::kThinpoolMetadataPersisted:
+      if (!RestorePartitionHeader()) {
+        LOG(ERROR) << "Failed to restore partition header to a pristine state";
+        return false;
+      }
+      SetState(MigrationState::kFilesystemResized);
+      [[fallthrough]];
+    case MigrationState::kPartitionHeaderCopied:
+    case MigrationState::kFilesystemResized:
+      if (!ExpandStatefulFilesystem()) {
+        LOG(ERROR) << "Failed to expand the stateful partition back to its"
+                      "earlier state";
+        return false;
+      }
+      // Reset the migration state so that we don't attempt to
+      // cleanup/restart migration from a certain point on next boot.
+      SetState(MigrationState::kNotStarted);
+      return true;
+    default:
+      LOG(ERROR) << "Invalid state";
+      return false;
+  }
+}
+
+bool ThinpoolMigrator::ResizeStatefulFilesystem(uint64_t size) {
+  brillo::ProcessImpl resize2fs;
+  resize2fs.AddArg("/sbin/resize2fs");
+  resize2fs.AddArg(block_device_.value());
+  if (size != 0)
+    resize2fs.AddArg(base::NumberToString(size / 4096));
+
+  resize2fs.SetCloseUnusedFileDescriptors(true);
+  if (resize2fs.Run() != 0) {
+    LOG(ERROR) << "Failed to resize the filesystem to " << size;
+    return false;
+  }
+
+  return true;
+}
+
+bool ThinpoolMigrator::DuplicateHeader(uint64_t from,
+                                       uint64_t to,
+                                       uint64_t size) {
+  brillo::ProcessImpl dd;
+  dd.AddArg("/bin/dd");
+  dd.AddArg(base::StringPrintf("if=%s", block_device_.value().c_str()));
+  dd.AddArg(base::StringPrintf("skip=%lu", from / kSectorSize));
+  dd.AddArg(base::StringPrintf("of=%s", block_device_.value().c_str()));
+  dd.AddArg(base::StringPrintf("seek=%lu", to / kSectorSize));
+  dd.AddArg(base::StringPrintf("count=%lu", size / kSectorSize));
+
+  dd.SetCloseUnusedFileDescriptors(true);
+  if (dd.Run() != 0) {
+    LOG(ERROR) << "Failed to duplicate contents from " << from << " to " << to;
+    return false;
+  }
+
+  sync();
+  return true;
+}
+
+}  // namespace thinpool_migrator

thinpool_migrator/BUILD.gn
@@ -35,6 +35,7 @@ shared_library("libthinpool_migrator") {
   sources = [
     "lvm_metadata.cc",
     "stateful_metadata.cc",
+    "thinpool_migrator.cc",
   ]
   configs += [ ":target_defaults" ]
   install_path = "lib"
@@ -44,6 +45,7 @@ install_config("install_headers") {
   sources = [
     "lvm_metadata.h",
     "stateful_metadata.h",
+    "thinpool_migrator.h",
   ]
   install_path = "/usr/include/thinpool_migrator"
 }
@@ -53,6 +55,7 @@ if (use.test) {
     sources = [
       "lvm_metadata_test.cc",
       "stateful_metadata_test.cc",
+      "thinpool_migrator_test.cc",
     ]
     configs += [
       "//common-mk:test",
@@ -62,6 +65,10 @@ if (use.test) {
     deps = [
       ":libthinpool_migrator",
       "//common-mk/testrunner",
+    ]
+    pkg_deps = [
+      "libbrillo-test",
+      "libchrome-test",
     ]
   }
 }

thinpool_migrator/stateful_metadata.cc
@@ -71,6 +71,27 @@ PhysicalVolumeMetadata StatefulMetadata::GeneratePhysicalVolumeMetadata()
           .pe_count = total_extent_count_};
 }
 
+// The first 1M of the partition contains LVM2 metadata, followed by the
+// physical extents. Given the size of the unencrypted stateful logical
+// volume in extents, the filesystem on the stateful partition needs to be
+// resized down to be able to write the header to end of the logical
+// volume.
+uint64_t StatefulMetadata::GetResizedFilesystemSize() const {
+  return (GetUnencryptedStatefulExtentCount() - 1) * kPhysicalExtentSize +
+         kStartingPhysicalExtentAddress;
+}
+
+// The thinpool's metadata partition starts at the end of thinpool's data
+// partition.
+uint64_t StatefulMetadata::GetThinpoolMetadataOffset() const {
+  return GetThinpoolExtentCount() * kPhysicalExtentSize +
+         kStartingPhysicalExtentAddress;
+}
+
+uint64_t StatefulMetadata::GetThinpoolMetadataSize() const {
+  return GetThinpoolMetadataExtentCount() * kPhysicalExtentSize;
+}
+
 std::vector<LogicalVolumeMetadata>
 StatefulMetadata::GenerateLogicalVolumeMetadata() const {
   std::vector<LogicalVolumeMetadata> lv_metadata;

thinpool_migrator/stateful_metadata.h
@@ -31,6 +31,11 @@ class BRILLO_EXPORT StatefulMetadata {
   uint64_t GetUnencryptedStatefulExtentCount() const {
     return unencrypted_stateful_extent_count_;
   }
+
+  uint64_t GetResizedFilesystemSize() const;
+  uint64_t GetThinpoolMetadataOffset() const;
+  uint64_t GetThinpoolMetadataSize() const;
+
   uint64_t GetThinpoolExtentCount() const { return thinpool_extent_count_; }
 
   uint64_t GetThinpoolMetadataExtentCount() const {

thinpool_migrator/thinpool_migrator.h
@@ -1,0 +1,121 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THINPOOL_MIGRATOR_THINPOOL_MIGRATOR_H_
+#define THINPOOL_MIGRATOR_THINPOOL_MIGRATOR_H_
+
+#include <memory>
+#include <string>
+
+#include <base/files/file_path.h>
+#include <brillo/blkdev_utils/device_mapper.h>
+#include <brillo/brillo_export.h>
+#include <thinpool_migrator/stateful_metadata.h>
+
+namespace thinpool_migrator {
+
+// Thinpool migration state.
+enum class MigrationState {
+  // By default, the migration begins in this state. Even in absence of
+  // a migration marker, this step is re-entrant.
+  kNotStarted = 0,
+  // The filesystem has already been resized to accommodate for thinpool
+  // metadata.
+  kFilesystemResized = 1,
+  // The partition header has been copied to the end of the filesystem.
+  kPartitionHeaderCopied = 2,
+  // The thinpool metadata has been persisted.
+  kThinpoolMetadataPersisted = 3,
+  // Migration completed.
+  kCompleted = 4,
+};
+
+// Thinpool migrator converts an existing partition with a filesystem on
+// it into a thinpool with one thinly provisioned logical volume.
+class BRILLO_EXPORT ThinpoolMigrator {
+ public:
+  ThinpoolMigrator(const base::FilePath& device_path,
+                   uint64_t size,
+                   const std::unique_ptr<brillo::DeviceMapper> device_mapper);
+
+  virtual ~ThinpoolMigrator() = default;
+
+  // Starts migration from |state|. In case of failure, the
+  // migration is reverted.
+  bool Migrate(bool dry_run);
+
+  // Shrinks filesystem to make space for the thinpool metadata.
+  bool ShrinkStatefulFilesystem();
+
+  // Duplicates the partition header at the end of the filesystem.
+  bool DuplicatePartitionHeader();
+
+  // Restore the partition header from the end of the filesystem.
+  bool RestorePartitionHeader();
+
+  // Persist thinpool metadata at the end of the partition.
+  bool PersistThinpoolMetadata();
+
+  // Persist LVM metadata at the beginning of the partition.
+  bool PersistLvmMetadata();
+
+  // Update migration state.
+  void SetState(MigrationState state) { current_state_ = state; }
+
+  // Revert migration. Until the last stage of the migration, the entire process
+  // is reversible. If writing the superblock fails and the device does not have
+  // a valid superblock, we copy over the existing superblock we copied at the
+  // current end of the filesystem. Additionally, resize the filesystem back to
+  // occupy the entire partition.
+  bool RevertMigration();
+
+  // Expand filesystem to take up the entire stateful partition again.
+  bool ExpandStatefulFilesystem();
+
+  // Get migration state
+  MigrationState GetState() const { return current_state_; }
+
+ protected:
+  // ResizeStatefulFilesyste is used for making space for the thinpool's
+  // metadata or for expanding the stateful filesystem iff the migration
+  // fails for some reason.
+  virtual bool ResizeStatefulFilesystem(uint64_t size);
+
+  // Uses `thin_dump` to convert the thinpool metadata to the binary format.
+  virtual bool ConvertThinpoolMetadataToBinary(const base::FilePath& path);
+
+  // Initializes the physical volume metadata on the block device. This will
+  // only cover the first 1M of the block device.
+  virtual bool InitializePhysicalVolume(const std::string& uuid);
+
+  // Restores the volume group configuration for a given volume group name.
+  virtual bool RestoreVolumeGroupConfiguration(const std::string& vgname);
+
+  // Duplicates the header of the device.
+  virtual bool DuplicateHeader(uint64_t from, uint64_t to, uint64_t size);
+
+ private:
+  // Generates the payload to be written to at the beginning of the stateful
+  // partition.
+  std::string GeneratePhysicalVolumePayload();
+
+  MigrationState current_state_;
+  const base::FilePath block_device_;
+
+  std::unique_ptr<StatefulMetadata> stateful_metadata_;
+
+  // The device-mapper layer is used to set up a temporary dm-linear target
+  // on top of the metadata location, for ease of use with `thin_dump`.
+  std::unique_ptr<brillo::DeviceMapper> device_mapper_;
+
+  const uint64_t partition_size_;
+  const uint64_t resized_filesystem_size_;
+  const uint64_t relocated_header_offset_;
+  const uint64_t thinpool_metadata_offset_;
+  const uint64_t thinpool_metadata_size_;
+};
+
+}  // namespace thinpool_migrator
+
+#endif  // THINPOOL_MIGRATOR_THINPOOL_MIGRATOR_H_

thinpool_migrator/thinpool_migrator_test.cc
@@ -1,0 +1,134 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "thinpool_migrator/thinpool_migrator.h"
+
+#include <string>
+
+#include <base/logging.h>
+#include <brillo/blkdev_utils/device_mapper_fake.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using ::testing::_;
+using ::testing::Return;
+
+namespace thinpool_migrator {
+
+class MockThinpoolMigrator : public ThinpoolMigrator {
+ public:
+  MockThinpoolMigrator()
+      : ThinpoolMigrator(
+            base::FilePath("/dev/nvme0n1p1"),
+            512UL * 1024 * 1024 * 1024,
+            std::make_unique<brillo::DeviceMapper>(
+                base::BindRepeating(&brillo::fake::CreateDevmapperTask))) {}
+
+  MOCK_METHOD(bool, ResizeStatefulFilesystem, (uint64_t), (override));
+
+  MOCK_METHOD(bool,
+              ConvertThinpoolMetadataToBinary,
+              (const base::FilePath&),
+              (override));
+
+  MOCK_METHOD(bool, InitializePhysicalVolume, (const std::string&), (override));
+
+  MOCK_METHOD(bool,
+              RestoreVolumeGroupConfiguration,
+              (const std::string&),
+              (override));
+
+  MOCK_METHOD(bool,
+              DuplicateHeader,
+              (uint64_t, uint64_t, uint64_t),
+              (override));
+};
+
+// Go through the possibilities of migration and ensure that the state machine
+// is consistent.
+TEST(ThinpoolMigratorTest, BasicSanity) {
+  MockThinpoolMigrator m;
+
+  EXPECT_EQ(m.GetState(), MigrationState::kNotStarted);
+
+  EXPECT_CALL(m, ResizeStatefulFilesystem(_)).WillOnce(Return(true));
+  EXPECT_CALL(m, DuplicateHeader(0, _, _)).WillOnce(Return(true));
+  EXPECT_CALL(m, ConvertThinpoolMetadataToBinary(_)).WillOnce(Return(true));
+  EXPECT_CALL(m, InitializePhysicalVolume(_)).WillOnce(Return(true));
+  EXPECT_CALL(m, RestoreVolumeGroupConfiguration(_)).WillOnce(Return(true));
+
+  EXPECT_TRUE(m.Migrate(false));
+  EXPECT_EQ(m.GetState(), MigrationState::kCompleted);
+}
+
+TEST(ThinpoolMigratorTest, FailedResize) {
+  MockThinpoolMigrator m;
+
+  EXPECT_CALL(m, ResizeStatefulFilesystem(_)).WillOnce(Return(false));
+
+  EXPECT_FALSE(m.Migrate(false));
+  EXPECT_EQ(m.GetState(), MigrationState::kNotStarted);
+}
+
+TEST(ThinpoolMigratorTest, FailedHeaderCopy) {
+  MockThinpoolMigrator m;
+
+  EXPECT_CALL(m, ResizeStatefulFilesystem(_)).WillOnce(Return(true));
+  EXPECT_CALL(m, DuplicateHeader(0, _, _)).WillOnce(Return(false));
+
+  // Revert path
+  EXPECT_CALL(m, ResizeStatefulFilesystem(0)).WillOnce(Return(false));
+
+  EXPECT_FALSE(m.Migrate(false));
+  EXPECT_EQ(m.GetState(), MigrationState::kFilesystemResized);
+}
+
+TEST(ThinpoolMigratorTest, FailedThinpoolMetadataPersist) {
+  MockThinpoolMigrator m;
+
+  EXPECT_CALL(m, ResizeStatefulFilesystem(_)).WillOnce(Return(true));
+  EXPECT_CALL(m, DuplicateHeader(0, _, _)).WillOnce(Return(true));
+  EXPECT_CALL(m, ConvertThinpoolMetadataToBinary(_)).WillOnce(Return(false));
+
+  // Revert path
+  EXPECT_CALL(m, ResizeStatefulFilesystem(0)).WillOnce(Return(false));
+
+  EXPECT_FALSE(m.Migrate(false));
+  EXPECT_EQ(m.GetState(), MigrationState::kPartitionHeaderCopied);
+}
+
+TEST(ThinpoolMigratorTest, FailedPhysicalVolumeInitialization) {
+  MockThinpoolMigrator m;
+
+  EXPECT_CALL(m, ResizeStatefulFilesystem(_)).WillOnce(Return(true));
+  EXPECT_CALL(m, DuplicateHeader(0, _, _)).WillOnce(Return(true));
+  EXPECT_CALL(m, ConvertThinpoolMetadataToBinary(_)).WillOnce(Return(true));
+  EXPECT_CALL(m, InitializePhysicalVolume(_)).WillOnce(Return(false));
+
+  // Revert path
+  EXPECT_CALL(m, ResizeStatefulFilesystem(0)).WillOnce(Return(false));
+  EXPECT_CALL(m, DuplicateHeader(_, 0, _)).WillOnce(Return(true));
+
+  EXPECT_FALSE(m.Migrate(false));
+  EXPECT_EQ(m.GetState(), MigrationState::kThinpoolMetadataPersisted);
+}
+
+TEST(ThinpoolMigratorTest, FailedVolumeGroupInitialization) {
+  MockThinpoolMigrator m;
+
+  EXPECT_CALL(m, ResizeStatefulFilesystem(_)).WillOnce(Return(true));
+  EXPECT_CALL(m, DuplicateHeader(0, _, _)).WillOnce(Return(true));
+  EXPECT_CALL(m, ConvertThinpoolMetadataToBinary(_)).WillOnce(Return(true));
+  EXPECT_CALL(m, InitializePhysicalVolume(_)).WillOnce(Return(true));
+  EXPECT_CALL(m, RestoreVolumeGroupConfiguration(_)).WillOnce(Return(false));
+
+  // Revert path
+  EXPECT_CALL(m, ResizeStatefulFilesystem(0)).WillOnce(Return(false));
+  EXPECT_CALL(m, DuplicateHeader(_, 0, _)).WillOnce(Return(true));
+
+  EXPECT_FALSE(m.Migrate(false));
+  EXPECT_EQ(m.GetState(), MigrationState::kThinpoolMetadataPersisted);
+}
+
+}  // namespace thinpool_migrator

commit da020cd162b188278f9cd89759f9f5e0877a04b9

thinpool_migrator: fix path to thin_restore
Path to thin_restore has changed in thin-provisioning-tools 1.0.13.

fixes: 2988096ec4 ("thinpool_migrator: Add stateful migrator class")

BUG=b:232006113
TEST=Run the migration 3 steps.

Change-Id: I9bdd68f167671063929127261f8081d2cd520308
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6300244
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Sarthak Kukreti <sarthakkukreti@google.com>

thinpool_migrator/thinpool_migrator.cc
@@ -4,8 +4,6 @@
 
 #include "thinpool_migrator/thinpool_migrator.h"
 
-#include <ext2fs/ext2fs.h>
-
 #include <memory>
 #include <string>
 #include <utility>
@@ -19,11 +17,11 @@
 #include <brillo/blkdev_utils/device_mapper.h>
 #include <brillo/process/process.h>
 #include <brillo/syslog_logging.h>
-#include <vpd/vpd.h>
-
+#include <ext2fs/ext2fs.h>
 #include <thinpool_migrator/migration_metrics.h>
 #include <thinpool_migrator/migration_status.pb.h>
 #include <thinpool_migrator/stateful_metadata.h>
+#include <vpd/vpd.h>
 
 namespace thinpool_migrator {
 namespace {
@@ -240,7 +238,7 @@ bool ThinpoolMigrator::RestorePartitionHeader() {
 bool ThinpoolMigrator::ConvertThinpoolMetadataToBinary(
     const base::FilePath& path) {
   brillo::ProcessImpl thin_restore;
-  thin_restore.AddArg("/sbin/thin_restore");
+  thin_restore.AddArg("/usr/sbin/thin_restore");
   thin_restore.AddArg("-i");
   thin_restore.AddArg(kThinpoolSuperblockMetadataPath);
   thin_restore.AddArg("-o");