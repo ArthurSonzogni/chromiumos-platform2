ArthurSonzogni/chromiumos-platform2

commit ce7ebd2d86ad79810f6a9ff4886dd019ffd79c88

chromeos-config: proto_converter: Add property to return the maximal allowed calibration data
For some devices, we need to ignore calibration data.

BUG=b:395212838, b:370095794
TEST=CQ

Change-Id: I53a18bceba26ac9520c85ffb17553949f17804a9
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6324490
Reviewed-by: Harvey Yang <chenghaoyang@chromium.org>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Jesse McGuire <jessemcguire@google.com>
Tested-by: Gwendal Grignou <gwendal@chromium.org>

chromeos-config/cros_config_host/cros_config_proto_converter.py
@@ -2165,13 +2165,19 @@ def _build_hardware_properties(hw_features):
     if hw_features.HasField("fan"):
         result["fan-count"] = hw_features.fan.fan_count.value
 
-    if (
-        hw_features.HasField("embedded_controller")
-        and hw_features.embedded_controller.max_sensor_odr_mhz.value > 0
-    ):
-        result[
-            "ec-max-sensor-odr"
-        ] = hw_features.embedded_controller.max_sensor_odr_mhz.value
+    if hw_features.HasField("embedded_controller"):
+        if hw_features.embedded_controller.HasField("max_sensor_odr_mhz"):
+            result[
+                "ec-max-sensor-odr"
+            ] = hw_features.embedded_controller.max_sensor_odr_mhz.value
+        if hw_features.embedded_controller.HasField(
+            "max_accelerometer_calibration"
+        ):
+            result[
+                "ec-max-calibration_accel"
+            ] = (
+                hw_features.embedded_controller.max_accelerometer_calibration.value
+            )
 
     return result
 

chromeos-config/cros_config_host/cros_config_schema.yaml
@@ -1518,6 +1518,11 @@ properties:
                   type: integer
                   minimum: 0
                   maximum: 400000
+                ec-max-calibration-accel:
+                  description: The maximal allowed calibration offset for accelerometer (in mg)
+                  type: integer
+                  minimum: 0
+                  maximum: 250
                 display-type: *display-type
                 stylus-category: *stylus-category
                 psu-type: *psu-type

commit 16e472dc5c83fb43de7c49149bfb9148a58f1509

chromeos-config: proto_converter: Fix spelling of "ec-max-calibration-accel"
"ec-max-calibration-accel" was misspelled "ec-max-calibration_accel" in
the python converter code.

BUG=b:395212838, b:370095794
TEST=CQ

Fixes: ce7ebd2d86ad ("chromeos-config: proto_converter: Add property to return the maximal allowed calibration data")
Change-Id: I946e90c5db81f4e2a505eee9cd686aec2be41b69
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6341590
Reviewed-by: Jesse McGuire <jessemcguire@google.com>
Reviewed-by: Harvey Yang <chenghaoyang@chromium.org>
Commit-Queue: Harvey Yang <chenghaoyang@chromium.org>
Auto-Submit: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>

chromeos-config/cros_config_host/cros_config_proto_converter.py
@@ -2174,7 +2174,7 @@ def _build_hardware_properties(hw_features):
             "max_accelerometer_calibration"
         ):
             result[
-                "ec-max-calibration_accel"
+                "ec-max-calibration-accel"
             ] = (
                 hw_features.embedded_controller.max_accelerometer_calibration.value
             )