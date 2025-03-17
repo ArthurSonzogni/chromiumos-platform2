5be1195fd85607dc56f0cd4d8e8562c402a28bbd
Dennis Kempin <denniskempin@google.com>
2025-03-17 14:51:44 -0700

    vm_tools: Turn off clang format for version pin
    This allows for more reasonable automatic processing by
    shell scripts to uprev the constants.
    
    BUG=b:404330289
    TEST=None
    
    Change-Id: Ia919e75a7861a1188a67ee37f32b94f1ee60d7b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6363255
    Tested-by: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: maciek swiech <drmasquatch@google.com>
    Commit-Queue: Dennis Kempin <denniskempin@google.com>

--- vm_tools/concierge/baguette_version.h
+++ vm_tools/concierge/baguette_version.h
@@ -9,11 +9,11 @@
 // Baguette.
 // TODO(crbug.com/393151776): Point to luci recipe and builders that update this
 // URL when new images are available.
-constexpr char kBaguetteVersion[] =
-    "2025-01-29-000057_6310e875487f154a58648db8fb3cc284401f856e";
-constexpr char kBaguetteSHA256X86[] =
-    "e21336031b00057afd4f3414369cbf98d8e12783cb38a98cd12f7b9318bdc443";
-constexpr char kBaguetteSHA256Arm[] =
-    "ab60ff3fc717c575aba8a26cd0b2b113ce29781a2e298d484f6e420a87416aec";
+
+// clang-format off
+constexpr char kBaguetteVersion[] = "2025-01-29-000057_6310e875487f154a58648db8fb3cc284401f856e";  // NOLINT
+constexpr char kBaguetteSHA256X86[] = "e21336031b00057afd4f3414369cbf98d8e12783cb38a98cd12f7b9318bdc443";  // NOLINT
+constexpr char kBaguetteSHA256Arm[] = "ab60ff3fc717c575aba8a26cd0b2b113ce29781a2e298d484f6e420a87416aec";  // NOLINT
+// cpplint:enable
 
 #endif  // VM_TOOLS_CONCIERGE_BAGUETTE_VERSION_H_
