ArthurSonzogni/chromiumos-platform2

commit fe745fc7356f6864ad4a810ca119d702752f5042

init: Add FilePreseeder
FilePreseeder implements a generic class for preseeding files, both
inline and extent files. FilePreseeder will be used in
chromeos_startup and in clobber_state for file preservation.

BUG=b:376103521
TEST=boot + chromeos-install + reboot; check preseeded files.
TEST=unittests

Cq-Depend: chromium:6247853
Change-Id: I38b2c5ab582bc746ec2d7af234a76515fbfd22aa
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6246924
Tested-by: Sarthak Kukreti <sarthakkukreti@google.com>
Commit-Queue: Sarthak Kukreti <sarthakkukreti@google.com>
Reviewed-by: Jae Hoon Kim <kimjae@chromium.org>

init/BUILD.gn
@@ -140,6 +140,7 @@ static_library("libpreservation") {
   sources = [
     "libpreservation/ext2fs.cc",
     "libpreservation/fake_ext2fs.cc",
+    "libpreservation/file_preseeder.cc",
     "libpreservation/filesystem_manager.cc",
   ]
   configs += [ ":target_defaults" ]
@@ -538,7 +539,10 @@ if (use.test) {
   }
 
   executable("libpreservation_test") {
-    sources = [ "libpreservation/filesystem_manager_test.cc" ]
+    sources = [
+      "libpreservation/file_preseeder_test.cc",
+      "libpreservation/filesystem_manager_test.cc",
+    ]
     configs += [
       "//common-mk:test",
       ":target_defaults",

init/libpreservation/file_preseeder.cc
@@ -1,0 +1,272 @@
+// Copyright 2025 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fcntl.h>
+#include <linux/fiemap.h>
+#include <linux/fs.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+
+#include <limits>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
+#include <base/memory/free_deleter.h>
+#include <base/strings/string_number_conversions.h>
+#include <brillo/file_utils.h>
+#include <init/libpreservation/file_preseeder.h>
+#include <init/libpreservation/filesystem_manager.h>
+#include <init/libpreservation/preseeded_files.pb.h>
+
+namespace libpreservation {
+namespace {
+
+// A container for fiemap that handles cleaning up allocated memory.
+// base::FreeDeleter must be used here because the underlying struct fiemap is
+// allocated with malloc().
+typedef std::unique_ptr<struct fiemap, base::FreeDeleter> ScopedFiemap;
+
+// ext4 limit for inline file sizes.
+constexpr size_t kInlineFileSizeLimit = 256;
+
+// Max extent count.
+constexpr int kMaxExtents = 128;
+
+// Block size.
+constexpr int kBlockSize = 4096;
+
+}  // namespace
+
+FilePreseeder::FilePreseeder(
+    const std::set<base::FilePath>& directory_allowlist,
+    const base::FilePath& fs_root,
+    const base::FilePath& mount_root,
+    const base::FilePath& metadata_path)
+    : directory_allowlist_(directory_allowlist),
+      fs_root_(fs_root),
+      mount_root_(mount_root),
+      metadata_path_(metadata_path) {}
+
+FilePreseeder::~FilePreseeder() {}
+
+bool FilePreseeder::SaveFileState(const std::set<base::FilePath>& file_list) {
+  bool ret = true;
+  for (auto preseeded_file : file_list) {
+    base::FilePath file = mount_root_.Append(preseeded_file);
+    if (!base::PathExists(file)) {
+      continue;
+    }
+
+    auto file_size = base::GetFileSize(file);
+    if (!file_size) {
+      LOG(ERROR) << "Failed to get file size for " << file;
+      continue;
+    }
+    std::string data;
+
+    if (*file_size < kInlineFileSizeLimit) {
+      if (!base::ReadFileToString(file, &data)) {
+        PLOG(ERROR) << "Failed to read config from " << file;
+        continue;
+      }
+    }
+
+    PreseededFile* pfile = preseeded_files_.add_file_list();
+    pfile->set_path(preseeded_file.value());
+    pfile->set_size(*file_size);
+
+    if (*file_size < kInlineFileSizeLimit) {
+      pfile->mutable_contents()->set_data(data);
+    } else {
+      if (!GetFileExtents(file, pfile->mutable_contents()->mutable_extents())) {
+        LOG(ERROR) << "Failed to get extents for " << preseeded_file;
+        ret = false;
+        continue;
+      }
+    }
+  }
+
+  return ret && PersistMetadata();
+}
+
+bool FilePreseeder::PersistMetadata() {
+  std::string serialized = preseeded_files_.SerializeAsString();
+  auto base64_encoded = base::Base64Encode(serialized);
+
+  return brillo::WriteStringToFile(metadata_path_, base64_encoded);
+}
+
+bool FilePreseeder::LoadMetadata() {
+  std::string base64_data;
+  if (!base::ReadFileToString(metadata_path_, &base64_data)) {
+    return false;
+  }
+
+  std::string decoded_pb;
+  base::Base64Decode(base64_data, &decoded_pb);
+  if (!preseeded_files_.ParseFromString(decoded_pb)) {
+    return false;
+  }
+  return true;
+}
+
+bool FilePreseeder::CreateDirectoryRecursively(FilesystemManager* fs_manager,
+                                               const base::FilePath& path) {
+  base::FilePath dir = fs_root_;
+  std::vector<std::string> components = path.GetComponents();
+
+  if (fs_manager->FileExists(fs_root_.Append(path))) {
+    return true;
+  }
+
+  // Start with first non-root component.
+  for (auto& comp : components) {
+    dir = dir.Append(comp);
+
+    if (fs_manager->FileExists(dir)) {
+      continue;
+    }
+
+    if (!fs_manager->CreateDirectory(dir)) {
+      LOG(ERROR) << "Failed to restore directory: " << dir;
+      return false;
+    }
+  }
+  return true;
+}
+
+bool FilePreseeder::CheckAllowlist(const base::FilePath& path) {
+  // Check if file is in the directory allowlist.
+  base::FilePath dir;
+  std::vector<std::string> components = path.GetComponents();
+  for (auto& comp : components) {
+    dir = dir.empty() ? base::FilePath(comp) : dir.Append(comp);
+    if (directory_allowlist_.find(dir) != directory_allowlist_.end()) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool FilePreseeder::RestoreExtentFiles(FilesystemManager* fs_manager) {
+  for (auto& file : preseeded_files_.file_list()) {
+    if (!CheckAllowlist(base::FilePath(file.path()))) {
+      LOG(ERROR) << "Skipping file: " << file.path() << "; not in allowlist";
+      continue;
+    }
+
+    // Skip small files.
+    if (file.contents().has_data()) {
+      continue;
+    }
+
+    base::FilePath pfile = fs_root_.Append(file.path());
+
+    if (!fs_manager->FileExists(pfile.DirName()) &&
+        !CreateDirectoryRecursively(fs_manager, base::FilePath(file.path()))) {
+      LOG(ERROR) << "Failed to create directory: " << pfile.DirName();
+      return false;
+    }
+
+    if (fs_manager->FileExists(pfile)) {
+      fs_manager->UnlinkFile(pfile);
+    }
+
+    if (!fs_manager->CreateFileAndFixedGoalFallocate(
+            pfile, file.size(), file.contents().extents())) {
+      fs_manager->UnlinkFile(pfile);
+    }
+  }
+
+  return true;
+}
+
+bool FilePreseeder::RestoreInlineFiles() {
+  for (auto& file : preseeded_files_.file_list()) {
+    if (!CheckAllowlist(base::FilePath(file.path()))) {
+      LOG(ERROR) << "Skipping file: " << file.path() << "; not in allowlist";
+      continue;
+    }
+
+    // Skip extent files.
+    if (file.contents().has_extents()) {
+      continue;
+    }
+
+    base::FilePath path = mount_root_.Append(file.path());
+    base::FilePath parent_dir = path.DirName();
+    if (!base::PathExists(parent_dir)) {
+      if (!base::CreateDirectory(parent_dir)) {
+        LOG(ERROR) << "Failed to create directory: " << parent_dir;
+        return false;
+      }
+    }
+
+    std::string contents = file.contents().data();
+    if (!base::WriteFile(path, contents)) {
+      LOG(ERROR) << "Failed to create file";
+    }
+  }
+
+  return true;
+}
+
+bool FilePreseeder::GetFileExtents(const base::FilePath& path,
+                                   ExtentArray* extents) {
+  base::ScopedFD fd(open(path.value().c_str(), O_RDONLY | O_CLOEXEC));
+  if (fd.get() < 0) {
+    PLOG(ERROR) << "Unable to open file: " << path.value();
+    return false;
+  }
+
+  size_t alloc_size = offsetof(struct fiemap, fm_extents[kMaxExtents]);
+  ScopedFiemap fm(static_cast<struct fiemap*>(malloc(alloc_size)));
+  memset(fm.get(), 0, alloc_size);
+
+  while (fm->fm_mapped_extents <= kMaxExtents) {
+    bool last_extent_found = false;
+
+    fm->fm_length = std::numeric_limits<uint64_t>::max();
+    fm->fm_flags |= FIEMAP_FLAG_SYNC;
+    fm->fm_extent_count = kMaxExtents;
+
+    if (HANDLE_EINTR(ioctl(fd.get(), FS_IOC_FIEMAP, fm.get())) < -1) {
+      PLOG(ERROR) << "Unable to get FIEMAP for file: " << path.value();
+      return false;
+    }
+
+    if (fm->fm_mapped_extents < 0 || fm->fm_mapped_extents > kMaxExtents) {
+      LOG(ERROR) << "Invalid extent count " << fm->fm_mapped_extents << "for "
+                 << "path " << path;
+      return false;
+    }
+
+    for (uint32_t i = 0; i < fm->fm_mapped_extents; i++) {
+      Extent* e = extents->add_extent();
+      e->set_goal(fm->fm_extents[i].fe_physical / kBlockSize);
+      e->set_start(fm->fm_extents[i].fe_logical / kBlockSize);
+      e->set_length(fm->fm_extents[i].fe_length / kBlockSize);
+
+      if (i == fm->fm_mapped_extents - 1) {
+        fm->fm_start =
+            fm->fm_extents[i].fe_logical + fm->fm_extents[i].fe_length;
+        if (fm->fm_extents[i].fe_flags & FIEMAP_EXTENT_LAST) {
+          last_extent_found = true;
+        }
+      }
+    }
+
+    if (last_extent_found) {
+      break;
+    }
+  }
+
+  return true;
+}
+
+}  // namespace libpreservation

init/libpreservation/file_preseeder.h
@@ -1,0 +1,56 @@
+// Copyright 2025 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef INIT_LIBPRESERVATION_FILE_PRESEEDER_H_
+#define INIT_LIBPRESERVATION_FILE_PRESEEDER_H_
+
+#include <set>
+
+#include <base/base64.h>
+#include <base/files/file_path.h>
+#include <brillo/brillo_export.h>
+#include <init/libpreservation/filesystem_manager.h>
+#include <init/libpreservation/preseeded_files.pb.h>
+
+namespace libpreservation {
+// Generic class for file preseeding across a filesystem reset:
+// this is used in two scenarios:
+// 1) Powerwash: preserve files across a TPM reset.
+// 2) Startup: preserve files across the establishment of a new
+//    dm-default-key layer.
+class BRILLO_EXPORT FilePreseeder {
+ public:
+  FilePreseeder(const std::set<base::FilePath>& directory_allowlist,
+                const base::FilePath& fs_root,
+                const base::FilePath& mount_root,
+                const base::FilePath& metadata_path);
+  virtual ~FilePreseeder();
+
+  virtual bool SaveFileState(const std::set<base::FilePath>& file_list);
+
+  // Intended to be used for crash resilience.
+  bool PersistMetadata();
+  bool LoadMetadata();
+
+  bool CheckAllowlist(const base::FilePath& path);
+  bool CreateDirectoryRecursively(FilesystemManager* fs_manager,
+                                  const base::FilePath& path);
+  bool RestoreExtentFiles(FilesystemManager* fs_manager);
+  bool RestoreInlineFiles();
+
+ protected:
+  virtual bool GetFileExtents(const base::FilePath& file, ExtentArray* extents);
+
+ private:
+  // Both allowlists are relative to the root of the filesystem.
+  const std::set<base::FilePath> directory_allowlist_;
+  const base::FilePath fs_root_;
+  const base::FilePath mount_root_;
+  const base::FilePath metadata_path_;
+  PreseededFileArray preseeded_files_;
+};
+
+}  // namespace libpreservation
+
+#endif  // INIT_LIBPRESERVATION_FILE_PRESEEDER_H_

init/libpreservation/file_preseeder_test.cc
@@ -1,0 +1,169 @@
+// Copyright 2025 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "init/libpreservation/file_preseeder.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include <base/files/file_path.h>
+#include <base/files/scoped_temp_dir.h>
+#include <brillo/file_utils.h>
+#include <brillo/files/file_util.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "base/files/file_util.h"
+#include "init/libpreservation/fake_ext2fs.h"
+#include "init/libpreservation/filesystem_manager.h"
+
+using ::testing::_;
+using ::testing::Return;
+
+namespace libpreservation {
+namespace {
+const base::FilePath kFoo = base::FilePath("foo");
+const base::FilePath kBarBaz = base::FilePath("bar/baz");
+const base::FilePath kBarFoo = base::FilePath("bar/foo");
+const base::FilePath kBarFooAr = base::FilePath("bar/foo/ar");
+const base::FilePath kBaz = base::FilePath("baz");
+const base::FilePath kBar = base::FilePath("bar");
+}  // namespace
+
+class FilePreseederTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
+    metadata_path_ = temp_dir_.GetPath().Append("metadata");
+    fs_root_ = base::FilePath("/");
+    mount_root_ = temp_dir_.GetPath().Append("mount_root");
+    ASSERT_TRUE(base::CreateDirectory(fs_root_));
+    ASSERT_TRUE(base::CreateDirectory(mount_root_));
+    fs_ = FakeExt2fs::Create(base::FilePath("/dev/null"));
+    fs_manager_ = std::make_unique<FilesystemManager>(std::move(fs_));
+  }
+
+ protected:
+  base::ScopedTempDir temp_dir_;
+  base::FilePath metadata_path_;
+  base::FilePath fs_root_;
+  base::FilePath mount_root_;
+  std::unique_ptr<Ext2fs> fs_;
+  std::unique_ptr<FilesystemManager> fs_manager_;
+};
+
+TEST_F(FilePreseederTest, SaveFileState) {
+  std::set<base::FilePath> file_allowlist = {kFoo, kBarBaz};
+  std::set<base::FilePath> directory_allowlist = {kBar};
+  FilePreseeder preseeder(directory_allowlist, fs_root_, mount_root_,
+                          metadata_path_);
+  base::FilePath file_foo = mount_root_.Append(kFoo);
+  base::FilePath file_baz = mount_root_.Append(kBarBaz);
+  ASSERT_TRUE(base::CreateDirectory(mount_root_.Append(kBar)));
+  ASSERT_TRUE(brillo::WriteStringToFile(file_foo, "foo"));
+  ASSERT_TRUE(brillo::WriteStringToFile(file_baz, "baz"));
+  EXPECT_TRUE(preseeder.SaveFileState(file_allowlist));
+  EXPECT_TRUE(base::PathExists(metadata_path_));
+}
+
+TEST_F(FilePreseederTest, SaveFileStateExtent) {
+  std::set<base::FilePath> file_allowlist = {kFoo, kBarBaz};
+  std::set<base::FilePath> directory_allowlist = {kBar};
+  FilePreseeder preseeder(directory_allowlist, fs_root_, mount_root_,
+                          metadata_path_);
+  base::FilePath file_foo = mount_root_.Append(kFoo);
+  base::FilePath file_baz = mount_root_.Append(kBarBaz);
+  ASSERT_TRUE(base::CreateDirectory(mount_root_.Append(kBar)));
+  std::string data(4096, 'a');
+  ASSERT_TRUE(brillo::WriteStringToFile(file_foo, data));
+  ASSERT_TRUE(brillo::WriteStringToFile(file_baz, data));
+  EXPECT_TRUE(preseeder.SaveFileState(file_allowlist));
+  EXPECT_TRUE(base::PathExists(metadata_path_));
+}
+
+TEST_F(FilePreseederTest, LoadMetadata) {
+  std::set<base::FilePath> file_allowlist = {kFoo, kBarBaz};
+  std::set<base::FilePath> directory_allowlist = {kBar};
+  FilePreseeder preseeder(directory_allowlist, fs_root_, mount_root_,
+                          metadata_path_);
+  base::FilePath file_foo = mount_root_.Append(kFoo);
+  base::FilePath file_baz = mount_root_.Append(kBarBaz);
+  ASSERT_TRUE(base::CreateDirectory(mount_root_.Append(kBar)));
+  ASSERT_TRUE(brillo::WriteStringToFile(file_foo, "foo"));
+  ASSERT_TRUE(brillo::WriteStringToFile(file_baz, "baz"));
+  EXPECT_TRUE(preseeder.SaveFileState(file_allowlist));
+  EXPECT_TRUE(base::PathExists(metadata_path_));
+
+  FilePreseeder preseeder2(directory_allowlist, fs_root_, mount_root_,
+                           metadata_path_);
+  EXPECT_TRUE(preseeder2.LoadMetadata());
+}
+
+TEST_F(FilePreseederTest, CreateDirectoryRecursively) {
+  std::set<base::FilePath> file_allowlist = {kFoo, kBarBaz};
+  std::set<base::FilePath> directory_allowlist = {kBar};
+  FilePreseeder preseeder(directory_allowlist, fs_root_, mount_root_,
+                          metadata_path_);
+  EXPECT_TRUE(preseeder.CreateDirectoryRecursively(fs_manager_.get(), kBarBaz));
+  EXPECT_TRUE(fs_manager_->FileExists(fs_root_.Append(kBarBaz)));
+  EXPECT_TRUE(fs_manager_->FileExists(fs_root_.Append(kBar)));
+}
+
+TEST_F(FilePreseederTest, RestoreExtentFiles) {
+  std::set<base::FilePath> file_allowlist = {kFoo, kBarBaz};
+  std::set<base::FilePath> directory_allowlist = {kBar};
+  FilePreseeder preseeder(directory_allowlist, fs_root_, mount_root_,
+                          metadata_path_);
+  base::FilePath file_foo = mount_root_.Append(kFoo);
+  base::FilePath file_baz = mount_root_.Append(kBarBaz);
+  ASSERT_TRUE(base::CreateDirectory(mount_root_.Append(kBar)));
+  std::string data(4096, 'a');
+  ASSERT_TRUE(brillo::WriteStringToFile(file_foo, data));
+  ASSERT_TRUE(brillo::WriteStringToFile(file_baz, data));
+  EXPECT_TRUE(preseeder.SaveFileState(file_allowlist));
+  EXPECT_TRUE(base::PathExists(metadata_path_));
+
+  FilePreseeder preseeder2(directory_allowlist, fs_root_, mount_root_,
+                           metadata_path_);
+  EXPECT_TRUE(preseeder2.LoadMetadata());
+  EXPECT_TRUE(preseeder2.RestoreExtentFiles(fs_manager_.get()));
+  EXPECT_TRUE(fs_manager_->FileExists(fs_root_.Append(kBarBaz)));
+  EXPECT_FALSE(fs_manager_->FileExists(fs_root_.Append(kFoo)));
+}
+
+TEST_F(FilePreseederTest, RestoreInlineFiles) {
+  std::set<base::FilePath> file_allowlist = {kFoo, kBarBaz};
+  std::set<base::FilePath> directory_allowlist = {kBar};
+  FilePreseeder preseeder(directory_allowlist, fs_root_, mount_root_,
+                          metadata_path_);
+  base::FilePath file_foo = mount_root_.Append(kFoo);
+  base::FilePath file_baz = mount_root_.Append(kBarBaz);
+  ASSERT_TRUE(base::CreateDirectory(mount_root_.Append(kBar)));
+  ASSERT_TRUE(brillo::WriteStringToFile(file_foo, "foo"));
+  ASSERT_TRUE(brillo::WriteStringToFile(file_baz, "baz"));
+  EXPECT_TRUE(preseeder.SaveFileState(file_allowlist));
+  EXPECT_TRUE(base::PathExists(metadata_path_));
+
+  FilePreseeder preseeder2(directory_allowlist, fs_root_, mount_root_,
+                           metadata_path_);
+  EXPECT_TRUE(preseeder2.LoadMetadata());
+  EXPECT_TRUE(preseeder2.RestoreInlineFiles());
+  EXPECT_TRUE(base::PathExists(file_foo));
+  EXPECT_TRUE(base::PathExists(file_baz));
+}
+
+TEST_F(FilePreseederTest, CheckAllowlist) {
+  std::set<base::FilePath> directory_allowlist = {kBarFoo};
+  FilePreseeder preseeder(directory_allowlist, fs_root_, mount_root_,
+                          metadata_path_);
+  EXPECT_FALSE(preseeder.CheckAllowlist(kFoo));
+  EXPECT_FALSE(preseeder.CheckAllowlist(kBarBaz));
+  EXPECT_TRUE(preseeder.CheckAllowlist(kBarFoo));
+  EXPECT_TRUE(preseeder.CheckAllowlist(kBarFooAr));
+
+  EXPECT_FALSE(preseeder.CheckAllowlist(kBaz));
+}
+
+}  // namespace libpreservation

init/libpreservation/proto/preseeded_files.proto
@@ -23,3 +23,26 @@ message Extent {
 message ExtentArray {
   repeated Extent extent = 1;
 }
+
+// Files can be of two types:
+// a. Inline files: files smaller than 44 bytes are stored inline in metadata.
+// b. Extent files: larger files are broken up into extents.
+message FileContents {
+  oneof Contents {
+    ExtentArray extents = 1;
+    string data = 2;
+  }
+}
+
+// Represents a preseeded file, with path (relative to root), size and
+// contents.
+message PreseededFile {
+  string path = 1;
+  uint64 size = 2;
+  FileContents contents = 3;
+}
+
+// List of all preseeded file structs.
+message PreseededFileArray {
+  repeated PreseededFile file_list = 1;
+}

commit 4e49cca53b47f59f6ed92487bb80b314a14dea4a

init: Add preservation helper for test/dev flows
Add a helper that can preserve preseeded file metadata in the
metadata partition. The helper is intended to be called
in test flows that may result in a reset of the system encryption
key.

This helper is invoked during the (dev) shutdown path to dump the
state of preseeded files that should be preserved. On subsequent
boots, if the TPM state is reset, the default-key-stateful
filesystem will be reset and the files from the last boot will be
preseeded. If the TPM state is not reset, the preseeding logic
will delete the stored state.

Additionally, do not attempt to reinitialize existing files. This
requires a slight fix in the existing logic which was mistakenly
first creating a directory instead of the file.

Fixes: fe745fc7356f6 ("init: Add FilePreseeder")

BUG=b:399570398
TEST=gbb transition from 0x0 <-> 0x9 with preserve_dev_image
TEST=tast run hwsec.FWMPAcrossTPMClear passes

Disallow-Recycled-Builds: test-failures
Change-Id: Ib828af71dcf9be4ff14ec2d6e01c1de75fa3775f
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6340228
Commit-Queue: Sarthak Kukreti <sarthakkukreti@google.com>
Tested-by: Sarthak Kukreti <sarthakkukreti@google.com>
Reviewed-by: Thomas Cedeno <thomascedeno@google.com>
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
Auto-Submit: Sarthak Kukreti <sarthakkukreti@google.com>

init/BUILD.gn
@@ -19,6 +19,7 @@ group("all") {
     ":net_poll_tool",
     ":periodic_scheduler",
     ":preseeded_files_proto",
+    ":preserve_dev_image",
     ":process_killer",
     ":sbin_init_scripts",
     ":static_node_tool",
@@ -376,6 +377,13 @@ proto_library("preseeded_files_proto") {
   sources = [ "${proto_in_dir}/preseeded_files.proto" ]
   standalone = true
   use_pic = true
+}
+
+executable("preserve_dev_image") {
+  sources = [ "libpreservation/preserve_dev_image.cc" ]
+  configs += [ ":target_defaults" ]
+  deps = [ ":libpreservation" ]
+  install_path = "/usr/local/bin"
 }
 
 if (use.test) {

init/dev_utils.sh
@@ -44,6 +44,9 @@ dev_unmount_packages() {
     done
   fi
 
+  # Preserve dev image for cases where the TPM might reset.
+  /usr/local/bin/preserve_dev_image
+
   # unmount /usr/local to match dev_mount_package.
   umount -n /usr/local
 }

init/libpreservation/file_preseeder.cc
@@ -166,15 +166,17 @@ bool FilePreseeder::RestoreExtentFiles(FilesystemManager* fs_manager) {
     }
 
     base::FilePath pfile = fs_root_.Append(file.path());
-
-    if (!fs_manager->FileExists(pfile.DirName()) &&
-        !CreateDirectoryRecursively(fs_manager, base::FilePath(file.path()))) {
-      LOG(ERROR) << "Failed to create directory: " << pfile.DirName();
-      return false;
+    if (fs_manager->FileExists(pfile)) {
+      continue;
     }
 
-    if (fs_manager->FileExists(pfile)) {
-      fs_manager->UnlinkFile(pfile);
+    base::FilePath parent_dir = base::FilePath(file.path()).DirName();
+
+    if (!fs_manager->FileExists(fs_root_.Append(parent_dir)) &&
+        !CreateDirectoryRecursively(fs_manager, parent_dir)) {
+      LOG(ERROR) << "Failed to create directory: "
+                 << fs_root_.Append(parent_dir);
+      return false;
     }
 
     if (!fs_manager->CreateFileAndFixedGoalFallocate(

init/libpreservation/preserve_dev_image.cc
@@ -1,0 +1,45 @@
+// Copyright 2025 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cstdlib>
+
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
+#include <base/logging.h>
+
+#include "init/libpreservation/file_preseeder.h"
+#include "init/libpreservation/preservation.h"
+
+int main(int argc, char** argv) {
+  std::string partition_layout;
+  base::FilePath rootdir("/");
+  base::FilePath stateful_mount = rootdir.Append("mnt/stateful_partition");
+  base::FilePath metadata_mount =
+      rootdir.Append("mnt/chromeos_metadata_partition");
+  base::FilePath system_encryption_key = metadata_mount.Append("encrypted.key");
+  base::FilePath preseeding_data = metadata_mount.Append("preseeder.proto");
+
+  // Check if the device is using the default key layout.
+  if (!base::PathExists(system_encryption_key)) {
+    return EXIT_SUCCESS;
+  }
+
+  libpreservation::FilePreseeder preseeder({base::FilePath("unencrypted")},
+                                           rootdir, stateful_mount,
+                                           preseeding_data);
+
+  std::set<base::FilePath> file_allowlist;
+  for (auto path : libpreservation::GetPreservationFileList()) {
+    file_allowlist.insert(base::FilePath(path));
+  }
+  for (auto path :
+       libpreservation::GetFactoryPreservationPathList(stateful_mount)) {
+    file_allowlist.insert(base::FilePath(path));
+  }
+  for (auto path : libpreservation::GetStartupPreseedingPaths()) {
+    file_allowlist.insert(base::FilePath(path));
+  }
+
+  return preseeder.SaveFileState(file_allowlist) ? EXIT_SUCCESS : EXIT_FAILURE;
+}