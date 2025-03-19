ArthurSonzogni/chromiumos-platform2

commit a218c06091eaa825d0fd799d0c4e1aa8a5e3ae77

init: Add pathway for default_key_migration_wipe
Add a pathway to migrate devices to default_key_stateful.
This pathway only gets triggered when two conditions are
met:
a) Device gets an auto-update with the "default_key_migration"
   preference set.
b) Device hasn't undergone OOBE yet.

In such cases, trigger the default_key_stateful migration.
At the same time, attempt to preserve existing LVM DLCs that
are intended to be powerwash safe.

BUG=b:376103572
TEST=echo /mnt/stateful_partition/.default_key_stateful_migration;
     check wipe

Change-Id: Ic77b9a9b33c8b81b7a39d63c6756bc7441c09e76
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6296923
Auto-Submit: Sarthak Kukreti <sarthakkukreti@google.com>
Reviewed-by: Jae Hoon Kim <kimjae@chromium.org>
Commit-Queue: Sarthak Kukreti <sarthakkukreti@google.com>
Tested-by: Sarthak Kukreti <sarthakkukreti@google.com>
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>

init/startup/chromeos_startup.cc
@@ -136,6 +136,8 @@ constexpr char kTpmFirmwareUpdateCleanup[] =
     "usr/sbin/tpm-firmware-update-cleanup";
 constexpr char kTpmFirmwareUpdateRequestFlagFile[] =
     "unencrypted/preserve/tpm_firmware_update_request";
+constexpr char kDefaultKeyStatefulMigrationTrigger[] =
+    "mnt/stateful_partition/unencrypted/.default_key_stateful_migration";
 
 constexpr char kLibWhitelist[] = "lib/whitelist";
 constexpr char kLibDevicesettings[] = "lib/devicesettings";
@@ -147,6 +149,8 @@ const std::array<const char*, 4> kPreserveDirs = {
     "var/lib/device_health_profile",
     "usr/local/etc/wifi_creds",
 };
+
+constexpr char kOobeCompletedFile[] = "/home/chronos/.oobe_completed";
 
 constexpr char kMetaDataFSType[] = "ext4";
 
@@ -1053,6 +1057,22 @@ int ChromeosStartup::Run() {
   DevCheckBlockDevMode(stateful_.Append(kDevModeFile));
 
   CheckForStatefulWipe();
+
+  // Check for default_key_stateful_migration pref from update_engine.
+  // If the device hasn't yet gone through the OOBE flow, trigger a transition
+  // to default_key_stateful partition.
+  if (USE_DEFAULT_KEY_STATEFUL && flags_->lvm_stateful &&
+      base::PathExists(root_.Append(kDefaultKeyStatefulMigrationTrigger)) &&
+      !base::PathExists(base::FilePath(kOobeCompletedFile))) {
+    brillo::DeleteFile(root_.Append(kDefaultKeyStatefulMigrationTrigger));
+    std::vector<base::FilePath> mnts;
+    mount_helper_->CleanupMountsStack(&mnts);
+
+    startup_dep_->Clobber("default_key_stateful_migration",
+                          {"default_key_migration_wipe"},
+                          "Transition to default-key-stateful");
+    return 0;
+  }
 
   // Cleanup the file attributes in the unencrypted stateful directory.
   base::FilePath unencrypted = stateful_.Append(kUnencrypted);

init/chromeos-boot-alert
@@ -256,6 +256,12 @@ mode_stateful_thinpool_migration() {
   show_assets_message "update_firmware_slow_extra_warning"
 }
 
+# Show a generic update message indicating that we're migrating to
+# dm-default-key, which can take upto a few minutes.
+mode_default_key_stateful_migration() {
+  show_assets_message "update_firmware_slow_extra_warning"
+}
+
 # Main initialization and dispatcher
 main() {
   # process args

init/clobber/clobber_lvm.cc
@@ -22,6 +22,7 @@
 #include <base/strings/string_split.h>
 #include <base/strings/string_util.h>
 #include <base/strings/stringprintf.h>
+#include <brillo/process/process.h>
 #include <chromeos/constants/imageloader.h>
 #include <libdlcservice/utils.h>
 
@@ -39,6 +40,9 @@ constexpr size_t kThinpoolSizePercent = 98;
 constexpr size_t kThinpoolMetadataSizePercent = 1;
 // Create thin logical volumes at 95% of the thinpool's size.
 constexpr size_t kLogicalVolumeSizePercent = 95;
+// Factory install dlc path.
+constexpr char kFactoryInstallDlcPath[] =
+    "/mnt/stateful_partition/unencrypted/dlc-factory-images";
 
 }  // namespace
 
@@ -377,6 +381,7 @@ ClobberLvm::DlcPreserveLogicalVolumesWipeArgs(
     // We add the active DLC logical volume.
     const auto& dlc_active_lv_name = utils->LogicalVolumeName(dlc, active_slot);
     verified_dlcs.emplace(PreserveLogicalVolumesWipeInfo{
+        .dlc_id = dlc,
         .lv_name = dlc_active_lv_name,
         .preserve = true,
         .zero = false,
@@ -424,4 +429,79 @@ ClobberLvm::PreserveLogicalVolumesWipeArgs(
       std::make_unique<dlcservice::Utils>());
   infos.insert(std::cbegin(dlcs), std::cend(dlcs));
   return infos;
+}
+
+bool ClobberLvm::MigratePowerwashSafeDlcs(
+    const base::FilePath& stateful_partition, dlcservice::PartitionSlot slot) {
+  ClobberLvm::PreserveLogicalVolumesWipeInfos infos =
+      PreserveLogicalVolumesWipeArgs(slot);
+
+  auto pv = lvm_->GetPhysicalVolume(stateful_partition);
+  if (!pv || !pv->IsValid()) {
+    LOG(WARNING) << "Failed to get physical volume.";
+    return false;
+  }
+  auto vg = lvm_->GetVolumeGroup(*pv);
+  if (!vg || !vg->IsValid()) {
+    LOG(WARNING) << "Failed to get volume group.";
+    return false;
+  }
+
+  if (!base::CreateDirectory(base::FilePath(kFactoryInstallDlcPath))) {
+    LOG(ERROR) << "Failed to create directory: " << kFactoryInstallDlcPath;
+    return false;
+  }
+
+  // Factory install path for preserving DLC
+  for (auto& info : infos) {
+    base::FilePath dlc_package_path = base::FilePath(kFactoryInstallDlcPath)
+                                          .Append(info.dlc_id)
+                                          .Append("package")
+                                          .Append("dlc.img");
+
+    // Check for logical volume existence and if this LV should be preserved.
+    if (info.preserve && !info.zero) {
+      auto lv = lvm_->GetLogicalVolume(*vg, info.lv_name);
+      if (!lv || !lv->IsValid()) {
+        continue;
+      }
+      if (!lv->Activate()) {
+        LOG(ERROR) << "Failed to active logical volume: " << info.lv_name;
+        continue;
+      }
+
+      if (!base::PathExists(dlc_package_path.DirName())) {
+        base::CreateDirectory(dlc_package_path.DirName());
+      }
+
+      LOG(INFO) << "Preserving DLC: " << lv->GetPath() << " to "
+                << dlc_package_path.value();
+
+      brillo::ProcessImpl dd;
+      dd.AddArg("/bin/dd");
+      dd.AddArg("if=" + lv->GetPath().value());
+      dd.AddArg("of=" + dlc_package_path.value());
+      dd.AddArg("bs=1");
+      dd.AddArg("count=" + std::to_string(info.digest_info->bytes));
+      dd.AddArg("conv=fsync");
+      dd.RedirectOutputToMemory(true);
+      if (dd.Run() != 0) {
+        LOG(ERROR) << "Failed to migrate logical volume: " << info.lv_name;
+        LOG(ERROR) << dd.GetOutputString(STDOUT_FILENO);
+        continue;
+      }
+
+      lv->Deactivate();
+
+      base::File dlc(dlc_package_path,
+                     base::File::FLAG_OPEN | base::File::FLAG_WRITE);
+
+      if (!dlc.IsValid()) {
+        PLOG(ERROR) << "Failed to open file: " << dlc_package_path.value();
+        continue;
+      }
+      dlc.SetLength(info.digest_info->bytes);
+    }
+  }
+  return true;
 }

init/clobber/clobber_lvm.h
@@ -43,6 +43,7 @@ class ClobberLvm {
   // be batched. This will come in handy for DLCs/logical volumes that want to
   // be atomically operated on.
   struct PreserveLogicalVolumesWipeInfo {
+    std::string dlc_id;
     std::string lv_name;
     bool preserve = false;
     bool zero = false;
@@ -100,6 +101,9 @@ class ClobberLvm {
 
   virtual std::optional<uint64_t> GetPartitionSize(
       const base::FilePath& base_device);
+
+  virtual bool MigratePowerwashSafeDlcs(
+      const base::FilePath& stateful_partition, dlcservice::PartitionSlot slot);
 
  protected:
   // These functions are marked protected so they can be overridden for tests.

init/clobber/clobber_state.cc
@@ -222,6 +222,12 @@ ClobberState::Arguments ClobberState::ParseArgv(int argc,
       args.preserve_lvs = USE_LVM_STATEFUL_PARTITION;
     } else if (arg == "disable_lvm_install" || USE_DISABLE_LVM_INSTALL) {
       args.disable_lvm_install = true;
+    } else if (arg == "default_key_migration_wipe") {
+      args.default_key_migration_wipe = true;
+      args.fast_wipe = true;
+      args.keepimg = true;
+      args.safe_wipe = true;
+      args.disable_lvm_install = true;
     }
   }
 
@@ -380,6 +386,11 @@ bool ClobberState::GetDevicesToWipe(
         base::FilePath(base_device + std::to_string(partitions.kernel_b));
   }
 
+  if (USE_DEFAULT_KEY_STATEFUL && partitions.cros_metadata != -1) {
+    wipe_info.cros_metadata_device =
+        base::FilePath(base_device + std::to_string(partitions.cros_metadata));
+  }
+
   *wipe_info_out = wipe_info;
   return true;
 }
@@ -429,7 +440,7 @@ std::vector<base::FilePath> ClobberState::GetPreservedFilesList() {
           args_.safe_wipe, args_.ad_migration_wipe, args_.rollback_wipe,
           args_.rma_wipe, debug_build == 1, false);
 
-  if (args_.factory_wipe) {
+  if (args_.factory_wipe || args_.default_key_migration_wipe) {
     for (auto& path :
          libpreservation::GetFactoryPreservationPathList(stateful_)) {
       stateful_paths.insert(path);
@@ -556,6 +567,50 @@ int ClobberState::Run() {
   LOG(INFO) << "RMA wipe: " << args_.rma_wipe;
   LOG(INFO) << "AD migration wipe: " << args_.ad_migration_wipe;
 
+  // As we move factory wiping from release image to factory test image,
+  // clobber-state will be invoked directly under a tmpfs. GetRootDevice cannot
+  // report correct output under such a situation. Therefore, the output is
+  // preserved then assigned to environment variables ROOT_DEV/ROOT_DISK for
+  // clobber-state. For other cases, the environment variables will be empty and
+  // it falls back to using GetRootDevice.
+  const char* root_disk_cstr = getenv("ROOT_DISK");
+  if (root_disk_cstr != nullptr) {
+    root_disk_ = base::FilePath(root_disk_cstr);
+  } else {
+    root_disk_ = utils::GetRootDevice(/*strip_partition=*/true);
+  }
+
+  base::FilePath root_device;
+  const char* root_device_cstr = getenv("ROOT_DEV");
+  if (root_device_cstr != nullptr) {
+    root_device = base::FilePath(root_device_cstr);
+  } else {
+    root_device = utils::GetRootDevice(/*strip_partition=*/false);
+  }
+
+  LOG(INFO) << "Root disk: " << root_disk_.value();
+  LOG(INFO) << "Root device: " << root_device.value();
+
+  partitions_.stateful = utils::GetPartitionNumber(root_disk_, "STATE");
+  partitions_.root_a = utils::GetPartitionNumber(root_disk_, "ROOT-A");
+  partitions_.root_b = utils::GetPartitionNumber(root_disk_, "ROOT-B");
+  partitions_.kernel_a = utils::GetPartitionNumber(root_disk_, "KERN-A");
+  partitions_.kernel_b = utils::GetPartitionNumber(root_disk_, "KERN-B");
+  partitions_.cros_metadata =
+      utils::GetPartitionNumber(root_disk_, "POWERWASH-DATA");
+
+  if (!GetDevicesToWipe(root_disk_, root_device, partitions_, &wipe_info_)) {
+    LOG(ERROR) << "Getting devices to wipe failed, aborting run";
+    return 1;
+  }
+
+  LOG(INFO) << "Stateful device: "
+            << wipe_info_.stateful_partition_device.value();
+  LOG(INFO) << "Inactive root device: "
+            << wipe_info_.inactive_root_device.value();
+  LOG(INFO) << "Inactive kernel device: "
+            << wipe_info_.inactive_kernel_device.value();
+
   // Most effective means of destroying user data is run at the start: Throwing
   // away the key to encrypted stateful by requesting the TPM to be cleared at
   // next boot.
@@ -597,6 +652,16 @@ int ClobberState::Run() {
     brillo::DeleteFile(stateful_.Append(kStatefulClobberLogPath));
   }
 
+  // Attempt to preserve installed powerwash-safe DLCs if switching to
+  // default_key_stateful from LVM.
+  if (args_.default_key_migration_wipe) {
+    clobber_lvm_->MigratePowerwashSafeDlcs(
+        wipe_info_.stateful_partition_device,
+        wipe_info_.active_kernel_partition == partitions_.kernel_a
+            ? dlcservice::PartitionSlot::A
+            : dlcservice::PartitionSlot::B);
+  }
+
   std::vector<base::FilePath> preserved_files = GetPreservedFilesList();
   for (const base::FilePath& fp : preserved_files) {
     LOG(INFO) << "Preserving file: " << fp.value();
@@ -616,48 +681,6 @@ int ClobberState::Run() {
     }
     UnmountEncryptedStateful();
   }
-
-  // As we move factory wiping from release image to factory test image,
-  // clobber-state will be invoked directly under a tmpfs. GetRootDevice cannot
-  // report correct output under such a situation. Therefore, the output is
-  // preserved then assigned to environment variables ROOT_DEV/ROOT_DISK for
-  // clobber-state. For other cases, the environment variables will be empty and
-  // it falls back to using GetRootDevice.
-  const char* root_disk_cstr = getenv("ROOT_DISK");
-  if (root_disk_cstr != nullptr) {
-    root_disk_ = base::FilePath(root_disk_cstr);
-  } else {
-    root_disk_ = utils::GetRootDevice(/*strip_partition=*/true);
-  }
-
-  base::FilePath root_device;
-  const char* root_device_cstr = getenv("ROOT_DEV");
-  if (root_device_cstr != nullptr) {
-    root_device = base::FilePath(root_device_cstr);
-  } else {
-    root_device = utils::GetRootDevice(/*strip_partition=*/false);
-  }
-
-  LOG(INFO) << "Root disk: " << root_disk_.value();
-  LOG(INFO) << "Root device: " << root_device.value();
-
-  partitions_.stateful = utils::GetPartitionNumber(root_disk_, "STATE");
-  partitions_.root_a = utils::GetPartitionNumber(root_disk_, "ROOT-A");
-  partitions_.root_b = utils::GetPartitionNumber(root_disk_, "ROOT-B");
-  partitions_.kernel_a = utils::GetPartitionNumber(root_disk_, "KERN-A");
-  partitions_.kernel_b = utils::GetPartitionNumber(root_disk_, "KERN-B");
-
-  if (!GetDevicesToWipe(root_disk_, root_device, partitions_, &wipe_info_)) {
-    LOG(ERROR) << "Getting devices to wipe failed, aborting run";
-    return 1;
-  }
-
-  LOG(INFO) << "Stateful device: "
-            << wipe_info_.stateful_partition_device.value();
-  LOG(INFO) << "Inactive root device: "
-            << wipe_info_.inactive_root_device.value();
-  LOG(INFO) << "Inactive kernel device: "
-            << wipe_info_.inactive_kernel_device.value();
 
   brillo::ProcessImpl log_preserve;
   log_preserve.AddArg("/sbin/clobber-log");
@@ -691,6 +714,9 @@ int ClobberState::Run() {
   if (args_.disable_lvm_install) {
     log_preserve.AddArg("disable_lvm_install");
   }
+  if (args_.default_key_migration_wipe) {
+    log_preserve.AddArg("default_key_migration_wipe");
+  }
 
   log_preserve.RedirectOutputToMemory(true);
   log_preserve.Run();
@@ -705,6 +731,12 @@ int ClobberState::Run() {
   // Ready for wiping.
   clobber_wipe_->SetPartitionInfo(partitions_);
   clobber_wipe_->SetFastWipe(args_.fast_wipe);
+
+  // Reset metadata partition.
+  if (!clobber_wipe_->WipeDevice(wipe_info_.cros_metadata_device)) {
+    LOG(ERROR) << "Unable to wipe device "
+               << wipe_info_.cros_metadata_device.value();
+  }
 
   base::ScopedClosureRunner reset_stateful(base::BindOnce(
       &ClobberState::ResetStatefulPartition, weak_ptr_factory_.GetWeakPtr()));
@@ -753,6 +785,14 @@ int ClobberState::Run() {
   ret = CreateStatefulFileSystem(wipe_info_.stateful_filesystem_device.value());
   if (ret) {
     LOG(ERROR) << "Unable to create stateful file system. Error code: " << ret;
+  }
+
+  if (USE_DEFAULT_KEY_STATEFUL && args_.default_key_migration_wipe) {
+    ret = CreateStatefulFileSystem(wipe_info_.cros_metadata_device.value());
+    if (ret) {
+      LOG(ERROR) << "Unable to create stateful file system. Error code: "
+                 << ret;
+    }
   }
 
   // Mount the fresh image for last minute additions.

init/clobber/clobber_state.h
@@ -55,6 +55,8 @@ class ClobberState {
     bool preserve_lvs = false;
     // Force disable LVM stateful, even if the device supports it.
     bool disable_lvm_install = false;
+    // Default-key-stateful migration wipe.
+    bool default_key_migration_wipe = false;
   };
 
   struct DeviceWipeInfo {
@@ -65,6 +67,8 @@ class ClobberState {
     base::FilePath stateful_filesystem_device;
     base::FilePath inactive_root_device;
     base::FilePath inactive_kernel_device;
+    // Used only on default_key_stateful layouts.
+    base::FilePath cros_metadata_device;
 
     // The partition number for the currently booted kernel partition.
     int active_kernel_partition = -1;
@@ -156,6 +160,9 @@ class ClobberState {
 
   // Helper to wrap calls removing logical volumes and device level wipes.
   void ResetStatefulPartition();
+
+  // Helper to reset metadata partition.
+  void ResetMetadataPartition();
 
   Arguments args_;
   std::unique_ptr<crossystem::Crossystem> cros_system_;

init/clobber/clobber_wipe.h
@@ -20,6 +20,7 @@ class ClobberWipe {
     int root_b = -1;
     int kernel_a = -1;
     int kernel_b = -1;
+    int cros_metadata = -1;
   };
 
   explicit ClobberWipe(ClobberUi* ui);

commit 66758b61d264e8be3162e69a410f2465e9e30cca

init: startup: Check for forced AU only once /home/chronos is mounted
The check for forced AU wipe is incorrect since it doesn't check
the presence of the OOBE complete file correctly.

Also fix the installer trigger to only create the trigger for
devices that support the new layout.

Fixes: a218c06091eaa ("init: Add pathway for default_key_migration_wipe")

BUG=b:404679935
TEST=trigger FAU check with users logged, check for lack of powerwash.

Change-Id: I58a2bd97155812d11ccc40d6e43a343f5af265e4
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6371242
Tested-by: Sarthak Kukreti <sarthakkukreti@google.com>
Commit-Queue: Sarthak Kukreti <sarthakkukreti@google.com>
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
Auto-Submit: Sarthak Kukreti <sarthakkukreti@google.com>

init/startup/chromeos_startup.cc
@@ -131,7 +131,7 @@ constexpr char kTpmFirmwareUpdateCleanup[] =
 constexpr char kTpmFirmwareUpdateRequestFlagFile[] =
     "unencrypted/preserve/tpm_firmware_update_request";
 constexpr char kDefaultKeyStatefulMigrationTrigger[] =
-    "mnt/stateful_partition/unencrypted/.default_key_stateful_migration";
+    "unencrypted/.default_key_stateful_migration";
 
 constexpr char kLibWhitelist[] = "lib/whitelist";
 constexpr char kLibDevicesettings[] = "lib/devicesettings";
@@ -1080,22 +1080,6 @@ int ChromeosStartup::Run() {
 
   CheckForStatefulWipe();
 
-  // Check for default_key_stateful_migration pref from update_engine.
-  // If the device hasn't yet gone through the OOBE flow, trigger a transition
-  // to default_key_stateful partition.
-  if (USE_DEFAULT_KEY_STATEFUL && flags_->lvm_stateful &&
-      base::PathExists(root_.Append(kDefaultKeyStatefulMigrationTrigger)) &&
-      !base::PathExists(base::FilePath(kOobeCompletedFile))) {
-    brillo::DeleteFile(root_.Append(kDefaultKeyStatefulMigrationTrigger));
-    std::vector<base::FilePath> mnts;
-    mount_helper_->CleanupMountsStack(&mnts);
-
-    startup_dep_->Clobber("default_key_stateful_migration",
-                          {"default_key_migration_wipe"},
-                          "Transition to default-key-stateful");
-    return 0;
-  }
-
   // Cleanup the file attributes in the unencrypted stateful directory.
   base::FilePath unencrypted = stateful_.Append(kUnencrypted);
   ForceCleanFileAttrs(unencrypted);
@@ -1125,6 +1109,22 @@ int ChromeosStartup::Run() {
   if (!mount_helper_->DoMountVarAndHomeChronos(key)) {
     mount_helper_->CleanupMounts("Unable to mount encrypted");
     // No return unless in unit tests.
+    return 0;
+  }
+
+  // Check for default_key_stateful_migration pref from update_engine.
+  // If the device hasn't yet gone through the OOBE flow, trigger a transition
+  // to default_key_stateful partition.
+  if (USE_DEFAULT_KEY_STATEFUL && flags_->lvm_stateful &&
+      base::PathExists(stateful_.Append(kDefaultKeyStatefulMigrationTrigger)) &&
+      !base::PathExists(base::FilePath(kOobeCompletedFile))) {
+    brillo::DeleteFile(stateful_.Append(kDefaultKeyStatefulMigrationTrigger));
+    std::vector<base::FilePath> mnts;
+    mount_helper_->CleanupMountsStack(&mnts);
+
+    startup_dep_->Clobber("default_key_stateful_migration",
+                          {"default_key_migration_wipe"},
+                          "Transition to default-key-stateful");
     return 0;
   }
 

installer/chromeos_postinst.cc
@@ -607,7 +607,7 @@ bool ESPPostInstall(const InstallConfig& install_config) {
 // On update, install file to check for default-key-stateful migraiton for
 // supported boards.
 void CheckForDefaultKeyStatefulMigration() {
-  if (USE_DEFAULT_KEY_STATEFUL) {
+  if (FormatMetaDataPartitionNeeded()) {
     base::WriteFile(base::FilePath(kDefaultKeyStatefulMigrationTrigger), "");
   }
 }