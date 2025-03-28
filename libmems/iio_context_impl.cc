ArthurSonzogni/chromiumos-platform2

commit 0f9d0fd0a7422399e08b30d8ca082541c513a36b

libmems: limit ODR
Based on a cros config properly, limit ODR
We need to make sure iio_device has a valid context (fake or real)
when GetMinMaxFrequencies() is called.

BUG=b:353354908
TEST=Unit test, on dewatt, check frequency is limited.

Cq-Depend: chromium:5841533
Change-Id: I86453400f35bd3a67362d78d8b01b1c384ca36c9
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/5847427
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Harvey Yang <chenghaoyang@chromium.org>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>

libmems/iio_context_impl.cc
@@ -12,7 +12,10 @@
 #include <base/check.h>
 #include <base/files/file_util.h>
 #include <base/logging.h>
+#include <base/strings/string_number_conversions.h>
+#include <base/strings/string_util.h>
 #include <base/strings/stringprintf.h>
+#include <chromeos-config/libcros_config/cros_config.h>
 
 #include "libmems/common_types.h"
 #include "libmems/iio_channel_impl.h"
@@ -23,6 +26,15 @@ namespace libmems {
 
 IioContextImpl::IioContextImpl() {
   Reload();
+
+  brillo::CrosConfig cros_config;
+  std::string max_sensor_odr_override;
+  cros_config.GetString("/hardware-properties", "ec-max-sensor-odr",
+                        &max_sensor_odr_override);
+  int max_sensor_odr_value;
+  if (base::StringToInt(max_sensor_odr_override, &max_sensor_odr_value)) {
+    *max_sensor_odr_ = max_sensor_odr_value / 1000.0;
+  }
 }
 
 void IioContextImpl::Reload() {

libmems/iio_device.cc
@@ -192,7 +192,7 @@ bool IioDevice::GetMinMaxFrequency(double* min_freq, double* max_freq) {
       }
 
       *max_freq = *min_freq;
-      return true;
+      break;
 
     default:
       if (!base::StringToDouble(sampling_frequencies.back(), max_freq) ||
@@ -217,9 +217,13 @@ bool IioDevice::GetMinMaxFrequency(double* min_freq, double* max_freq) {
           return false;
         }
       }
-
-      return true;
   }
+  std::optional<int> max_sensor_odr = GetContext()->GetMaxSensorOdr();
+  if (max_sensor_odr && *max_sensor_odr < *max_freq) {
+    *max_freq = *max_sensor_odr;
+  }
+
+  return true;
 }
 
 }  // namespace libmems

iioservice/daemon/samples_handler_test.cc
@@ -99,7 +99,7 @@ class SamplesHandlerTestBase : public cros::mojom::SensorDeviceSamplesObserver {
  protected:
   void SetUpAccelBase(bool with_hrtimer, bool with_matrix = false) {
     device_ = std::make_unique<libmems::fakes::FakeIioDevice>(
-        nullptr, fakes::kAccelDeviceName, fakes::kAccelDeviceId);
+        context_.get(), fakes::kAccelDeviceName, fakes::kAccelDeviceId);
 
     if (with_matrix) {
       EXPECT_TRUE(device_->WriteStringAttribute(kAccelMatrixAttribute,
@@ -116,7 +116,7 @@ class SamplesHandlerTestBase : public cros::mojom::SensorDeviceSamplesObserver {
 
   void SetUpLightBase(bool with_hrtimer) {
     device_ = std::make_unique<libmems::fakes::FakeIioDevice>(
-        nullptr, kFakeLightName, kFakeLightId);
+        context_.get(), kFakeLightName, kFakeLightId);
 
     auto light_channel = std::make_unique<libmems::fakes::FakeIioChannel>(
         cros::mojom::kLightChannel, true);

iioservice/daemon/sensor_device_fusion_gravity_test.cc
@@ -49,7 +49,7 @@ class SensorDeviceFusionGravityTest : public ::testing::Test {
     context_ = std::make_unique<libmems::fakes::FakeIioContext>();
 
     auto device = std::make_unique<libmems::fakes::FakeIioDevice>(
-        nullptr, fakes::kAccelDeviceName, fakes::kAccelDeviceId);
+        context_.get(), fakes::kAccelDeviceName, fakes::kAccelDeviceId);
 
     EXPECT_TRUE(
         device->WriteStringAttribute(libmems::kSamplingFrequencyAvailable,

iioservice/daemon/sensor_device_fusion_test.cc
@@ -174,7 +174,7 @@ class IioDeviceHandlerTest : public ::testing::Test,
  protected:
   void SetUp() override {
     auto device = std::make_unique<libmems::fakes::FakeIioDevice>(
-        nullptr, fakes::kAccelDeviceName, fakes::kAccelDeviceId);
+        &context_, fakes::kAccelDeviceName, fakes::kAccelDeviceId);
 
     EXPECT_TRUE(
         device->WriteStringAttribute(libmems::kSamplingFrequencyAvailable,

iioservice/daemon/sensor_device_impl_test.cc
@@ -67,7 +67,7 @@ class SensorDeviceImplTest : public ::testing::Test {
     EXPECT_FALSE(remote_.is_bound());
 
     auto device = std::make_unique<libmems::fakes::FakeIioDevice>(
-        nullptr, fakes::kAccelDeviceName, fakes::kAccelDeviceId);
+        context_.get(), fakes::kAccelDeviceName, fakes::kAccelDeviceId);
     EXPECT_TRUE(
         device->WriteStringAttribute(libmems::kSamplingFrequencyAvailable,
                                      fakes::kFakeSamplingFrequencyAvailable));

libmems/BUILD.gn
@@ -20,6 +20,7 @@ pkg_config("target_defaults_pkg_deps") {
   pkg_deps = [
     "libbrillo",
     "libchrome",
+    "libcros_config",
     "libiio",
     "libudev",
   ]

libmems/iio_context.h
@@ -90,10 +90,18 @@ class LIBMEMS_EXPORT IioContext {
   // object is valid.
   virtual std::vector<IioDevice*> GetAllTriggers() = 0;
 
+  // If present, return the maximal ODR the sensors managed by the EC can used
+  // at. It allows reducing the maximal ODR without releasing a new firmware. On
+  // device when the ODR is not limited by cros_config or devices without EC,
+  // the property is not present.
+  std::optional<double> GetMaxSensorOdr() { return max_sensor_odr_; }
+
  protected:
   IioContext() = default;
   IioContext(const IioContext&) = delete;
   IioContext& operator=(const IioContext&) = delete;
+
+  std::optional<double> max_sensor_odr_;  // in Hz.
 };
 
 }  // namespace libmems

commit fb4d891064a33f4ce97aeb9af2f170af0bd15ef5

libmems: Fix limit ODR
Fix initialization error that crash libmems in the iio_context
constructor.
Fix type.

BUG=b:353354908,b:406277521
TEST=Unit test, Check `cros-health-tool diag sensitive_sensor_v2` on
dewatt.

Fixes: 0f9d0fd0a74 ("libmems: limit ODR")
Change-Id: Id8e955a2bae0f6821bd08c9f48378e41aea3c30f
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6405837
Reviewed-by: Byron Lee <byronlee@chromium.org>
Auto-Submit: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Harvey Yang <chenghaoyang@chromium.org>
Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>
Tested-by: Gwendal Grignou <gwendal@chromium.org>

libmems/iio_context_impl.cc
@@ -33,7 +33,7 @@ IioContextImpl::IioContextImpl() {
                         &max_sensor_odr_override);
   int max_sensor_odr_value;
   if (base::StringToInt(max_sensor_odr_override, &max_sensor_odr_value)) {
-    *max_sensor_odr_ = max_sensor_odr_value / 1000.0;
+    max_sensor_odr_ = max_sensor_odr_value / 1000.0;
   }
 }
 

libmems/iio_device.cc
@@ -218,7 +218,7 @@ bool IioDevice::GetMinMaxFrequency(double* min_freq, double* max_freq) {
         }
       }
   }
-  std::optional<int> max_sensor_odr = GetContext()->GetMaxSensorOdr();
+  std::optional<double> max_sensor_odr = GetContext()->GetMaxSensorOdr();
   if (max_sensor_odr && *max_sensor_odr < *max_freq) {
     *max_freq = *max_sensor_odr;
   }