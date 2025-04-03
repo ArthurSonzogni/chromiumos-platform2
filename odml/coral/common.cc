b476897a00869b0fb0579c7a2602f99b11dd7922
Howard Yang <hcyang@google.com>
2025-04-02 22:52:13 -0700

    Revert "odml: [CORAL] Reduce supported language list"
    This reverts commit 8e6790ddd46e676513178b6132f82dcd9bbb5fbf.
    
    Reason for revert: This CL is not needed for M137+.
    
    Original change's description:
    > odml: [CORAL] Reduce supported language list
    >
    > Some languages are not supported by the safety model yet. Remove those
    > from supported language list.
    >
    > BUG=b:407690790
    > TEST=odml unittests
    >
    > Change-Id: Ic82b7a29548bf99ad4fca8a4da395c748b5c2736
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6420891
    > Tested-by: Howard Yang <hcyang@google.com>
    > Reviewed-by: Yi Chou <yich@google.com>
    > Commit-Queue: Howard Yang <hcyang@google.com>
    
    BUG=b:407690790
    
    Change-Id: Ief1daf60f600f4d590ab554c3b598c7e592b7d6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6429224
    Commit-Queue: Howard Yang <hcyang@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- odml/coral/common.cc
+++ odml/coral/common.cc
@@ -13,7 +13,9 @@ namespace coral {
 
 bool IsLanguageSupported(const std::string& locale) {
   static constexpr auto kSupportedLanguages =
-      base::MakeFixedFlatSet<std::string_view>({"en", "ja", "de", "fr"});
+      base::MakeFixedFlatSet<std::string_view>({"en", "ja", "de", "fr", "da",
+                                                "es", "it", "pt", "nl", "no",
+                                                "fi", "sv"});
   return base::Contains(kSupportedLanguages, locale);
 }
 
