ce995d1af1c7b6fb64a24cd4d5802a85106433c0
Justin Green <greenjustin@google.com>
2025-04-11 12:31:45 -0400

    cros-codecs: Build image_processor_perf_test in AL
    BUG=b:409361773
    TEST=Manual testing on Brya and Corsola
    
    Change-Id: Ifb422882f8ce9d02f0067d1d3c5aecdc79453d63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6599595
    Commit-Queue: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Auto-Submit: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Tested-by: Bijan Choobineh‎ <bchoobineh@chromium.org>

--- cros-codecs/examples/perf_test.rs
+++ cros-codecs/examples/perf_test.rs
@@ -2,13 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+//! perf_test is a binary that contains a number of performance tests for our image processing
+//! functions.
+
 use cros_codecs::image_processing::*;
+#[cfg(feature = "v4l2")]
 use std::ptr;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 const K_NUMBER_OF_TEST_CYCLES: u64 = 2000;
+const K_WIDTH: usize = 480;
+const K_HEIGHT: usize = 288;
+#[cfg(feature = "v4l2")]
 const K_ADDR_ALIGN: usize = 16;
-const K_DATA_LEN: usize = 480 * 288 * 3 / 2;
 
 #[cfg(feature = "v4l2")]
 fn test_mm21_to_nv12_perf() {
@@ -16,30 +22,30 @@ fn test_mm21_to_nv12_perf() {
     let test_expected_output = include_bytes!("../src/test_data/puppets-480x270_20230825.nv12.yuv");
 
     // Byte align test input.
-    let mut aligned_input = [0u8; (K_DATA_LEN + K_ADDR_ALIGN)];
+    let mut aligned_input = vec![0u8; test_input.len() + K_ADDR_ALIGN];
     let mut index = K_ADDR_ALIGN - (ptr::addr_of!(aligned_input[0]) as usize % K_ADDR_ALIGN);
-    let input_data = aligned_input.get_mut(index..(index + K_DATA_LEN)).unwrap();
+    let input_data = aligned_input.get_mut(index..(index + test_input.len())).unwrap();
     input_data.copy_from_slice(test_input);
 
     // Byte align test output.
-    let mut aligned_output = [0u8; (K_DATA_LEN + K_ADDR_ALIGN)];
+    let mut aligned_output = vec![0u8; test_input.len() + K_ADDR_ALIGN];
     index = K_ADDR_ALIGN - (ptr::addr_of!(aligned_output[0]) as usize % K_ADDR_ALIGN);
-    let output_data = aligned_output.get_mut(index..(index + K_DATA_LEN)).unwrap();
-    let (test_y_output, test_uv_output) = output_data.split_at_mut(480 * 288);
+    let output_data = aligned_output.get_mut(index..(index + test_input.len())).unwrap();
+    let (test_y_output, test_uv_output) = output_data.split_at_mut(output_data.len() * 2 / 3);
 
     let start_time = Instant::now();
     for _cycle in 0..K_NUMBER_OF_TEST_CYCLES {
         let _ = mm21_to_nv12(
-            &input_data[0..480 * 288],
-            480,
+            &input_data[..(test_input.len() * 2 / 3)],
+            K_WIDTH,
             test_y_output,
-            480,
-            &input_data[480 * 288..480 * 288 * 3 / 2],
-            480,
+            K_WIDTH,
+            &input_data[(test_input.len() * 2 / 3)..],
+            K_WIDTH,
             test_uv_output,
-            480,
-            480,
-            288,
+            K_WIDTH,
+            K_WIDTH,
+            K_HEIGHT as isize,
         );
     }
 
@@ -60,25 +66,25 @@ fn test_nv12_to_i420_perf() {
     let test_input = include_bytes!("../src/test_data/puppets-480x270_20230825.nv12.yuv");
     let test_expected_output = include_bytes!("../src/test_data/puppets-480x270_20230825.i420.yuv");
 
-    let mut test_output = [0u8; 480 * 288 * 3 / 2];
-    let (test_y_output, test_uv_output) = test_output.split_at_mut(480 * 288);
-    let (test_u_output, test_v_output) = test_uv_output.split_at_mut(480 * 288 / 4);
+    let mut test_output = vec![0u8; test_input.len()];
+    let (test_y_output, test_uv_output) = test_output.split_at_mut(test_input.len() * 2 / 3);
+    let (test_u_output, test_v_output) = test_uv_output.split_at_mut(test_uv_output.len() / 2);
 
     let start_time = Instant::now();
     for _cycle in 0..K_NUMBER_OF_TEST_CYCLES {
-        let _ = nv12_to_i420(
-            &test_input[0..480 * 288],
-            480,
+        nv12_to_i420(
+            &test_input[..test_input.len() * 2 / 3],
+            K_WIDTH,
             test_y_output,
-            480,
-            &test_input[480 * 288..480 * 288 * 3 / 2],
-            480,
+            K_WIDTH,
+            &test_input[test_input.len() * 2 / 3..],
+            K_WIDTH,
             test_u_output,
-            240,
+            K_WIDTH / 2,
             test_v_output,
-            240,
-            480,
-            288,
+            K_WIDTH / 2,
+            K_WIDTH,
+            K_HEIGHT,
         );
     }
 
@@ -99,16 +105,16 @@ fn test_i420_to_nv12_perf() {
     let test_input = include_bytes!("../src/test_data/puppets-480x270_20230825.i420.yuv");
     let test_expected_output = include_bytes!("../src/test_data/puppets-480x270_20230825.nv12.yuv");
 
-    let mut test_output = [0u8; 480 * 288 * 3 / 2];
-    let (test_y_output, test_uv_output) = test_output.split_at_mut(480 * 288);
+    let mut test_output = vec![0u8; test_input.len()];
+    let (test_y_output, test_uv_output) = test_output.split_at_mut(test_input.len() * 2 / 3);
 
     let start_time = Instant::now();
     for _cycle in 0..K_NUMBER_OF_TEST_CYCLES {
-        let _ = i420_to_nv12(
-            &test_input[0..(480 * 288)],
+        i420_to_nv12(
+            &test_input[..test_input.len() * 2 / 3],
             test_y_output,
-            &test_input[(480 * 288)..(480 * 288 * 5 / 4)],
-            &test_input[(480 * 288 * 5 / 4)..(480 * 288 * 3 / 2)],
+            &test_input[(test_input.len() * 2 / 3)..(test_input.len() * 5 / 6)],
+            &test_input[(test_input.len() * 5 / 6)..],
             test_uv_output,
         );
     }
