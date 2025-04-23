ffe80bc632dfff2bfe94d3b08ff2ffc438b66ba2
Hang Nguyen <hnt@google.com>
2025-04-23 09:26:16 -0700

    cros-codecs: Extract and rename tests with configs
    This CL renames the sources files for ccdec_vp8_test and its
    corresponding ccdec_vp8_test.xml config file.
    The CL also renames and moves the test config file for ccdec_test into
    the common place: tests/configs/.
    
    TEST=atest ccdec_test
    TEST=atest ccdec_vp8_test
    BUG=b:394638011
    
    Change-Id: Ie65d916d674e72d70b60e15fd6d9b906e56cae91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6599611
    Reviewed-by: Hang Nguyen <hnt@google.com>
    Tested-by: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Commit-Queue: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Auto-Submit: Bijan Choobineh‎ <bchoobineh@chromium.org>

--- cros-codecs/tests/ccdec/ccdec_vp8_test.rs
+++ cros-codecs/tests/ccdec/ccdec_vp8_test.rs
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This module provides helper functions for the ccdec_[codec]_test modules.
-// It is kept separate and not exposed in a common file to avoid to having all
-// test modules grouped together by test harness.
-
 mod execution_utils;
 mod verification_test_vectors;
 
