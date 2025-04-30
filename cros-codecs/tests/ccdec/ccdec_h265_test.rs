53b1eaba3eb3e8fb523b20a9b0821e5a950c9483
Hang Nguyen <hnt@google.com>
2025-04-30 13:22:53 -0700

    cros-codecs: Add H.265 decode test with basic test setup
    This CL adds the Android build definition for H.265 decode test with
    cros-codecs. This change is required before having the test module
    exempt from the general-tests presubmit check to use GS dynamic link for
    external data.
    
    The test is enabled on x86_64 for brya only.
    
    BUG=b:414855002
    TEST=atest ccdec_h265_test
    
    Change-Id: Ia67414506cd825a641a3b576e5d80611ad4c5870
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6599630
    Auto-Submit: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Commit-Queue: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Tested-by: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Reviewed-by: Hang Nguyen <hnt@google.com>

---
+++ cros-codecs/tests/ccdec/ccdec_h265_test.rs
@@ -0,0 +1,10 @@
+// Copyright 2025 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#[cfg(test)]
+#[allow(dead_code)]
+mod tests {
+    // TODO(b/400788075): Add tests with external data dependencies once exempt
+    // to use GS dynamic link.
+}
