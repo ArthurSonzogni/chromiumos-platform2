868097af081558584f51590e1775efa556e5a51e
Hang Nguyen <hnt@google.com>
2025-04-29 19:21:20 -0700

    cros-codecs: Add VP9 decode test for profile 0 group 1
    This CL adds a new test module ccdec_vp9_test for VP9 hardware decode
    test with cros-codecs. The test module includes tests on external test
    vectors from profile 0 group 1.
    
    BUG=b:400788075
    TEST=atest ccdec_vp9_test -- --enable-module-dynamic-download
    TEST=atest ccdec_vp9_test:ccdec_vp9_test#tests::profile_0_group1_buf
    -- --enable-module-dynamic-download
    
    Change-Id: I975fd3b0c37737500da2568212ea25a13f62d1cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6599626
    Reviewed-by: Hang Nguyen <hnt@google.com>
    Auto-Submit: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Tested-by: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Commit-Queue: Bijan Choobineh‎ <bchoobineh@chromium.org>

--- cros-codecs/tests/ccdec/ccdec_vp9_test.rs
+++ cros-codecs/tests/ccdec/ccdec_vp9_test.rs
@@ -3,8 +3,81 @@
 // found in the LICENSE file.
 
 #[cfg(test)]
-#[allow(dead_code)]
 mod tests {
-    // TODO(b/400788075): Add tests with external data dependencies once exempt
-    // to use GS dynamic link.
+    use cros_codecs_test_common::ccdec::execution_utils::run_ccdec_test_by_codec_group;
+    use cros_codecs_test_common::ccdec::verification_test_vectors::VP9_WEBM_FILES;
+
+    #[test]
+    fn profile_0_group1_buf() {
+        let filenames: &Vec<&'static str> = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group1")
+            .expect("No test vectors found for group1 in profile_0")
+            .get("buf")
+            .expect("No test vectors found for buf in profile_0/group1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_group1_frm_resize() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group1")
+            .expect("No test vectors found for group1 in profile_0")
+            .get("frm_resize")
+            .expect("No test vectors found for frm_resize in profile_0/group1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group1_gf_dist() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group1")
+            .expect("No test vectors found for group1 in profile_0")
+            .get("gf_dist")
+            .expect("No test vectors found for gf_dist in profile_0/group1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group1_odd_size() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group1")
+            .expect("No test vectors found for group1 in profile_0")
+            .get("odd_size")
+            .expect("No test vectors found for odd_size in profile_0/group1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group1_sub8x8() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group1")
+            .expect("No test vectors found for group1 in profile_0")
+            .get("sub8x8")
+            .expect("No test vectors found for sub8x8 in profile_0/group1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_group1_sub8x8_sf() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group1")
+            .expect("No test vectors found for group1 in profile_0")
+            .get("sub8x8_sf")
+            .expect("No test vectors found for sub8x8_sf in profile_0/group1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
 }
--- cros-codecs/tests/ccdec/verification_test_vectors.rs
+++ cros-codecs/tests/ccdec/verification_test_vectors.rs
@@ -10,11 +10,14 @@ use lazy_static::lazy_static;
 use std::collections::HashMap;
 
 lazy_static! {
-    /// A HashMap lazily initialized to contain VP8 test filenames categorized by group.
+    /// VP8 test filenames categorized by group.
     pub static ref VP8_FILENAMES: HashMap<&'static str, Vec<&'static str>> = vp8_files();
+    /// VP9 test filenames categorized by profile, group and test type.
+    pub static ref VP9_WEBM_FILES: HashMap<&'static str, HashMap<&'static str, HashMap<&'static str, Vec<&'static str>>>> = vp9_webm_files();
 }
 
-/// Provides a collection of VP8 test vector files categorized by test type.
+#[allow(dead_code)]
+/// Provides VP8 test vector files categorized by group.
 pub fn vp8_files() -> HashMap<&'static str, Vec<&'static str>> {
     let mut vp8_files: HashMap<&'static str, Vec<&'static str>> = HashMap::new();
     vp8_files.insert(
@@ -29,12 +32,10 @@ pub fn vp8_files() -> HashMap<&'static str, Vec<&'static str>> {
             "test_vectors/vp8/inter_multi_coeff/vp80-04-partitions-1406.ivf",
         ],
     );
-
     vp8_files.insert(
         "inter_segment",
         vec!["test_vectors/vp8/inter_segment/vp80-03-segmentation-1407.ivf"],
     );
-
     vp8_files.insert(
         "inter",
         vec![
@@ -64,17 +65,14 @@ pub fn vp8_files() -> HashMap<&'static str, Vec<&'static str>> {
             "test_vectors/vp8/inter/vp80-05-sharpness-1443.ivf",
         ],
     );
-
     vp8_files.insert(
         "intra_multi_coeff",
         vec!["test_vectors/vp8/intra_multi_coeff/vp80-03-segmentation-1414.ivf"],
     );
-
     vp8_files.insert(
         "intra_segment",
         vec!["test_vectors/vp8/intra_segment/vp80-03-segmentation-1415.ivf"],
     );
-
     vp8_files.insert(
         "intra",
         vec![
@@ -85,7 +83,6 @@ pub fn vp8_files() -> HashMap<&'static str, Vec<&'static str>> {
             "test_vectors/vp8/intra/vp80-03-segmentation-1401.ivf",
         ],
     );
-
     vp8_files.insert(
         "comprehensive",
         vec![
@@ -109,6 +106,88 @@ pub fn vp8_files() -> HashMap<&'static str, Vec<&'static str>> {
             "test_vectors/vp8/vp80-00-comprehensive-018.ivf",
         ],
     );
-
     vp8_files
+}
+
+#[allow(dead_code)]
+/// These files come from the WebM test streams and are grouped according to
+/// (rounded down) level, i.e. "group1" consists of level 1 and 1.1 streams,
+/// "group2" of level 2 and 2.1, etc. This helps to keep together tests with
+/// similar amounts of intended behavior/expected stress on devices.
+pub fn vp9_webm_files(
+) -> HashMap<&'static str, HashMap<&'static str, HashMap<&'static str, Vec<&'static str>>>> {
+    let mut vp9_webm_files = HashMap::new();
+    // --- Profile 0 ---
+    let mut profile_0_groups = HashMap::new();
+    // Profile 0 - Group 1
+    let mut p0_group1 = HashMap::new();
+    p0_group1.insert(
+        "buf",
+        vec![
+            "test_vectors/vp9/Profile_0_8bit/buf/crowd_run_256X144_fr15_bd8_8buf_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/buf/grass_1_256X144_fr15_bd8_8buf_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/buf/street1_1_256X144_fr15_bd8_8buf_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/buf/crowd_run_384X192_fr30_bd8_8buf_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/buf/grass_1_384X192_fr30_bd8_8buf_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/buf/street1_1_384X192_fr30_bd8_8buf_l11.ivf",
+        ],
+    );
+    p0_group1.insert(
+        "frm_resize",
+        vec![
+            "test_vectors/vp9/Profile_0_8bit/frm_resize/crowd_run_256X144_fr15_bd8_frm_resize_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/frm_resize/grass_1_256X144_fr15_bd8_frm_resize_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/frm_resize/street1_1_256X144_fr15_bd8_frm_resize_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/frm_resize/crowd_run_384X192_fr30_bd8_frm_resize_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/frm_resize/grass_1_384X192_fr30_bd8_frm_resize_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/frm_resize/street1_1_384X192_fr30_bd8_frm_resize_l11.ivf",
+        ],
+    );
+    p0_group1.insert(
+        "gf_dist",
+        vec![
+            "test_vectors/vp9/Profile_0_8bit/gf_dist/crowd_run_256X144_fr15_bd8_gf_dist_4_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/gf_dist/grass_1_256X144_fr15_bd8_gf_dist_4_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/gf_dist/street1_1_256X144_fr15_bd8_gf_dist_4_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/gf_dist/crowd_run_384X192_fr30_bd8_gf_dist_4_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/gf_dist/grass_1_384X192_fr30_bd8_gf_dist_4_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/gf_dist/street1_1_384X192_fr30_bd8_gf_dist_4_l11.ivf",
+        ],
+    );
+    p0_group1.insert(
+        "odd_size",
+        vec![
+            "test_vectors/vp9/Profile_0_8bit/odd_size/crowd_run_248X144_fr15_bd8_odd_size_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/odd_size/grass_1_248X144_fr15_bd8_odd_size_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/odd_size/street1_1_248X144_fr15_bd8_odd_size_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/odd_size/crowd_run_376X184_fr30_bd8_odd_size_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/odd_size/grass_1_376X184_fr30_bd8_odd_size_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/odd_size/street1_1_376X184_fr30_bd8_odd_size_l11.ivf",
+        ],
+    );
+    p0_group1.insert(
+        "sub8x8",
+        vec![
+            "test_vectors/vp9/Profile_0_8bit/sub8X8/crowd_run_256X144_fr15_bd8_sub8X8_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/sub8X8/grass_1_256X144_fr15_bd8_sub8X8_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/sub8X8/street1_1_256X144_fr15_bd8_sub8X8_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/sub8X8/crowd_run_384X192_fr30_bd8_sub8X8_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/sub8X8/grass_1_384X192_fr30_bd8_sub8X8_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/sub8X8/street1_1_384X192_fr30_bd8_sub8X8_l11.ivf",
+        ],
+    );
+    p0_group1.insert(
+        "sub8x8_sf",
+        vec![
+            "test_vectors/vp9/Profile_0_8bit/sub8x8_sf/crowd_run_256X144_fr15_bd8_sub8x8_sf_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/sub8x8_sf/grass_1_256X144_fr15_bd8_sub8x8_sf_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/sub8x8_sf/street1_1_256X144_fr15_bd8_sub8x8_sf_l1.ivf",
+            "test_vectors/vp9/Profile_0_8bit/sub8x8_sf/crowd_run_384X192_fr30_bd8_sub8x8_sf_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/sub8x8_sf/grass_1_384X192_fr30_bd8_sub8x8_sf_l11.ivf",
+            "test_vectors/vp9/Profile_0_8bit/sub8x8_sf/street1_1_384X192_fr30_bd8_sub8x8_sf_l11.ivf",
+        ],
+    );
+    profile_0_groups.insert("group1", p0_group1);
+    vp9_webm_files.insert("profile_0", profile_0_groups);
+    vp9_webm_files
 }
