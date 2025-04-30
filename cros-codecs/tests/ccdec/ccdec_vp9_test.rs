aa3aebfdd1dc2f5de7d75aeb644d840ca0a50906
Hang Nguyen <hnt@google.com>
2025-04-29 19:21:56 -0700

    cros-codecs: Update tests to include VP9 groups 2, 3, 4 and levels 5.0 and 5.1
    This covers the verification tests for the VP9 test vectors by including
    the groups 2, 3, 4 and levels 5.0 and 5.1.
    
    BUG=b:400788075
    TEST=atest ccdec_vp9_test -- --enable-module-dynamic-download
    
    Change-Id: I4fb89028c6ea3eb2bcc31536196648e34ba4f982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6599627
    Reviewed-by: Hang Nguyen <hnt@google.com>
    Auto-Submit: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Commit-Queue: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Tested-by: Bijan Choobineh‎ <bchoobineh@chromium.org>

--- cros-codecs/tests/ccdec/ccdec_vp9_test.rs
+++ cros-codecs/tests/ccdec/ccdec_vp9_test.rs
@@ -80,4 +80,382 @@ mod tests {
             .expect("No test vectors found for sub8x8_sf in profile_0/group1");
         run_ccdec_test_by_codec_group(filenames, "vp9");
     }
+
+    #[test]
+    fn profile_0_group2_buf() {
+        let filenames: &Vec<&'static str> = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group2")
+            .expect("No test vectors found for group2 in profile_0")
+            .get("buf")
+            .expect("No test vectors found for buf in profile_0/group2");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_group2_frm_resize() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group2")
+            .expect("No test vectors found for group2 in profile_0")
+            .get("frm_resize")
+            .expect("No test vectors found for frm_resize in profile_0/group2");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group2_gf_dist() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group2")
+            .expect("No test vectors found for group2 in profile_0")
+            .get("gf_dist")
+            .expect("No test vectors found for gf_dist in profile_0/group2");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group2_odd_size() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group2")
+            .expect("No test vectors found for group2 in profile_0")
+            .get("odd_size")
+            .expect("No test vectors found for odd_size in profile_0/group2");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group2_sub8x8() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group2")
+            .expect("No test vectors found for group2 in profile_0")
+            .get("sub8x8")
+            .expect("No test vectors found for sub8x8 in profile_0/group2");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_group2_sub8x8_sf() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group2")
+            .expect("No test vectors found for group2 in profile_0")
+            .get("sub8x8_sf")
+            .expect("No test vectors found for sub8x8_sf in profile_0/group2");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group3_buf() {
+        let filenames: &Vec<&'static str> = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group3")
+            .expect("No test vectors found for group3 in profile_0")
+            .get("buf")
+            .expect("No test vectors found for buf in profile_0/group3");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_group3_frm_resize() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group3")
+            .expect("No test vectors found for group3 in profile_0")
+            .get("frm_resize")
+            .expect("No test vectors found for frm_resize in profile_0/group3");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group3_gf_dist() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group3")
+            .expect("No test vectors found for group3 in profile_0")
+            .get("gf_dist")
+            .expect("No test vectors found for gf_dist in profile_0/group3");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group3_odd_size() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group3")
+            .expect("No test vectors found for group3 in profile_0")
+            .get("odd_size")
+            .expect("No test vectors found for odd_size in profile_0/group3");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group3_sub8x8() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group3")
+            .expect("No test vectors found for group3 in profile_0")
+            .get("sub8x8")
+            .expect("No test vectors found for sub8x8 in profile_0/group3");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_group3_sub8x8_sf() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group3")
+            .expect("No test vectors found for group3 in profile_0")
+            .get("sub8x8_sf")
+            .expect("No test vectors found for sub8x8_sf in profile_0/group3");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group4_buf() {
+        let filenames: &Vec<&'static str> = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group4")
+            .expect("No test vectors found for group4 in profile_0")
+            .get("buf")
+            .expect("No test vectors found for buf in profile_0/group4");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_group4_frm_resize() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group4")
+            .expect("No test vectors found for group4 in profile_0")
+            .get("frm_resize")
+            .expect("No test vectors found for frm_resize in profile_0/group4");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group4_gf_dist() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group4")
+            .expect("No test vectors found for group4 in profile_0")
+            .get("gf_dist")
+            .expect("No test vectors found for gf_dist in profile_0/group4");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group4_odd_size() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group4")
+            .expect("No test vectors found for group4 in profile_0")
+            .get("odd_size")
+            .expect("No test vectors found for odd_size in profile_0/group4");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_group4_sub8x8() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group4")
+            .expect("No test vectors found for group4 in profile_0")
+            .get("sub8x8")
+            .expect("No test vectors found for sub8x8 in profile_0/group4");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_group4_sub8x8_sf() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("group4")
+            .expect("No test vectors found for group4 in profile_0")
+            .get("sub8x8_sf")
+            .expect("No test vectors found for sub8x8_sf in profile_0/group4");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_level5_0_buf() {
+        let filenames: &Vec<&'static str> = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_0")
+            .expect("No test vectors found for level5_0 in profile_0")
+            .get("buf")
+            .expect("No test vectors found for buf in profile_0/level5_0");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_level5_0_frm_resize() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_0")
+            .expect("No test vectors found for level5_0 in profile_0")
+            .get("frm_resize")
+            .expect("No test vectors found for frm_resize in profile_0/level5_0");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_level5_0_gf_dist() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_0")
+            .expect("No test vectors found for level5_0 in profile_0")
+            .get("gf_dist")
+            .expect("No test vectors found for gf_dist in profile_0/level5_0");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_level5_0_odd_size() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_0")
+            .expect("No test vectors found for level5_0 in profile_0")
+            .get("odd_size")
+            .expect("No test vectors found for odd_size in profile_0/level5_0");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_level5_0_sub8x8() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_0")
+            .expect("No test vectors found for level5_0 in profile_0")
+            .get("sub8x8")
+            .expect("No test vectors found for sub8x8 in profile_0/level5_0");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_level5_0_sub8x8_sf() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_0")
+            .expect("No test vectors found for level5_0 in profile_0")
+            .get("sub8x8_sf")
+            .expect("No test vectors found for sub8x8_sf in profile_0/level5_0");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_level5_1_buf() {
+        let filenames: &Vec<&'static str> = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_1")
+            .expect("No test vectors found for level5_1 in profile_0")
+            .get("buf")
+            .expect("No test vectors found for buf in profile_0/level5_1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_level5_1_frm_resize() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_1")
+            .expect("No test vectors found for level5_1 in profile_0")
+            .get("frm_resize")
+            .expect("No test vectors found for frm_resize in profile_0/level5_1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_level5_1_gf_dist() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_1")
+            .expect("No test vectors found for level5_1 in profile_0")
+            .get("gf_dist")
+            .expect("No test vectors found for gf_dist in profile_0/level5_1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_level5_1_odd_size() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_1")
+            .expect("No test vectors found for level5_1 in profile_0")
+            .get("odd_size")
+            .expect("No test vectors found for odd_size in profile_0/level5_1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    fn profile_0_level5_1_sub8x8() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_1")
+            .expect("No test vectors found for level5_1 in profile_0")
+            .get("sub8x8")
+            .expect("No test vectors found for sub8x8 in profile_0/level5_1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn profile_0_level5_1_sub8x8_sf() {
+        let filenames = VP9_WEBM_FILES
+            .get("profile_0")
+            .expect("No test vectors found for profile_0")
+            .get("level5_1")
+            .expect("No test vectors found for level5_1 in profile_0")
+            .get("sub8x8_sf")
+            .expect("No test vectors found for sub8x8_sf in profile_0/level5_1");
+        run_ccdec_test_by_codec_group(filenames, "vp9");
+    }
+
+    // TODO(400788075): Add tests with files from bugs
 }
