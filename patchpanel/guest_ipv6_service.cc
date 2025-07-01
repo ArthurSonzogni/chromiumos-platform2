8f5deb4f2c59c7f1abd8521ad0cb237674de61e5
Nathan Muggli <nmuggli@google.com>
2025-07-01 17:00:45 -0600

    patchpanel: Add header for NOTIMPLEMENTED
    In CL:6657459, notimplemented.h was removed from notreached.h.  Update
    any includes that were depending on this to prepare for this change.
    
    BUG=b:427844205
    TEST=emerge patchpanel, CQ
    
    Change-Id: Ic9c363e05b3fa70f062014624741b402e1719a04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6695720
    Tested-by: Nathan Muggli <nmuggli@google.com>
    Commit-Queue: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Auto-Submit: Nathan Muggli <nmuggli@google.com>

--- patchpanel/guest_ipv6_service.cc
+++ patchpanel/guest_ipv6_service.cc
@@ -21,6 +21,7 @@
 
 #include <base/files/file_util.h>
 #include <base/logging.h>
+#include <base/notimplemented.h>
 #include <base/notreached.h>
 #include <base/strings/string_number_conversions.h>
 #include <base/strings/string_util.h>
