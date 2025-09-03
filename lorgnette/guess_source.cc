03a45067dee5a48d37b1592f102b3178d07470f0
Aaron Massey <aaronmassey@google.com>
2025-09-03 21:14:26 +0000

    lorgnette: Add "duplex" and "simplex" to GuessSourceType
    Some scanners or drivers may report "ADF Duplex: and "ADF Simplex"
    sources without the "ADF" prefix. The GuessSourceType routine
    currently reports these as Default source type because "simplex" and
    "duplex" are unrecognized.
    
    Add support for ADF scanners/drivers that report their sources as
    "duplex" and "simplex".
    
    BUG=b:374798059
    TEST=emerge deploy test canon and ricoh scanners
    
    Change-Id: I762c21efc73937338c4bdf1008321042b7f1118d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/6913038
    Commit-Queue: Aaron Massey <aaronmassey@google.com>
    Tested-by: Aaron Massey <aaronmassey@google.com>
    Reviewed-by: Nathan Muggli <nmuggli@google.com>

--- lorgnette/guess_source.cc
+++ lorgnette/guess_source.cc
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "lorgnette/guess_source.h"
+
 #include <base/strings/string_util.h>
 #include <chromeos/dbus/service_constants.h>
-
-#include "lorgnette/guess_source.h"
 
 lorgnette::SourceType GuessSourceType(const std::string& name) {
   std::string lowercase = base::ToLowerASCII(name);
@@ -14,12 +14,15 @@ lorgnette::SourceType GuessSourceType(const std::string& name) {
     return lorgnette::SOURCE_PLATEN;
   }
 
+  // Simplex & duplex always refer to ADF. We have not discovered a
+  // case of "platen simplex" or "platen duplex".
   if (lowercase == "adf" || lowercase == "adf front" ||
-      lowercase == "adf simplex" || lowercase == "automatic document feeder") {
+      lowercase == "adf simplex" || lowercase == "automatic document feeder" ||
+      lowercase == "simplex") {
     return lorgnette::SOURCE_ADF_SIMPLEX;
   }
 
-  if (lowercase == "adf duplex") {
+  if (lowercase == "adf duplex" || lowercase == "duplex") {
     return lorgnette::SOURCE_ADF_DUPLEX;
   }
 
