94200c78dbf96a28bc2792da0240dc4c0cf29ffb
Hirokazu Honda <hiroh@google.com>
2025-10-08 12:41:53 -0700

    hwdrm-videoproc-ta: Remove deprecated H264 slice header structs
    The deprecated slice header and stream data structs are no longer
    needed as the client has been updated. This removes the code for the
    backward-compatibility.
    
    BUG=b:449845359
    TEST=video.PlayDRM.*
    
    Cq-Depend: chrome-internal:8652192
    Change-Id: Id8c4172d168327f22695142d5aa3013295996da2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/7021616
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Tested-by: Hirokazu Honda <hiroh@chromium.org>

--- hwdrm-videoproc-ta/videoproc_ta_service.c
+++ hwdrm-videoproc-ta/videoproc_ta_service.c
@@ -19,77 +19,6 @@
 #define TZCMD_TA_MEM_MAP (TZCMD_TA_MEM_FIRST_CMD + 3)
 #define FLAG_TA_MAP_CACHED (1 << 2)
 
-// TODO(hiroh): Remove these functions once Chromium side is upreved.
-static void FillStreamDataFromDeprecated(
-    struct StreamDataForSliceHeader* out,
-    const struct StreamDataForSliceHeaderDeprecated* in) {
-#define IN2OUT(x)   \
-  do {              \
-    out->x = in->x; \
-  } while (0)
-
-  memset(out, 0, sizeof(struct StreamDataForSliceHeader));
-  out->version = 0;
-  IN2OUT(log2_max_frame_num_minus4);
-  IN2OUT(log2_max_pic_order_cnt_lsb_minus4);
-  IN2OUT(pic_order_cnt_type);
-  IN2OUT(num_ref_idx_l0_default_active_minus1);
-  IN2OUT(num_ref_idx_l1_default_active_minus1);
-  IN2OUT(weighted_bipred_idc);
-  IN2OUT(chroma_array_type);
-  IN2OUT(frame_mbs_only_flag);
-  IN2OUT(bottom_field_pic_order_in_frame_present_flag);
-  IN2OUT(delta_pic_order_always_zero_flag);
-  IN2OUT(redundant_pic_cnt_present_flag);
-  IN2OUT(weighted_pred_flag);
-#undef IN2OUT
-}
-
-static void FillSliceHeaderDeprecated(struct H264SliceHeaderDataDeprecated* out,
-                                      const struct H264SliceHeaderData* in) {
-#define IN2OUT(x)   \
-  do {              \
-    out->x = in->x; \
-  } while (0)
-
-  memset(out, 0, sizeof(struct H264SliceHeaderDataDeprecated));
-
-  IN2OUT(nal_ref_idc);
-  IN2OUT(idr_pic_flag);
-  IN2OUT(slice_type);
-  IN2OUT(field_pic_flag);
-  IN2OUT(frame_num);
-  IN2OUT(idr_pic_id);
-  IN2OUT(pic_order_cnt_lsb);
-  IN2OUT(delta_pic_order_cnt_bottom);
-  IN2OUT(delta_pic_order_cnt0);
-  IN2OUT(delta_pic_order_cnt1);
-  out->ref_pic_fields.bits.no_output_of_prior_pics_flag =
-      in->no_output_of_prior_pics_flag;
-  out->ref_pic_fields.bits.long_term_reference_flag =
-      in->long_term_reference_flag;
-  out->ref_pic_fields.bits.adaptive_ref_pic_marking_mode_flag =
-      in->adaptive_ref_pic_marking_mode_flag;
-  out->ref_pic_fields.bits.dec_ref_pic_marking_count =
-      in->dec_ref_pic_marking_size;
-
-  for (size_t i = 0; i < in->dec_ref_pic_marking_size; ++i) {
-    out->memory_management_control_operation[i] =
-        in->dec_ref_pic_marking[i].memory_management_control_operation;
-    out->difference_of_pic_nums_minus1[i] =
-        in->dec_ref_pic_marking[i].difference_of_pic_nums_minus1;
-    out->long_term_pic_num[i] = in->dec_ref_pic_marking[i].long_term_pic_num;
-    out->max_long_term_frame_idx_plus1[i] =
-        in->dec_ref_pic_marking[i].max_long_term_frame_idx_plus1;
-    out->long_term_frame_idx[i] =
-        in->dec_ref_pic_marking[i].long_term_frame_idx;
-  }
-
-  IN2OUT(dec_ref_pic_marking_bit_size);
-  IN2OUT(pic_order_cnt_bit_size);
-#undef IN2OUT
-}
-
 TEE_Result ParseH264SliceHeader(uint32_t param_types,
                                 TEE_Param params[TEE_NUM_PARAMS]) {
   uint32_t ptypes = TEE_PARAM_TYPES(
@@ -139,14 +68,11 @@ TEE_Result ParseH264SliceHeader(uint32_t param_types,
     goto out;
   }
   // Verify the size of the input/output structs.
-  if (params[2].memref.size !=
-          sizeof(struct StreamDataForSliceHeaderDeprecated) &&
-      params[2].memref.size != sizeof(struct StreamDataForSliceHeader)) {
+  if (params[2].memref.size != sizeof(struct StreamDataForSliceHeader)) {
     res = TEE_ERROR_BAD_PARAMETERS;
     goto out;
   }
-  if (params[3].memref.size != sizeof(struct H264SliceHeaderDataDeprecated) &&
-      params[3].memref.size != sizeof(struct H264SliceHeaderData)) {
+  if (params[3].memref.size != sizeof(struct H264SliceHeaderData)) {
     res = TEE_ERROR_BAD_PARAMETERS;
     goto out;
   }
@@ -170,14 +96,6 @@ TEE_Result ParseH264SliceHeader(uint32_t param_types,
 
   struct H264SliceHeaderData slice_hdr;
   struct StreamDataForSliceHeader* stream_data = params[2].memref.buffer;
-  struct StreamDataForSliceHeader stream_data_in_stack;
-  if (params[2].memref.size ==
-      sizeof(struct StreamDataForSliceHeaderDeprecated)) {
-    FillStreamDataFromDeprecated(
-        &stream_data_in_stack,
-        (struct StreamDataForSliceHeaderDeprecated*)params[2].memref.buffer);
-    stream_data = &stream_data_in_stack;
-  }
   if (!ParseSliceHeader(((uint8_t*)in_addr) + params[1].value.a,
                         in_size - params[1].value.a, stream_data, &slice_hdr)) {
     EMSG("ParseSliceHeader failed");
@@ -185,15 +103,9 @@ TEE_Result ParseH264SliceHeader(uint32_t param_types,
     goto out;
   }
   // Copy the slice header to the memref so it gets sent back.
-  if (params[3].memref.size == sizeof(struct H264SliceHeaderDataDeprecated)) {
-    struct H264SliceHeaderDataDeprecated* slice_hdr_out =
-        params[3].memref.buffer;
-    FillSliceHeaderDeprecated(slice_hdr_out, &slice_hdr);
-    params[3].memref.size = sizeof(struct H264SliceHeaderDataDeprecated);
-  } else {
-    memcpy(params[3].memref.buffer, &slice_hdr, sizeof(slice_hdr));
-    params[3].memref.size = sizeof(slice_hdr);
-  }
+  memcpy(params[3].memref.buffer, &slice_hdr, sizeof(slice_hdr));
+  params[3].memref.size = sizeof(slice_hdr);
+
   res = TEE_SUCCESS;
 
 out:
