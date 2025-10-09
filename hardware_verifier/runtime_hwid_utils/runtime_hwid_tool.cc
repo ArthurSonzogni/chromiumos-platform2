e3348de6cdd030aa33eb6859367bbebb0f0ad758
Allen Shih <allenshihmc@google.com>
2025-10-09 05:38:52 +0000

    hardware_verifier: Add runtime_hwid_tool
    Add runtime_hwid_tool executable for getting Runtime HWID.
    
    BUG=b:445568895
    TEST=FEATURES=test emerge-${BOARD} runtime_hwid_utils
    TEST=(on DUT) runtime_hwid_tool get
    
    Change-Id: Ic7bab9c3c44a1f2f27d0678a83124984c0a452d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/7024922
    Reviewed-by: Yu-An Wang <wyuang@google.com>
    Commit-Queue: Allen Shih <allenshihmc@chromium.org>
    Tested-by: Allen Shih <allenshihmc@chromium.org>

---
+++ hardware_verifier/runtime_hwid_utils/runtime_hwid_tool.cc
@@ -0,0 +1,57 @@
+// Copyright 2025 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <iostream>
+
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
+#include <base/logging.h>
+#include <brillo/flag_helper.h>
+#include <brillo/syslog_logging.h>
+#include <hardware_verifier/runtime_hwid_utils/runtime_hwid_utils_impl.h>
+
+namespace {
+
+constexpr char kInfoText[] =
+    "ChromeOS Runtime HWID Tool.\n\n"
+    "This tool is used to manage the Runtime HWID on the device.\n\n"
+    "Available Commands:\n"
+    "  get   - Gets the Runtime HWID.\n";
+constexpr char kGetAction[] = "get";
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  brillo::InitLog(brillo::kLogToStderr);
+
+  DEFINE_int32(verbosity, 0,
+               "Verbosity level, range from 0 to 5.  The greater number is "
+               "set, the more detail messages will be printed.");
+
+  brillo::FlagHelper::Init(argc, argv, kInfoText);
+
+  logging::SetMinLogLevel(-FLAGS_verbosity);
+
+  const auto* cl = base::CommandLine::ForCurrentProcess();
+  const auto args = cl->GetArgs();
+  if (args.size() != 1) {
+    LOG(ERROR) << "Too many command line arguments. Use --help for the usage.";
+    return EXIT_FAILURE;
+  }
+  if (args[0] != kGetAction) {
+    LOG(ERROR) << "Unknown command line arguments. Use --help for the usage.";
+    return EXIT_FAILURE;
+  }
+
+  hardware_verifier::RuntimeHWIDUtilsImpl runtime_hwid_utils;
+  const auto runtime_hwid = runtime_hwid_utils.GetRuntimeHWID();
+  if (!runtime_hwid.has_value()) {
+    LOG(ERROR) << "Failed to get Runtime HWID.";
+    return EXIT_FAILURE;
+  }
+
+  std::cout << *runtime_hwid << std::endl;
+
+  return EXIT_SUCCESS;
+}
