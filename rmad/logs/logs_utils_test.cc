181a7e81824581ea23aba43357153b83b484f9ab
Nathan Muggli <nmuggli@google.com>
2026-03-11 21:00:07 -0600

    rmad: Remove unused headers
    This will allow libchrome to remove a patch containing this header.
    
    BUG=b:477205087
    TEST=CQ
    
    Change-Id: I8434eab444b1e9fd0bb379229c1a20858d9c42f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/7659151
    Commit-Queue: Kevin Lin <kevinptt@chromium.org>
    Tested-by: Kevin Lin <kevinptt@chromium.org>
    Reviewed-by: Kevin Lin <kevinptt@chromium.org>

--- rmad/logs/logs_utils_test.cc
+++ rmad/logs/logs_utils_test.cc
@@ -8,7 +8,6 @@
 #include <utility>
 #include <vector>
 
-#include <base/containers/contains.h>
 #include <base/files/file_util.h>
 #include <base/files/scoped_temp_dir.h>
 #include <base/json/json_writer.h>
