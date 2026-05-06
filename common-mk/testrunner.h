669f19b5abc646b006b4084258fa30c6ff8eceb3
Nathan Muggli <nmuggli@google.com>
2026-05-06 12:23:38 -0600

    common-mk: Update logging include files
    Some of the logging framework has moved from base/logging.h to
    base/logging/logging_settings.h.  Update the appropriate includes.
    
    BUG=b:465651491
    TEST=CQ
    
    Change-Id: If64fe55f3699b75847745c0a311a9b688d1adc67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/7822272
    Tested-by: Nathan Muggli <nmuggli@google.com>
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>
    Auto-Submit: Nathan Muggli <nmuggli@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

--- common-mk/testrunner.h
+++ common-mk/testrunner.h
@@ -10,6 +10,7 @@
 #include <base/at_exit.h>
 #include <base/command_line.h>
 #include <base/logging.h>
+#include <base/logging/logging_settings.h>
 #include <base/test/test_timeouts.h>
 #include <brillo/syslog_logging.h>
 
