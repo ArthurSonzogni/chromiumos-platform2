0e09dcc3bae0b5158496be8f64467dc7a8473180
Nathan Muggli <nmuggli@google.com>
2026-05-06 12:23:49 -0600

    screen-capture-utils: Update logging include files
    Some of the logging framework has moved from base/logging.h to
    base/logging/logging_settings.h.  Update the appropriate includes.
    
    BUG=b:465651491
    TEST=CQ
    
    Change-Id: I6e4e1be0b15fee6433598d4c5c1cbbefd0154140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/7822138
    Reviewed-by: Andres Calderon Jaramillo <andrescj@google.com>
    Tested-by: Nathan Muggli <nmuggli@google.com>
    Auto-Submit: Nathan Muggli <nmuggli@google.com>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@google.com>

--- screen-capture-utils/kmsvnc.cc
+++ screen-capture-utils/kmsvnc.cc
@@ -11,6 +11,7 @@
 #include <base/check.h>
 #include <base/command_line.h>
 #include <base/logging.h>
+#include <base/logging/logging_settings.h>
 #include <base/strings/string_number_conversions.h>
 #include <rfb/rfb.h>
 
