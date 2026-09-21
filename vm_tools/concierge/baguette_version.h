b11e89ca9ca4648335ede4d9b777a65eefd8d4c7
crosvm-luci-ci-builder <crosvm-luci-ci-builder@crosvm-infra.iam.gserviceaccount.com>
2026-09-21 05:00:34 -0700

    vm_tools: Uprev baguette_image version
    Generated by https://cr-buildbucket.appspot.com/build/8670051057404030049.
    
    Change-Id: I4bac84fe618c43d69eebdacdb7fd7fa619d61b36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/8434266
    Commit-Queue: chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>
    Bot-Commit: crosvm LUCI CI <crosvm-luci-ci-builder@crosvm-infra.iam.gserviceaccount.com>

--- vm_tools/concierge/baguette_version.h
+++ vm_tools/concierge/baguette_version.h
@@ -11,9 +11,9 @@
 // URL when new images are available.
 
 // clang-format off
-constexpr char kBaguetteVersion[] = "2026-09-20-000110_5a2009835642e9a0e198e0094cfae9537515386b";  // NOLINT
-constexpr char kBaguetteSHA256X86[] = "0f53e7de4400f1f594f15d2e7e30e5222d5d8c4064469d664dc8218f0996717b";  // NOLINT
-constexpr char kBaguetteSHA256Arm[] = "8dad2224f56897f48ca857343799f6365684081e72e111942e852902ec64aa39";  // NOLINT
+constexpr char kBaguetteVersion[] = "2026-09-21-000112_b1242cfba1a9b8d40e19513a59f191e29e443fcf";  // NOLINT
+constexpr char kBaguetteSHA256X86[] = "53f37f8df8c243d393a5ab002635186a3b5de3486660bd4b359e6d47bbcfa21d";  // NOLINT
+constexpr char kBaguetteSHA256Arm[] = "0b5ddaebc9ad40a623afd94556add4037154b344bda941647ad6cbde8269f305";  // NOLINT
 // cpplint:enable
 
 #endif  // VM_TOOLS_CONCIERGE_BAGUETTE_VERSION_H_
